3f119a4a90915ba2b17b09e5ffe00cda1d1424d8
Sam Joseph <tansaku@gmail.com>
2014-11-21 09:53:31 +0000

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+source 'https://rubygems.org'
+ruby '2.2.1'
+
+ruby '3.0.2'
+
+group :test do
+  gem 'rspec'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
+end
+
+group :development, :test do
+  gem 'rubocop', '1.20'
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
+end
