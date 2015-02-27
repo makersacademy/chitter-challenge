8ac2ec201f568ab1b5027002db14a0f3250654af
SebastienPires <pires.sebastien@gmail.com>
2015-02-27 18:11:11 +0000

    Added functionality to check if email is duplicated

--- app/server.rb
+++ app/server.rb
@@ -43,7 +43,7 @@ post '/users' do
       session[:user_id] = @user.id
       redirect to('/')
     else
-      flash[:notice] = "Sorry, your passwords don't match"
+      flash.now[:errors] = @user.errors.full_messages
       erb :"users/new"
     end
 end
