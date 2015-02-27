1c65ba1229dba89e499a932d72fa15cce0064efc
Kate <katebeavis84@gmail.com>
2015-02-27 15:43:16 +0000

    Added db cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,29 @@
-ENV['RACK_ENV'] = 'test'
+ENV["RACK_ENV"] = 'test'
 
-require 'server'
+require './lib/server' 
+require 'database_cleaner'
+require 'capybara/rspec'
+
+Capybara.app = Chitter
 
 RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
 
-  config.expect_with :rspec do |expectations|
+  config.order = 'random'
 
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.mock_with :rspec do |mocks|
- 
-    mocks.verify_partial_doubles = true
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 
 end
