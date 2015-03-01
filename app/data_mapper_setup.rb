1d6be78183c03b00fa2dee49c8b80fb606a45821
Tom Coakes <tomcoakes@gmail.com>
2015-03-01 22:00:42 +0000

    Experimenting with DATABASE_URL

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,5 +1,5 @@
   env = ENV['RACK_ENV'] || 'development'
-  DataMapper.setup(:default, "postgres://tjazqcxmtuqzvm:zflznKcp6tP24pk-74jgtA0jMM@ec2-107-21-93-97.compute-1.amazonaws.com:5432/d9csv0cgk3q1uq")
+  DataMapper.setup(:default, DATABASE_URL)
   require './app/models/user'
   require './app/models/cheep'
   DataMapper.finalize
