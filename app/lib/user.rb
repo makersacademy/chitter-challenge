cd55540e54a59b35e98525c17fb0eb344ee83318
Sean Haughton <seanphaughton@gmail.com>
2015-03-01 18:27:33 +0000

    Removed empty file

---
+++ app/lib/user.rb
@@ -0,0 +1,17 @@
+require 'bcrypt'
+
+class User
+
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :email, String
+  property :name, String
+  property :username, String
+  property :password_digest, Text
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+end
