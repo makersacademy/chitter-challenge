b140ac75c3ff855b3f36c71106c679ec6fb29708
guidovitafinzi <guido.vitafinzi@gmail.com>
2015-03-01 21:41:00 +0000

    Added user management

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,26 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, :unique => true, :message => "This email is already taken"
   property :password_digest, Text
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
+  validates_uniqueness_of :email
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  def self.authenticate(email, password)
+    user = first(:email => email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
+  end
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,20 +1,69 @@
 require 'spec_helper'
+require_relative '../../app/views/helpers/sessions'
+
+include SessionHelpers
+
+feature "User signs in" do
+
+  before(:each) do
+    User.create(:email => "guido@gmail.com",
+                :password => 'guido',
+                :password_confirmation => 'guido')
+  end
+
+  scenario "with correct credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, guido@gmail.com")
+    sign_in('guido@gmail.com', 'guido')
+    expect(page).to have_content("Welcome, guido@gmail.com")
+  end
+
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, guido@gmail.com")
+    sign_in('guido@gmail.com', 'wrong')
+    expect(page).not_to have_content("Welcome, guido@gmail.com")
+  end
+end
+
+feature 'User signs out' do
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in('test@test.com', 'test')
+    click_button "Sign out"
+    # expect(page).to have_content("Good bye!")
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+end
+
 
 feature "User signs up" do
-
-scenario "when being a new user visiting the site" do
+  scenario "when being a new user visiting the site" do
     expect{ sign_up }.to change(User, :count).by(1)
     expect(page).to have_content("Welcome, guido@gmail.com")
     expect(User.first.email).to eq("guido@gmail.com")
   end
 
-  def sign_up(email = "guido@gmail.com",
-              password = "guido")
-    visit '/users/new'
-    expect(page.status_code).to eq(200)
-    fill_in :email, :with => email
-    fill_in :password, :with => password
-    click_button "Sign up"
+  scenario "with a password that doesn't match" do
+    expect{ sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content("Sorry, your passwords don't match")
   end
 
+  scenario "with an email that is already registered" do
+    expect{ sign_up }.to change(User, :count).by(1)
+    expect{ sign_up }.to change(User, :count).by(0)
+    expect(page).to have_content("This email is already taken")
+  end
 end
+
+
+
+
+
