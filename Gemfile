75c66ea830efb2fb13e96b99a66c746153435ddf
Tom Coakes <tomcoakes@gmail.com>
2015-03-01 15:45:22 +0000

    Passwords now encrypted, using the bcrypt ruby gem

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'bcrypt'
 
 gem 'rspec'
 gem 'cucumber'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,25 @@
+require 'bcrypt'
+
 class User
   include DataMapper::Resource
 
-  property :id,         Serial
-  property :name,       String
-  property :username,   String, unique: true
-  property :email,      String, unique: true
-  property :password,   String
+  property :id,                 Serial
+  property :name,               String
+  property :username,           String, unique: true
+  property :email,              String, unique: true
+  property :password_digest,    Text
 
   has n, :cheeps
+
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
   def self.authenticate(email, password)
     user = first(email: email)
     if user
-      user.password == password ? user : false
+      BCrypt::Password.new(user.password_digest) == password ? user : false
     else
       nil
     end
