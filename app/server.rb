136f765102fe2a5b485276d03803a3426da4a6cb
SebastienPires <pires.sebastien@gmail.com>
2015-03-01 14:49:01 +0000

    Design is nearly finished. Only few tweaks needed now

--- app/server.rb
+++ app/server.rb
@@ -21,7 +21,7 @@ end
 post '/tweets' do
   message = params["message"]
   user = params["user"]
-  Tweet.create(:message => message, :user => user)
+  Tweet.create(:message => message, :user => current_user.name)
   redirect to('/')
 end
 
