3893d4f9e099602daddaf46610dccaa91836c1b0
Alex Parkinson <aporkinson@gmail.com>
2015-03-01 21:30:28 +0000

    User can sign in

--- lib/user.rb
+++ lib/user.rb
@@ -8,11 +8,20 @@ class User
   validates_confirmation_of :password
 
   property :id, Serial
-  property :email, String
+  property :email, String, :unique => true, :message => "This email is already taken"
   property :password_digest, Text
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = first(:email => email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
   end
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -22,6 +22,13 @@ feature "User signs up" do
   scenario "with a password that doesn't match" do
     expect{ sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
     expect(current_path).to eq('/users')
-    expect(page).to have_content("Sorry, your passwords don't match")
+    expect(page).to have_content("Password does not match the confirmation")
   end
+
+  scenario "with an email that is already registered" do
+    expect{ sign_up }.to change(User, :count).by(1)
+    expect{ sign_up }.to change(User, :count).by(0)
+    expect(page).to have_content("This email is already taken")
+  end
+
 end
