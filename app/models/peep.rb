6fb5af0058435b4f487705eb335754c042c514b6
Chris Ward <chriswarddev@gmail.com>
2015-03-02 13:08:53 +0000

    changed display of peeps

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -11,7 +11,7 @@ class Peep
     text = self.created_at.to_s
     months = { "01" => "Jan", "02" => "Feb", "03" => "March", "04" => "April", "05" => "May", "06" => "June", "07" => "July", "08" => "Aug", "09" => "Sept", "10" => "Oct", "11" => "Nov", "12" => "Dec"}
     month = months[text[5..6]]
-    display = "#{month} #{text[8,2]} : #{text[11,5]}"
+    display = "#{month} #{text[8,2]} at #{text[11,5]}"
   end
 
 end
