8963878480a3e63afd74336c5b1671c5bf8a1ca0
Erik Griffin <griffin.erik@gmail.com>
2015-03-02 17:18:21 +0000

    Fixed sessions not being part of RSpec

--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,8 @@
 require 'hobbit'
 require 'hobbit/contrib'
 require 'data_mapper'
+require 'rack/protection'
+
 
 require_relative 'custom-hobbit'
 require_relative 'data_mapper_setup'
@@ -8,20 +10,19 @@ require_relative 'data_mapper_setup'
 class Server < Hobbit::Base
   include Hobbit::Mote
   include Hobbit::Session
+  use Rack::Session::Cookie, secret: SecureRandom.hex(64)
+  use Rack::Protection
 
   # Currently having a problem, curious how .erb is different.
   # In my index file, if I haven't passed in the param of a
   # variable I'm using, it throws an undefined local variable message,
   # instead of that variable just being nil.
-  # Does erb allow this? I don't remember.
+  # ERB will just treat those undeclared variables as nil
   # Current workaround means that for every page render, I have to
   # pass in all used local variables, and give them a nil value if unused.
 
-  # However I suppose I would eventually always be passing in the user
-  # from the session...
-
   get '/' do
-    render 'index', peeps: Peep.all, user: nil
+    render 'index', peeps: Peep.all, user: current_user
   end
 
   get '/users/new' do
@@ -30,13 +31,16 @@ class Server < Hobbit::Base
 
   post '/users' do
     me = ChatterUser.create(name: params[:name], email: params[:email], creation_date: Time.now)
-    #session[:user_id] = me.id
-    puts "!!!!"
-    p session
-    render 'index', user: me, peeps: Peep.all
-    # Eventually will be using
-    # redirect '/'
+    session[:user_id] = me.id
+    redirect '/'
   end
+
+
+  def current_user
+    @current_user ||= ChatterUser.get(session[:user_id]) if session[:user_id]
+  end
+
+
 
 
 end
--- config.ru
+++ config.ru
@@ -1,7 +1,5 @@
-require 'rack/protection'
 require './app/server'
 
-use Rack::Session::Cookie, secret: SecureRandom.hex(64)
-use Rack::Protection
+
 
 run Server.new
