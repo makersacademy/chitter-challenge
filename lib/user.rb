13227ced3f83721f6a0ea3a9ea183b852cb75835
Kate <katebeavis84@gmail.com>
2015-03-02 16:55:04 +0000

    removed key unique

--- lib/user.rb
+++ lib/user.rb
@@ -4,7 +4,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String,:unique => true
+  property :email, String
   property :password_digest, Text
   property :password_confirmation, Text
   property :name, String
