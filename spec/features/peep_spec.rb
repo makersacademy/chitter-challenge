ae59744ca9b0e44b54ada94cf89637b99cacb8c2
Louise Beh <louise.beh@gmail.com>
2015-03-02 14:35:51 +0000

    Added feature: maker can only post a peep only when signed in

--- spec/features/peep_spec.rb
+++ spec/features/peep_spec.rb
@@ -2,11 +2,24 @@ require 'spec_helper'
 
 feature "Maker posts a peep" do
 
+  before(:each) do
+    Maker.create( :name => "Snow White",
+                  :username => "red_apple",
+                  :email => "snow_white@example.com",
+                  :password => "seven_dwarfs",
+                  :password_confirmation => "seven_dwarfs")
+  end
+
   scenario "when browsing the homepage" do
     visit '/'
     expect{add_peep("Hello Chitter!", "Snow White", "seven_dwarfs")}.to change{Peep.count}.by(1)
   end
 
+  scenario "only when signed in with correct credentials" do
+    visit ('/')
+    expect(page).not_to have_content("What's Peepin'?:")
+    sign_in('red_apple', 'seven_dwarfs')
+    expect(page).to have_content("What's Peepin'?:")
   end
 
   def add_peep(message, name, username)
@@ -16,8 +29,7 @@ feature "Maker posts a peep" do
     end
   end
 
-  # scenario "only when logged in" do
-  # end
+end
 
 
 # feature "Maker replies to a peep" do
