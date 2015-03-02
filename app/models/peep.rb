54b24f7a3d5022c5218c07bf922b6a794d6891a6
Chris Ward <chriswarddev@gmail.com>
2015-03-02 08:08:22 +0000

    updated month hash in peep

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -9,7 +9,7 @@ property :created_at, DateTime
 
 def display_date_and_time
   text = self.created_at.to_s
-  months = { "01" => "Jan", "02" => "Feb", "03" => "March", "04" => "April"}
+  months = { "01" => "Jan", "02" => "Feb", "03" => "March", "04" => "April", "05" => "May", "06" => "June", "07" => "July", "08" => "Aug", "09" => "Sept", "10" => "Oct", "11" => "Nov", "12" => "Dec"}
   month = months[text[5..6]]
   display = "#{month} #{text[8,2]} : #{text[11,5]}"
 
