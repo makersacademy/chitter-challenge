dcc4959e0cf70310ee601df9ec99eefab527dcb9
Kate <katebeavis84@gmail.com>
2015-03-02 16:57:50 +0000

    removed key unique

--- lib/user.rb
+++ lib/user.rb
@@ -14,7 +14,7 @@ class User
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
-  validates_uniqueness_of :email
+  # validates_uniqueness_of :email
 
   def password=(password)
     @password = password
