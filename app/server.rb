e8d070e03852296dc73b9d661aadc819daa90d49
Diego <diegoromero.audio@gmail.com>
2015-03-02 18:47:01 +0000

    added some html and css

--- app/server.rb
+++ app/server.rb
@@ -11,6 +11,9 @@ require_relative 'controllers/users_controller'
 require_relative 'controllers/sessions_controller'
 require_relative 'controllers/index_controller'
 
+set :public_folder, Proc.new { File.join(root, '..', "public") }
+set :views, Proc.new { File.join(root, "views") }
+
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
