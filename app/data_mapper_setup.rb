a71978eda2b6be61d11fe2d29091a0f57f5d37a5
Julian Veling <julianveling@Julians-MacBook-Pro.local>
2015-03-02 11:37:13 +0000

    changed layout

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,5 +1,5 @@
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
