04c81722c8003f5780ea0c55946d273b2b2d7dc0
Erik Griffin <griffin.erik@gmail.com>
2015-03-02 21:24:49 +0000

    Added some email validation to users

--- app/model/chatter_user.rb
+++ app/model/chatter_user.rb
@@ -7,7 +7,7 @@ class ChatterUser
 
   property :id, Serial
   property :name, String
-  property :email, String
+  property :email, String, format: :email_address, required: true, unique: true
   property :creation_date, DateTime
   property :password_hash, Text
   has n, :peeps
