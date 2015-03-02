f022cb2662cfee4ca4917fa5c9056f5476d7cba2
Diego <diegoromero.audio@gmail.com>
2015-03-02 14:58:30 +0000

    user can logout

--- server.rb
+++ server.rb
@@ -9,6 +9,7 @@ require_relative 'data_mapper_setup'
 enable :sessions
 set :session_secret, 'super secret'
 use Rack::Flash
+use Rack::MethodOverride
 
 get '/' do
   @chit = Chit.all
@@ -31,11 +32,35 @@ post '/users' do
   @user = User.new(:email => params[:email], 
               :password => params[:password],
               :password_confirmation => params[:password_confirmation])  
-  if @user.save
-    session[:user_id] = @user.id
-    redirect to('/')
-  else
-    flash.now[:errors] = @user.errors.full_messages
-    erb :"users/new"
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      erb :"users/new"
+    end
   end
-end
+
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    email, password = params[:email], params[:password] 
+    user = User.authenticate(email, password)
+    if user
+      session[:user_id] = user.id
+      redirect to('/')
+    else
+      flash[:errors] = ["The email or password is incorrect"]
+      erb :"sessions/new"
+    end
+  end
+
+  delete '/sessions' do
+    flash[:notice] = "Good bye!"
+    session[:user_id] = nil
+    redirect to('/')
+  end
+
+
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,48 @@
 require 'spec_helper'
+require 'helpers/session'
+
+include SessionHelpers
+
+feature "User signs in" do
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario "with correct credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'test')
+    expect(page).to have_content("Welcome, test@test.com")
+  end
+
+  scenario "with incorrect credentials" do
+    visit '/'
+    expect(page).not_to have_content("Welcome, test@test.com")
+    sign_in('test@test.com', 'wrong')
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+end
+
+feature 'User signs out' do
+
+  before(:each) do
+    User.create(:email => "test@test.com",
+                :password => 'test',
+                :password_confirmation => 'test')
+  end
+
+  scenario 'while being signed in' do
+    sign_in('test@test.com', 'test')
+    click_button "Sign out"
+    expect(page).to have_content("Good bye!") # where does this message go?
+    expect(page).not_to have_content("Welcome, test@test.com")
+  end
+
+end
 
 feature "User signs up" do
 
@@ -19,15 +63,7 @@ feature "User signs up" do
     lambda { sign_up}.should change(User, :count).by(0)
     expect(page).to have_content("this email has already been taken")
   end
-
-  def sign_up(email ="diego@example.com", password = "test",
-              password_confirmation = "test")
-    visit '/users/new'
-    fill_in :email, :with => email
-    fill_in :password, :with => password
-    fill_in :password_confirmation, :with => password_confirmation
-    click_button "Sign up"
-  end
   
 end
+
 
