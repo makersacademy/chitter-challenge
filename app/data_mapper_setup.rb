ea4ca9fc92a9e77f8048aa6bd85a2abb4dc93282
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-04 15:24:22 +0000

    all tests passing

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{env}")
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
