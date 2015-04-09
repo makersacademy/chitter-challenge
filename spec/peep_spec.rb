f44f8c05313514a4323ca768c67b2b0869b5ee08
sanjsanj <sanjsanj@hotmail.com>
2015-04-09 23:04:41 +0100

    Basic Peep database functions are operational

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,41 +1,35 @@
 require './app/peep'
 
 describe Peep do
+  let(:peep) { described_class.first }
 
-  context '' do
-    described_class.each(&:destroy) if described_class
-
-    it 'database is empty' do
-      expect(described_class.count).to eq 0
-    end
+  it 'database is empty' do
+    described_class.each(&:destroy)
+    expect(described_class.count).to eq 0
   end
 
-  context '' do
-    before(:each) do
-      described_class.create(message: 'My first peep',
-                             user_name: 'Sanjay Purswani',
-                             user_handle: 'sanjsanj')
-    end
+  before(:each) do
+    described_class.create message: 'My first peep',
+                           user_name: 'Sanjay Purswani',
+                           user_handle: 'sanjsanj'
+  end
 
-    after(:each) do
-      described_class.each(&:destroy) if described_class
-    end
+  after(:each) do
+    described_class.each(&:destroy)
+  end
 
-    peep = described_class.first
+  it 'can be created in the database' do
+    expect(described_class.count).to eq 1
+  end
 
-    it 'can be created in the database' do
-      expect(described_class.count).to eq 1
-    end
+  it 'can be retrieved from the database' do
+    expect(peep.message).to eq 'My first peep'
+    expect(peep.user_name).to eq 'Sanjay Purswani'
+    expect(peep.user_handle).to eq 'sanjsanj'
+  end
 
-    it 'can be retrieved from the database' do
-      expect(peep.message).to eq 'My first peep'
-      expect(peep.user_name).to eq 'Sanjay Purswani'
-      expect(peep.user_handle).to eq 'sanjsanj'
-    end
-
-    it 'can be deleted from the database' do
-      peep.destroy
-      expect(described_class.count).to eq 0
-    end
+  it 'can be deleted from the database' do
+    peep.destroy
+    expect(described_class.count).to eq 0
   end
 end
