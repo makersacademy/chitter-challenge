fe1dd7f37bd049a98bd5929a991441cf36cc56ed
Ed O'Brien <edmund.obrien.2010@live.rhul.ac.uk>
2015-04-10 18:59:46 +0100

    user can sign in

--- app/server.rb
+++ app/server.rb
@@ -40,7 +40,11 @@ class Chitter < Sinatra::Base
                        password: params[:password],
                        password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
-    redirect to('/')
+    redirect to('/welcome_message')
+  end
+
+  get '/welcome_message' do
+    erb :welcome_message
   end
 
   helpers do
