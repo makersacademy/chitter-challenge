ad2e7c0a55927aa3a81bb0b27bb67be87356bc42
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-10 16:46:51 +0100

    added tasks to Rakefile to handle schema changes

--- Rakefile
+++ Rakefile
@@ -1,6 +1,7 @@
 require 'cucumber/rake/task'
 require 'rspec/core/rake_task'
 require 'rubocop/rake_task'
+require_relative './app/data_mapper_setup'
 
 RuboCop::RakeTask.new :cop
 RSpec::Core::RakeTask.new :spec
@@ -8,4 +9,12 @@ Cucumber::Rake::Task.new :cuke
 
 task default: [:cop, :spec, :cuke]
 
-tast
+task :auto_migrate do
+  DataMapper.auto_migrate!
+  puts "Database Migrated Data May Have Been Lost"
+end
+
+task :auto_upgrade do
+  DataMapper.auto_upgrade!
+  puts "Database Upgraded Data Safe"
+end
--- app/server.rb
+++ app/server.rb
@@ -1,16 +1,9 @@
 require 'sinatra/base'
-require 'data_mapper'
-
-env = ENV['RACK_ENV'] || 'development'
-
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
-
+require_relative 'data_mapper_setup'
 require_relative './models/user'
 
-DataMapper.finalize
-DataMapper.auto_upgrade!
-
 class Chitter < Sinatra::Base
+
   get '/' do
     erb :index
   end
@@ -24,5 +17,6 @@ class Chitter < Sinatra::Base
   end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
+  run! if app_file == $PROGRAM_NAME
+
 end
