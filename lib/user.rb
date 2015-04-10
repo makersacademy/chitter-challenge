c221d764e58e83e9c576c7b23f26d67b87a46840
Sam Joseph <tansaku@gmail.com>
2015-04-10 08:09:07 +0100

    tests for necessary properties on datamapper model

--- lib/user.rb
+++ lib/user.rb
@@ -2,4 +2,7 @@ class User
 
   include DataMapper::Resource
 
+  property :id, Serial
+  property :email, String
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -10,4 +10,12 @@ describe User do
     expect(described_class.ancestors).to include DataMapper::Resource
   end
 
+  it 'has an id' do
+    expect(subject.send(:properties).map(&:name)).to include :id
+  end
+
+  it 'has an email' do
+    expect(subject.send(:properties).map(&:name)).to include :email
+  end
+
 end
