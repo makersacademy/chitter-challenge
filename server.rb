83423ad9582f7e773e6ad1482cb263f87bf53e32
stacca <caronstace@gmail.com>
2015-04-10 15:08:02 +0100

    sorted structure

--- server.rb
+++ server.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'data_mapper'
 env = ENV["RACK_ENV"] || "development"
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
-require_relative 'post'
+require './lib/post'
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
