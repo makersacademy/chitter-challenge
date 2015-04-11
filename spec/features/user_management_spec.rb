c4f10a10788fc2e819730cc17757b6bbdcb5a5fd
Kanishk Walia <kanishk.walia@gmail.com>
2015-04-11 18:52:07 +0100

    Css fix

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -64,7 +64,7 @@ feature 'User signs out' do
   scenario 'while being signed in' do
     sign_in('test@test.com', 'test')
     click_button 'Sign out'
-    expect(page).to have content('Good bye!')
+    expect(page).to have content('Good bye! :(')
     expect(page).not_to have_content('Welcome, test@test.com')
   end
 end
