4ea298488c72aeb061b25bbc2dba00ff5af6f003
Richard Lynch <rjlynch86@googlemail.com>
2015-04-11 19:11:22 +0100

    fixes broken test

--- server.rb
+++ server.rb
@@ -95,8 +95,6 @@ class Chittter < Sinatra::Base
   post '/reply' do
     reply = Reply.create(reply: params[:reply])
     peep = Peep.first(id: session[:reply_id])
-    p session[:reply_id]
-    p peep
     peep.replys << reply
     peep.save
     redirect '/'
--- spec/features/showing_all_peeps_spec.rb
+++ spec/features/showing_all_peeps_spec.rb
@@ -13,7 +13,7 @@ feature 'Viewing peeps' do
     sleep 1.0
     add_peep '3rd'
     visit '/'
-    expect(page).to have_content('peeps: "3rd" peeps: "2nd" peeps: "1st"')
+    expect(page).to have_content('peeps: "3rd" reply peeps: "2nd" reply peeps: "1st" reply')
     expect(page).not_to have_content('test')
   end
 
