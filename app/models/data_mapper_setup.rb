c1c352a82dfdcf199fa8fea0ed02cf9608df9045
Joseph Knowles <joejknowles@gmail.com>
2015-04-11 09:29:38 +0100

    remove h url

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 heroku = ENV['DATABASE_URL']
 env = ENV['RACK_ENV'] || 'development'
 local = "postgres://localhost/chitter_#{env}"
-DataMapper.setup(:default, 'postgres://baaatxqjtpmvgh:lWtXR1VyQU2_Z37PTkSgN2pC9b@ec2-54-163-239-102.compute-1.amazonaws.com:5432/d2rhv5b5d21i9r')
+DataMapper.setup(:default, local)
 require_relative 'user'
 DataMapper.finalize
