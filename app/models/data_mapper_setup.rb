22e2536d9e47d2f05cd1944a20ef525bc0715cf7
Joseph Knowles <joejknowles@gmail.com>
2015-04-11 09:38:47 +0100

    heroku url

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 heroku = ENV['DATABASE_URL']
 env = ENV['RACK_ENV'] || 'development'
 local = "postgres://localhost/chitter_#{env}"
-DataMapper.setup(:default, local)
+DataMapper.setup(:default, heroku || local)
 require_relative 'user'
 DataMapper.finalize
 DataMapper.auto_upgrade!
