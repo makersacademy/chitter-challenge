22ed9e371d5a89f0bb4a57f1a1520884b1643584
Kanishk Walia <kanishk.walia@gmail.com>
2015-04-11 17:21:04 +0100

    Fixed formatting errors

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,15 +2,11 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-if env == 'production'
-  DataMapper.setup(:default, "#{ENV['BLABBER_DB_URL']}")
-else
-  DataMapper.setup(:default, "postgres://localhost/blabber_#{env}")
-end
-
-require './app/models/blabbs'
-require './app/models/user'
+DataMapper.setup(:default, "postgres://localhost/blabber_#{env}")
 
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
+
+# require './app/models/blabbs'
+# require './app/models/user'
