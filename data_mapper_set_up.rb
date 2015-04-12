100114f0141b4aaadd80976ed0c9ad5c1c837db8
Richard Lynch <rjlynch86@googlemail.com>
2015-04-12 20:29:32 +0100

    sets ref for postgres back to local host

--- data_mapper_set_up.rb
+++ data_mapper_set_up.rb
@@ -1,6 +1,6 @@
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "/chittter_#{env}")
+DataMapper.setup(:default, "postgres://localhost/chittter_#{env}")
 
 require './lib/user'
 require './lib/peep'
