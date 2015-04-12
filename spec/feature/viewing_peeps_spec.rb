14de7cd31dcc4a35ae53545a33bc4765a4112933
Benjamin Kollek <benkollek@gmail.com>
2015-04-12 18:57:48 +0100

    minor refactoring

--- spec/feature/viewing_peeps_spec.rb
+++ spec/feature/viewing_peeps_spec.rb
@@ -1,7 +1,6 @@
 feature 'Viewing peeps' do
-  # Sessions? Have someone else log in and post then view all
-  # Capybara session names
-  scenario 'can see ALL peeps on the homepage' do
+
+  before :each do
     sign_up
     post_peep('Hiya!')
     post_peep('Howdy!')
@@ -9,6 +8,9 @@ feature 'Viewing peeps' do
     sign_up('b@b.com', 'hello', 'hello')
     post_peep('Cheerio!')
     post_peep('Hola!')
+  end
+
+  scenario 'can see ALL peeps on the homepage' do
     expect(page).to have_content('Hiya!')
     expect(page).to have_content('Howdy!')
     expect(page).to have_content('Cheerio!')
@@ -16,13 +18,6 @@ feature 'Viewing peeps' do
   end
 
   scenario 'user peeps are visible under "My Peeps" after submission' do
-    sign_up
-    post_peep('Hiya!')
-    post_peep('Howdy!')
-    sign_out
-    sign_up('b@b.com', 'hello', 'hello')
-    post_peep('Cheerio!')
-    post_peep('Hola!')
     find_button('My Peeps').click
     expect(current_path).to eq('/mypeeps/')
     expect(page).to have_no_content('Hiya!')
