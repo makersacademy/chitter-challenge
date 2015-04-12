d6514f1b43689ac4ac50c336ecbd850dfb3d0b07
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-12 20:49:26 +0100

    added env variable for heroku database access

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{env}")
 
 require_relative './models/user'
 require_relative './models/peep'
