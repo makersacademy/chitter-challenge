20c325be8a219919771151a7f060da9b0a01861b
Richard Lynch <rjlynch86@googlemail.com>
2015-04-12 15:17:55 +0100

    adds some not very stylish styling

--- spec/features/forogt_passowrd_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'user has forgotten their password' do 
-  scenario 'user requests new password' do
-    visit '/sessions/new'
-    fill_in :email, with: 'test@test.com'
-    click_button 'forgoten password'
-    puts "UN FINISHED"
-  end
-end
