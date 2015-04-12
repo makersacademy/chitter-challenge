03e6919655ba00e29fcc55e07051e57a1de4066c
Benjamin Kollek <benkollek@gmail.com>
2015-04-12 18:46:02 +0100

    Can see ALL peeps on homepage

--- spec/feature/viewing_peeps_spec.rb
+++ spec/feature/viewing_peeps_spec.rb
@@ -1,13 +1,18 @@
 feature 'Viewing peeps' do
   # Sessions? Have someone else log in and post then view all
   # Capybara session names
-  scenario 'can see logged in user\'s peeps on the homepage' do
+  scenario 'can see ALL peeps on the homepage' do
     sign_up
     post_peep('Hiya!')
     post_peep('Howdy!')
-    expect(current_path).to eq('/')
+    sign_out
+    sign_up('b@b.com', 'hello', 'hello')
+    post_peep('Cheerio!')
+    post_peep('Hola!')
     expect(page).to have_content('Hiya!')
     expect(page).to have_content('Howdy!')
+    expect(page).to have_content('Cheerio!')
+    expect(page).to have_content('Hola!')
   end
 
   xscenario 'peep is visible under "My Peeps" after submission'
