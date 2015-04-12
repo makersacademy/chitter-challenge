a1b67b786f594f6ec5f18604bb336960392f93fe
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-12 18:48:05 +0100

    adds check for duplicate email

--- server.rb
+++ server.rb
@@ -41,7 +41,7 @@ post '/users' do
     session[:user_id] = @user.id
     redirect to('/')
   else
-    flash[:notice] = "Sorry, your passwords don't match"
+    flash.now[:errors] = @user.errors.full_messages
     erb :'users/new'
   end
 end
