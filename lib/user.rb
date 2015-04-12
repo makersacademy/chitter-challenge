6a6f3c37c5b390a3411f6f8143dc7354752436b1
Sammy Abukmeil <sammyabukmeil@gmail.com>
2015-04-12 16:04:03 +0100

    adds name and username to signup form + greet user by name not email

--- lib/user.rb
+++ lib/user.rb
@@ -5,6 +5,8 @@ class User
   include DataMapper::Resource
 
   property :id,                 Serial
+  property :name,               String
+  property :username,           String
   property :email,              String
   property :password_digest,    Text
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,14 +4,18 @@ feature 'User signs up' do
 
   scenario 'when being a new user visiting the site' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, sammy@example.com')
+    expect(page).to have_content('Welcome, Sammy')
     expect(User.first.email).to eq('sammy@example.com')
   end
 
-  def sign_up(email = 'sammy@example.com',
+  def sign_up(name = 'Sammy',
+              username = 'Vinyls',
+              email = 'sammy@example.com',
               password = 'lolol')
     visit '/users/new'
     expect(page.status_code).to eq(200)
+    fill_in :name, with: name
+    fill_in :username, with: username
     fill_in :email, with: email
     fill_in :password, with: password
     click_button 'Sign up'
