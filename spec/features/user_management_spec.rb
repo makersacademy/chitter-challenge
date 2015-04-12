cb227b52e90cc6f5936cbf2086bc2ae3f34cb8a8
M1lena <stoevami@gmail.com>
2015-04-12 14:56:57 +0100

    fixed user handle welcome error

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,7 +3,7 @@ require './spec/spec_helper'
 feature 'User signs up' do
   scenario 'when being a new user visiting the site' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, yoyo@ma.com')
+    expect(page).to have_content('Welcome, aliceB')
     expect(User.first.email).to eq('yoyo@ma.com')
   end
 
