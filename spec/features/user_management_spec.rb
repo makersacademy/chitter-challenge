48eec9f6b120f9b67b7f2ea792013056bb50c02b
andygout <andygout@hotmail.co.uk>
2015-05-22 19:46:43 +0100

    Added: Preventing duplicate registrations

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -13,7 +13,19 @@ feature 'User signs up' do
   scenario 'with a password that does not match' do
     expect { sign_up('Big G', 'Andy Gout', 'example@test.com', 'pass', 'no_match') }.to change(User, :count).by(0)
     expect(current_path).to eq('/users')
-    expect(page).to have_content('Sorry, your passwords do not match')
+    expect(page).to have_content('Password does not match the confirmation')
+  end
+
+  scenario 'with an email that is already registered' do
+    expect { sign_up('Big G', 'Andy Gout', 'example@test.com') }.to change(User, :count).by(1)
+    expect { sign_up('Robbo', 'Robert Gout', 'example@test.com') }.to change(User, :count).by(0)
+    expect(page).to have_content('Email is already taken')
+  end
+
+  scenario 'with a username that is already registered' do
+    expect { sign_up('Big G') }.to change(User, :count).by(1)
+    expect { sign_up('Big G') }.to change(User, :count).by(0)
+    expect(page).to have_content('Username is already taken')
   end
 
 end
