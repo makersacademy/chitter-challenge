8b0c9363823633b68bf28a8c58c6f1fecb20728c
andygout <andygout@hotmail.co.uk>
2015-05-22 19:36:31 +0100

    Added: Unique constraints on username & email

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,17 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :username, String
+  property :username, String, unique: true
   property :name, String
-  property :email, String
+  property :email, String, unique: true
   property :password_digest, Text
 
   attr_reader :password
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
 
   def password=(password)
     @password = password
