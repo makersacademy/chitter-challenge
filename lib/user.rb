c77f1f327b0c1e62b0d95018a20661b824d0f5c3
Ashleigh <ashleighmaund@gmail.com>
2015-05-22 11:50:16 +0100

    defined encrypted_password as a attribute in user class

--- lib/user.rb
+++ lib/user.rb
@@ -6,6 +6,7 @@ class User
 
 	property :id, Serial
   property :email, String, unique: true
+  property :encrypted_password, Text
 
   def password=(password)
   	self.encrypted_password = BCrypt::Password.create(password) #encrypts the user's password
