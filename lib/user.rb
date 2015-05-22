6977e7b15dc1732dacb4d7ae134e753b37f7c886
Ashleigh <ashleighmaund@gmail.com>
2015-05-22 11:48:01 +0100

    added password= method to encrypt user's password

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,14 @@
+require 'bcrypt'
+
 class User
 
 	include DataMapper::Resource
 
 	property :id, Serial
   property :email, String, unique: true
+
+  def password=(password)
+  	self.encrypted_password = BCrypt::Password.create(password) #encrypts the user's password
+  end
 
 end
