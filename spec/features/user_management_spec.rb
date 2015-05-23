8bded7b7bf9ce9efaa73480749e13efa30b8a4f3
Tim <timrobertson0122@gmail.com>
2015-05-23 18:01:20 +0100

    flash notices for non-matching password/email already registered

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -7,7 +7,20 @@ feature 'User signs up' do
 
   scenario 'when being the user visiting the site' do 
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, test@test.com')
+    expect(page).to have_content('Welcome, Tim')
     expect(User.first.email).to eq('test@test.com')
   end
+
+  scenario 'with a password that does not match' do
+    expect { sign_up('a@a.com', 'pass', 'wrong') }.to change(User, :count).by(0)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Sorry, there were the following problems with the form.')
+  end
+
+  scenario 'with an email that is already registered' do
+    expect { sign_up }.to change(User, :count).by(1)
+    expect { sign_up }.to change(User, :count).by(0)
+    expect(page).to have_content('Sorry, there were the following problems with the form.')
+  end
+
 end
