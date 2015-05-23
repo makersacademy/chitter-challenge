30f6c99453e37f3209e233a74b34e72597ac6353
Alex Handy <handy437@gmail.com>
2015-05-23 15:16:48 +0100

    Working reply handle [PENDING FEATURE TESTS]

--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -11,3 +11,19 @@ post '/peeps' do
     erb :'peeps/new'
   end
 end
+
+post '/peeps/makereply' do
+  @original_id = params['original_id']
+  erb :'peeps/reply'
+end
+
+post '/peeps/getreply' do
+  @original = Peep.get(params['original_id'])
+  @peep = Peep.create(message: "@#{@original.user.username}:" + params['message'], user_id: current_user.id)
+  if @peep
+    redirect to ('/')
+  else
+    flash[:notice] = "You didn't submit a peep"
+    erb :'peep/reply'
+  end
+end
