f7f759d363af7fd027974fd9e1fc0e4fd2ed129d
andygout <andygout@hotmail.co.uk>
2015-05-23 19:25:06 +0100

    Additional tests: listing_all_peeps_spec.rb

--- spec/features/listing_all_peeps_spec.rb
+++ spec/features/listing_all_peeps_spec.rb
@@ -12,4 +12,18 @@ feature 'User views all peeps' do
     expect(page).to have_content('Look at my breakfast, lolz')
   end
 
+  scenario 'and can see username and name for each peep' do
+    sign_up
+    add_peep('Look at my breakfast, lolz')
+    visit '/'
+    expect(page).to have_content('Posted by Andy Gout (aka Big G)')
+  end
+
+  scenario 'and can see when each peep was posted' do
+    sign_up
+    add_peep('Look at my breakfast, lolz')
+    visit '/'
+    expect(page).to have_content(/[A-Z][a-z]{5,8} [123]*[0-9] [A-Z][a-z]{2,8} [0-9]{4} at [1]*[0-9]:[0-5][0-9](A|P)M \([0-5][0-9] secs\)/)
+  end
+
 end
