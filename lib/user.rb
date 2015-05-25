7fa06985a4d4f2fb2926330d00658f536fd81dbb
Chidumaga <chidumaga@gmail.com>
2015-05-25 13:00:40 +0100

    Password now BCrypted

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,4 @@
+require 'bcrypt'
 class User
 
   include DataMapper::Resource
@@ -9,6 +10,10 @@ class User
   property :name, String
   property :username, String, unique: true, message: "This username has been taken"
   property :email, String, unique: true, message: "This email has been registered"
-  property :password, String
+  property :password_digest, Text
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
 end
