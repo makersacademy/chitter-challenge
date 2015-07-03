9d8e326736b7e69c928acd08a11b10d3fb9e188b
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-03 19:43:15 +0100

    Testing peeps appear with timestamp and username

--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -13,11 +13,62 @@ feature 'Peeps' do
     expect(page).to have_content 'Hello world!'
   end
 
+  skip 'peeps appear in reverse chronological order' do
+    user = User.create(user_params)
+    visit '/'
+    within 'form#sign_in' do
+      fill_in 'username', with: user.username
+      fill_in 'password', with: user.password
+    end
+    click_button 'Sign in'
+    fill_in 'peep', with: 'Hello world!'
+    click_button 'Peep'
+    fill_in 'peep', with: 'It is me'
+    click_button 'Peep'
+    within 'ul#peep_list' do
+      expect()
+    end
+  end
 
+  scenario 'peeps time stamp is attached' do
+    user = User.create(user_params)
+    visit '/'
+    within 'form#sign_in' do
+      fill_in 'username', with: user.username
+      fill_in 'password', with: user.password
+    end
+    click_button 'Sign in'
+    fill_in 'peep', with: 'Hello world!'
+    click_button 'Peep'
+    expect(page).to have_content Peep.first(user_id: user.id).time_stamp
+  end
+
+  scenario 'peeps username is attached' do
+    user = User.create(user_params)
+    visit '/'
+    within 'form#sign_in' do
+      fill_in 'username', with: user.username
+      fill_in 'password', with: user.password
+    end
+    click_button 'Sign in'
+    fill_in 'peep', with: 'Hello world!'
+    click_button 'Peep'
+    expect(page).to have_content user.username
+  end
 
   skip 'if user is not logged in they can still view peeps' do
-
-
+    user = User.create(user_params)
+    visit '/'
+    within 'form#sign_in' do
+      fill_in 'username', with: user.username
+      fill_in 'password', with: user.password
+    end
+    click_button 'Sign in'
+    fill_in 'peep', with: 'Hello world!'
+    click_button 'Peep'
+    click_button 'Sign out'
+    visit '/test_name'
+    expect(page).to have_content 'Hello world!'
   end
 
   scenario 'if user is not logged in she cannot peep' do
