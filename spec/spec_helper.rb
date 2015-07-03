a03588ae90a84225062dd91d8556269f0e947ad3
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-03 11:43:39 +0100

    finalizing project opening setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(__FILE__), '..', 'lib/chitter.rb')
+require File.join(File.dirname(__FILE__), '..', 'app/chitter.rb')
 
 require 'capybara'
 require 'capybara/rspec'
