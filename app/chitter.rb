6a23361dd1108b81904ee8056db526c64135a71c
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-03 15:22:37 +0100

    'user can see existing peeps on the main page test' is now passing

--- app/chitter.rb
+++ app/chitter.rb
@@ -1,10 +1,22 @@
 require 'sinatra/base'
+require 'data_mapper'
+require_relative 'data_mapper_setup'
 
 class CHITTERchallenge < Sinatra::Base
-  get '/' do
-    'Hello CHITTERchallenge!'
+
+  set :views, proc { File.join(root, '..', 'views') }
+
+  enable :sessions
+
+  get '/peeps' do
+    @peeps = Peep.all
+    erb :'peeps'
   end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
+
+
+
+# '/peeps/:id'
