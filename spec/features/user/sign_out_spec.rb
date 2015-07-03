82987c7d5065a55c4378eb37cfbbb5b815a85220
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-03 19:11:48 +0100

    corrected sign-out test. dont need separate signout path

--- spec/features/user/sign_out_spec.rb
+++ spec/features/user/sign_out_spec.rb
@@ -2,7 +2,6 @@ feature 'Sign out' do
   scenario 'removes the sign in information from the browser session' do
     sign_up
     sign_in
-    visit '/users/sign_out'
     click_button('Sign out')
     expect(page).to have_content('Goodbye')
     expect(page).not_to have_content('Welcome, kj')
