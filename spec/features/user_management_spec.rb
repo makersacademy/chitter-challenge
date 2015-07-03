f9ce806826966d6ad3a155472a00ec6498fcebf3
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-03 18:21:43 +0100

    adding a test to check if I can sign up as a new user

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,10 +1,20 @@
-# feature 'User sign up' do
+feature 'User sign up' do
 
-#   scenario 'I can sign up as a new user' do
-#     expect { sign_up }.to change(User, :count).by (1)
-#     expect(page).to have_content('Welcome, SardiniaAwesome')
-#     expect(User.first.username).to eq('SardiniaAwesome')
-#   end
+  scenario 'I can sign up as a new user' do
+    expect { sign_up }.to change(User, :count).by (1)
+    expect(page).to have_content('Welcome, SardiniaAwesome')
+    expect(User.first.username).to eq('SardiniaAwesome')
+  end
+
+  def sign_up(email: 'ichnusa@sardinia.com',
+            password: '123456')
+    visit '/users/new'
+    expect(page.status_code).to eq(200)
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign up'
+  end
 
 
-# end
+
+end
