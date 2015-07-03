dd4de096548104c2f4bad813c5fbce4b22b741ac
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-03 19:09:15 +0100

    add test for sign-out

---
+++ spec/features/user/sign_out_spec.rb
@@ -0,0 +1,10 @@
+feature 'Sign out' do
+  scenario 'removes the sign in information from the browser session' do
+    sign_up
+    sign_in
+    visit '/users/sign_out'
+    click_button('Sign out')
+    expect(page).to have_content('Goodbye')
+    expect(page).not_to have_content('Welcome, kj')
+  end
+end
--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -1,5 +1,5 @@
 feature 'User sign up' do
-
+  
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, kj')
