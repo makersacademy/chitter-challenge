48cfd4f49cf686e5e16aafe4dce76ea20d8becb3
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-03 14:49:01 +0100

    made email field recognised for sign-in

--- app/app.rb
+++ app/app.rb
@@ -4,4 +4,12 @@ require './app/models/user'
 
 class Chitter < Sinatra::Base
 
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  # post '/users' do
+  #   @user = User.create(email: params)
+  #
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
+require 'bcrypt'
 require 'data_mapper'
 
 class User
 
   include DataMapper::Resource
 
+  attr_reader :password
+
   property :id, Serial
+  property :email, String, unique: true
+  property :username, String, unique: true
+  property :password_digest, Text
+  #
+  # def password=(password)
+  #   @password = password
+  #   self.password_digest = BCrypt::Password.create(password)
+  # end
 end
