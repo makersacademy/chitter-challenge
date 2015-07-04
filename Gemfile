4c7c0a9e664afd11605b14cb53cd11102ea475f2
TheoLeanse <theoleanse@gmail.com>
2015-07-04 14:16:57 +0100

    displays error messages with sinatra-flash

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 ruby '2.2.1'
 
 gem 'sinatra'
+gem 'sinatra-flash'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'bcrypt'
@@ -11,6 +12,7 @@ group :test do
   gem 'rspec'
   gem 'capybara'
   gem 'factory_girl'
+  gem 'byebug'
 
   gem 'rubocop-rspec'
   gem 'rubocop'
--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,13 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
 
   get '/' do
-    'Hi'
+    user = User.first id: session['user_id']
+    "Welcome, #{user.email}"
   end
 
   get '/users/new' do
@@ -11,11 +15,17 @@ class Chitter < Sinatra::Base
   end
 
   post '/users/new' do
-    user = User.create(
-      email: params['email'],
-      password: params['password'],
+    user = User.new(
+      email:                 params['email'],
+      password:              params['password'],
       password_confirmation: params['password_confirmation']
     )
-    redirect '/'
+    if user.save
+      session['user_id'] = user.id
+      redirect '/'
+    else
+      flash.now[:errors] = user.errors.full_messages
+      erb :'users/new'
+    end
   end
 end
