c95c69b6d5c1bb9251b5b5854f1ded116022dda8
TheoLeanse <theoleanse@gmail.com>
2015-07-04 12:00:32 +0100

    requires factory_girl in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,17 @@
 require 'coveralls'
 require 'simplecov'
 require 'capybara/rspec'
+require 'factory_girl'
+
+require './app/app.rb'
+
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+end
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
-
-RSpec.configure do |config|
-
-end
