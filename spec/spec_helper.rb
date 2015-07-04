d30b85407297a5ac3a62084aa944638627511ad5
TheoLeanse <theoleanse@gmail.com>
2015-07-04 11:21:16 +0100

    requires 'capybara/rspec' in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'coveralls'
 require 'simplecov'
+require 'capybara/rspec'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+RSpec.configure do |config|
+
+end
