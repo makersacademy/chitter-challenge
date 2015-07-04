935576f566f89ae789bef4939c030a05fd6a6a54
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-04 11:14:08 +0100

    remove byebug activation lines. still required in gemfile.

--- spec/features/user/sign_out_spec.rb
+++ spec/features/user/sign_out_spec.rb
@@ -3,8 +3,6 @@ feature 'Sign out' do
     sign_up
     sign_in
     click_button('Sign out')
-    require 'byebug'
-    byebug
     expect(page).to have_content('Goodbye!')
     expect(page).not_to have_content('Welcome, kj')
   end
