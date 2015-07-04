104c92f468d8596a0d0907c44798ad2996376712
TheoLeanse <theoleanse@gmail.com>
2015-07-04 11:58:17 +0100

    introduces factory girl to feature test: user sign-up

--- spec/features/user_signing_in_spec.rb
+++ spec/features/user_signing_in_spec.rb
@@ -1,9 +1,20 @@
+require './spec/factory/user'
+
 feature 'Visitor signs up' do
   scenario 'with valid email and password' do
     visit '/users/new'
+    valid_user = build :user
 
-    sign_up_with 'me@example.com', 'password'
+    sign_up_with valid_user
 
     expect(page).to have_content 'Welcome, me@example.com'
   end
+end
+
+def sign_up_with opts={}
+  fill_in 'email',                  with: :email
+  fill_in 'password',               with: :password
+  fill_in 'password_confirmation',  with: :password_confirmation
+
+  click_button 'Sign up'
 end
