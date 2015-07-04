3c733cfd468d271c665a0954807b55c360eb516b
TheoLeanse <theoleanse@gmail.com>
2015-07-04 17:47:03 +0100

    tests feature: valid user log-in

--- spec/features/user_logging_in_spec.rb
+++ spec/features/user_logging_in_spec.rb
@@ -1,7 +1,11 @@
 feature 'User logs in' do
 
   scenario 'with valid email and password' do
+    valid_user = build :user
 
+    log_in valid_user
+
+    expect(page).to have_content "Welcome, #{valid_user.email}"
   end
 
   scenario 'with incorrect email and password' do
@@ -9,6 +13,13 @@ feature 'User logs in' do
   end
 
   scenario 'with forgotten email or password' do
-    
+
+  end
+
+  def log_in user
+    visit '/sessions/new'
+    fill_in 'email', with: user.email
+    fill_in 'password', with: user.password
+    click_button 'Log in'
   end
 end
