0c520e6a9988cd3be1737841c8313c5986cdc525
TheoLeanse <theoleanse@gmail.com>
2015-07-04 14:18:58 +0100

    validates password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,17 @@ require 'bcrypt'
 class User
   include DataMapper::Resource
 
+  attr_reader :password
   attr_accessor :password_confirmation
+
+  validates_confirmation_of :password 
 
   property :id,            Serial
   property :email,         String
   property :password_hash, Text
 
   def password= password
+    @password = password
     self.password_hash = BCrypt::Password.create password
   end
 end
