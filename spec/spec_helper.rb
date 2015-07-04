f951148e1b7b6122db14d05c1e4e727b132889df
TheoLeanse <theoleanse@gmail.com>
2015-07-04 12:32:53 +0100

    requries DataMapper setup file and defines RACK_ENV

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,16 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
 require 'coveralls'
 require 'simplecov'
 require 'capybara/rspec'
 require 'factory_girl'
 
 require './app/app.rb'
+require './app/data_mapper_setup'
 
 Capybara.app = Chitter
 
 RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
-  
+
 end
 
 SimpleCov.formatters = [
