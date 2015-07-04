83e2b1831d5652303c9ff6b556dbde7ca6afd887
TheoLeanse <theoleanse@gmail.com>
2015-07-04 17:08:56 +0100

    implements User#authenticate

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User
   attr_reader :password
   attr_accessor :password_confirmation
 
-  validates_confirmation_of :password 
+  validates_confirmation_of :password
 
   property :id,            Serial
   property :email,         String
@@ -15,5 +15,14 @@ class User
   def password= password
     @password = password
     self.password_hash = BCrypt::Password.create password
+  end
+
+  def self.authenticate email, password
+    user = User.first(email: email)
+    if user && BCrypt::Password.new(user.password_hash) == password
+      user
+    else
+      nil
+    end
   end
 end
