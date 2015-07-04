ab1b3cbd09e1d46e3ea727d544a26464cdc97556
Kirsten Jones <kirstenjones@yahoo.co.uk>
2015-07-04 11:29:30 +0100

    required database cleaner and added config to spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
 
 require 'capybara/rspec'
 require 'coveralls'
-# require 'database_cleaner'
+require 'database_cleaner'
 require 'factory_girl'
 require 'simplecov'
 
@@ -23,16 +23,16 @@ Coveralls.wear!
 
 RSpec.configure do |config|
 
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :transaction
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
-  #
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
-  #
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
