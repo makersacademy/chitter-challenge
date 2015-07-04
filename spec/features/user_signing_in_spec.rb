f9767949a138dc1cfdc3c08dca1e0de792bb0477
TheoLeanse <theoleanse@gmail.com>
2015-07-04 14:13:57 +0100

    tests for feature: invalid password confirmation at sign-up

--- spec/features/user_signing_in_spec.rb
+++ spec/features/user_signing_in_spec.rb
@@ -1,20 +1,32 @@
-require './spec/factory/user'
+require './spec/factory/user.rb'
 
 feature 'Visitor signs up' do
-  scenario 'with valid email and password' do
+
+  before(:each) do
     visit '/users/new'
+  end
+
+  scenario 'with valid email and password' do
     valid_user = build :user
 
     sign_up_with valid_user
 
     expect(page).to have_content 'Welcome, me@example.com'
   end
+
+  scenario 'with valid email and invalid password' do
+    invalid_user = build :user, password_confirmation: 'wrong'
+
+    sign_up_with invalid_user
+
+    expect(page).to have_content 'Password does not match the confirmation'
+  end
 end
 
-def sign_up_with opts={}
-  fill_in 'email',                  with: :email
-  fill_in 'password',               with: :password
-  fill_in 'password_confirmation',  with: :password_confirmation
+def sign_up_with user
+  fill_in 'email',                  with: user.email
+  fill_in 'password',               with: user.password
+  fill_in 'password_confirmation',  with: user.password_confirmation
 
   click_button 'Sign up'
 end
