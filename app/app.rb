82db31a5a2cf371aff073714693f73bb5c08a5ad
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-05 18:47:18 +0100

    Drying out code

--- app/app.rb
+++ app/app.rb
@@ -19,10 +19,7 @@ class Chitter < Sinatra::Base
   get '/:username' do
     @user = User.first(username: params[:username])
     if @user
-      @peeps = @user.peeps.all(:order => :time_stamp.desc )
-      all_users_peep_ids = @peeps.map{ |peep| peep.id }
-      replies = Peep.all(reply: true)
-      @users_replies = replies.select{ |peep| all_users_peep_ids.include?(peep.replied_id) }
+      get_users_peeps_and_replies
       erb :'sessions/peeps'
     else
       erb :'users/not_found'
@@ -33,10 +30,7 @@ class Chitter < Sinatra::Base
     @user = User.first(username: params[:username])
     peep = Peep.create(text: params[:reply], time_stamp: Time.now, user_id: current_user.id, reply: true, replied_id: params[:peep_id], replied_to: @user.username)
     current_user.peeps << peep
-    @peeps = @user.peeps.all(:order => :time_stamp.desc )
-    all_users_peep_ids = @peeps.map{ |peep| peep.id }
-    replies = Peep.all(reply: true)
-    @users_replies = replies.select{ |peep| all_users_peep_ids.include?(peep.replied_id) }
+    get_users_peeps_and_replies
     erb :'sessions/peeps'
   end
 
@@ -56,10 +50,7 @@ class Chitter < Sinatra::Base
       @user = User.authenticate(username: params[:username], password: params[:password])
       if @user
         session[:user_id] = @user.id unless session[:user_id]
-        @peeps = current_user.peeps.all(:order => :time_stamp.desc )
-        all_users_peep_ids = @peeps.map{ |peep| peep.id }
-        replies = Peep.all(reply: true)
-        @users_replies = replies.select{ |peep| all_users_peep_ids.include?(peep.replied_id) }
+get_users_peeps_and_replies
         erb :'sessions/peeps'
       else
         flash[:errors] = ['Incorrect password']
@@ -73,10 +64,7 @@ class Chitter < Sinatra::Base
 
   get '/sessions/new' do
     @user = current_user
-    @peeps = current_user.peeps.all(:order => :time_stamp.desc )
-    all_users_peep_ids = @peeps.map{ |peep| peep.id }
-    replies = Peep.all(reply: true)
-    @users_replies = replies.select{ |peep| all_users_peep_ids.include?(peep.replied_id) }
+    get_users_peeps_and_replies
     erb :'sessions/peeps'
   end
 
@@ -89,10 +77,7 @@ class Chitter < Sinatra::Base
     @user = current_user
     peep = Peep.create(text: params[:peep], time_stamp: Time.now, user_id: current_user.id)
     current_user.peeps << peep
-    @peeps = current_user.peeps.all(:order => :time_stamp.desc )
-    all_users_peep_ids = @peeps.map{ |peep| peep.id }
-    replies = Peep.all(reply: true)
-    @users_replies = replies.select{ |peep| all_users_peep_ids.include?(peep.replied_id) }
+    get_users_peeps_and_replies
     erb :'sessions/peeps'
   end
 
@@ -100,6 +85,13 @@ class Chitter < Sinatra::Base
 
     def current_user
       User.get(session[:user_id])
+    end
+
+    def get_users_peeps_and_replies
+      @peeps = @user.peeps.all(:order => :time_stamp.desc )
+      all_users_peep_ids = @peeps.map{ |peep| peep.id }
+      replies = Peep.all(reply: true)
+      @users_replies = replies.select{ |peep| all_users_peep_ids.include?(peep.replied_id) }
     end
 
   end
