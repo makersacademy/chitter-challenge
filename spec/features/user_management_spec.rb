5813e7fd3ac0f537a4c3db5350779f3b1dd617e1
Nicola Hughes <nicolarhughes@gmail.com>
2015-07-05 22:28:11 +0100

    First three user stories complete

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -52,6 +52,12 @@ feature 'User\'s' do
     expect(page).to have_content "#{user.username}"
   end
 
+  scenario 'can\'t log in with the wrong username' do
+    user = build(:user, username: 'Goo')
+    log_in(user)
+    expect(page).to have_content('username or password is incorrect')
+  end
+
   scenario 'can log out once signed in' do
     log_in(user)
     click_button 'Log out'
