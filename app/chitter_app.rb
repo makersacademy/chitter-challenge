e9af5947cd34b289b1e8e8796d47ac5d42a5d815
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro-17.local>
2015-07-05 17:32:45 +0100

    failing log in test

--- app/chitter_app.rb
+++ app/chitter_app.rb
@@ -46,12 +46,12 @@ class Chitter < Sinatra::Base
   end
 
   post '/sessions' do
-    @user = User.authenticate(params[:username], params[:password])
-    if @user
+    user = User.authenticate(params[:username], params[:password])
+    if user
       session[:user_id] = user.id
       redirect '/posts'
     else
-      flash.now[:errors] = ['The email or password is incorrect']
+      flash.now[:errors] = ['The username or password is incorrect']
       erb :'sessions/new'
     end
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,11 +5,11 @@ describe User do
   end
 
   it 'authenticates when given a valid email address and password' do
-    authenticated_user = User.authenticate(user.email, user.password)
+    authenticated_user = User.authenticate(user.username, user.password)
     expect(authenticated_user).to eq user
   end
 
   it 'does not authenticate when given an incorrect password' do
-    expect(User.authenticate(user.email, 'wrong_stupid_password')).to be_nil
+    expect(User.authenticate(user.username, 'wrong_stupid_password')).to be_nil
   end
 end
