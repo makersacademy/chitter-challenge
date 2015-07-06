15bb30417aa1c3c2efcbd5bf04530cc9af72cce1
Natalie Akam <natalie.akam@gmail.com>
2015-07-06 10:36:46 +0100

    Corrected spacing

--- app/chitter.rb
+++ app/chitter.rb
@@ -22,8 +22,8 @@ class App < Sinatra::Base
   end
 
   post '/users' do
-  	@user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], name: params[:name], username: params[:username])
-  	if @user.save
+    @user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], name: params[:name], username: params[:username])
+    if @user.save
       session[:user_id] = @user.id
       redirect to('/')
     else
