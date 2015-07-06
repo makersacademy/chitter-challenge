24c38b21aa1e45f863207f10e03bd4b4f3ba0f4b
Natalie Akam <natalie.akam@gmail.com>
2015-07-06 10:25:20 +0100

    Corrected spacing

--- app/chitter.rb
+++ app/chitter.rb
@@ -5,6 +5,7 @@ require_relative './models/user'
 require_relative './models/peep'
 
 class App < Sinatra::Base
+
   set :views, proc { File.join(root, '..', 'views') }
   enable :sessions
   set :session_secret, 'super secret'
@@ -16,14 +17,14 @@ class App < Sinatra::Base
   end
 
   get '/users/new' do
-  	@user = User.new
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
   	@user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], name: params[:name], username: params[:username])
   	if @user.save
-  	  session[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect to('/')
     else
       flash.now[:error] = @user.errors.full_messages#'Password does not match'	
@@ -72,16 +73,17 @@ class App < Sinatra::Base
   end
 
   get '/peeps' do
-  	@peeps = Peep.all
+    @peeps = Peep.all
     erb :'/peeps/peeps'
   end
 
   helpers do
-  	def current_user
-  	  return false unless session[:user_id]
+    def current_user
+      return false unless session[:user_id]
       current_user ||= User.first(id: session[:user_id])
     end
   end
   # start the server if ruby file executed directly
   run! if app_file == $0
+
 end
