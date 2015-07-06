06f60a2c5853e0403098f7f43d0143779498d312
Jonathan Ng <jonngkh@hotmail.com>
2015-07-06 09:47:50 +0100

    spec up

---
+++ spec/features/user_management_spec.rb
@@ -0,0 +1,15 @@
+feature 'User sign up' do
+
+  # xscenario 'can sign up as a new user' do
+  #   expect { sign_up }.to change(User, :count).by(1)
+  #   expect(page).to have_content('alice@example.com ExemplaryAlice')
+  #   expect(User.first.email).to eq('alice@example.com')
+  #   expect(User.first.username).to eq('ExemplaryAlice')
+  # end
+
+  # def sign_up(username: 'ExemplaryAlice', email: 'alice@example.com', password: 'wonderland')
+  #   visit 'users/new'
+
+
+  # end
+end
