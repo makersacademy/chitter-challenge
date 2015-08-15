e7fec0adbe8645c05b352e6da97c5cfa65d6e255
Christopher Alcock <christopher.alcock@gmail.com>
2015-08-15 22:33:54 +0100

    first test passing

--- app/app.rb
+++ app/app.rb
@@ -10,7 +10,9 @@ class Chitter < Sinatra::Base
   register Sinatra::Flash
   use Rack::MethodOverride
 
-  get '/' do
+  get '/peeps' do
+    @peeps = Peep.all
+    erb :'peeps/index'
   end
 
   run! if app_file == $0
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,10 +1,10 @@
 feature 'Viewing peeps' do
 
-  scenario 'I can see existing peeps on the links page' do
-    Peep.new(user: 'chrisalcockdev', content: 'hello world!', created_at: '12:00')
-    visit '/'
+  scenario 'I can see existing peeps on the peeps page' do
+    Peep.create(user: 'chrisalcockdev', content: 'hello world!', created_at: '12:00')
+    visit '/peeps'
     expect(page.status_code).to eq 200
-    within 'ul#links' do
+    within 'ul#peeps' do
       expect(page).to have_content('hello world!')
     end
   end
