5add45096e26754a9eea8e3e25d792b237002a36
Richard Charman <richardcharman@live.com>
2015-08-15 22:22:17 +0000

    Changed timestamp format and added aditional styling

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 feature 'Viewing peeps' do
 
   scenario 'I can see existing peeps on the peeps page' do
-    Peep.new(text: 'This is a peep')
+    Peep.new(text: 'This is a new peep')
     visit '/peeps'
     # as this is our first feature test,
     # the following expectation is a quick check that everything is working.
@@ -12,7 +12,7 @@ feature 'Viewing peeps' do
     # why do we use within here?
     # might we get a false positive if we just test for 'Makers Academy'?
     within 'ul#peeps' do
-      expect(page).to have_content('This is a peep')
+      expect(page).to have_content('This is a new peep')
     end
   end
 end
