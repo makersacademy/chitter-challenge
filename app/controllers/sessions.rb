f1aee2bb73f66dc940e673659ffeae25968a3d6c
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-08-15 23:23:32 +0100

    Fixed a test

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,13 +1,21 @@
 module Chitter
+
   module Routes
+
     class Sessions < Base
+
       get '/sessions/new' do
         erb :'sessions/new'
       end
 
       post '/sessions' do
-        redirect '/'
+        user = User.authenticate(params[:email], params[:password])
+        session[:user_id] = user.id
+        redirect('/')
       end
+
     end
+
   end
+
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,14 +1,18 @@
-feature do
+feature 'User can sign up' do
   let(:user) { user = build(:user) }
 
-  scenario 'User can sign up' do
+  scenario 'User can sign up as a new user' do
     sign_up(user)
     expect(current_path).to eq('/')
+    expect(page).to have_content("Welcome, #{user.user_name}!")
   end
+end
+
+feature 'User can log in' do
+  let(:user) { user = build(:user) }
 
   scenario 'User can log in after signing up' do
-    sign_up(user)
-    click_button('Log out')
+    user = create(:user)
     sign_in(user)
     expect(current_path).to eq('/')
     expect(page).to have_content("Welcome, #{user.user_name}!")
