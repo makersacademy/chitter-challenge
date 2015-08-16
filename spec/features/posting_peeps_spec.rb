715514101c5cd7f8beb7d739da2e93241df408df
Leon Wee <leonwee27@gmail.com>
2015-08-16 18:02:54 +0700

    Chronological Order

--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -18,4 +18,22 @@ feature '#Posting peeps' do
     expect(current_path).to eq('/peeps')
     expect(page).to have_content('Please sign up or login first!')
   end
+
+  scenario 'Users will see peeps ordered chronologically' do
+    user = create(:user)
+    sign_in_as(user)
+    visit('/peeps/new')
+    fill_in('peep', with: 'Coding is fun')
+    click_button('Submit')
+    visit('/peeps/new')
+    fill_in('peep', with: 'Capybara tests needs refactoring')
+    click_button('Submit')
+    expect(current_path).to eq('/peeps')
+    within 'ul#peeps li:nth-child(1)' do
+      expect(page).to have_content('Capybara tests needs refactoring')
+    end
+    within 'ul#peeps li:nth-child(2)' do
+      expect(page).to have_content('Coding is fun')
+    end
+  end
 end
