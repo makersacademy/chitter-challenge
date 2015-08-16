7575b30b8ec815e0be7b142047cc1c0d1d49392b
zlahham <zlahham@gmail.com>
2015-08-16 20:46:41 +0100

    Pre Heroku

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -11,4 +11,8 @@ feature 'Creating peeps' do
       expect(page).to have_content('This is my first peep')
     end
   end
+
+  scenario 'there are no peeps in the database at the start of the test' do
+    expect(Peep.count).to eq 0
+  end
 end
