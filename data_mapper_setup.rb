09933de8dfdb0e1d933f7701b8acc9c9060cda9d
Sarah Middleton <middleton.sarahlouise@gmail.com>
2015-08-16 19:33:01 +0100

    beginnings of styling

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -4,7 +4,7 @@ require 'dm-validations'
 env = ENV['RACK_ENV'] || 'development'
 
 # we're telling datamapper to use a postgres database on localhost. The name will be "bookmark_manager_test" or "bookmark_manager_development" depending on the environment
-DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{env}")
 
 require './app/models/user'
 require './app/models/peep' # require each model individually - the path may vary depending on your file structure.
