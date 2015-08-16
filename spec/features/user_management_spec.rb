ef814dfee3a5ed9559bffd672cde48ff0aa40585
giuseppe de santis <giusdesan@gmail.com>
2015-08-16 20:39:15 +0100

    added first test

---
+++ spec/features/user_management_spec.rb
@@ -0,0 +1,11 @@
+feature 'User can sign up and sign in' do
+  let(:user) { user = build(:user) }
+
+  context 'Sign up' do
+    scenario 'I can sign up as a new user' do
+      sign_up(user)
+      expect(page).to have_content('Welcome, #{user.user_name}!')
+    end
+  end
+
+end
