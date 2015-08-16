74e7c0a6e40dbc79f66df60bb0325976d4b26dac
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-16 15:23:24 +0100

    user sees peeps on homepage

--- app/chitter_app.rb
+++ app/chitter_app.rb
@@ -3,5 +3,8 @@ require 'sinatra/base'
 require_relative 'datamapper_setup.rb'
 
 class Chitter < Sinatra::Base
-
+  get '/' do
+    @peeps = Peep.all
+    erb :'peeps/index'
+  end
 end
---
+++ spec/features/viewing_peeps_spec.rb
@@ -0,0 +1,16 @@
+feature 'Viewing Peeps' do
+  before :each do
+    create(:peep, content: "First Peep")
+    create(:peep, content: "Second Peep")
+    create(:peep, content: "Third Peep")
+  end
+
+  context 'when on homepage' do
+    scenario 'users sees peeps' do
+      visit '/'
+      expect(page).to have_content "First Peep"
+      expect(page).to have_content "Second Peep"
+      expect(page).to have_content "Third Peep"
+    end
+  end
+end
