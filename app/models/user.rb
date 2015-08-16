0da3c644d6c0cb656962fc684b8ea757a74238bd
Haroon <haroon.hassan@gmail.com>
2015-08-16 01:46:07 +0100

    email and username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,11 @@ class User
   validates_presence_of :name
   validates_presence_of :user_name
   validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_uniqueness_of :user_name
 
   def password=(password)
     self.password_digest = BCrypt::Password.create(password)
   end
-  
+
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -36,12 +36,21 @@ feature 'In order to sign up'  do
     expect{ sign_up_a(no_email) }.to change(User, :count).by(0)
     expect(page).to have_content "Email must not be blank"
   end
-  # scenario 'email must be unique' do
-  # end
-  # scenario 'username must be unique' do
-  # end
+  scenario 'email must be unique' do
+    user = User.new(name: 'John', user_name: 'Johnnyboy', email: 'john@example.com')
+    second_user = User.new(name: 'John Doe', user_name: 'Johnny', email: 'john@example.com')
+    sign_up_a user
+    expect{ sign_up_a second_user }.to change(User, :count).by(0)
+  end
+  scenario 'username must be unique' do
+    user = User.new(name: 'John', user_name: 'Johnnyboy', email: 'jo@example.com')
+    second_user = User.new(name: 'John Doe', user_name: 'Johnnyboy', email: 'john@example.com')
+    sign_up_a user
+    expect{ sign_up_a second_user }.to change(User, :count).by(0)
+  end
   scenario 'password required' do
     expect{ sign_up_a(user) }.to change(User, :count).by(1)
   end
 
 end
+
