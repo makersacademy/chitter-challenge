e90122b120de1f665fa60f61cc9b3a8e783f2c3b
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-16 17:06:16 +0100

    user is taken to home page after creating peep

--- app/chitter_app.rb
+++ app/chitter_app.rb
@@ -8,6 +8,17 @@ class Chitter < Sinatra::Base
     erb :'peeps/index'
   end
 
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  post '/peeps/new' do
+    Peep.create(content: params['content'])
+    redirect to('/')
+  end
+
+  private
+
   helpers do
     def formatted_time_stamp(time_stamp)
       time_stamp.ctime
---
+++ spec/features/creating_peeps_spec.rb
@@ -0,0 +1,22 @@
+feature 'Creating Peeps' do
+  context 'when on Create Peep page' do
+    scenario 'user sees content box' do
+      visit '/peeps/new'
+      expect(page).to have_field "content"
+    end
+  end
+
+  context 'after creating a Peep' do
+    before :each do
+      visit '/peeps/new'
+      fill_in 'content', with: "My first peep"
+      click_on 'Post'
+    end
+
+    scenario 'user is taken to home page' do
+      expect(current_path).to eq '/'
+    end
+
+    scenario 'user can see Peep on page'
+  end
+end
