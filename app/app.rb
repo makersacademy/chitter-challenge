a6d0544c69d47e68b9ff86ad56b7b60eefc1c185
zlahham <zlahham@gmail.com>
2015-08-16 15:35:54 +0100

    Set up file structure

---
+++ app/app.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+
+class Chitter < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $PROGRAM_NAME
+end
