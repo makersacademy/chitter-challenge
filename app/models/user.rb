c2abb7d8b797772e4691ab2744ba52bb310718e0
Antonio Voce <antonio.v0c3@gmail.com>
2015-08-16 02:18:50 +0100

    adds password encryption

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,18 @@
 class User
 
+  require 'bcrypt'
   include DataMapper::Resource
+  attr_reader :password
 
   property :id, Serial
   property :username, String, required: true
   property :name, String, required: true
   property :email, String, required: true, :format => :email_address
-  property :password, String
+  property :password_digest, Text
 
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
   validates_uniqueness_of :email, :username
 end
