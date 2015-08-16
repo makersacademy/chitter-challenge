d6da0c7901e2990b534d3c13d8cf81db67089f31
Ajitsy <sanghera.p@gmail.com>
2015-08-16 22:27:46 +0100

    user can view all peeps in reverse chronological order

--- spec/features/creating_peep_spec.rb
+++ spec/features/creating_peep_spec.rb
@@ -1,25 +1,3 @@
-# require 'spec_helper'
-#
-# feature 'Creating Peeps' do
-#
-#   before(:each) do
-#     User.create(email: 'sam@makersacademy.com', password: 'secret1234', first_name: 'Samuel', last_name: 'Joseph', username:'tansaku')
-#   end
-#
-#   scenario 'can post a new peep to Chitter' do
-#     sign_in(email: user.email,   password: user.password)
-#     # visit '/peeps/new_peep'
-#     # fill_in 'text', with: 'peep message'
-#     # click_button 'Submit peep'
-#     #
-#     # expect(current_path).to eq '/peeps'
-#     # expect(Peep.count).to eq(1)
-#     # within 'ul#peeps' do
-#     #   expect(page).to have_content('peep message')
-#     # end
-#   end
-# end
-
 feature 'Creating Peeps' do
 
   let(:user) do
--- spec/features/viewing_peep_spec.rb
+++ spec/features/viewing_peep_spec.rb
@@ -4,8 +4,7 @@ feature 'Viewing peeps' do
     User.create(email: 'sam@makersacademy.com', password: 'secret1234', first_name: 'Samuel', last_name: 'Joseph', username:'tansaku')
   end
 
-  scenario 'I can see existing peeps on the peeps page' do
-    expect(Peep.count).to eq(0)
+  scenario 'User can see existing peeps on the peeps page' do
     sign_in(email: user.email,   password: user.password)
     visit '/peeps/new_peep'
     fill_in 'text', with: 'peep message'
@@ -15,5 +14,29 @@ feature 'Viewing peeps' do
     within 'ul#peeps' do
       expect(page).to have_content('peep message')
     end
+  end
+
+  scenario 'User can view all peeps in reverse chronolgical order' do
+    sign_in(email: user.email,   password: user.password)
+    visit '/peeps/new_peep'
+    fill_in 'text', with: 'first message'
+    click_button 'Submit peep'
+    visit '/peeps/new_peep'
+    fill_in 'text', with: 'second message'
+    click_button 'Submit peep'
+    expect(Peep.count).to eq(2)
+    visit '/peeps'
+    expect(page.status_code).to eq 200
+    expect(find("ul#peeps > li:nth-child(1)")).to have_text("second message")
+    expect(find("ul#peeps > li:nth-child(2)")).to have_text("first message")
+    #expect(page.all('ul#peeps')).to have_content('first message')
+    # within 'ul#peeps' do
+    #   expect(page.all('/peeps')[0]).to have_content('first message')
+    # end
+    # visit '/peeps'
+    # expect(page.status_code).to eq 200
+    # within 'ul#peeps' do
+    #   expect(page).to have_content('peep message')
+    # end
   end
 end
