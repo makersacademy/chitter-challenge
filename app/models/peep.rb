554fee5f00c580a7e9080e37df643bf06657a570
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-08-16 13:30:33 +0100

    model spec file working

---
+++ app/models/peep.rb
@@ -0,0 +1,3 @@
+class Peep
+
+end
--- spec/models/peep_spec.rb
+++ spec/models/peep_spec.rb
@@ -1,4 +1,6 @@
-describe Chitter do
+require_relative '../../app/models/peep.rb'
+
+describe Peep do
   xit 'cannot be created without content'
 
   xit 'is created with a time stamp'
