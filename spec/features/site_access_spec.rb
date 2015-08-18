1fe7e96ee158d66d025be007c9f875fc48fe0531
tochman <thomas@nocebo.se>
2015-08-18 11:39:30 +0100

    pairing on 'site_access_spec'

--- spec/features/site_access_spec.rb
+++ spec/features/site_access_spec.rb
@@ -3,24 +3,24 @@ feature 'As a member' do
       let(:user) do 
         User.new(name: 'Janet', 
                  user_name: 'Jane', 
-                 email: 'janet@example.com',
-                 password: '12345') 
+                 email: 'janet@example.com') 
       end
       
-    scenario 'I can log out, after logging in' do
-      sign_up_a user
-      click_button 'Sign out'
+      before do
+        sign_up_a user, '12345'
+        click_button 'Sign out'  
+      end
+      
+    scenario 'I can log out, after logging in' do   
       expect(page).to have_content "Goodbye"  
     end
+    
     scenario 'I can sign in with a valid password' do
-      sign_up_a user
-      click_button 'Sign out'
       sign_in_a user, '12345'
       expect(page).to have_content "#{user.user_name} is logged in."
     end
-    scenario 'I cannot sign in with an invalid password' do
-      sign_up_a user
-      click_button 'Sign out' 
+    
+    scenario 'I cannot sign in with an invalid password' do 
       sign_in_a user, 'wrong'
       expect(page).to have_content "Wrong Password"
     end 
--- spec/helpers/user.rb
+++ spec/helpers/user.rb
@@ -1,11 +1,12 @@
 module UserHelpers
 
-  def sign_up_a user
+  def sign_up_a user, password
     visit '/user/new'
     fill_in :name, with: user.name
     fill_in :user_name, with: user.user_name
     fill_in :email, with: user.email
-    fill_in :password, with: user.password
+    #fill_in :password, with: user.password
+    fill_in :password, with: password
     click_button 'Sign up'
   end
   def sign_in_a user, password
