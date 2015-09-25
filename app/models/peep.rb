4081ed8b089d9461f0287decf36c0b0e2ac3457a
Jongmin Kim <kjm141215@gmail.com>
2015-09-25 20:01:41 +0100

    Delete title from peep model

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -3,7 +3,6 @@ class Peep
   include DataMapper::Resource
 
   property :id, Serial
-  property :title, String
   property :content, Text
 
 end
--- spec/features/viewing_peep_spec.rb
+++ spec/features/viewing_peep_spec.rb
@@ -1,11 +1,10 @@
 require './app/models/peep'
 feature 'Viewing peep' do
   scenario 'I can view peep on the screen' do
-    Peep.create(title: 'Friday', content: 'TGI Friday!')
+    Peep.create(content: 'TGI Friday!')
     visit '/peeps'
     expect(page.status_code).to eq 200
     within('ul#peeps') do
-      expect(page).to have_content('Friday')
       expect(page).to have_content("TGI Friday!")
     end
   end
