d6833d40e8a5142fa601d0de3ad3d672ea9c492d
Michael Roger <bodgeuk12@gmail.com>
2015-09-25 19:10:46 +0100

    Test added for matching password when signing up

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,10 +1,15 @@
 feature 'New user sign up' do
 
-  let(:user){build :user}
-
   scenario 'I can sign up as a new user' do
+    user = build :user
     expect{ sign_up(user) }.to change(User, :count).by(1)
     expect(page).to have_content "Welcome to Chitter, MickTheMiller"
+  end
+
+  scenario 'password and confirmation must match' do
+    user = build(:user, password_confirmation: 'wrong')
+    expect{ sign_up(user) }.not_to change(User, :count)
+    expect(page).to have_content "Password and confirmation password don't match"
   end
 
   def sign_up(user)
