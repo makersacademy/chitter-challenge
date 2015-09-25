ee499001590e5cae652f4d18514f0c773b81d49e
zsid <zhivko.siderov@gmail.com>
2015-09-25 23:02:36 +0100

    Added a sign-up form

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,10 @@
 require 'sinatra/base'
+require './app/data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -21,7 +21,6 @@ feature 'User can register' do
     visit '/'
     fill_in 'email',                 with: user.email
     fill_in 'password',              with: user.password
-    fill_in 'password_confirmation', with: user.password_confirmation
     fill_in 'name',                  with: user.name
     fill_in 'username',              with: user.username
     click_button 'Sign up'
