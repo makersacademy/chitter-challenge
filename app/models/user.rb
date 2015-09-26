921879058d36bfe962b58f261fbd6ed7c8582148
philip <philip.clarke@redhood.co.uk>
2015-09-26 09:59:14 +0100

    changed user_mangement_spec.rb test with a new method for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
+  property :name, String, required: true
   property :username, String, required: true, unique: true
   property :email, String, required: true, unique: true
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,12 +4,19 @@ feature 'User sign up' do
 
 
   scenario 'I can sign up as a new user' do
-    user = User.new
-    fill_in :username, with: 'pip'
-    fill_in :username, with:("pip@example.com")
-    click_button 'Sign up'
+    expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, pip')
-    expect(User.first.email).to eq('dom@example.com')
+    expect(User.first.email).to eq('pip@example.com')
   end
 
+
+  def sign_up(email: 'pip@example.com',
+              password: 'sausages')
+      visit '/users/new'
+      expect(page.status_code).to eq(200)
+      fill_in :name, with: 'Philip Clarke'
+      fill_in :username, with: 'pip'
+      fill_in :email, with:("pip@example.com")
+      click_button 'Sign up'
+  end
 end
