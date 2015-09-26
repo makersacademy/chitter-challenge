97d67b5f1e1ddd7350094b12805b5884d39f1fe5
Emily Worrall <worrall.emily@gmail.com>
2015-09-26 16:29:18 +0100

    user can only write peep if they are logged in

--- spec/features/writing_peeps_spec.rb
+++ spec/features/writing_peeps_spec.rb
@@ -16,4 +16,12 @@ feature 'Writing peeps' do
   scenario 'there are no peeps in the database at the start of the test' do
     expect(Peep.count).to eq 0
   end
+
+  scenario 'I can only write a peep if logged in' do
+    user = create :user
+    sign_in(user)
+    click_button 'Sign out'
+    visit '/feed'
+    expect(page).not_to have_selector("input[type=submit][value='Write peep']")
+  end
 end
