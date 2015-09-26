30d40d056407078ba42b819e5ca230c6ffacb049
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-26 17:48:08 +0100

    little refactoring

--- app/chitter_web.rb
+++ app/chitter_web.rb
@@ -8,12 +8,6 @@ class Chitter < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
 
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id]) if session[:user_id]
-    end
-  end
-
   get '/' do
     erb :index
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'data_mapper'
+require './app/data_mapper_setup'
 require 'bcrypt'
 
 class User
@@ -10,7 +12,7 @@ class User
   property :id, Serial
   property :name, String
   property :username, String, unique: true, required: true
-  property :email, String, unique: true, required: true, format: :email_address
+  property :email, String, unique: true, required: true
   property :password_digest, Text
 
   validates_confirmation_of :password,
@@ -32,46 +34,3 @@ class User
   end
 
 end
-
-# require 'data_mapper'
-# require './app/data_mapper_setup'
-# require 'bcrypt'
-#
-# class User
-#
-#   include DataMapper::Resource
-#
-#   attr_reader :password
-#   attr_accessor :password_confirmation
-#
-#   property :id, Serial
-#   property :email, String, required: true
-#   property :name, String
-#   property :username, String
-#   property :password_digest, Text
-#   property :password_confirmation, Text
-#
-#   validates_confirmation_of :password,
-#     message: "Password and confirmation password don't match"
-#
-#   validates_uniqueness_of   :email,
-#     messages: { is_unique: "Email already taken",
-#                   presence: "Email address needed"}
-#
-#   def password=(password)
-#     @password = password
-#     self.password_digest = BCrypt::Password.create(password)
-#   end
-#
-#   def self.authenticate(email, password)
-#     user = first(email: email)
-#     if user && BCrypt::Password.new(user.password_digest) == password
-#       user
-#     else
-#       nil
-#     end
-#   end
-#
-#
-#
-# end
