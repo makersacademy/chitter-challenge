61eb971238889e6c4b7e5ac3772127247d53667f
Jongmin Kim <kjm141215@gmail.com>
2015-09-26 20:03:32 +0100

    Add more examples to user_spec

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,6 +1,14 @@
 describe User do
+  let(:user) { create(:user) }
   it 'authenticates a user with valid information' do
-    user = create(:user)
     expect(User.authenticate(user.email, user.password)).to eq(user)
+  end
+
+  it 'does not authenticate with invalid email' do
+    expect(User.authenticate('wrong@email.com', user.password)).to be_nil
+  end
+
+  it 'does not authenticate with invalid password' do
+    expect(User.authenticate(user.email, 'wrong_password')).to be_nil
   end
 end
