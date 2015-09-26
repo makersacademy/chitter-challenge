dfd1b3071dec074a1c9dacff426ef93ff2bebfd0
philip <philip.clarke@redhood.co.uk>
2015-09-26 10:15:22 +0100

    sign up form created

--- app/app.rb
+++ app/app.rb
@@ -5,9 +5,21 @@ require_relative 'data_mapper_setup'
 
 class ChitterWeb < Sinatra::Base
 
+  get '/' do
+    'hello world'
+  end
 
+  get '/users/new' do
+    erb :'new_users'
+  end
 
-
+  post 'users/new' do
+    User.create(email: params[:email],
+                name: params[:name],
+                username: params[:username],
+                password: params[:password])
+    redirect to('/')
+  end
 
 
 
