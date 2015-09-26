e33f118753dd7bc7233dce2088df6aac1b4ef680
Usman Jamil <usman.jamil@live.co.uk>
2015-09-26 22:04:19 +0100

    tests passing after adding restriction to who can post peeps

--- spec/features/adding_hash_tags_spec.rb
+++ spec/features/adding_hash_tags_spec.rb
@@ -1,11 +1,11 @@
 feature 'Adding hash tags' do
 
   scenario 'I can add a single hash tag to a new peep' do
-    visit '/peep/new'
+    user = build :user
+    sign_up(user)
+    click_button 'Peep'
     fill_in 'message',   with: 'hi, it is saturday'
-
     fill_in 'tag',  with: 'day'
-
     click_button 'Post peep'
     peep = Peep.first
     expect(peep.tags.map(&:name)).to include 'day'
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -10,11 +10,18 @@ feature 'Viewing peeps' do
   feature 'Creating peeps' do
 
     scenario 'I can create a new peep' do
-      visit '/peep/new'
-      fill_in 'message',   with: 'today is saturday'
+      user = build :user
+      sign_up(user)
+      click_button 'Peep'
+      fill_in 'message', with: 'today is saturday'
       click_button 'Post peep'
       expect(current_path).to eq '/'
-      expect(page).to have_content('today is saturda')
+      expect(page).to have_content('today is saturday')
+    end
+
+    scenario 'Only members can post peeps' do
+      visit '/peep/new'
+      expect(page).to have_content('You must sign up first to peep or sign in!')
     end
   end
 
@@ -26,7 +33,7 @@ feature 'Viewing peeps' do
                 tags: [Tag.first_or_create(name: 'education')])
   end
 
-  scenario 'I can filter links by tag' do
+  scenario 'I can filter peeps by tag' do
     visit '/tags/education'
       expect(page).not_to have_content('Makers Academy')
       expect(page).not_to have_content('Code.org')
