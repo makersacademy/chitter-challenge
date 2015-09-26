b34adff429dee63a9b69abdf2c96b1c417bc4449
Usman Jamil <usman.jamil@live.co.uk>
2015-09-26 23:07:23 +0100

    added test for displaying username with peeps but not passed

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -7,25 +7,37 @@ feature 'Viewing peeps' do
       expect(page).to have_content('Hello')
   end
 
-  feature 'Creating peeps' do
+  scenario 'Peeps display the username of poster' do
+    user = build :user
+    sign_up(user)
+    click_button 'Peep'
+    fill_in 'message', with: 'today is saturday'
+    click_button 'Post peep'
+    expect(current_path).to eq '/'
+    expect(page).to have_content('foooo')
+  end
+end
 
-    scenario 'I can create a new peep' do
-      user = build :user
-      sign_up(user)
-      click_button 'Peep'
-      fill_in 'message', with: 'today is saturday'
-      click_button 'Post peep'
-      expect(current_path).to eq '/'
-      expect(page).to have_content('today is saturday')
-    end
+feature 'Creating peeps' do
 
-    scenario 'Only members can post peeps' do
-      visit '/peep/new'
-      expect(page).to have_content('You must sign up first to peep or sign in!')
-    end
+  scenario 'I can create a new peep' do
+    user = build :user
+    sign_up(user)
+    click_button 'Peep'
+    fill_in 'message', with: 'today is saturday'
+    click_button 'Post peep'
+    expect(current_path).to eq '/'
+    expect(page).to have_content('today is saturday')
   end
 
+  scenario 'Only members can post peeps' do
+    visit '/peep/new'
+    expect(page).to have_content('You must sign up')
+  end
+end
 
+
+feature 'Filtering peeps' do
   before(:each) do
     Peep.create(message: 'http://www.makersacademy.com',
                 tags: [Tag.first_or_create(name: 'education')])
