f8a0b1f5a0c7bc4b5727f0be6d024fca2cd8b892
gavin.w.cheng <gavin.w.cheng@gmail.com>
2015-09-26 21:29:03 +0100

    Setting up

---
+++ lib/chitter.rb
@@ -0,0 +1,11 @@
+require 'sinatra/base'
+require_relative 'data_mapper_setup'
+
+class Chitter < Sinatra::Base
+  get '/' do
+    'Hello Chitter!'
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
---
+++ lib/data_mapper_setup.rb
@@ -0,0 +1,8 @@
+require 'data_mapper'
+require './lib/models/user'
+
+env = ENV['RACK_ENV'] || 'development'
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{env}")
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
