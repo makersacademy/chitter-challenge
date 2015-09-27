04ba578ca4cb612fa1edf3a4483de1e99a401cd1
chweeks <chrisharryweeks@gmail.com>
2015-09-27 19:05:50 +0100

    added flash notice

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
 require './app/helpers'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
 
@@ -8,6 +9,7 @@ class Chitter < Sinatra::Base
 
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   get '/peeps' do
     @peeps = Peep.all
@@ -44,8 +46,13 @@ class Chitter < Sinatra::Base
                 handle: params[:handle],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/peeps')
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/peeps')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   run! if app_file == Chitter
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -22,4 +22,10 @@ feature 'User sign up' do
     expect {sign_up(password_confirmation: 'wrong')}.not_to change(User,:count)
   end
 
+  scenario 'with a password that does not match' do
+  expect {sign_up(password_confirmation: 'wrong')}.not_to change(User,:count)
+  expect(current_path).to eq('/users')
+  expect(page).to have_content 'Password and confirmation password do not match'
+end
+
 end
