a76545076c239e64a8b55067d04f46fb28d1ca67
Adrian Booth <Adrian.booth17@gmail.com>
2015-09-27 16:20:03 +0100

    Signing in test passing

--- app/app.rb
+++ app/app.rb
@@ -48,5 +48,21 @@ end
     end
   end
 
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:username], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect '/peeps'
+    else
+      flash.now[:errors] = ["The username or password is incorrect"]
+      erb :'sessions/new'
+    end
+  end
+
+
 run! if app_file == $0
 end
--- spec/features /user_management_spec.rb
+++ spec/features /user_management_spec.rb
@@ -5,7 +5,7 @@ feature 'User sign up' do
   scenario 'I can sign up as a new user' do
     user = User.new(email: "Adrian@gmail.com", username: "Adrian17", password: 'password', password_confirmation: 'password')
     expect { sign_up(user) }.to change(User, :count).by(1)
-    expect(page).to have_content("Welcome, Adrian@gmail.com")
+    expect(page).to have_content("Welcome, Adrian17")
     expect(User.first.email).to eq("Adrian@gmail.com")
   end
 
@@ -37,6 +37,26 @@ feature 'User sign up' do
   fill_in :password, with: user.password
   fill_in :password_confirmation, with: user.password_confirmation
   click_button 'Sign up'
+  end
+
+end
+
+feature 'User sign in' do
+
+  let(:user) do
+    create :user
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(username: user.username, password: user.password)
+    expect(page).to have_content "Welcome, #{user.username}"
+  end
+
+  def sign_in(username:, password:)
+    visit '/sessions/new'
+    fill_in :username, with: username
+    fill_in :password, with: password
+    click_button 'Sign in'
   end
 
 end
