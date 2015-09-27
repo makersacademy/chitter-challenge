626ec069d2ae5be673a7c386d6a214b61cccd52b
Jongmin Kim <kjm141215@gmail.com>
2015-09-27 17:39:47 +0100

    Add datamapper_setup for production

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,5 +6,9 @@ require 'dm-validations'
 env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, "postgres://localhost/chitter_#{env}")
 
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
+
 DataMapper.finalize
 DataMapper.auto_upgrade!
