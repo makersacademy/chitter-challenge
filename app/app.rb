affe14111acadcfbb36cd258b914fab74d7a1872
Dom Batten <dominic.batten@googlemail.com>
2015-09-27 15:42:05 +0100

    Test passing for password validation

--- app/app.rb
+++ app/app.rb
@@ -46,6 +46,21 @@ class Chitter < Sinatra::Base
     end
   end
 
+  get '/sessions/new' do
+    erb :new_sessions
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect('/feed')
+    else
+      flash.now[:errors] = ['The email or password is incorrect']
+      erb :new_sessions
+    end
+  end
+
 
 
   # start the server if ruby file executed directly
---
+++ spec/features/user_sign_in_spec.rb
@@ -0,0 +1,17 @@
+feature 'User sign in' do
+
+  # scenario 'Clicking sign in takes me to the sign in page' do
+  #   visit '/users/new'
+  #   click_button('Sign in')
+  #   expect(current_path).to eq('/sessions/new')
+  #   expect(page).to have_content('Sign In')
+  # end
+
+  scenario 'I am welcomed after signing in' do
+    visit '/sessions/new'
+    user = create :user
+    sign_in_as(user)
+    expect(page).to have_content("Welcome, dbatten")
+  end
+
+end
