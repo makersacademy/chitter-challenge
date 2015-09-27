ecdb37683a0407f561d9ed4038e3e38c62e06705
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-09-27 19:35:53 +0100

    required factory_girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,50 @@
+require 'capybara'
+require 'capybara/rspec'
 require 'coveralls'
+require 'rspec'
 require 'simplecov'
+require 'database_cleaner'
+require 'factory_girl'
+require_relative 'helpers/session'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', '/app/chitter.rb')
+
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+
+  config.include Capybara::DSL
+
+  config.include FactoryGirl::Syntax::Methods
+
+  # config.include SessionHelpers
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+  #
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
+  #
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
