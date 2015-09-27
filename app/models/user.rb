45e49e523da190fe2a8517d929abc5b2550c4b9a
catherinestevenson <catherine.stevenson14@yahoo.co.uk>
2015-09-27 19:42:23 +0100

    added User model

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+require 'bcrypt'
+
+class User
+
+  include DataMapper::Resource
+
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  property :id, Serial
+  property :email, String, required: true, unique: true
+  property :name, String, required: true
+  property :username, String, required: true, unique: true
+  property :password_digest, Text
+
+  has n, :peeps, through: Resource
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
+  end
+end
