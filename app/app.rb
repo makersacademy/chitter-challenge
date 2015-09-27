1e3d5da73039ae5b3b43b608e3d6f14aecc2ebf4
ana <adscarlos@gmail.com>
2015-09-27 22:39:56 +0100

    adds sessions and helper method to sign up functionality

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,12 @@ class Chitter < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
 
+  helpers do
+    def current_user
+      @current_user = User.get(session[:user_id])
+    end
+  end
+
   get '/' do
     erb :index
   end
@@ -15,10 +21,11 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    User.create(name: params[:name],
-                username: params[:username],
-                email: params[:email],
-                password: params[:password])
+    user = User.create(name: params[:name],
+                       username: params[:username],
+                       email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
     redirect to('/')
   end
 
