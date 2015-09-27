59bfc566ef7dcaba7586c68d5a288a43697d6cef
Harriet Craven <harrietcraven@Harriets-MacBook-Pro.local>
2015-09-27 15:14:55 +0100

    name and username is displayed with peep

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -5,7 +5,10 @@ feature 'Viewing peeps' do
 
   scenario 'User does not have to be signed in' do
     user = create(:user)
-    Peep.create(content: 'First peep', username: user.username, name: user.name, time: Time.now)
+    Peep.create(content: 'First peep',
+                username: user.username,
+                name: user.name,
+                time: Time.now)
     visit '/peeps'
     expect(page.status_code).to eq 200
 
@@ -16,7 +19,10 @@ feature 'Viewing peeps' do
 
   scenario 'peep displays name of user' do
     user = create(:user)
-    Peep.create(content: 'First peep', username: user.username, name: user.name, time: Time.now)
+    Peep.create(content: 'First peep',
+                username: user.username,
+                name: user.name,
+                time: Time.now)
     visit '/peeps'
 
     within 'ul#peeps' do
