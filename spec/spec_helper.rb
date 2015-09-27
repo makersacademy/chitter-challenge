d47dfee29517b4ea36748b0e4927981984d4e5eb
ana <adscarlos@gmail.com>
2015-09-27 13:17:14 +0100

    minor formatting changes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,6 @@ require 'database_cleaner'
 
 require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
 
-
-
 Capybara.app = Chitter
 
 RSpec.configure do |config|
@@ -33,11 +31,9 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
   end
 
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
