69b430983f9c4bcfa91771a4c348e98f5e7a5606
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-28 08:43:39 +0100

    calm down hound

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,8 @@ require 'data_mapper'
 require 'dm-timestamps'
 
 env = ENV['RACK_ENV'] || 'development'
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{env}")
+LOCAL_DATABASE = "postgres://localhost/chitter_challenge_#{env}"
+DataMapper.setup(:default, ENV['DATABASE_URL'] || LOCAL_DATABASE)
 
 require './app/models/peep'
 require './app/models/user'
