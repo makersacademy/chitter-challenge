2be767f053ac57ae94ff8b6551dd8a98efcc7e9e
Anindya Bhattacharyya <bat020@gmail.com>
2015-09-28 08:14:33 +0100

    heroku database setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 require 'dm-timestamps'
 
 env = ENV['RACK_ENV'] || 'development'
-DataMapper.setup(:default, "postgres://localhost/chitter_challenge_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{env}")
 
 require './app/models/peep'
 require './app/models/user'
