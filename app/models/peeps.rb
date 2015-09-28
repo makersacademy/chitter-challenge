c0bd9ac87e7acbecc037dbeaf95670052e3447a7
kyle macpherson <kyle1@kyles-MacBook-Pro.local>
2015-09-28 09:58:22 +0100

    changed peeps message property to text instead of string

--- app/models/peeps.rb
+++ app/models/peeps.rb
@@ -2,7 +2,7 @@ class Peeps
   include DataMapper::Resource
 
   property :id, Serial
-  property :message, String
+  property :message, Text
   property :user_id, Integer
   property :created_at, DateTime
   belongs_to :user
