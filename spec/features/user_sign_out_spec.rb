581d49bcf07f90e4e19d30f3fd5473711872f25a
Dom Batten <dominic.batten@googlemail.com>
2015-09-28 09:40:50 +0100

    Added buttons to return to the home screen and feed in the goodbye page

--- spec/features/user_sign_out_spec.rb
+++ spec/features/user_sign_out_spec.rb
@@ -10,8 +10,26 @@ feature 'User signs out' do
     user = create :user
     sign_in_as(user)
     click_button 'Sign out'
-    expect(page).to have_content('goodbye!')
+    expect(page).to have_content('come back soon')
     expect(page).not_to have_content('Welcome, test@test.com')
+  end
+
+  scenario 'i can go back to the homescreen after signing out' do
+    user = create :user
+    sign_in_as(user)
+    click_button 'Sign out'
+    expect(page).to have_button('Home')
+    click_button 'Home'
+    expect(current_path).to eq('/')
+  end
+
+  scenario 'i can go back to the feed after signing out' do
+    user = create :user
+    sign_in_as(user)
+    click_button 'Sign out'
+    expect(page).to have_button('Feed')
+    click_button 'Feed'
+    expect(current_path).to eq('/feed')
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,8 +9,8 @@ describe User do
  end
 
  it 'authenticates when given a valid email address and password' do
-   authenticated_user = User.authenticate(user.email, user.password)
-   expect(authenticated_user).to eq user
+  authenticated_user = User.authenticate(user.email, user.password)
+  expect(authenticated_user).to eq user
  end
 
  it 'does not authenticate when given an incorrect password' do
