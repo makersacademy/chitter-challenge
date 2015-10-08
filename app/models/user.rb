c04e7fa0daa447c4f6fc68f151d7095385bb3ae4
ana <adscarlos@gmail.com>
2015-10-08 14:27:27 +0100

    prevents duplicate registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,17 @@ class User
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
-  validates_format_of :email, as: :email_address
 
-  property :id, Serial
-  property :email, String, required: true
-  property :name, String
-  property :username, String
+  property :id,              Serial
+  property :email,           String, required: true, unique: true,
+    format: :email_address,
+    messages: {
+      :presence  => "We need your email address.",
+      :is_unique => "We already have that email.",
+      :format    => "Doesn't look like an email address to me ..."
+    }
+  property :name,            String
+  property :username,        String
   property :password_digest, Text
 
   def password=(password)
--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -1,19 +1,31 @@
 feature "User sign up" do
 
-  def sign_up(name: 'Person',
-              username: 'pers0n',
-              email: 'person@example.com',
-              password: 'bananas!',
+  def sign_up(name:                  'Person',
+              username:              'pers0n',
+              email:                 'person@example.com',
+              password:              'bananas!',
               password_confirmation: 'bananas!')
     visit 'users/sign_up'
     expect(page.status_code).to eq(200)
-    fill_in :name,     with: name
-    fill_in :username, with: username
-    fill_in :email,    with: email
-    fill_in :password, with: password
+    fill_in :name,                  with: name
+    fill_in :username,              with: username
+    fill_in :email,                 with: email
+    fill_in :password,              with: password
     fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up for Chitter'
   end
+
+# #--> FactoryGirl refactoring
+# def sign_up_as(user)
+#   visit 'users/sign_up'
+#
+#   fill_in :name,                  with: user.name
+#   fill_in :username,              with: user.username
+#   fill_in :email,                 with: user.email
+#   fill_in :password,              with: user.password
+#   fill_in :password_confirmation, with: user.password_confirmation
+#   click_button 'Sign up for Chitter'
+# end
 
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
@@ -28,15 +40,21 @@ feature "User sign up" do
   scenario 'returns an error when a password does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq ('/users')
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content('Password does not match the confirmation')
   end
 
   scenario 'requires an email' do
-    expect { sign_up(email: '')}.not_to change(User, :count)
+    expect { sign_up(email: '') }.not_to change(User, :count)
   end
 
   scenario 'requires a valid email address' do
-    expect {sign_up(email: 'test')}.not_to change(User, :count)
+    expect { sign_up(email: 'test') }.not_to change(User, :count)
+  end
+
+  scenario 'I cannot sign up with an existing email' do
+    sign_up
+    expect{ sign_up }.to change(User, :count).by(0)
+    expect(page).to have_content('We already have that email.')
   end
 
 end
