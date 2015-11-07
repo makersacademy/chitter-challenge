a39c40b6e04d1911fd1d07feb73771c3a23bb5f8
alaanzr <alaanzr@gmail.com>
2015-11-07 22:50:24 +0000

    Heroku database environment added

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,6 +2,6 @@ require 'data_mapper'
 require './model/maker'
 require './model/peep'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_migrate!
