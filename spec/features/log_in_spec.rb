d314708677e8e66847cf271d2a0533c8ac0f3641
alaanzr <alaanzr@gmail.com>
2015-11-07 17:14:56 +0000

    UI clean-up

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -26,13 +26,12 @@ feature "Registered users can log in" do
   end
 
   scenario "A user logs in successfully when supplying the correct credentials" do
-    sign_up
-    sign_in(maker.name, maker.username, maker.email, maker.password_digest)
+    Maker.create(name: 'Alaan', username: 'Alaanzr', email: 'Alaanzr@gmail.com', password: 'password')
+    sign_in(maker.name, maker.username, maker.email, maker.password)
     expect(page).to have_content("Welcome #{maker.name}!")
   end
 
   scenario "A user receives an error message when they attempt to sign in with an incorrect password" do
-    sign_up
     sign_in(maker.name, maker.username, maker.email, 'incorrect password')
     expect(page).to have_content('Incorrect email or password')
   end
