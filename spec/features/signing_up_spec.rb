39cb5135eb778dd520eb47631d964a55fdf9fb8f
Tom Barnes <ruralmyth@gmail.com>
2015-11-07 13:00:18 +0000

    Initial commit. Implemented signing in.

---
+++ spec/features/signing_up_spec.rb
@@ -0,0 +1,56 @@
+feature 'Signing up:' do
+  scenario 'User can sign up' do
+    visit '/users/new'
+    expect(page.status_code).to eq(200)
+    expect { sign_up }.to change(User, :count).by 1
+    expect(page).to have_content('Hi, incredible@yev.com')
+    expect(User.first.email).to eq('incredible@yev.com')
+  end
+
+  scenario 'Requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq ('/users')
+    expect(page).to have_content 'Password does not match the confirmation'
+  end
+
+  scenario "User can't sign up without an email address" do
+    visit '/users/new'
+    expect(page.status_code).to eq(200)
+    fill_in :name, with: 'Yev Insomniak'
+    fill_in :username, with: 'yev_insomniak'
+    fill_in :password, with: 'bla-bla-bla'
+    fill_in :password_confirmation, with: 'bla-bla-bla'
+    click_button 'Sign up!'
+    expect(page).to have_content('Email must not be blank')
+  end
+
+  scenario "User can't sign up without a valid email address" do
+    visit '/users/new'
+    expect(page.status_code).to eq(200)
+    fill_in :name, with: 'Yev Insomniak'
+    fill_in :username, with: 'yev_insomniak'
+    fill_in :email, with: 'incredibleyev.com'
+    fill_in :password, with: 'bla-bla-bla'
+    fill_in :password_confirmation, with: 'bla-bla-bla'
+    click_button 'Sign up!'
+    expect(page).to have_content("Doesn't look like an email address to me")
+  end
+
+  scenario "Can't sign up twice with the same email address" do
+    sign_up
+    sign_up(name: 'Another Yev',
+            username: 'another_yev',
+            password: 'alb-alb-alb',
+            password_confirmation: 'alb-alb-alb')
+    expect(page).to have_content "Email is already taken"
+  end
+
+    scenario "Can't sign up twice with the same username" do
+    sign_up
+    sign_up(name: 'Another Yev',
+            email: 'another@yev.com',
+            password: 'alb-alb-alb',
+            password_confirmation: 'alb-alb-alb')
+    expect(page).to have_content "Username is already taken"
+  end
+end
---
+++ spec/web_helpers.rb
@@ -0,0 +1,31 @@
+def sign_up(name: 'Yev Insomniak',
+            username: 'yev_insomniak',
+            email: 'incredible@yev.com',
+            password: 'bla-bla-bla',
+            password_confirmation: 'bla-bla-bla')
+  visit '/users/new'
+  fill_in :name, with: name
+  fill_in :username, with: username  
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
+  click_button 'Sign up!'
+end
+
+def sign_in
+  visit '/sessions/new'
+  fill_in :email, with: 'incredible@yev.com'
+  fill_in :password, with: 'bla-bla-bla'
+  click_button 'Sign in'
+end
+
+def sign_out
+  click_button 'Sign out'
+end
+
+def sign_in_with_wrong_password
+  visit '/sessions/new'
+  fill_in :email, with: 'incredible@yev.com'
+  fill_in :password, with: 'the-yev'
+  click_button 'Sign in'
+end
