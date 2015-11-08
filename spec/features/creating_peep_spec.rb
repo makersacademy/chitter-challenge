0dd1e209f62685cd099661ae077b82e5763f1c8b
jbhdeconinck <jbhdeconinck@gmail.com>
2015-11-08 23:18:53 +0000

    final commit, pre-refactoring

--- spec/features/creating_peep_spec.rb
+++ spec/features/creating_peep_spec.rb
@@ -12,6 +12,6 @@ feature "creating a peep" do
     expect{create_peep(email: user.email,password: user.password,content: 'Nadal just won the Roland Garros final')}.to change(Peep, :count).by(1)
     expect(page).to have_content('Nadal just won the Roland Garros final')
     expect(page).to have_content(user.name)
-    expect(page).to have_content(Time.now)
+    expect(page).to have_content(Time.new.strftime("Sent on %d/%m/%Y at %I:%M%p"))
   end
 end
