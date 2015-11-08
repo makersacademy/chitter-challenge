dd63f95602dd7f71cb71c868eb13dd309eebff3d
Yev Dyko <evgeny.dyko@gmail.com>
2015-11-09 02:27:11 +0300

    Updated password confirmation

--- app/chitter.rb
+++ app/chitter.rb
@@ -7,6 +7,7 @@ require_relative 'data_mapper_setup'
 class Chitter < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   helpers do
     def current_user
@@ -28,8 +29,13 @@ class Chitter < Sinatra::Base
                         email: params[:email],
                         password: params[:password],
                         password_confirmation: params[:password_confirmation])
-    session[:user_id] = @user.id
-    redirect to('/')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   # start the server if ruby file executed directly
