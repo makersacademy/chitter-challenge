ef376fd0112d9e9b0f84298ec45b4a8ae5222e19
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-08 20:16:32 +0000

    create sign in spec

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,4 +5,3 @@ require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
---
+++ spec/features/sign_in_spec.rb
@@ -0,0 +1,16 @@
+feature 'User sign in' do
+  let(:user) do
+    User.create(name: 'Dave',
+                username: 'Big Dave',
+                email: 'dave@email.com',
+                password: 'secret123')
+  end
+
+  scenario 'with correct credentials' do
+    visit '/'
+    fill_in :username, with: 'Big Dave'
+    fill_in :password, with: 'secret123'
+    click_button 'Sign in'
+    expect(page).to have_content "Welcome, #{user.username}"
+  end
+end
