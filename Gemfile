8de3ed3a6ce6f63572bbc06622546f0b41b42290
Eleni Skouroupathi <eleni59@hotmail.com>
2015-11-09 09:06:11 +0000

    First data mapper setup

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 ruby '2.2.3'
 
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+
 group :test do
   gem 'rspec'
   gem 'cucumber'
---
+++ spec/Features/users_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+feature "User sign up" do
+	scenario "I can sign up as a new user" do
+		visit "/"
+		click_button "sign up"
+		expect(page.status_code).to eq 200
+		fill_in :email, with: "eleni_s_2015@hotmail.com"
+		fill_in :password, with: "Passw0rd"
+		fill_in :name, with: "Eleni Skouroupathi"
+		fill_in :user_name, with: "Dubai_Girl_15"
+	click_button "Create user"
+	expect(page).to have_content "Welcome Eleni"
+end
+end
