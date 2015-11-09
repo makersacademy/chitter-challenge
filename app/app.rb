a4025f88fc67d358c1ad4edb0989db7bef36b0d9
George Maddocks <georgemaddocks@yahoo.co.uk>
2015-11-09 10:08:00 +0000

    fixed duplicate email/username bug

--- app/app.rb
+++ app/app.rb
@@ -17,7 +17,7 @@ class Chitter < Sinatra::Base
 
   post '/signup' do
     users = User.all
-    if !(User.get(email: params[:email]).nil?) || !(User.get(username: params[:username]).nil?)
+    if User.count(email: params[:email]) + User.count(username: params[:username]) > 0
       redirect '/signuperror'
     else
     user = User.create(username: params[:username],
