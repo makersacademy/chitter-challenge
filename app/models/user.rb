df03757bd8b965963c6e1aedabda5fa9732c766c
Fergus Lemon <fergus.lemon@gmail.com>
2015-11-09 08:04:14 +0000

    refactor sign in to authenticate user

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,16 @@ class User
                                :messages => { :presence => 'Please enter an email address',
                                              :is_unique => 'We already have that email',
                                              :format => 'Please enter a valid email address' }
-  property :password,  BCryptHash
+  property :password_digest,  Text
+
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
   def self.authenticate(email, password)
     user = first(email: email)
-    if user && BCrypt::Password.new(user.password) == password
+    if user && BCrypt::Password.new(user.password_digest) == password
        user
     else
        nil
