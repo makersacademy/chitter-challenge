ec31d7e394ba2c44fcb81cb76073a9ded729dfe6
Robert Youngman <rob_youngman@yahoo.com>
2015-11-09 11:18:46 +0000

    refactoring hound

--- app/app.rb
+++ app/app.rb
@@ -19,7 +19,7 @@ class Chitter < Sinatra::Base
     erb :'sessions/new'
   end
 
-  post'/sessions' do
+  post '/sessions' do
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
@@ -72,7 +72,8 @@ class Chitter < Sinatra::Base
   end
 
   post '/peeps' do
-    Peep.create(message: params[:message], name: current_user.name, user_name: current_user.user_name, time_stamp: Time.now)
+    Peep.create(message: params[:message], name: current_user.name,
+      user_name: current_user.user_name, time_stamp: Time.now)
     redirect to('/peeps')
   end
 
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,6 +2,7 @@ require 'data_mapper'
 require_relative './models/peep'
 require_relative './models/user'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||
+  "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
