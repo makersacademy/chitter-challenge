922d4275e2076f6095a218cfbd85e9d6e31c48a0
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-11-12 18:22:04 +0000

    minor change

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,20 +16,20 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
+    RSpec.configure do |config|
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
 
-end
+  end
 
 end
