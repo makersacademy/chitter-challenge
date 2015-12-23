b2fb57703cd3a142b0febe555d66c6027cec5774
emmabeynon <emma.beynon@gmail.com>
2015-12-23 17:17:37 +0000

    Messages now show user's name and username above the message.

--- spec/features/post_message_spec.rb
+++ spec/features/post_message_spec.rb
@@ -2,18 +2,18 @@
 # So that I can let people know what I am doing
 # I want to post a message (peep) to chitter
 feature 'Post message' do
-
-  scenario 'when logged in' do
+  before do
     sign_up
     log_in
+  end
+
+  scenario 'when logged in' do
     fill_in(:message, with: 'My first peep')
     click_button 'Peep!'
     expect(page).to have_content('My first peep')
   end
 
   scenario 'cannot happen if logged out' do
-    sign_up
-    log_in
     click_button('Log out')
     visit '/peeps'
     fill_in(:message, with: 'This peep shouldn\'t be here')
@@ -22,7 +22,10 @@ feature 'Post message' do
     expect(page).to have_content('Oops! You need to be logged in to peep.')
   end
 
-  xscenario 'displays name and username' do
-
+  scenario 'displays name and username' do
+    fill_in(:message, with: 'My first peep')
+    click_button 'Peep!'
+    expect(page).to have_content('My first peep')
+    expect(page).to have_content('Maker Makerson @makermakerson')
   end
 end
