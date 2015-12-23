9f637103b903fce8e2b3335d1d48a3dec2bdf1c0
emmabeynon <emma.beynon@gmail.com>
2015-12-23 10:00:05 +0000

    User now sees a congratulations message that displays their chosen username after signing up successfully.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,5 +1,4 @@
 class Chitter < Sinatra::Base
-  enable :sessions
 
   get '/user/new' do
     erb :'user/new'
@@ -9,15 +8,16 @@ class Chitter < Sinatra::Base
     user = User.create(name: params[:name], email: params[:email],
                 username: params[:username], password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:username] = user.username
-    if user
+
+    if user.save
+      session[:user_id] = user.id
       redirect '/peeps'
     end
 
   end
 
   get '/peeps' do
-    @username = session[:username]
+    # @username = session[:username]
     erb :'peeps'
   end
 
--- app/server.rb
+++ app/server.rb
@@ -1,5 +1,12 @@
 class Chitter < Sinatra::Base
   set :root, File.dirname(__FILE__)
+  enable :sessions
+
+  helpers do
+      def current_user
+        @current_user ||= User.get(session[:user_id])
+      end
+    end
 
   run! if app_file == $0
 end
