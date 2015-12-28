13e32b04ff1f1208aae86d58b7c66d6cc5d8349a
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-28 12:12:07 +0000

    Adds DateTime field for peeps table, and layout changes

--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -10,73 +10,73 @@ feature 'Signing up to Chitter' do
 
   scenario 'A user can sign up' do
     sign_up(user)
-    expect{ click_button 'Submit' }.to change(User, :count).by(1)
+    expect{ click_button 'Create account' }.to change(User, :count).by(1)
   end
 
   scenario 'A user cannot sign up with a username that is already taken' do
     user2[:username] = 'Smithy'
     sign_up(user)
-    click_button 'Submit'
+    click_button 'Create account'
     sign_up(user2)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Username is already taken'
   end
 
   scenario 'A user cannot sign up with an email that is already taken' do
     user2[:email] = 'K@email.com'
     sign_up(user)
-    click_button 'Submit'
+    click_button 'Create account'
     sign_up(user2)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Email is already taken'
   end
 
   scenario 'A user can only sign up with a valid email' do
     user[:email] = 'myemail.com'
     sign_up(user)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Email has an invalid format'
   end
 
   scenario 'A user must enter a name' do
     user[:name] = ''
     sign_up(user)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Name must not be blank'
   end
 
   scenario 'A user must enter a username' do
     user[:username] = ''
     sign_up(user)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Username must not be blank'
   end
 
   scenario 'A user must enter an email' do
     user[:email] = ''
     sign_up(user)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Email must not be blank'
   end
 
   scenario 'A user must enter a password' do
     user = User.new(name: 'Katie', username: 'Smithy', email: 'K@email.com', password: '', password_confirmation: 'secret')
     sign_up(user)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Password must not be blank'
   end
 
   scenario 'A user must enter a password_confirmation' do
     user = User.new(name: 'Katie', username: 'Smithy', email: 'K@email.com', password: 'secret', password_confirmation: '')
     sign_up(user)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Password confirmation must not be blank'
   end
 
   scenario 'A user must enter matching passwords' do
     user = User.new(name: 'Katie', username: 'Smithy', email: 'K@email.com', password: 'top', password_confirmation: 'secret')
     sign_up(user)
-    expect{ click_button 'Submit' }.not_to change(User, :count)
+    expect{ click_button 'Create account' }.not_to change(User, :count)
     expect(page).to have_content 'Password does not match the confirmation'
   end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,15 +1,19 @@
 def sign_up(user)
   visit '/users/new'
-  fill_in :name, with: user.name
-  fill_in :username, with: user.username
-  fill_in :email, with: user.email
-  fill_in :password, with: user.password
-  fill_in :password_confirmation, with: user.password_confirmation
+  within('#new_user') do
+    fill_in :name, with: user.name
+    fill_in :username, with: user.username
+    fill_in :email, with: user.email
+    fill_in :password, with: user.password
+    fill_in :password_confirmation, with: user.password_confirmation
+  end
 end
 
 def log_in(username, password)
   visit '/'
-  fill_in :username, with: username
-  fill_in :password, with: password
-  click_button 'Submit'
+  within('#login_bar') do
+    fill_in :username, with: username
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
 end
