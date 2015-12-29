61820c4e82a9c1be4e51ce9c14d81a040bd8860d
Chris Wynne <chris.wynndow@gmail.com>
2015-12-29 15:46:02 +0000

    Adds reply link to peeps

--- spec/features/reply_spec.rb
+++ spec/features/reply_spec.rb
@@ -3,7 +3,7 @@
 # I want to reply to a peep from another maker.
 
 feature 'Replying to peeps' do
-  scenario 'I can reply to a peep and see it' do
+  scenario 'I can reply to a peep and view it' do
     sign_up
     log_in
     visit('/peeps/new')
@@ -11,14 +11,13 @@ feature 'Replying to peeps' do
     click_button('Post')
     click_button('Log out')
     sign_up(name: 'Vic', username: 'Reeves',
-                email: 'vic@gmail.com'
-    log_in(mail: 'chris.wynndow@gmail.com', password: 'password')
-    visit('/peeps/new')
+                email: 'vic@gmail.com')
+    log_in(email: 'chris.wynndow@gmail.com', password: 'password')
+    visit('/peeps')
     click_link('Reply')
     fill_in(:message, with: 'This is a test reply.')
     click_button('Post')
     expect(page).to have_content('This is a test reply.')
-
   end
 
 
