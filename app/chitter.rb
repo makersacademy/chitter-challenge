4cdea147917746af9cf5ac85ba8d0e35bf2a8fa6
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-29 17:45:52 +0000

    Splits the controller

--- app/chitter.rb
+++ app/chitter.rb
@@ -4,65 +4,7 @@ require 'sinatra/base'
 require 'sinatra/flash'
 require 'sinatra/partial'
 require './data_mapper_setup'
-
-class Chitter < Sinatra::Base
-  register Sinatra::Flash
-  register Sinatra::Partial
-  use Rack::Session::Cookie, key: 'rack.session', secret: 'super secret'
-  use Rack::MethodOverride
-  set :partial_template_engine, :erb
-  enable :partial_underscores
-
-  get '/' do
-    erb :index
-  end
-
-  get '/peeps' do
-    user = User.first(id: env["rack.session"][:user_id])
-    @username = (user ? user[:username] : nil)
-    @peeps = Peep.all(:order => [:created_at.desc])
-    erb :peeps
-  end
-
-  post '/peeps' do
-    user = User.first(id: env["rack.session"][:user_id])
-    message = Peep.create(content: params[:message], user_id: (env["rack.session"][:user_id]))
-    user.peeps << message
-    user.save # use save! here instead?
-    redirect to('/peeps')
-  end
-
-  delete '/logout' do
-    env["rack.session"][:user_id] = nil
-    flash.next[:notice] = 'Goodbye!'
-    redirect to('/')
-  end
-
-  post '/login' do
-    user = User.authenticate(params[:username], params[:password])
-    if user
-      env["rack.session"][:user_id] = user.id
-      redirect to('/peeps')
-    else
-      flash.next[:login_fail] = ["Either you've not registered, or your password is wrong"]
-      redirect to '/'
-    end
-  end
-
-  get '/users/new' do
-    erb :'users/new'
-  end
-
-  post '/users/new' do
-    user = User.new(name: params[:name], username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    if user.save
-      redirect to '/'
-    else
-      flash.now[:errors] = user.errors.full_messages
-      erb :'users/new'
-    end
-  end
-
-  # start the server if ruby file executed directly
-  run! if app_file == $0
-end
+require_relative 'server'
+require_relative './controllers/users'
+require_relative './controllers/peeps'
+require_relative './controllers/login'
