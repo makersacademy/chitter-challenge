e2f5f8c115d09757310722573b6619a0413b5008
Katie Smith <kls.smith@yahoo.co.uk>
2015-12-29 16:57:35 +0000

    Refactors views

--- app/chitter.rb
+++ app/chitter.rb
@@ -10,6 +10,8 @@ class Chitter < Sinatra::Base
   register Sinatra::Partial
   use Rack::Session::Cookie, key: 'rack.session', secret: 'super secret'
   use Rack::MethodOverride
+  set :partial_template_engine, :erb
+  enable :partial_underscores
 
   get '/' do
     erb :index
