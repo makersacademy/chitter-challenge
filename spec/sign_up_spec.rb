6e2d79d5881117598f117a813f2002b33e813f7f
Tony Young <ty_training@hotmail.co.uk>
2015-12-31 16:40:40 +0000

    Added flash errors for sign up validation

--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -9,7 +9,7 @@ feature 'sign up' do
   scenario 'requires a matching password for confirmation' do
     expect {sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario 'requires an email address to sign up' do
