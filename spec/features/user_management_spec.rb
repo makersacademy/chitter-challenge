fce97b300b25e4f48d04ff2453085f67d0f022e3
willibaur <williambautista@gmail.com>
2016-01-02 09:47:23 +0000

    sign up validation fails when no email provided

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -11,4 +11,10 @@ feature 'Client sign up' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change { User.count }
     expect(page).to have_content('Password does not match the confirmation')
   end
+
+  scenario "I can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change { User.count }
+    expect(current_path).to eq '/users'
+    expect(page).to have_content('Email must not be blank')
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,4 +14,8 @@ describe User do
   it 'does not authenticate when given a wrong password confirmation' do
     expect(User.authenticate(user.email, 'wrong_password')).to be nil
   end
+
+  it 'does not authenticate whithout email' do
+    expect(User.authenticate(nil, user.password)).to be nil
+  end
 end
