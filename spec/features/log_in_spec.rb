11867071b57c1521919ad39a8f23b069844e2891
willibaur <williambautista@gmail.com>
2016-01-02 10:40:32 +0000

    User story 2 added, log in function

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -2,10 +2,10 @@
 # So that I can post messages on Chitter as me
 # I want to log in to Chitter
 
-feature 'User logs in' do
-  xscenario 'with correct credentials' do
+feature 'Client logs in' do
+  scenario 'with correct credentials' do
     sign_up
-    log_in(email: user.email, password: user.password)
-    expect(page).to have_content "Welcome, #{user.username}"
+    log_in(email: 'test@email.com', password: 'secret_password')
+    expect(page).to have_content 'Welcome, nickname'
   end
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,3 +1,7 @@
+# As a Maker
+# So that I can post messages on Chitter as me
+# I want to sign up for Chitter
+
 feature 'Client sign up' do
   scenario 'I can sign up as a new user with valid password' do
     expect { sign_up }.to change { User.count }.by(1)
