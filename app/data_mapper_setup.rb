c5f133c56265eb07d90f2455c76b231fb8a513bf
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-02 18:09:40 +0000

    Initial commit and set up

---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,6 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require 'bcrypt'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.finalize
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,34 @@ SimpleCov.formatters = [
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'database_cleaner'
+
+require_relative 'helpers/session'
+
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.include SessionHelpers
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
