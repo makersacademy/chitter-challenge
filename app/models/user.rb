e82693b02f75601799fbe441b007a9be6b828344
Jelgar <jonathanelgardiner@gmail.com>
2016-01-02 04:23:28 +0000

    log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,11 +23,18 @@ class User
     self.password_digest = BCrypt::Password.create(password)
   end
 
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
+  end
+
   validates_presence_of :email
   validates_format_of :email, as: :email_address
   validates_confirmation_of :password
-
-
 
 end
 
---
+++ spec/features/signing_in_spec.rb
@@ -0,0 +1,23 @@
+feature 'User sign in' do
+
+  let(:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234',
+                name: 'Jonathan Gardiner',
+                username: 'jelgar')
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(email: user.email,   password: user.password)
+    expect(page).to have_content "Hello, #{user.name}"
+  end
+
+  def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in 'email', with: email
+    fill_in 'password', with: password
+    click_button 'Sign in'
+  end
+
+end
