2578c59d70f78b962c4a1fb14e15b5373628d2a8
Sara Tateno <sara.tateno@gmail.com>
2016-01-02 22:25:15 +0000

    Users must be logged in to peep

--- spec/features/create_peeps_spec.rb
+++ spec/features/create_peeps_spec.rb
@@ -1,12 +1,18 @@
 feature 'creates peeps' do
   before(:all) do
     sign_up
-    sign_in
   end
 
   scenario 'user posts a peep to Chitter' do
+    sign_in
     expect{ post_peep }.to change{ User.first.peeps.count }.by 1
     expect(page.status_code).to eq(200)
     expect(page).to have_content 'Hello world'
+  end
+
+  scenario 'cannot peep if not signed in' do
+    sign_in
+    click_button 'Sign out'
+    page.assert_no_selector("input[type=submit][value='Peep']")
   end
 end
