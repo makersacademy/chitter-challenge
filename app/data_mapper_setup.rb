fd6c7f98aa333293855fc432dba7d3dfe9699afe
edwardkerry <edwardkerry@gmail.com>
2016-01-02 16:50:54 +0000

    heroku database env added

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,5 +4,5 @@ require 'dm-postgres-adapter'
 require_relative 'models/user'
 require_relative 'models/peep'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
