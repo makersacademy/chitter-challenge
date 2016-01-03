b415824f7945b9444acd9c45e65d71c287929fb9
vegim qehaja <med.batt.uk@gmail.com>
2016-01-03 18:00:31 +0000

    refactored controllers and partials

--- Gemfile
+++ Gemfile
@@ -2,12 +2,15 @@ source 'https://rubygems.org'
 ruby '2.2.3'
 
 gem 'sinatra'
+gem 'sinatra-partial'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'bcrypt'
 gem 'sinatra-flash'
 
+
 group :test do
+  gem 'byebug'
   gem 'rspec'
   gem 'cucumber'
   gem 'rake'
---
+++ app/controllers/peeps.rb
@@ -0,0 +1,16 @@
+class Chitter < Sinatra::Base
+  get '/peeps' do
+    @peeps = Peep.all
+    erb :'peeps/index'
+  end
+
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  post '/peeps' do
+    peep = Peep.create(user: params[:user], message: params[:message])
+    peep.save
+    redirect to('/peeps')
+  end
+end
