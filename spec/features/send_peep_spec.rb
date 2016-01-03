10d5de7751d8c0135f3b7d3ea0018fb60a904e95
Thomas Bradley <bradley.tom.r@gmail.com>
2016-01-03 23:53:07 +0000

    Adds basic peep functionality

--- spec/features/send_peep_spec.rb
+++ spec/features/send_peep_spec.rb
@@ -1,11 +1,14 @@
 feature 'Send Peep' do
+
+  # As a Maker
+  # So that I can let people know what I am doing
+  # I want to post a message (peep) to chitter
+
   scenario 'a user can send a peep' do
     sign_up
     send_peep
     expect(current_path).to eq '/peeps'
-    within 'ul#peeps' do
-      expect(page).to have_content('This is my first peep!')
-      expect(page).to have_content('From: Mr Example - @3xampl3')
-    end
+    expect(page).to have_content('This is my first peep!')
+    expect(page).to have_content('From: Mr Example - @3xampl3')
   end
 end
