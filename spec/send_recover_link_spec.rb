527dfa47ed4ae761f9cb4be01711cc644bbd685e
edwardkerry <edwardkerry@gmail.com>
2016-01-04 10:59:42 +0000

    Password reset url fixed

--- spec/send_recover_link_spec.rb
+++ spec/send_recover_link_spec.rb
@@ -10,7 +10,7 @@ describe SendRecoverLink do
     params = {from: 'postmaster@mail.ultra-chitter.com',
               to: user.email,
               subject: 'Reset your password',
-              text: 'Click here to reset password: http://ultra-chitter.herokuapp.com/reset_password?token=#{user.password_token}'}
+              text: "Click here to reset password: http://ultra-chitter.herokuapp.com/users/reset_password?token=#{user.password_token}"}
     expect(mail_gun_client).to receive(:send_message).with(sandbox_domain_name, params)
     described_class.call(user, mail_gun_client)
   end
