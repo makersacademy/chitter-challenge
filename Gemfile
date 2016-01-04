5a102b0af3c5c14989835010a7a2b2f9257f3527
Tristan Crossley <trcrossley@gmail.com>
2016-01-04 04:20:08 +0000

    App setup

--- Gemfile
+++ Gemfile
@@ -1,10 +1,18 @@
 source 'https://rubygems.org'
 ruby '2.2.3'
 
+group :development do
+  gem 'bcrypt'
+  gem 'data_mapper'
+  gem 'dm-postgres-adapter'
+  gem 'sinatra'
+end
+
 group :test do
+  gem 'capybara'
   gem 'rspec'
+  gem 'rspec-sinatra'
   gem 'cucumber'
   gem 'rake'
   gem 'coveralls', require: false
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,25 @@ SimpleCov.formatters = [
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'lib/app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = App
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
