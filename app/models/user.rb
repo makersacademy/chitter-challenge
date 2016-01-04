1c5f387ec3f819ccbe51b962a0c5acd46c0428d7
Andrew <andy_htun@hotmail.com>
2016-01-04 08:52:25 +0000

    updated solution

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,22 +30,22 @@ class User
 
   # has n, :peeps
 end
-
-class Peep
-  include DataMapper::Resource
-
-  property :id, Serial
-  property :message, String
-  property :time, String
-  property :peeper, String
-
-  # belongs_to :user
-
-  def short_time
-   time[0..18]
-  end
-
-end
+# 
+# class Peep
+#   include DataMapper::Resource
+#
+#   property :id, Serial
+#   property :message, String
+#   property :time, String
+#   property :peeper, String
+#
+#   # belongs_to :user
+#
+#   def short_time
+#    time[0..18]
+#   end
+#
+# end
 
 
 DataMapper.finalize
