7a38eb3c0feec6910c21cf5eddbe5c71f1aaf04e
emmabeynon <emma.beynon@gmail.com>
2016-01-10 21:51:00 +0000

    Extracted layout.erb into partial files.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'bcrypt'
 gem 'sinatra-contrib'
+gem 'sinatra-partial'
 
 group :test do
   gem 'rspec'
--- app/server.rb
+++ app/server.rb
@@ -3,8 +3,11 @@ class Chitter < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
   register Sinatra::MultiRoute
+  register Sinatra::Partial
   use Rack::MethodOverride
 
+  enable :partial_underscores
+  set :partial_template_engine, :erb
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
