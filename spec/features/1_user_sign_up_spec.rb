d1c394817d9df07213d810a2e0e8e912ee5dfe08
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-13 17:58:16 +0000

    user sign up tests refactored

--- spec/features/1_user_sign_up_spec.rb
+++ spec/features/1_user_sign_up_spec.rb
@@ -4,44 +4,48 @@ feature 'User Sign Up' do
     expect(page).to have_content('Welcome Deadpool! Chit away!')
   end
 
-  scenario 'fails with no full name' do
-    expect { user_sign_up(full_name: nil) }.
+  scenario 'fails with invalid email' do
+    expect { user_sign_up(email: 'chimichongas.com') }.
     not_to change{ User.count }
   end
 
-  scenario 'fails with no username' do
-    expect { user_sign_up(username: nil) }.
-    not_to change{ User.count }
-  end
-
-  scenario 'fails with no email' do
-    expect { user_sign_up(email: nil) }.
-    not_to change{ User.count }
-  end
-
-  scenario 'fails with no password' do
-    expect { user_sign_up(password: nil) }.
-    not_to change{ User.count }
-  end
-
-  scenario 'password must be confirmed' do
+  scenario 'fails with mismatching password' do
     expect { user_sign_up(password_confirmation: 'not password') }.
     not_to change{ User.count }
   end
 
-  scenario 'fails with already registered email' do
-    user_sign_up(full_name: 'Wilson', username: 'Wade')
-    expect { user_sign_up }.not_to change{ User.count }
+  feature 'Required fields' do
+    scenario 'full name' do
+      expect { user_sign_up(full_name: nil) }.
+      not_to change{ User.count }
+    end
+
+    scenario 'username' do
+      expect { user_sign_up(username: nil) }.
+      not_to change{ User.count }
+    end
+
+    scenario 'email' do
+      expect { user_sign_up(email: nil) }.
+      not_to change{ User.count }
+    end
+
+    scenario 'password' do
+      expect { user_sign_up(password: nil) }.
+      not_to change{ User.count }
+    end
   end
 
-  scenario 'fails with already registered username' do
-    user_sign_up(full_name: 'Wilson', email: 'dead@pool.com')
-    expect { user_sign_up }.not_to change{ User.count }
-  end
+  feature 'Unique fileds' do
+    scenario 'username' do
+      user_sign_up(full_name: 'Wilson', email: 'dead@pool.com')
+      expect { user_sign_up }.not_to change{ User.count }
+    end
 
-  scenario 'fails with invalid email' do
-    expect { user_sign_up(email: 'chimichongas.com') }.
-    not_to change{ User.count }
+    scenario 'email' do
+      user_sign_up(full_name: 'Wilson', username: 'Wade')
+      expect { user_sign_up }.not_to change{ User.count }
+    end
   end
 end
 
