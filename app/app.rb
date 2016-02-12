6d35f881554d4b9f1ccf51327950da0452964a70
Matt <mattpgough@gmail.com>
2016-02-12 22:40:05 +0000

    Added a development database and environmental variables.

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,5 @@
-#require 'sinatra'
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require_relative 'models/message'
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+ENV["RACK_ENV"] = "test"
+
 require 'capybara/rspec'
 require 'coveralls'
 require 'simplecov'
