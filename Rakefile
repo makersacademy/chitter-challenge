1d69d8cc9b30232c14823271991939654eb5a98d
Marco Bagnasco <admin@hecticmojo.com>
2016-02-12 19:22:06 +0000

    Init Commit, README updated

--- Rakefile
+++ Rakefile
@@ -1,7 +1,27 @@
+require 'data_mapper'
+require './app/app.rb'
+
 if ENV['RACK_ENV'] != 'production'
   require 'rspec/core/rake_task'
   
   RSpec::Core::RakeTask.new :spec
   
   task default: [:spec]
+end
+
+
+namespace :db do
+  desc 'Non destructive upgrade'
+  task :auto_upgrade do
+    DataMapper.auto_upgrade! do
+    puts 'Auto Upgrade Complete (no Data loss)'
+  end
+ end
+  
+  desc 'Destructive upgrade'
+  task :auto_migrate do
+    DataMapper.auto_migrate! do
+    puts 'Auto Migrate Complete (Data was lost)'
+  end
+ end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,41 @@
-require 'coveralls'
-require 'simplecov'
 
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-Coveralls.wear!
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', './app/app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.default_driver = :rake_test
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+  
+#  config.before(:suite) do
+#      DatabaseCleaner.strategy = :transaction
+#      DatabaseCleaner.clean_with(:truncation)
+#    end
+#
+#    config.around(:each) do |example|
+#      DatabaseCleaner.cleaning do
+#        example.run
+#      end
+#    end
+    
+  config.include Capybara::DSL
+ 
+  config.expect_with :rspec do |expectations|
+    
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+ 
+  config.mock_with :rspec do |mocks|
+   
+    mocks.verify_partial_doubles = true
+  end
+
+
+end
