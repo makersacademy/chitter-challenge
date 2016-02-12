20a75602e373e0bfbaa0125a23c8d240a8ce51d9
Tom Pickard <tcpickard94@gmail.com>
2016-02-12 21:13:48 +0000

    Configured databasecleaner to clean test database before and after each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,11 +5,25 @@ require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'database_cleaner'
 
 Capybara.app = Chitter
 
 RSpec.configure do |config|
   config.include Capybara::DSL
+  
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
