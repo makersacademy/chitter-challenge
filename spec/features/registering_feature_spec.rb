423e87aa07fbcd8ef1b04db0da97193c0ff9f692
russellvaughan <russellvaughan@gmail.com>
2016-02-13 22:45:41 +0000

    add encrypted password

--- spec/features/registering_feature_spec.rb
+++ spec/features/registering_feature_spec.rb
@@ -26,8 +26,24 @@ feature 'I want to be able to sign up for Chitter' do
 		fill_in('username', with: 'Rusty')
 		fill_in('email', with: 'Rusty@gmail.com')
 		fill_in('password', with: 'secert_password123')
+		fill_in('password_confirmation', with: 'secert_password123')		
 		click_button('submit')
 		expect(page).to have_content "Email is already taken"
 	end
+
+
+  scenario 'requires a matching confirmation password' do
+    register
+		visit '/'
+		fill_in('first_name', with: 'Russell')
+		fill_in('last_name', with: 'Vaughan')
+		fill_in('username', with: 'Rusty')
+		fill_in('email', with: 'Rusty@gmail.com')
+		fill_in('password', with: 'secert_password123')
+		fill_in('password_confirmation', with: 'secert_password234')
+    expect {click_button('submit')}.not_to change(User, :count)
+  end
+
+
 
 end
