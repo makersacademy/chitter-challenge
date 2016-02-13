dddf07a0a232d6d6056bcee8b55a6f198182d522
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-13 16:17:38 +0000

    Removed auto_upgrade! function from data_mapper_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,4 +4,4 @@ require './app/models/peep'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
+#DataMapper.auto_upgrade!
