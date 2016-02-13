8cc27fe31a8807adfce52243081c3a9a10521678
itsindigo <matthewbridgesbusiness@gmail.com>
2016-02-13 12:53:47 +0000

    Built initial file structure, gemfile and spec_helper

--- Gemfile
+++ Gemfile
@@ -1,5 +1,13 @@
 source 'https://rubygems.org'
-ruby '2.2.3'
+ruby '2.3.0'
+
+gem 'database_cleaner'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'dm-transactions'
+gem 'sinatra'
+gem 'bcrypt'
+gem 'capybara'
 
 group :test do
   gem 'rspec'
@@ -7,4 +15,3 @@ group :test do
   gem 'rake'
   gem 'coveralls', require: false
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,34 @@
+ENV['RACK_ENV'] = 'test'
+
 require 'coveralls'
 require 'simplecov'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'database_cleaner'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+
+require File.join(File.dirname(__FILE__), '..', '/app/app.rb')
+
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+  #
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
+  #
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
+end
