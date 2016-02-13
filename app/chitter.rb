6acde4a6dc432603eca73bac5df7f92ff7ecbff2
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-13 20:58:23 +0000

    Implemented flash message when user sign up password and confirmation do not match

--- app/chitter.rb
+++ app/chitter.rb
@@ -2,8 +2,12 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
 require './app/data_mapper_setup'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
+
+  register Sinatra::Flash
+
   enable :sessions
   set :session_secret, 'super secret'
 
@@ -21,15 +25,21 @@ class Chitter < Sinatra::Base
   end
   
   get '/users/new' do
-    erb :sign_up
+    @user = User.new
+    erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
                        password: params[:password],
                        password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   get '/compose_peep' do
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,6 +3,14 @@ feature 'User sign up' do
   scenario 'requires a matching confirmation password' do
     expect {sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
   end
+  
+  scenario 'with a password that does not match' do
+    expect {sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
+  end
+
+
 
   def sign_up(email: 'bingo@mail.com', password: 'abcd123', password_confirmation: '231321')
     visit '/users/new'
