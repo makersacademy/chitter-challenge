6f16e36137463b6496af1901875d7c52ea98ce94
ViolaCrellin <viola.crellin@gmail.com>
2016-02-13 20:43:49 +0000

    Refactored to a controller model

--- app/app.rb
+++ app/app.rb
@@ -1,89 +1,9 @@
 ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
 require 'sinatra/flash'
+
 require_relative 'data_mapper_setup'
 
-
-class Chitter < Sinatra::Base
-  register Sinatra::Flash
-  set :sessions, true
-  set :session_secret, 'super secret'
-
-  helpers do
-    def session_user
-      @session_user ||= User.get(session[:user_id])
-    end
-
-    def sign_up_errors?
-      @new_user.errors.full_messages.size > 0
-    end
-
-    def sign_up_errors
-      @new_user.errors.full_messages
-    end
-
-    def sign_in_error_type
-      if @user.nil? && User.first(user_name: params[:existing_user_name])
-        'Access Denied Impersonator'
-      else
-        'Brother, is that really you?'
-      end
-    end
-
-
-  end
-
-  get '/' do
-    @new_user = User.new
-    erb :'user/index'
-  end
-
-  post '/sign_up' do
-    @new_user = User.new(user_name: params[:user_name],
-    email: params[:email],
-    password: params[:password],
-    password_confirmation: params[:password_confirmation])
-
-    if @new_user.save
-      session[:user_id] = @new_user.id
-      redirect '/welcome'
-    else
-      flash.now[:error_password] =
-      sign_up_errors.select {|message| message.include? 'Dur-brain'}.join("/n")
-      flash.now[:error_user_name] =
-      sign_up_errors.select {|message| message.include? 'Brother'}.join("/n")
-      flash.now[:error_email] =
-      sign_up_errors.select {|message| message.include? 'Friend'}.join("/n")
-      flash.now[:new_user_email_memo] = @new_user.email
-      flash.now[:new_user_user_name_memo] = @new_user.user_name
-    end
-    erb :'user/index'
-  end
-
-
-  post '/sign_in' do
-    @user =
-      User.authenticate(params[:existing_user_name], params[:existing_password])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/welcome'
-    else
-      flash.now[:authentication_error] = sign_in_error_type
-      erb :'user/index'
-    end
-  end
-
-  get '/welcome' do
-    @user = session_user.user_name
-    erb :'user/welcome'
-  end
-
-
-  run! if app_file == $0
-end
-
-
-# register Sinatra::Partial
-# use Rack::MethodOverride
-# set :partial_template_engine, :erb
-# enable :partial_underscores
+require_relative 'server'
+require_relative 'controllers/new_users'
+require_relative 'controllers/sessions'
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,23 +8,6 @@ class User
   attr_reader :password
   attr_accessor :password_confirmation
 
-  validates_confirmation_of :password,
-  :message => 'Dur-brain, you need to type the SAME password twice'
-
-  validates_presence_of :email,
-  :message => 'Friend, without an email, how can we contact you again?'
-  validates_uniqueness_of :email,
-  :message => 'Friend? Can you not remember that you\'re a member?'
-  validates_format_of :email, as: :email_address,
-  :message => 'Friend, something looks a bit fishy about your email'
-
-  validates_presence_of :user_name,
-  :message => 'You may have forgotten something Brother. Something crucial.'
-  validates_uniqueness_of :user_name,
-  :message => 'We already have a Brother by that name. There can be only one'
-
-
-
   property :id, Serial
   property :user_name, String, required: true, unique: true
   property :email, String, required: true, unique: true
@@ -44,9 +27,24 @@ class User
     end
   end
 
+
+  validates_confirmation_of :password,
+  :message => 'Dur-brain, you need to type the SAME password twice'
+
+  validates_presence_of :email,
+  :message => 'Friend, without an email, how can we contact you again?'
+  validates_uniqueness_of :email,
+  :message => 'Friend? Can you not remember that you\'re a member?'
+  validates_format_of :email, as: :email_address,
+  :message => 'Friend, something looks a bit fishy about your email'
+
+  validates_presence_of :user_name,
+  :message => 'You may have forgotten something Brother. Something crucial.'
+  validates_uniqueness_of :user_name,
+  :message => 'We already have a Brother by that name. There can be only one'
+
 end
 
-# DataMapper::Logger.new($stdout, :debug)
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-# DataMapper.auto_upgrade!
