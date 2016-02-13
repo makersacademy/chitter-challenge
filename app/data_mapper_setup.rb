80cc1f01074101d7439900761d3cba27cb2a63eb
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-02-13 17:29:30 +0000

    Changed require to require_relative for peep.rb in data_mapper_setup, re-added finalize and auto_upgrade! methods

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
-require './app/models/peep'
+require_relative 'models/peep'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-
+DataMapper.auto_upgrade!
