c4e8d762457c912627e7838d4fd56f4a6c2c21ef
JoeWroe <joe_wroe@icloud.com>
2016-02-14 01:40:33 +0000

    USER STORY ONE
    Added 'dm-postgres-adapter' & 'data_mapper' gems
    Added '/sign_up'
    Added '/new_user' POST request (incomplete)
    Added 'data_mapper_setup.rb' to run DataMapper
    Added 'User.rb' model to map a User table to Database

--- CHALLENGE/app/app.rb
+++ CHALLENGE/app/app.rb
@@ -1,12 +1,25 @@
+ENV['RACK_ENV'] ||= 'dev'
+
 require 'sinatra/base'
+require_relative './data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
   get '/' do
     redirect '/homepage'
   end
+
   get '/homepage' do
     erb :homepage
+  end
+
+  get '/sign_up' do
+    erb :sign_up
+  end
+
+  post '/new_user' do
+    User.create(username: params['Username'], email: params['E-mail'], password: params['Password'])
+    # redirect '/:Username'
   end
 
   # start the server if ruby file executed directly
--- CHALLENGE/spec/web_helpers.rb
+++ CHALLENGE/spec/web_helpers.rb
@@ -1,7 +1,7 @@
 def sign_up
   visit '/homepage'
   click_button 'Sign up'
-  fill_in('Username'   with: 'username')
+  fill_in('Username',   with: 'username')
   fill_in('E-mail',    with: 'e-mail@e-mail.com')
   fill_in('Password',  with: 'password')
   click_button 'Register'
