f6c8a9e1f76972710ec8f8fc7cf8b7469a278049
Marco Bagnasco <admin@hecticmojo.com>
2016-02-14 09:54:04 +0000

    Added ENV['DATABASE_URL'] to DM config

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,6 +1,6 @@
 require_relative 'models/user'
 
-DataMapper.setup(:default, "postgres://localhost/chitter-#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter-#{ENV['RACK_ENV']}")
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.finalize
 DataMapper.auto_upgrade!
