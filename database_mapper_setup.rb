07222b6ab8cb37da4c57b6276bf10ffe55381a6d
Reiss Johnson <reissjohnson@me.com>
2016-02-14 12:45:47 +0000

    pushing to heroku

--- database_mapper_setup.rb
+++ database_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'models/peep'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||  "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
