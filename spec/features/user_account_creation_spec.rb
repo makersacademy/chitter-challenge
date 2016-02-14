aaa9b5110283121d86639e8c599da2c9cefaff8c
Alain Lemaire <jaxdid@gmail.com>
2016-02-15 00:22:32 +0100

    Updated feature test: unathenticated users should be able to log in via index.erb. Added additional test helper methods for logging in and out.

--- spec/features/user_account_creation_spec.rb
+++ spec/features/user_account_creation_spec.rb
@@ -1,9 +1,18 @@
-feature 'Creating a new user account' do
+feature 'User account management' do
   scenario 'User can create a new account and automatically get logged in' do
     expect{create_account}.to change{User.count}.by(1)
-    expect(User.first.username).to eq ('Chet')
+    expect(User.first.username).to eq ('chet')
     within 'article#header' do
-      expect(page).to have_content('Welcome, Chet!')
+      expect(page).to have_content('Welcome, chet!')
+    end
+  end
+
+  scenario 'An unauthenticated user can log in with correct details' do
+    User.create(username: 'chet', password: 'cutthechatterred2')
+    log_in
+    within 'article#header' do
+      expect(page).to have_content('Welcome, chet!')
+      expect(page).to have_content('Log out')
     end
   end
 end
