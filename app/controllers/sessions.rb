e40edd7ede90e49f70d7223139fd212c57921776
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-14 23:09:15 +0000

    log out

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -10,9 +10,16 @@ class Chitter < Sinatra::Base
       session[:user_id] = user.id
       redirect '/peeps'
     else
-      flash[:notice] = "error"
+      flash.now[:notice] = "error"
       erb :'sessions/new'
     end
   end
+
+  delete '/sessions' do
+    session[:user_id] = nil
+    flash.keep[:notice] = 'Goodbye!'
+    redirect '/peeps'
+  end
+
 
 end
