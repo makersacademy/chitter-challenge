9a4dedb82ae13f23816bb57517998aba9edd280d
Frazer Watson <frazerw@tutanota.com>
2016-02-14 12:09:57 +0000

    As a Maker So that I can let people know what I am doing I want to post a message (peep) to chitter
    A user can now post a new chit to the main page.

--- app/app.rb
+++ app/app.rb
@@ -7,4 +7,14 @@ class Chitter < Sinatra::Base
     @chits = Chit.all
     erb :'chits/index'
   end
+
+  get '/chits/new' do 
+    erb :'chits/new'
+  end
+
+  post '/chits' do
+  Chit.create(chitter_post: params[:chitter_post], user: params[:user])
+  redirect to('/chits')
+  end
+
 end
--- spec/creating_chits_spec.rb
+++ spec/creating_chits_spec.rb
@@ -5,11 +5,11 @@ feature 'Creating chits' do
     visit '/chits/new'
     fill_in 'user',   with: 'Ninja'
     fill_in 'chitter_post', with: 'ZEF TO DEF'
-    click_button 'Create link'
+    click_button 'Post Chit'
 
     expect(current_path).to eq '/chits'
 
-    within 'ul#new_posts' do
+      within 'ul#posts' do
       expect(page).to have_content('ZEF TO DEF')
     end
   end
