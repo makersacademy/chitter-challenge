510e5697d8a919671862235a48bc102a68071050
innlouvate <l.j.franklin85@googlemail.com>
2016-02-14 16:24:05 +0000

    split out controllers

--- app/app.rb
+++ app/app.rb
@@ -2,7 +2,7 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
 require 'sinatra/flash'
-# require 'sinatra/partial'
+require 'sinatra/partial'
 require_relative 'data_mapper_setup'
 
 require_relative 'helpers'
