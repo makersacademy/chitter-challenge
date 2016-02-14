e87be7cfa05109609a083b970b2c5e175983dcad
Scott Gledhill <ScottGledhill@hotmail.co.uk>
2016-02-14 22:25:50 +0000

    sign in added at startup

--- app/app.rb
+++ app/app.rb
@@ -8,6 +8,7 @@ require_relative 'models/peeps'
 
 class ChitterChallenge < Sinatra::Base
   register Sinatra::Flash
+  use Rack::MethodOverride
   enable :sessions
   set :session_secret, 'super secret'
 
