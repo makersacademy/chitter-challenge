3ecac0a915ab8331eb3c01273a656f4be840272c
Alain Lemaire <jaxdid@gmail.com>
2016-02-14 22:09:09 +0100

    Added root route view (index.erb), which greets either a logged in user (by username) or 'friend' for non-logged-in users.

--- app/app.rb
+++ app/app.rb
@@ -4,17 +4,21 @@ require 'sinatra/base'
 require './dm_setup'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+
   get '/signup' do
     erb(:signup)
   end
 
   post '/signup' do
     User.create(username: params[:username], password: params[:password])
+    session[:current_user] = params[:username]
     redirect('/')
   end
 
   get '/' do
-    'Lorem &c.'
+    @username = session[:current_user] || 'friend'
+    erb(:index)
   end
 
   run! if app_file == $0
--- spec/features/user_account_creation_spec.rb
+++ spec/features/user_account_creation_spec.rb
@@ -2,6 +2,8 @@ feature 'Creating a new user account' do
   scenario 'User can create a new account and automatically get logged in' do
     expect{create_account}.to change{User.count}.by(1)
     expect(User.first.username).to eq ('Chet')
-    expect(page).to have_content('Welcome, Chet!')
+    within 'article#header' do
+      expect(page).to have_content('Welcome, Chet!')
+    end
   end
 end
