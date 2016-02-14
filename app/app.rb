a1dc37bf984788e85c2b4ca33c7503ba6462fe16
Iryna Howarth <irynahowarth@gmail.com>
2016-02-14 09:01:47 +0000

    improve singup by adding flash

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 ENV['RACK_ENV'] = 'development'
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 require_relative 'models/user'
 
@@ -8,23 +9,30 @@ class Chitter < Sinatra::Base
 
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   get '/' do
     erb :index
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(name: params[:name],
+    @user = User.create(name: params[:name],
                 username: params[:username],
                 email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/users/new')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
--- spec/feature/user_management_spec.rb
+++ spec/feature/user_management_spec.rb
@@ -10,8 +10,10 @@ feature 'User sign up' do
     expect(User.first.email).to eq('iryna@mail.com')
   end
 
-  scenario 'requires a matching confirmation password' do
+  scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 
 end
