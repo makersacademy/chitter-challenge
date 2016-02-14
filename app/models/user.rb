f49c08a2ed60ccb488ac17bcf307e4c1282594ad
Frazer Watson <frazerw@tutanota.com>
2016-02-14 18:44:37 +0000

    As a Maker So that I can post messages on Chitter as me I want to log in to Chitter
    Users can now sign in, if they have previously sighed up.

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,5 +20,16 @@ class User
     @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
+  
+  def self.authenticate(email, password)
+  user = first(email: email)
+   if user && BCrypt::Password.new(user.password_digest) == password
+    # return this user
+    user
+  else
+    nil
+    end
+  end
+  
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -43,6 +43,25 @@ feature 'User sign up' do
     expect(current_path).to eq('/users')
     expect(page).to have_content('Email must not be blank')
    end
+   
+   let(:user) do
+    User.create(email: 'user@example.com',
+                password: 'secret1234',
+                password_confirmation: 'secret1234')
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(email: user.email,   password: user.password)
+    expect(page).to have_content "Welcome, #{user.email}"
+  end
+
+  def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
+
 
   
 end
