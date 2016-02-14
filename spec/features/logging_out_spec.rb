47b1de78ab8b68144a8c25d0877457945c1dd625
Hannah Smyth-Osbourne <h.smythosbourne@gmail.com>
2016-02-14 19:32:04 +0000

    additional scenario to logging out feature added

--- spec/features/logging_out_spec.rb
+++ spec/features/logging_out_spec.rb
@@ -1,10 +1,16 @@
 feature 'logging out' do
+  
   scenario 'signed in user can log out' do
     User.create(name: 'user', email: 'user@email.com', password: 'password',
      password_confirmation: 'password')
     log_in(email: 'user@email.com', password: 'password')
     click_button('Log out')
     expect(page).to have_content('Goodbye!')
+  end
+
+  scenario 'not logged in' do
+    visit '/peeps'
+    expect(page).not_to have_button('Goodbye!')
   end
 
 
