6dbdb0e4517fa5fc3bac0fafb18339736157a6ad
Frazer Watson <frazerw@tutanota.com>
2016-02-14 12:13:41 +0000

    added database_cleaner to spec_help.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,25 @@ SimpleCov.formatters = [
 Coveralls.wear!
 
 RSpec.configure do |config|
+  # Everything in this block runs once before all the tests run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
+
+RSpec.configure do |config|
   config.include Capybara::DSL
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
