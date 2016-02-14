43d9ab794e1accfbb2f0123afa48b1ddbf970885
rachelsmithcode <rachelcodesmith@outlook.com>
2016-02-14 18:22:17 +0000

    new peep can now be saved

--- spec/feature/peeps_feature_spec.rb
+++ spec/feature/peeps_feature_spec.rb
@@ -5,33 +5,35 @@ require './app/chitter.rb'
 
 feature 'Peeps' do
 
-  scenario 'peeps display the username of the person that created them' do
+  scenario 'I can create a new peep after signing in' do
     sign_up
     sign_in
-    create_peep('I am the first peep', 'Peep 1')
-
-    visit '/'
-
-    expect(page.status_code).to eq 200
-
-    within 'ul#links' do
-      expect(page).to have_content('User 1')
-    end
+    click_button('New peep')
+    fill_in 'title',   with: 'Peep 1'
+    fill_in 'message',   with: 'I am the first peep'
+    click_button('Peep!')
+    expect(page).to have_content('Peep 1')
   end
 
-  scenario 'peeps display the time and date that they were created' do
-    sign_up
-    sign_in
-    create_peep('I am the first peep', 'Peep 1')
-
-    visit '/'
-
-    expect(page.status_code).to eq 200
-
-    within 'ul#links' do
-      # expect(page).to have_content()
-    end
-  end
+  # scenario 'peeps display the username of the person that created them' do
+  #   sign_up
+  #   sign_in
+  #   create_peep('I am the first peep', 'Peep 1')
+  #   visit '/'
+  #   expect(page.status_code).to eq 200
+  #   expect(page).to have_content('User 1')
+  # end
+  #
+  # scenario 'peeps display the time and date that they were created' do
+  #   sign_up
+  #   sign_in
+  #   create_peep('I am the first peep', 'Peep 1')
+  #
+  #   visit '/'
+  #
+  #   expect(page.status_code).to eq 200
+  #
+  # end
 
 
 end
