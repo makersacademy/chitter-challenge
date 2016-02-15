3dec5fe90a66f4b83025541a0fe48bd69b4df007
Ben Forrest <ben@silvabox.com>
2016-02-15 15:14:48 +0000

    testing db fails again

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,4 +7,4 @@ require_relative 'models/peep'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 
-DataMapper.auto_upgrade!
+#DataMapper.auto_upgrade!
