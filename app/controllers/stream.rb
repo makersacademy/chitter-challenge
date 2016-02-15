8c32ff584bcfa511bd19b2d318ed9e564296c102
Marco Bagnasco <admin@hecticmojo.com>
2016-02-15 00:44:10 +0000

    changed style with CSS

--- app/controllers/stream.rb
+++ app/controllers/stream.rb
@@ -2,8 +2,8 @@ class Chitter < Sinatra::Base
 
   post '/stream' do
     @message = Message.create(body: params[:body], date: DateTime.now)
-    @user = current_user.messages << @message
-    @user.save
+    user = current_user.messages << @message
+    user.save
     redirect '/stream'
   end
   
@@ -12,7 +12,6 @@ class Chitter < Sinatra::Base
   end
   
   get '/stream' do
-    @user_mess = current_user.messages if current_user
     @global_stream = User.all.messages(:order => [:date.desc])
     @users = User.all
     erb :stream
