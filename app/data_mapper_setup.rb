c7ca7eee51b56efcdfa0021d1251fb6de6f18212
Marco Bagnasco <admin@hecticmojo.com>
2016-02-15 09:25:44 +0000

    Edited datamapper config, database with underscore

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 require_relative 'models/user'
 require_relative 'models/message'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter-#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.finalize
 DataMapper.auto_upgrade!
