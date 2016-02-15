6a5a4958b964d554266ee90db69fe88f272e3bd3
Reiss Johnson <reissjohnson@me.com>
2016-02-15 13:30:40 +0000

    all tests passing, abot to impliment functionality for dated peeps, test written

--- spec/features/sending_peeps_spec.rb
+++ spec/features/sending_peeps_spec.rb
@@ -1,16 +1,29 @@
-feature 'sending a peep' do
-  scenario 'sends a peep' do
-    Peep.create(text: 'My First Peep')
-    visit '/homepage'
-    expect(page.status_code).to eq 200
-    within 'ul#peeps' do
-    expect(page).to have_content('My First Peep')
-    end
+feature 'sending a peep' do 
+  
+  let(:user) do
+    User.create(email: 'user123@example.com',
+		 name: 'John Smith',
+		 user_name: 'JohnSmith',
+		 password: '1234567',
+		 password_confirmation: '1234567')
   end
-   scenario 'user can send a tweet' do
+
+   scenario 'user can send a peep' do
+    sign_in(user_name: user.user_name,
+	    password: user.password)
     visit '/new-peep'
     fill_in('peep', with: 'My Second Peep')
     click_button('Peep!')
     expect(page).to have_content('My Second Peep')
-  end
+    click_button('Sign Out')  
+ end
+   scenario 'peep has a layout' do
+     sign_in(user_name: user.user_name,
+	    password: user.password)
+     visit '/new-peep'
+     fill_in('peep', with: 'My Third Peep')
+     click_button('Peep!') 
+     current_time = Time.now
+     expect(page).to have_content("My Third Peep - #{current_time.strftime('%a, %d %b %Y %H:%M:%S')}")
+   end
 end
