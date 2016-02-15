6b615a1c23efdb23bc6ef52937211e919f76d8a9
Makers Academy <makers@Makerss-MacBook-Pro-2.local>
2016-02-15 01:55:12 +0000

    Updated data_mapper_setup for heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,6 @@ require_relative 'models/peep'
 require_relative 'models/user'
 require_relative 'models/response'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV["RACK_ENV"]}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
