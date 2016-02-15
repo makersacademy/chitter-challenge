e9dcc897800737aa2e9b984b7b5eb4d99ddf9570
hedudelgado <hedu.delgado@gmail.com>
2016-02-15 19:34:04 +0000

    comments

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,7 +6,7 @@ require 'dm-timestamps'
 require_relative 'models/user'
 require_relative 'models/peep'
 
-
+#a
 DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 
