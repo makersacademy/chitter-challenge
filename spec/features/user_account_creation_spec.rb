4c5c651910a07d73ee5dfaf76679f9666e85b9cf
Alain Lemaire <jaxdid@gmail.com>
2016-02-15 10:19:27 +0100

    [Version control] Deleted duplicated logic added through merging.

--- spec/features/user_account_creation_spec.rb
+++ spec/features/user_account_creation_spec.rb
@@ -1,20 +1,12 @@
 feature 'User account management' do
-<<<<<<< HEAD
   scenario 'An unauthenticated user can create a new account and be automatically logged in' do
     expect{create_account('chet', 'cutthechatterred2')}.to change{User.count}.by(1)
     expect(User.first.username).to eq ('chet')
     within 'header' do
-=======
-  scenario 'User can create a new account and automatically get logged in' do
-    expect{create_account}.to change{User.count}.by(1)
-    expect(User.first.username).to eq ('chet')
-    within 'article#header' do
->>>>>>> 2d2b367b2fb71cd937686a9df489814059793285
       expect(page).to have_content('Welcome, chet!')
     end
   end
 
-<<<<<<< HEAD
   scenario 'An unauthenticated user can log in to an existing account with correct details' do
     User.create(username: 'chet', password: 'cutthechatterred2')
     log_in('chet', 'cutthechatterred2')
@@ -42,19 +34,10 @@ feature 'User account management' do
   scenario 'An authenticated user can log out' do
     User.create(username: 'chet', password: 'cutthechatterred2')
     log_in('chet', 'cutthechatterred2')
-    puts "Successfully logged in! Path: #{current_path}"
     log_out
     within 'header' do
       expect(page).to have_content('Welcome!')
       expect(page).to have_content('Log in')
-=======
-  scenario 'An unauthenticated user can log in with correct details' do
-    User.create(username: 'chet', password: 'cutthechatterred2')
-    log_in
-    within 'article#header' do
-      expect(page).to have_content('Welcome, chet!')
-      expect(page).to have_content('Log out')
->>>>>>> 2d2b367b2fb71cd937686a9df489814059793285
     end
   end
 end
