ec905581f6ddb35fbaf652058b14da2df901c076
Rufus Raghunath <rufusraghunath@gmail.com>
2016-02-16 16:29:47 +0000

    added user input safety checks

--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -7,7 +7,7 @@ class ChitterApp < Sinatra::Base
     post '/peeps/new' do
       flash.keep[:length] = "Message too long!" if params[:message].length > 140
       # move peep into current_user?
-      Peep.create(message: params[:message],
+      Peep.create(message: URI.escape(params[:message]),
                   author: current_user.real_name,
                   handle: current_user.username,
                   time: Time.now)
