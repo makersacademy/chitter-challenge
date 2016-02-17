0aab1d8e4c660e4836cb102d4c8560567ee90545
barrygrubb <barrygrubb@users.noreply.github.com>
2016-02-17 20:44:02 +0000

    Configure rack env

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -10,6 +10,6 @@ class Peep
   property :text, String
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_challenge_test")
+DataMapper.setup(:default, "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
