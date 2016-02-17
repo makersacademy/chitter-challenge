9610812eeda5202d635f2049202a5773a3586a14
barrygrubb <barrygrubb@users.noreply.github.com>
2016-02-17 20:54:28 +0000

    Add Heroku Database rack env

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -10,6 +10,6 @@ class Peep
   property :text, String
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
