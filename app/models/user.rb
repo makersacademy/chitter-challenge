07c231630d9b7abb46d5074d4dea898f9cba84e0
barrygrubb <barrygrubb@users.noreply.github.com>
2016-02-18 21:44:13 +0000

    Add user sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,14 @@ class User
   end
 
   def self.authenticate(email, password)
-    first(email: email)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+    # return this user
+    user
+  else
+    nil
+  end
+
   end
 
 end
