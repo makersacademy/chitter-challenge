cc123b54a84f8475e5b2c251812f1e891ac64073
Junyuan Xue <junyuanxue.is@gmail.com>
2016-03-25 20:56:42 +0000

    Tested for password mismatch scenario

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,21 +1,25 @@
 class Chitter < Sinatra::Base
   get "/" do
-    "Hello Chitter!"
+    "Welcome Luke!"
   end
 
   get "/users/new" do
+    @user = User.new
     erb :"/users/new"
   end
 
-  post "/users" do
-    user = User.new(name: params[:name],
+  post "/users/new" do
+    @user = User.new(name: params[:name],
                      username: params[:username],
                      email: params[:email],
                      password: params[:password],
                      password_confirmation: params[:password_confirmation])
-    if user.save
-      session[:user_id] = user.id
-      redirect "/users/new"
+    if @user.save
+      session[:user_id] = @user.id
+      redirect "/"
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      erb :"/users/new"
     end
   end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,4 +5,16 @@ feature "User sign up" do
     expect(User.last.username).to eq "skywalker"
     expect(User.last.email).to eq "luke@starwars.com"
   end
+
+  context "invalid sign up: password" do
+    scenario "no user created when passwords mismatch" do
+      expect { sign_up(password_confirmation: "wrongpw") }.not_to change { User.count }
+    end
+
+    scenario "display message when passwords mismatch" do
+      sign_up(password_confirmation: "wrongpw")
+      expect(page.current_path).to eq "/users/new"
+      expect(page).to have_content "Password does not match the confirmation"
+    end
+  end
 end
