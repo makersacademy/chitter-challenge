e2ca7b6c616df5d7cdd048ac80982fff95d22f95
Misa Ogura <misa.ogura01@gmail.com>
2016-03-25 15:01:27 +0000

    Initialise chitter.rb with rspec-sinatra, create database for test/development, create user model for basic sign up, setup database-cleaner, feature test for basic signup passing

---
+++ app/chitter.rb
@@ -0,0 +1,34 @@
+require 'sinatra/base'
+require_relative 'models/user'
+
+class Chitter < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
+  get '/home' do
+    current_user
+    erb(:'home')
+  end
+
+  get '/users/new' do
+    erb(:'users/new')
+  end
+
+  post '/users' do
+    user = User.create(user_name: params[:user_name],
+                       email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect('/home')
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
---
+++ spec/features/user_management_spec.rb
@@ -0,0 +1,11 @@
+feature 'User management' do
+
+  scenario 'User can sign up with username, email, password' do
+    visit('/users/new')
+    fill_in('user_name', with: 'jinis')
+    fill_in('email', with: 'jinis@aol.jp')
+    fill_in('password', with: 'jinis1219')
+    click_button('Sign up')
+    expect(page).to have_content('Welcome to Chitter, jinis!')
+  end
+end
