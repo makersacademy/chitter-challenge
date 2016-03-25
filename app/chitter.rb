98a59e3e2f71c17b2c391a17f0cd2b5850c32892
Misa Ogura <misa.ogura01@gmail.com>
2016-03-25 15:48:09 +0000

    Setup DataMapper setup file

--- app/chitter.rb
+++ app/chitter.rb
@@ -2,6 +2,7 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
 require_relative 'models/user'
+require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,7 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+require_relative 'models/user'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.finalize
