51c35af451c3249cd8eecb8c5bb04e8c113293cb
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-03-26 14:38:19 +0000

    added heroku database option

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,6 @@ class User
     end
   end
 end
-DataMapper.setup(:default, "postgres://localhost/chitter_#{RACK_ENV}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{RACK_ENV}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
