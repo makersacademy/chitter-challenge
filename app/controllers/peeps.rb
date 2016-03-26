d82707b6254c8c4895ae158c719c66167e703fb4
Murilo Dal Ri <murilodalri@gmail.com>
2016-03-26 20:49:05 +0000

    minor changes

--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -4,10 +4,6 @@ class Chitter < Sinatra::Base
     erb :'peeps/index'
   end
 
-  get '/peeps/new' do
-    erb :'peeps/new'
-  end
-
   post '/peeps' do
     peep = Peep.new(content: params[:content],
                     time: DateTime.now)
