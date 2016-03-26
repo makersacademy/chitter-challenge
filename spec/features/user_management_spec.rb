abe8dfaaeeda82aeb9d08676497c2dfeaae1a178
Hanif Fakira <fakira.work@gmail.com>
2016-03-26 17:43:57 +0000

    US1 email and password validation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -8,6 +8,31 @@ feature 'User management' do
   scenario 'mismatch of passwords' do
     expect{sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content('Password did not match...')
+    expect(page).to have_content('Password does not match the confirmation')
+  end
+  
+  context 'email' do
+    scenario 'entering no email will not update database' do
+      expect do
+         visit '/'
+         click_button 'Sign up'
+       end.not_to change(User, :count)
+      expect(page).to have_content('Email must not be blank')
+    end
+
+    scenario ' entering invalid email will not update database' do
+      expect do
+        visit '/'
+        fill_in 'email', with: 'johnsmithgmail.com'
+        click_button 'Sign up'
+      end.not_to change(User, :count)
+      expect(page).to have_content('Email has an invalid format')
+    end
+
+    scenario 'user must have unique email address to sign up' do
+      sign_up
+      expect{ sign_up }.not_to change{ User.count }
+      expect(page).to have_content('Email is already taken')
+    end
   end
 end
