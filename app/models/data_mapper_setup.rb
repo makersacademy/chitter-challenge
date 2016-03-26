db63663023faf7c80d2648254beeaf7b5888b143
Pete Burch <peteburch223@gmail>
2016-03-26 17:17:17 +0100

    added conditional auto_migrate to data_mapper_setup

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -10,4 +10,8 @@ require_relative 'user'
 connection_string = "postgres://localhost/#{DB}_#{ENV['RACK_ENV']}"
 DataMapper.setup(:default, ENV['DATABASE_URL'] || connection_string)
 DataMapper::Logger.new($stdout, :debug)
+if ENV['RACK_ENV'] == 'test'
+    DataMapper.auto_migrate!
+end
+
 DataMapper.finalize
