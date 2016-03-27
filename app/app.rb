46ec35248ce5eb83256ead658141794dda81158e
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-27 13:01:21 +0100

    Added layout

--- app/app.rb
+++ app/app.rb
@@ -7,16 +7,23 @@ class Chittr < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
 
+  helpers do
+      def current_user
+        @current_user ||= User.get(session[:user_id])
+      end
+    end
+
   get '/' do
     erb :'/peeps/index'
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'/users/new'
   end
 
   post '/users' do
-    user = User.create(
+    @user = User.create(
       first_name: params[:first_name],
       last_name: params[:last_name],
       username: params[:username],
@@ -24,6 +31,7 @@ class Chittr < Sinatra::Base
       password: params[:password],
       password_confirmation: params[:password_confirmation]
     )
+    session[:user_id] = @user.id
     redirect to '/'
   end
 
