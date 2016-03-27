9cbd207150c56fb828cefcb4e9592960584037d9
eripheebs <erikapheby@gmail.com>
2016-03-27 12:18:26 +0100

    First feature test for signing up written

--- Gemfile
+++ Gemfile
@@ -1,10 +1,19 @@
 source 'https://rubygems.org'
-ruby '2.2.3'
 
+ruby '2.2.3'
+gem 'database_cleaner'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
+gem 'shotgun'
+gem 'selenium-webdriver'
+gem 'bcrypt'
+gem 'sinatra-flash'
 group :test do
   gem 'rspec'
+  gem 'capybara'
   gem 'cucumber'
   gem 'rake'
+  gem 'pry'
   gem 'coveralls', require: false
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,26 @@ SimpleCov.formatters = [
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
+
+require 'database_cleaner'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require_relative '../app/app.rb'
+require_relative 'web_helper'
+
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
