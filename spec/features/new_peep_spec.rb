a09127126b3289a7543a5f42c76b698d39601cc9
Lorenzo Turrino <lorenzo.turrino@gmail.com>
2016-03-27 10:49:26 +0100

    add new peep form

--- spec/features/new_peep_spec.rb
+++ spec/features/new_peep_spec.rb
@@ -3,7 +3,7 @@ feature 'creation of new peeps' do
   feature 'main page has a form to send a new peep when logged in' do
 
     before :each do
-      visit '/peeps'
+      register_to_site
     end
 
     scenario 'new peep form has a text field' do
@@ -11,15 +11,19 @@ feature 'creation of new peeps' do
     end
 
     scenario 'new peep form has a submit button' do
-      expect(page).to have_button 'Peep!'
+      expect(page).to have_button 'peep'
     end
 
   end
 
   feature 'main page has no form is the user is not logged in' do
 
+    before :each do
+      visit '/peeps'
+    end
+
     scenario 'page has no peep button' do
-      expect(page).not_to have_button 'Peep!'
+      expect(page).not_to have_button 'peep'
     end
 
     scenario 'page has a warning explaining why user can not peep' do
