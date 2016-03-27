33feb6f727c30494ae9b8d05bf95e31d98029053
Paul <paulalexrees@gmail.com>
2016-03-28 00:06:18 +0100

    Not working with peeps

--- app/app.rb
+++ app/app.rb
@@ -18,15 +18,20 @@ class Chitter < Sinatra::Base
   helpers Helpers
 
   get '/sms' do
+    twiml = Twilio::TwiML::Response.new do |r|
+      r.Message "Thanks for the message!"
+    end
     peep = Peep.new(text:params[:Body], time:Time.new)
     current_user.peeps << peep
     peep.save
     current_user.save
-    response = "Thanks for your peep - view it at https://pauly-chitter.herokuapp.com"
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.Message response
-    end
-    twiml.text
+    # response = "Thanks for your peep - view it at https://pauly-chitter.herokuapp.com"
+    # twiml = Twilio::TwiML::Response.new do |r|
+    #   r.Message response
+    # end
+    # twiml.text
+
+  twiml.text
   end
 
   get '/' do
