ffd63f28cf81fa97d3df2bb14307f66c02ed8540
Claudia Green <claudia@claudiag.me.uk>
2016-03-27 17:50:22 +0100

    Tests now all passing.

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -6,9 +6,7 @@ feature 'Viewing peeps' do
 
   scenario "I can see a peep I posted on the peep feed" do
     sign_in(email: user.email, password: user.password)
-    click_button 'Peep'
-    fill_in :content, with: "This is a test peep"
-    click_button 'Post peep!'
+    post_peep(message: 'This is a test peep')
     expect(page.status_code).to eq 200
     expect(page).to have_content('This is a test peep')
   end
