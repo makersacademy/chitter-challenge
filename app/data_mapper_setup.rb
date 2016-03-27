f26595980dd46ce5453c09640742c758c96ef6fe
Gabrielle <gabivk@gmail.com>
2016-03-27 17:50:05 +0100

    CSS nip/tuck 1.0

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,7 +6,7 @@ require 'bcrypt'
 require_relative 'models/maker'
 require_relative 'models/peep'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||  "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 if ENV['RACK_ENV'] == 'test'
    DataMapper.auto_migrate!
