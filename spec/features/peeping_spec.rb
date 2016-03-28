b6b4e47ba00014d7aaa96eb7257411504ee525b1
Hanif Fakira <fakira.work@gmail.com>
2016-03-28 09:34:04 +0100

    US4 done

--- spec/features/peeping_spec.rb
+++ spec/features/peeping_spec.rb
@@ -20,13 +20,13 @@ feature 'User can peep' do
     scenario 'with no message' do
       sign_up
       sign_in
-      #fill_in('message', with: nil)
       expect{ click_button('Peep') }.not_to change(Peep, :count)
     end
 
-    # scenario 'if not logged in' do
-    #   visit('/home')
-    #   expect(page).not_to have_xpath('//div[@id = "new-message"]')
-    # end
+    scenario 'if not logged in' do
+      visit('/home')
+      expect(page).not_to have_css('div.post')
+      expect(page).to have_content('Peep something')
+    end
   end
 end
