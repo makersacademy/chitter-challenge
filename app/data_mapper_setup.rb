a1455dba03db803b58be8055e5ba7897f76398d9
Tobenna <tobennandu@gmail.com>
2016-03-28 22:39:09 +0100

    Trying to fix travis CI

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,3 +1,4 @@
 DB_URL = "postgres://localhost/chitter_#{ENV['RACK_ENV']}".freeze
 DataMapper.setup(:default, ENV['DATABASE_URL'] || DB_URL)
+DataMapper.auto_migrate! if ENV['RACK_ENV'] == 'test'
 DataMapper.finalize
