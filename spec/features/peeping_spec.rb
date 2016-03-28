ce5c5e11bcfd31239bd5701d887a21eece8a55fb
Hanif Fakira <fakira.work@gmail.com>
2016-03-28 08:45:20 +0100

    US4 no peeping blank message

--- spec/features/peeping_spec.rb
+++ spec/features/peeping_spec.rb
@@ -16,17 +16,17 @@ feature 'User can peep' do
       expect(page).to have_content("ghost peeped")
     end
   end
-  # context 'Cannot peep' do
-  #   scenario 'with no message' do
-  #     sign_up
-  #     sign_in
-  #     #fill_in('message', with: nil)
-  #     expect{ click_button('Peep') }.not_to change(Peep, :count)
-  #   end
-  #
-  #   # scenario 'if not logged in' do
-  #   #   visit('/home')
-  #   #   expect(page).not_to have_xpath('//div[@id = "new-message"]')
-  #   # end
-  # end
+  context 'Cannot peep' do
+    scenario 'with no message' do
+      sign_up
+      sign_in
+      #fill_in('message', with: nil)
+      expect{ click_button('Peep') }.not_to change(Peep, :count)
+    end
+
+    # scenario 'if not logged in' do
+    #   visit('/home')
+    #   expect(page).not_to have_xpath('//div[@id = "new-message"]')
+    # end
+  end
 end
