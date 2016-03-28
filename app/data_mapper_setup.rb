0999a4357de798cf489429b9fb954df8a25a28f7
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-28 22:47:39 +0100

    extracted auto-migrate and auto-update of DB into Rakefile

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,4 +7,4 @@ require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chittr_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_upgrade!
+# DataMapper.auto_upgrade!
