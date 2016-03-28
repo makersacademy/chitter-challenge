5d7607e930788348ca93de6e758fb22200b80e55
Shane Oston Stowe <shane@shaneoston.com>
2016-03-28 18:03:34 +0100

    added partials to server

--- app/server.rb
+++ app/server.rb
@@ -1,11 +1,17 @@
 class Chitter < Sinatra::Base
 
   enable :sessions
-  set :session_secret, 'super secret'
-  use Rack::MethodOverride
 
   register Sinatra::Flash
+  register Sinatra::Partial
 
+  set :session_secret, 'super secret'
+  set :partial_template_engine, :erb
+
+  enable :partial_underscores
+
+  use Rack::MethodOverride
+  
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
