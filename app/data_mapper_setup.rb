1f6333fae28536c4d80e1bbaa91428910a633c1b
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-29 11:05:00 +0100

    Updated auto mapper setup for travis CI

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,4 +6,10 @@ require_relative 'models/peep'
 require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chittr_#{ENV['RACK_ENV']}")
+DataMapper::Logger.new($stdout, :debug)
+
+if ENV['RACK_ENV'] == 'test'
+    DataMapper.auto_migrate!
+end
+
 DataMapper.finalize
