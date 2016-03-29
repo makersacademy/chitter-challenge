1ef9df26a1997cf21d867d9550f2dc8fb753f665
olmesm <olmesm@gmail.com>
2016-03-29 11:04:55 +0100

    added auto_migrate to dm

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -7,4 +7,9 @@ require_relative 'models/peep'
 require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ ENV["RACK_ENV"] }")
+
+if ENV['RACK_ENV'] = 'test'
+  DataMapper.auto_migrate!
+end
+
 DataMapper.finalize
