3fa0b740fd473c4824b8c9fbef9a3c23d727836f
Patrick <p_donati@hotmail.com>
2016-03-29 10:46:14 +0100

    almost finished third user story

---
+++ spec/helpers/session.rb
@@ -0,0 +1,23 @@
+module SessionHelpers
+
+ def sign_in(email:, password:)
+   visit '/sessions/new'
+   fill_in :email, with: email
+   fill_in :password, with: password
+   click_button 'Sign in'
+ end
+
+ def sign_up(name: 'Patrick',
+             email: 'pat@test.com',
+             password: '12345678',
+             password_confirmation: '12345678')
+   visit '/users/new'
+   fill_in :name, with: name
+   fill_in :password, with: password
+   fill_in :password_confirmation, with: password_confirmation
+   fill_in :email, with: email
+   click_button 'Sign up'
+ end
+
+
+end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -10,5 +10,9 @@ def sign_up(name: 'Patrick',
   click_button 'Sign up'
 end
 
-def sign_in
+def sign_in(email:, password:)
+  visit '/sessions/new'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  click_button 'Sign in'
 end
