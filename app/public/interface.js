7410792718de341a188583d3606c585239d230ee
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-31 13:34:46 +0100

    Added more jquery

--- app/public/interface.js
+++ app/public/interface.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 
-  $('#bird_pic').fadeTo('slow', 1);
+  $('#bird_homepage').fadeTo('slow', 1);
 
-  $('#bird_pic').animate({
+  $('#bird_homepage').animate({
     right: '510px',
     top: '250px',
     height: '250px',
     width: 'auto'
   });
+
+  $('#bird_homepage').mouseenter(function() {
+    $('#bird_speech').text("Peep peep...");
+  });
+
+  $('#bird_homepage').mouseleave(function() {
+    $('#bird_speech').text("");
+  });
+
 
 });
