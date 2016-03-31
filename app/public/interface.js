41a26749e904914a5790db83662e89c33ecdae33
Yasmin Green <yasmingreen@Yasmins-MacBook-Pro.local>
2016-03-31 13:22:44 +0100

    Added jquery to html

---
+++ app/public/interface.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+  $('#bird_pic').fadeTo('slow', 1);
+
+  $('#bird_pic').animate({
+    right: '510px',
+    top: '250px',
+    height: '250px',
+    width: 'auto'
+  });
+
+});
