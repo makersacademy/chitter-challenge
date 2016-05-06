7740ae6e2133561580370d8e9fdc616ecd416432
Emma Sjöström <emma.sjoestroem@gmail.com>
2016-05-07 00:29:30 +0200

    Added auto_upgrade! to data_mapper_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,3 +7,4 @@ env = ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{env}")
 #DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
+DataMapper.auto_upgrade!
