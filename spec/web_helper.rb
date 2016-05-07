acbffc850c3bebae4b940bcb9624964c7b19ab9b
sitypop <sityemilyshah@gmail.com>
2016-05-07 14:40:45 +0100

    Created web_helper for sign_up

---
+++ spec/web_helper.rb
@@ -0,0 +1,9 @@
+def sign_up
+  visit '/users/new'
+  expect(page.status_code).to eq(200)
+  fill_in :username, with: 'sitypop'
+  fill_in :name,     with: 'Sity Shah'
+  fill_in :email,    with: 'sity@ilikefalafel.com'
+  fill_in :password, with: 'secret'
+  click_button 'Sign up'
+end
