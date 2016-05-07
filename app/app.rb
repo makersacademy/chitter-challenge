cbd09606299aeb0db4590cbecc1fbf4549ba675f
Alex Robertson <lex1989@gmail.com>
2016-05-07 22:05:00 +0100

    Added logic to control what buttons appear based on login status

--- app/app.rb
+++ app/app.rb
@@ -45,9 +45,9 @@ class Chitter < Sinatra::Base
   end
 
   post '/sessions' do
-    user = User.authenticate(params[:username], params[:password])
-    if user
-      session[:user_id] = user.id
+    @user = User.authenticate(params[:username], params[:password])
+    if @user
+      session[:user_id] = @user.id
       redirect to '/'
     else
       flash.now[:errors] = ['The username or password is incorrect']
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,17 +42,17 @@ Capybara.app = Chitter
 RSpec.configure do |config|
 
   config.before(:suite) do
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
-    end
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   config.include Capybara::DSL
   # rspec-expectations config goes here. You can use an alternate
