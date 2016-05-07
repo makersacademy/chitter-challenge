5252d5b92371ce7a25763fc78c3de7da31ddc06b
Ruta <ruta@baltiejus.com>
2016-05-07 15:14:57 +0100

    cannot sign up with an invalid email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
+  validates_format_of :email, as: :email_address
 
-  property :id,              Serial
-  property :name,            String
-  property :username,        String
-  property :email,           String
+  property :id, Serial
+  property :name, String, required: true
+  property :username, String, required: true
+  property :email, String, required: true
   property :password_digest, Text
 
   def password=(password)
