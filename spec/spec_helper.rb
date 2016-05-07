c3a8db95f5f974bec9631edf7a139159cb880b37
Omar Alvarez <omajul2@hotmail.com>
2016-05-07 08:42:45 +0200

    Configure database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'coveralls'
 require 'simplecov'
 require  'capybara'
 require 'capybara/rspec'
+require 'database_cleaner'
 require './app/app'
 require './app/models/peep'
 
@@ -17,25 +18,18 @@ RSpec.configure do |config|
 
   # config.include SessionHelpers
 
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :transaction
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
 
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
-
-  # Also can be done with...(combines before/after)
-  # config.around(:each) do |example|
-  #   DatabaseCleaner.cleaning do
-  #     example.run
-  #   end
-  # end
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
