77315c7dd815a2af0e8ebcf65e2ab29e6bbb1f87
sitypop <sityemilyshah@gmail.com>
2016-05-07 13:23:39 +0100

    Posting a new peep

---
+++ spec/features/posting_peeps_spec.rb
@@ -0,0 +1,14 @@
+feature 'Posting peeps' do
+
+  scenario 'I can post a new peep' do
+    visit '/peeps/new'
+    fill_in 'body',   with: 'I like peeping!'
+    click_button 'Post peep!'
+
+    expect(current_path).to eq '/peeps'
+
+    within 'ul#peeps' do
+      expect(page).to have_content('I like peeping!')
+    end
+  end
+end
