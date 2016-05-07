dd4386136501cd3f27d7ab3dd4f451e33cc8ee21
Omar Alvarez <omajul2@hotmail.com>
2016-05-07 08:38:44 +0200

    Add time format to Peep and print them in reverse order

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -8,6 +8,11 @@ class Peep
 	property :content, Text
 	property :time, DateTime
 
+	def time_format
+		# self.time.strftime("%c")
+		self.time.strftime("Posted on %m/%d/%Y at %I:%M%p")
+	end
+
 end
 
 DataMapper.setup(:default, "postgres://localhost/chitter_test")
