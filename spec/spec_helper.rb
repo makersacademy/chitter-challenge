60b19245cc0bd71a949c98f0d6291f1b26fcb338
Omar Alvarez <omajul2@hotmail.com>
2016-05-07 07:28:16 +0200

    Creating a modular sinatra app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,46 @@
 require 'coveralls'
 require 'simplecov'
+require  'capybara'
+require 'capybara/rspec'
+require './app/app'
+
+Capybara.app = Chitter
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+RSpec.configure do |config|
+
+  # config.include SessionHelpers
+
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
+
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
+
+  # Also can be done with...(combines before/after)
+  # config.around(:each) do |example|
+  #   DatabaseCleaner.cleaning do
+  #     example.run
+  #   end
+  # end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
