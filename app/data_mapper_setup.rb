8332297394310d431a850805bf938bf79cb44f1a
Robin Heathcote <robin.heathcote@icloud.com>
2016-05-08 19:23:19 +0100

    heroku and trying to get coveralls to work

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,3 +7,4 @@ require_relative 'models/peep'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
+DataMapper.auto_migrate! if ENV['RACK_ENV'] == 'test'
