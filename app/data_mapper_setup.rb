fe52e90a16158ccd4ac062d47e2686b3aeaeb96a
chriselevate <coates.c@hotmail.com>
2016-05-08 14:26:30 +0100

    DB change

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,5 +4,6 @@ require 'dm-validations'
 
 require_relative './models/user'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
