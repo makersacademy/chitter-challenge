7b6934351749a00f8ae5ed7d11c3cd02b07804ae
Sergio Enrech Trillo <senrech@yahoo.fr>
2016-05-08 21:11:39 +0100

    make it a bit prettier

--- spec/helpers/session.rb
+++ spec/helpers/session.rb
@@ -29,7 +29,7 @@ module SessionHelpers
     fill_in :password_confirmation, with: password_confirmation
     fill_in :name, with: name
     fill_in :username, with: username
-    click_button 'Sign up'
+    click_button 'Register'
   end
 
   def sign_in(email:, password:)
