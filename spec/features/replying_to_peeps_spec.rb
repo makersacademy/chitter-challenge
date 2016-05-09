4d15cbec0a9561e5f876962d01d4be4cbf8389b7
gerauf <fo4786@gmail.com>
2016-05-09 09:10:42 +0100

    migrated to new computer and clean up

--- spec/features/replying_to_peeps_spec.rb
+++ spec/features/replying_to_peeps_spec.rb
@@ -6,10 +6,10 @@ feature "reply to peeps" do
     sign_in "ferg", "ferg@gmail.com"
     peep "Naaah Makers"
     peep_id_2 = Peep.first(content: "Naaah Makers").id
-    within "li##{peep_id_1}" do
+    within "article##{peep_id_1}" do
       find_button 'reply'
     end
-    within "li##{peep_id_2}" do
+    within "article##{peep_id_2}" do
       expect(page).not_to have_button 'reply'
     end
   end
@@ -19,7 +19,7 @@ feature "reply to peeps" do
     peep "General asembly for life"
     peep_id_1 = Peep.first(content: "General asembly for life").id
     sign_in "ferg", "ferg@gmail.com"
-    within "li##{peep_id_1}" do
+    within "article##{peep_id_1}" do
       click_button 'reply'
     end
     expect(page).to have_content "original peep"
@@ -27,7 +27,7 @@ feature "reply to peeps" do
     fill_in :content, with: "Naah GA sucks balls"
     click_button 'reply'
     peep_id_2 = Peep.first(content: "Naah GA sucks balls").id
-    within "li##{peep_id_2}" do
+    within "article##{peep_id_2}" do
       expect(page).to have_content "in response to MacDaNNy"
     end
   end
