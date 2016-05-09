733dc591dcff5cfee90daaa011d4ee69ef110d71
Aday Mesa <adaymesagc@gmail.com>
2016-05-09 15:40:22 +0100

    Hound improves

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,7 @@ require 'dm-validations'
 require_relative 'models/user'
 require_relative 'models/peeps'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] ||
+ "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_migrate! if ENV['RACK_ENV'] == 'test'
