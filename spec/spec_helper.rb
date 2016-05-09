532e0a1610fd85db5a8ca21173ae858941717769
Lexi Tran <hazukitran@gmail.com>
2016-05-09 18:51:33 +0700

    Add partial

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ ENV["RACK_ENV"] = "test"
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-# require './app/models/link'
 require './app/app'
 require 'database_cleaner'
 require 'features/web_helper'
