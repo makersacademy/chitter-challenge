c8879bf666feae252b9e865cbbada408167ddaf1
Ben Stein <bumstain@gmail.com>
2016-05-10 15:48:17 +1000

    Added improved layout for the peeps view

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,7 @@ require_relative 'models/peep'
 
 env = ENV['RACK_ENV'] || 'development'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 
+  "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
