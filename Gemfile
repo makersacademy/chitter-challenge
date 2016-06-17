7bdf78b7046ce320e07287979dcdeba53651d00d
Elena <elena.vilimaite@gmail.com>
2016-06-17 17:51:12 +0100

    login and sign up and register

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'dm-postgres-adapter'
 gem 'rake'
 gem 'bcrypt'
 gem 'sinatra-flash'
+gem 'sinatra-partial'
 
 group :test do
 	gem 'rspec'
--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,66 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
+require 'sinatra/flash'
+
+require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
-  get '/' do
-    'Hello Chitter!'
-  end
+	enable :sessions
+	set :session_secret, 'super secret'
+	register Sinatra::Flash
 
-  # start the server if ruby file executed directly
-  run! if app_file == $0
+	get '/' do
+		@username = session['user']
+		erb :index
+	end
+
+	get '/user/new' do
+		erb :'user/new'
+	end
+
+	post '/user/new' do
+		user = User.new(
+		email: params[:email],
+		password: params[:password],
+		password_confirmation: params[:password_confirmation]
+		)
+		if (user.save)
+			session[:user_id] = user.id
+			redirect ('/')
+		else
+			flash.now[:errors] = user.errors.full_messages
+			erb :'user/new'
+		end
+	end
+
+	get '/user/login' do
+		erb :'user/login'
+	end
+
+	post '/user/login' do
+		user = User.authenticate(params[:email], params[:password])
+		if user
+			session[:user_id] = user.id
+			redirect ('/')
+		else
+			flash.now[:errors] = ["The email or password is incorrect"]
+			erb :'user/login'
+		end
+	end
+
+	get '/user/logout' do
+		@last_user = current_user
+		@current_user = nil
+		session[:user_id] = nil
+		erb :index
+	end
+
+	helpers do
+		def current_user
+			@current_user ||= User.get(session[:user_id])
+		end
+	end
+	# start the server if ruby file executed directly
+	run! if app_file == $0
 end
