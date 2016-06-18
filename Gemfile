9feef2d418cd039f7ce877e830adc61d253837c1
srMarquinho <sr.marquinho@gmail.com>
2016-06-18 09:14:01 +0100

    first rspec and app setup

--- Gemfile
+++ Gemfile
@@ -1,10 +1,17 @@
 source 'https://rubygems.org'
 ruby '2.2.3'
 
+gem 'sinatra'
+
+group :data_base do
+  gem 'data_mapper'
+  gem 'dm-postgres-adapter'
+end
+
 group :test do
   gem 'rspec'
   gem 'cucumber'
   gem 'rake'
   gem 'coveralls', require: false
+  gem 'capybara'
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,29 @@ SimpleCov.formatters = [
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
+  config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+end
