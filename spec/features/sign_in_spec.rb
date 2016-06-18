61b3e01b930afcd48836b60d0f6d6b5f1ec6f104
tobywilkins <tobywilkinsuK@hotmail.co.uk>
2016-06-18 11:02:57 +0100

    Added verification on duplicate email/username & have messaging appearing on front end in a <ul>

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -16,4 +16,12 @@ feature 'sign in' do
   scenario 'does not create a user if confirmation password is wrong' do
       expect{sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
   end
+
+
+  scenario 'does not allow sign up if username or password is duplicated' do
+      sign_up
+      sign_up
+      expect(page).to have_content "Email is already taken"
+      expect(page).to have_content "Username is already taken"
+  end
 end
