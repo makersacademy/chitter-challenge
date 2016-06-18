04dc7b441685274baf58d48af9ae7f114012e0ca
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-18 17:57:31 +0100

    Stores password securly using bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,15 @@ class User
 
   include DataMapper::Resource
   
-  property :id,       Serial
-  property :name,     String
-  property :surname,  String 
-  property :email,    String 
-  property :password, String 
-  property :username, String 
+  property :id,              Serial
+  property :name,            String
+  property :surname,         String 
+  property :email,           String 
+  property :password_digest, String, length: 60
+  property :username,        String 
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
   
 end
