e3dc4b4b6a06675fc5a567db03c258ceb2f961d2
riyapabari <riya.pabari@gmail.com>
2016-06-18 11:54:45 +0200

    Added Rakefile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 require 'bcrypt'
-require 'data_mapper'
-require 'dm-postgres-adapter'
 
 class User
 
@@ -19,8 +17,3 @@ def password=(password)
 
 end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
-
-DataMapper.finalize
-
-DataMapper.auto_upgrade!
