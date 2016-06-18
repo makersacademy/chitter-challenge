70fadb6aad8b0b49e2added7081773ef2469c73b
Luke <lukeaveil@yahoo.com>
2016-06-18 17:41:33 +0100

    sign in function added

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,7 +4,7 @@ feature 'User sign up' do
   
   scenario 'I can sign up as a new user' do
   	expect { sign_up }.to change(User, :count).by(1)
-  	expect(page).to have_content('Welcome, Luke')
+  	expect(page).to have_content('Welcome, luke@example.com')
   	expect(User.first.email).to eq('luke@example.com')
   end
 
@@ -21,19 +21,19 @@ feature 'User sign up' do
   scenario "I can't sign up without an email address" do
     expect { sign_up(email: nil) }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content('Email must not be blank')
+    expect(page).to have_content 'Email must not be blank'
   end
 
   scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content('Email has an invalid format')
+    expect(page).to have_content 'Email has an invalid format'
   end
 
   scenario 'I cannot sign up with an existing email' do
     sign_up
     expect { sign_up }.to_not change(User, :count)
-    expect(page).to have_content('Email is already taken')
+    expect(page).to have_content 'Email is already taken'
   end
   	
 end
