657e6625539e7dc11a6697d0ea891d351f8aa41e
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-18 17:08:07 +0100

    Refactor to make Hound happy and update travis file

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,6 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative './models/user'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV["RACK_ENV"]}")
+local_environments = "postgres://localhost/chitter_#{ENV["RACK_ENV"]}"
+DataMapper.setup(:default, ENV['DATABASE_URL'] || local_environments)
 DataMapper.finalize
 DataMapper.auto_upgrade!
