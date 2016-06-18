36c36c7714245ac879dff11149da50112073b539
Luke <lukeaveil@yahoo.com>
2016-06-18 16:21:17 +0100

    validating a blank email

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,12 @@ class User
   attr_accessor :password_confirmation 
 
   validates_confirmation_of :password
+  validates_format_of :email, as: :email_address
 
   property :id, Serial
   property :name, String
-  property :handle, String
-  property :email, String
+  property :handle, String, required: true, unique: true
+  property :email, String, required: true, unique: true
   property :password_digest, String, length: 60
 
   def password=(password)
