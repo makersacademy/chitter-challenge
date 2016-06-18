b950804bae37a712e02a84b840cae89224487225
Stephen Dawes <sdawes@outlook.com>
2016-06-18 18:45:03 +0100

    add password and email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User
   attr_accessor :password_confirmation
 
   property :id,               Serial
-  property :email,            String, format: :email_address, required: true
+  property :email,            String, format: :email_address, required: true, unique: true
   property :handle,           String, required: true
   property :password_digest,  Text, length: 60
 
