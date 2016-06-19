f2d0a3c3efdc2fea056c79fa72102a4424c5d79e
toby676 <tjcmui@gmail.com>
2016-06-19 13:10:45 +0100

    refactor

--- app.rb
+++ app.rb
@@ -1,67 +1,17 @@
 require 'sinatra/base'
 require 'sinatra/flash'
+require 'sinatra/partial'
 require_relative 'data_mapper_setup.rb'
 
+require_relative 'server'
+require_relative 'controllers/users'
+require_relative 'controllers/sessions'
+
 class Chitter < Sinatra::Base
-
-  use Rack::MethodOverride
-
-  enable :sessions
-  set :session_secret, 'super secret'
-
-  register Sinatra::Flash
 
   get '/' do
     erb :index
   end
-
-  get '/users/new' do
-    @user = User.new
-    erb :'users/new'
-  end
-
-  post '/users' do
-    @user = User.create(username: params[:username],
-                name: params[:name],
-                email: params[:email],
-                password: params[:password],
-                password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to '/'
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'users/new'
-    end
-  end
-
-  get '/sessions/new' do
-    erb :'sessions/new'
-  end
-
-  post '/sessions' do
-  user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect to('/')
-    else
-      flash.now[:errors] = ['The email or password is incorrect']
-      erb :'sessions/new'
-    end
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    flash.keep[:notice] = 'goodbye!'
-    redirect to '/'
-  end
-
-  helpers do
-   def current_user
-     @current_user ||= User.get(session[:user_id])
-   end
-  end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
---
+++ server.rb
@@ -0,0 +1,24 @@
+class Chitter < Sinatra::Base
+
+  use Rack::MethodOverride
+
+  enable :sessions
+  register Sinatra::Flash
+  register Sinatra::Partial
+
+  set :session_secret, 'super secret'
+  set :partial_template_engine, :erb
+
+  enable :partial_underscores
+
+
+  helpers do
+   def current_user
+     @current_user ||= User.get(session[:user_id])
+   end
+  end
+
+
+
+
+end
