bb18cd0f52825e66af8dd0534bfc15c8555b0a24
toby676 <tjcmui@gmail.com>
2016-06-19 11:46:18 +0100

    add sign in function

--- models/user.rb
+++ models/user.rb
@@ -9,14 +9,23 @@ class User
   validates_confirmation_of :password
 
   property :id, Serial
-  property :username, String
-  property :name, String
-  property :email, String
-  property :password_digest, String, length: 60
+  property :username, String, required: true, unique: true
+  property :name, String, required: true
+  property :email, String, required: true, unique: true
+  property :password_digest, String, length: 60, required: true
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_digest) == password
+      user
+    else
+      nil
+    end
   end
 
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -12,8 +12,19 @@ feature 'user sign up' do
   scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
+  scenario 'I cannot sign up with an existing email' do
+    sign_up
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
+  end
+
+  scenario 'I cannot sign up without an email address' do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
+  end
 
 end
