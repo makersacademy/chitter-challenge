7bdf97314270c700616f3c1f090dc1cfa116b727
Crescenzo D'Alterio <crescenzo.dalterio@gmail.com>
2016-06-19 17:30:23 +0100

    Sixth user story implemented:
    As a maker
    So that I can better appreciate the context of a peep
    I want to see the time at which it was made

--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -15,16 +15,28 @@ feature 'Peeps' do
   scenario 'peeps are showed in reverse chronological order' do
     sign_up
     post_a_peep(content: 'This is my FIRST peep')
-    sleep(2)
+    sleep(1)
     post_a_peep(content: 'This is my SECOND peep')
-    sleep(2)
+    sleep(1)
     post_a_peep(content: 'This is my THIRD peep')
 
     within 'ul#peeps' do
-      expect(find(:li, 1).text).to eq ('This is my THIRD peep')
-      expect(find(:li, 2).text).to eq ('This is my SECOND peep')
-      expect(find(:li, 3).text).to eq ('This is my FIRST peep')
+      expect(find(:li, 1).text).to have_content ('This is my THIRD peep')
+      expect(find(:li, 2).text).to have_content ('This is my SECOND peep')
+      expect(find(:li, 3).text).to have_content ('This is my FIRST peep')
     end
   end
+
+  scenario 'each peep has its username and timestamp' do
+    sign_up
+    post_a_peep(content: 'This is my FIRST peep')
+    peep = Peep.first(content: 'This is my FIRST peep')
+    username = peep.user.username
+    time = peep.created_at.strftime("%a %b %d  %H:%M:%S")
+    expect(page).to have_content time
+    expect(page).to have_content username
+  end
+
+  
 
 end
