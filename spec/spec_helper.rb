804b83f2270c13f02c7eb002ca47661eb907b10d
Anna <anna.hollandsmith3@gmail.com>
2016-06-19 10:06:17 +0100

    changes made in preparation for deploying to heroku

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 require './app/models/peep'
+require 'database_cleaner'
 
 Capybara.app = Chitter
 
@@ -26,5 +27,18 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
