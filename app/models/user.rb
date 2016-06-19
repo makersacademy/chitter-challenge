5e792bf913e3387f41cebf91060befbbd0befad9
awye765 <awye765@googlemail.com>
2016-06-19 08:09:03 +0100

    User Story 1 - Added additional property to user model and conditional logic to user controller to prevent duplicate email registrations and introduced flash errors for password mismatch errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User
   property :id, Serial
   property :name, String, required: true
   property :username, String, required: true
-  property :email, String, required: true
+  property :email, String, required: true, unique: true
   property :password_digest, String, length: 60
 
   def password=(password)
