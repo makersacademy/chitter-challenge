8d9ba3b2420ddd5b3182aca4d87bc5a61f3c9dce
Thomas Harker <tom.harker@hotmail.co.uk>
2016-06-19 21:43:56 +0100

    Adjust datamapper settings for heroku

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -1,6 +1,5 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
-
 require_relative './models/peep'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
