dd322373b3199abf1ffaea6bfa04cb12137e1c78
awye765 <awye765@googlemail.com>
2016-06-19 10:47:55 +0100

    User Story 4 - Complete.  Added ability to view all spits in reverse chronological order

--- spec/features/posting_spits_spec.rb
+++ spec/features/posting_spits_spec.rb
@@ -11,14 +11,3 @@ feature "FEATURE: Creating Spits" do
     end
   end
 end
-
-xfeature "FEATURE: Posting spits" do
-  scenario "users can post spits" do
-    visit '/spit/new'
-    fill_in :content, with: "I'm more awesome than you"
-    click_button "Spit!"
-    within "ul#spits" do
-      expect(page).to have_content "I'm more awesome than you"
-    end
-  end
-end
