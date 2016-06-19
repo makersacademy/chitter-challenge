66afc42eb9d6c5384a86bb61bf2cbdce2fe656c3
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-19 13:57:01 +0100

    User story 5: peeps shown in cronological order

--- spec/features/peep_on_chitter_spec.rb
+++ spec/features/peep_on_chitter_spec.rb
@@ -2,6 +2,10 @@
 #So that I can let people know what I am doing  
 #I want to post a message (peep) to chitter
 
+#As a maker
+#So that I can see what others are saying  
+#I want to see all peeps in reverse chronological order
+
 feature 'Peep' do
 
   context 'After signup' do
@@ -9,14 +13,21 @@ feature 'Peep' do
 
     scenario 'User can post a peep, which is then shown on page' do
       peep 'My first peep!'
-      expect(page).to have_content'My first peep!'
+      expect(page).to have_content 'My first peep!'
     end
 
-    scenario 'User can post 2 peeps, which are both shown on page' do
+    scenario 'User can post 2 peeps, which are shown in reverse order' do
       peep 'My first peep!'
       peep 'My second peep!'
-      expect(page).to have_content'My first peep!'
-      expect(page).to have_content'My second peep!'
+     
+      within('li:nth-child(1)') do
+        expect(page).to have_content 'second'
+      end
+
+      within('li:nth-child(2)') do
+        expect(page).to have_content 'first'
+      end
     end
+
   end
 end
