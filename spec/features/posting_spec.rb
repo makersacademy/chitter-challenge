8d0c72c43be2faca56b9adc9e75591e0ec6eb70c
lomlo <michael.lo@live.co.uk>
2016-06-19 14:40:20 +0100

    more notes added to rspec to find error

--- spec/features/posting_spec.rb
+++ spec/features/posting_spec.rb
@@ -16,10 +16,13 @@ feature 'Users can create a post' do
   scenario 'User can add a post to their account' do
     sign_up
     post
-    user = User.first(id: 1)
-    p '=============d'
-    p user
-    expect(Post.all(:user_id => 1).to include('This is the best app ever!'))
+    p '=============d1'
+    p user = User.first(id: 2)
+    p '=============d2'
+    p user = User.first(id: 1)
+
+
+    expect(Post.all(:user_id => 2).to include('This is the best app ever!'))
     # expect(link.tags.map(&:name)).to include('education')
   end
 end
