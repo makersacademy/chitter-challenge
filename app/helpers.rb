24b7a88643b766cf365d436fdfc5d1c3d97b9a83
joestephens <joe.stephens@live.co.uk>
2016-06-19 21:30:24 +0100

    Timezone adjustment removed

--- app/helpers.rb
+++ app/helpers.rb
@@ -1,12 +1,9 @@
-require 'active_support/core_ext/time'
-
 module Helpers
   def get_user_by_id(user_id)
     User.first(id: user_id)
   end
 
   def pretty_date(timestamp)
-    time_plus_one = timestamp + 7200
-    time_plus_one.strftime("%a %d %B %H:%M")
+    timestamp.strftime("%a %d %B %H:%M")
   end
 end
