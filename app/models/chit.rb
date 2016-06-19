9d3b94a294b47043682134928509f873d608b96f
Carlos <chdezmar@gmail.com>
2016-06-19 01:59:43 +0100

    heroku

--- app/models/chit.rb
+++ app/models/chit.rb
@@ -12,6 +12,6 @@ class Chit
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
