7debc8ef694768a35aa942e5d6cd5c660d7ec100
Carlos <chdezmar@gmail.com>
2016-06-19 02:05:12 +0100

    DataMapper setup

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,7 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
 require_relative 'models/chit'
+require_relative 'data_mapper_setup'
 
 
 class ChitterChallenge < Sinatra::Base
--- app/models/chit.rb
+++ app/models/chit.rb
@@ -11,7 +11,3 @@ class Chit
 	property :time, Time
 
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
