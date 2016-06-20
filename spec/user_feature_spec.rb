23d78efeb29cf4a4b0d12a67b496effe02d88524
Elena <elena.vilimaite@gmail.com>
2016-06-20 01:04:37 +0100

    eyyy we're testing like pros now

--- spec/user_feature_spec.rb
+++ spec/user_feature_spec.rb
@@ -1,35 +1,13 @@
-require 'capybara'
-
-def sign_up(
-		email: 'email@mail.com', 
-		username: 'Test_user', 
-		password: 'super secret', 
-		password_confirmation: 'super secret' 
-	)
-	visit '/user/new'
-	fill_in 'email', with: email
-	fill_in 'password', with: password
-	fill_in 'password_confirmation', with: password_confirmation
-	click_button 'Sign up'
-end
-
-def sign_in(email: 'email@mail.com', password: 'super secret')
-	visit '/user/login'
-	fill_in 'email', with: email
-	fill_in 'password', with: password
-	click_button 'Sign in'
-end
-
 feature 'user sign up' do
 
 	scenario 'successful sign up' do
 		expect { sign_up }.to change(User, :count).by(1)
-		expect(page).to have_content 'Welcome Test_user'
+		expect(page).to have_content 'Welcome, Test_user'
 	end
 
 	scenario 'Passwords dont match' do
 		expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-		expect(current_path).to eq("/user/login")
+		expect(current_path).to eq("/user/new")
 		expect(page).to have_content "Password does not match the confirmation"
 	end
 
@@ -57,19 +35,24 @@ feature 'Sign in' do
 
 	scenario 'can sign in' do
 		sign_in
-		expect(page).to have_content 'Welcome Test_user'
+		expect(page).to have_content 'Welcome, Test_user'
 	end
 
-	scenario 'can sign in' do
+	scenario 'can\'t sign in with wrong password' do
 		sign_in(password: 'wrong')
 		expect(page).to have_content "The email or password is incorrect"
 	end
 
-	# scenario 'can sign out' do
-	# 	sign_in
-	# 	click_link "Sign out"
-	# 	expect(page).to have_content "Goodbye!"
-	# end
+	scenario 'can\'t sign in with wrong email' do
+		sign_in(email: 'wrong@email.com')
+		expect(page).to have_content "The email or password is incorrect"
+	end
+
+	scenario 'can sign out' do
+		sign_in
+		click_link "Sign out"
+		expect(page).to have_content "CHITTLR: ˱(ْ۬ ˂̵ْ۬ )˲"
+	end
 
 
 end
