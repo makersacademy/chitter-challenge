59eca83c0b1223c8142040916076279680ba0ac9
Nomi Vos <nvoswork@gmail.com>
2016-07-30 10:56:54 -0500

    sets up project

---
+++ spec/web_helper.rb
@@ -0,0 +1,21 @@
+def sign_up(username: 'nomi811',
+            name: 'Nomi Vos',
+            email: 'nomi@example.com',
+            password: 'password',
+            confirm_password: 'password')
+  visit '/users/new'
+  fill_in :username, with: username
+  fill_in :name, with: name
+  fill_in :email,    with: email
+  fill_in :password, with: password
+  fill_in :confirm_password, with: confirm_password
+  click_button 'Sign up'
+end
+
+# def sign_in(email:, password:)
+# visit '/users/signin'
+# expect(page.status_code).to eq(200)
+# fill_in :email, with: email
+# fill_in :password, with: password
+# click_button 'Sign in'
+# end
