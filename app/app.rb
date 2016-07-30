29f4be91da9c8be4f9efc0c40526184f710d01dd
Thady Condon <thadycondon@hotmail.com>
2016-07-30 11:33:33 +0100

    Added feature test for error message when password entered incorrectly. Test now passing.

--- app/app.rb
+++ app/app.rb
@@ -2,26 +2,34 @@ ENV["RACK_ENV"] ||= "development"
 
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   get '/' do
     'Hello Chitter!'
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(user_name: params[:user_name], email: params[:email],
+    @user = User.create(user_name: params[:user_name], email: params[:email],
     name: params[:name], password_confirmation: params[:password_confirmation],
     password: params[:password])
-    session[:user_id] = user.id
-    redirect to('/users/new')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/users/new')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
