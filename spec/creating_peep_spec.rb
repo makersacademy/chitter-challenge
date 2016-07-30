64d2109feaaf13f1fa77a784e35a7fd59efe41dc
Thady Condon <thadycondon@hotmail.com>
2016-07-30 14:35:34 +0100

    Created feature test that checks makes user login to leave a peep. Test is passing

--- spec/creating_peep_spec.rb
+++ spec/creating_peep_spec.rb
@@ -1,6 +1,8 @@
 feature 'Creating peeps' do
 
   scenario 'I can create a new peep' do
+    sign_up
+    sign_in
     visit '/peeps/new'
     fill_in 'peep',   with: 'This is an exciting test'
     click_button 'Create peep'
@@ -8,5 +10,10 @@ feature 'Creating peeps' do
     within 'ul#peeps' do
       expect(page).to have_content('This is an exciting test')
     end
+  end
+
+  scenario "I can't create a peep without being logged in" do
+    visit '/peeps/new'
+    expect(page).to have_content 'Hi, please sign up or login to leave a peep'
   end
 end
