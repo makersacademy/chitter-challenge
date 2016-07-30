a1a9aba41e5f0f46cbd6a702a58687041ded72ec
Richard Bates <richo225@gmail.com>
2016-07-30 19:35:43 +0100

    Encrypt passwords. Add required fields for HTML form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
+require 'bcrypt'
+
 class User
   include DataMapper::Resource
 
-  property :id,        Serial
-  property :email,     String
-  property :password,  String
-  property :name,      String
-  property :username,  String
+  property :id,               Serial
+  property :email,            String
+  property :password_digest,  String, length: 60
+  property :name,             String
+  property :username,         String
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
 end
