1188399e2912e7502b85c3a3fd92633365555fd1
Nomi Vos <nvoswork@gmail.com>
2016-07-30 12:02:35 -0500

    sets up next test which is failing.  Changes made to datamapper_setup to fix Travis-CI

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,4 +4,9 @@ feature 'Sign up' do
     sign_up
     expect(page).to have_content 'Welcome, nomi811'
   end
+  scenario 'User cannot sign up if the confirmed password does not match' do
+    expect{ sign_up(confirm_password: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq '/users/new'
+    expect(page).to have_content 'Password does not match'
+  end
 end
