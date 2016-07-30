278b31fa6fb55dbacf4efac983419d8e01d232f0
Sophie Gill <sophie-gill@hotmail.com>
2016-07-30 11:58:49 +0100

    adds password validation check

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,11 @@ require 'bcrypt'
 class User
   include DataMapper::Resource
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  validates_confirmation_of :password
+
   property  :id, Serial
   property  :name, String
   property  :email, String
@@ -10,6 +15,7 @@ class User
   property  :password_digest, String, length: 60
 
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,13 +1,18 @@
 
 feature 'User sign up' do
 
-  def sign_up
+  def sign_up(name: "John Smith",
+              email: "john.smith@example.com",
+              username: "jsmith",
+              password: '12345',
+              password_confirmation: '12345')
     visit '/users/new'
-    fill_in :name, with: "John Smith"
-    fill_in :email, with: "john.smith@example.com"
-    fill_in :username, with: "jsmith"
-    fill_in :password, with: "12345"
-    click_button('Sign up')
+    fill_in :name, with: name
+    fill_in :email, with: email
+    fill_in :username, with: username
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
   end
 
   scenario 'I can sign up as a new user' do
@@ -15,5 +20,10 @@ feature 'User sign up' do
     expect(page).to(have_content("Hello, jsmith"))
     expect(User.first.email).to(eq("john.smith@example.com"))
   end
+
+  scenario 'requires a matching password confirmation' do
+    expect { sign_up(password_confirmation: "wrong") }.not_to(change(User, :count))
+  end
+
 
 end
