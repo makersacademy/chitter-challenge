1489ef32763ac00b711ef57d6a5de204e8f3d9e5
Jonny Moore <jonnymoore121212@gmail.com>
2016-07-30 19:35:27 +0100

    If user sings up with mismatching passwords, email remains on form

--- app/app.rb
+++ app/app.rb
@@ -10,17 +10,18 @@ class Chitter < Sinatra::Base
   register Sinatra::Flash
 
   get '/signup' do
+    @user = User.new
     erb :'signup'
   end
 
   post '/signup' do
     # We cannot directly access the password attribute, so we use bcrypt
     # and instead deal with the password_digest:
-    user = User.create(email: params[:email],
+    @user = User.create(email: params[:email],
                       password: params[:password],
                       password_confirmation: params[:password_confirmation])
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect '/peeps'
     else
       flash.now[:notice] = "Sorry, password and password confirmation do not match"
