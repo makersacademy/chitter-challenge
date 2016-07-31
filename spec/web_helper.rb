d3f5b04e03e1df35e8fc19be5c0600f396d2449b
albiebabie <albiebabie@gmail.com>
2016-07-31 14:10:18 +0100

    sign up - test passing - now with name field

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,6 +1,7 @@
 def sign_up
   visit('/user/sign_up')
   expect(page.status_code).to eq 200
+  fill_in(:name, with: 'name')
   fill_in(:username, with: 'user')
   fill_in(:email, with: 'email@example.com')
   fill_in(:password, with: 'password')
