f3ff4b6a9e8ccdc932af2258745fefa4d8e648cf
Jason Luong <jasonluong@Jasons-MacBook-Pro.local>
2016-07-31 21:26:37 +0100

    added posting peeps

--- app/app.rb
+++ app/app.rb
@@ -35,7 +35,6 @@ class Chitter < Sinatra::Base
   end
 
   get '/users' do
-    # erb :'users/new'
     redirect '/users/new'
   end
 
@@ -82,6 +81,16 @@ class Chitter < Sinatra::Base
   get '/messages' do
     @messages = Message.all
     erb :'messages/index'
+  end
+
+  post '/messages/add' do
+    time = Time.new.strftime("%d/%m/%Y %R")
+    message = Message.create(message: params[:message],
+                             created_on: time)
+    current_user.messages << message
+    current_user.save
+    # message.save
+    redirect '/messages'
   end
 
   get '/messages/new' do
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,13 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'dm-timestamps'
 
 class Message
   include DataMapper::Resource
 
-  property :id,        Serial
-  property :message,   String, length: 140, required: true
-  property :time,      Time
+  property :id,         Serial
+  property :message,    String, length: 140, required: true
+  property :created_on, DateTime
 
   belongs_to :user
 end
