6efcdd0a1fdbce7e667aae7808f77f012979afe6
Bart <kluczynskib@gmail.com>
2016-07-31 17:01:18 +0200

    Chitter challenge
    Implement the graphic layer of the project

--- app/app.rb
+++ app/app.rb
@@ -86,20 +86,6 @@ class Chitter < Sinatra::Base
   end
 
 
-
-  #
-  # post '/links' do
-  #   link = Link.new(url: params[:url],     # 1. Create a link
-  #                 title: params[:title])
-  #   tag  = Tag.first_or_create(name: params[:tags])  # 2. Create a tag for the link
-  #   link.tags << tag                       # 3. Adding the tag to the link's DataMapper collection.
-  #   link.save                              # 4. Saving the link.
-  #   redirect to('/links')
-  # end
-
-
-
-
   helpers do
  def current_user
    @current_user ||= User.get(session[:user_id])
