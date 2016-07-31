71be5ece79d2d1b6387e835980c36b853e774d52
Sophie Gill <sophie-gill@hotmail.com>
2016-07-31 17:11:28 +0100

    adds restriction of cheep creation to signed in users

--- spec/features/create_cheep_spec.rb
+++ spec/features/create_cheep_spec.rb
@@ -1,5 +1,12 @@
 feature 'Create cheep' do
 
+  scenario 'when not signed in' do
+    visit('/cheeps/new')
+    expect(page).to(have_content("You must be signed in to cheep"))
+    click_button("Back to homepage")
+    expect(current_path).to(eq('/cheeps'))
+  end
+
   #scenario 'I can create a new cheep' do
   #  visit('/cheeps/new')
   #  fill_in(:content, with: 'This is a cheep')
@@ -7,5 +14,5 @@ feature 'Create cheep' do
   #  expect(current_path).to(eq('/cheeps'))
   #  expect(page).to(have_content('This is a cheep'))
   #end
-  
+
 end
