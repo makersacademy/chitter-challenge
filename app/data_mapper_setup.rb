f9170553f5d0ad0ddb39035409a36e2708970418
liskowsky <karollebiedzinski@gmail.com>
2016-07-31 13:59:31 +0200

    Travis CI test

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
-require_relative 'models/user'
-require_relative 'models/peep'
+require_relative '../app/models/user'
+require_relative '../app/models/peep'
 require 'sinatra/flash'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
