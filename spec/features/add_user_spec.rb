c97d1d7108ef8ba41c1261a7a0a4940872213804
nikklein <objcoding@gmail.com>
2016-07-31 15:58:49 +0100

    Update tests

--- spec/features/add_user_spec.rb
+++ spec/features/add_user_spec.rb
@@ -21,10 +21,14 @@ feature 'User sign up' do
 
   scenario "I can't sign up without an email address" do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario "I can't sign up with an invalid email address" do
    expect { sign_up(email: "invalid@email") }.not_to change(User, :count)
+   expect(current_path).to eq('/users')
+  expect(page).to have_content('Email has an invalid format')
   end
 
   scenario 'it raises an error if password does not match' do
@@ -32,6 +36,7 @@ feature 'User sign up' do
     expect(current_path).to eq('/users')
     expect(page).to have_content 'Password does not match the confirmation'
   end
+
   scenario 'I cannot sign up with an existing email' do
     sign_up
     expect { sign_up }.to_not change(User, :count)
