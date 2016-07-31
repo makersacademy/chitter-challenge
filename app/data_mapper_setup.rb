943a22d876d9ca09cb0361f8bdedc7630f30f014
liskowsky <karollebiedzinski@gmail.com>
2016-07-31 14:16:50 +0200

    Travis CI test 2

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,6 +4,6 @@ require_relative '../app/models/user'
 require_relative '../app/models/peep'
 require 'sinatra/flash'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
