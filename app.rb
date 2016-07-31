74aaaf2c06fcdafe26bed77b000a7f9f38ab943a
Georgia Mills <georgee_123@hotmail.com>
2016-07-31 14:13:49 +0100

    user password flash error

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require 'bcrypt'
 require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
+register Sinatra::Flash
 enable :sessions
 set :session_secret, 'super secret'
 
@@ -13,17 +15,23 @@ set :session_secret, 'super secret'
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(name: params[:name],
+    @user = User.create(name: params[:name],
                 username: params[:username],
                 email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
+    if @user.save
+    session[:user_id] = @user.id
     redirect to '/posts'
+    else
+      flash.now[:notice] = "Password's don't match"
+      erb :'posts/new'
+    end
   end
 
   get '/posts' do
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -9,6 +9,10 @@ feature 'sign up' do
     expect{ sign_up }.to change(User, :count).by 1
     expect(page).to have_content 'Hello there, grmillsy'
     expect(User.first.email).to eq 'g@hotmail.com'
+  end
 
+  scenario 'passwords dont match' do
+    expect {sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(page).to have_content "Password's don't match"
   end
 end
