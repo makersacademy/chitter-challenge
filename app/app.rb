123115047931e440df48ad78f9460092ad6370c7
sus111 <sgraham535@gmail.com>
2016-07-31 18:31:19 +0100

    basic sign up

--- app/app.rb
+++ app/app.rb
@@ -1,43 +1,67 @@
 ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require 'sinatra/flash'
 require_relative 'models/user'
-require_relative 'data_mapper_setup'
+require_relative 'models/data_mapper_setup'
 
 class Chitter < Sinatra::Base
-use Rack::MethodOverride
-enable :sessions
-set :session_secret, 'super secret'
-register Sinatra::Flash
+  use Rack::MethodOverride
+  register Sinatra::Flash
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
-    erb :'peeps/home'
+  erb :home
   end
 
   get '/users/new' do
     @user = User.new
-    erb :'users/sign_up'
+    erb :'users/new'
   end
 
   post '/users' do
-  @user = User.create(name: params[:name],
-                    email: params[:email],
-                    password: params[:password],
-                    password_confirmation: params[:password_confirmation])
+    @user = User.new(email: params[:email],
+                password: params[:password],
+                password_confirmation: params[:password_confirmation],
+                name: params[:name],
+                username: params[:username])
     if @user.save
-    session[:user_id] = @user.id
-    redirect '/'
-  else
-    flash.now[:bad] = "Your passwords don\'t match"
-    erb :'/users/sign_up'
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      flash.now[:bad] = "Your passwords don\'t match"
+      #flash[:errors] = @user.errors.full_messages
+      erb :'users/new'
+    end
   end
-end
 
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
     end
   end
+#
+#   get '/sessions/new' do
+#   erb :'sessions/new'
+# end
+#
+# post '/sessions' do
+#   @user = User.authenticate(params[:email], params[:password])
+#   if @user
+#     session[:user_id] = @user.id
+#     redirect '/'
+#   else
+#     flash.now[:errors] = ['The email or password is incorrect']
+#     erb :'sessions/new'
+#   end
+# end
+#
+#   delete '/sessions' do
+#    session[:user_id] = nil
+#    flash.keep[:notice] = "Bye!"
+#    redirect '/'
+#  end
 
   run! if app_file == $0
 end
--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -1,6 +1,5 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
-require './app/models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
