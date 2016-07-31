ce48587ed6e2297336b3ad8f175ea1f77d0ca222
JulieLev <jkgee1@yahoo.co.uk>
2016-07-31 23:02:58 +0100

    Actually sends a peep

--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -2,14 +2,12 @@
 class Chitter < Sinatra::Base
 
   get '/peeps' do
-    @peep = Peep.new
     @peeps = Peep.all
     erb :'peeps/index'
   end
 
   post '/peeps/new' do
-    @peep = Peep.new(message: params[:message])
-    @current_user.peeps << @peep
+    current_user.peeps.create(message: params[:message])
     redirect to('/peeps')
   end
 
