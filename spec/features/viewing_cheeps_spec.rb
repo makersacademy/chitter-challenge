3d060065a50e6d7a0f78bd702c85562cbbe41c98
Sophie Gill <sophie-gill@hotmail.com>
2016-08-01 09:59:42 +0100

    corrects failing rspec tests

--- spec/features/viewing_cheeps_spec.rb
+++ spec/features/viewing_cheeps_spec.rb
@@ -1,10 +1,10 @@
-=begin
+begin
 
 feature 'Viewing cheeps' do
 
   scenario 'I can view all peeps' do
     sign_up
-    Cheep.create(content: "This is a cheep", user_id: 1)
+    Cheep.create(content: "This is a cheep", user_id: User.first.id)
     click_button('Sign out')
     visit('/cheeps')
     expect(page.status_code).to(eq(200))
@@ -15,13 +15,22 @@ feature 'Viewing cheeps' do
 
   scenario 'cheeps display posted time' do
     sign_up
-    cheep = Cheep.create(content: "This is a cheep", user_id: 1)
+    cheep = Cheep.create(content: "This is a cheep", user_id: User.first.id)
     visit('/cheeps')
     within 'ul#cheeps' do
       expect(page).to(have_content("#{cheep.time}"))
     end
   end
 
+  scenario 'cheeps display posted time' do
+    sign_up
+    cheep = Cheep.create(content: "This is a cheep", user_id: User.first.id)
+    visit('/cheeps')
+    within 'ul#cheeps' do
+      expect(page).to(have_content("#{cheep.user}"))
+    end
+  end
+
 
 end
-=end
+end
