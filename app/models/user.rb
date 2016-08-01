cb0335ce7056d35c88977e84f6b6005dd44c5cce
Adam <adamjbrowner@gmail.com>
2016-08-01 08:03:20 +0100

    Edited for heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,6 +29,6 @@ class User
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
