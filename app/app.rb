1134811f163a69c1a9277899fdbc587e268eb039
Thady Condon <thadycondon@hotmail.com>
2016-08-01 06:09:26 +0100

    Updated the controller file

--- app/app.rb
+++ app/app.rb
@@ -5,7 +5,7 @@ require 'sinatra/base'
 require 'sinatra/flash'
 require 'sinatra/partial'
 
-class BookmarkManager < Sinatra::Base
+class Chitter < Sinatra::Base
 
 require_relative './lib/send_email'
 require_relative 'data_mapper_setup'
@@ -14,5 +14,5 @@ require_relative 'controllers/peeps'
 require_relative 'controllers/users'
 require_relative 'controllers/sessions'
 
-  run! if app_file == $0
+run! if app_file == $0
 end
