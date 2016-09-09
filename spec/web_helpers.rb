c818cfb0a648f4c711a95bdec453d1f3b286803e
TomStuart92 <Thomascstuart@gmail.com>
2016-09-09 09:25:34 +0100

    Add Unique Email Constraint

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -6,3 +6,12 @@ def sign_up(options = {})
   fill_in :password_confirmation, with: options.fetch(:password_confirmation, 'foobar')
   click_button('Sign Up')
 end
+
+def sign_in(options = {})
+  fill_in :email, with: options.fetch(:email, 'test@example.com')
+  fill_in :name, with: options.fetch(:name, 'Chit Ter')
+  fill_in :username, with: options.fetch(:username, 'sir_chitter')
+  fill_in :password, with: options.fetch(:password, 'foobar')
+  fill_in :password_confirmation, with: options.fetch(:password_confirmation, 'foobar')
+  click_button('Sign Up')
+end
