0341cbe02762b25d0d9de6d6a01be7610f96a7bc
supasuma <supasuma@gmail.com>
2016-09-10 21:40:10 +0100

    travis bloody shit again hopefully for the final final time

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,7 +7,7 @@ require_relative 'models/user'
 # later you'll need to require the models that are using DM to connect to your DB
 ENV['RACK_ENV'] ||= "development"
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/Chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.finalize
 DataMapper.auto_upgrade!
