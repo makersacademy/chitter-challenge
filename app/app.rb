93243da5bbbc94f6db63a9298dbf60e390a2d9fe
Tam Borine <tamborine@Tams-Air.home>
2016-09-10 22:38:25 +0100

    first green test, dumbed down registration

---
+++ app/app.rb
@@ -0,0 +1,20 @@
+require 'sinatra/base'
+
+class Chitter < Sinatra::Base
+  register
+
+  get '/' do
+    erb :'/users/index'
+  end
+
+  get '/users/new' do
+    erb :'/users/new'
+  end
+
+  post '/users' do
+    #@user = User.new(name: params(:name), handle: params(:handle), email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+    erb :'/users/welcome'
+  end
+
+
+end
