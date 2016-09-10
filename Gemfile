2894e2674748dad9322459be526cc2f6fd10a386
Alfie <alfie_ab@hotmail.co.uk>
2016-09-10 19:04:15 +0100

    initial set up without rake done, rspec not working but think that's because no tables have been created

--- Gemfile
+++ Gemfile
@@ -1,10 +1,18 @@
 source 'https://rubygems.org'
 ruby '2.2.3'
 
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'bcrypt'
+gem 'sinatra'
+gem 'sinatra-flash'
+gem 'pg'
+
 group :test do
+  gem 'capybara'
   gem 'rspec'
   gem 'cucumber'
   gem 'rake'
   gem 'coveralls', require: false
+  gem 'database_cleaner'
 end
-
---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,9 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+# later you'll need to require the models that are using DM to connect to your DB
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.finalize
+DataMapper.auto_upgrade!
