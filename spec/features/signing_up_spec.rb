ef86e1805bd4c25a14f4232bd5f5997e55473373
lsewilson <laurawilson@hotmail.co.uk>
2016-09-10 17:15:00 +0100

    sign up credentials persist after failed attempt

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -11,24 +11,29 @@ feature 'signing up for Chitter' do
     expect{sign_up(name: nil)}.to_not change(User, :count)
     expect{sign_up(username: nil)}.to_not change(User, :count)
     expect{sign_up(email: nil)}.to_not change(User, :count)
-
   end
 
   scenario 'user must confirm with identical password to sign up' do
     expect{sign_up(password_confirmation: 'blitzen')}.to_not change(User, :count)
     expect(page).to have_content('Password does not match the confirmation')
+    expect(page).to have_field('name', with: 'Santa')
+    expect(page).to have_field('username', with: 'santaclaus')
+    expect(page).to have_field('email', with: 'santa@northpole.com')
   end
 
   scenario 'user cannot sign up with already registered email' do
     sign_up
     expect{sign_up(username: 'FatherChristmas')}.to_not change(User, :count)
     expect(page).to have_content('Email is already taken')
+    expect(page).to have_field('username', with: 'FatherChristmas')
   end
 
   scenario 'user cannot sign up with already taken username' do
     sign_up
     expect{sign_up(email: 'father@christmas.com')}.to_not change(User, :count)
     expect(page).to have_content('Username is already taken')
+    expect(page).to have_field('email', with: 'father@christmas.com')
   end
+
 
 end
