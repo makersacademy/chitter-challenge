61e2ba3a17f6c5fb4b7d29f6aa062260085c886b
johnnydee8 <j.dworzynski@me.com>
2016-09-12 00:10:22 +0100

    added partials

--- app/server.rb
+++ app/server.rb
@@ -3,10 +3,13 @@ ENV['RACK_ENV'] ||= 'development'
 class Chitter < Sinatra::Base
 
   register Sinatra::Flash
-
+  register Sinatra::Partial
+  
   enable :sessions
+  enable :partial_underscores
   set :session_secret, 'super secret'
   set :public_folder, Proc.new { File.join(root, 'static') }
+  set :partial_template_engine, :erb
 
   helpers do
     def current_user
