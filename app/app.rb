d19335e0fd165bef20eca6e0006022aa4b145c65
Stéphan Gaud <stephgoeseast@gmail.com>
2016-09-11 16:35:51 +0100

    latest code

--- app/app.rb
+++ app/app.rb
@@ -4,9 +4,19 @@ require_relative '../data_mapper_setup'
 ENV["RACK_ENV"] ||= "development"
 
 class Chitter < Sinatra::Base
+enable :sessions
+set :session_secret, 'super secret'
+
+
   get '/' do
     'Hello Chitter!'
   end
+
+
+      def current_user
+        @current_user ||= User.get(session[:user_id])
+      end
+
 
   get '/sign_up' do
     erb :'user/sign_up'
@@ -20,7 +30,6 @@ class Chitter < Sinatra::Base
     get '/peeps' do
     erb :'peeps/index'
   end
-
 
   # start the server if ruby file executed directly
   run! if app_file == $0
