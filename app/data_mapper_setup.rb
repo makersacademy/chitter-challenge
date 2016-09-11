471139762505ce63651a4eedd65ae85689582277
johnnydee8 <j.dworzynski@me.com>
2016-09-12 00:48:45 +0100

    minor changes data mapper setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -7,3 +7,4 @@ require_relative 'models/peeps'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.finalize
+DataMapper.auto_upgrade!
