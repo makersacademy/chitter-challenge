a594b92d40ec7d36cfcc5235b8518ab595c4ddc2
Matthew Perkins <mfperkins@gmail.com>
2016-09-11 10:57:19 +0100

    added log out

--- app/app.rb
+++ app/app.rb
@@ -49,6 +49,11 @@ class Chitter < Sinatra::Base
     end
   end
 
+  post '/user/log_out' do
+    session[:user_name] = nil
+    redirect '/feed'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
