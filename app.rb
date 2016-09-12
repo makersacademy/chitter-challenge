a521ea43e7643d9ac44fa5ff7ccc1458d94bdd4f
mrmurtz <murtaza.s.abidi@gmail.com>
2016-09-12 11:24:37 +0100

    Improved displayed time on peeps

--- app.rb
+++ app.rb
@@ -77,6 +77,7 @@ class ChitterApp < Sinatra::Base
       @current_user ||= User.get(session[:user_id])
     end
   end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
