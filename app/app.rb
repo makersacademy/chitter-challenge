bbde078e27a763ddc477995487417ecde1f8d531
Joseph <joseph.jr@gmail.com>
2016-09-12 04:50:50 +0100

    add peep class and user class to model

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,17 @@ require 'sinatra/flash'
 require 'bcrypt'
 
 class Chitter < Sinatra::Base
-  enable :sessions
-  set :session_secret; 'super_secret'
-  register Sinatra::Flash
+  # enable :sessions
+  # set :session_secret; 'super_secret'
+  # register Sinatra::Flash
+
+  get '/home/new' do
+    erb :'new_peep'
+  end
+
+  post '/home' do
+    peep = Peep.new()
+    redirect 'home'
+  end
+
+end
