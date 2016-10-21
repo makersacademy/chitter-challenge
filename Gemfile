d4da1e09f3bbfaffbd332065ae5f13dc0bf42592
James <bakerje@mac.com>
2016-10-21 21:11:13 +0100

    Added rspec-sinatra init

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'bcrypt'
+gem 'rspec-sinatra'
 
 group :test do
   gem 'capybara'
--- app/app.rb
+++ app/app.rb
@@ -3,9 +3,11 @@ require_relative 'data_mapper_setup'
 
 ENV["RACK_ENV"] ||= 'development'
 
-class ChitterChallenge
+class ChitterChallenge < Sinatra::Base
+  get '/' do
+    'Hello ChitterChallenge!'
+  end
 
-
-
+  # start the server if ruby file executed directly
   run! if app_file == $0
 end
