251c980933a19149fd1dfe33ed534bb01a606289
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-22 12:46:43 +0100

    Added sessions and welcome page now welcomes the user who has signed up

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ require './models/data_mapper_setup.rb'
 
 class Chitter < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
   get '/' do
     redirect 'users/new'
   end
@@ -13,10 +22,11 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    User.create(user_name: params[:user_name],
+    user = User.create(user_name: params[:user_name],
     name: params[:name],
     email: params[:email],
     password: params[:password])
+    session[:user_id] = user.id
     redirect to('/peeps')
   end
 
@@ -26,6 +36,6 @@ class Chitter < Sinatra::Base
 
 
 
-# start the server if ruby file executed directly
-run! if app_file == $0
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
