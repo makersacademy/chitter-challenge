a12bba022f45a02c4eaa7d4f323cb1d2e6b6426c
hanrattyjen <hanratty.jen@gmail.com>
2016-10-22 22:26:37 +0100

    Added flash message for password/password confirmations

--- app.rb
+++ app.rb
@@ -1,9 +1,10 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative './app/models/user'
 
 class Chitter < Sinatra::Base
 
-
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
 
@@ -22,21 +23,22 @@ class Chitter < Sinatra::Base
    end
 
   get '/users/new' do
-    # @user = User.new
+    @user = User.new
     erb(:'users/new')
   end
 
   post '/users' do
-    user = User.new(username: params[:username],
+    @user = User.new(username: params[:username],
                       name: params[:name],
                       email: params[:email],
                       password: params[:password],
                       password_confirmation: params[:password_confirmation])
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect to('/home')
     else
-      redirect to('users/new')
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb(:'users/new')
     end
   end
 
--- spec/user_sign_up_spec.rb
+++ spec/user_sign_up_spec.rb
@@ -3,15 +3,20 @@ require 'spec_helper'
 feature 'I want to sign up for Chitter' do
   scenario 'Allow user to create a Chitter account' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page.current_path).to eq '/home'
+    expect(current_path).to eq '/home'
     expect(page).to have_content("Welcome tansaku")
     expect(User.first.email).to eq("sam@makersacademy.com")
   end
 
-  scenario 'If user password and password_confirmation do not match, no new Chitter account is created' do
+  scenario 'If user password and password_confirmation do not match, a new Chitter account is not created' do
     expect { sign_up(password_confirmation: 'secret') }.not_to change(User, :count)
-    expect(page.current_path).to eq '/users/new'
-    # expect(page).to have_content("Passwords to do not match")
+    expect(current_path).to eq '/users'
+    expect(page).to have_content("Password and confirmation password do not match")
+  end
+
+  scenario 'If user has already registered, a new Chitter account is not created' do
+    expect { sign_up }.not_to change(User, :count)
+
   end
 
 
