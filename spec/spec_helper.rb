54e592ee9334f9dc7c66735a53a520ce57c767f8
Laura Weston <laura.weston@outlook.com>
2016-10-22 17:29:00 +0100

    Configure capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,9 +7,13 @@ SimpleCov.formatters = [
 ]
 Coveralls.wear!
 
+require File.join(File.dirname(__FILE__), "..", "app", "app")
 require "capybara"
 require "capybara/rspec"
 require "dm-rspec"
+
+Capybara.app = ChitterApp
+
 
 RSpec.configure do |config|
   config.include(DataMapper::Matchers)
