f4b2511b421d510c771dcc2559a184348f50a518
Darrenf <darrenf54321@gmail.com>
2016-10-22 10:04:29 +0100

    database update

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -5,7 +5,10 @@ require_relative 'tag'
 require_relative 'link'
 require_relative 'user'
 
+if ENV["RACK_ENV"]  == "development"
+  ENV["DATABASE_URL"] = "postgres:///chitter_development"
+end
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres:///bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'])
 DataMapper.finalize
 DataMapper.auto_upgrade!
