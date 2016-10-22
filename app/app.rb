b5b09efb99ebdcce0ebc59eb0a88fcde33272904
James <bakerje@mac.com>
2016-10-22 17:36:43 +0100

    Created initial index page to display peeps

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,17 @@
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
+require_relative 'models/peep'
 
 ENV["RACK_ENV"] ||= 'development'
 
 class ChitterChallenge < Sinatra::Base
   get '/' do
     'Hello ChitterChallenge!'
+  end
+
+  get '/peeps' do
+    @peeps = Peep.all
+    erb :'peeps/index'
   end
 
   # start the server if ruby file executed directly
