fd18beb742fa1e483ce52814c8fe6233b172283f
Rosie <r.gollancz@gmail.com>
2016-10-22 13:20:08 +0100

    Sets up spec helper and directory structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,40 @@ SimpleCov.formatters = [
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..','app','app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require './app/models/link'
+require './app/app'
+require 'database_cleaner'
+require 'dm-transactions'
+require './spec/features/web_helper'
+
+Capybara.app = Chitter
+
+RSpec.configure do |config|
+  config.include Capybara::DSL
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+end
