3f6fd0ac541a04fc895cbf10896f6ccc0d6734d1
andy jenkins <andygjenkins@gmail.com>
2016-10-22 17:30:31 +0100

    Add further sign up test

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -3,7 +3,35 @@ require 'spec_helper'
 feature 'User sign up' do
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, dave')
+    expect(page).to have_content('Welcome, Dave')
     expect(User.first.email).to eq('dave@example.com')
   end
+
+
+  scenario 'with a password that does not match' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password does not match the confirmation'
+  end
+
+  scenario "I can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email must not be blank')
+  end
+
+  scenario "can't sign up without valid email" do
+    expect { sign_up(email: "no-way@email")}.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content('Email has an invalid format')
+  end
+
+  scenario "can't use an existing user's email" do
+    sign_up
+    expect {sign_up}.not_to change(User, :count)
+    expect(page).to have_content 'Email is already taken'
+  end
+
+
+
 end
