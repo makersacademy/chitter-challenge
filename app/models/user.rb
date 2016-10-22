07a9db62ba7d1e7417957df3020f818c801a309e
Laura Weston <laura.weston@outlook.com>
2016-10-22 14:10:49 +0100

    Extract datamapper setup to helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,3 @@
-require "data_mapper"
-require "dm-postgres-adapter"
-
 class User
   include DataMapper::Resource
 
@@ -10,7 +7,3 @@ class User
   property :email, String
 
 end
-
-DataMapper.setup(:default, "postgres://localhost/chitter_test")
-DataMapper.finalize
-DataMapper.auto_upgrade!
