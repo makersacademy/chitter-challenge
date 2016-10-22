6b91c30bf71c56679dffab9e3babd555a0176843
alexanderwjrussell <awjrussell@live.co.uk>
2016-10-22 23:10:02 +0100

    First user story underway. Failing on creating a user within the database.

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,44 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
+require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
+  use Rack::MethodOverride
+  register Sinatra::Flash
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
   get '/' do
-    'Hello Chitter!'
+    erb :'welcome'
+  end
+
+  get '/create_user' do
+    @user = User.new
+    erb :'create_user'
+  end
+
+  post '/create_user' do
+    @user = User.create(name: params[:name],
+                        username: params[:username],
+                        email: params[:email],
+                        password: params[:password],
+                        password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      erb :'create_user'
+    end
   end
 
   run! if app_file == $0
---
+++ spec/web_helpers.rb
@@ -0,0 +1,14 @@
+def create_user(name: "default",
+                username: "defaultio",
+                email: "default@example.com",
+								password: "password",
+								password_confirmation: "password")
+
+  visit('/create_user')
+  fill_in :name, with: name
+  fill_in :username, with: username
+	fill_in :email, with: email
+	fill_in :password, with: password
+	fill_in :password_confirmation, with: password_confirmation
+	click_button('Create User')
+end
