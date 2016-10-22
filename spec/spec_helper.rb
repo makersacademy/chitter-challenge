ca28f40a55e2250257309f5002c4426415d67e0c
glynester <glynester@hotmail.com>
2016-10-22 17:53:21 +0100

    Database cleaner added.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require 'simplecov'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require 'database_cleaner'
 require './app/app.rb'
 require './app/models/peep'
 
@@ -19,6 +20,17 @@ Capybara.app = ChitterChallenge
 RSpec.configure do |config|
   config.include Capybara::DSL
 
+  config.before(:suite) do # Everything in this block runs once before all the tests run
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+  config.before(:each) do # Everything in this block runs once before each individual test
+    DatabaseCleaner.start
+  end
+  config.after(:each) do # Everything in this block runs once after each individual test
+    DatabaseCleaner.clean
+  end
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -27,9 +39,9 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  # if config.files_to_run.one?
-  #   config.default_formatter = 'doc'
-  # end
+  if config.files_to_run.one?
+    config.default_formatter = 'doc'
+  end
 
   config.profile_examples = 10
 
