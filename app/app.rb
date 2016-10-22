baabc70d50a50b9513d255acea1d9151ba86f617
glynester <glynester@hotmail.com>
2016-10-22 17:03:21 +0100

    Added css. Test for new peep passes.

--- app/app.rb
+++ app/app.rb
@@ -18,5 +18,11 @@ get '/peeps/new' do
   erb :'peeps/new'
 end
 
+post '/peeps' do
+  #Name will default to logged_in user name. It won't be editable.
+  Peep.create(name: params['name'], message: params['message'] )
+  redirect '/peeps'
+end
+
   #run! if app_file == $0
 end
