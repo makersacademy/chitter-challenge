58a2f1bc8e29e104a03a2e6bffe0b97e77009b49
Laura Weston <laura.weston@outlook.com>
2016-10-23 12:49:30 +0100

    Posts display creation date and time

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post
   include DataMapper::Resource
 
   property :id, Serial
-  property :message, String, :required => true
+  property :message, Text, :length => 100, :required => true
   property :created_at, DateTime
 
   belongs_to :user
