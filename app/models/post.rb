118aba970edb553673f84c2c300e0c81cdfc78a4
Laura Weston <laura.weston@outlook.com>
2016-10-23 11:41:41 +0100

    Make user and post properties required

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post
   include DataMapper::Resource
 
   property :id, Serial
-  property :message, String
+  property :message, String, :required => true
   property :created_at, DateTime
 
   belongs_to :user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :name, String
-  property :user_name, String
-  property :email, String
-
+  property :name, String, :required => true
+  property :user_name, String, :required => true
+  property :email, String, :required => true
   has n, :posts
 
 end
