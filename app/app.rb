425813fd45cabcd4aed155fa24ded240ea18f0b0
souljuse <oppo.irene@gmail.com>
2016-10-23 22:10:05 +0200

    refactored

--- app/app.rb
+++ app/app.rb
@@ -1,65 +1,14 @@
-ENV["RACK_ENV"] ||= "development"
+ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-require_relative 'data_mapper_setup'
 require 'sinatra/flash'
+require 'sinatra/partial'
 
-class Chitter < Sinatra::Base
-  enable :sessions
-  set :session_secret, 'super secret'
-  register Sinatra::Flash
-  use Rack::MethodOverride
+require_relative 'data_mapper_setup'
 
-  get '/' do
-    @users = User.all
-    erb :index
-  end
-
-  post '/sessions' do
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect to('/')
-    else
-      flash.now[:errors] = ['The email or password is incorrect']
-      erb :index
-    end
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    flash.keep[:notice] = 'goodbye!'
-    redirect to '/'
-  end
-
-  get '/users/new' do
-    @user = User.new
-    erb :'users/new'
-  end
-
-  post '/users' do
-    @user = User.new
-    user = User.create(email: params[:email],
-                password: params[:password],
-                password_confirmation: params[:password_confirmation])
-    if user.save # #save returns true/false depending on whether the model is successfully saved to the database.
-      session[:user_id] = user.id
-      redirect('/')
-    else
-      flash.now[:errors] = ['The email or password is incorrect']
-      erb :'users/new'
-    end
-  end
-
-  get '/goodbye' do
-    erb :goodbye
-  end
-
-  helpers do
-   def current_user
-     @current_user ||= User.get(session[:user_id])
-   end
-  end
+require_relative 'server'
+require_relative 'controllers/sessions'
+require_relative 'controllers/users'
 
   # post '/links' do
   #   link = Link.new(title: params[:title], url: params[:url])
@@ -86,5 +35,3 @@ class Chitter < Sinatra::Base
   # end
 
   # start the server if ruby file executed directly
-  run! if app_file == $0
-end
