57a83f90ef40116a149fd065805ede3093a7138b
Rory Aitchison <rory.aitchison@gmail.com>
2016-10-23 18:33:28 +0100

    addadds a few tests, controller, file structure, index and sign up views

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -2,11 +2,17 @@ ENV['RACK_ENV'] ||= "development"
 
 require 'sinatra/base'
 
-class Chitter < Sinatra::base
+class Chitter < Sinatra::Base
 
   get '/' do
-
+    erb :index
   end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  
 
   # start the server if ruby file executed directly
   run! if app_file == $0
