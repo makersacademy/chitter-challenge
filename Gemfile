4bf4a42acec58937f399f8eb9a33a0f6a2fbe844
Dionysis <Dionysis.k22@gmail.com>
2016-10-23 19:14:40 +0100

    rspec test for time of peep, timecop gem

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'rake'
 gem 'bcrypt'
 gem 'dm-validations'
 gem 'sinatra-flash'
+gem 'timecop'
 
 group :test do
   gem 'rake'
--- spec/features/new_peep_spec.rb
+++ spec/features/new_peep_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 require '././app/models/peep'
+require "timecop"
 
 RSpec.feature 'Peep', type: :feature do
   scenario "posting a peep on chitter" do
@@ -11,4 +12,18 @@ RSpec.feature 'Peep', type: :feature do
     expect(current_path).to eq '/chitter'
     expect(page).to have_content('this is a peep')
   end
+
+  scenario "can view timestamp" do
+    sign_up
+    sign_in(email: 'Dio@test.com', password: '123456')
+    visit '/peeps/new'
+    fill_in 'peep',   with: 'this is a peep'
+    click_button 'Save'
+    Timecop.freeze do
+      within(".peep .timestamp") do
+        expect(page).to have_content DateTime.now.strftime("%c")
+      end
+    end
+  end
+
 end
