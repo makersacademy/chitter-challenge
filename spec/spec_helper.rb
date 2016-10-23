5502a9b86e587e888d9f911f6a772c8262550e09
Laura Weston <laura.weston@outlook.com>
2016-10-23 13:50:24 +0100

    Add gem database_cleaner and configure for rspec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,10 +11,26 @@ require File.join(File.dirname(__FILE__), "..", "app", "app")
 require "capybara"
 require "capybara/rspec"
 require "dm-rspec"
+require "database_cleaner"
 
 Capybara.app = ChitterApp
 
 
 RSpec.configure do |config|
   config.include(DataMapper::Matchers)
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 end
