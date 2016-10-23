64ef09ef3bf8e6952daf6fa1eb7eab240918e370
hanrattyjen <hanratty.jen@gmail.com>
2016-10-23 21:55:02 +0100

    DataMapper setup production environement for heroku deployment.

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -5,6 +5,6 @@ require_relative './app/models/user'
 require_relative './app/models/peep'
 
 # DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres:///chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
