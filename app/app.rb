066ed08f8f883d83cd0950ad32b4da7636e3c0ce
Tom Nunn <t@tomnunn.uk>
2016-10-23 19:04:03 +0100

    add further funtionality to User Sign Up andadd styling with bootstrap

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,13 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 
 require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
+
+  register Sinatra::Flash
 
   enable :sessions
   set :session_secret, 'super secret'
@@ -14,18 +17,24 @@ class Chitter < Sinatra::Base
   end
 
   get '/users/sign_up' do
+    @user = User.new
     erb :'users/sign_up'
   end
 
   post '/users' do
-    user = User.create(first_name: params[:first_name],
+    @user = User.new(first_name: params[:first_name],
                 last_name: params[:last_name],
                 email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation],
                 user_name: params[:user_name])
-    session[:user_id] = user.id
-    redirect('/')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect('/')
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      erb :'users/sign_up'
+    end
   end
 
   helpers do
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,15 @@ class User
   property :id, Serial
   property :first_name, String
   property :last_name, String
-  property :email, String
+  property :email, String, format: :email_address, required: true, unique: true
   property :password_digest, Text
-  property :user_name, String
+  property :user_name, String, required: true, unique: true
 
   attr_reader :password
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
+  validates_presence_of :email
 
   def password=(password)
     @password = password
