c188cf63e63b4a39d879c005ef4b90bb3ab8898c
andy jenkins <andygjenkins@gmail.com>
2016-10-23 17:04:40 +0100

    Add peeps test - failing

---
+++ spec/features/add_peep_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature 'Making peeps' do
+  let!(:user) do
+    User.create(email: 'dave@example.com',
+                name: 'dave',
+                user_name: 'B1GDAVE',
+                password: '123',
+                password_confirmation: '123')
+  end
+  scenario 'user can add peep from homepage' do
+    sign_in(email: user.email, password: user.password)
+    visit '/peeps/new'
+    expect(page.status_code).to eq(200)
+    fill_in :peep_text, with: "This is my first peep"
+    click_button 'Send Peep'
+    expect(Peep.count).to eq(1)
+    expect(page).to have_content('This is my first peep')
+  end
+end
