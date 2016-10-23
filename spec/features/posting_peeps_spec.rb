0873c83d03b9c9eb9fd400606302a07c475bb96a
Frances Maxwell <fmaxwell@gmail.com>
2016-10-24 00:49:34 +0100

    Added username details to peeps listing

--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -8,4 +8,14 @@ feature 'Posting peeps' do
     expect(current_path).to eq '/'
     expect(page).to have_content('Woke up, ate some breakfast, purred at the cat.')
   end
+  scenario 'I can see the time a peep was posted' do
+    sign_up
+    log_in
+    visit '/peeps/new'
+    fill_in('peep_text', with: 'Posting this message at a timely moment.')
+    click_button('Post')
+    expect(current_path).to eq '/'
+    expect(page).to have_content('Posting this message at a timely moment.')
+    expect(page).to have_content(Time.now.strftime("%A %d %B %Y, %H:%M"))
+  end
 end
