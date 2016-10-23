835442e5eca48228cc5fab2399e1c647dd9d99c8
James <bakerje@mac.com>
2016-10-23 16:50:41 +0100

    Added sign up and password check

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
 require_relative 'models/peep'
+require_relative 'models/user'
 
 ENV["RACK_ENV"] ||= "development"
 
@@ -8,6 +9,9 @@ class ChitterChallenge < Sinatra::Base
   get '/' do
     redirect to('/peeps')
   end
+
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/peeps' do
     @peeps = Peep.all
@@ -19,14 +23,25 @@ class ChitterChallenge < Sinatra::Base
   end
 
   post '/peeps' do
-    Peep.create(author: params[:author],
-                comment: params[:comment],
-                created: Time.now)
+    Peep.create(author: params[:author], comment: params[:comment],
+    created: Time.now)
     redirect to('/peeps')
   end
 
-  get 'users/new' do
-    erb :'users/new'
+  get '/users/new' do
+    erb :'/users/new'
+  end
+
+  post '/users' do
+    user = User.create(username: params[:username], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/peeps')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
   # start the server if ruby file executed directly
