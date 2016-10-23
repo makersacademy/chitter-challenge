52a75d6aefafc89a6e76382a7d9c18af227af125
Darrenf <darrenf54321@gmail.com>
2016-10-23 09:09:06 +0100

    updated data_mapper_setup to include development mode

--- app/models/data_mapper_setup.rb
+++ app/models/data_mapper_setup.rb
@@ -4,7 +4,9 @@ require 'dm-postgres-adapter'
 require_relative 'peep'
 require_relative 'user'
 
-
+if ENV["RACK_ENV"]  == "development"
+  ENV["DATABASE_URL"] = "postgres:///chitter_development"
+end
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://mvivlzerbshyri:xWz6KudjQVFr_EJkG434pYmZZF@ec2-54-225-100-236.compute-1.amazonaws.com:5432/d6uuhd81p6seae")
 DataMapper.finalize
