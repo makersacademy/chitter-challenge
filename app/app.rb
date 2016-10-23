b962447b320b3b3fd4b23b2e62cb31a4f5cb7e43
elizabethvenner <elizabethvenner@googlemail.com>
2016-10-23 12:54:37 +0100

    Add user to session

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,9 @@ require 'sinatra/base'
 require_relative 'datamapper_setup'
 
 class ChitterChatter < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     erb :index
   end
@@ -10,11 +13,18 @@ class ChitterChatter < Sinatra::Base
     erb :sign_up
   end
 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
   post '/users' do
-    User.create(email: params[:email],
+    user = User.create(email: params[:email],
                 password: params[:password],
                 name: params[:name],
                 username: params[:username])
+    session[:user_id] = user.id
     redirect to '/'
   end
 
