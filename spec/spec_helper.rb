6369a9d7e396a7ddbf59a18f69eb215331af541a
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-23 13:41:37 +0100

    database cleaner back

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require File.join(File.dirname(__FILE__), '..', './app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-# require 'database_cleaner'
+require 'database_cleaner'
 require 'web_helpers'
 
 Capybara.app = Chitter
@@ -56,20 +56,20 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :transaction
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
-  #
-  # # Everything in this block runs once before each individual test
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
-  #
-  # # Everything in this block runs once after each individual test
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
