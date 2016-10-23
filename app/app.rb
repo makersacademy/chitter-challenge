c3bef6023193f413a413d33eaa4b7f86bc90b7b2
Stevie Winston-Gore <Floki@Stevies-MBP.home>
2016-10-23 09:48:34 +0100

    updated user class

--- app/app.rb
+++ app/app.rb
@@ -5,5 +5,7 @@ require 'sinatra/flash'
 require 'sinatra/partial'
 
 require_relative 'data_mapper_setup'
+require_relative 'server'
 
 require_relative 'controllers/users'
+require_relative 'controllers/peeps'
