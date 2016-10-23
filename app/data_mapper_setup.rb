e3e97977bbe723da7511243b19fb04f1ab40ef94
Stephen Gregory <stejgregory@gmail.com>
2016-10-23 23:19:58 +0100

    Still problem solving with Heroku

---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,8 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+# Now let's set up a connection with a database
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
+# Let's check that everything we wrote in our models was OK
+DataMapper.finalize
+# And let's build any new columns or tables we added
+DataMapper.auto_upgrade!
