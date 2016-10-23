2d406f22d033c7a6f3673b1c3fb4c0ea0bc790d0
glynester <glynester@hotmail.com>
2016-10-23 19:44:14 +0100

    Attempt to get Travis test to pass.

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,10 +5,11 @@ require_relative 'models/peep'
 require_relative 'models/comment'
 require_relative 'models/user'
 
-DataMapper.setup(:default, "postgres://postgres:password@localhost/chitter_db_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, "postgres://localhost/chitter_db_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
+#"postgres://postgres:password@localhost/chitter_db_
 #"postgres://localhost/chitter_db_#
 #chitter_db_test
 #chitter_db_development
