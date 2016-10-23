6f9603b239d8bcdea46bbbd22e6cdd21c7919afd
antonyCastineiras <antonycastineiras80@gmail.com>
2016-10-23 21:17:11 +0100

    added more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,34 @@
 require './app/models/user'
 
 describe User do
-	
+
+	subject { described_class.new(name: :name,
+																		 email: :'example@example.com',
+																		 username: :username,
+																		 password: :password,
+											               password_confirmation: :password)}
+
+	describe '#name' do
+		it 'returns the users name' do
+			expect(subject.name).to eq 'name'
+		end
+	end
+
+	describe '#email' do
+		it 'returns the users email' do
+			expect(subject.email).to eq 'example@example.com'
+		end
+	end
+
+	describe '#username' do
+		it 'returns the users username' do
+			expect(subject.username).to eq 'username'
+		end
+	end
+
+	describe '#password' do
+		it 'returns the users password' do
+			expect(subject.password).to eq :'password'
+		end
+	end
 end
