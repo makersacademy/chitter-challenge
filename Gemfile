7cc41671a1db8a03d1779753986ae7d834f100d9
Kyaw Tun Sein <kyawtunsein.gla@gmail.com>
2016-10-23 22:17:23 +0100

    Add DatabaseCleaner

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.2.3'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
+gem 'database_cleaner'
 
 group :test do
   gem 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 require './app/models/user'
+require 'database_cleaner'
 
 
 Capybara.app = Chitter
@@ -23,15 +24,27 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 
   config.expect_with :rspec do |expectations|
-
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
 
-
   config.mock_with :rspec do |mocks|
-
     mocks.verify_partial_doubles = true
   end
+end
 
+RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
 end
