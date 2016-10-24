be809c91c6ac5becaa5785b7fd0701128ed2b062
Lawrence Hunt <lawrence.hunt@hotmail.com>
2016-10-24 13:46:20 +0100

    Peep reverse order method added

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -10,4 +10,8 @@ class Peep
   property(:text, String, length: 140, required: true)
   property(:timestamp, DateTime, required: true)
 
+  def self.all_in_reverse_order
+    self.all(order: :created_at.desc)
+  end
+
 end
