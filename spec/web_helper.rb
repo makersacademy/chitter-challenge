3d556d80da454c00cb5c4573248c721cfdd3aa15
Katherine Pobee-Norris <kathwath@hotmail.com>
2016-10-24 10:41:40 +0100

    Deleting old files that are affecting travis build

--- spec/web_helper.rb
+++
@@ -1,8 +0,0 @@
-def sign_up
-  visit '/users/new'
-  expect(page.status_code).to eq(200)
-  fill_in :email,    with: 'bob@bob.com'
-  fill_in :password, with: 'bob123'
-  fill_in :password_confirmation, with: 'bob123'
-  click_button 'Create'
-end
