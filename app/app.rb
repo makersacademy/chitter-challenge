b1bf0631b5a70ec170a65588bf1b3e347e7b2948
Ed Powderham <edpowderham@outlook.com>
2016-10-24 10:17:51 +0100

    fixed sign-in issue by adding .id to user

--- app/app.rb
+++ app/app.rb
@@ -55,7 +55,7 @@ class Chitter < Sinatra::Base
     user = User.authenticate(params[:email], params[:password])
     p user
     if user
-      session[:user_id] = user
+      session[:user_id] = user.id
       redirect '/peeps'
     else
       flash.now[:errors] = ['The email or password is incorrect']
