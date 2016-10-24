470a9e14b45b44038c19c3605082947930b166ca
Ed Powderham <edpowderham@outlook.com>
2016-10-24 10:09:38 +0100

    Debugging sign-in process

--- app/app.rb
+++ app/app.rb
@@ -53,6 +53,7 @@ class Chitter < Sinatra::Base
 
   post '/sessions' do
     user = User.authenticate(params[:email], params[:password])
+    p user
     if user
       session[:user_id] = user
       redirect '/peeps'
