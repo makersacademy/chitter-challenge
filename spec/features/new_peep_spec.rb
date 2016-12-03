6d4d809f95325cbb6919c360fe0659e06d863c2e
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-03 18:58:37 +0000

    formatting scenario description

--- spec/features/new_peep_spec.rb
+++ spec/features/new_peep_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 RSpec.feature 'New peep' do
 
-  scenario "Peeps can be created" do
+  scenario "- peeps can be created" do
     Peep.create(content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.')
     expect(Peep.count).to eq 1
   end
