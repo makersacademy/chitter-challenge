8e8706b428f4997c4025bbb1841c709ff17588fe
Rob Adams <robert.andrew.adams@gmail.com>
2016-12-03 10:22:19 +0000

    addded extra signup fields

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,6 +5,8 @@ require './app/app.rb'
 feature 'sign up' do
   scenario 'user can sign up for chitter' do
     visit '/sign_up'
+    fill_in 'name', with: 'Bob'
+    fill_in 'username', with: 'Bobmcbobface'
     fill_in 'email', with: 'bob@bob.com'
     fill_in 'password', with: 'pwd'
     fill_in 'confirm_password', with: 'pwd'
