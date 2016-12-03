ce7affdc98e5f711eb54aad1fa373b2bedb98854
Simon <simon.conway@condaal.com>
2016-12-03 12:34:03 +0000

    Added a simple website on my local machine

--- app/app.rb
+++ app/app.rb
@@ -1,7 +1,21 @@
-ENV['RACK_ENV'] ||= 'development'
+#ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
-require_relative 'data_mapper_setup'
+#require_relative 'data_mapper_setup'
 
 
 class Chitter < Sinatra::Base
+
+get '/' do
+"Hello world!"
+end
+
+
+
+
+
+
+
+# start the server if ruby file executed directly
+run! if app_file == $0
+
 end
