6095883ff240fab3eec04cc02de92e108c9b3ed8
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-03 13:17:47 +0000

    app greets user by name when they sign up

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,10 @@ require_relative 'data_mapper_setup'
 require_relative './models/user'
 
 class Chitter < Sinatra::Base
+
+  enable  :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     'Hello Chitter!'
   end
@@ -14,8 +18,22 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(email: params[:email], password:params[:password],
-                        username: params[:username], name: params[:name])
+    @user = User.new(email: params[:email], password:params[:password], username: params[:username], name: params[:name])
+    @user.save
+    session[:user_id] = @user.id
+    redirect '/peeps'
+  end
+
+  get '/peeps' do
+    erb :'peeps/index'
+  end
+
+
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
   # start the server if ruby file executed directly
