a66c6f42fda12660c4f02631fffefda201243cb0
Kim Wilson <k.wilson541@gmail.com>
2016-12-03 20:51:06 +0000

    Added Sinatra Flash and error for invalid password confirmation

--- app/app.rb
+++ app/app.rb
@@ -1,12 +1,14 @@
 ENV['RACK_ENV'] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
 	enable :sessions
 	set :session_secret, 'super secret'
+	register Sinatra::Flash
 
 	get '/' do
 		erb :'index'
@@ -24,8 +26,13 @@ class Chitter < Sinatra::Base
 			username: params[:username],
 			password: params[:password],
 			password_confirmation: params[:password_confirmation])
-		session[:user_id] = @user.id
-		redirect '/'
+		if @user.save
+			session[:user_id] = @user.id
+			redirect '/'
+		else
+			flash.now[:notice] = "Password and confirmation password do not match"
+			erb :'users/new'
+		end
 	end
 
 	helpers do
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -11,4 +11,15 @@ RSpec.feature 'account sign up', :type => :feature do
 	scenario 'user count increases when user signs up' do
 		expect { sign_up }.to change(User, :count).by(1)
 	end
+
+	scenario 'user count does not increase when password confirmation is incorrect' do
+		expect { invalid_sign_up }.to change(User, :count).by(0)
+	end
+
+	scenario 'user receives error when password confirmation is incorrect' do
+		invalid_sign_up
+		expect(page).to have_current_path('/users/add')
+		expect(page).to have_content('Password and confirmation password do not match')
+	end
+
 end
