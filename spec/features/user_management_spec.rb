6a4f9208c5f80f7ec392dee4a5f75e3fddac6cd9
Dan Pelensky <dan@pelensky.com>
2016-12-03 10:32:19 +0000

    email address required

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -12,4 +12,13 @@ feature 'User signs up' do
     expect(current_path).to eq '/users'
     expect(page).to have_content 'Password and confirmation password do not match'
   end
+
+  scenario "the user must have an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+  scenario "the user must use a valid email address" do
+    expect {sign_up(email: 'dan@dan') }.not_to change(User, :count)
+  end
+
 end
