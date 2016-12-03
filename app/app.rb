81620503b9f284e6e2e5bec1986667479e2e1bab
Simon <simon.conway@condaal.com>
2016-12-03 13:08:06 +0000

    Added a simple user file and checked this created a users database in psql

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,6 @@
-#ENV['RACK_ENV'] ||= 'development'
+ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
-#require_relative 'data_mapper_setup'
+require_relative 'data_mapper_setup'
 
 
 class Chitter < Sinatra::Base
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require 'rake'
 
-#require_relative 'models/[ADD NAME HERE]'
+require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
