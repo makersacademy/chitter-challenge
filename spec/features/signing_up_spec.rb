8a08f1c106aad6e5783a1a9be7b37e305702fc5a
Asuka Ochi <asukaochi@gmail.com>
2016-12-03 14:56:46 +0000

    Clean layout

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -7,4 +7,15 @@ describe "Sing up" do
     expect(page).to have_button("Submit")
   end
 
+  scenario "Users can sign up for Chitter" do
+    visit('/')
+    click_button "Sign up"
+    fill_in :name, with: "one of makers"
+    fill_in :username, with: "cat"
+    fill_in :email, with: "cat@email.com"
+    fill_in :password, with: "secret"
+    click_button "Submit"
+    users = User.all
+    expect(users.count).to eq 1
+  end
 end
