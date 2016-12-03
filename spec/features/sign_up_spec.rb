471f63354b22910999dcebb8b5b4f8a853a94146
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-12-03 15:27:56 +0000

    user has to sign up with a unique  email

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,13 +2,11 @@ require_relative 'web_helpers'
 
 feature "Signing up" do
   scenario "Signing up for Chitter increases the user count by 1" do
-    sign_up
     expect{sign_up}.to change{User.count}.by 1
   end
 
-  scenario "User has to sign up with an unique username and email" do
-    sign_up
-    not_unique_sign_up
-    expect{not_unique_sign_up}.to change{User.count}.by 0
-  end
+   scenario "User has to sign up with an unique username and email" do
+     sign_up
+     expect{sign_up}.to change{User.count}.by 0
+   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -6,12 +6,3 @@ def sign_up
   fill_in 'password', :with => "password"
   click_button('sign-up')
 end
-
-def not_unique_sign_up
-  visit('/sign_up')
-  fill_in 'name', :with => "Samuel"
-  fill_in 'email', :with => "samuel@gmail.com"
-  fill_in 'username', :with => "sam123"
-  fill_in 'password', :with => "password"
-  click_button('sign-up')
-end
