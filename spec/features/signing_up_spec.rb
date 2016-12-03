422026e5f5c11f99ba0b2b8895acb55cc34b1254
George Eaton <george_eaton@hotmail.co.uk>
2016-12-03 16:33:15 +0000

    users cannot sign up with existing email addresses

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -7,12 +7,17 @@ feature 'User sign up' do
   scenario 'requires a matching password' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users/new')
-    expect(page).to have_content 'Your two passwords do not match.'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
   scenario "can't sign up without an email address" do
     expect { sign_up(email: nil) }.not_to change(User, :count)
   end
   scenario "can't sign up without a valid email" do
     expect { sign_up(email:nil) }.not_to change(User, :count)
+  end
+  scenario "can't sign up with an existing email" do
+    sign_up
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 end
