7a8b7faecc44665ff5266b43175b6bc9d72d981a
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-03 15:12:14 +0000

    remove redundant test

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -2,14 +2,6 @@ require 'spec_helper'
 
 RSpec.feature 'User sign-up' do
 
-  scenario "- user visits sign-up page and sees sign-up form" do
-    visit '/users/new'
-    expect(page).to have_content "Email:"
-    expect(page).to have_content "Password:"
-    expect(page).to have_content "Username:"
-    expect(page).to have_content "Name:"
-  end
-
   scenario "- user fills out form and creates a new user account" do
     expect{ new_user_sign_up('foo@foo.com', '123secret', 'foo', 'Foo McFooface') }.to change(User, :count).by(1)
   end
