e742c64d0d8839438ed1dec540a06fe10dba553e
TudorTacal <tudortacal@gmail.com>
2016-12-03 18:42:56 +0000

    Added ENV variables for differentiating between  test and development databases

--- app/models/datamapper_settings.rb
+++ app/models/datamapper_settings.rb
@@ -3,7 +3,7 @@ require 'data_mapper'
 def data_mapper_settings
 
   DataMapper::Logger.new($stdout, :debug)
-  DataMapper.setup(:default, 'postgres://localhost/chitter_development')
+  DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 
