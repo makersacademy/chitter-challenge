4bc07ca7a5b0021885ea645d9381258bfbcd946a
ajbater <amanda22@hotmail.co.uk>
2016-12-03 14:21:06 +0000

    updated data_mapper_setup

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 require_relative 'models/user.rb'
+require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
   get '/' do
--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -4,6 +4,6 @@ require 'dm-validations'
 
 require_relative 'models/user.rb'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}')
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
