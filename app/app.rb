657b8fd61fa10bc148996cf0c2f12514a70d9980
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-03 16:26:33 +0000

    Adds ability to log out

--- app/app.rb
+++ app/app.rb
@@ -5,37 +5,55 @@ require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
+  enable :sessions
+
+  use Rack::MethodOverride
+
   get '/' do
     "Hello"
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'/users/new'
   end
 
   post '/users' do
     user = User.new(email: params[:email], name: params[:name], username: params[:username], password: params[:password])
     if user.save
+      session[:user_id] = user.id
       redirect '/peeps'
     end
   end
 
   get '/session/new' do
+    @user = User.new
     erb :'/session/new'
   end
 
   post '/session' do
     user = User.authenticate(params[:email], params[:password])
     if user
+      session[:user_id] = user.id
       redirect '/peeps'
     end
   end
 
   get '/peeps' do
-
+    @peeps = Peep.all
+    erb :'/peeps/peeps'
   end
 
+  delete '/session' do
+    session[:user_id] = nil
+    redirect '/peeps'
+  end
 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
 
   run! if app_file == $0
 
