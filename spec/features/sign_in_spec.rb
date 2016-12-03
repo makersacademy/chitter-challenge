5d2292adcc8652d3ee74d78f525abb2a0106dc1c
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2016-12-03 13:34:42 +0000

    Add test to ensure a user cannot login with an inccorect password

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,9 +3,16 @@ feature "Signing In" do
   scenario "I want to successfully sign in to Chitter" do
     sign_up
     sign_in
-
     expect(current_path).to include '/peeps'
+  end
 
+  scenario "I want to fail to sign in to Chitter" do
+    sign_up
+    visit '/session/new'
+    fill_in :email, with: 'tester1@test.com'
+    fill_in :password, with: 'test999'
+    click_button "Sign In"
+    expect(current_path).to include '/session'
   end
 
 end
