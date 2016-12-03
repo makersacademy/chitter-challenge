2a221c632765c591fdeb0fba6522b73b0b367399
George Eaton <george_eaton@hotmail.co.uk>
2016-12-03 10:10:17 +0000

    adds new user functionality

--- app/app.rb
+++ app/app.rb
@@ -3,8 +3,24 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+  set :sessions_secret, 'super secret'
+
   get '/users/new' do
     erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(name: params[:name],
+                       username: params[:username],
+                       email: params[:email],
+                       password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/home')
+  end
+
+  get '/home' do
+    erb :home
   end
 
   # start the server if ruby file executed directly
