af96df1ac21f957eef0a3f2fa033c6331068443d
Agata Sumowska <agata.sumowska@gmail.com>
2016-12-04 17:08:52 +0000

    Peeps have name and user name displayed

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -11,4 +11,33 @@ feature 'Creating peeps' do
       expect(page).to have_content('Hello world')
     end
   end
+
+
+  scenario 'a new peep displays name and user name' do
+    sign_up
+    visit '/'
+    fill_in 'text',   with: 'Hello world'
+    click_button 'Post a peep'
+
+
+    within 'ul#peeps' do
+      expect(page).to have_content('Agata')
+      expect(page).to have_content('Agatina')
+    end
+  end
+
+
+  scenario 'I can create multiple peeps' do
+    sign_up
+    visit '/'
+    fill_in 'text',   with: 'Hello world'
+    click_button 'Post a peep'
+    fill_in 'text',   with: 'Hello world'
+    click_button 'Post a peep'
+    expect(current_path).to eq '/'
+
+    within 'ul#peeps' do
+      expect(page).to have_content('Hello world')
+    end
+  end
 end
