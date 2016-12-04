07d3f132f5b59d4abde7f8cc0a67ed8c66418393
TudorTacal <tudortacal@gmail.com>
2016-12-04 13:19:27 +0000

    Added a error message if user's login credentials are not good.

--- app/app.rb
+++ app/app.rb
@@ -6,11 +6,20 @@ require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
   enable :sessions
+  set :session_secret, 'super secret'
   register Sinatra::Flash
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+ end
 
   get '/' do
     erb :first_page
   end
+
+
 
   get '/sign_up' do
     erb :sign_up
@@ -21,20 +30,23 @@ class Chitter < Sinatra::Base
   end
 
   post '/log_in' do
-    @user = User.authenticate(params[:username],params[:password])
-    if @user
+    user = User.authenticate(params[:username],params[:password])
+    if user
+      session[:user_id] = user.id
       erb :user
     else
+      flash.now[:errors] = ['The email or password is incorrect']
       redirect 'log_in/new'
     end
   end
 
   post '/users' do
-    @user = User.new(email: params[:email], name: params[:name], password: params[:password], username: params[:username])
-    if @user.save
+    user = User.new(email: params[:email], name: params[:name], password: params[:password], username: params[:username])
+    if user.save
+      session[:user_id] = user.id
       erb :user
     else
-      flash[:errors] = @user.errors.full_messages.join(" ")
+      flash[:errors] = user.errors.full_messages.join(" ")
       redirect '/sign_up'
     end
   end
