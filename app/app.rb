cf1f0c19a657071a0e0395a425c78558b3b60fa6
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-04 11:55:15 +0000

    refactor peeps to separate controller

--- app/app.rb
+++ app/app.rb
@@ -7,6 +7,7 @@ require_relative 'data_mapper_setup'
 
 require_relative 'controllers/users'
 require_relative 'controllers/sessions'
+require_relative 'controllers/peeps'
 
 require_relative './models/user'
 require_relative './models/peep'
@@ -24,27 +25,6 @@ class Chitter < Sinatra::Base
   get '/' do
     @peeps = Peep.all(:order => [ :id.desc ])
     erb :'peeps/index'
-  end
-
-  post '/peeps' do
-    peep = Peep.create(content: params[:content], user_id: current_user.id)
-    redirect '/'
-  end
-
-  get '/peeps/new' do
-    if current_user == nil
-      redirect '/sessions/new'
-    else
-      erb :'peeps/new'
-    end
-  end
-
-  get '/peeps/:id' do
-    @peep = Peep.get(params[:id])
-    @username = User.get(@peep.user_id).username
-    # p @peep
-    # p @username
-    erb :'peeps/get'
   end
 
   helpers do
