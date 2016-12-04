22975331dbeca5300d1859c1e4c7880e3c9ceda5
TudorTacal <tudortacal@gmail.com>
2016-12-04 13:57:29 +0000

    Fixed minor bug

--- app/models/datamapper_settings.rb
+++ app/models/datamapper_settings.rb
@@ -3,7 +3,7 @@ require 'data_mapper'
 def data_mapper_settings
 
   # DataMapper::Logger.new($stdout, :debug)
-  DataMapper.setup(:default, ENV['DATABASE_URL'], "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 
