b884b3c12db302b061b8c3c548b23762afd9f2fc
Leke Abolade <leke_abolade92@hotmail.co.uk>
2016-12-04 18:27:13 +0000

    removing ability to peep if not logged in

--- spec/features/peeping_spec.rb
+++ spec/features/peeping_spec.rb
@@ -4,29 +4,33 @@ feature "Peeps homepage" do
   let(:peep2) {"Cow meatloaf flank, spare ribs be."}
 
   scenario "a user can write a peep" do
-
     sign_up
     correct_log_in
     post_peep
     expect(page).to have_content(peep)
     expect{post_peep}.to change{Peep.count}.by 1
+  end
 
+  scenario "only a signed in user can write a peep" do
+    visit("/peeps")
+    expect(page).not_to have_button('peep')
   end
 
   scenario "peeps appear in reverse chronological order" do
-
     sign_up
     correct_log_in
     post_two_peeps
     expect(page.body.index(peep)).to be > page.body.index(peep2)
-
-
   end
 
-  scenario "post has a timestamp" do
+  scenario "peep has a timestamp" do
     sign_up
     correct_log_in
     post_peep
     expect(Peep.first.created_at).not_to eq nil
+  end
+
+  scenario "Every peep belongs to a user" do
+
   end
 end
