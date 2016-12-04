ac7b0484c6cd43297ed905dc6c13eee887de9c46
keomony khun <keomony@gmail.com>
2016-12-04 09:07:27 +0000

    replaces sign up with login

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,11 +1,11 @@
 
-feature "Sign up" do
-  scenario "When user doesn't have a Chitter account" do
+feature "Login" do
+  scenario "user fills in their email address and password" do
     visit "/"
+    expect(page.status_code).to eq(200)
     fill_in "email", :with => "keomony@gmail.com"
     fill_in "password", :with => "this is the password"
-    click_button "Sign up"
+    click_button "Login"
     expect(page.current_path).to eq "/peeps"
-    expect(page.status_code).to eq 200
   end
 end
