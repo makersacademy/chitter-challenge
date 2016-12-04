462f5fe9a3c623a66c7335a3e2d24ab5c742dc68
unicornelia <ez.kornelia@gmail.com>
2016-12-04 00:51:46 +0000

    Updated spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,24 +16,24 @@ require 'capybara/rspec'
 require 'rspec'
 require 'features/web_helpers'
 require './app/app'
-
+require 'database_cleaner'
 
 Capybara.app = Chitter
 
 RSpec.configure do |config|
 
-  # config.before(:suite) do
-  #   DatabaseCleaner.strategy = :transaction
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
-  #
-  # config.before(:each) do
-  #   DatabaseCleaner.start
-  # end
-  #
-  # config.after(:each) do
-  #   DatabaseCleaner.clean
-  # end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
