df67ecaf19f44b7cd91e8e4109f49481c3e1f285
Kim Wilson <k.wilson541@gmail.com>
2016-12-04 12:12:38 +0000

    Refactored feature tests to expect appropriate error messages

--- spec/features/unsuccessful_sign_up_spec.rb
+++ spec/features/unsuccessful_sign_up_spec.rb
@@ -4,22 +4,27 @@ RSpec.feature 'unsuccessful account sign up', :type => :feature do
 
 	scenario 'user cannot sign up without entering a firstname' do
 		expect { sign_up(firstname: nil) }.not_to change(User, :count)
+		expect(page).to have_content('Firstname must not be blank')
 	end
 
 	scenario 'user cannot sign up without entering a surname' do
 		expect { sign_up(surname: nil) }.not_to change(User, :count)
+		expect(page).to have_content('Surname must not be blank')
 	end
 
 	scenario 'user cannot sign up without entering an email' do
 		expect { sign_up(email: nil) }.not_to change(User, :count)
+		expect(page).to have_content('Email must not be blank')
 	end
 
 	scenario 'user cannot sign up without entering a username' do
 		expect { sign_up(username: nil) }.not_to change(User, :count)
+		expect(page).to have_content('Username must not be blank')
 	end
 
 	scenario 'user cannot sign up with an invalid email address' do
 		expect { sign_up(email: 'not-an-email') }.not_to change(User, :count)
+		expect(page).to have_content('Email has an invalid format')
 	end
 
 	scenario 'user cannot sign up with an already registered email' do
