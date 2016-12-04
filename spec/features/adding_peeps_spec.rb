9be808f504ba3e6709a6c3f207fed7c0a19beb02
Kim Wilson <k.wilson541@gmail.com>
2016-12-04 16:06:43 +0000

    Added feature test for adding a peep

---
+++ spec/features/adding_peeps_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+RSpec.feature 'adding peeps', :type => :feature do
+
+	include Helpers
+
+	let!(:user) do
+		User.create(firstname: 'John',
+			surname: 'Doe',
+			email: 'john@doe.com',
+			username: 'JayDawg',
+			password: 'password123',
+			password_confirmation: 'password123')
+	end
+	
+	scenario 'user can add a new peep' do
+		visit '/peeps/new'
+		expect(page.status_code).to eq 200
+
+		fill_in :text, :with => 'Hello world!'
+		click_button 'Peep!'
+		
+		within 'ul#peeps' do
+			expect(page).to have_content('John Doe @JayDawg')
+			expect(page).to have_content('Hello world!')
+		end
+	end
+end
