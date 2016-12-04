e4a64c86287c95c2848569829cff7b8a0fb9b69d
Mike Hurl <mikehurl@gmail.com>
2016-12-04 15:26:58 +0000

    second test passed, users can complete sign up forms

--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -4,11 +4,12 @@ feature 'a user can sign up to chitter' do
     visit('/')
     click_button('Sign up')
     expect(current_path).to eq('/sign-up')
+    expect(page.status_code).to eq(200)
     expect(page).to have_content('Please sign up to peep to the world!')
-    expect(page).to have_field('Name')
-    expect(page).to have_field('User name')
-    expect(page).to have_field('Email')
-    expect(page).to have_field('Password')
+    expect(page).to have_field('name')
+    expect(page).to have_field('user_name')
+    expect(page).to have_field('email')
+    expect(page).to have_field('password')
     expect(page).to have_button('Sign me up!')
   end
 
