6d3eb39d62a428b2b92b07072d2a72d68bb1b50f
ajbater <amanda22@hotmail.co.uk>
2016-12-04 13:19:10 +0000

    user can only peep when logged in, but can view peeps without logging in

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper.rb'
 
 feature 'creating new peeps' do
 
+  let!(:user) do
+    User.create(name: 'Amanda',
+                username: 'amanda',
+                email: 'hello@example.com',
+                password: 'password',
+                password_confirmation: 'password')
+  end
+
   scenario 'user can create a new peep' do
+    sign_in(email: user.email, password: user.password)
     visit '/chitter'
     fill_in 'message', with: 'I have just joined Chitter!'
     click_button 'peep'
