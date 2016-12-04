588e11510528ced149376e8afc22dfa90d8be094
TudorTacal <tudortacal@gmail.com>
2016-12-04 14:42:22 +0000

    Wrote the feature test for creating a new peep.

---
+++ spec/features/peep_posting_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require_relative '../web-helper'
+
+# As a Maker
+# So that I can let people know what I am doing
+# I want to post a message (peep) to chitter
+
+feature "Posting peeps" do
+  scenario "I want to post a peep to chitter" do
+    sign_up
+    log_in
+    visit '/peeps/new'
+    fill_in 'peep' , with: "This is my first peep. And what a peep it is"
+    click_button 'Peep'
+    expect(page.current_path).to eq '/peeps'
+    expect(page).to have_content "This is my first peep. And what a peep it is"
+  end
+end
