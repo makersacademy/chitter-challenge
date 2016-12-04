e5479eabd0a91a364d271f202f2cd3e37d0f0a07
malin patel <malin.patel@icloud.com>
2016-12-04 14:21:40 +0000

    Sign up test passing

---
+++ app/app.rb
@@ -0,0 +1,25 @@
+ENV["RACK_ENV"] ||= "development"
+
+require 'sinatra/base'
+# require_relative 'data_mapper_setup.rb'
+
+class Chitter < Sinatra::Base
+  enable :sessions
+
+  get '/' do
+    erb :'sign_up'
+  end
+
+  get '/sign_up' do
+    erb :'sign_up'
+  end
+
+  post '/sign_up' do
+    # erb :'index'
+  end
+
+
+
+
+run! if app_file == $0
+end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper.rb'
 feature 'Sign Up' do
 
   scenario 'sign up to Chitter' do
-    visit '/'
-    fill_in :email, with 'test@123.com'
-    fill_in :handle, with '@test_123'
-    fill_in :password, with 'password123'
+    visit '/sign_up'
+    fill_in :email, with: 'test@123.com'
+    fill_in :handle, with: '@test_123'
+    fill_in :password, with: 'password123'
     click_button "Sign Up"
   end
 end
