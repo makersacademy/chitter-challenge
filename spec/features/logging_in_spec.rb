06e8708bea3d3aed22806102749e50c5e9c70250
Russell Yeo <rusyeo@gmail.com>
2016-12-04 17:31:51 +0000

    update tests to have user double

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -1,22 +1,22 @@
 feature "Logging in:" do
 
-  before do
-    created_user = User.create(
-    email: "fly@high.com",
-    password: "bowling1234",
-    password_confirmation: "bowling1234",
-    name: "The Dude",
-    username: "thedude"
-    )
+  let!(:user) do
+    User.create(
+      email: "fly@high.com",
+      password: "bowling1234",
+      password_confirmation: "bowling1234",
+      name: "The Dude",
+      username: "thedude"
+      )
   end
 
   scenario "user logs in with a valid account" do
     log_in
     expect(current_path).to eq '/peeps'
-    expect(page).to have_content 'Welcome to chitter thedude'
+    expect(page).to have_content "Welcome to chitter #{user.username}"
   end
 
-  def log_in(username: "thedude", password:"bowling1234")
+  def log_in(username: user.username, password: user.password)
     visit '/sessions/new'
     fill_in("username", with: username)
     fill_in("password", with: password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 describe User do
 
-  before do
-    created_user = User.create(
+  let!(:user) do
+    User.create(
       email: "fly@high.com",
       password: "bowling1234",
       password_confirmation: "bowling1234",
@@ -11,12 +11,12 @@ describe User do
   end
 
   it 'authenticates when given correct details' do
-    authenticated_user = User.authenticate("thedude", "bowling1234")
-    expect(authenticated_user.name).to eq "The Dude"
+    authenticated_user = User.authenticate(user.username, user.password)
+    expect(authenticated_user).to eq user
   end
 
   it 'does not authenticate when given an incorrect password' do
-    expect(User.authenticate("thedude", "it_really_tied_the_room_together")).to be_nil
+    expect(User.authenticate(user.username, "it_really_tied_the_room_together")).to be_nil
   end
 
 end
