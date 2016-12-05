e7f076fdda0b4701ab800842d1a32c712ea9948d
keomony khun <keomony@gmail.com>
2016-12-05 18:01:33 +0000

    passes authentication when given a valid email and password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,14 @@
 describe User do
-  let!(:user) { User.create(email: "keomony@gmail.com", password: "this is password") }
+  let!(:user) { User.create(email: "keomony@gmail.com",
+                            password: "this is password",
+                            password_confirmation: "this is password") }
 
-  xit "authenticates when given a valid email address and password" do
-    authenticated_user = User.authenticate(user.email, "this is password")
+  it "authenticates when given a valid email address and password" do
+    authenticated_user = User.authenticate("keomony@gmail.com", "this is password")
     expect(authenticated_user).to eq user
   end
 
   it "does not authenticate when given an incorrect password" do
-    expect(User.authenticate(user.email, "wrong password")).to be_nil
+    expect(User.authenticate("keomony@gmail.com", "wrong password")).to be_nil
   end
 end
