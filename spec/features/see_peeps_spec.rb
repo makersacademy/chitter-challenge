70fb4fa30dd03851f3513654d92a7cd25436e7ce
Oscar Barlow <oscar.barlow@gmail.com>
2016-12-05 11:11:07 +0000

    Squash duplicated features test into one test with multiple expects

--- spec/features/see_peeps_spec.rb
+++ spec/features/see_peeps_spec.rb
@@ -5,10 +5,6 @@ RSpec.feature 'See peeps' do
   scenario "- everyone can see can see peeps" do
     peep_and_log_out
     expect(page).to have_content('Lorem ipsum dolor sit amet.')
-  end
-
-  scenario "- everyone can see when a peep was peeped" do
-    peep_and_log_out
     expect(page).to have_content('Sent at')
   end
 
