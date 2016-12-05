1b3891857de5794537f4b0cf30a2ecf3234a5cbc
George Eaton <george_eaton@hotmail.co.uk>
2016-12-05 16:44:56 +0000

    starting styling

--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -9,6 +9,6 @@ feature 'Posting a peep' do
   end
   scenario 'I cannot post a peep if not signed in' do
     visit '/peeps/new'
-    expect(page).to have_content("Please sign in:")
+    expect(page).to have_content("Please log in:")
   end
 end
