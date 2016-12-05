e088242e517064d60a4ee8e6cab3146e5d95fa1f
Russell Yeo <rusyeo@gmail.com>
2016-12-05 14:37:25 +0000

    add search for css elements to posting peeps tests

--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -12,6 +12,7 @@ feature "Posting peeps:" do
 
   scenario "user is logged in and posts a peep" do
     log_in(username: "bob", password: "averystrongpassword")
+    expect(page).to have_css('div.peep_field')
     expect{ peep }.to change(Peep, :count).by(1)
     expect(current_path).to eq '/peeps'
     expect(page).to have_content("Thank you, your peep has been posted.")
@@ -20,6 +21,7 @@ feature "Posting peeps:" do
   scenario "User is not logged in. Should not see new peep entry field." do
     visit '/peeps'
     expect(page).not_to have_content("New Peep")
+    expect(page).not_to have_css('div.peep_field')
   end
 
 
