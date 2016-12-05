89ccde4312bea5997c61e440b41b04abbbead231
Dan Pelensky <dan@pelensky.com>
2016-12-05 15:32:15 +0000

    date and time updated

--- app/chitter.rb
+++ app/chitter.rb
@@ -40,7 +40,6 @@ class Chitter < Sinatra::Base
     @user = User.create(name: params[:name], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
-      session[:user_name] = @user.name
       redirect '/peeps/new'
     else
       flash.now[:errors] = @user.errors.full_messages
