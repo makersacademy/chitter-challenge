dea15f78ada15ad2c68f95fd332724ce6ee44b2d
Russell Yeo <rusyeo@gmail.com>
2016-12-05 14:27:53 +0000

    User cannot see form to enter peep if they are not logged in

--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -10,13 +10,17 @@ feature "Posting peeps:" do
     )
   end
 
-  scenario "user is signed in and posts a peep" do
+  scenario "user is logged in and posts a peep" do
     log_in(username: "bob", password: "averystrongpassword")
     expect{ peep }.to change(Peep, :count).by(1)
     expect(current_path).to eq '/peeps'
     expect(page).to have_content("Thank you, your peep has been posted.")
   end
 
+  scenario "User is not logged in. Should not see new peep entry field." do
+    visit '/peeps'
+    expect(page).not_to have_content("New Peep")
+  end
 
 
 end
