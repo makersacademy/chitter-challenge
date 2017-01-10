a6199adc412899121869a04d6df046946bf2c1cd
Elizabeth <elizabethvenner@googlemail.com>
2017-01-10 11:40:24 +0000

    refactor /post sessions to include log_in_user helper function

--- app/app.rb
+++ app/app.rb
@@ -60,7 +60,7 @@ class ChitterChatter < Sinatra::Base
   post '/sessions' do
     user = User.authenticate(params[:email], params[:password])
     if user
-      session[:user_id] = user.id
+      log_in_user(user.id)
       redirect to '/'
     else
       flash.now[:errors] = 'The email or password is incorrect'
