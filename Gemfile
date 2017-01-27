c65b7250707450f1afa5708e504edc63b64bf485
Justinio14 <jbutwell@hotmail.com>
2017-01-27 18:09:22 +0000

    initial commit

--- Gemfile
+++ Gemfile
@@ -1,10 +1,21 @@
 source 'https://rubygems.org'
 ruby '2.4.0'
 
+gem 'sinatra'
+gem 'sinatra-flash'
+gem 'bcrypt'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+
 group :test do
   gem 'rspec'
   gem 'cucumber'
   gem 'rake'
   gem 'coveralls', require: false
+  gem 'rubocop-rspec'
+  gem 'rubocop'
+  gem 'rspec-sinatra'
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl'
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,39 @@
 require 'coveralls'
 require 'simplecov'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'database_cleaner'
+require 'sinatra'
+#require 'web_helper'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+
+RSpec.configure do |config|
+
+
+  config.before(:suite) do # <-- before entire test run
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do # <-- create a "save point" before each test
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do # <-- after each individual test roll back to "save point"
+    DatabaseCleaner.clean
+  end
+
+
+  config.include Capybara::DSL # good
+  #config.include FactoryGirl::Syntax::Methods # sure
+  #config.include TestHelpers # good
+  #config.include ChitterHelpers # nooooooooooo
+
+end
