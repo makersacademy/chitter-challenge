50b5582fbf22951616ce2dcc2026660b25d226ea
James Turner <mail.jt@gmail.com>
2017-01-28 16:03:36 +0000

    heroku push

--- lib/data_mapper_setup.rb
+++ lib/data_mapper_setup.rb
@@ -4,6 +4,6 @@ require 'dm-postgres-adapter'
 require_relative './model/user'
 require_relative './model/twit'
 
-DataMapper.setup(:default, "postgres://localhost/tweeter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/tweeter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
