066d1d0891f5038b591afdce74f244fab0899929
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-01-28 19:18:32 +0000

    Add further testing for sign up view

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -17,3 +17,22 @@ def wrong_sign_up
   fill_in :name, with: 'dylan'
   click_button 'Sign Up'
 end
+
+def no_email
+  visit '/users/new'
+  fill_in :password, with: 'bossman'
+  fill_in :password_confirmation, with: 'wrong_password_confirmation'
+  fill_in :username, with: 'dylanrhodius'
+  fill_in :name, with: 'dylan'
+  click_button 'Sign Up'
+end
+
+def wrong_email_format
+  visit '/users/new'
+  fill_in :email, with: 'dylanbossman.com'
+  fill_in :password, with: 'bossman'
+  fill_in :password_confirmation, with: 'wrong_password_confirmation'
+  fill_in :username, with: 'dylanrhodius'
+  fill_in :name, with: 'dylan'
+  click_button 'Sign Up'
+end
