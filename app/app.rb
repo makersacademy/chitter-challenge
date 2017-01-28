effe62d84ebb0c03469a0d46dcb8fefba8d4477e
Ben Kielty <bwk103@gmail.com>
2017-01-28 18:22:58 +0000

    User Story 1 - Added functionality - email / username must be unique

--- app/app.rb
+++ app/app.rb
@@ -6,21 +6,37 @@ require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
   register Sinatra::Flash
+  enable :sessions
+  set :session_secret, 'super secret'
+
 
   get '/home' do
     "Hello"
   end
 
   get '/sign_up' do
-      erb :sign_up
+    @user = User.new
+    erb :sign_up
     end
 
   post '/users' do
-    @user = User.create(name: params[:name],
+    @user = User.new(name: params[:name],
                     email: params[:email],
                     username: params[:username],
                     password: params[:password])
-    redirect '/home'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/home'
+    else
+      flash.now[:error] = "Signup failed: Those details have already been used"
+      erb :sign_up
+    end
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
   # start the server if ruby file executed directly
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User
   property :email, String
   property :username, String
   property :password, String
+
+  validates_uniqueness_of(:username, :email)
+
 end
