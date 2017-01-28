905bc9e46a62fbf053d6f78284c7bf1dfe618d91
Ben Kielty <bwk103@gmail.com>
2017-01-28 17:53:36 +0000

    User Story 1 - Fixed bug with datamapper

--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -2,10 +2,35 @@ feature "sign up to Chitter" do
   scenario "it allows users to sign up using their name" do
     visit '/sign_up'
       fill_in :name, with: "James"
-      fill_in :email, with: "james@email.com"
-      fill_in :username, with: "jimbo"
-      fill_in :password, with: "llama123"
       click_button :Submit
       expect(current_path).to eq '/home'
     end
+
+  scenario "it allows users to sign up using their email" do
+    visit '/sign_up'
+    fill_in :name, with: "James"
+    fill_in :email, with: "james@email.com"
+    click_button :Submit
+    expect(current_path).to eq '/home'
+  end
+
+  scenario "it allows users to sign up with a username" do
+    visit '/sign_up'
+    fill_in :name, with: "James"
+    fill_in :email, with: "james@email.com"
+    fill_in :username, with: "jimbo"
+    click_button :Submit
+    expect(current_path).to eq '/home'
+  end
+
+  scenario "it allows users to sign up with a password" do
+    visit '/sign_up'
+    fill_in :name, with: "James"
+    fill_in :email, with: "james@email.com"
+    fill_in :username, with: "jimbo"
+    fill_in :password, with: "llama123"
+    click_button :Submit
+    expect(current_path).to eq '/home'
+  end
+
 end
