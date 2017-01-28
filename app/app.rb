c9a483de86cff018e77df8265e08b9264e121b70
Adrian Eyre <adrian.eyre@hotmail.co.uk>
2017-01-28 10:52:35 +0000

    Added more tests for user sign up

--- app/app.rb
+++ app/app.rb
@@ -47,6 +47,5 @@ class Chitter < Sinatra::Base
     end
   end
 
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,4 +8,31 @@ feature 'User sign up:' do
     expect(page).to have_content("Welcome, test@test.com")
     expect(User.first.email).to eq('test@test.com')
   end
+
+  scenario 'User cant sign in with a blank email address' do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(page).to have_content("Email must not be blank")
+  end
+
+  scenario 'User cant sign in with a blank name' do
+    expect { sign_up(name: nil) }.not_to change(User, :count)
+    expect(page).to have_content("Name must not be blank")
+  end
+
+  scenario 'User cant sign in with a blank username' do
+    expect { sign_up(username: nil) }.not_to change(User, :count)
+    expect(page).to have_content("Username must not be blank")
+  end
+
+  scenario 'Make sure that the password and password_confirmation match' do
+    expect{sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
+    expect(page).to have_content('Password does not match')
+    expect(current_path).to eq('/users/new')
+  end
+
+  scenario 'User cant sign in without valid email' do
+    sign_up
+    expect { sign_up(email: "test.com") }.not_to change(User, :count)
+    expect(page).to have_content("Email has an invalid format")
+  end
 end
