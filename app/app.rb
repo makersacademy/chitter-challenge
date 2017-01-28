46d6c39b079a27b88ffac89d359e492fbda78ae0
Mike Field-May <mfieldmay@gmail.com>
2017-01-28 12:48:16 +0000

    Created failing tests for user

--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,9 @@
 require 'sinatra/base'
 
 class ChitterApp < Sinatra::Base
-  
+
+  get '/sign_up' do
+    erb :sign_up_form
+  end
+
 end
--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -7,5 +7,9 @@ require './spec/spec_helper'
 feature 'Signing up' do
   scenario "so I can sign up to Chitter" do
     sign_up
+    expect(page).to have_content('Welcome m1k3')
+  end
+  scenario 'increases the user count' do
+    expect{sign_up}.to change{User.count}.by(1)
   end
 end
