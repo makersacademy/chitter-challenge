6af360ee21beb3179fb1b4c4d53bed364ad01d24
Sam Jones <sam.jones494@googlemail.com>
2017-01-28 14:44:23 +0000

    Working on sign up

--- app/app.rb
+++ app/app.rb
@@ -6,6 +6,10 @@ require_relative './models/peep'
 require_relative './models/tag'
 
 class Chitter < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     redirect '/peeps'
   end
@@ -32,6 +36,24 @@ class Chitter < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @peeps = tag ? tag.peeps : []
     erb :'peeps/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(name: params[:name], email: params[:email], user_name: params[:user_name], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/peeps'
+  end
+
+  helpers do
+
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+
   end
 
   # start the server if ruby file executed directly
