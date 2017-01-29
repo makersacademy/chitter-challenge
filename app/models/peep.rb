04c38329dedaf4c1f39ca7a6bcb869d9b6caec2b
herecomesjaycee <jaycee.cheong04@gmail.com>
2017-01-29 17:14:11 +0000

    Edit string format in peep

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -2,7 +2,7 @@ class Peep
 	include DataMapper::Resource
 
 property :id, Serial
-property :content, String, :length => 140
+property :content, String, length: 140
 property :created_at, DateTime
 
 belongs_to :user
