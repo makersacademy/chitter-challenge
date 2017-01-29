36636685a57c8d66899a5b54e7a18d2f91e1478b
Matthew Perry <thesedatedprince@gmail.com>
2017-01-29 12:14:00 +0000

    user story 1 completed

--- Gemfile
+++ Gemfile
@@ -1,10 +1,21 @@
 source 'https://rubygems.org'
-ruby '2.4.0'
+ruby '2.3.3'
+
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'database_cleaner'
+gem 'bcrypt'
+gem 'sinatra-flash'
+gem 'sinatra-partial'
+gem 'timecop'
+
 
 group :test do
   gem 'rspec'
   gem 'cucumber'
   gem 'rake'
+  gem 'capybara'
+  gem 'rspec-sinatra'
+  gem 'sinatra', '2.0.0.beta2'
   gem 'coveralls', require: false
 end
-
---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'sinatra/base'
+require './models/user.rb'
+
+class Chitter < Sinatra::Base
+  enable :sessions
+  get '/' do
+    redirect '/timeline'
+  end
+
+  get '/users/new' do
+    erb :'/users/new'
+  end
+
+  post '/users' do
+    user = User.create(name: params[:name], email: params[:email], username: params[:username])
+    session[:username] = user.username
+    redirect '/timeline'
+  end
+
+  get '/timeline' do
+    @username = session[:username]
+    erb :timeline
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
