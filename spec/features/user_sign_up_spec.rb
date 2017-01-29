58468390342ef8b0ce9606c8ea7928f2fb36dfd9
Joe Marriott <marriott90@gmail.com>
2017-01-29 12:08:04 +0000

    validates uniqueness of username

--- spec/features/user_sign_up_spec.rb
+++ spec/features/user_sign_up_spec.rb
@@ -9,9 +9,22 @@ feature 'New user sign up' do
   end
   scenario 'Requires confirmation password to match on sign up' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
   scenario 'User unable to sign up if email field is blank' do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(page).to have_content 'Email must not be blank'
+  end
+  scenario 'Requires valid email address to sign up' do
+    expect { sign_up(email: 'joe@example') }.not_to change(User, :count)
+    expect(page).to have_content 'Email has an invalid format'
+  end
+  scenario 'Unable to sign up if email already used' do
+    expect { sign_up }.not_to change(User, :count)
+    expect(page).to have_content 'Email is already taken'
+  end
+  scenario 'Unable to sign up with username that is taken' do
+    expect { sign_up }.not_to change(User, :count)
+    expect(page).to have_content 'Username is already taken'
   end
 end
