54ccc6729cc366fc618a33ea28e0478a3b369432
bvjones <benvaughanjones@gmail.com>
2017-01-29 18:36:18 +0000

    final commit

--- app/app.rb
+++ app/app.rb
@@ -74,6 +74,5 @@ class Chitter < Sinatra::Base
     redirect to '/'
   end
 
-  # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,11 +17,6 @@ include DataMapper::Resource
 
   validates_confirmation_of :password
 
-  # def password=(password)
-  #   @password = password
-  #   self.password_digest = BCrypt::Password.create(password)
-  # end
-
   def self.authenticate(email, password)
     user = first(email: email)
     if user && BCrypt::Password.new(user.password_digest) == password
