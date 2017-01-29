84f3ed9440099e90a03a7738efd22a41a9726502
Joe Marriott <marriott90@gmail.com>
2017-01-29 18:42:04 +0000

    refactored chitter.rb into /controllers and server.rb

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,76 +1,11 @@
+ENV['RACK_ENV'] ||= 'development'
+
 require 'sinatra/base'
 require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 require_relative './models/user'
 require_relative './models/peep'
-
-class Chitter < Sinatra::Base
-  use Rack::MethodOverride
-  ENV['RACK_ENV'] ||= 'development'
-  register Sinatra::Flash
-  enable :sessions
-  set :session_secret, 'super secret'
-
-  get '/' do
-    redirect '/feed'
-  end
-
-  get '/feed' do
-    @peeps = Peep.all(order: [:created_at.desc])
-    erb :'feed/index'
-  end
-
-  post '/feed' do
-    p params[:body]
-    user = current_user
-    Peep.create(body: params[:body], created_at: params[:created_at], user_id: session[:user_id])
-    redirect to '/feed'
-  end
-
-  get '/users/new' do
-    @user = User.new
-    erb :'users/new'
-  end
-
-  post '/users' do
-    @user = User.new(email: params[:email],
-                     name: params[:name],
-                     username: params[:username],
-                     password: params[:password],
-                     password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect to '/feed'
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'users/new'
-    end
-  end
-
-  get '/sessions/new' do
-    erb :'sessions/new'
-  end
-
-  post '/sessions' do
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect to('/feed')
-    else
-      flash.now[:errors] = ['The email or password is incorrect']
-      erb :'sessions/new'
-    end
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    flash.keep[:notice] = 'Goodbye!'
-    redirect to '/feed'
-  end
-
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
-end
+require_relative './controllers/users'
+require_relative './controllers/sessions'
+require_relative './controllers/peeps'
+require_relative 'server'
