0d0bd2978cbeb86595eb43431f09dc656a5996f2
Sam Jones <sam.jones494@googlemail.com>
2017-01-30 08:56:06 +0000

    final

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -5,7 +5,7 @@ class Peep
   property :id,				Serial
   property :user_handle,	String
   property :user_name,		String
-  property :content,		String
+  property :content,		String,  :length => 140
   property :time,			Time
 
   has n, :tags, through: Resource
