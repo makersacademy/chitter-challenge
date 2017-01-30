c080f2993f7d58661a3d17ca584024f7b2b780c6
Shereen Finney <shereenfinney@gmail.com>
2017-01-30 11:55:21 +0000

    Created a chitter_test db

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,8 +3,8 @@ require 'dm-postgres-adapter'
 require 'dm-migrations'
 require_relative 'models/user.rb'
 
-DataMapper.setup(:default, 'postgres://localhost/chitter_development')
-# DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+# DataMapper.setup(:default, 'postgres://localhost/chitter_development')
 # DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
