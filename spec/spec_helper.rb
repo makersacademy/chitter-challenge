6907f42ff5fde3defa9db49c044af192536b0343
Basile Koko <basile_koko@hotmail.com>
2017-01-30 07:46:38 +0000

    modified spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,18 +12,10 @@ SimpleCov.start
 
 
 
-
-# SimpleCov.formatters = [
-#   SimpleCov::Formatter::HTMLFormatter,
-#   Coveralls::SimpleCov::Formatter
-# ]
-# Coveralls.wear!
-
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
-# require 'coveralls'
-# require 'simplecov'
+
 
 
 
