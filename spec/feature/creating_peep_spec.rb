dd5d8c746ac298bb1465c359ef08f00af64ea04c
bvjones <benvaughanjones@gmail.com>
2017-01-30 11:28:00 +0000

    refactoring and tests passing

--- spec/feature/creating_peep_spec.rb
+++ spec/feature/creating_peep_spec.rb
@@ -1,6 +1,15 @@
 feature 'Creating a peep' do
   scenario 'want to be able to post a peep' do
-
+    visit '/sign_up'
+    fill_in :name, with: 'Fred'
+    fill_in :email, with: 'fred@example.com'
+    fill_in :username, with: 'FRED'
+    fill_in :password, with: 'fred123'
+    fill_in :password_confirmation, with: 'fred123'
+    click_button 'submit'
+    visit '/log_in'
+    fill_in :email, with: 'fred@example.com'
+    fill_in :password, with: 'fred123'
     visit '/chitters'
     fill_in 'message', with: 'Hello world'
     click_button 'Post peep'
