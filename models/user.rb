ea799eced59f851bb6db2b65442b45d150f30f30
Matthew Perry <thesedatedprince@gmail.com>
2017-01-30 08:26:49 +0000

    Updated readme

---
+++ models/user.rb
@@ -0,0 +1,26 @@
+require 'bcrypt'
+
+class User
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :email, String
+  property :name, String
+  property :username, String
+
+  property :password_digest, Text
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(username, password)
+  user = first(username: username)
+  if user && BCrypt::Password.new(user.password_digest) == password
+    user
+  else
+    nil
+  end
+  end
+
+end
