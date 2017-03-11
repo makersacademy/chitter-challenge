3078a1e18fbafaedd27a3d077ee7cda35c08d1df
Flora Harvey <fharvey555@gmail.com>
2017-03-11 14:59:51 +0000

    Heroku postgres setup

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -1,3 +1,6 @@
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+require 'data_mapper'
+require 'dm-postgres-adapter'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
