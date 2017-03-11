1814ca795bda48ac1703b7dc176a30fb677c2072
Ashwini <ashie.oh@gmail.com>
2017-03-11 11:39:54 +0000

    Adds a User model with schema, db-level-validation & pwd encryption functions

---
+++ app/models/user.rb
@@ -0,0 +1,31 @@
+require 'bcrypt'
+
+class User
+  include DataMapper::Resource
+
+  property :id,           Serial
+  property :email,        String, :required => true, :format => :email_address, :unique => true,
+  :messages => {
+     :presence  => "We need your email address.",
+     :is_unique => "We already have that email.",
+     :format    => "Doesn't look like an email address to me ..."
+   }
+
+   property :password_salt, Text
+   property :name,          String
+   property :username,      String, :required => true, :unique => true,:length => 5..10,
+   :messages => {
+      :presence  => "We need you to enter a valid username",
+      :is_unique => "We already have that username."
+    }
+
+    attr_accessor :password_confirmation
+    attr_reader :password
+
+    def password=(password)
+      @password = password
+      self.password_salt = BCrypt::Password.create(password)
+    end
+
+    validates_confirmation_of :password, :message => "Password and confirmation password do not match"
+end
