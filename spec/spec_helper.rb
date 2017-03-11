5201884f48cbfe442d500489e5d7f9a092612263
Albert <ayanit1@googlemail.com>
2017-03-11 12:49:06 +0000

    update spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,27 @@
 require 'coveralls'
 require 'simplecov'
+require 'capybara/rspec'
+require_relative './app/app'
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+# ENV['RACK_ENV']='test'
+Capybara.app = App
+
+RSpec.configure do |config|
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
+end
