ac47185f862127ebad2418ae25d344950401927e
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-11 08:06:15 +0000

    deploying to heroku

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require_relative './app/models/dm_config.rb'
 
--- app/models/dm_config.rb
+++ app/models/dm_config.rb
@@ -4,7 +4,7 @@ require 'dm-postgres-adapter'
 require_relative './peep.rb'
 
 def setup
-  DataMapper.setup(:default, "postgres://localhost/chitter_test")
+  DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
