a2f0bf4bc8b93bcffbbaaf905fbdd588aa877459
Ashwini <ashie.oh@gmail.com>
2017-03-11 12:17:42 +0000

    Completes tests for signup feature

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -21,6 +21,7 @@ feature "New user should be able to register/sign up" do
     expect{click_button 'Sign Up'}.to_not change(User, :count)
     expect(page).to have_current_path('/users/new')
     expect(page).to have_content('We need your email address.')
+    expect(page).to have_content('We need you to enter a username')
   end
 
   scenario "doesn't allow user to sign up with invalid email " do
@@ -41,15 +42,28 @@ feature "New user should be able to register/sign up" do
   end
 
   scenario "doesn't allow user to sign up if password and confirm password don't match" do
-
-  end
-
-  scenario "doesn't allow user to sign up without a password of min 4 chars" do
-
+    visit('/users/new')
+    fill_in :email, with: 'test@test.com'
+    fill_in :password, with: 'test1'
+    fill_in :password_confirmation, with: 'test2'
+    fill_in :name, with: 'Elizabeth Windsor'
+    fill_in :username, with: 'ewindsor'
+    expect{click_button 'Sign Up'}.to_not change(User, :count)
+    expect(page).to have_current_path('/users/new')
+    expect(page).to have_content('Password and confirmation password do not match')
   end
 
   scenario "doesn't allow user to sign up without a unique username" do
-
+    User.create(email: 'test@test.com', password: 'test', password_confirmation: 'test', name: 'Ex Name', username: 'ename')
+    visit('/users/new')
+    fill_in :email, with: 'test_next@test.com'
+    fill_in :password, with: 'test'
+    fill_in :password_confirmation, with: 'test'
+    fill_in :name, with: 'Elizabeth Windsor'
+    fill_in :username, with: 'ename'
+    expect{click_button 'Sign Up'}.to_not change(User, :count)
+    expect(page).to have_current_path('/users/new')
+    expect(page).to have_content('We already have that username.')
   end
 
 end
