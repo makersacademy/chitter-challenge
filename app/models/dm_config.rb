7e93b34a982c781599e1e3b52bb75b4638358667
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-11 08:10:46 +0000

    added proc

--- app/models/dm_config.rb
+++ app/models/dm_config.rb
@@ -4,7 +4,7 @@ require 'dm-postgres-adapter'
 require_relative './peep.rb'
 
 def setup
-  DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
   DataMapper.finalize
   DataMapper.auto_upgrade!
 end
