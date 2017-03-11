b776f2ea368cc50dd4e801ce658b5eff314d4870
Bernard Malhame <bernard.malhame@gmail.com>
2017-03-11 17:06:17 +0000

    adds flash notices for sign up errors

--- app/app.rb
+++ app/app.rb
@@ -28,15 +28,17 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(name: params[:name],
+    @user = User.create(name: params[:name],
                         username: params[:username],
                         email: params[:email],
                         password: params[:password],
                         password_confirmation: params[:password_confirmation])
 
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect '/'
+    else
+      flash.now[:errors] = @user.errors.full_messages
     end
   end
 
