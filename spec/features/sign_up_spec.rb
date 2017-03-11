b3f1bf7f2597985dc2f3f7326ffc8d939afa6967
Ashwini <ashie.oh@gmail.com>
2017-03-11 19:25:55 +0000

    Uses the helper method

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,64 +4,48 @@
 
 feature "New user should be able to register/sign up" do
 
+  before (:each) do
+    User.create(email: 'existing@email.com', password: 'test', password_confirmation: 'test', name: 'Ex Name', username: 'ename')
+  end
+
   scenario "allows user to sign up with all the details" do
-    visit('/users/new')
-    fill_in :email, with: 'test@test.com'
-    fill_in :password, with: 'test'
-    fill_in :password_confirmation, with: 'test'
-    fill_in :name, with: 'Elizabeth Windsor'
-    fill_in :username, with: 'ewindsor'
-    expect{click_button 'Sign Up'}.to change(User, :count)
+    sign_up
     expect(page).to have_current_path('/peeps')
     expect(page).to have_content('Welcome, ewindsor')
   end
 
   scenario "doesn't allow user to sign up without email" do
-    visit('/users/new')
-    expect{click_button 'Sign Up'}.to_not change(User, :count)
+    sign_up(email: nil, password: '12345678', password_confirmation: '12345678', name: 'Elizabeth Windsor', username: 'ewindsor')
     expect(page).to have_current_path('/users/new')
     expect(page).to have_content('We need your email address.')
+  end
+
+  scenario "doesn't allow user to sign up without username" do
+    sign_up(email: 'test@test.com', password: '12345678', password_confirmation: '12345678', name: 'Elizabeth Windsor', username: nil)
+    expect(page).to have_current_path('/users/new')
     expect(page).to have_content('We need you to enter a username')
   end
 
   scenario "doesn't allow user to sign up with invalid email " do
-    visit('/users/new')
-    fill_in :email, with: 'invalid@mail'
-    expect{click_button 'Sign Up'}.to_not change(User, :count)
+    sign_up(email: 'test@test', password: '12345678', password_confirmation: '12345678', name: 'Elizabeth Windsor', username: nil)
     expect(page).to have_current_path('/users/new')
     expect(page).to have_content("Doesn't look like an email address to me ...")
   end
 
   scenario "doesn't allow user to sign up with an existing email" do
-    User.create(email: 'test@test.com', password: 'test', password_confirmation: 'test', name: 'Ex Name', username: 'ename')
-    visit('/users/new')
-    fill_in :email, with: 'test@test.com'
-    expect{click_button 'Sign Up'}.to_not change(User, :count)
+    sign_up(email: 'existing@email.com', password: '12345678', password_confirmation: '12345678', name: 'Elizabeth Windsor', username: 'ewindsor')
     expect(page).to have_current_path('/users/new')
     expect(page).to have_content("We already have that email.")
   end
 
   scenario "doesn't allow user to sign up if password and confirm password don't match" do
-    visit('/users/new')
-    fill_in :email, with: 'test@test.com'
-    fill_in :password, with: 'test1'
-    fill_in :password_confirmation, with: 'test2'
-    fill_in :name, with: 'Elizabeth Windsor'
-    fill_in :username, with: 'ewindsor'
-    expect{click_button 'Sign Up'}.to_not change(User, :count)
+    sign_up(email: 'alice@example.com', password: '123456789', password_confirmation: '12345678', name: 'Elizabeth Windsor', username: 'ewindsor')
     expect(page).to have_current_path('/users/new')
     expect(page).to have_content('Password and confirmation password do not match')
   end
 
   scenario "doesn't allow user to sign up without a unique username" do
-    User.create(email: 'test@test.com', password: 'test', password_confirmation: 'test', name: 'Ex Name', username: 'ename')
-    visit('/users/new')
-    fill_in :email, with: 'test_next@test.com'
-    fill_in :password, with: 'test'
-    fill_in :password_confirmation, with: 'test'
-    fill_in :name, with: 'Elizabeth Windsor'
-    fill_in :username, with: 'ename'
-    expect{click_button 'Sign Up'}.to_not change(User, :count)
+    sign_up(email: 'alice@example.com', password: '12345678', password_confirmation: '12345678', name: 'Elizabeth Windsor', username: 'ename')
     expect(page).to have_current_path('/users/new')
     expect(page).to have_content('We already have that username.')
   end
