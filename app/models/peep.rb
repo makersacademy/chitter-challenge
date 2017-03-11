9a6ed2bf64222dc5b915cd619c4e9f7156754210
Ashwini <ashie.oh@gmail.com>
2017-03-11 18:28:30 +0000

    Changs error message for peep length

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -2,7 +2,7 @@ class Peep
   include DataMapper::Resource
 
   property :id,         Serial
-  property :text,       Text, :length => 10..140, :message => "Chars between 10 and 140"
+  property :text,       Text, :length => 10..140, :message => "Please make sure your peep has between 10 and 140 chars"
   property :created_at, DateTime
 
   belongs_to :user
