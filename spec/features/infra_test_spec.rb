51f480d026f4c9d34417ce6b2e16f3468f5b1261
Ashwini <ashie.oh@gmail.com>
2017-03-12 15:03:09 +0000

    Updates test for root route

--- spec/features/infra_test_spec.rb
+++ spec/features/infra_test_spec.rb
@@ -1,8 +1,11 @@
 feature "Root page" do
 
-  scenario "Infra check" do
+  scenario "Should have button to navigate" do
     visit('/')
-    expect(page).to have_content "Hello World"
+    expect(page).to have_selector("input[type=submit][value='View Peeps']")
+    expect(page).to have_selector("input[type=submit][value='Sign Up']")
+    expect(page).to have_selector("input[type=submit][value='Sign In']")
+    expect(page).to have_content "Welcome to Ashwini's Chitter"
   end
 
 end
