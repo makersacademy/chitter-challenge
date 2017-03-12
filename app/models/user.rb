a09e07e296104ce5a6fe24680c9d790263553c92
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-12 17:49:23 +0000

    Add name and username to sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,12 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
+  property :name, String
+  property :username, String
   property :email, String
   property :password, String
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_test")
+DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -6,11 +6,13 @@ feature 'Sign up' do
   scenario 'user can create an account' do
     visit '/'
     click_button 'Sign up'
+    fill_in :name, with: 'Vicky ledsom'
+    fill_in :username, with: 'ledleds'
     fill_in :email, with: 'vicky@makers.co.uk'
     fill_in :password, with: '1234'
     click_button 'Create account'
 
-    User.create(email: 'vicky@makers.co.uk', password: '1234')
+    User.create(name: 'Vicky Ledsom', username: 'ledleds', email: 'vicky@makers.co.uk', password: '1234')
     expect(User.first.email).to eq('vicky@makers.co.uk')
   end
 end
