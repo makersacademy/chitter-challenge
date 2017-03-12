60e77dda528de6637c151e119610a27da083a9be
Vicky Roberts <vicki_uk@hotmail.com>
2017-03-12 22:38:02 +0000

    feature tests amend

--- spec/features/add_hashtag_spec.rb
+++ spec/features/add_hashtag_spec.rb
@@ -1,28 +1,20 @@
-# feature 'Adding hashtags' do
-#   scenario 'I can add a hashtag to my peeps' do
-#
-#     visit '/peeps/add'
-#     fill_in 'peep_content', with: 'Something different for now'
-#     fill_in 'hashtags', with: 'random'
-#     click_button 'Add peep'
-#
-#     peep = Peep.first
-#     expect(peep.hashtags.map(&:name)).to include('random')
-#     expect(current_path).to eq '/peeps'
-#
-#   end
-#
-#   scenario 'I can add multiple hashtags to my peeps' do
-#
-#     visit '/peeps/add'
-#     fill_in 'peep_content', with: 'Christmas pudding in summer?'
-#     fill_in 'hashtags', with: 'food seasonal'
-#     click_button 'Add peep'
-#
-#     peep = Peep.first
-#     expect(peep.hashtags.map(&:name)).to include('food', 'seasonal')
-#     expect(current_path).to eq '/peeps'
-#
-#   end
-#
-# end
+feature 'Adding hashtags' do
+  scenario 'I can add a hashtag to my peeps' do
+    sign_up
+    visit '/peeps/add'
+    fill_in 'peep_content', with: 'Something different for now'
+    fill_in 'hashtags', with: 'random'
+    click_button 'Add peep'
+    expect(current_path).to eq '/peeps'
+
+  end
+
+  # scenario 'I can add multiple hashtags to my peeps' do
+  #   sign_up
+  #   visit '/peeps/add'
+  #   fill_in 'peep_content', with: 'Christmas pudding in summer?'
+  #   fill_in 'hashtags', with: 'food seasonal'
+  #   click_button 'Add peep'
+  # end
+
+end
