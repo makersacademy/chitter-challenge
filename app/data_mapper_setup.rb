fb687c5e42c8edab4e9a69f5c27867c05b9e838d
Vicky Ledsom <victorialedsom@gmail.com>
2017-03-12 23:23:03 +0000

    Add heroku database URL to data_mapper_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,6 +3,6 @@ require 'dm-postgres-adapter'
 require_relative 'app'
 require_relative 'models/user'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
