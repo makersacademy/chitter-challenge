3feb4d0ae20b426303f81b9fa99ccbef9a567ad2
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-19 08:57:07 +0000

    Re-setup on new computer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,13 +43,3 @@ RSpec.configure do |config|
 
   end
 end
-
-  # config.expect_with :rspec do |expectations|
-  #
-  #   expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  # end
-
-  # config.mock_with :rspec do |mocks|
-  #
-  #   mocks.verify_partial_doubles = true
-  # end
