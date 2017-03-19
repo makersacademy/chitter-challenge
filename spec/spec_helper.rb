6399bd06bbf0e88b3829a4dd09474576756723e7
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-19 08:51:13 +0000

    Re-set up chitter on new computer

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,17 +42,14 @@ RSpec.configure do |config|
     DatabaseCleaner.clean
 
   end
-end 
-
-  config.expect_with :rspec do |expectations|
-
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-
-    mocks.verify_partial_doubles = true
-  end
-
-
 end
+
+  # config.expect_with :rspec do |expectations|
+  #
+  #   expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  # end
+
+  # config.mock_with :rspec do |mocks|
+  #
+  #   mocks.verify_partial_doubles = true
+  # end
