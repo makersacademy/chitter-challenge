4d453a6b26887c20be766eeaca65947aeffe0f3c
Ruan Odendaal <ruanodendaal@gmail.com>
2017-04-07 14:22:29 +0100

    refactor tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -12,22 +12,14 @@ feature 'Sign up for Chitter' do
   end
 
   scenario 'do not enter email' do
-    visit '/users/new'
-    fill_in 'password', with: 's3cr3t'
-    fill_in 'password_confirmation', with: 's3cr3t'
-    expect(current_path).to eq '/users/new'
-    click_button 'Sign up'
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
     expect(page).to have_content('Email must not be blank')
-    expect(User.count).to eq 0
   end
 
   scenario 'enter invalid email' do
-    visit '/users/new'
-    fill_in 'email', with: 'hi'
-    fill_in 'password', with: 's3cr3t'
-    fill_in 'password_confirmation', with: 's3cr3t'
-    expect(current_path).to eq '/users/new'
-    click_button 'Sign up'
+    expect { sign_up(email: 'invalid.com') }.not_to change(User, :count)
+    expect(current_path).to eq '/users'
     expect(page).to have_content('Email has an invalid format')
     expect(User.count).to eq 0
   end
