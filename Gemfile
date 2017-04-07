b700a9288cd16c3ab2ff63b21a99b5dad69ac57f
Nick Rupp <nickrupp95@gmail.com>
2017-04-07 20:10:11 +0100

    Set up underlying file structure

--- Gemfile
+++ Gemfile
@@ -1,10 +1,20 @@
 source 'https://rubygems.org'
 ruby '2.4.0'
 
+gem 'bcrypt'
+gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+gem 'database_cleaner'
+gem 'selenium-webdriver'
+gem 'rack'
+gem 'pg'
+gem 'pry'
+gem 'sinatra-flash'
+
 group :test do
   gem 'rspec'
   gem 'cucumber'
   gem 'rake'
   gem 'coveralls', require: false
 end
-
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,42 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'capybara'
+require 'capybara/rspec'
+require 'database_cleaner'
+require 'rspec'
+
+# require './app/models/link'
+require './app/controller'
+require 'features/web_helpers'
+
+Capybara.app = Chitter
+
 require 'coveralls'
 require 'simplecov'
 
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-Coveralls.wear!
+# SimpleCov.formatters = [
+#   SimpleCov::Formatter::HTMLFormatter,
+#   Coveralls::SimpleCov::Formatter
+# ]
+# Coveralls.wear!
+
+
+# require 'simplecov'
+# SimpleCov.start
+
+RSpec.configure do |config|
+
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+  #
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
+  #
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
+
+end
