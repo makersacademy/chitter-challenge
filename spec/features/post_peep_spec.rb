3d6c8b2344b2425fb163b4c17d4f3e16d564da12
Emily Chai <echai93@gmail.com>
2017-04-08 17:08:15 +0100

    Post peep test

---
+++ spec/features/post_peep_spec.rb
@@ -0,0 +1,13 @@
+feature 'Posting peeps' do
+  scenario 'page works' do
+    visit '/peeps/new'
+    expect(page.status_code).to eq 200
+  end
+
+  scenario 'post a new peep' do
+    visit '/peeps/new'
+    fill_in(:peep, with: 'This is my first peep')
+    click_button 'Post'
+    expect(current_path).to eq '/peeps'
+  end
+end
