aff39f1c2765d47331d4c58ba3cfc910a631976b
kittysquee <kittysquee@gmail.com>
2017-04-08 17:27:38 +0100

    Adds sessions/new erb file

--- app/app.rb
+++ app/app.rb
@@ -19,18 +19,39 @@ class Kitter < Sinatra::Base
     erb :'users/new'
   end
 
-  post '/users' do
-    @user = User.create(email: params[:email],
-                      username: params[:username]
-                      password: params[:password],
-                      password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect '/meows'
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'users/new'
-    end
+  # post '/users' do
+  #   @user = User.create(email: params[:email],
+  #                     username: params[:username],
+  #                     password: params[:password],
+  #                     password_confirmation: params[:password_confirmation])
+  #   if @user.save
+  #     session[:user_id] = @user.id
+  #     redirect '/meows'
+  #   else
+  #     flash.now[:errors] = @user.errors.full_messages
+  #     erb :'users/new'
+  #   end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+#   post '/sessions' do
+#   user = User.authenticate(params[:username], params[:password])
+#   if user
+#     session[:user_id] = user.id
+#     redirect '/meows'
+#   else
+#     flash.now[:errors] = ['The username or password is incorrect']
+#     erb :'sessions/new'
+#   end
+# end
+
+# delete '/sessions' do
+#   session[:user_id] = nil
+#   flash.keep[:notice] = 'Goodbye kitty!'
+#   redirect '/meows'
+# end
 
   run! if app_file == $0
 end
