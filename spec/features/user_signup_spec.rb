a6966f912ce90b03d947d08d6026a7baf1d79352
Naz <nazmalik08@gmail.com>
2017-04-08 15:54:01 +0100

    add tests for invalid email/username

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -12,4 +12,23 @@ feature 'signing up users' do
     expect(current_path).to eq '/users'
     expect(page).to have_content 'Password does not match the confirmation'
   end
+
+  scenario 'cant leave email box empty' do
+    expect{signup(email: nil)}.not_to change(User, :count)
+  end
+
+  scenario 'cant give an invalid email address' do
+    expect{signup(email: 'baaaanaaanaaaa')}.not_to change(User, :count)
+  end
+
+  scenario 'cant leave username box empty' do
+    expect{signup(username: nil)}.not_to change(User, :count)
+  end
+
+  scenario 'should prevent duplicate sign up' do
+    signup
+    visit '/users/new'
+    expect{signup}.not_to change(User, :count)
+    expect(page).to have_content 'Email is already taken'
+  end
 end
