bd6e0e343f3934823d03d6d9f72bbf23271af6e7
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-08 12:14:07 +0100

    rack env configured so that database_cleaner doesn't clean the development database

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -11,6 +11,6 @@ class Peep
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_challenge_test")
+DataMapper.setup(:default, "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
