ea1c1bfdcf965a94aab6fd2a9ce08a3511cedd7c
Stephanie Crampin <steph.crampin@gmail.com>
2017-04-08 15:00:53 +0100

    sign in tests passing

--- app/app.rb
+++ app/app.rb
@@ -60,7 +60,7 @@ class Chitter < Sinatra::Base
   post '/sessions' do
     user = User.authenticate(params[:username], params[:password])
   if user
-    session[:user_id] = user.id
+    session[:id] = user.id
     redirect to('/posts')
   else
     flash.now[:errors] = ['The email or password is incorrect']
