9f85e1c07abbd1e3a69eac25804b4d933c209c0b
Naz <nazmalik08@gmail.com>
2017-04-08 15:43:27 +0100

    create layout and flash views to display error messages

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -6,4 +6,10 @@ feature 'signing up users' do
     expect(current_path).to eq '/peeps'
     expect(page).to have_content 'Welcome, HappyFriends99'
   end
+
+  scenario 'mismatching password and password confirmation' do
+    expect{signup(password_confirmation: 'BANANAS!#@$')}.not_to change(User, :count)
+    expect(current_path).to eq '/users'
+    expect(page).to have_content 'Password does not match the confirmation'
+  end
 end
