6cf3c6e542e045f1556d3366830b6ca0b23de670
Eugenia Guerrero <eugenia.guerrero@me.com>
2017-04-09 00:43:36 +0100

    adds instance var to address Travis failure

--- app/app.rb
+++ app/app.rb
@@ -46,9 +46,8 @@ class Chitter < Sinatra::Base
 
   post '/sessions' do
     @user = User.authenticate(params[:email], params[:password])
-
     if @user
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect to('/peeps')
     else
       flash.now[:errors] = ['The email or password is incorrect']
