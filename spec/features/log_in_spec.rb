109879dfc611300e6590dc1e45bccda77e98b99c
emmpak <eiliev05@yahoo.com>
2017-04-08 18:06:49 +0100

    Fix sign up scenarios

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -2,5 +2,8 @@ feature 'log in' do
   scenario 'user can log in' do
     sign_up
     visit '/sessions/new'
+    log_in
+    expect(current_path).to eq '/sessions'
+    expect(page).to have_content 'Logged in as Darth_Vader'
   end
 end
