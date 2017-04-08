29a4543cfdbf39f3dcc030696b449ee90992fe7b
Emily Chai <echai93@gmail.com>
2017-04-08 15:08:21 +0100

    User sign up route

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,31 @@
 ENV['RACK_ENV']||= 'development'
 require 'sinatra/base'
-require_relative 'data_mapper_setup'
 require 'sinatra/flash'
+require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::base
 
-end 
+  enable :sessions
+  set :session_secret, 'super secret'
+  register Sinatra::flash
+
+  # User sign up
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  # Store user details
+  post 'users' do
+    User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect to :'/peeps'
+  end
+
+  # View peeps
+  get '/peeps' do
+    erb :'peeps'
+  end
+
+
+end
