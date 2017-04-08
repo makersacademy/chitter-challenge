c05f8152c91213de02ef75a6dd70e70499d7619e
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-04-08 13:23:41 +0100

    Add sign up

---
+++ spec/helpers/session.rb
@@ -0,0 +1,10 @@
+module SessionHelpers
+
+  def sign_up(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
+end
