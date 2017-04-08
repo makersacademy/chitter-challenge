95272131107d1d33f439bd0f18b97848462f68e6
Naz <nazmalik08@gmail.com>
2017-04-08 16:11:21 +0100

    add sign in fuctionality

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -5,12 +5,12 @@ class Knitter < Sinatra::Base
   end
 
   post '/sessions' do
-    user = User.authenticate(params[:email], params[:password])
+    user = User.authenticate(params[:username], params[:password])
     if user
       session[:user_id] = user.id
       redirect('/peeps')
     else
-      flash.now[:errors] = ['The email or password is incorrect']
+      flash.now[:errors] = ['The username or password is incorrect']
       erb :'/sessions/new'
     end
   end
