9af77103ac08b20b8a8c207b955d33adf5c44a39
alicespyglass <alicespyglass@gmail.com>
2017-04-08 14:10:17 +0100

    bundle update. signup feature test failing

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,12 @@
+feature 'Sign Up' do
+
+  scenario 'displays welcome message when signing up' do
+    visit '/users/new'
+    expect(page.status_code).to eq(200)
+    fill_in('email', with: 'newbie@gmail.com')
+    fill_in('chitter handle', with: 'newbie')
+    click_button('Sign up')
+    expect(User.count).to change by(1)
+  end
+
+end
