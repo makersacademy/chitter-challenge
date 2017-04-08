021bb4b9b69ffd1b9581e1bbc8edaef135c9810a
Naz <nazmalik08@gmail.com>
2017-04-08 12:28:07 +0100

    update spec helper

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../app/models/user'
 
-features 'signing up users' do
+feature 'signing up users' do
   scenario 'user signs up' do
     expect{signup}.to change(User, :count).by 1
     expect(current_path).to eq '/peeps'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,47 @@
+ENV["RACK_ENV"] = 'test'
+
 require 'coveralls'
 require 'simplecov'
+
+require 'database_cleaner'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require './app/app'
+require 'pry'
+# require_relative './features/web_helpers'
+require_relative './helpers/session'
+Capybara.app = Knitter
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
   Coveralls::SimpleCov::Formatter
 ]
 Coveralls.wear!
+
+RSpec.configure do |config|
+  config.include SessionHelpers
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  
+end
