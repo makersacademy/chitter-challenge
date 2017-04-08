3571a079e45dd94b17d65e6bf9d9c7a95cd4f26a
Colin Turner <colinturner34@gmail.com>
2017-04-08 23:51:00 +0100

    User can filter peeps by tag

--- app/app.rb
+++ app/app.rb
@@ -23,4 +23,10 @@ class Chitter < Sinatra::Base
     redirect '/peeps'
   end
 
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @peeps = tag ? tag.peeps : []
+    erb :'peeps/index'
+  end
+
 end
