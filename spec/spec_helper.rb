152a3b41182444b11d153732bde8df564d5a8d13
Colin Turner <colinturner34@gmail.com>
2017-04-08 22:57:22 +0100

    Use Env variables to separate test and development environments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,30 @@
+ENV["RACK_ENV"] = "test"
+
 require 'coveralls'
 require 'simplecov'
 require 'capybara/rspec'
+require 'database_cleaner'
 
 require './app/app'
 require './app/models/peep'
 
 Capybara.app = Chitter
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
 
 SimpleCov.formatters = [
   SimpleCov::Formatter::HTMLFormatter,
