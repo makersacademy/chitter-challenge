7cd1af79a8bc1c81d4dd74e91bcda981d62d56e6
Naz <nazmalik08@gmail.com>
2017-04-08 12:17:05 +0100

    add test that user can sign_up

---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../app/models/user'
+
+features 'signing up users' do
+  scenario 'user signs up' do
+    expect{signup}.to change(User, :count).by 1
+    expect(current_path).to eq '/peeps'
+    expect(page).to have_content 'Welcome to Knitter, happyfriends@treehouse.com'
+  end
+end
