4dd6e57ae4ad997b61f2c4e6ecd79746e306598b
Stephanie Crampin <steph.crampin@gmail.com>
2017-04-08 13:33:51 +0100

    password blank test failing

---
+++ spec/features/signing_in_spec.rb
@@ -0,0 +1,69 @@
+feature 'user uses sign in form' do
+
+  scenario 'successful sign in' do
+    visit '/user/new'
+    fill_in('name', with: 'Steph')
+    fill_in('username', with: 'StephanieJane')
+    fill_in('email', with: 'Steph@stephanie.com')
+    fill_in('password', with: 'steph123')
+    fill_in('password_confirmation', with: 'steph123')
+    click_button 'Submit'
+    expect(page).to have_content 'Welcome Steph!'
+  end
+
+  scenario 'email incorrectly formatted' do
+    visit '/user/new'
+    fill_in('name', with: 'Steph')
+    fill_in('username', with: 'StephanieJane')
+    fill_in('email', with: 'sdf.com')
+    fill_in('password', with: 'steph123')
+    fill_in('password_confirmation', with: 'steph123')
+    expect {click_button('Submit')}.to change(User, :count).by(0)
+  end
+
+  scenario 'email blank' do
+    visit '/user/new'
+    fill_in('name', with: 'Steph')
+    fill_in('username', with: 'StephanieJane')
+    fill_in('password', with: 'steph123')
+    fill_in('password_confirmation', with: 'steph123')
+    expect {click_button('Submit')}.to change(User, :count).by(0)
+  end
+
+  scenario 'name blank' do
+    visit '/user/new'
+    fill_in('username', with: 'StephanieJane')
+    fill_in('email', with: 'sdf@steph.com')
+    fill_in('password', with: 'steph123')
+    fill_in('password_confirmation', with: 'steph123')
+    expect {click_button('Submit')}.to change(User, :count).by(0)
+  end
+
+  scenario 'username blank' do
+    visit '/user/new'
+    fill_in('name', with: 'Steph')
+    fill_in('email', with: 'asd@sdf.com')
+    fill_in('password', with: 'steph123')
+    fill_in('password_confirmation', with: 'steph123')
+    expect {click_button('Submit')}.to change(User, :count).by(0)
+  end
+
+  scenario 'password blank' do
+    visit '/user/new'
+    fill_in('name', with: 'Steph')
+    fill_in('username', with: 'StephanieJane')
+    fill_in('email', with: 'sdf@jgh.com')
+    fill_in('password_confirmation', with: 'steph123')
+    expect {click_button('Submit')}.to change(User, :count).by(0)
+  end
+
+  scenario 'password conformation blank' do
+    visit '/user/new'
+    fill_in('name', with: 'Steph')
+    fill_in('username', with: 'StephanieJane')
+    fill_in('email', with: 'sdf@hjbi.com')
+    fill_in('password', with: 'steph123')
+    expect {click_button('Submit')}.to change(User, :count).by(0)
+  end
+
+end
