8d275173aafb437487fcd51f1519979bfe739294
connie-reinholdsson <connie.reinholdsson@gmail.com>
2017-04-08 12:02:45 +0100

    Updated syntax errors

--- models/user.rb
+++ models/user.rb
@@ -13,6 +13,6 @@ class User
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
