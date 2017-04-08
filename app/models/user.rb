babe3e5b97ec0597a11d7ae988ba0ed046602ec8
Emily Chai <echai93@gmail.com>
2017-04-08 16:30:33 +0100

    DataMapper password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,14 @@ class User
   property :email, String
   property :password_digest, Text
 
+  validates_confirmation_of :password
+  attr_reader :password
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
-  
+
+  attr_accessor :password_confirmation
+
 end
