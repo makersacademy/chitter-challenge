8322ffa3f2715f741cdd68201b2e56c3358dd995
emmpak <eiliev05@yahoo.com>
2017-04-08 13:26:14 +0100

    Update sign up form

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,16 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+
+  get '/users/new' do
+    erb :'users/signup'
+  end
+
+  post '/users' do
+    redirect to '/peeps'
+  end
+
+  get '/peeps' do
+
+  end
 end
