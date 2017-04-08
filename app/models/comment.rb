9bd13ba9ac719f3c11a65a018feb0417c83802eb
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-08 23:20:14 +0100

    all working atm

---
+++ app/models/comment.rb
@@ -0,0 +1,11 @@
+# class Comment
+#
+#   include DataMapper::Resource
+#
+#   property :id, Serial
+#   property :text, Text, required: true
+#   property :time, String
+#
+#   belongs_to :peep
+#
+# end
