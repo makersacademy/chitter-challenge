4959f2888e7f870b653f53dc93418ced67aab7f4
Stephanie Crampin <steph.crampin@gmail.com>
2017-04-08 14:19:20 +0100

    password blank test passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,7 @@ class User
   validates_presence_of :email
   validates_presence_of :name
   validates_presence_of :username
-  validates_presence_of :password_digest
+  validates_presence_of :password
   validates_uniqueness_of(:email)
   validates_uniqueness_of(:username)
 
