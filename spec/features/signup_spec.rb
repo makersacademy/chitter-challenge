e8f0cb5dac493679eb0d7e7e23fc853c8577a57e
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-09 16:13:59 +0100

    cannot signup with duplicate email

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -13,7 +13,7 @@ feature 'User sign up' do
   scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
-    expect(page).to have_content 'Password and confirmation password do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario "I can't sign up without an email address" do
@@ -22,6 +22,12 @@ feature 'User sign up' do
 
   scenario "I can't sign up with an invalid email address" do
     expect { sign_up(email: "isthisanemail?") }.not_to change(User, :count)
+  end
+
+  scenario 'I cannot sign up with an existing email' do
+  sign_up
+  expect { sign_up }.to_not change(User, :count)
+  expect(page).to have_content('Email is already taken')
   end
 
 end
