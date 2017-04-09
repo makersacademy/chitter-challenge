40780ec44fea01cec9e67caaf700ecc070e582dc
Emily Chai <echai93@gmail.com>
2017-04-09 20:56:46 +0100

    Test to post a peep

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -9,5 +9,7 @@ feature 'Posting peeps' do
     fill_in(:peep, with: 'This is my first peep')
     click_button 'Post'
     expect(current_path).to eq '/peeps'
+    expect(page).to have_content 'This is my first peep'
   end
+
 end
