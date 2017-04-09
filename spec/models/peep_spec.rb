882ae2ec82b84f7811b08d5d1c93c55ec11c430f
emmpak <eiliev05@yahoo.com>
2017-04-09 15:19:43 +0100

    Fix test for chronological method

--- spec/models/peep_spec.rb
+++ spec/models/peep_spec.rb
@@ -1,11 +1,17 @@
 require './app/models/peep'
 
 describe Peep do
+  let! (:user) {User.create(first_name: "Darth",
+                           last_name: "Vader",
+                           email: 'darth_vader@gmail.com',
+                           username: "Darth_Vader",
+                           password: "notanakin",
+                           password_confirmation: "notanakin")}
 
   describe '.chronological' do
     it 'returns the peeps in chronological order' do
-      old_peep = Peep.create(experience: "Coast to coast")
-      new_peep = Peep.create(experience: "Surfing with a view")
+      old_peep = Peep.create(experience: "Coast to coast", user: user)
+      new_peep = Peep.create(experience: "Surfing with a view", user: user)
       expect(Peep.chronological).to eq [new_peep, old_peep]
     end
   end
