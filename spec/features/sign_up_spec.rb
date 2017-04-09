f26c25f006b20ed5dd6677eaeb1c273f3c17ce15
Simba14 <seanblundell@gmail.com>
2017-04-09 10:20:23 +0100

    Added error message expectations to sign up tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,6 +8,7 @@ feature 'sign up' do
 
   scenario 'cannot sign up without an email address' do
     expect { sign_up(email: nil) }.not_to change(User, :count)
+    expect(page).to have_content('Email must not be blank')
   end
 
   scenario 'cannot sign up with an invalid email' do
@@ -20,22 +21,26 @@ feature 'sign up' do
     sign_up
     expect { sign_up(username: 'Muggle_Trish') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
+    expect(page).to have_content('Email is already taken')
   end
 
   scenario 'cannot sign up with existing username' do
     sign_up
     expect { sign_up(email: 'Ben@person.com') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
+    expect(page).to have_content('Username is already taken')
   end
 
   scenario 'cannot sign up without username' do
     expect { sign_up(username: nil) }.not_to change(User, :count)
     expect(current_path).to eq('/users')
+    expect(page).to have_content('Username must not be blank')
   end
 
   scenario 'requires a matching confirmation password' do
     expect { sign_up(password_confirmation: 'incorrect') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
+    expect(page).to have_content('Password does not match the confirmation')
   end
 
   def sign_up(email: 'Trisha@person.com',
