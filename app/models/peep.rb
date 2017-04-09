963ec3c50cdd29a40dfda0d5e97d330227eb6e91
emmpak <eiliev05@yahoo.com>
2017-04-09 14:41:54 +0100

    Only members can post experiences

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -4,6 +4,7 @@ class Peep
   property :id,         Serial
   property :experience, String
   property :created_at, DateTime
+  property :created_on, Date
 
   def self.chronological
     Peep.all.reverse
--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,9 +1,15 @@
-feature 'add peep' do
+feature 'adding peep' do
   scenario 'to homepage' do
-    visit('/peeps')
+    sign_up
+    log_in
     fill_in :experience, with: "Life is good"
     click_button 'Share'
     expect(current_path).to eq '/peeps'
     expect(page).to have_content "Life is good"
+  end
+
+  scenario 'is not possible prior to signing in' do
+    visit('/peeps')
+    expect(page).not_to have_css "input#experience"
   end
 end
