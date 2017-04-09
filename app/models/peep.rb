344463462c870c112614ff14d89aa0fda7df4706
Emily Chai <echai93@gmail.com>
2017-04-09 20:56:04 +0100

    DateTime DM property to view time of peep

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -1,10 +1,13 @@
+require 'dm-timestamps'
+
 class Peep
 
   include DataMapper::Resource
 
   has n, :hashtags, through: Resource
-  
+
   property :id, Serial
   property :peep, Text
+  property :created_at, DateTime
 
 end
