ebb3b0fc4fcd8362bf83cc7d193c048e876ff661
Pete Wilkins <pete.p.wilkins@gmail.com>
2017-04-09 17:13:32 +0100

    changed database names to try and pass travis build

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,6 +6,6 @@ require_relative 'models/peep'
 require_relative 'models/hashtag'
 require_relative 'models/user'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
