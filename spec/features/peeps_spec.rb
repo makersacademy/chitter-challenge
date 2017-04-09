aa91eb960cc245cf78b2e3f34afcd97434e08996
emmpak <eiliev05@yahoo.com>
2017-04-09 11:12:51 +0100

    Add time stamp on peeps

--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -1,9 +1,9 @@
 feature 'peeps' do
   scenario 'are displayed to everyone' do
-    time = Time.now.strftime("Posted on %d %B %Y")
+    time = Time.now
     Peep.create(text:"Backpacking SouthEast Asia", created_at: time)
     visit('/peeps')
     expect(Peep.count).to eq 1
-    expect(page).to have_content "Backpacking SouthEast Asia #{time}"
+    expect(page).to have_content "Backpacking SouthEast Asia #{time.strftime('Posted on %d %B %Y')} #{time.strftime("%l:%M %p")}"
   end
 end
