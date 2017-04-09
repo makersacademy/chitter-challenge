80ed238909648d4bdecd6aae370c6cf4f3c1d958
Colin Turner <colinturner34@gmail.com>
2017-04-09 10:28:22 +0100

    Add password confirmation functionality

--- app/app.rb
+++ app/app.rb
@@ -38,7 +38,7 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
+    user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
     redirect '/peeps'
   end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,9 +1,23 @@
-def sign_up
-  visit '/users/new'
-  expect(page.status_code).to eq 200
-  fill_in :name, with: 'Jean Tremblay'
-  fill_in :username, with: 'jt14'
-  fill_in :email, with: 'jean@example.com'
-  fill_in :password, with: 'oranges!'
+# def sign_up
+#   visit '/users/new'
+#   expect(page.status_code).to eq 200
+#   fill_in :name, with: 'Jean Tremblay'
+#   fill_in :username, with: 'jt14'
+#   fill_in :email, with: 'jean@example.com'
+#   fill_in :password, with: 'trout'
+#   click_button 'Sign up'
+# end
+
+def sign_up(name: 'Jean Tremblay',
+                         username: 'jt14',
+                         email: 'jean@example.com',
+                         password:'trout',
+                         password_confirmation: 'trout')
+  visit 'users/new'
+  fill_in :name, with: name
+  fill_in :username, with: username
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
