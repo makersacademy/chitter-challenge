ffcda0e4fb94ffd959a624da0257197f52bd783c
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-09 15:40:08 +0100

    if password confirmation and password don't match, error comes up on signup page

--- app/app.rb
+++ app/app.rb
@@ -4,20 +4,27 @@ require_relative 'models/user'
 
 class Chitter < Sinatra::Base
 
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
 
   get '/users/new' do
-    erb :'users/new'
-  end
+  @user = User.new
+  erb :'users/new'
+end
 
   post '/users' do
-  user = User.create(email: params[:email],
-                     password: params[:password],
-                     password_confirmation: params[:password_confirmation])
-  session[:user_id] = user.id
-  redirect to('/peeps/peeps')
-  end
+  @user = User.new(email: params[:email],
+                  password: params[:password],
+                  password_confirmation: params[:password_confirmation])
+    if @user.save
+     session[:user_id] = @user.id
+     redirect to('/peeps/peeps')
+   else
+     flash.now[:notice] = "Password and confirmation password do not match"
+     erb :'users/new'
+   end
+end
 
   get '/peeps/peeps' do
     erb :'peeps/peeps'
