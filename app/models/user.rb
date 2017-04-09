26fe18a2e8406d70c193c2ce6df88b1b7f4a3b4a
Jack Bittiner <jackbittiner@hotmail.com>
2017-04-09 15:53:37 +0100

    User can't signup up with an empty email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User
   include DataMapper::Resource
 
   property :id,    Serial
-  property :email, String
+  property :email, String, required: true
   property :password_digest, Text
 
   def password=(password)
--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,4 +1,5 @@
 feature 'User sign up' do
+
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, jackdopeyman@hotmail.com')
@@ -7,12 +8,16 @@ feature 'User sign up' do
 
   scenario 'requires a matching confirmation password' do
    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
- end
+  end
 
- scenario 'with a password that does not match' do
-  expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-  expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
-  expect(page).to have_content 'Password and confirmation password do not match'
-end
+  scenario 'with a password that does not match' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
+    expect(page).to have_content 'Password and confirmation password do not match'
+  end
+
+  scenario "I can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
 
 end
