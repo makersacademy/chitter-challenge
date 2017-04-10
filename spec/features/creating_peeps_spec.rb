9679fe12f5454fac0a1d38bef20a4e3b44c9b9be
Simba14 <seanblundell@gmail.com>
2017-04-10 11:10:26 +0100

    refactored

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -13,23 +13,4 @@ feature 'User creates peeps' do
     end
   end
 
-  scenario 'multiple peeps created' do
-    user = User.create(name: 'tester',
-                       username: 'tester99',
-                       email: 'test@test.com',
-                       password: 'test',
-                       password_confirmation: 'test')
-
-    Peep.create(content: "I see trees of green",
-                created_at: Time.now)
-    Peep.create(content: "Red roses too",
-                created_at: Time.now)
-    Peep.create(content: "The circle",
-                created_at: Time.now)
-
-    expect(Peep.count).to eq(3)
-    visit '/peeps'
-    expect(page).to have_content("I see trees of green")
-  end
-
 end
