5c9c7682d55ba1a9015669cb3e528310bcf70160
emmpak <eiliev05@yahoo.com>
2017-04-10 07:19:34 +0100

    Fix sharing a peep

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -5,8 +5,10 @@ feature 'adding peep' do
     fill_in :experience, with: "Life is good"
     click_button 'Share'
     expect(current_path).to eq '/peeps'
-    expect(page).to have_content "Life is good"
-    within('.peep') { expect(page).to have_content 'Darth_Vader'}
+    within('.peep') do
+      expect(page).to have_content 'Darth_Vader'
+      expect(page).to have_content "Life is good"
+    end
   end
 
   scenario 'is not possible prior to signing in' do
