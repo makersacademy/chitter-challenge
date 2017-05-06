1389162d5d74ad46b7b4911d2965eefb953f0ffd
Timothy Robinson <timmyrobby@gmail.com>
2017-05-06 23:17:41 +0100

    Name, username and email now persists after incorrect passwords

--- app/app.rb
+++ app/app.rb
@@ -12,24 +12,31 @@ class Chitter < Sinatra::Base
   end
 
   post '/signup' do
-    user = User.create(name: params[:name], username: params[:username],
+    @user = User.create(name: params[:name], username: params[:username],
                        email: params[:email], password: params[:password],
                        password_confirmation: params[:confirm_password])
-    session[:user_id] = user.id
-    flash[:signup_error] = user.id ? '' : 'Passwords do not match.'
-    redirect '/signup' unless user.id
-    redirect '/home'
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/home'
+    else
+      flash.now[:signup_error] = 'Passwords do not match.'
+      erb(:signup)
+    end
   end
 
   get '/signup' do
+    @user = User.new
     erb(:signup)
   end
 
   get '/home' do
-    "Welcome to Chitter, #{User.get(session[:user_id]).name}"
+    current_user
+    "Welcome to Chitter, #{@user.name}"
   end
 
   helpers do
-
+    def current_user
+      @user ||= User.get(session[:user_id])
+    end
   end
 end
