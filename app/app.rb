3189767b7aa9fe432a25dfd5da9d93efc65dd921
Sophie Mann <sklm@live.co.uk>
2017-05-06 16:59:05 +0100

    adds comment post route

--- app/app.rb
+++ app/app.rb
@@ -66,4 +66,14 @@ class Chitter < Sinatra::Base
     redirect to ('/posts')
   end
 
+  post '/comments' do
+    comment = Comment.first_or_create(comment: params[:comment], user: current_user, created_at: Time.now, post_id: params[:post_id])
+    post = Post.get(params[:post_id])
+    post.comments << comment
+    post.save
+    current_user.comments << comment
+    current_user.save
+    redirect to ('/posts')
+  end
+
 end
