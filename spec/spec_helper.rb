ca6b2ca7ee007ca9cad3c872b1db05e8378c13d6
Anthony Crisp <anthony_crisp@hotmail.com>
2017-05-06 18:19:32 +0100

    first commit

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
@@ -8,7 +15,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
+Capybara.app = Chitter
+
 RSpec.configure do |config|
+  config.include Capybara::DSL
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
