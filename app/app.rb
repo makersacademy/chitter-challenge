845415291e43b9e360db2646b3df35f98ea0089f
Robert Jones <robert.g.jones@gmail.com>
2017-05-07 00:11:51 +0100

    Configure app to use RACK_ENV in dev and test environments

--- app/app.rb
+++ app/app.rb
@@ -1,3 +1,5 @@
+ENV['RACK_ENV'] ||= 'development' 
+
 require_relative 'data_mapper_setup'
 require 'sinatra/base'
 
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,7 +4,7 @@ require 'dm-postgres-adapter'
 require_relative './models/user'
 
 
-DataMapper.setup(:default, 'postgres://localhost/chitter_test')
+DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 
 DataMapper.finalize
 
