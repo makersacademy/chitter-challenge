839c780bbadde16b713ed07f12b534ecb4cee8eb
Robert Jones <robert.g.jones@gmail.com>
2017-05-06 14:05:17 +0100

    Create task README.md with preliminary install instructions and user stories

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
