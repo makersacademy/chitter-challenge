e1a7dc3d0d9213f86776e26ecd543d464fdc726a
Filipe Fasolin <ffasolin@gmail.com>
2017-05-06 15:34:47 +0100

    Sets test/dev env and databases

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,8 @@
+ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
+require 'data_mapper'
+require_relative './models/user'
+require_relative './models/data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
@@ -6,7 +10,14 @@ class Chitter < Sinatra::Base
     erb(:signup_login)
   end
 
+  post '/' do
+    @user = User.first_or_create(name: params[:user_name],
+          email: params[:user_email], password: params[:password])
+    redirect '/feed'
+  end
+
   get '/feed' do
+    @user = User.first
     erb(:feed)
   end
 end
