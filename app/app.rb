c00b25ae377e5a892412ea6db001d2a7d8d187a4
Sophie Mann <sklm@live.co.uk>
2017-05-06 17:38:00 +0100

    refactors controllers

--- app/app.rb
+++ app/app.rb
@@ -5,75 +5,15 @@ require 'sinatra/flash'
 require 'sinatra/partial'
 require_relative 'data_mapper_setup'
 require_relative 'server'
+require_relative 'controllers/posts'
+require_relative 'controllers/sessions'
+require_relative 'controllers/users'
+require_relative 'controllers/comments'
 
 class Chitter < Sinatra::Base
 
   get '/' do
     erb :'index'
-  end
-
-  get '/users/new' do
-    erb :'users/new'
-  end
-
-  post '/users/new' do
-    user = User.new(name: params[:name], username: params[:username],
-    email: params[:email], password: params[:password],
-    password_confirmation: params[:password_confirmation])
-    if user.save
-      session[:user_id] = user.id
-      redirect to('/')
-    else
-      flash.now[:errors] = user.errors.full_messages
-      erb :'users/new'
-    end
-  end
-
-  get '/sessions/new' do
-    erb :'sessions/new'
-  end
-
-  post '/sessions/new' do
-    user = User.authenticate(params[:username], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect to('/posts')
-    else
-      flash.now[:notice] = "Username or password incorrect"
-      erb :'/sessions/new'
-    end
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    flash.next[:notice] = "Goodbye!"
-    redirect to '/'
-  end
-
-  get '/posts' do
-    @posts = Post.all
-    erb :'posts/index'
-  end
-
-  get '/posts/new' do
-    erb :'posts/new'
-  end
-
-  post '/posts' do
-    post = Post.first_or_create(message: params[:message], user: current_user, created_at: Time.now)
-    current_user.posts << post
-    current_user.save
-    redirect to ('/posts')
-  end
-
-  post '/comments' do
-    comment = Comment.first_or_create(comment: params[:comment], user: current_user, created_at: Time.now, post_id: params[:post_id])
-    post = Post.get(params[:post_id])
-    post.comments << comment
-    post.save
-    current_user.comments << comment
-    current_user.save
-    redirect to ('/posts')
   end
 
 end
---
+++ app/controllers/posts.rb
@@ -0,0 +1,19 @@
+class Chitter < Sinatra::Base
+
+  get '/posts' do
+    @posts = Post.all
+    erb :'posts/index'
+  end
+
+  get '/posts/new' do
+    erb :'posts/new'
+  end
+
+  post '/posts' do
+    post = Post.first_or_create(message: params[:message], user: current_user, created_at: Time.now)
+    current_user.posts << post
+    current_user.save
+    redirect to ('/posts')
+  end
+
+end
