be1d1e06a2002aa0bdc2f3c6d8e0dfb8b8e0019e
bruxelles86 <w_gant@yahoo.co.uk>
2017-05-06 23:25:00 +0100

    Puts auto_upgrade back in data_mapper_setup as rakefile not working on heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,4 +4,4 @@ require 'dm-postgres-adapter'
 require_relative 'models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
-DataMapper.finalize
+DataMapper.finalize.auto_upgrade!
