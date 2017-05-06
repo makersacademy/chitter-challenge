1fa2df48e68b4e3bc102624ca2653e26f6bbb7f4
Adam Snow <adamjohnsnow@icloud.com>
2017-05-06 19:07:02 +0100

    trying to fix heroku bug

--- app.rb
+++ app.rb
@@ -58,7 +58,7 @@ class Chitter < Sinatra::Base
   post '/log_reply' do
     @user_id = session[:user_id]
     @peep_id = session[:peep_id]
-    Reply.create(user_id: @user_id, text: params[:reply], peep_id: @peep_id, date_time: DateTime.now)
+    Reply.create(user_id: @user_id, text: "test", peep_id: @peep_id, date_time: DateTime.now)
     link = "/"
     redirect link
   end
