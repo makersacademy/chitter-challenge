5792a287449a78329fcb799fb0f61d30968f6204
Sophie Mann <sklm@live.co.uk>
2017-05-06 13:51:46 +0100

    extracts server details to server file

--- app/app.rb
+++ app/app.rb
@@ -4,22 +4,9 @@ require 'sinatra/base'
 require 'sinatra/flash'
 require 'sinatra/partial'
 require_relative 'data_mapper_setup'
+require_relative 'server'
 
 class Chitter < Sinatra::Base
-
-  register Sinatra::Flash
-  register Sinatra::Partial
-  use Rack::MethodOverride
-  set :partial_template_engine, :erb
-  enable :partial_underscores
-  enable :sessions
-  set :session_secret, 'super secret'
-
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
 
   get '/' do
     erb :'index'
