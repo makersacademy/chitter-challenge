897243884e580d0e6b45e84165be38554ba32ff0
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-06 12:44:40 +0100

    Sign up requires unique email address and username

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User
 
   property :id,            Serial
   property :name,          String
-  property :username,      String
-  property :email,         String
+  property :username,      String, required: true, unique: true
+  property :email,         String, required: true, unique: true
   property :password_digest, Text
 
   def password=(password)
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,6 +1,12 @@
-feature 'Sign up to Chitter' do
-  scenario 'User submits form to sign up to Chitter' do
+feature 'Sign up' do
+  scenario 'I can sign up to Chitter' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(User.first.email).to eq('jessica@email.com')
   end
+
+  scenario 'I must sign up with a unique email and username' do
+    sign_up
+    expect { sign_up }.not_to change(User, :count)
+  end
+
 end
