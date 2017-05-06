aee5c369bba67814b0c5035adffa3955d502e2c9
Alex Chalk <alexchalk17@gmail.com>
2017-05-06 14:37:51 -0400

    User password: Encrypt (salted hash) using bcrypt

--- lib/user.rb
+++ lib/user.rb
@@ -1,11 +1,17 @@
+require 'bcrypt'
+
 class User
 
   include DataMapper::Resource
+  include BCrypt
 
   property :id, 		Serial
   property :email_address, 	String
-  property :password, 		String
   property :user_name, 		String
   property :real_name, 		String
+  property :password_digest, 	Text
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+  end
 
 end
