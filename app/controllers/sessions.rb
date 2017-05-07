07f26f4269df8e1a22ac277cd5fd425ce7fac01c
Paula <pmuldoon@umich.edu>
2017-05-07 15:20:28 +0100

    deleted welcome.erb, replaced with partial

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -7,7 +7,7 @@ class Chitter < Sinatra::Base
     user = User.authenticate(params[:username], params[:password])
     if user
       session[:user_id] = user.id
-      redirect to ('/welcome')
+      redirect to ('/')
     else
       flash.now[:errors] = ['The username or password is incorrect']
       erb :'sessions/new'
--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -12,7 +12,7 @@ class Chitter < Sinatra::Base
     @user = User.create(username: params[:username], email: params[:email], name: params[:name], password: params[:password], password_confirmation: params[:password_confirmation])
     if @user.save
       session[:user_id] = @user.id
-      redirect to ('/welcome')
+      redirect to ('/')
     else
       flash.now[:errors] = @user.errors.full_messages
       erb :'/users/new'
