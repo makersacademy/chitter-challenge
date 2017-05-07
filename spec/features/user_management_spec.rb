28f6f64b97010232a72b4ed7c73fc54737f01df0
milesillsley <milesillsley@hotmail.co.uk>
2017-05-07 16:19:37 +0100

    add tests for password confirmation"

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,10 +1,16 @@
 require 'web_helper'
 
 feature 'User sign up' do
+
   scenario 'user can sign up for a Chitter account' do
     expect { sign_up }.to change(User, :count).by 1
     expect(page).to have_content('Welcome, chitmeister49')
     expect(User.first.email).to eq('chittychitty@peeppeep.com')
     expect(User.first.name).to eq('John')
   end
+
+  scenario 'requires  a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'worng') }.not_to change(User, :count)
+  end
+
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,10 +1,15 @@
-def sign_up
+def sign_up(email: 'chittychitty@peeppeep.com',
+            name: 'John',
+            username: 'chitmeister49',
+            password: 'peep247',
+            password_confirmation: 'peep247')
   visit '/'
   click_link 'Sign up'
   expect(page.status_code).to eq(200)
-  fill_in :email, with: 'chittychitty@peeppeep.com'
-  fill_in :name, with: 'John'
-  fill_in :username, with: 'chitmeister49'
-  fill_in :password, with: 'peep247'
+  fill_in :email, with: email
+  fill_in :name, with: name
+  fill_in :username, with: username
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
