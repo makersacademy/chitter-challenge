9f7dcdedb54c7a40b21c19689c9c4d674fb28373
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-07 17:29:19 +0100

    Prevents duplicate registrations using DataMapper error methods -Updates Sinatra flash notice to flash error in controller

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,11 +8,17 @@ feature 'User sign up' do
   scenario 'requires a matching confirmation password' do
     expect { incorrect_sign_up }.not_to change(User, :count)
     expect(current_path).to eq('/users')
-    expect(page).to have_content 'Passwords do not match'
+    expect(page).to have_content 'Password does not match the confirmation'
   end
 
   scenario "I can't sign up without an email address" do
     expect { email_field_nil }.not_to change(User, :count)
+  end
+
+  scenario 'I cannot sign up with an existing email' do
+    sign_up
+    expect { sign_up }.to_not change(User, :count)
+    expect(page).to have_content('Email is already taken')
   end
 
 end
