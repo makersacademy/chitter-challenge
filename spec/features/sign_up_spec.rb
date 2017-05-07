54a546e8626ca1e335026fe293bad9a5d1d4360d
Paula <pmuldoon@umich.edu>
2017-05-07 10:39:44 +0100

    changed names in spec sign_up

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,8 +3,9 @@ feature '#sign_up' do
   scenario 'user signs up to Chitter' do
     sign_up
     expect { sign_up }.to change(User, :count).by(1)
-    expect(page).to have_content("Welcome to Chitter, Spock.")
-    expect(User.first.username).to eq("spock45")
+    p User, :count
+    expect(page).to have_content("Welcome to Chitter, Deanna Troi.")
+    expect(User.first.username).to eq("troi91")
   end
 
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -2,10 +2,10 @@ def sign_up
   visit '/'
   expect(page.status_code).to eq(200)
   click_button "Create new account"
-  fill_in :username, with: "spock45"
-  fill_in :email, with: "spock@starfleet.com"
-  fill_in :name, with: "Spock"
-  fill_in :password, with: "l0gicaL"
-  fill_in :password_confirmation, with: "l0gicaL"
+  fill_in :username, with: "troi99"
+  fill_in :email, with: "dtroi@starfleet.com"
+  fill_in :name, with: "Deanna Troi"
+  fill_in :password, with: "imzadi"
+  fill_in :password_confirmation, with: "imzadi"
   click_button 'Sign up'
 end
