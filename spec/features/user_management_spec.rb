3b81b9064ad772f21cb6ef729853eb871de100bb
isabellarner <larner.isabel@gmail.com>
2017-05-07 11:33:48 +0100

    user login

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,7 +3,7 @@ require_relative 'web_helper'
 feature 'Signing up' do
   scenario 'I can sign up for an account' do
     expect { sign_up }. to change(User, :count).by(1)
-    expect(page).to have_content('Welcome, ilarne')
+    expect(page).to have_content('Welcome, izzy@example.com')
     expect(User.first.email).to eq('izzy@example.com')
   end
 end
