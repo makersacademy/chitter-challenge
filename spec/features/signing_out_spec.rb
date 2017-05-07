f1bae6361ebaf38d1b3b5939c35b33b0885da978
Timothy Robinson <timmyrobby@gmail.com>
2017-05-07 13:02:49 +0100

    CSS with Bootstrap covers most pages

--- spec/features/signing_out_spec.rb
+++ spec/features/signing_out_spec.rb
@@ -1,13 +1,13 @@
 feature 'Signing out' do
   scenario 'Users can sign up successfully' do
     signs_in_successfully
-    click_button 'Sign out'
+    click_button 'Log out'
     expect(page).to have_content 'You logged out!'
   end
 
   scenario 'Users are presented with options after signing out' do
     signs_in_successfully
-    click_button 'Sign out'
+    click_button 'Log out'
     expect(page).to have_button 'Sign up to Chitter!'
     expect(page).to have_button 'Log in'
   end
