1ee7ac80a3ce17025c04a858403c2ef38c7a0b9d
Slaiman Ahmadshah <sulaimanahmad321@gmail.com>
2017-05-07 17:48:10 +0100

    Creates heroku app

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,6 @@ require_relative 'models/peep'
 require_relative 'models/user'
 require 'sinatra/flash'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
