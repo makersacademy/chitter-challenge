248d564c81c600a7ee07e0315c795d7a41661781
milesillsley <milesillsley@hotmail.co.uk>
2017-05-07 17:07:35 +0100

    Add persisting sign up form details

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -10,7 +10,7 @@ feature 'User sign up' do
   end
 
   scenario 'requires a matching confirmation password' do
-    expect { sign_up(password_confirmation: 'worng') }.not_to change(User, :count)
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
     expect(page).to have_content 'Password and confirmation password do not match'
   end
