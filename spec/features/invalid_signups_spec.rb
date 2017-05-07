b211505f9da002065879bb6f44782b902fbc4cd8
Timothy Robinson <timmyrobby@gmail.com>
2017-05-07 14:21:34 +0100

    Satisfies rubocop using slightly different testing syntax

--- spec/features/invalid_signups_spec.rb
+++ spec/features/invalid_signups_spec.rb
@@ -1,17 +1,17 @@
 feature 'User cannot sign up with invalid information' do
   scenario 'User cannot sign in without email address' do
-    expect { signs_in_without_email }.not_to change { User.count }
+    expect { signs_in_without_email }.to change { User.count }.by(0)
     expect(page).to have_content 'Email must not be blank'
   end
 
   scenario 'User cannot sign in with invalid email address' do
-    expect { signs_in_with_bad_email }.not_to change { User.count }
+    expect { signs_in_with_bad_email }.to change { User.count }.by(0)
     expect(page).to have_content 'Email has an invalid format'
   end
 
   scenario 'User cannot sign up with existing information' do
     signs_in_successfully
-    expect { signs_in_successfully }.not_to change { User.count }
+    expect { signs_in_successfully }.to change { User.count }.by(0)
     expect(page).to have_content 'Email is already taken'
     expect(page).to have_content 'Username is already taken'
   end
