53cae541f9b03bd9fe334c18108175f530c74fde
BenNoonan1991 <ben.noonan2@gmail.com>
2017-05-07 19:32:21 +0100

    Adds and passes first test in user_management spec

--- Gemfile
+++ Gemfile
@@ -1,7 +1,12 @@
 source 'https://rubygems.org'
 ruby '2.4.0'
 
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 gem 'sinatra'
+gem 'rack'
+gem 'bcrypt'
+gem 'database_cleaner'
 
 group :test do
   gem 'cucumber'
--- app/app.rb
+++ app/app.rb
@@ -1,5 +1,31 @@
+ENV["RACK_ENV"] = "development"
+
 require 'sinatra/base'
+require_relative 'models/user'
 
-class Chitter < Sinatra::base
+class Chitter < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
-end 
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
+  get '/layout' do
+    erb :'layout'
+  end
+
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    User.create(email: params[:email],
+                password: params[:password])
+    redirect to('/layout')
+  end
+
+end
