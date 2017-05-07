9c2370da3af9158509718a1648c245fbbc8d604f
Paula <pmuldoon@umich.edu>
2017-05-07 11:54:16 +0100

    added sign in button to index.erb

--- app/app.rb
+++ app/app.rb
@@ -45,7 +45,7 @@ class Chitter < Sinatra::Base
       session[:user_id] = user.id
       redirect to ('/welcome')
     else
-      flash.now[:errors] = ['The email or password is incorrect']
+      flash.now[:errors] = ['The username or password is incorrect']
       erb :'sessions/new'
     end
   end
