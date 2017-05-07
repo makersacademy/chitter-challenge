cab23e73dc1e292fd09d68fda4e208259d2aea5c
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-07 19:18:07 +0100

    Changes order of requirements in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,21 @@
-ENV['RACK_ENV'] = 'test'
-require File.join(File.dirname(__FILE__), '..', './app/app.rb')
-require 'database_cleaner'
-require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
-require './app/data_mapper_setup'
-require_relative 'web_helpers'
-require_relative 'helpers/session'
-
-Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov::Formatter::Console,
 SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+ENV['RACK_ENV'] = 'test'
+require File.join(File.dirname(__FILE__), '..', './app/app.rb')
+require 'database_cleaner'
+require 'capybara/rspec'
+require './app/data_mapper_setup'
+require_relative 'web_helpers'
+require_relative 'helpers/session'
+
+Capybara.app = Chitter
 
 RSpec.configure do |config|
   config.include SessionHelpers
