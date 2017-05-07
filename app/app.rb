33095219bf63e98f9252f2c8926db42fdb9e5869
isabellarner <larner.isabel@gmail.com>
2017-05-07 13:03:47 +0100

    sign out WIP

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,7 @@ ENV["RACK_ENV"] ||= "development"
 require 'sinatra/base'
 require_relative '../data_mapper_setup'
 require 'sinatra/flash'
+require 'rack'
 
 class Chitter < Sinatra::Base
   enable :sessions
