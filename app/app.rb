6b8bcb776d11d38a9712cfca5e794777cbb58da0
Chris Mitchell <chrisjmit@gmail.com>
2017-05-07 19:52:20 +0100

    sign in added

--- app/app.rb
+++ app/app.rb
@@ -46,4 +46,19 @@ class Chitter < Sinatra::Base
     erb :'users/new'
   end
 
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect to('/users')
+    else
+      flash.now[:errors] = ['Your email or password is incorrect']
+      erb :'sessions/new'
+    end
+  end
+
 end
