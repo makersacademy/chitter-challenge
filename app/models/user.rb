a7a1efecd2f28b76268ae0e3f60feea04fb2b2a2
isabellarner <larner.isabel@gmail.com>
2017-05-07 09:11:32 +0100

    edit homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'bcrypt'
+
 class User
     include DataMapper::Resource
 
@@ -7,4 +9,6 @@ class User
     property :email,    String
     property :password_digest, Text
 
+def password(=password)
+  self.password_digest = BCrypt::Password.create(password)
   end
