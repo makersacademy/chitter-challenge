b6d2e64c1b78e3bd1b144b6827591fedda75c70e
Tim Hawkins <timothyhaydenhawkins@gmail.com>
2017-05-07 13:27:26 +0100

    adds web helper

--- spec/features/sign_up_form_spec.rb
+++ spec/features/sign_up_form_spec.rb
@@ -1,22 +1,14 @@
 feature 'Sign Up Form' do
 
   scenario 'User can enter details into the sign up form' do
-    visit('/users/new')
-    fill_in('name', :with => 'Tim Hawkins')
-    fill_in('username', :with => 'therealtimhawkins')
-    fill_in('email', :with => 'timhawkins@gmail.com')
-    fill_in('password', :with => 'cliveallen')
+    enter_details
     click_button 'Submit Details'
 
     expect(page).to have_content 'Welcome to Chitter Tim Hawkins!'
   end
 
   scenario 'Requires a matching confirmation password from user' do
-    visit('/users/new')
-    fill_in('name', :with => 'Tim Hawkins')
-    fill_in('username', :with => 'therealtimhawkins')
-    fill_in('email', :with => 'timhawkins@gmail.com')
-    fill_in('password', :with => 'cliveallen')
+    enter_details
     fill_in('confirm_password', :with => 'cliveallen')
 
   expect { click_button 'Submit Details' }.not_to change(User, :count)
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,1 +1,7 @@
-
+def enter_details
+  visit('/users/new')
+  fill_in('name', :with => 'Tim Hawkins')
+  fill_in('username', :with => 'therealtimhawkins')
+  fill_in('email', :with => 'timhawkins@gmail.com')
+  fill_in('password', :with => 'cliveallen')
+end
