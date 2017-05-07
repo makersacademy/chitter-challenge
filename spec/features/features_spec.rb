c76e33b64fb602f6d074235ead9fdd2b7de9bd1b
bruxelles86 <w_gant@yahoo.co.uk>
2017-05-07 17:17:59 +0100

    Adds failing feature test for sign out

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,4 +1,3 @@
-
 # This relies on 'sign up', a helper method set out in web_helper.rb
 feature 'allows users to register new accounts' do
   scenario 'user can sign up with e-mail, password, name and username' do
@@ -11,12 +10,11 @@ feature 'allows users to register new accounts' do
 end
 
 feature 'User sign in' do
-
   # This gives us a 'memoized' helper method, with user equal to the return value of .create (the new object)
-  let!(:user) do
-    User.create(email: 'user@example.com',
-    password: 'secret1234')
-  end
+    let!(:user) do
+      User.create(email: 'user@example.com',
+      password: 'secret1234')
+    end
   # This uses keyword arguments (ruby v2.0 and later only) to accept the parameters, working like a hash
   scenario 'with correct credentials' do
     DatabaseCleaner.start
@@ -25,12 +23,15 @@ feature 'User sign in' do
     DatabaseCleaner.clean
   end
 
-  # As of ruby v2.1 no default arguments required (e.g. "email: "blah@blah.com"")
-  def sign_in(email:, password:)
-    visit '/sessions/new'
-    fill_in :email, with: email
-    fill_in :password, with: password
-    click_button 'Sign in'
+end
+
+feature 'User sign out' do
+  scenario 'shows "You have logged out" after clicking \'sign out\' button' do
+    DatabaseCleaner.start
+    sign_up
+    click_button 'Sign out'
+    expect(page).to have_content "You have signed out"
+    DatabaseCleaner.clean
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,3 +32,11 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+# As of ruby v2.1 no default arguments required (e.g. "email: "blah@blah.com"")
+def sign_in(email:, password:)
+  visit '/sessions/new'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  click_button 'Sign in'
+end
