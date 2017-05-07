a3e4df242c40df9ea403d535f11a2a20c7f627a2
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-05-07 19:16:04 +0100

    Reformats to pass Rubocop

--- spec/helpers/session.rb
+++ spec/helpers/session.rb
@@ -1,15 +1,15 @@
 module SessionHelpers
 
- def sign_in(email:, password:)
-   visit '/sessions/new'
-   fill_in :email, with: email
-   fill_in :password, with: password
-   click_button 'Sign in'
- end
+  def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
 
- def sign_up(email: 'alice@example.com',
-             password: '12345678',
-             password_confirmation: '12345678')
+  def sign_up(email: 'alice@example.com',
+              password: '12345678',
+              password_confirmation: '12345678')
     visit '/users/new'
     fill_in :name, with: 'example'
     fill_in :username, with: 'example123'
@@ -17,6 +17,6 @@ module SessionHelpers
     fill_in :password, with: 'example'
     fill_in :password_confirmation, with: 'example'
     click_button 'Sign up'
- end
+  end
 
 end
