a93ee9310ad7fda901028366fda52dbf81fd5698
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-08 02:20:24 +0100

    green release: datamapper_setup file working correctly

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source 'https://rubygems.org'
 ruby '2.4.0'
 
   gem 'sinatra'
-  gem 'datamapper'
+  gem 'data_mapper'
+  gem 'dm-postgres-adapter'
 
 group :test do
   gem 'cucumber'
--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -1,4 +1,8 @@
-require 'datamapper'
-# require 'dm-postgres-adapter'
+require 'data_mapper'
+require 'dm-postgres-adapter'
 require_relative 'models/peep'
 require_relative 'models/user'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
