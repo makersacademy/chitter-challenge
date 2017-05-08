5d9a975bc87013677d0ceb565d10245891bcbae2
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-05-08 02:33:14 +0100

    wrote failing test for user_log_in feature

--- spec/features/user_log_in_spec.rb
+++ spec/features/user_log_in_spec.rb
@@ -1,6 +1,10 @@
 feature 'user log up' do
   scenario 'user can log in to the website' do
     sign_up    
-    expect(page).to have_content 'Hi axcochrane! You have successfully signed up!'
+    visit 'user/login'
+    fill_in 'username', with: 'axcochrane'
+    fill_in 'password', with: 'test123'
+    click_on 'Log in!'
+    expect(page).to have_content 'Hey axcochrane! You have logged in!'
   end
 end
