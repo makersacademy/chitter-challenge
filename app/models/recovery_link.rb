7591ec83bc0d7472a78e9bb9beab1bb8fc0ce0a8
isabellarner <larner.isabel@gmail.com>
2017-05-08 10:45:42 +0100

    mailing token WIP

--- app/models/recovery_link.rb
+++ app/models/recovery_link.rb
@@ -2,7 +2,7 @@ require 'mailgun'
 class RecoveryLink
 
  def initialize(mailer: nil)
-   @mailer = mailer || Mailgun::Client.new(ENV["your_api_key"])
+   @mailer = mailer || Mailgun::Client.new(ENV["key-5d43c10a85ab55067bfcb9053c3fea24"])
  end
 
  def self.call(user, mailer = nil)
@@ -10,7 +10,7 @@ class RecoveryLink
  end
 
  def call(user)
-   mailer.send_message(ENV["mailgun_domain_name"], {from: "bookmarkmanager@mail.com",
+   mailer.send_message(ENV["mailgun_domain_name"], {from: "chitter@mail.com",
        to: user.email,
        subject: "reset your password",
        text: "click here to reset your password http://ilarne-chitter.heroku.com/reset_password?token=#{user.password_token}" })
