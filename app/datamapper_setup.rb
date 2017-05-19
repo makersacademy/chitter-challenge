7167d131ef833872fe02d123dee52968e6339acb
Freddy Fallon <freddyfallon@gmail.com>
2017-05-19 11:56:45 +0100

    Update README

--- app/datamapper_setup.rb
+++ app/datamapper_setup.rb
@@ -5,6 +5,6 @@ require 'dm-validations'
 require_relative 'models/user'
 require_relative 'models/peep'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV["RACK_ENV"]}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
