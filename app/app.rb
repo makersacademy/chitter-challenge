986d7f74531c763fcb403bebd134e672776ef06b
wemmm <jenny.wem@gmail.com>
2017-06-02 23:08:00 +0100

    Passed first feature test
    - Added /views folder
    - Added get_peep route to controller
    - Created corresponding erb file

--- app/app.rb
+++ app/app.rb
@@ -2,4 +2,8 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
 
+  get '/add_peep' do
+    erb :get_peep
+  end
+
 end
--- spec/feature/type_a_peep_spec.rb
+++ spec/feature/type_a_peep_spec.rb
@@ -1,7 +1,7 @@
 feature 'Peep entry' do
   scenario 'user has option to enter a peep' do
     visit('/add_peep')
-    expect(page).to have_field 'peep'
+    expect(page).to have_field 'peep_content'
     expect(page).to have_button 'Submit'
   end
 end
