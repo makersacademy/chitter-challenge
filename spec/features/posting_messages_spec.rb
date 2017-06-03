fdf55aeb393eecad0b022553489cadce2fe18f91
Kynosaur <kye.bracey@gmail.com>
2017-06-03 13:59:16 +0100

    Updates peeps.erb to show peeps in reverse chronological order

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -10,10 +10,11 @@ feature "Posting messages (peeps)" do
   scenario "User posts two messages and sees them in reverse chronological order" do
     visit "/"
     click_button "Post new Peep"
-    fill_in("peep_body", with: "First!")
+    fill_in("peep_body", with: "Second!")
     click_button "Submit"
     click_button "Post new Peep"
-    fill_in("peep_body", with: "Fir- oh, never mind :(")
-    expect(page).to have_content("Fir- oh, never mind :( First!")
+    fill_in("peep_body", with: "Seco- oh, never mind :(")
+    click_button "Submit"
+    expect(page).to have_content("Seco- oh, never mind :( Second!")
   end
 end
