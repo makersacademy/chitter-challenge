2ee92c5b2f72bdd47c11c2be06ff820c821270da
bannastre <bannastre@gmail.com>
2017-06-03 18:38:03 +0100

    Update datamapper_config for heroku

--- app/datamapper_config.rb
+++ app/datamapper_config.rb
@@ -3,6 +3,6 @@ require 'dm-postgres-adapter'
 
 require_relative 'models/message.rb'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
