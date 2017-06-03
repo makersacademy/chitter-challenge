bb45cbbdbb113a7f75dba90c1d32b0c06767d764
Panteha <panteha.ahmadi@gmail.com>
2017-06-03 19:02:20 +0100

    Updated code

--- spec/features/post_a_message_spec.rb
+++ spec/features/post_a_message_spec.rb
@@ -22,4 +22,14 @@ feature 'Post message' do
     expect(first_idx).to be > second_idx
   end
 
+  scenario 'User can see the time at which the message was made' do
+    visit('/messages/new')
+    fill_in 'content', with: 'peep'
+    click_button 'Send message'
+    expect(page.status_code).to eq 200
+    expect(page).to have_content 'peep'
+  end
+
+
+
 end
