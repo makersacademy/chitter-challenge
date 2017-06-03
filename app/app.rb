14d5f0a6dc0d6d0d8ac9411c4121f14107cda4b2
Panteha <panteha.ahmadi@gmail.com>
2017-06-03 18:15:14 +0100

    Implemented second feature test

--- app/app.rb
+++ app/app.rb
@@ -5,17 +5,18 @@ class Chitter < Sinatra::Base
 
   get '/messages' do
     @messages = Message.all
+
     erb :index
   end
 
   get '/messages/new' do
-    #message = Message.all
     erb :post_message
   end
 
   post '/messages' do
     message = Message.create(content: params[:content])
     message.save
+    puts "****#{message}****"
     redirect '/messages'
   end
 
--- spec/features/post_a_message_spec.rb
+++ spec/features/post_a_message_spec.rb
@@ -1,5 +1,6 @@
 
 feature 'Post message' do
+
   scenario 'User is able to post a message to chitter' do
     visit('/messages/new')
     fill_in 'content', with: 'peep'
@@ -7,4 +8,18 @@ feature 'Post message' do
     expect(page.status_code).to eq 200
     expect(page).to have_content 'peep'
   end
+
+  scenario 'User is able to see messages in reverse chronological order' do
+    visit('/messages/new')
+    fill_in 'content', with: 'first message'
+    click_button 'Send message'
+    visit('/messages/new')
+    fill_in 'content', with: 'second message'
+    click_button 'Send message'
+    visit('/messages')
+    first_idx = page.body.index('first message')
+    second_idx = page.body.index('second message')
+    expect(first_idx).to be > second_idx
+  end
+
 end
