1fd07c4b7284c466d3a65a9b9b76e6f394cc17ba
Panteha <panteha.ahmadi@gmail.com>
2017-06-03 17:14:11 +0100

    User can post a message to chitter/tested

--- app/app.rb
+++ app/app.rb
@@ -3,9 +3,20 @@ require './app/datamapper_setup'
 
 class Chitter < Sinatra::Base
 
+  get '/messages' do
+    @messages = Message.all
+    erb :index
+  end
+
   get '/messages/new' do
-    message = Message.all
+    #message = Message.all
     erb :post_message
+  end
+
+  post '/messages' do
+    message = Message.create(content: params[:content])
+    message.save
+    redirect '/messages'
   end
 
 end
