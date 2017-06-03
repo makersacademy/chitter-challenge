75178569f514907983665a9910d391d3f20291ac
Kynosaur <kye.bracey@gmail.com>
2017-06-03 13:40:16 +0100

    Adds feature test for posting two peeps and seeing them in reverse chronological order

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -1,9 +1,19 @@
 feature "Posting messages (peeps)" do
   scenario "User can post a Peep to Chitter" do
-    visit '/'
-    click_button 'Post new Peep'
-    fill_in('peep_body', with: 'First!')
-    click_button 'Submit'
-    expect(page).to have_content('First!')
+    visit "/"
+    click_button "Post new Peep"
+    fill_in("peep_body", with: "First!")
+    click_button "Submit"
+    expect(page).to have_content("First!")
+  end
+
+  scenario "User posts two messages and sees them in reverse chronological order" do
+    visit "/"
+    click_button "Post new Peep"
+    fill_in("peep_body", with: "First!")
+    click_button "Submit"
+    click_button "Post new Peep"
+    fill_in("peep_body", with: "Fir- oh, never mind :(")
+    expect(page).to have_content("Fir- oh, never mind :( First!")
   end
 end
