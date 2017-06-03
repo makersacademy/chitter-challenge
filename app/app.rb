07b04e9228a09a0dc663b9294a8b287c031103fd
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-03 15:02:29 +0100

    Updates controller with get route for peeps and peeps index erb file

--- app/app.rb
+++ app/app.rb
@@ -3,4 +3,13 @@ require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
+  get '/' do
+    redirects to '/peeps'
+  end
+
+  get '/peeps' do
+    @peeps = Peep.all
+    erb :'peeps/index'
+  end
+
 end
