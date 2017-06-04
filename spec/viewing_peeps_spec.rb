6a1599537c6c14ab315f2e945c20683da0d89f91
Aaron <aaronrod@gmail.com>
2017-06-04 19:05:47 +0100

    Added feature test for user story 3 and it passes!

--- spec/viewing_peeps_spec.rb
+++ spec/viewing_peeps_spec.rb
@@ -15,3 +15,20 @@ feature 'Viewing peeps' do
     end
   end
 end
+# User Story 3.
+# As a Maker
+# So that I can better appreciate the context of a peep
+# I want to see the time at which it was made
+feature 'Timestamp' do
+  scenario 'See the time at which peep was made' do
+    visit '/peeps/new'
+    fill_in 'peep', with: 'Double McTwist 1260!'
+    time = Time.now.to_s.delete(' ').insert(10,'T').insert(-3,':')
+    click_button 'Create peep'
+    expect(current_path).to eq '/peeps'
+
+    within 'ul#peeps' do
+      expect(page).to have_content(time)
+    end
+  end
+end
