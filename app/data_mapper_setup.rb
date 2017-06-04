534da664fd276151f9201103a43ce0e5773a2c28
charliefea <charlotte.feather@yahoo.com>
2017-06-04 11:32:09 +0100

    Remove line greater than 100 char

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -4,7 +4,9 @@ require 'dm-postgres-adapter'
 require_relative 'models/peep'
 require_relative 'models/user'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
+databasepath = "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}"
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || databasepath)
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
