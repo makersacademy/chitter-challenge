c684d485cd8991b9b0ed2a49f47abb1b06006377
wemmm <jenny.wem@gmail.com>
2017-06-04 10:50:07 +0100

    Included DataMapper gems and added new feature test for seeing a submitted peep

---
+++ spec/feature/see_peeps_spec.rb
@@ -0,0 +1,9 @@
+feature 'Peep Visibility' do
+  scenario 'user can post and then see a peep' do
+    visit('/add_peep')
+    expect(page.status_code).to eq 200
+    fill_in "peep_content", with: "an experimental test peep"
+    click_button('Submit')
+    expect(page).to have_content("an experimental test peep")
+  end
+end
--- spec/feature/type_a_peep_spec.rb
+++ spec/feature/type_a_peep_spec.rb
@@ -1,4 +1,4 @@
-feature 'Peep entry' do
+feature 'Peep Entry' do
   scenario 'user has option to enter a peep' do
     visit('/add_peep')
     expect(page).to have_field 'peep_content'
