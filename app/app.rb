bd6a839af5493cad03833fc16b1de1404aa2e36d
Dom <dom.mcdonnell@gmail.com>
2017-06-04 21:19:28 +0100

    - User can now sign in and out. - Update some names/files for clarity. - Feature tests and not user spec tests passing.

--- app/app.rb
+++ app/app.rb
@@ -57,6 +57,21 @@ class Chitter < Sinatra::Base
     erb :'peeps/peeps_index'
   end
 
+  get '/sessions/new' do
+    erb :'sessions/new_session'
+  end
+
+  post '/sessions' do
+  user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect to('/peeps')
+    else
+      flash.now[:errors] = ['The email or password is incorrect']
+      erb :'sessions/new_session'
+    end
+  end
+
   run! if app_file == $0
 
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -15,11 +15,18 @@ def sign_up(email: 'john@doe.com',
   click_button 'Sign up'
 end
 
-def sign_in(email: 'john@doe.com',
-            password: 'password')
-  visit '/'
-  expect(page.status_code).to eq(200)
-  fill_in 'email', with: email
-  fill_in 'password', with: password
-  click_button 'Sign In'
+# def sign_in(email: 'john@doe.com',
+#             password: 'password')
+#   visit '/'
+#   expect(page.status_code).to eq(200)
+#   fill_in 'email', with: email
+#   fill_in 'password', with: password
+#   click_button 'Sign In'
+# end
+
+def sign_in(email:, password:)
+  visit '/sessions/new'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  click_button 'Sign in'
 end
