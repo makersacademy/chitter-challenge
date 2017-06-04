eb9f7fd8ee1778db3bd7fcb042d4de2a2dfcfc69
Aaron <aaronrod@gmail.com>
2017-06-04 16:12:03 +0100

    Set up a development database and environment so that peeps are not wiped after RSpec is run.

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -2,7 +2,6 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require 'dm-timestamps'
 
-
 class Peep
   include DataMapper::Resource
 
@@ -13,6 +12,6 @@ class Peep
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_test")
+DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
