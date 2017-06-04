f0d271bd28247fd6e316cdc9f30947f322af8351
Aaron <aaronrod@gmail.com>
2017-06-04 12:54:22 +0100

    Added User story comment on top of viewing peeps feature test

--- spec/viewing_peeps_spec.rb
+++ spec/viewing_peeps_spec.rb
@@ -1,3 +1,8 @@
+# User story 2,
+# As a maker
+# So that I can see what others are saying
+# I want to see all peeps in reverse chronological order
+
 feature 'Viewing peeps' do
   scenario 'I can see existing links on the links page' do
     Peep.create(post: 'Makers Academy Rocks', user: 'George')
