36518153a2553413ff2f45f4c977e4144d289189
Colin Scally <colin.scally@gmail.com>
2017-06-04 12:56:03 +0100

    - Configure for heroku

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -11,6 +11,6 @@ class Peep
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV["DATABASE_URL"] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
