5de6292c28d6ee98f9608dc3cfef539927192e67
Aaron <aaronrod@gmail.com>
2017-06-04 19:29:28 +0100

    Added comment for matching Time formats in feature test for Timestamp

--- spec/viewing_peeps_spec.rb
+++ spec/viewing_peeps_spec.rb
@@ -23,7 +23,8 @@ feature 'Timestamp' do
   scenario 'See the time at which peep was made' do
     visit '/peeps/new'
     fill_in 'peep', with: 'Double McTwist 1260!'
-    time = Time.now.to_s.delete(' ').insert(10,'T').insert(-3,':')
+    # This variable is used to match the current time format with that of DataMapper
+    time = Time.now.to_s.delete(' ').insert(10, 'T').insert(-3, ':')
     click_button 'Create peep'
     expect(current_path).to eq '/peeps'
 
