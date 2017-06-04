c5451c741113aaac48ac7afd29269840de1a9a4c
Kavita <kkavita92@gmail.com>
2017-06-04 16:53:33 +0100

    Incorporate Sinatra flash for invalid sign up

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,12 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   helpers do
     def current_user
@@ -18,13 +20,18 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(email: params[:email],
+    user = User.new(email: params[:email],
              name: params[:name],
              username: params[:username],
              password: params[:password],
              password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect('/posts/all')
+    if user.save
+      session[:user_id] = user.id
+      redirect('/posts/all')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb(:'users/new')
+    end
   end
 
   get '/posts/all' do
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -7,6 +7,8 @@ feature 'user sign up' do
 
   scenario 'sign up requires matching confirmation password' do
     expect { wrong_sign_up }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 
 end
