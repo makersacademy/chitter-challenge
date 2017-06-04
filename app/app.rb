eb5677ce7b24553d7abc39664e095ca9bb2a16dd
Kavita <kkavita92@gmail.com>
2017-06-04 09:55:46 +0100

    Allow user to create new post and view posts on feed

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
+require_relative 'models/peep'
 
 class Chitter < Sinatra::Base
 
-  get '/posts' do
-    erb(:posts)
+  get '/posts/all' do
+    @peeps = Peep.all
+    erb(:'posts/all')
+  end
+
+  post '/posts/new' do
+    peep = Peep.create(content: params[:new_peep])
+    redirect('/posts/all')
+  end
+
+  get '/posts/new' do
+    erb(:'posts/new')
   end
 
 end
--- spec/making_posts_spec.rb
+++ spec/making_posts_spec.rb
@@ -1,7 +1,9 @@
 feature 'make peeps on Chitter' do
-  scenario 'user can post messages' do
-    Peep.create(content: 'Hello world!')
-    visit('/posts')
-    expect(page).to have_content 'Hello world!'
+  scenario 'user can make a new post' do
+    visit('/posts/new')
+     fill_in('new_peep', :with => 'Hello world!')
+     click_button('PEEP')
+     expect(current_path).to eq('/posts/all')
+     expect(page).to have_content 'Hello world!'
   end
 end
