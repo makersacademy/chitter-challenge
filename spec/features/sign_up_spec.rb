5834c3bed5d2a04d0d9c0476f611279d64eec72c
Kynosaur <kye.bracey@gmail.com>
2017-06-04 19:48:37 +0100

    Removes test for not creating User when field is missing.  Adds 'required' attribute to all sign_up fields, which apparently capybara tests ignore so can't be tested

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -7,12 +7,4 @@ feature "Signing up for Chitter" do
     sign_up
     expect(page).to have_content("Welcome to Chitter, Kynosaur!")
   end
-
-  scenario "User leaves a field empty upon signing up and account is not created" do
-    visit "sign_up"
-    fill_in("username", with: "Kynosaur")
-    fill_in("password", with: "amazing_password")
-    fill_in("email", with: "kynosnore@gmail.com")
-    expect{ click_button("Submit") }.to change{ User.count }.by(0)
-  end
 end
