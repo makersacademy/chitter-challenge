bcdff42d699d1ebc3ec20eb3dc94b45d0ba200aa
Kavita <kkavita92@gmail.com>
2017-06-04 16:58:00 +0100

    Improve sign up workflow

--- app/app.rb
+++ app/app.rb
@@ -16,17 +16,18 @@ class Chitter < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb(:'users/new')
   end
 
   post '/users' do
-    user = User.new(email: params[:email],
+    @user = User.new(email: params[:email],
              name: params[:name],
              username: params[:username],
              password: params[:password],
              password_confirmation: params[:password_confirmation])
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect('/posts/all')
     else
       flash.now[:notice] = "Password and confirmation password do not match"
