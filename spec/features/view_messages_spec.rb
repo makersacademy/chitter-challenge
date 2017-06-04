673ed48eae9c2b1eb9b29d8fc51104b16679a9ec
bannastre <bannastre@gmail.com>
2017-06-04 10:39:06 +0100

    Refactor web_helper and view_message tests

--- spec/features/view_messages_spec.rb
+++ spec/features/view_messages_spec.rb
@@ -1,14 +1,19 @@
 feature 'users can see multiple messages in reverse chronological order' do
   scenario 'users can see multiple messages on the notice board' do
-    Message.create(message: 'I also love tartan scarves #ScarvesForFoxesToo')
-    expect { post_new_message }.to change(Message, :count).by 1
-    expect(page).to have_content '#ScarvesForFoxesToo'
+    post_new_message
+    expect { post_different_mesage }.to change(Message, :count).by 1
+    expect(page).to have_content 'first_message'
+    expect(page).to have_content 'different_message'
   end
 
   scenario 'messages appear with a time-stamp' do
-    time = Time.now
-    Message.create(message: 'Go Badgers #BadgersBadgersBadgers')
-    visit '/messages'
-    expect(page).to have_content time.strftime("%Y-%m-%dT%H:%M:%S")
+    post_new_message
+    expect(page).to have_content Time.now.strftime("%Y-%m-%dT%H:%M:%S")
+  end
+
+  scenario 'messages appear in chronological order' do
+    post_new_message
+    post_different_mesage
+    expect(page).to have_css('ul#messages li:first-child', text: 'different_message')
   end
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,6 +1,11 @@
 def post_new_message
   visit '/messages/new'
   fill_in 'user_name', with: 'Rupert'
-  fill_in 'message', with: 'I do love this tartan scarf #ScarfsForBears'
+  fill_in 'message', with: 'I do love this tartan scarf #ScarfsForBears #my_first_message'
   click_button 'Peep'
+end
+
+def post_different_mesage
+  Message.create(message: 'different_message', created_at: Time.now + 5)
+  visit '/messages'
 end
