b901ae52f0302ee1835060c4543ede13bd6f7ad7
wemmm <jenny.wem@gmail.com>
2017-06-04 18:46:16 +0100

    Added feature test for user sign up

---
+++ spec/feature/chitter_sign_up_spec.rb
@@ -0,0 +1,12 @@
+feature 'User Sign Up' do
+  scenario 'user can sign up with a username, name, email address and password' do
+    visit('/sign_up')
+    fill_in 'username', with: 'wemmm'
+    fill_in 'name', with: 'Jenny'
+    fill_in 'email', with: 'jenny@wem.com'
+    fill_in 'password', with: '1234'
+    click_button('submit')
+    expect(page).to have_content "Hi Jenny! Welcome to Chitter!"
+    expect(User.count).to eq 1
+  end
+end
