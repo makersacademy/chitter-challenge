e4ac8e189926279d1a74fa2cbc7a248fd5ed1793
Charlotte Fereday <cbrfereday@gmail.com>
2017-06-04 11:00:15 +0100

    Updates developer environment alongside test environment to project

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,7 +2,6 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative 'models/peep'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_test")
-# TODO arrange so goes to developer environment not just test 
+DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
