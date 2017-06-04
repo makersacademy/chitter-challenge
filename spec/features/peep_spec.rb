4eb24b53ee9f8c995cb83fffb604042709ed69b4
Prabu Sathan <prabu.sathan@gmail.com>
2017-06-04 22:03:41 +0100

    Peeps are displayed in reverse chronological order.Tests passing.

--- spec/features/peep_spec.rb
+++ spec/features/peep_spec.rb
@@ -1,8 +1,20 @@
 feature 'Peeps' do
    scenario 'Send a new peep' do
-     visit '/peeps/new'
-     fill_in 'message', with: 'BEER'
-     click_button 'Send'
-     expect(page).to have_content 'BEER'
+     visit('/')
+     click_button('New Peep')
+     fill_in('message', with: 'BEER')
+     click_button('Send')
+     expect(page).to have_content('BEER')
+   end
+
+   scenario 'Peeps are displayed in reverse chronological order' do
+     visit('/')
+     click_button('New Peep')
+     fill_in('message', with: 'beer for a buzz')
+     click_button('Send')
+     click_button('New Peep')
+     fill_in('message', with: 'vodka for a smash')
+     click_button('Send')
+     expect(page).to have_content('vodka for a smash beer for a buzz')
    end
  end
