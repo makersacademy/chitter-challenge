949444cc1891727ca6b39439dcbe9da86285e72d
Kynosaur <kye.bracey@gmail.com>
2017-06-04 14:09:05 +0100

    Removes 'Post new peep' button from homepage (button is present in layout)

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -1,5 +1,6 @@
 feature "Posting messages (peeps)" do
   scenario "User can post a message to Chitter" do
+    sign_up
     visit "/"
     click_button "Post new Peep"
     fill_in("message_body", with: "First!")
@@ -8,6 +9,7 @@ feature "Posting messages (peeps)" do
   end
 
   scenario "User posts two messages and sees them in reverse chronological order" do
+    sign_up
     visit "/"
     click_button "Post new Peep"
     fill_in("message_body", with: "Second!")
@@ -17,5 +19,12 @@ feature "Posting messages (peeps)" do
     click_button "Submit"
     expect(page.find('li:nth-child(1)')).to have_content("Seco- oh, never mind :(")
     expect(page.find('li:nth-child(2)')).to have_content("Second!")
+  end
+
+  scenario "User cannot post a message without being logged in" do
+    visit "/"
+    click_button "Post new Peep"
+    expect(page).not_to have_field("message_body")
+    expect(page).to have_content("Sorry, you must be logged in to post a Peep")
   end
 end
