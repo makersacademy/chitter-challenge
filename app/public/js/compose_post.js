470c98192143a1e1c8a9456140b051be48383126
Michael Jacobson <michael.jacobson89@gmail.com>
2017-06-07 18:41:28 +0100

    • added close button for post lightbox

--- app/public/js/compose_post.js
+++ app/public/js/compose_post.js
@@ -1,6 +1,9 @@
 $(document).ready(function(){
     $("body").on("click", "#peep_link", function(event) {
         event.preventDefault();
-        $('.lightbox').css('visibility', 'visible');
+        $(".lightbox").css("visibility", "visible");
+        $("#close_lightbox").click(function() {
+            $(".lightbox").css("visibility", "hidden");
+        });
     });
 });
