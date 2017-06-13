a9f99f734af4f091097b33f52bfa62f1004860d5
Michael Jacobson <michael.jacobson89@gmail.com>
2017-06-13 01:03:59 +0100

    • begin to implement ability for users to upload photos from their local storage

--- app/models/image_uploader.rb
+++ app/models/image_uploader.rb
@@ -4,8 +4,8 @@ require 'carrierwave/datamapper'
 CarrierWave.configure do |config|
   config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV[:AWS_ACCESS_KEY_ID],
-      aws_secret_access_key: ENV[:AWS_SECRET_ACCESS_KEY],
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region: 'eu-west-2'
   }
   config.fog_directory = 'tinyblogger'
