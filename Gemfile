966e895ee113f933be4f3adeffc3f86db19a8445
alwinruby <alwinsolanky@gmail.com>
2017-06-15 20:45:18 +0100

    rebooting to the start

--- Gemfile
+++ Gemfile
@@ -1,18 +1,11 @@
 source 'https://rubygems.org'
-ruby '2.2.3'
+ruby '2.4.0'
 
-gem 'data_mapper'
-gem 'dm-postgres-adapter'
-gem 'bcrypt'
-gem 'sinatra-flash'
-
-group :test, :development do
-  gem 'rspec'
+group :test do
   gem 'cucumber'
+  gem 'rspec'
   gem 'rake'
-  gem 'coveralls', require: false
-  gem 'sinatra'
-  gem 'rspec-sinatra'
-  gem 'capybara'
-  gem 'database_cleaner'
+  gem 'rubocop'
+  gem 'simplecov', require: false
+  gem 'simplecov-console', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,53 +1,17 @@
-require 'coveralls'
 require 'simplecov'
+require 'simplecov-console'
 
-SimpleCov.formatters = [
-  SimpleCov::Formatter::HTMLFormatter,
-  Coveralls::SimpleCov::Formatter
-]
-Coveralls.wear!
-
-ENV['RACK_ENV'] = 'test'
-
-require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
-
-require 'capybara'
-require 'capybara/rspec'
-require 'rspec'
-require './app/app'
-require 'database_cleaner'
-require_relative 'helpers/session'
-
-Capybara.app = Chitter
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 RSpec.configure do |config|
-  config.include Capybara::DSL
-  config.include SessionHelpers
-
-  config.expect_with :rspec do |expectations|
-
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  config.after(:suite) do
+    puts
+    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
-
-  config.mock_with :rspec do |mocks|
-
-    mocks.verify_partial_doubles = true
-  end
-
-  RSpec.configure do |config|
-    config.before(:suite) do
-      DatabaseCleaner.strategy = :transaction
-      DatabaseCleaner.clean_with(:truncation)
-    end
-
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
-
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
-
-  end
-  
 end
