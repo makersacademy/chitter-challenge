7ff843ba5a53e26cadb455e65b3851ac72be46d7
jinimcoroneo <jinim1983@gmail.com>
2017-07-02 17:14:13 +0100

    update datamapper configuration

--- app/init.rb
+++ app/init.rb
@@ -3,6 +3,6 @@ require 'dm-postgres-adapter'
 
 require './app/models/peep.rb'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_test")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
