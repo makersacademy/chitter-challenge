7bb068ff3e04b2481c2e0b4092635ee66f2e7dae
Alistair Kung <alistaircckung@gmail.com>
2017-07-02 22:27:20 +0100

    completed sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,13 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String
+  property :email, String, required: true, unique: true
   property :password_digest, Text
   attr_reader :password
   attr_accessor :password_confirmation
 
   validates_confirmation_of :password
+  validates_format_of :email, as: :email_address
 
   def password=(password)
     @password = password
