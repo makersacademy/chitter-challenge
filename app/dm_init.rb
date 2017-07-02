bd9e170afbf4803aa972515d3e292afdc14d7e02
Corina <fluturelcorina@gmail.com>
2017-07-02 23:20:41 +0100

    Setting up heroku

--- app/dm_init.rb
+++ app/dm_init.rb
@@ -4,6 +4,6 @@ require 'dm-postgres-adapter'
 require './app/models/peep'
 require './app/models/user'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default,  ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
