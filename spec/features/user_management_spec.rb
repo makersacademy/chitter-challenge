7927ed9890ead21303e2a4031ed109a07b0644e5
Alistair Kung <alistaircckung@gmail.com>
2017-07-02 21:58:45 +0100

    implemented password confirmations

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -4,4 +4,9 @@ feature 'adding a user' do
     expect(page).to have_content('Welcome johnsmith@aol.com')
     expect(User.first.email).to eq('johnsmith@aol.com')
   end
+
+  scenario 'requires a matching confirmation password' do
+    expect {sign_up(password_confirmation: 'wrong')}.not_to change(User, :count)
+  end
+  
 end
--- spec/helpers/sessions.rb
+++ spec/helpers/sessions.rb
@@ -1,7 +1,8 @@
-def sign_up
+def sign_up(email: "johnsmith@aol.com", password: "password123", password_confirmation: "password123")
   visit '/users/new'
   expect(page.status_code).to eq(200)
-  fill_in :email,    with: "johnsmith@aol.com"
-  fill_in :password, with: "password123"
+  fill_in :email,    with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
