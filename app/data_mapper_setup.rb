e07f7c6c49caf6d04edcfd23b8cb040a45b3c118
Simon Hempenstall <simonjhemp@hotmail.com>
2017-07-02 20:11:48 +0100

    Fix heroku setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,6 @@ require_relative 'models/peep'
 require_relative 'models/user'
 require 'dm-timestamps'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
