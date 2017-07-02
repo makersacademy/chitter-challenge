479e5bfb43fb4d1868b871529ca4ffaa02360938
Miho Tse <mihotse@gmail.com>
2017-07-02 11:22:26 +0100

    Change timestamps

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,9 @@ require_relative 'models/peep'
 require_relative 'models/tag'
 require_relative 'models/user'
 
+t = DateTime.now
+t.strftime("%b %d %Y %I:%M %p ")
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
--- app/models/peep.rb
+++ app/models/peep.rb
@@ -2,7 +2,7 @@ class Peep
   include DataMapper::Resource
 
   has n, :tags, through: Resource
-  
+
   property :id, Serial
   property :message, String
   property :created_at, DateTime
