848d374e2f6d7974aa126f4926c9de29c12f0148
jinimcoroneo <jinim1983@gmail.com>
2017-07-03 00:42:50 +0100

    must sign up to peep

--- spec/feature/sign_up_spec.rb
+++ spec/feature/sign_up_spec.rb
@@ -5,3 +5,19 @@ feature 'user can sign up' do
     expect(User.first.email).to eq('john@gmail.com')
   end
 end
+
+feature 'user can sign up' do
+  scenario 'I can\'t peep if I haven\'t signed up' do
+    visit('/chitter-home')
+    click_button('peep!')
+    expect(page).to have_content('Sign up for Chitter')
+  end
+end
+
+feature 'user can sign up' do
+  scenario 'I can peep if I have signed up' do
+    sign_up
+    click_button('peep!')
+    expect(page).to have_content('Peep your friends')
+  end
+end
