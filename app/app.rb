71d8bf962a0e020d4e8351a3fca286f58b3c9e09
annalaise <annalaise.gibbons@gmail.com>
2017-07-29 18:45:06 +0100

    foundations of styling added

---
+++ app/app.rb
@@ -0,0 +1,17 @@
+ENV['RACK_ENV'] ||= 'development'
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require 'sinatra/base'
+require 'sinatra/flash'
+
+class Chitter < Sinatra::Base
+  register Sinatra::Flash
+  enable :sessions
+  set :session_secret, 'super secret'
+
+  get '/' do
+    erb :index
+  end
+
+  run! if app_file == $0
+end
