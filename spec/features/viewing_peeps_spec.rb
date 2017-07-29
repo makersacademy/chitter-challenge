00ab2b083b243c8c6c12af4334a1954c1108b0a5
Imogen Hardy <imogen.hardy@gmail.com>
2017-07-29 19:18:22 +0100

    improved tests to account for multiple users

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -10,15 +10,18 @@ feature "Viewing peeps" do
   before :each do
     sign_up
     peep_1
+    sign_up(username: "penfold", email: "hamster@dmhideout.com",
+            password: "crumbs", password_confirmation: "crumbs")
+    peep_2
   end
 
   scenario "I want to see the time a peep was made" do
     visit("/peeps")
     expect(page).to have_content "#{Peep.first.time.strftime("%R %v")}"
+    expect(page).to have_content "#{Peep.last.time.strftime("%R %v")}"
   end
 
   scenario "I want to see peeps in reverse chronological order" do
-    peep_2
     visit("/peeps")
     expect(Peep.last.content).to appear_before(Peep.first.content)
   end
@@ -26,5 +29,6 @@ feature "Viewing peeps" do
   scenario "I want to see who posted a peep" do
     visit("/peeps")
     expect(page).to have_content "@#{Peep.first.user.username}"
+    expect(page).to have_content "@#{Peep.last.user.username}"
   end
 end
--- spec/helpers/peeping.rb
+++ spec/helpers/peeping.rb
@@ -1,14 +1,14 @@
 module PeepingHelpers
   def peep_1
     visit('/peeps/new')
-    fill_in :peep, with: "My first peep!"
+    fill_in :peep, with: "Good grief!"
     click_button "Peep!"
   end
 
   def peep_2
     sleep 1
     visit('/peeps/new')
-    fill_in :peep, with: "My second peep!"
+    fill_in :peep, with: "Oh, carrots!"
     click_button "Peep!"
   end
 end
