861d1d828f2e8a87559df370d316188f10a458e3
Alexander-Blair <zanderb@hotmail.co.uk>
2017-07-29 23:45:25 +0100

    Third user story - shows date and time

---
+++ spec/features/see_time_message_posted_spec.rb
@@ -0,0 +1,8 @@
+feature 'peep shows when it was written' do
+  let(:content) { 'comment' }
+  let(:time_posted) { Time.now.strftime('%F %H:%M')}
+  scenario 'shows date and time' do
+    new_peep(content)
+    expect(page).to have_content("Posted at #{time_posted} ")
+  end
+end
