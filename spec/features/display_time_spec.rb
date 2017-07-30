5d7751abc4844ce6644f9b7a87afb3e8b2dc1270
kitkat119 <gprydal@yahoo.co.uk>
2017-07-30 19:48:18 +0100

    changed how time & date are displayed and reversed order of peeps

--- spec/features/display_time_spec.rb
+++ spec/features/display_time_spec.rb
@@ -5,7 +5,7 @@ feature 'time of peep is displayed' do
     fill_in :message, with: 'My first peep'
     click_button 'Post Peep'
     within 'ul#peeps' do
-    expect(page).to have_content '2017'
+    expect(page).to have_content '07/30/17'
     end
   end
 end
