6bb4c268ea6769443321ab89dfe3a9d4a1fcfe29
Phil Lang <plangphoto@gmail.com>
2017-07-30 19:12:59 +0100

    cleanup

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post
   include DataMapper::Resource
 
   property :id,      Serial
-  property :text,    String
+  property :text,    Text, :length => 140
   property :time,    String
 
   has n, :user, through: Resource
