c4a34ecabc4bc40997aae89a958e46e830b79a4e
annalaise <annalaise.gibbons@gmail.com>
2017-07-30 14:20:59 +0100

    web helper added

---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,15 @@
+def post_a_peep(test_peep)
+peep = test_peep
+visit '/peeps/new'
+fill_in :message, with: peep
+click_button 'Post'
+end
+
+
+def test_peep_one
+  "Joffrey...Cersei...Walder Frey...Meryn Trant...Tywin Lannister...The Red Woman...Beric Dondarrion...Thoros of Myr...Illyn Payne...The Mountain.."
+end
+
+def test_peep_two
+  "No need to seize the last word, Lord Baelish. I'll assume it was something clever"
+end
