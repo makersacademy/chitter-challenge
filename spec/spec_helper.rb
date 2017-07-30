2cec5e51a998daf7b0985ea605080294acd87317
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-30 14:47:59 +0100

    Configured Rack Env on my development and testing environments

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
+ENV['RACK_ENV'] = 'test'
 require 'simplecov'
 require 'simplecov-console'
+require File.join(File.dirname(__FILE__), '..', './app/app.rb')
 require './app/models/peep'
 require 'capybara/rspec'
 require './app/app'
