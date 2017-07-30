106ea584842922d138ceb3a6aceff9bd253d7dfc
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-31 00:47:26 +0100

    new feature for password condifrmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -9,4 +9,18 @@ feature 'User sign up' do
     expect(page).to have_content('Welcome, yuliya@example.com')
     expect(User.first.email).to eq('yuliya@example.com')
   end
+
+  scenario 'requires a matching confirmation password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+  def sign_up(email: 'yuliya@example.com',
+              password: 'hitter!',
+              password_confirmation: 'hitter!')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Wanna PeeP? Sign up'
+  end
 end
