fb907b15797a6c2c1446cbd03627066c8cdc6d7a
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-30 12:31:30 +0100

    Updated Spec-Helper: Required Capybara and app files

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
-ENV['RACK_ENV'] = 'test'
-require File.join(File.dirname(__FILE__), '..', './app/app.rb')
-require 'capybara'
-require 'capybara/rspec'
-require 'database_cleaner'
-require './app/models/link'
 require 'simplecov'
 require 'simplecov-console'
+require './app/models/peep'
+require 'capybara/rspec'
+require './app/app'
 
 Capybara.app = Chitter
 
@@ -17,18 +14,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
-
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
   config.after(:suite) do
-    DatabaseCleaner.clean
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
