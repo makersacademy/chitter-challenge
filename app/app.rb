40613be2c65f5272a651ad3a3a333ca4b0223c0e
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-30 12:22:42 +0100

    implement signup verification

--- app/app.rb
+++ app/app.rb
@@ -1,12 +1,12 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'data_mapper_setup'
 
-# require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
-
+  register Sinatra::Flash
   enable :sessions
   set :session_secret, 'super secret'
 
@@ -25,15 +25,21 @@ class Chitter < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(name: params[:name], nickname: params[:nickname],
+    @user = User.new(name: params[:name], nickname: params[:nickname],
                       email: params[:email], password: params[:password],
                       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/peeps')
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to('/peeps')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
