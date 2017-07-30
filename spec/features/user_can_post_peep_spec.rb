99afab42ff9f01139a7f30b0c923c10ac3f2e571
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-30 20:49:35 +0100

    peeps feature user's name and username

--- spec/features/user_can_post_peep_spec.rb
+++ spec/features/user_can_post_peep_spec.rb
@@ -2,20 +2,20 @@ feature 'posting peeps' do
 
   before do
     Timecop.freeze(Time.local(1990))
+    user_sign_up("user", "user123", 'user@user.com', 'password123')
+    visit '/peeps'
+    post_peep('Peeping, peeping, 123')
   end
 
   scenario 'user can post a peep' do
-    user_sign_up("user", "user123", 'user@user.com', 'password123')
-    post_peep('Peeping, peeping, 123')
-    visit '/peeps'
     expect(page).to have_content 'Peeping, peeping, 123'
   end
 
   scenario 'peep features the date and time' do
-    visit '/peeps/new'
-    user_sign_up("user", "user123", 'user@user.com', 'password123')
-    post_peep('Peeping, peeping, 123')
-    visit '/peeps'
     expect(page).to have_content Time.now.strftime("%c")
+  end
+
+  scenario "peep features user's name and username" do
+    expect(page).to have_content "user (user123)"
   end
 end
