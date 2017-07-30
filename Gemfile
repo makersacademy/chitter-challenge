1f341d10abba82c73d2bff7441733f3c5da6b114
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-30 21:41:19 +0100

    introdused Dababase Cleaner gem

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@
 source "https://rubygems.org"
 
 ruby '2.4.0'
+
+gem 'database_cleaner'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,27 +8,27 @@ require './app/models/peep'
 
 Capybara.app = Chitter
 
-# require 'database_cleaner'
-#
-# RSpec.configure do |config|
+require 'database_cleaner'
 
-#   # Everything in this block runs once before all the tests run
-#   config.before(:suite) do
-#     DatabaseCleaner.strategy = :transaction
-#     DatabaseCleaner.clean_with(:truncation)
-#   end
-#
-#   # Everything in this block runs once before each individual test
-#   config.before(:each) do
-#     DatabaseCleaner.start
-#   end
-#
-#   # Everything in this block runs once after each individual test
-#   config.after(:each) do
-#     DatabaseCleaner.clean
-#   end
-#
-# end
+RSpec.configure do |config|
+
+  # Everything in this block runs once before all the tests run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
 
 require 'simplecov'
 require 'simplecov-console'
