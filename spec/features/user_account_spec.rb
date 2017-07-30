0bf1ee3af1ffd6203531b710d3564df7ac800ed0
kitkat119 <gprydal@yahoo.co.uk>
2017-07-30 23:06:27 +0100

    checks confirmation password with password

--- spec/features/user_account_spec.rb
+++ spec/features/user_account_spec.rb
@@ -1,7 +1,11 @@
 feature 'user accounts' do
-  scenario 'user can sign up for an account' do
+  scenario 'user can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content "Welcome to Chitter, example@example.com"
     expect(User.first.email).to eq "example@example.com"
+  end
+
+  scenario 'user cannot sign up with an incorrect password' do
+    expect { sign_up(password_confirmation: 'exanple') }.not_to change(User, :count)
   end
 end
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,7 +1,10 @@
-def sign_up
+def sign_up(email: 'example@example.com',
+                password: 'example',
+                password_confirmation: 'example')
   visit('/users/new')
   expect(page.status_code).to eq 200
-  fill_in :email, with: 'example@example.com'
-  fill_in :password, with: 'example'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up to Chitter'
 end
