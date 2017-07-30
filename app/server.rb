c0953a8de63c078ec5f2b5cc042da527635fd9a8
Phil Lang <plangphoto@gmail.com>
2017-07-30 15:30:40 +0100

    attempting to fix serving of static files for Heroku

--- app/server.rb
+++ app/server.rb
@@ -6,6 +6,7 @@ class CHITTER < Sinatra::Base
   use Rack::MethodOverride
   set :session_secret, 'super secret'
   set :partial_template_engine, :erb
+  set :public_folder, Proc.new { File.join(root, 'static') }
 
   enable :partial_underscores
 
