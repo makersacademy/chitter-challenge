f09678aa5fb3a9e3b649cb4bb807e6490fdb9055
kitkat119 <gprydal@yahoo.co.uk>
2017-07-30 14:35:34 +0100

    configured the Rack Env, database_cleaner  and deployed to heroku

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ ruby '2.4.0'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
+gem 'database_cleaner'
 
 group :test do
   gem 'cucumber'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,10 @@
+ENV["RACK_ENV"] = "test"
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 require './app/models/peep'
 require './app/app'
+require 'database_cleaner'
 
 Capybara.app = Chitter
 
@@ -14,6 +16,21 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  # Everything in this block runs once before all the tests run
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
