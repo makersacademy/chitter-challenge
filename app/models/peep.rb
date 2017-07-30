dff3fbc4b92c5f491af17e976ab7c61598b275fb
annalaise <annalaise.gibbons@gmail.com>
2017-07-31 00:34:04 +0100

    refactoring user validation

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -5,9 +5,9 @@ require 'dm-timestamps'
 class Peep
   include DataMapper::Resource
 
+  belongs_to :user
+
   property    :id,          Serial
   property    :message,     String, :length => 1..160
   property    :created_at,  DateTime
-
-  has 1, :user, {:through=>DataMapper::Resource}
 end
