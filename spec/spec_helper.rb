4ca5cad74eb9949773cdbef96af6866c164d3e6d
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-30 13:45:11 +0100

    Updated Spec-Helper: configured RSpec to use database_cleaner (gem was already in Gemfile), then required gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'simplecov-console'
 require './app/models/peep'
 require 'capybara/rspec'
 require './app/app'
+require 'database_cleaner'
 
 Capybara.app = Chitter
 
@@ -14,6 +15,19 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
