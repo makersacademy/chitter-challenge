a9de44cdcf3ee565f2a0992bdfbc5822d4b8dc4a
Akshatha Ballal <akshatha.ballal@gmail.com>
2017-07-30 17:21:09 +0100

    Added a home path, added sign up and sign in buttons on home path

--- app/server.rb
+++ app/server.rb
@@ -4,11 +4,14 @@ class Chitter < Sinatra::Base
   set :session_secret, 'super secret'
   register Sinatra::Flash
   use Rack::MethodOverride
-  
+
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
     end
   end
 
+  get '/' do
+    redirect '/peeps'
+  end
 end
--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -8,4 +8,18 @@ feature 'Displaying peeps' do
     peek_at_peeps
     expect(page).to have_content page_content(peeps)
   end
+
+  scenario 'A user can click on Sign up on home page' do
+    visit('/')
+    expect(current_path).to eq '/peeps'
+    click_button 'Sign Up'
+    expect(current_path).to eq '/users/new'
+  end
+
+  scenario 'A user can click on Sign in on home page' do
+    visit('/')
+    expect(current_path).to eq '/peeps'
+    click_button 'Sign In'
+    expect(current_path).to eq '/sessions/new'
+  end
 end
