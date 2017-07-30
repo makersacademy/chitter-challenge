836d73c8371b6cc3ea627168ae184fffb11b9478
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-31 00:34:02 +0100

    minor update on the user spec file

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,3 +1,8 @@
+=begin
+As a Maker
+So that I can post messages on Chitter as me
+I want to sign up for Chitter
+=end
 feature 'User sign up' do
   scenario 'I can sign up as a new user' do
     expect { sign_up }.to change(User, :count).by(1)
