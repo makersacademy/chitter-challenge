de285153f79c10c02a0f1cbdf77cd661635f5795
annalaise <annalaise.gibbons@gmail.com>
2017-07-31 01:06:33 +0100

    rake bugfix

--- app/controllers/peeps.rb
+++ app/controllers/peeps.rb
@@ -14,8 +14,6 @@ class Chitter < Sinatra::Base
   post '/peeps' do
     peep = Peep.new(message: params[:message], user: current_user)
     peep.save
-    user_id = peep.user_id
-    p user_id
     redirect '/peeps/view-all'
   end
 
