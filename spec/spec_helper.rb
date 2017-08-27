51ee16720686577e993600e98d3f1f16db09cbd8
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-27 11:02:30 +0100

    database cleaner activated

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,18 +19,18 @@ SimpleCov.start
 
 RSpec.configure do |config|
 
-  # config.before(:suite) do # <-- before entire test run
-  #   DatabaseCleaner.strategy = :transaction
-  #   DatabaseCleaner.clean_with(:truncation)
-  # end
+  config.before(:suite) do # <-- before entire test run
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-  # config.before(:each) do # <-- create a "save point" before each test
-  #   DatabaseCleaner.start
-  # end
+  config.before(:each) do # <-- create a "save point" before each test
+    DatabaseCleaner.start
+  end
 
-  # config.after(:each) do # <-- after each individual test roll back to "save point"
-  #   DatabaseCleaner.clean
-  # end
+  config.after(:each) do # <-- after each individual test roll back to "save point"
+    DatabaseCleaner.clean
+  end
 
   config.after(:suite) do
     puts
