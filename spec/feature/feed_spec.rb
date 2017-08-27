9d9a1dc9b968041288b0ce6960791db7e41f5b62
pyan83 <puyanwei@gmail.com>
2017-08-27 18:07:25 +0100

    Rubocop approved

--- spec/feature/feed_spec.rb
+++ spec/feature/feed_spec.rb
@@ -7,14 +7,14 @@ feature "Feed Page" do
   scenario "Posts are shown on the feed page" do
     post_tweet
     # within 'ul#feed' do
-      expect(page).to have_content "Hello World"
+    expect(page).to have_content "Hello World"
     # end
   end
 
   scenario "Posts shows the date of creation" do
     post_tweet
     # within 'ul#feed' do
-    expect(page).to have_content {"#{DateTime.now.strftime("%d %b %Y %l:%M %p")}"}
+    expect(page).to(have_content { "#{DateTime.now.strftime("%d %b %Y %l:%M %p")}" })
     # end
   end
 end
