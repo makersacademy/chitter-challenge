c551480eab98bbd3777854b3e16ac5ac1564e799
Abi Travers <atravers4007@gmail.com>
2017-08-27 08:29:04 +0100

    1 test for homepage existance and failing as need to create. Still unable to get data_base cleaner to work but it is not as important as other features as in my simple understanding all it does is clear the test database and can solve this issue at the moment by just changing the examples i put in my tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,27 +4,10 @@ require 'simplecov'
 require 'simplecov-console'
 require 'capybara/rspec'
 require './app.rb'
-# require 'database_cleaner'
+# require './database_cleaner.rb'
 require './models/data_mapper_setup.rb'
 
 Capybara.app = Chitter
-
-# RSpec.configure do |config|
-#  config.before(:suite) do
-#   DatabaseCleaner.strategy = :transaction
-#   DatabaseCleaner.clean_with(:truncation)
-#  end
-#
-#  config.before(:each) do
-#      DatabaseCleaner.start
-#    end
-#
-#    config.after(:each) do
-#    DatabaseCleaner.clean
-#  end
-#
-# end
-
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -39,4 +22,38 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+end
+
+
+RSpec.configure do |config|
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  # rspec-mocks config goes here. You can use an alternate test double
+  # library (such as bogus or mocha) by changing the `mock_with` option here.
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
+  # have no way to turn it off -- the option exists only for backwards
+  # compatibility in RSpec 3). It causes shared context metadata to be
+  # inherited by the metadata hash of host groups and examples, rather than
+  # triggering implicit auto-inclusion in groups with matching metadata.
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+
 end
