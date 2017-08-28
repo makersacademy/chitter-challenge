cf8cd160ad817807814ce12ed07e7c74321c4e71
Abi Travers <atravers4007@gmail.com>
2017-08-28 07:44:56 +0100

    Changed the example to check test works still as database cleaner is not activated at the moment for the test database

--- spec/new_post_spec.rb
+++ spec/new_post_spec.rb
@@ -2,7 +2,7 @@ feature 'Submit a new post' do
   scenario 'I can use a form to submit a new post' do
     visit '/new_post'
 
-    fill_in "title", :with => "Best Idea Created by ME"
+    fill_in "title", :with => "Best Idea Created by MEE"
     fill_in "chit", :with => "Electrons can be in many places at the same time"
 
     click_button "Everyone will be so pleased to hear about me!"
@@ -10,7 +10,7 @@ feature 'Submit a new post' do
     expect(current_path).to eq '/home'
 
     # within 'ul#posts' do
-       expect(page).to have_content("Best Idea Created by ME")
+       expect(page).to have_content("Best Idea Created by MEE")
     # end
   end
 end
