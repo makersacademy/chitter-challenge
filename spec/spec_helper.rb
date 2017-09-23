d0ee4abd316a6c92463af1fc7ace0d86c545eaee
William <wmcabangon@gmail.com>
2017-09-23 18:25:03 +0100

    can see existing peeps

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
+ENV['RACK_ENV'] = 'test'
+
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara/rspec'
+
+require File.join(File.dirname(__FILE__), '..', './app/app.rb')
+
+require './app/app'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
---
+++ spec/viewing_peeps_spec.rb
@@ -0,0 +1,11 @@
+feature 'Viewing peeps' do
+
+  scenario 'I can see existing peeps' do
+    Peep.create(message: 'Test Peep')
+    visit '/peeps'
+    expect(page.status_code).to eq 200
+    within 'ul#peeps' do
+      expect(page).to have_content('Test Peep')
+    end
+  end
+end
