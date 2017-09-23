9f9402f3fd7b4bc6a5c465423465e764297d604e
benjaminsunderland <ben@sunderland.eu.com>
2017-09-23 14:31:30 +0100

    Added views folder with index.erb (listing peeps) and new.erb (creating a peep). Controller functionality implemented

--- app/app.rb
+++ app/app.rb
@@ -9,5 +9,20 @@ class Chitter < Sinatra::Base
     'You arrived at the homepage'
   end
 
+  get '/peeps' do
+    @peeps = Peep.all
+    erb(:index)
+  end
+
+  get '/peeps/new' do
+    erb(:new)
+  end
+
+  post '/peeps' do
+    peep = Peep.new(title: params[:title], text: params[:text])
+    peep.save
+    redirect to('/peeps')
+  end
+
   run! if app_file == $PROGRAM_NAME
 end
