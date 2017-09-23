704391bd448400b93524518e2023818c90260d13
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-23 18:41:55 +0100

    created layout and updated user class

--- app/app.rb
+++ app/app.rb
@@ -5,6 +5,9 @@ require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
 
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/' do
     redirect '/peeps'
   end
@@ -21,6 +24,24 @@ class Chitter < Sinatra::Base
   post '/peeps' do
     Peep.create(message: params[:message])
     redirect '/peeps'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    user = User.create(name: params[:name],
+                email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/peeps')
+  end
+
+  helpers do
+    def current_user
+        @current_user ||= User.get(session[:user_id])
+    end
   end
 
 end
