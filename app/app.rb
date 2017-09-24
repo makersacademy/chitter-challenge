49da75885580de399a31e2e93b44e8e35b16dddf
mengchenwang <mengchenwang@hotmail.com>
2017-09-24 16:22:56 +0100

    added sinatra/flash

--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,13 @@ ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
 require_relative 'data_mapper_setup'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
+
+  register Sinatra::Flash
 
   get '/peeps' do
     @peeps = Peep.all
@@ -22,17 +25,23 @@ class Chitter < Sinatra::Base
   end
 
   get '/users/new' do
+    @user = User.new
     erb :'users/new'
   end
 
   post '/users' do
-    user = User.create(name: params[:name],
-                       user_name: params[:user_name],
-                       email: params[:email],
-                       password: params[:password],
-                       password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect '/peeps'
+    @user = User.new(name: params[:name],
+                     user_name: params[:user_name],
+                     email: params[:email],
+                     password: params[:password],
+                     password_confirmation: params[:password_confirmation])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect '/peeps'
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
   helpers do
