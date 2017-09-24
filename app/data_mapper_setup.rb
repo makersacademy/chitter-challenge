10b1aedb2712cd10a5b1dd122a8a09ad646f5013
James Hughes <hugjimbo77@gmail.com>
2017-09-24 01:10:02 +0100

    created datamapper file

---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,10 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require_relative 'models/peep'
+
+# Now let's set up a connection with a database
+DataMapper.setup(:default, "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
+# Let's check that everything we wrote in our models was OK
+DataMapper.finalize
+# And let's build any new columns or tables we added
+DataMapper.auto_upgrade!
--- app/models/peep.rb
+++ app/models/peep.rb
@@ -1,6 +1,3 @@
-require 'data_mapper'
-require 'dm-postgres-adapter'
-
 class Peep
   # add DataMapper functionality to this class so it can communicate with the database
   include DataMapper::Resource
@@ -9,12 +6,5 @@ class Peep
   property :id,     Serial # Serial means that it will be auto-incremented for every record
   property :title,  String
   property :message,    String
-
-  # Now let's set up a connection with a database
-  DataMapper.setup(:default, "postgres://localhost/chitter_challenge_test")
-  # Let's check that everything we wrote in our models was OK
-  DataMapper.finalize
-  # And let's build any new columns or tables we added
-  DataMapper.auto_upgrade!
 
 end
