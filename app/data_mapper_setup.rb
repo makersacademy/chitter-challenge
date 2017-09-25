7091c4e8ed719c87439f915f7cfdb47a7ff50b7a
ComaToastUK <jesusmanyouatethedog@gmail.com>
2017-09-25 14:47:06 +0100

    troubleshooting db errors

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,4 +6,4 @@ require_relative './models/user'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-DataMapper.auto_migrate!
+DataMapper.auto_upgrade!
