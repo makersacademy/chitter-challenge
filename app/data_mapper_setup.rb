31c5a5483eeb495d43857135878143248a549f72
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-10-06 14:52:16 +0100

    Views update with bootstrap

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,6 +5,7 @@ require_relative '../models/message'
 require_relative '../models/user'
 
 # mighty-lake-47923
+# rackup to start server
 ENV['RACK_ENV'] || 'development'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_challenge_#{ENV['RACK_ENV']}")
 DataMapper.finalize
