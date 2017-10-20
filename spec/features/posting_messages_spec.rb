ddfb56376f741751fc6cc435e70393b49bd3cdd1
ainsleybc <ab.chang@me.com>
2017-10-20 20:57:46 +0100

    print peeps in reverse chronological order

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -1,7 +1,9 @@
 feature 'posting messages to chitter' do
 
+  text = 'Consequat ea in voluptate magna est veniam non laboris pariatur reprehenderit culpa consectetur.'
+
   scenario 'user can post a message to chitter' do
-    text = 'Consequat ea in voluptate magna est veniam non laboris pariatur reprehenderit culpa consectetur.'
+    
     visit '/peeps/new'
     fill_in :peep, with: text
     fill_in :handle, with: '#ainsleybc'
@@ -9,6 +11,15 @@ feature 'posting messages to chitter' do
     click_button 'Post'
     visit '/peeps'
     expect(page).to have_content text
+  end
+
+  scenario 'user can view all messages in reverse chronological order' do
+    Peep.create(text: 'added first')
+    Peep.create(text: 'added second')
+    Peep.create(text: 'added third')
+    visit '/peeps'
+    expect(page.find('li:first-child')).to have_content 'added third'
+    expect(page.find('li:last-child')).to have_content 'added first'
   end
 
 end
