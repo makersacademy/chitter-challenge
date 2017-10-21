824612cea8534695fec7dee912ab0a04879f52b1
David Halewood <davidnhalewood@hotmail.com>
2017-10-21 09:18:36 +0100

    confirguring database_url for heroku

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,6 @@ class Post
   property :tip,    String
 end
 
-DataMapper.setup(:default, "postgres://localhost/fitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
