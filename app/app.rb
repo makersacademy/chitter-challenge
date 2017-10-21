e18e88397b27e0f5e4464463336124a1eac5674f
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-21 15:24:42 +0100

    Added rakefile and new tasks.

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,8 @@
 ENV["RACK_ENV"] ||= "development"
 
 require 'sinatra/base'
-require_relative 'models/peep'
-require_relative 'database'
+require_relative 'data_mapper_setup'
 
-Database.connect
 
 class Chitter < Sinatra::Base
   get '/peeps' do
