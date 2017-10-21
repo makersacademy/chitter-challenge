f50cf26c084753cb69b996c15946199b0b13f2c1
Tim Jones <timjones10@hotmail.com>
2017-10-21 15:10:59 +0100

    Updated controller to allow a Peep to be created

--- app/app.rb
+++ app/app.rb
@@ -2,8 +2,20 @@ require 'sinatra/base'
 require './app/models/peep'
 
 class Chitter < Sinatra::Base
+
   get '/peeps' do
   @peeps = Peep.all
   erb :'peeps/index'
   end
+
+  get'/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  post '/peeps' do
+    Peep.create(name: params[:name], username: params[:username],
+    message: params[:message], time: Time.new.strftime("%I:%M%p"))
+    redirect '/peeps'
+  end
+
 end
