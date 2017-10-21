8a7219bab757f44f41d983221138650acaa98446
ainsleybc <ab.chang@me.com>
2017-10-21 15:10:26 +0100

    prevent posting peeps unless logged in

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -9,5 +9,10 @@ feature 'posting messages to chitter' do
     click_button 'Post'
     expect(page).to have_content text
   end
+  scenario 'I cannot post a message to Chitter unless logged in' do
+    error = 'You must be logged in to post a new peep'
+    visit '/peeps/new'
+    expect(page).to have_content error
+  end
 
 end
