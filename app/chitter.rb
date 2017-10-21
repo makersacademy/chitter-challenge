894137e72ac3abbef632806e8253ac1e32ff4370
ainsleybc <ab.chang@me.com>
2017-10-21 01:06:21 +0100

    added log out functionality

--- app/chitter.rb
+++ app/chitter.rb
@@ -9,6 +9,7 @@ class Chitter < Sinatra::Base
 
   enable :sessions
   register Sinatra::Flash
+  use Rack::MethodOverride
 
   helpers do
     def current_user
@@ -63,6 +64,12 @@ class Chitter < Sinatra::Base
       flash.now[:errors] = ['The email or password is incorrect']
       erb :'sessions/new'
     end
+  end
+
+  delete '/sessions' do
+    session[:user_id] = nil
+    flash.keep[:notice] = 'goodbye!'
+    redirect to '/peeps'
   end
 
 end
