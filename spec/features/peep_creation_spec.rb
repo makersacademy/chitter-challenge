bda6954f04bfa9e8853224133514789e0472b81c
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-21 10:50:05 +0100

    User's name and username are published next to tweets

--- spec/features/peep_creation_spec.rb
+++ spec/features/peep_creation_spec.rb
@@ -4,4 +4,9 @@ feature 'Peep' do
     post_peep "Test peep"
     expect(page).to have_content "Test peep"
   end
+  scenario 'user handle and name are published alongside peep' do
+    sign_up
+    post_peep "Test peep"
+    expect(page).to have_content "posted by Ed Lowther, @edlowther"
+  end
 end
