7cee3fcbf227c6c8a62fe2b3d765cd08c86e9118
Jon Sanders <jonsanders101@aol.com>
2017-10-21 11:27:20 +0100

    Add autoupgrade and automigrate tasks to Rakefile

--- setup.rb
+++ setup.rb
@@ -6,4 +6,3 @@ require_relative 'app/models/peep' #Does this need to be after DataMapper.setup?
 DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 
 DataMapper.finalize
-DataMapper.auto_upgrade!
