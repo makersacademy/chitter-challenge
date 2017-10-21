faf47727651f02d83049612aadf5e8b685fe0e1e
ainsleybc <ab.chang@me.com>
2017-10-21 13:15:00 +0100

    added log in & out to web helpers

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -2,13 +2,11 @@ feature 'posting messages to chitter' do
 
   text = 'Consequat ea in voluptate magna est veniam non laboris pariatur reprehenderit culpa consectetur.'
 
-  scenario 'I can post a message to chitter' do
+  scenario 'I can post a message to chitter when logged in' do
+    sign_up
     visit '/peeps/new'
     fill_in :peep, with: text
-    fill_in :handle, with: '#ainsleybc'
-    fill_in :name, with: 'Ainsley Chang'
     click_button 'Post'
-    visit '/peeps'
     expect(page).to have_content text
   end
 
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,5 +1,4 @@
 def sign_up(args = {})
-
   email = args.fetch(:email, 'no@email.com')
   handle = args.fetch(:handle, 'ainsleybc')
   name = args.fetch(:name, 'Ainsley Chang')
@@ -11,4 +10,18 @@ def sign_up(args = {})
   fill_in :name, with: name
   fill_in :password, with: password
   click_button 'Register'
+end
+
+def log_in(args)
+  email = args.fetch(:email, 'no@email.com')
+  password = args.fetch(:password, 'password')
+
+  visit '/sessions/new'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  click_button 'Log in'
+end
+
+def log_out
+  click_button 'Log out'
 end
