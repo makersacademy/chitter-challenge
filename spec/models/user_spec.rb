56fce16ca5d04cb76f871c3188229d74ab7d422b
Austin Rowsell <andyrow123@hotmail.co.uk>
2017-10-21 21:55:49 +0100

    Added sign in tests and code.

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+describe User do
+
+  let!(:user) do
+    User.create(email: 'test@test.com', password: 'password',
+                password_confirmation: 'password')
+  end
+
+  it 'authenticates when given valid credentials' do
+    authenticated_user = User.authenticate(user.email, user.password)
+    expect(authenticated_user).to eq user
+  end
+
+  it 'does not authenticate when given incorrect password' do
+    expect(User.authenticate(user.email, 'wrong_password')).to be_nil
+  end
+end
