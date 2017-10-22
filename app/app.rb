ce009acd15bdbec37bc4491f85d7b7838690c5f0
hugosykes <hugo@denmeadfarm.co.uk>
2017-10-22 14:16:30 +0100

    Added a rebuff message if password and confirmation password do not match

--- app/app.rb
+++ app/app.rb
@@ -29,6 +29,7 @@ class Chitter < Sinatra::Base
   end
 
   get '/users/new' do
+    @fail = 'Password and password confirmation do not match!' if session[:fail]
     erb :'users/new_user'
   end
 
@@ -36,8 +37,13 @@ class Chitter < Sinatra::Base
     user = User.create(email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id if !!user
-    redirect '/users/confirmation'
+    if user.save
+      session[:user_id] = user.id
+      redirect '/users/confirmation'
+    else
+      session[:fail] = true
+      redirect '/users/new'
+    end
   end
 
   get '/users/confirmation' do
--- spec/features/password_confirmation_spec.rb
+++ spec/features/password_confirmation_spec.rb
@@ -1,5 +1,6 @@
 feature 'Password confirmation' do
   scenario 'I want an account not to be created if the passwords don\'t match' do
     expect { sign_up(password_confirmation: 'hello321') }.not_to change(User, :count)
+    expect(page).to have_content 'Password and password confirmation do not match!'
   end
 end
