b1f7547ad2dd923ccc7e3d9a811ece5fc06d6e6d
Chris <cdunham1989@gmail.com>
2017-10-22 11:49:32 +0100

    user can now sign in to chitter and e-mail is displayed on the homepage

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     redirect '/peeps'
@@ -19,9 +21,25 @@ class Chitter < Sinatra::Base
   end
 
   post '/peeps' do
-    peep = Peep.create(content: params[:content])
+    peep = Peep.create(content: params[:content], created_at: [Time.now.strftime("%d %b %Y %l:%M")])
     peep.save
     redirect '/peeps'
+  end
+
+  get '/users/new' do
+    erb:'users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/peeps'
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
   end
 
   run! if app_file == $0
---
+++ spec/features/new_user_spec.rb
@@ -0,0 +1,9 @@
+require 'web_helpers'
+
+feature 'User sign up' do
+  scenario 'I can sign up as a new user' do
+    expect { sign_up }.to change(User, :count).by(1)
+    expect(page).to have_content('Welcome, sample@example.com')
+    expect(User.first.email).to eq('sample@example.com')
+  end
+end
