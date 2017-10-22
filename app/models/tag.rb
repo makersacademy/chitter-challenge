e0a34d427d969ce4b9980b4d8ee611de8c24ffe8
Jon Sanders <jonsanders101@aol.com>
2017-10-22 08:46:31 +0100

    Add many-to-many association between peeps and tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,8 @@
 class Tag
   include DataMapper::Resource
 
-  property :id,        Serial
-  property :name,      String
+  property :id, Serial
+  property :name, String
 
   has n, :peeps, :through => Resource
 
