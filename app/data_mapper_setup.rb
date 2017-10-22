fa20a82e5d508a28216de80010c7c89ca5dc1f30
Ainsley <ab.chang@me.com>
2017-10-22 21:32:23 +0100

    added database config for heroku

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -6,6 +6,6 @@ require_relative './models/user'
 require_relative './models/tags'
 require_relative './models/reply'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default,ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
