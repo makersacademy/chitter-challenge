622d829065c4e1709d911698a116794cb5d82c39
Lucas Salmins <lucasasalmins@gmail.com>
2017-10-22 16:34:34 +0100

    adding new peeps and displaying on the homepage

---
+++ app/app.rb
@@ -0,0 +1,31 @@
+ENV['RACK_ENV'] ||= 'development'
+
+require 'sinatra/base'
+
+require './app/models/peeps'
+require './app/data_mapper_setup'
+
+class Chitter < Sinatra::Base
+
+  get '/' do
+    redirect '/peeps'
+  end
+
+  post '/peeps' do
+    Peep.create(message: params[:message])
+    p Peep.all
+    redirect '/peeps'
+  end
+
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  get '/peeps' do
+    @peeps = Peep.all
+    p Peep.all
+    erb :'peeps/timeline'
+  end
+
+  run! if app_file == $PROGRAM_NAME
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,25 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'capybara/rspec'
+require 'dm-postgres-adapter'
 require 'simplecov'
 require 'simplecov-console'
+require './app/app'
+require './app/models/peeps'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
-RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
-end
+# RSpec.configure do |config|
+#   config.after(:suite) do
+#     puts
+#     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+#     puts "\e[33mTry it now! Just run: rubocop\e[0m"
+#   end
+# end
