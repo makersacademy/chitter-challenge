ffa7a7d1cdc03d03960f68e35dc0246cc577ad3c
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-17 09:19:12 +0000

    setting up Chitter project carcass and supporting files

---
+++ app/data_mapper_setup.rb
@@ -0,0 +1,10 @@
+require 'data_mapper'
+require 'dm-migrations'
+require 'dm-postgres-adapter'
+
+#require_relative 'models/file_name'
+#require_relative 'models/another_file_name'
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,23 @@
+ENV['RACK_ENV'] = 'test'
+
+require './app/app.rb'
+require './app/data_mapper_setup'
+require 'capybara'
+require 'capybara/rspec'
+require 'database_cleaner'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require './spec/features/web_helpers.rb'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+# Capybara.app = Bookmark
+
+# SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+#   SimpleCov::Formatter::Console,
+#   # Want a nice code coverage website? Uncomment this next line!
+#   # SimpleCov::Formatter::HTMLFormatter
+# ])
+# SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
@@ -14,4 +25,18 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+
+    config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+ config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+ config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
