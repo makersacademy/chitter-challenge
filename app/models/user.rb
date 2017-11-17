f46638d1fade78e82353f0d83df1f717674bc82d
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-17 12:33:08 +0000

    adds ID to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ require 'data_mapper'
 class User
   include DataMapper::Resource
 
+  property :id, Serial
   property :username, String
   property :digest, Text
 
   def password=(password)
-    self.digest = Bcrypt::Password.create(password)
+    self.digest = BCrypt::Password.create(password)
   end
 end
