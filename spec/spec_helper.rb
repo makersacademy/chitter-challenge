e4f36c4e64064be411413ad2623547545407a3fc
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-17 12:32:15 +0000

    configures for database, include adapters and database cleaning

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,31 @@
 ENV['RACK_ENV'] = 'test'
 
+require_relative '../app/app'
+
 require 'capybara'
 require 'capybara/rspec'
+require 'database_cleaner'
 require 'simplecov'
 require 'simplecov-console'
 require 'selenium-webdriver'
-require_relative '../app/app'
 
 Capybara.default_driver = :selenium
 Capybara.app = App
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
