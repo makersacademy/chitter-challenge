58537abb678c8edc6e460e87b5194c11de51909f
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-17 15:12:29 +0000

    revises sign up feature spec to use name and handle, username refactored to email

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,29 +1,43 @@
-def sign_in(username, password)
+def sign_in(name: 'theo', password: 'secret', email: 't@bw.com', 
+            handle: 'tdog')
   visit('/sign-up')
-  fill_in('username', with: username)
+  fill_in('name', with: name)
   fill_in('password', with: password)
+  fill_in('handle', with: handle)
+  fill_in('email', with: email)
   click_button('Complete')
 end
 
 feature 'Feature: signing up' do
   scenario 'user does not enter email' do
-    sign_in('', 'secret')
+    sign_in(email: '')
+    expect(page.current_path).to eq '/sign-up'
+  end 
+
+  scenario 'user does not enter handle' do
+    sign_in(handle: '')
+    expect(page.current_path).to eq '/sign-up'
+  end 
+
+  scenario 'user does not enter name' do
+    sign_in(name: '')
     expect(page.current_path).to eq '/sign-up'
   end 
 
   scenario 'user does not enter password' do
-    sign_in('example@domain.com', '')
+    sign_in(password: '')
     expect(page.current_path).to eq '/sign-up'
   end 
 
   scenario 'invalid sign-up attempt does not generate a user' do
-    expect { sign_in('example@domain.com', '') }.to_not change { User.count }
-    expect { sign_in('', 'secret') }.to_not change { User.count }
+    expect { sign_in(name: '') }.to_not change { User.count }
+    expect { sign_in(password: '') }.to_not change { User.count }
+    expect { sign_in(handle: '') }.to_not change { User.count }
+    expect { sign_in(email: '') }.to_not change { User.count }
   end
 
   scenario 'user enters valid details' do
-    expect { sign_in('example@domain.com', 'secret') }
-      .to change { User.count }.by(1)
-    expect(page).to have_content 'Welcome, example@domain.com' 
+    expect { sign_in }.to change { User.count }.by(1)
+    expect(page).to have_content 'Welcome, theo'
   end
 end
