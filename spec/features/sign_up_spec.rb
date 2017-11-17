cd9abeac430730d83adc5e309d8654a76b9994d2
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-17 12:32:47 +0000

    modifies sign-up feature test to fix a few typos

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,5 +1,5 @@
 def sign_in(username, password)
-  visit('/sign-in')
+  visit('/sign-up')
   fill_in('username', with: username)
   fill_in('password', with: password)
   click_button('Complete')
@@ -8,12 +8,12 @@ end
 feature 'Feature: signing up' do
   scenario 'user does not enter email' do
     sign_in('', 'secret')
-    expect(page.current_path).to eq '/sign_in'
+    expect(page.current_path).to eq '/sign-up'
   end 
 
   scenario 'user does not enter password' do
     sign_in('example@domain.com', '')
-    expect(page.current_path).to eq '/sign_in'
+    expect(page.current_path).to eq '/sign-up'
   end 
 
   scenario 'invalid sign-up attempt does not generate a user' do
