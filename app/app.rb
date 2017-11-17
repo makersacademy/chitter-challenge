fb02e9a0746092ab54e8aed5baa4c2bfd8770f40
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-17 12:33:40 +0000

    implements basic user sign-up

--- app/app.rb
+++ app/app.rb
@@ -1,4 +1,31 @@
 require 'sinatra/base'
+require_relative 'models/db-setup'
+require_relative 'models/user'
+
+ENV['RACK_ENV'] ||= 'development'
 
 class App < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'narcissus'
+
+  helpers do 
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
+  get '/sign-up' do
+    erb(:signup)
+  end
+
+  post '/users/new' do
+    user = User.create(
+      username: params[:username], password: params[:password])
+    session[:user_id] = user.id
+    redirect('/home')
+  end
+
+  get '/home' do
+    erb(:home)
+  end
 end
