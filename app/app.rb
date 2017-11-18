9880d0b2f5bdf54f91b429c465dbdee6a9bfacdb
Tabish Raza <tabishraza@gmail.com>
2017-11-18 15:51:32 +0000

    create message, store in database

--- app/app.rb
+++ app/app.rb
@@ -1,16 +1,26 @@
+require 'data_mapper'
+require 'rubygems'
 require 'sinatra'
+
+require_relative 'models/peep'
 
 class Chitter < Sinatra::Base
 
   enable :sessions
 
   get '/' do
+    # "hello"
     @peep = session[:peep]
     erb(:index)
   end
 
   post '/post_peep' do
-    session[:peep] = params[:message]
+    @peep = Peep.create(text: params[:message])
+    session[:peep] = @peep.text
+    p "PEEPPP",@peep
     redirect '/'
   end
+
+
+  run! if app_file == $0
 end
