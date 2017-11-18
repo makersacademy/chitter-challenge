fb523366ccfc3bbfedfa6c7e01bef0871e406ebd
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-18 11:43:10 +0000

    extends login feature test to include bad credentials

--- spec/features/logging_in_spec.rb
+++ spec/features/logging_in_spec.rb
@@ -1,10 +1,10 @@
-def sign_in
+def sign_in(email: 'example@domain.com', password: 'secret')
   User.create(name: 'example', email: 'example@domain.com',
               password: 'secret', password_confirmation: 'secret',
               handle: 'handle')
   visit('/sessions/new')
-  fill_in('email', with: 'example@domain.com')
-  fill_in('password', with: 'secret')
+  fill_in('email', with: email)
+  fill_in('password', with: password)
   click_button('Log in')
 end
 
@@ -13,4 +13,15 @@ feature 'Feature: logging in' do
     sign_in
     expect(page).to have_content 'Welcome, example'
   end
+
+  scenario 'user doens\'t have valid password' do
+    sign_in(password: 'wrong')
+    expect(page).to have_content 'Email or password incorrect'
+  end
+
+  scenario 'user doens\'t have valid email' do
+    sign_in(email: 'wrong@domain.com')
+    expect(page).to have_content 'Email or password incorrect'
+  end
 end
+
