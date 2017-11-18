29d8b4898028fcaced255936405ff2d9bab9ab6f
Lewis <lewisyoul@gmail.com>
2017-11-18 13:37:42 +0000

    create test for posting

--- spec/feature/posts_feature_spec.rb
+++ spec/feature/posts_feature_spec.rb
@@ -2,7 +2,7 @@ feature 'posts' do
   scenario 'filling in post fields results in post appearing' do
     visit('/posts/new')
     fill_in('title', with: 'Test Post')
-    fill_in('post', with: 'This is a test Chitter post')
+    fill_in('body', with: 'This is a test Chitter post')
     click_button('Post')
     expect(page).to have_content('This is a test Chitter post')
   end
