f5813b90c78ff78e1f22bd5f7cb7018db0a8ecd8
thatdania@gmail.com <thatdania@gmail.com>
2017-11-18 15:03:42 +0000

    Added test and function that the recent post pops up first

--- spec/features/creatingpost_spec.rb
+++ spec/features/creatingpost_spec.rb
@@ -1,11 +1,18 @@
+
+
 feature 'Creating a post on Chitter' do
   scenario 'User can create a post on Chitter' do
-    visit '/newpost'
-    fill_in 'title', with: 'A first go'
-    fill_in 'post', with: 'Chittery Do Dah! What a great app!'
-    fill_in 'name', with: 'thatdania'
-    click_on('Create Post')
+    fill_in_first
     expect(current_path).to eq('/home')
-    expect(page).to have_content('A first go: Chittery Do Dah! What a great app! ~@thatdania')
+    expect(page).to have_content('First: Chittery Do Dah! What a great app! ~@thatdania')
   end
+end
+
+feature 'Showing most recent post created on Chitter' do
+  scenario 'Users post shows up first on Chitter' do
+    fill_in_first
+    fill_in_second
+    expect(current_path).to eq('/home')
+    expect(page.body.index('Second')).to be < page.body.index('First')
+ end
 end
