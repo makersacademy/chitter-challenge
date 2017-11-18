9a2cd0fedacd31f2eaf1de4b08d982147845e170
Tabish Raza <tabishraza@gmail.com>
2017-11-18 13:27:55 +0000

    initializing capybara, require app

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
+require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+require './app/app'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -10,8 +15,6 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  
   end
 end
