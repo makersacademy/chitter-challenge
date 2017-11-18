29ab765c9e4aa639cd99665efae48550d0128983
Lewis <lewisyoul@gmail.com>
2017-11-18 18:03:22 +0000

    require user.rb

--- models/data_mapper_setup.rb
+++ models/data_mapper_setup.rb
@@ -2,6 +2,7 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require 'dm-migrations'
 require './models/post.rb'
+require './models/user.rb'
 
 DataMapper::Model.raise_on_save_failure = true
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
