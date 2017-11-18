7cbd398a429f545d18726b351dee0f76611a85b3
Lewis <lewisyoul@gmail.com>
2017-11-18 13:40:10 +0000

    create post model - link to database

---
+++ models/post.rb
@@ -0,0 +1,12 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require 'dm-migrations'
+
+class Post
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :title, String
+  property :body, Text
+
+end
