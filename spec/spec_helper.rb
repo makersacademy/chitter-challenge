64552d0e5211f854a398ff071af11241cb2b57f4
Lewis <lewisyoul@gmail.com>
2017-11-18 21:15:22 +0000

    add DatabaseCleaner.clean to run in after block

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,7 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
+    DatabaseCleaner.clean
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
