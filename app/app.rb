9f569de5b1d36acea4114a1e50fbef8f6e2f36fa
Lewis <lewisyoul@gmail.com>
2017-11-18 13:39:44 +0000

    controller and views now use post model

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,11 @@
 ENV['RACK_ENV'] = 'development'
 
 require 'sinatra/base'
+require './models/data_mapper_setup'
 
 class Chitter < Sinatra::Base
+
+  enable :sessions
 
   get '/' do
     redirect '/posts'
@@ -12,7 +15,17 @@ class Chitter < Sinatra::Base
     erb(:new_post)
   end
 
+  post '/posts' do
+    Post.create(title: params[:title], body: params[:body])
+    # session[:title] = params[:title]
+    # session[:body] = params[:body]
+    redirect '/posts'
+  end
+
   get '/posts' do
+    @posts = Post.all
+    # @title = session[:title]
+    # @post = session[:body]
     erb(:posts)
   end
 
