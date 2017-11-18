4b087dbfe96ca8fd20c0f7bdb7a221f779468212
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-18 08:33:13 +0000

    adds unit test for authentication

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,12 +1,23 @@
 require './app/models/user'
 require 'bcrypt'
 
+def get_user
+  User.create(
+    password: 'a', 
+    password_confirmation: 'a',
+    name: 'name',
+    email: 'name@domain.com',
+    handle: 'handle'
+  )
+end
+
 describe User do
   subject do
-    described_class.new(
+    described_class.create(
       password: 'a', 
-      password_confirmation: 'b',
+      password_confirmation: 'a',
       name: 'name',
+      email: 'name@domain.com',
       handle: 'handle'
     )
   end
@@ -14,7 +25,7 @@ describe User do
   describe '#password' do
     context 'when creating digest attribute' do
       after(:each) do 
-        described_class.new(password: 'a', password_confirmation: 'b')
+        described_class.new(password: 'a', password_confirmation: 'a')
       end
 
       it 'encrypts password' do
@@ -25,6 +36,27 @@ describe User do
     context 'after password encryption' do
       it 'sets @password' do
         expect(subject.password).to eq 'a'
+      end
+    end
+  end
+
+  describe '#authenticate' do
+    context 'when has valid credentials' do
+      it 'returns user object' do
+        user = get_user
+        expect(User.authenticate('name@domain.com', 'a')).to eq user
+      end
+    end
+
+    context 'when has invalid credentials' do
+      it 'returns nil' do
+        user = get_user
+        expect(User.authenticate('name@nowhere.com', 'a')).to eq nil
+      end
+
+      it 'returns nil' do
+        user = get_user
+        expect(User.authenticate('name@domain.com', 'b')).to eq nil
       end
     end
   end
