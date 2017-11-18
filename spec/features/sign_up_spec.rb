c71bbabea51041753af827d94a19124120b093ac
Theo Breuer-Weil <theobreuerweil@gmail.com>
2017-11-18 07:39:49 +0000

    adds tests for sign-up validations

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,26 +1,36 @@
 shared_examples 'required' do |key|
-  it "cannot sign up without #{key}" do
-    sign_up(**{ key => '' })
-    expect(page).to have_content 'Please fill in all fields'
+  scenario  "cannot sign up without #{key}" do
+    expect { sign_up(**{ key => nil }) }.not_to change { User.count }
+  end
+end
+
+shared_examples 'unique' do |key|
+  scenario "cannot sign up with duplicate #{key}" do
+    sign_up(**{ key => nil })
+    expect { sign_up(**{ key => nil }) }.not_to change { User.count }
   end
 end
 
 feature 'Feature: signing up' do
-  # UNUSED - include when fixed later
-  # include_examples('required', :name)
-  # include_examples('required', :password)
-  # include_examples('required', :password_confirmation)
-  # include_examples('required', :handle)
-  # include_examples('required', :email)
+  include_examples('required', :name)
+  include_examples('required', :password)
+  include_examples('required', :confirmation)
+  include_examples('required', :handle)
+  include_examples('required', :email)
+
+  include_examples('unique', :email)
+  include_examples('unique', :handle)
 
   scenario 'user enters mismatching passwords' do
-    sign_up(confirmation: 'mismatch')
-    expect(page.current_path).to eq '/users/new'
-    expect(page).to have_content 'Passwords do not match'
+    expect { sign_up(confirmation: 'mismatch') }.not_to change { User.count }
+  end
+
+  scenario 'user enters invalid email' do
+    expect { sign_up(email: 'malformed') }.not_to change { User.count }
   end
 
   scenario 'user enters valid details' do
-    expect { sign_up }.to change { User.count }.by(1)
+    expect { sign_up }.to change { User.count }.by 1
     expect(page).to have_content 'Welcome, person'
   end
 end
