0360ecbd66a658799b1d035fc5810566b5214064
majdeddine <majda-jambouri@hotmail.fr>
2017-11-19 15:50:34 +0000

    add test for peep model

---
+++ spec/peep_spec.rb
@@ -0,0 +1,8 @@
+require './app/models/peep.rb'
+
+describe Peep do
+  it 'store the peep in database' do
+    peep = Peep.create(content: 'blablabla!')
+    expect { peep }.to change { Peep.all.length }.by(1)
+  end
+end
