a13477c83916710a0adfeb47f68bd8a05584ec7a
thatdania@gmail.com <thatdania@gmail.com>
2017-11-19 16:28:54 +0000

    User cannot sign in without an email

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User
   attr_accessor :password_confirmation
 
   property :id,            Serial
-  property :email,         String
+  property :email,         String, required: true
   property :username,      String
   property :password_digest, Text
   validates_confirmation_of :password
--- spec/features/usermanagement_spec.rb
+++ spec/features/usermanagement_spec.rb
@@ -5,9 +5,15 @@ feature 'User sign up' do
     expect(User.first.email).to eq('thatdania@gmail.com')
   end
 
-  scenario 'requires a matching confirmation password' do
+  scenario 'Sign up page requires a matching confirmation password' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
     expect(current_path).to eq('/users')
     expect(page).to have_content 'Password and confirmation password do not match'
   end
+
+  scenario "User can't sign up without an email address" do
+    expect { sign_up(email: nil) }.not_to change(User, :count)
+  end
+
+
 end
