d55aa887406fe67d0dbdefa53c82a8096b6aac0a
Lewis <lewisyoul@gmail.com>
2017-11-19 12:24:21 +0000

    unable to peep when not logged in

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -1,11 +1,15 @@
 feature 'posts' do
 
   scenario 'filling in post fields results in post appearing' do
+    sign_up
+    sign_in
     submit_post_1
     expect(page).to have_content('This is Chitter test post 1')
   end
   scenario 'page returns the time' do
     allow(Time).to receive(:now).and_return('2017-11-18 17:02:05 +0000')
+    sign_up
+    sign_in
     submit_post_1
     expect(page).to have_content('Posted at 17:02:05 on 18-11-2017')
   end
