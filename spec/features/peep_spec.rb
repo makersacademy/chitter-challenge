93b782fc8465eea465cdb5121a6c9e7faf2da805
Robert Clayton <robbie.c.clayton@gmail.com>
2017-11-19 15:16:55 +0000

    peep appears with user name

--- spec/features/peep_spec.rb
+++ spec/features/peep_spec.rb
@@ -1,4 +1,7 @@
 feature 'Create a peep' do
+  @message1 = 'Hello World, this is my peep!'
+  @message2 = 'Hi, this is my second peep!'
+
   before(:each) do
     visit('/user/new')
     expect(page.status_code).to eq(200)
@@ -7,53 +10,36 @@ feature 'Create a peep' do
     fill_in 'name',      with: 'Robert'
     fill_in 'user_name', with: 'Rob'
     click_on('Sign up')
+    visit('/home')
+    fill_in 'new_peep_box', with: @message1
+    click_on('Create Peep')
+    visit('/home')
+    fill_in 'new_peep_box', with: @message2
+    click_on('Create Peep')
   end
 
   scenario 'User can post a peep to chitter' do
-    visit('/home')
-
-    fill_in 'new_peep_box', with: 'Hello World, this is my peep!'
-    click_on('Create Peep')
-
     expect(page.status_code).to eq 200
-    expect(page).to have_content('Hello World, this is my peep!')
+    expect(page).to have_content(@message1)
   end
 
   scenario 'Peeps appear in reverse chronological order' do
-    visit('/home')
-    message1 = 'Hello World, this is my peep!'
-    message2 = 'Hi, this is my second peep!'
-
-    fill_in 'new_peep_box', with: message1
+    fill_in 'new_peep_box', with: 'one'
     click_on('Create Peep')
-
-    fill_in 'new_peep_box', with: 'Hi, this is my second peep!'
+    fill_in 'new_peep_box', with: 'two'
     click_on('Create Peep')
-
-    expect(message2).to appear_before(message1)
+    expect('two').to appear_before('one')
   end
 
   scenario 'Peeps appear with time of peep' do
-    visit('/home')
-    message1 = 'Hello World, this is my peep!'
-    message2 = 'Hi, this is my second peep!'
-
-    fill_in 'new_peep_box', with: message1
-    click_on('Create Peep')
-    time = Time.now
-
-    expect(page).to have_content(time.strftime(" %I:%M %p "))
+    expect(page).to have_content(Time.now.strftime(" %I:%M %p "))
   end
 
-  scenario 'Peeps appear with date of peep' do
-    visit('/home')
-    message1 = 'Hello World, this is my peep!'
-    message2 = 'Hi, this is my second peep!'
+  scenario 'Peeps appears with date of peep' do
+    expect(page).to have_content(Time.now.strftime(" %d-%b-%Y "))
+  end
 
-    fill_in 'new_peep_box', with: message1
-    click_on('Create Peep')
-
-    time = Time.now
-    expect(page).to have_content(time.strftime(" %d-%b-%Y "))
+  scenario 'Peep appears with User Name' do
+    expect(page).to have_content("Rob: #{@message1}")
   end
 end
