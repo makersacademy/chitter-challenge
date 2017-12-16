226311f35a1685449f41ae374d2497fdbe607b4d
Keran <kerbraich@gmail.com>
2017-12-16 14:32:45 +0000

    add post /peeps

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,21 @@
 ENV["RACK_ENV"] ||= "development"
-require 'sinatra/base'
-require_relative 'data_mapper_setup'
+require "sinatra/base"
+require_relative "data_mapper_setup"
 
 class Chitter < Sinatra::Base
 
-  get '/peeps' do
+  get "/peeps/new" do
+    erb(:new_peeps)
+  end
+
+  get "/peeps" do
+    @peeps = Peep.all
     erb(:peeps)
+  end
+
+  post "/peeps" do
+    Peep.create(message: params[:message])
+    redirect("/peeps")
   end
 
   run! if app_file == $0
