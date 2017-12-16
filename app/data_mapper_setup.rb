c105c7084352d5b9694d1aebc6696c55a7064e59
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-12-16 19:08:47 +0000

    refactored, rubocop yaml file updated

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,7 +5,7 @@ require 'dm-migrations'
 require_relative 'models/message'
 require_relative 'models/user'
 
-#DataMapper::Logger.new($stdout, :debug)
+# DataMapper::Logger.new($stdout, :debug)
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
--- spec/chat_spec.rb
+++ spec/chat_spec.rb
@@ -3,9 +3,9 @@ require './lib/chat.rb'
 describe Chat do
   subject(:chat) { described_class.new }
   let(:message_class) { double(:my_message_class, all: messages) }
-  let(:message1) { double(:my_message_1, time: Time.now)}
-  let(:message2) { double(:my_message_2, time: (Time.now + 60))}
-  let(:messages)  { [message2, message1] }
+  let(:message1) { double(:my_message_1, time: Time.now) }
+  let(:message2) { double(:my_message_2, time: (Time.now + 60)) }
+  let(:messages) { [message2, message1] }
 
   it 'orders messages by time' do
     raw_msg_list = chat.find_msgs(message_class)
