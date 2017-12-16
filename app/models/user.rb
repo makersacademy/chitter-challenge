e15f443348753a0a62b83584bd381452b61caaf3
Lan Pham <lanpham312@gmail.com>
2017-12-16 23:38:00 +0000

    adding password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,13 @@ class User
   property :username, String
   property :password_digest, Text
 
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  validates_confirmation_of :password
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -5,4 +5,21 @@ feature 'User sign up' do
     expect(User.first.email).to eq('lan@example.com')
     expect(User.first.username).to eq('Lan')
   end
+
+  scenario 'asking user to confirm their password' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+  def sign_up(email:    'lan@example.com',
+              username: 'Lan',
+              password: '12345678',
+              password_confirmation: '12345678')
+    visit '/users/new'
+    fill_in :email, with: email
+    fill_in :username, with: username
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
+  end
+
 end
