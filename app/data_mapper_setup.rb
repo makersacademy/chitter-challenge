5b50495a2e249c2cb15076978ac188de90770419
Keran <kerbraich@gmail.com>
2017-12-16 11:56:24 +0000

    update postgres URL

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -2,6 +2,6 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative './models/peep'
 
-DataMapper.setup(:default, "postgres://localhost/chitter_test")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
