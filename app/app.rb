8273a6b8b12f144e2bbd2f8b7b474876a637e27b
Lan Pham <lanpham312@gmail.com>
2017-12-16 14:50:54 +0000

    refactoring to data_mapper_setup

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,8 @@
 ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require 'data_mapper'
+require_relative 'data_mapper_setup'
 require_relative 'models/peep'
 
 class Chitter < Sinatra::Base
--- app/models/peep.rb
+++ app/models/peep.rb
@@ -8,7 +8,3 @@ class Peep
   property :message, String
 
 end
-
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
