ad1d708a22ad4a983b178e1dab3f29c3dc1d2f9b
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-12-16 15:03:12 +0000

    users can post messages

--- app/chitter.rb
+++ app/chitter.rb
@@ -2,5 +2,17 @@ ENV['RACK_ENV'] = 'development'
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+
+  get '/chat' do
+    
+    erb :chat
+  end
+
+  post '/chat' do
+    session[:message] = params[:message]
+    redirect '/chat'
+  end
+
   run! if app_file == $0
 end
