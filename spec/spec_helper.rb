f9567779f006a18cd704b909f0db8e75b2617b07
Lan Pham <lanpham312@gmail.com>
2017-12-16 14:19:05 +0000

    adding database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 ENV["RACK_ENV"] = "test"
 require 'sinatra'
 require 'capybara'
+require 'database_cleaner'
 require 'rspec'
 require 'capybara/rspec'
 require 'simplecov'
@@ -23,5 +24,20 @@ RSpec.configure do |config|
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  # Everything in this block runs once before each individual test
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  # Everything in this block runs once after each individual test
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 end
