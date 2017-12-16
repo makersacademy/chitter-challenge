490aa0d7cddd291d608ef1b796e4a6720f0e034c
Keran <kerbraich@gmail.com>
2017-12-16 15:16:10 +0000

    add created_at property

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -1,9 +1,11 @@
 require "data_mapper"
 require "dm-postgres-adapter"
+require "dm-timestamps"
 
 class Peep
   include DataMapper::Resource
 
-  property :id,      Serial
-  property :message, String
+  property :id,         Serial
+  property :message,    String
+  property :created_at, DateTime
 end
