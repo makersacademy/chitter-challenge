5e7af8455458f33f8a9b09b4cf5fd1af1b0fc805
George Whiting <george_whiting@hotmail.com>
2017-12-16 22:18:51 +0000

    adds validation error messages and uniqueness requirement for name and username on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User
   include DataMapper::Resource
 
   property :id, Serial
-  property :email, String, required: true, format: :email_address
-  property :username, String, required: true
-  property :name, String, required: true
+  property :email, String, required: true, format: :email_address, unique: true
+  property :username, String, required: true, unique: true
+  property :name, String, required: true, unique: true
   property :secure_password, Text
 
   attr_reader :password
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -13,17 +13,39 @@ feature 'Sign up' do
 
   scenario 'cannot sign up with a blank email' do
     expect {sign_up(email: nil)}.not_to change(User, :count)
+    expect(page).to have_content 'Email must not be blank'
   end
 
   scenario 'cannot sign up with a blank name' do
     expect {sign_up(name: nil)}.not_to change(User, :count)
+    expect(page).to have_content 'Name must not be blank'
   end
 
   scenario 'cannot sign up with a blank username' do
     expect {sign_up(username: nil)}.not_to change(User, :count)
+    expect(page).to have_content 'Username must not be blank'
   end
 
   scenario 'cannot sign up with an invalid email' do
     expect {sign_up(email: 'invalidemail')}.not_to change(User, :count)
+    expect(page).to have_content 'Email has an invalid format'
+  end
+
+  scenario 'cannot sign up with an already taken email' do
+    sign_up
+    expect {sign_up}.not_to change(User, :count)
+    expect(page).to have_content 'Email is already taken'
+  end
+
+  scenario 'cannot sign up with an already taken name' do
+    sign_up
+    expect {sign_up}.not_to change(User, :count)
+    expect(page).to have_content 'Name is already taken'
+  end
+
+  scenario 'cannot sign up with an already taken Username' do
+    sign_up
+    expect {sign_up}.not_to change(User, :count)
+    expect(page).to have_content 'Username is already taken'
   end
 end
