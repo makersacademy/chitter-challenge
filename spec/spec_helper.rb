3e828d4f614e886f897a05b69a2363c1cef1d63b
Keran <kerbraich@gmail.com>
2017-12-16 14:33:03 +0000

    require database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV['RACK_ENV'] = 'test'
 require './app/app'
 require './app/models/peep'
 require 'capybara/rspec'
+require 'database_cleaner'
 require 'pry'
 require 'simplecov'
 require 'simplecov-console'
@@ -16,6 +17,19 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
