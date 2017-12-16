24afc0f57acdb8add8fdead784e4b6923cbc1f6f
Keran <kerbraich@gmail.com>
2017-12-16 12:33:53 +0000

    add peep html content

---
+++ app/app.rb
@@ -0,0 +1,13 @@
+ENV["RACK_ENV"] ||= "development"
+require 'sinatra/base'
+require_relative 'data_mapper_setup'
+
+class Chitter < Sinatra::Base
+
+  get '/peeps' do
+    erb(:peeps)
+  end
+
+  run! if app_file == $0
+
+end
