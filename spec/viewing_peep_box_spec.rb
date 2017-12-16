0270b97bb7051de2c03332502b4ce6241e147ab5
Keran <kerbraich@gmail.com>
2017-12-16 12:57:54 +0000

    replace single with double quotes

--- spec/viewing_peep_box_spec.rb
+++ spec/viewing_peep_box_spec.rb
@@ -1,11 +1,11 @@
 feature "Viewing the message box" do
   scenario "I can view the message box" do
-    visit('/peeps')
+    visit("/peeps/new")
     expect(page).to have_selector("textarea")
   end
 
   scenario "I can view the post button" do
-    visit('/peeps')
+    visit("/peeps/new")
     expect(page).to have_selector("input")
   end
 end
