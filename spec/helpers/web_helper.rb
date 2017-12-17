184c9d50d7e37d7c692135baf0e30e80235ea3d8
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-17 15:34:11 +0000

    Datacleaner enabled for test

--- spec/helpers/web_helper.rb
+++ spec/helpers/web_helper.rb
@@ -1,6 +1,10 @@
+module SessionHelpers
+
 def post_new_message
   visit '/messages/new'
   fill_in 'user_name', with: 'Nikolaos'
   fill_in 'message', with: 'MA Chitter is a cool app'
   click_button 'Peep'
+end
+
 end
