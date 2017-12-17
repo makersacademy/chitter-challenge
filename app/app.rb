6b2717b450d98ce3730846e6efba70bb34c19eb2
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-17 14:24:15 +0000

    User can post a message - post message - pass

--- app/app.rb
+++ app/app.rb
@@ -7,12 +7,31 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
 
+enable :sessions
+set :session_secret, 'secretpass'
+
 get '/' do
  erb :index
 end
 
+get '/messages' do
+  @user = session[:user]
+  @message = session[:message]
+  erb :'messages/messages'
+  end
 
-  # run if file is run directly by Ruby
-  run! if app_file == $0
+get '/messages/new' do
+  erb :'messages/new_messages'
+end
+
+post '/messages/new' do
+  session[:user] = params[:user_name]
+  session[:message] = params[:message]
+  redirect '/messages'
+end
+
+
+# run if file is run directly by Ruby
+run! if app_file == $0
 
 end
