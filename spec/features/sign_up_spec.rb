e407de8e040bd9b8cdb4d0b4d38e06460e6f8346
Xin Wang <wangxin00163@yahoo.co.uk>
2017-12-17 14:36:04 +0000

    mismatched password

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -5,4 +5,10 @@ feature 'has a functioning sign up page' do
     expect(page).to have_content "Welcome, yan@example.com"
     expect(User.first.email).to eq ('yan@example.com')
   end
+
+  scenario 'does not create new user if password is mismatched' do
+    expect { sign_up_badly }.to change(User, :count).by(0)
+    expect(page).to have_content "Password does not match the confirmation"
+  end
+
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -8,6 +8,16 @@ def sign_up
   click_button "Sign up"
 end
 
+def sign_up_badly
+  visit '/users/new'
+  fill_in :email, with: 'yan@example.com'
+  fill_in :name, with: 'Yan'
+  fill_in :username, with: 'yanny'
+  fill_in :password, with: 'password'
+  fill_in :password_confirmation, with: 'incorrect'
+  click_button "Sign up"
+end
+
 def post_a_peep(message)
   visit '/peeps/new'
   Peep.create(user_id: User.first.id, message: message)
