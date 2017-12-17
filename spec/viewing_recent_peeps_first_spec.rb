c52c1c2e20c7957e323d655cdbf0302886951df8
Keran <kerbraich@gmail.com>
2017-12-17 15:44:54 +0000

    update test for seeing recent peeps first

--- spec/viewing_recent_peeps_first_spec.rb
+++ spec/viewing_recent_peeps_first_spec.rb
@@ -1,9 +1,11 @@
 feature "Viewing peeps in reverse chronological order" do
   scenario "I can see the most recent peeps first" do
-    Peep.create(message: "My first peep!", created_at: DateTime.now - 1.0/(24*60)) # one minute
+    sign_up
+    post_a_peep
     visit("/peeps/new")
+    sleep(1) # try using timecop in the future
     fill_in :message, with: "My second peep!"
     click_button("Post")
-    expect(page.body.index("My second peep!") < page.body.index("My first peep!")).to eql(true)
+    expect("My second peep!").to appear_before("My first peep!")
   end
 end
