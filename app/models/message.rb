8918ad0736e4847004710d9df06f442ab0d72974
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-17 16:02:12 +0000

    User can view mesasges in chronological order  - time stamp messages - pass

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,11 @@
+require 'dm-timestamps'
+
  class Message
 
    include DataMapper::Resource
 
    property :id,       Serial
    property :message,  Text
+   property :created_at, DateTime
 
  end
