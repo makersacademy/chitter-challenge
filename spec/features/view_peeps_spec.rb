17b75501375eb8f9b1b402ef6cc9a66dac4f3df1
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-17 15:39:49 +0000

    User can view mesasges in chronological order  - board displays many messaged - fail

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,6 +1,7 @@
 feature 'users can see multiple messages in reverse chronological order' do
    scenario 'users can see multiple messages on the notice board' do
-     post_new_message
+     Message.create(message: 'MA Chitter is the best!')
      expect { post_new_message }.to change(Message, :count).by 1
+     expect(page).to have_content 'MA Chitter is the best!'
    end
  end
