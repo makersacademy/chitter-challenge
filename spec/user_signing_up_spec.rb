4d43ca6c2dba959fd0b082eabb8a94f9bbfc536d
Keran <kerbraich@gmail.com>
2017-12-17 20:20:43 +0000

    test for invalid email

--- spec/user_signing_up_spec.rb
+++ spec/user_signing_up_spec.rb
@@ -15,7 +15,21 @@ feature "User signs up" do
     fill_in :password_confirmation, with: "54321"
     click_button("Sign up")
     expect(current_path).to eq("/users")
-    message = "Password and confirmation password do not match"
+    message = "Invalid email, password and/or password confirmation"
+    expect(page).to have_content(message)
+  end
+
+  scenario "I am unable to sign up if my email is not in the right format" do
+    visit("/users/new")
+    expect(page.status_code).to eq(200)
+    fill_in :name,                  with: "John Smith"
+    fill_in :username,              with: "John"
+    fill_in :email,                 with: nil
+    fill_in :password,              with: "12345"
+    fill_in :password_confirmation, with: "54321"
+    click_button("Sign up")
+    expect(current_path).to eq("/users")
+    message = "Invalid email, password and/or password confirmation"
     expect(page).to have_content(message)
   end
 end
