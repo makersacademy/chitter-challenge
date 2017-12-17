259979a7746215102d3bd928839e656e41e6b672
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-17 14:05:28 +0000

    User can post a message - post message - fail

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -3,4 +3,13 @@ feature 'user can post a message (peep) to chitter' do
     visit '/'
     expect(page).to have_content 'Welcome to Chitter, the best replacement to Twitter'
   end
+
+  scenario 'user can add messages to the board' do
+    visit '/messages/new'
+    fill_in 'user_name', with: 'Nikolaos'
+    fill_in 'message', with: 'MA Chitter is a cool app'
+    click_button 'Peep'
+    expect(current_path).to eq '/messages'
+    expect(page).to have_content 'MA Chitter is a cool app'
+ end
 end
