d81a18c502d1f88cdca14224b3b51d00580f22da
Xin Wang <wangxin00163@yahoo.co.uk>
2017-12-17 11:25:58 +0000

    feature 3: time

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -7,6 +7,7 @@ class Peep
   property :id, Serial
   property :name, String
   property :message, String
+  property :created_at, DateTime
 
 
 end
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,12 +1,13 @@
 feature "Viewing peeps" do
 
   scenario "I can view all the peeps" do
-    Peep.create(message: "Morning London!")
+    peep = Peep.create(message: "Morning London!")
     visit('/peeps')
     expect(page.status_code).to eq 200
-    
+
     within 'ul#peeps' do
       expect(page).to have_content "Morning London!"
+      expect(page).to have_content "Posted at: #{peep.created_at}"
     end
   end
 end
