1e37d4542b5f7de0c7135adb7d5a712b47c0654f
Keran <kerbraich@gmail.com>
2017-12-17 18:56:41 +0000

    add validation datamapper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,21 @@ require "dm-validations"
 class User
   include DataMapper::Resource
 
-  property :id,              Serial
-  property :name,            String
-  property :username,        String
-  property :email,           String
-  property :password_digest, Text
+  attr_reader   :password
+  attr_accessor :password_confirmation
+
+  property :id,                    Serial
+  property :name,                  String
+  property :username,              String
+  property :email,                 String
+  property :password_digest,       Text
 
   has n, :peeps
 
-  def password_digest=(password)
-    super BCrypt::Password.create(password)
+  validates_confirmation_of :password
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
   end
 end
