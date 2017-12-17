a3dda2175872ba15cdeb38add5d49d518bfc52d2
Nikolaos Sakellarios <lunaticnick@hotmail.com>
2017-12-17 16:34:17 +0000

    User can view mesasges in chronological order  - time stamp messages & in order - fail

--- spec/helpers/web_helper.rb
+++ spec/helpers/web_helper.rb
@@ -3,8 +3,13 @@ module SessionHelpers
 def post_new_message
   visit '/messages/new'
   fill_in 'user_name', with: 'Nikolaos'
-  fill_in 'message', with: 'MA Chitter is a cool app'
+  fill_in 'message', with: 'MA Chitter is a cool app - 1'
   click_button 'Peep'
+end
+
+def post_other_message
+  Message.create(message: 'MA Chitter kicks Twitter A@@! - 2', created_at: Time.now + 60 )
+  visit '/messages'
 end
 
 end
