daac633ed1ce1e268702a30de7627c851decc2f7
Keran <kerbraich@gmail.com>
2017-12-17 16:29:48 +0000

    add click_button

--- spec/posting_peep_spec.rb
+++ spec/posting_peep_spec.rb
@@ -1,7 +1,7 @@
 feature "Posting a peep" do
   scenario "I can post a peep to the Chitter webpage" do
     sign_up
-    visit("/peeps/new")
+    click_button("Post a Peep")
     fill_in :message, with: "My first peep!"
     click_button("Post")
     expect(page).to have_content("My first peep!")
--- spec/viewing_recent_peeps_first_spec.rb
+++ spec/viewing_recent_peeps_first_spec.rb
@@ -2,7 +2,7 @@ feature "Viewing peeps in reverse chronological order" do
   scenario "I can see the most recent peeps first" do
     sign_up
     post_a_peep
-    visit("/peeps/new")
+    click_button("Post a Peep")
     sleep(1) # try using timecop in the future
     fill_in :message, with: "My second peep!"
     click_button("Post")
