e9bccdaa2552293b037a03ee0c4bd16c96863165
Eleanor Wintram <eleanorwintram@gmail.com>
2017-12-17 16:04:09 +0000

    updates test for time of peep

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,7 +1,6 @@
 feature "add peep" do
   before :each do
     user_sign_up
-    add_peep
   end
 
   scenario "peep count increases by 1" do
@@ -9,11 +8,12 @@ feature "add peep" do
   end
 
   scenario "user's peep is shown on the homepage" do
+    add_peep
     expect(current_path).to eq('/')
     within "ul#peeps" do
       expect(page).to have_content("If everybody minded their own business, the world would go around a great deal faster than it does.")
       expect(page).to have_content("Ellie W | @winnie")
-      expect(page).to have_content("17/12/17")
+      expect(page).to have_content(Time.new.strftime("%d/%m/%y at %H:%M"))
     end
   end
 
