23f542d418274834f795c490e8bf7dc88ac3e415
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-12-17 16:27:54 +0000

    multiple users can be tagged in a single peep

--- lib/taghandler.rb
+++ lib/taghandler.rb
@@ -6,17 +6,17 @@ module TagHandler
     message.content.split(' ').select { |word| word[0] == '@' }
   end
 
-  def check_tag_against_user(tag_string)
-    User.first(username: tag_string[1..-1])
+  def check_tag_against_user(tag_arr)
+    tag_arr.map { |tag_string| User.first(username: tag_string[1..-1]) }
   end
 
-  def create_tag(user, message)
-    Tag.create(user_id: user.id, message_id: message.id)
+  def create_tag(users, message)
+    users.each { |user| Tag.create(user_id: user.id, message_id: message.id) }
   end
 
   def tags(message)
     return nil if find_tag(message).empty?
-    user = check_tag_against_user(find_tag(message)[0])
+    user = check_tag_against_user(find_tag(message))
     user ? create_tag(user, message) : nil
   end
 
--- spec/features/tagging_spec.rb
+++ spec/features/tagging_spec.rb
@@ -7,4 +7,12 @@ feature 'Tagging' do
     expect(Tag.count).to eq 1
   end
 
+  scenario 'multiple users can be tagged in each peep' do
+    signup(email: 'jeff@hotmail.com', username: 'Jeff')
+    signup(email: 'jimbob@hotmail.com', username: 'JimBob')
+    signup
+    send_peep('Hey guys @JimBob @Jeff')
+    expect(Tag.count).to eq 2
+  end
+
 end
