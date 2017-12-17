691d626f0a89939c3d1519e893c834f3a2aa9a5a
Keran <kerbraich@gmail.com>
2017-12-17 18:57:15 +0000

    add test for mismatched passwords

--- spec/user_signing_up_spec.rb
+++ spec/user_signing_up_spec.rb
@@ -4,4 +4,18 @@ feature "User signs up" do
     expect(page).to have_content("Welcome, John!")
     expect(User.first.username).to eq("John")
   end
+
+  scenario "I am unable to sign up if passwords do not match" do
+    visit("/users/new")
+    expect(page.status_code).to eq(200)
+    fill_in :name,                  with: "John Smith"
+    fill_in :username,              with: "John"
+    fill_in :email,                 with: "john@gmail.com"
+    fill_in :password,              with: "12345"
+    fill_in :password_confirmation, with: "54321"
+    click_button("Sign up")
+    expect(current_path).to eq("/users")
+    message = "Password and confirmation password do not match"
+    expect(page).to have_content(message)
+  end
 end
