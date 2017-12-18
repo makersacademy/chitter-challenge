07964b820d6cbd8169965fdcb8f39c81a1e1fa3a
Samuel Johnson <samjohnson@samuels-mbp.home>
2017-12-18 08:17:42 +0000

    Added test for signing up new users

---
+++ spec/web_helper.rb
@@ -0,0 +1,7 @@
+def sign_up
+  visit('users/new')
+  expect(page.status_code).to eq(200)
+  fill_in :email, with: 'jimmy_the_dude@cool.com'
+  fill_in :password, with: 'password'
+  click_button('Register')
+end
