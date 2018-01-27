fc4d2dce9925ded250363e470ab088d0c8cdaa91
Lewis Moore <lewis.moore2707@gmail.com>
2018-01-27 15:44:19 +0000

    Add and install DB Cleaner

--- Gemfile
+++ Gemfile
@@ -6,11 +6,14 @@ gem 'rake'
 gem 'rubocop'
 gem 'data_mapper', '1.2.0'
 gem 'dm-postgres-adapter'
+gem 'dm-migrations'
+gem 'dm-validations'
 gem 'capybara'
 gem 'sinatra'
 
 group :test do
   gem 'rspec'
+  gem 'database_cleaner'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 ENV['RACK_ENV'] = 'test'
 require 'simplecov'
 require 'simplecov-console'
-
+require 'database_cleaner'
 require 'capybara/rspec'
 require './app/models/tweet'
 
@@ -18,9 +18,22 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
-  config.after(:suite) do
+config.before(:suite) do
+  DatabaseCleaner.strategy = :transaction
+  DatabaseCleaner.clean_with(:truncation)
+end
+
+# Everything in this block runs once before each individual test
+config.before(:each) do
+  DatabaseCleaner.start
+end
+
+# Everything in this block runs once after each individual test
+config.after(:each) do
+  DatabaseCleaner.clean
+end
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
+
 end
