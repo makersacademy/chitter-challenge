0c8dfabbfd5442ced49121358d1bc84559a94bd1
Ellie <elliewem@gmail.com>
2018-01-27 17:40:34 +0000

    Configure development and test environments

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -11,6 +11,6 @@ class Peep
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/peep_manager_test")
+DataMapper.setup(:default, "postgres://localhost/peep_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
