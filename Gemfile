4fc093bc2ceb03ee8e114b5dc5d02d0f8a467d60
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-01-27 16:38:42 +0000

    gem file faffs

--- Gemfile
+++ Gemfile
@@ -2,8 +2,12 @@ source 'https://rubygems.org'
 
 gem 'rake'
 gem 'rubocop'
+gem 'data_mapper', '1.2.0'
+gem 'dm-postgres-adapter'
+gem 'sinatra'
 
 group :test do
+  gem 'capybara'
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,13 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+ENV['RACK_ENV']="test"
+
+require './app/chitter'
+require 'rspec'
+require 'capybara'
+require 'capybara/rspec'
+require './app/models/peep'
+
+Capybara.app = Chitter
