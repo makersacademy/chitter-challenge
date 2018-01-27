d5d07dc2551a57d143f15d9b19f6306d94f43a39
rcvink <rrvink@gmail.com>
2018-01-27 21:27:53 +0000

    Upgrade DataMapper

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
-require_relative './model/user'
+# require_relative './model/user'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
