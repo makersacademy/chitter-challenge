1985639a80675e118c369b15d3df5e683f176ad1
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-01-27 17:26:24 +0000

    controller updated for post redirect

--- app/chitter.rb
+++ app/chitter.rb
@@ -6,8 +6,21 @@ require_relative 'data_mapper_setup'
 class Chitter < Sinatra::Base
 
   get '/' do
-    erb(:index)
+    redirect '/new/peeps'
   end
 
+  get '/new/peeps' do
+    erb(:add_peep)
+  end
+
+  post '/peeps' do
+    Peep.create(peep: params[:new_peep])
+    redirect to '/peeps'
+  end
+
+  get '/peeps' do
+    @peeps = Peep.all
+    erb(:peeps)
+  end
 
 end
