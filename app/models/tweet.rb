0b25f122f924ca50fa83b94602f5122d4f1703cc
Lewis Moore <lewis.moore2707@gmail.com>
2018-01-27 16:56:46 +0000

    add timestamps to tweets

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,6 @@
 require 'data_mapper'
 require 'dm-postgres-adapter'
+require 'dm-timestamps'
 
 class Tweet
   include DataMapper::Resource
@@ -7,5 +8,6 @@ class Tweet
   property :id, Serial
   property :subject, String
   property :text, String
+  property :created_at, Time
 
 end
