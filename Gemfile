28e779ac37a30bd8f446c3714b2d1988b18b1f0b
Ed <ea.litten@gmail.com>
2018-01-27 21:48:22 +0000

    Add test for showing peep timestamp

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ group :test do
   gem 'capybara'
   gem 'dm-rspec'
   gem 'pry'
+  gem 'timecop'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
 end
--- spec/features/view_peep_spec.rb
+++ spec/features/view_peep_spec.rb
@@ -6,6 +6,7 @@
 # So that I can better appreciate the context of a peep
 # I want to see the time at which it was made
 
+require 'timecop'
 
 feature 'View peep' do
     scenario 'User can view a peep on chitter' do
@@ -13,4 +14,12 @@ feature 'View peep' do
       visit '/'
       expect(page).to have_content("My first peep")
     end
+
+  scenario "I can see the time a peep was posted" do
+    Timecop.freeze do
+      Peep.create(body: 'Frozen in time!')
+      visit '/'
+      expect(page).to have_content(DateTime.now.strftime("%H:%M on %d %b %Y"))
+    end
   end
+end
