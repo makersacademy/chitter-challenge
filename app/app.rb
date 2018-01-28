54c5f2f705b211314687fa5a10a74b17ee2af24b
Tagrand <thomas.a.grand@gmail.com>
2018-01-28 12:15:18 +0000

    add welcome message to user

--- app/app.rb
+++ app/app.rb
@@ -1,15 +1,33 @@
 ENV['RACK_ENV'] ||= 'development'
 require 'sinatra/base'
-require "./app/models/cheep"
+require "./app/models/database_setup"
 
 class Chitter < Sinatra::Base
   enable :sessions
+
+  helpers do
+    def member_name
+      User.first.nil? ? "" : User.first.name
+    end
+  end
+
   get "/" do
     "Hello world"
   end
 
   get "/cheeps/new" do
     erb :new_cheep
+  end
+
+  get "/signup" do
+    erb :signup
+  end
+
+  post "/signup" do
+    User.create(:name => params[:name],
+                :username => params[:username],
+                :password => params[:password])
+    redirect "/cheeps"
   end
 
   post "/cheeps/new" do
