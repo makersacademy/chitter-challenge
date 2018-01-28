b36bf78c86df1d2e1052f6d8fbebe11632d87a1e
Alexy <alexandra.fina@yahoo.com>
2018-01-28 16:13:28 +0000

    Require rspec and capybara in spec_helper.rb

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -5,7 +5,7 @@ require 'dm-sqlite-adapter'
 require './app/models/maker.rb'
 require './app/models/peep.rb'
 
-p ENV['RACK_ENV']
+# p ENV['RACK_ENV']
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 # DataMapper.auto_upgrade!
