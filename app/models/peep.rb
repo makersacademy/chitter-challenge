1c0b85a3b2cdb255f0af26bc7b4dc55dadd9ff22
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-28 18:27:07 +0000

    tidies up peep model

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -1,7 +1,7 @@
 class Peep
   include DataMapper::Resource
 
-  property :id, Serial
-  property :message, Text
+  property :id,         Serial
+  property :message,    Text
   property :created_at, DateTime
 end
