dce436dd8476d9bfe133b257eff5bf6bec3626c1
Tagrand <thomas.a.grand@gmail.com>
2018-01-28 12:56:05 +0000

    refactor tests for signup

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,21 +1,26 @@
 feature 'signup' do
 
-  scenario 'I should be welcomed with my email after signing up' do
+  scenario 'I am stored in User table after signing up for the first time' do
+    expect { sign_up("dan", "dan0", "aa@aa.com", "maths") }.to change(User, :count).by(1)
+  end
+
+  background do
     sign_up_as_clement
+  end
+
+  scenario 'I should be welcomed with my email after signing up' do
     expect(page).to have_text "Welcome Clement!"
   end
 
-  scenario 'I am stored in User table after signing up for the first time' do
-    expect { sign_up_as_clement }.to change(User, :count).by(1)
-  end
-
   scenario 'I am not stored in User table after signing up with the same email twice' do
-    sign_up_as_clement
     expect { sign_up("cle", "attle", "cricket@hotmail.com", "ilovecricket") }.not_to change(User, :count)
   end
 
   scenario 'I am not stored in User table after signing up with the same username twice' do
-    sign_up_as_clement
+    expect { sign_up("cle", "Attles1883", "cricket2@hotmail.com", "ilovecricket") }.not_to change(User, :count)
+  end
+
+  scenario 'I am not stored in User table after signing up with the same username twice' do
     expect { sign_up("cle", "Attles1883", "cricket2@hotmail.com", "ilovecricket") }.not_to change(User, :count)
   end
 
