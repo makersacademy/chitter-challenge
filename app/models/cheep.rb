65522628dae32c595528afc02734f5d25de41034
Tagrand <thomas.a.grand@gmail.com>
2018-01-28 12:15:38 +0000

    move datamapper setup to separate file

--- app/models/cheep.rb
+++ app/models/cheep.rb
@@ -1,15 +1,7 @@
 
-require 'data_mapper'
-require 'dm-postgres-adapter'
-require 'dm-timestamps'
-
 class Cheep
   include DataMapper::Resource
   property :id, Serial
   property :message, String
   property :created_at, Time
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
