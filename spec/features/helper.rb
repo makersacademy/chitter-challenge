21e4d8ffa1e71617c0295c715ad01c4995b143c7
Tagrand <thomas.a.grand@gmail.com>
2018-01-28 12:29:13 +0000

    test new user is stored

--- spec/features/helper.rb
+++ spec/features/helper.rb
@@ -8,3 +8,11 @@ def add_hello_world_cheep
   visit 'cheeps/new'
   add_cheep("Hello world!")
 end
+
+def sign_up_as_clement
+  visit "/signup"
+  fill_in 'name', :with => "Clement"
+  fill_in 'username', :with => "Attlee83"
+  fill_in 'password', :with => "ilovecricket"
+  click_button "join chitter!"
+end
--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,12 +1,13 @@
 feature 'signup' do
 
   scenario 'I should be welcomed with my email after signing up' do
-    visit "/signup"
-    fill_in 'name', :with => "Clement"
-    fill_in 'username', :with => "Attlee83"
-    fill_in 'password', :with => "ilovecricket"
-    click_button "join chitter!"
+    sign_up_as_clement
     expect(page).to have_text "Welcome Clement!"
+  end
+
+  scenario 'I am stored in User table after signing up for the first time' do
+
+    expect{ sign_up_as_clement }.to change(User, :count).by(2) 
   end
 
 end
