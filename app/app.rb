ce06afe3b3f292571b2076458bfc107f925b5b2c
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-28 14:36:28 +0000

    updated login erb to store email if failed attempt for reuse

--- app/app.rb
+++ app/app.rb
@@ -47,12 +47,15 @@ class  Chitter < Sinatra::Base
   end
 
   post '/sessions' do
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
+    @user = User.authenticate(params[:email], params[:password])
+
+    if @user
+      session[:user_id] = @user.id
       redirect to('/home/homepage')
     else
       flash.now[:errors] = ['The email or password is incorrect']
+      @logemail = params[:email]
+      erb :'/sessions/new'
     end
   end
 
