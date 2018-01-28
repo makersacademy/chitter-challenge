33028e4e582e85db098c16ad10dcb6f42cf5bbc3
jenniferemshepherd <jennifer.elaine.mairead@gmail.com>
2018-01-28 22:07:41 +0000

    homepage tested

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -3,17 +3,11 @@
 # I want to post a message (peep) to chitter
 
 feature 'peeps' do
-#   scenario 'user can view a peep on chitter' do
-#     Peep.create(peep: 'My first peep')
-#     visit '/'
-#     expect(page).to have_content("My first peep")
-#   end
 
   scenario 'user can post a peep on chitter' do
     visit '/peeps/new'
     fill_in :new_peep, with: "My first peep"
     click_button 'peep!'
     expect(page).to have_content("My first peep")
-    # expect(page).to have_content("pm")
   end
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,7 +1,16 @@
+feature 'homepage' do
+  scenario 'there are options on the homepage' do
+    visit '/'
+    expect(page).to have_content("boring homepage")
+  end
+end
+
+
+
 feature 'signing up' do
   scenario 'user can sign up on the homepage' do
     expect{ sign_up_correctly}.to change(User, :count).by(1)
-    expect(page).to have_content("Welcome to Chitter, cat@cats.com")
+    expect(page).to have_content("Welcome to Chitter, catlover")
   end
 
   scenario 'user cannot sign up if passwords do not match' do
