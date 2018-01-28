db7620f8a19200ac4b031b8a6ca90af3c4304dd2
alextwilson <github@alextwilson.co.uk>
2018-01-28 17:10:32 +0000

    database tasks added to rakefile

--- Rakefile
+++ Rakefile
@@ -1,7 +1,22 @@
+require 'data_mapper'
+require './app/app.rb'
+
 if ENV['RACK_ENV'] != 'production'
   require 'rspec/core/rake_task'
-  
+
   RSpec::Core::RakeTask.new :spec
-  
+
   task default: [:spec]
+end
+
+desc "upgrade"
+task :upgrade do
+  DataMapper.auto_upgrade!
+  puts 'auto-upgrade complete'
+end
+
+desc "migrate"
+task :migrate do
+  DataMapper.auto_migrate!
+  puts 'auto-migrate complete'
 end
--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,10 +1,10 @@
 feature 'signup' do
   scenario 'Expect user count to have increased by one' do
-    expect{ sign_up }.to change(User, :count). by(1)
+    expect { sign_up } .to change(User, :count). by(1)
   end
 
   scenario 'User not created when wrong confirmation password given' do
-    expect{ bad_sign_up }.not_to change(User, :count)
+    expect { bad_sign_up } .not_to change(User, :count)
   end
 
   scenario 'Page shows intervention when User gives wrong password confirmation' do
