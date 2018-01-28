2ad47a3dd020d7089eb8408f3ab1255c180e87c1
domvernon <3686359+domvernon@users.noreply.github.com>
2018-01-28 18:20:02 +0000

    adds validation for unique email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
   include DataMapper::Resource
 
   property :id,                 Serial
-  property :email,              String, required: true
+  property :email,              String, format: :email_address, required: true, unique: true
   property :encrypted_password, String, length: 60
   property :name,               String
   property :username,           String
--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -5,13 +5,30 @@ feature 'User signup' do
     expect(User.first.email).to eq('someone@emailaddress.com')
   end
 
-  scenario 'Requires a matching confirmation password' do
-    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-    expect(current_path).to eq('/users')
-    expect(page).to have_content 'Password and confirmation password do not match'
-  end
+  context 'Validation errors for signup' do
+    scenario 'Requires a matching confirmation password' do
+      expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+      expect(current_path).to eq '/users'
+      expect(page).to have_content 'Password does not match the confirmation'
+    end
 
-  scenario 'Requires an email address for signup' do
-    expect { sign_up(email: nil) }.not_to change(User, :count)
+    scenario 'Requires an email address for signup' do
+      expect { sign_up(email: nil) }.not_to change(User, :count)
+      expect(current_path).to eq '/users'
+      expect(page).to have_content('Email must not be blank')
+    end
+
+    scenario 'Requires a valid email address for signup' do
+      expect { sign_up(email: 'someone@wrongemail') }.not_to change(User, :count)
+      expect(current_path).to eq '/users'
+      expect(page).to have_content('Email has an invalid format')
+    end
+
+    scenario 'Requires a unique email address' do
+      sign_up
+      expect { sign_up(username: 'different') }.to_not change(User, :count)
+      expect(current_path).to eq '/users'
+      expect(page).to have_content('Email is already taken')
+    end
   end
 end
