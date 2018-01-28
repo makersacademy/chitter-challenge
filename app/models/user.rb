efd9cc17b52267ffc577b6bdc3a571a985487fdf
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-28 11:52:56 +0000

    email validation complete, username validation done at browser stage

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,11 @@ class User
 
   property :id, Serial
   property :username, String
-  property :email, String
+  property :email, String, format: :email_address, required: true
   property :password_hash, Text
 
   # validates_confirmation_of :password
-  validates_format_of :email, as: :email_address
+  # validates_format_of :email, as: :email_address
 
   def password=(password)
     self.password_hash = BCrypt::Password.create(password)
