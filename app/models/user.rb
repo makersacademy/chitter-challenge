eadd731ebda1a6fa486523d0a4a1c5695d2864cd
Tagrand <thomas.a.grand@gmail.com>
2018-01-28 12:52:27 +0000

    make username unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User
   include DataMapper::Resource
   property :id, Serial
   property :name, String
-  property :username, String
+  property :username, String, :unique_index => true
   property :email, Text, :unique_index => true
   property :password, Text
 
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, :username
 end
 
 #:index=>true
