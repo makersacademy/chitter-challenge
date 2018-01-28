477e48b20bea9a328471c2f33d3a035a800948d8
Ed <ea.litten@gmail.com>
2018-01-28 14:08:27 +0000

    Add user story to user registration spec

--- spec/features/user_reg_spec.rb
+++ spec/features/user_reg_spec.rb
@@ -1,3 +1,7 @@
+# As a Maker
+# So that I can post messages on Chitter as me
+# I want to sign up for Chitter
+
 feature 'First-time user can register' do
   scenario 'User enters registration details and is added to db' do
     expect{ sign_up }.to change { User.count }.by 1
