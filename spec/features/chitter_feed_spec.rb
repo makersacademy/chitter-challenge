23015c3d5d23c8af8e2ac4cfa8c4b86707275f39
Tagrand <thomas.a.grand@gmail.com>
2018-01-28 09:29:16 +0000

    test cheep feed is ordered in reverse chronological

--- spec/features/chitter_feed_spec.rb
+++ spec/features/chitter_feed_spec.rb
@@ -1,12 +1,12 @@
 feature 'add a cheep' do
 
   scenario 'a new cheep should appear on the feed page' do
-    visit 'cheeps/new'
-    fill_in 'cheep', :with => 'Hello world!'
-    click_button 'send cheep!'
+    add_hello_world_cheep
+    sleep(1)
     fill_in 'cheep', :with => 'Bonjour world!'
     click_button 'send cheep!'
     click_button 'visit feed'
+    save_and_open_page
     expect('Bonjour world!').to appear_before('Hello world!')
   end
 
