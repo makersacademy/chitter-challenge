03dec8fe9c973fd68452c0d082edb8854d08cdaa
Noel Vock <noel1uk@gmail.com>
2018-02-16 08:52:58 +0000

    Added testing infrastructure test and created basic site infrastructure.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara/rspec'
+require 'rspec'
+
+# This allows the app to run. It requires 'capybara/rspec' and 'path_to_app'
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
