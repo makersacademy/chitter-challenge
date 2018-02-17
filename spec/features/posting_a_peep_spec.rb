b698264f94d0042159e7f581da38c15de172900e
Laura Chan <laurachan205@gmail.com>
2018-02-17 13:18:31 +0000

    Feature test for posting a peep

---
+++ spec/features/posting_a_peep_spec.rb
@@ -0,0 +1,7 @@
+feature 'Posting a peep' do
+  scenario 'Allows user to post a peep and see their peep on the page' do
+    visit('/')
+    fill_in('peep', with: 'Hello world!')
+    expect(page).to have_content 'Hello world!'
+  end
+end
