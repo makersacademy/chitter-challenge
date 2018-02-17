8b31b3520c91d9d8a81cb6bd6a599c636ba1da98
Stephen Tilley <stephentilley@gmail.com>
2018-02-17 23:37:36 +0000

    Peeps now created directly from /chitter page, files and tests updated, tests passing

--- app.rb
+++ app.rb
@@ -13,10 +13,6 @@ class Chitter < Sinatra::Base
     erb(:index)
   end
 
-  get '/chitter/new' do
-    erb(:new)
-  end
-
   post '/chitter' do
     Peep.create(params[:peep_text])
     redirect("/chitter")
--- spec/features/create_new_peep_spec.rb
+++ spec/features/create_new_peep_spec.rb
@@ -2,7 +2,6 @@ feature "Create new peep" do
 
   scenario "User can create a new peep and it is added to the list" do
     visit("/")
-    click_button("New peep")
     fill_in("peep_text", with: "A new peep")
     click_button("Submit")
     expect(page).to have_content("A new peep")
