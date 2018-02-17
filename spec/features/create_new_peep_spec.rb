1647256c46df6cda376646b340c64dda8fcbb81f
Stephen Tilley <stephentilley@gmail.com>
2018-02-17 21:55:58 +0000

    Created feature test for adding new peeps

---
+++ spec/features/create_new_peep_spec.rb
@@ -0,0 +1,10 @@
+feature "Create new peep" do
+
+  scenario "User can add a new peep and it is added to the list" do
+    visit("/")
+    click_button("new_peep")
+    fill_in("peep_text", with: "A new peep")
+    click_button("Submit")
+    expect(page).to have_content("A new peep")
+  end
+end
