55f3c83f2765b8e04e80111d081e7c49d716c933
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-18 11:28:04 +0000

    Storing user id in a session instead of pulling from database

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/flash'
 require_relative 'lib/cheet'
+require_relative 'lib/user'
 require 'rubygems'
 require 'data_mapper'
 require_relative 'database_connection_setup'
@@ -20,7 +21,24 @@ class Chitter < Sinatra::Base
 
   post '/cheets/new' do
     Cheet.add_cheet(params[:title], params[:body])
-    redirect '/'
+    redirect '/cheets'
+  end
+
+  get '/user/new' do
+    erb(:sign_up)
+  end
+
+  post '/user/new' do
+    @user = User.sign_up(params[:email], params[:password], params[:name], params[:username])
+    session[:user_id] = @user.id
+    redirect '/user'
+  end
+
+  get '/user' do
+    # TODO - currently taking user from persistent database. Would cause issues if another user signed up at some time.
+    # Need to create live logged in user to pull this from
+    @user = User.get(session[:user_id])
+    erb(:sign_up_successful)
   end
 
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,12 +1,13 @@
 feature 'sign up to Chitter' do
   scenario 'click sign up button, sign up with your details' do
     visit '/cheets'
+    click_button 'Sign up to Cheeter'
     fill_in('email', with: 'marcus@gmail.com')
     fill_in('password', with: 'password123')
     fill_in('name', with: 'Marco Polo')
     fill_in('user name', with: 'MarcoPo')
     click_button 'Sign up'
-    expect(page).to have content 'Congratulations, you have signed up.'
+    expect(page).to have_content 'Congratulations Marco Polo, you have signed up.'
     expect(page).to have_selector(:button, "Sign in")
     expect(page).to have_selector(:button, "Return to homepage")
   end
