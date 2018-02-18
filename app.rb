725153771bee7a881d7f6eb59d3b2c268b192301
kotpsot <zygmunt.justyna@gmail.com>
2018-02-18 14:08:45 +0000

    Logged in users don't have to insert their username to add a peep.

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class Chitter < Sinatra::Base
   end
 
   post '/peeps/new' do
-    redirect '/peeps' if Peep.create(params[:text], params[:author])
+    redirect '/peeps' if Peep.create(params[:text], session[:user_username])
     params[:text].chars.length > 240 ? flash[:n] = Flash.long_peep : flash[:n] = Flash.no_name
     redirect '/peeps'
   end
@@ -47,6 +47,7 @@ class Chitter < Sinatra::Base
     redirect '/sessions/new' if !User.matching_data(params[:username], params[:password])
     user = User.instanciate(params[:username])
     session[:user_id] = user.id
+    session[:user_username] = user.username
     flash[:n] = Flash.after_log_in(params[:username])
     redirect '/peeps'
   end
--- spec/features/adding_peeps_to_database_spec.rb
+++ spec/features/adding_peeps_to_database_spec.rb
@@ -6,7 +6,6 @@ feature "adding peeps to the database" do
     sign_up
     log_in
     fill_in "text", with: "Good day for coding"
-    fill_in "author", with: "Justyna"
     click_button("Add Peep")
     expect(page).to have_content "Justyna wrote: Good day for coding"
   end
@@ -17,18 +16,7 @@ feature "adding peeps to the database" do
     sign_up
     log_in
     fill_in "text", with: "W systematyce (należącego do nauki o literaturze) piśmiennictwa tekst literacki to dzieło literackie stanowiące przedmiot badania w odróżnieniu od utrwalonych wyników badań, tj. opracowań teoretycznych, historycznych i krytycznych (podręczników, monografii, rozpraw, recenzji itp.)"
-    fill_in "author", with: "Justyna"
     click_button("Add Peep")
     expect(page).to have_content "Your peep is too long. The max limit is 240 characters."
-  end
-end
-
-feature "adding peeps to the database" do
-  scenario "user didn't add name" do
-    sign_up
-    log_in
-    fill_in "text", with: "Good day for coding"
-    click_button("Add Peep")
-    expect(page).to have_content "Please enter your name."
   end
 end
