648ae14ee8225fdf36d106353ebe9fa27b6af2d8
Laura Chan <laurachan205@gmail.com>
2018-02-18 17:12:02 +0000

    Modify css

--- spec/features/order_of_peeps_spec.rb
+++ spec/features/order_of_peeps_spec.rb
@@ -4,9 +4,9 @@ feature 'Order of peeps' do
     new_peep = 'Programming has never been more fun'
     visit('/')
     fill_in('peep', with: old_peep)
-    click_button('Submit')
+    click_button('Post your peep')
     fill_in('peep', with: new_peep)
-    click_button('Submit')
+    click_button('Post your peep')
     expect(old_peep).to appear_below(new_peep)
   end
 end
--- spec/features/posting_a_peep_spec.rb
+++ spec/features/posting_a_peep_spec.rb
@@ -2,7 +2,7 @@ feature 'Posting a peep' do
   scenario 'Allows user to post a peep and see their peep on the page' do
     visit('/')
     fill_in('peep', with: 'Hello world!')
-    click_button('Submit')
+    click_button('Post your peep')
     expect(page).to have_content 'Hello world!'
   end
 end
