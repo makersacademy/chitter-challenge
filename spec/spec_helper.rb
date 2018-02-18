e49df81b6c4e143972b3b429445496d49c638b8c
Noel Vock <noel1uk@gmail.com>
2018-02-18 16:36:53 +0000

    Refactored spec_helper.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,20 @@
+ENV['ENVIRONMENT'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'simplecov'
 require 'simplecov-console'
 require 'capybara/rspec'
 require 'rspec'
+require 'rake'
+
+# Load the Rakefile
+Rake.application.load_rakefile
+
+# Then, in the RSpec config...
+RSpec.configure do |config|
+  config.before(:each) do
+    Rake::Task['populate'].execute
+  end
+end
 
 # This allows the app to run. It requires 'capybara/rspec' and 'path_to_app'
 Capybara.app = Chitter
@@ -13,10 +25,3 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
-
-RSpec.configure do |config|
-  config.before(:each) do
-  # system("rake setup")
-  system("rake populate")
-  end
-end
