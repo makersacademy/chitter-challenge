0ee28f82aed6d9f11b72731a8fbb5e1fb6cb8981
marcusfgardiner <marcusfgardiner@gmail.com>
2018-02-18 12:09:35 +0000

    In progress - encrypting user passwords

--- app.rb
+++ app.rb
@@ -35,7 +35,9 @@ class Chitter < Sinatra::Base
   end
 
   get '/user' do
-    @user = User.last
+    # TODO - currently taking user from persistent database. Would cause issues if another user signed up at some time.
+    # Need to create live logged in user to pull this from
+    @user = User.get(session[:user_id])
     erb(:sign_up_successful)
   end
 
