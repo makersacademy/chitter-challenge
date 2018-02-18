19c6e815a452232b04991b4419e7764c9f129b08
Noel Vock <noel1uk@gmail.com>
2018-02-18 11:12:19 +0000

    When a user name is already in the user table that user may now add a peep which is saved to the peep table with a related user_id

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require 'pg'
 
 class Chitter < Sinatra::Base
 
@@ -10,7 +11,30 @@ class Chitter < Sinatra::Base
   end
 
   post '/peep' do
+    p params['user']
+    user = params['user']
+    p params['peep']
     #1. Connect to database
+    connection = PG.connect(dbname: "chitter")
+    # find whether user is in database
+    result = connection.exec("SELECT * FROM users WHERE author = '#{user}'")
+    username = result.map { |user| user['author'] }
+    user_id = result.map { |user| user['id'] }
+    user_id = user_id[0]
+    peep = params['peep']
+    p user_id[0]
+    if username.empty?
+      p 'the user is not in the database'
+    else
+      p 'the user is in the database'
+      p peep
+      connection.exec("INSERT INTO peeps(user_id, peep) VALUES('#{user_id}', '#{peep}')")
+
+    end
+    p result
+    # connection.exec("INSERT INTO peeps(author) VALUES('Noel');")
+    # result = connection.exec("SELECT * FROM users")
+    # p result.map { |user| user['author'] }
 
     #2. Query the database with Insert sql
 
@@ -22,4 +46,4 @@ class Chitter < Sinatra::Base
 end
 
 
-# Add these classes to a module 
+# Add these classes to a module
