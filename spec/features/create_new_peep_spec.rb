cae37c4d2c27059263265b1d431314ef083fafd2
Stephen Tilley <stephentilley@gmail.com>
2018-02-19 00:33:26 +0000

    Cannot creates peeps without logging in

--- spec/features/create_new_peep_spec.rb
+++ spec/features/create_new_peep_spec.rb
@@ -1,10 +1,21 @@
 feature "Create new peep" do
 
   scenario "User can create a new peep and it is added to the list" do
-    visit("/")
+    User.create(name: 'Jim', username: 'Jimmy',
+      email: 'abc@123.com', password: 'abc123')
+
+    visit "/"
+    fill_in(:email, with: "abc@123.com")
+    fill_in(:password, with: "abc123")
+    click_button("Sign in")
     fill_in("peep_text", with: "A new peep")
     click_button("Submit")
     expect(page).to have_content("A new peep")
+  end
+
+  scenario "User cannot create a new peep if not logged in" do
+    visit("/")
+    expect(page).not_to have_button("Submit")
   end
 
 end
