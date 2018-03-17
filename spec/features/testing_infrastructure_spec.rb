3f94fb19bb7db7345cc600b5f5bde53ed7010d49
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-17 12:47:34 +0000

    Set up

---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,7 @@
+
+feature 'Testing infrastructure' do
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Testing infrastructure working!'
+  end
+end
