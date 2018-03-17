62f0f5ce9a066b182f6b66a83bad7a70ae5735b4
Alfie Darko <me@alfiedarko.co.uk>
2018-03-17 19:34:33 +0000

    added failing rspec for Timeline.user method

--- spec/timeline_spec.rb
+++ spec/timeline_spec.rb
@@ -9,4 +9,15 @@ describe Timeline, '.all' do
 
     expect(peep).to include('My first tweet!')
   end
+
+  describe '.user' do
+    it "displays the users tweets on his homepage timeline" do
+
+      result = Timeline.user(author)
+
+      user_timeline = result.map(&:text)
+
+      expect(user_timeline). to include('My personal tweet')
+    end
+  end
 end
