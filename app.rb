db1f07d68990009a3c3296dbf2dd2c03d4289398
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-03-17 17:03:18 +0000

    Add feature add_a_new_peep.rb red and green phase

--- app.rb
+++ app.rb
@@ -8,6 +8,17 @@ class Chitter < Sinatra::Base
     erb :index
   end
 
+  get '/add_a_new_peep' do
+    erb :add_a_new_peep
+  end
+
+  post '/create_new_peep' do
+  message = params['message']
+  connection = PG.connect(dbname: 'chitter_test')
+  connection.exec("INSERT INTO peeps (message) VALUES('#{message}')")
+  redirect '/'
+  end
+
   run! if app_file == $0
 
 end
