ba0de6c82142e00ac3814c61388078934c8e09b8
Charlene Bastians <charlenebastians@hotmail.com>
2018-03-17 23:37:38 +0000

    rspecs refactored

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,10 +1,22 @@
 require 'user'
 
 describe User do
-  describe '#create' do
-  it 'creates a new user' do
-    user = User.create(username: 'Tester', email: 'test@example.com', password: 'password123')
-    expect(user.id).not_to be_nil
+  before(:each) do
+    @user = User.create(username: 'Tester', email: 'test@example.com', password: 'password123')
+  end
+  describe '.all' do
+  it 'returns all users, wrapped in a User instance' do
+    expect(User.all.map(&:id)).to include @user.id
   end
 end
+  describe '#create' do
+  it 'creates a new user' do
+    expect(@user.id).not_to be_nil
+  end
+end
+  describe '#find' do
+    it 'finds a user by ID' do
+      expect(User.find(@user.id).email).to eq @user.email
+    end
+  end
 end
