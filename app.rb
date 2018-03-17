6d2653601f53f419fe0b8d1db69ec652000c20ac
danielcampos <ddregalo@gmail.com>
2018-03-17 18:19:32 +0000

    Create config file and homepage view with channel displayed - features passed

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
 require_relative './lib/chitter_connection_setup'
+require_relative './lib/peep'
 
 class Chitter < Sinatra::Base
 
-  get '/homepage' do
+  get '/' do
     @peeps_channel = Peep.channel
+    erb :homepage
   end
 
 end
---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,13 @@
+feature 'Homepage' do
+  scenario 'Displays the peep channel' do
+    visit('/')
+    expect(page).to have_content 'My first peep!'
+    expect(page).to have_content '@ddregalo'
+    expect(page).to have_content 'Daniel Campos'
+    expect(page).to have_content '2018-03-17 13:40:22'
+    expect(page).to have_content 'The world is flat.'
+    expect(page).to have_content '@bhavy'
+    expect(page).to have_content 'Bhav P'
+    expect(page).to have_content '2018-03-17 13:45:44'
+  end
+end
