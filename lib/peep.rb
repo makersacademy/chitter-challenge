c66741dcc5204ef112348e61865f18e29bd0401d
Vivian Allen <mrvivianallen@gmail.com>
2018-03-17 15:11:12 +0000

    peep class returning time-sorted array of peeps from database query, red->green

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,7 +2,12 @@ require 'database_connection'
 
 class Peep
 
+  attr_reader :id, :text, :time
+
   def initialize(id, text, time)
+    @id = id
+    @text = text
+    @time = time
   end
 
   def self.setup(db_connect = DatabaseConnection)
@@ -10,8 +15,20 @@ class Peep
   end
 
   def self.all
-    @con.query('SELECT * FROM peeps').map do |peep|
-      Peep.new(peep['id'], peep['text'], peep['time'])
-    end
+    self.sort(self.peepify(self.get_all))
+  end
+
+  private
+
+  def self.get_all
+    @con.query('SELECT * FROM peeps')
+  end
+
+  def self.peepify(rs)
+    rs.map{ |peep| Peep.new(peep['id'], peep['text'], peep['time']) }
+  end
+
+  def self.sort(peep_array)
+    peep_array.sort_by { |peep| peep.time }
   end
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -6,10 +6,15 @@ describe Peep do
     double :DatabaseConnection,
     query:
     [
-      { id: 1,
-        text: 'I am a peep',
-        time: Time.mktime(0).strftime("%Y-%D-%H:%M:%S")
-      }
+      {'id' => 1,
+        'text' => 'I am peep 1',
+        'time' => Time.mktime(0).strftime("%Y-%D-%H:%M:%S")},
+      {'id' => 3,
+        'text' => 'I am peep 3',
+        'time' => Time.mktime(2).strftime("%Y-%D-%H:%M:%S")},
+      {'id' => 2,
+        'text' => 'I am peep 2',
+        'time' => Time.mktime(1).strftime("%Y-%D-%H:%M:%S")}
     ]
   }
 
@@ -24,6 +29,10 @@ describe Peep do
     end
     it 'wraps the results from the database as an array of peep objects' do
       expect(described_class.all[0]).to be_an_instance_of(described_class)
+    end
+    it 'sorts the returned array by time' do
+      peep_array = described_class.all
+      expect(peep_array.map { |peep| peep.id }).to eq [1, 2, 3]
     end
   end
 end
