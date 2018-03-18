7bb554b4c48d186657a65210d102e3e3408469ce
Alfie Darko <me@alfiedarko.co.uk>
2018-03-18 22:38:50 +0000

    Moved simplecov formatter to the top of rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,16 @@
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
 ENV['RACK_ENV'] = 'test'
 ENV['ENVIRONMENT'] = 'test'
 
-require 'simplecov'
-require 'simplecov-console'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 # require File.join(File.dirname(__FILE__), '..', 'Rakefile')
 # Require all the testing gems
@@ -12,13 +20,6 @@ require 'rspec'
 require 'rake'
 
 Capybara.app = Chitter
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
 
 RSpec.configure do |config|
   config.before(:each) do
