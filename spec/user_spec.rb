68ceb6a9b47f1da943e4959f2bfb2f1a304b6b9f
Cindy <jl7e12@gmail.com>
2018-03-18 21:48:13 +0000

    .find tests passed

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -9,10 +9,23 @@ describe User do
   end
 
   describe '.all' do
-  it 'returns all users, wrapped in a User instance' do
-    user = User.create(email: "jl7e12@gmail.com", password: "password123", name: "cindyliu", username: "Cindy")
+    it 'returns all users, wrapped in a User instance' do
+      user = User.create(email: "jl7e12@gmail.com", password: "password123", name: "cindyliu", username: "Cindy")
 
-    expect(User.all.map(&:id)).to include user.id
+      expect(User.all.map(&:id)).to include user.id
+    end
   end
-end
+
+  describe '.find' do
+    it 'finds a user by id' do
+      user = User.create(email: "jl7e12@gmail.com", password: "password123", name: "cindyliu", username: "Cindy")
+
+      expect(User.find(user.id).email).to eq user.email
+    end
+
+    it 'returns nil if there is no id given' do
+    expect(User.find(nil)).to eq nil
+  end
+  end
+
 end
