713b3c9d64ee2b7b84338a509a23499b71857721
Reenakoudi <kreena92@gmail.com>
2018-03-18 01:29:44 +0000

    Adding a new peep implemented

--- chitter_app.rb
+++ chitter_app.rb
@@ -3,8 +3,19 @@ require './lib/peep.rb'
 
 class Chitter < Sinatra::Base
   get '/' do
-    @peeps = Peep.all 
+    @peeps = Peep.all
     erb :index
+  end
+
+  get '/add-a-new-peep' do
+    erb :add_a_new_peep
+  end
+
+  post '/create-a-peep' do
+    message = params['message']
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (message) VALUES('#{message}')")
+    redirect('/')
   end
 
   run! if app_file == $0
