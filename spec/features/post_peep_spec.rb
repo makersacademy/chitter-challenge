ca37447c5d6a4cc16c38e4369e6d22f9a5445dcc
paosch <paola_schiaffino@yahoo.co.uk>
2018-03-19 22:35:10 +0000

    Fixed rspec failures caused by latest changes

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -2,12 +2,12 @@ require 'capybara/rspec'
 
 feature 'Posting a peep' do
   scenario 'the user can see an empty input field' do
-    visit('/')
+    visit('/signed_in')
     expect(page).to have_selector('textarea')
   end
 
   scenario 'the user can write and post a peep' do
-    visit('/')
+    visit('/signed_in')
     fill_in('text', with: "test_peep")
     click_on 'Post peep'
     expect(page).to have_content "test_peep"
