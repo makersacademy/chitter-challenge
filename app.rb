d8f51043aa4dbd78b8fe7e2590be20bb4a315058
George Drayson <drayson.g@mac.com>
2018-03-19 08:17:38 +0000

    Made my code restful

--- app.rb
+++ app.rb
@@ -10,45 +10,49 @@ class Chitter < Sinatra::Base
   register Sinatra::Flash
 
   get '/' do
+    redirect to('/peeps')
+  end
+
+  get '/peeps' do
     @peeps = Peep.all.reverse
     @user = User.find(session[:user_id])
-    erb(:index)
+    erb :"peeps/index"
   end
 
-  post '/save_peep' do
+  post '/peeps' do
     Peep.add(params[:Comment_box], params[:Username_box], (Time.now).strftime('%r'))
-    redirect to('/')
+    redirect to('/peeps')
   end
 
-  get '/sign_up_page' do
-    erb(:signup)
+  get '/users/new' do
+    erb :"users/new"
   end
 
-  post '/save_user' do
+  post '/users' do
     user = User.create(params[:username], params[:email], params[:password])
     session[:user_id] = user.id
-    redirect to('/')
+    redirect to('/peeps')
   end
 
-  get '/sign_in_page' do
-    erb(:signin)
+  get '/sessions/new' do
+    erb :"sessions/new"
   end
 
-  get '/sign_out_page' do
-    session[:user_id] = nil
-    flash[:notice] = 'You have succesfully signed out'
-    redirect to('/')
-  end
-
-  post '/sign_in' do
+  post '/sessions' do
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect to('/')
+      redirect to('/peeps')
     else
       flash[:warning] = 'Please check your email or password'
-      redirect to('/sign_in_page')
+      redirect to('/sessions/new')
     end
+  end
+
+  get '/sessions/delete' do
+    session[:user_id] = nil
+    flash[:notice] = 'You have succesfully signed out'
+    redirect to('/peeps')
   end
 
   run! if app_file == $0
