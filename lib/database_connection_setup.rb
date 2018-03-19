87ee2faad934385d1e5b5cff46e4dd088c93c784
durranee <durranee@hotmail.com>
2018-03-19 23:06:57 +0000

    database_connection_setup.rb updated for heroku

--- lib/database_connection_setup.rb
+++ lib/database_connection_setup.rb
@@ -1,3 +1,5 @@
 require './lib/database_connection'
-db = ENV['ENVIRONMENT'] == 'test' ? 'chitter_test' : 'chitter'
+# db = ENV['ENVIRONMENT'] == 'test' ? 'chitter_test' : 'chitter'
+db = ENV['ENVIRONMENT'] == 'test' ? 'chitter_test' : 'd5dvkeooapmp51'
+
 DatabaseConnection.connect(db)
