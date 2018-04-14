1188fa988bbbe85d64755e61301b49475d5b5fc6
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-14 20:57:20 +0100

    07: app | hoem page | all peeps & CSS

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require 'sinatra'
+require './lib/peep.rb'
 
 class Chitter < Sinatra::Base
 
   enable :sessions
+
+  get '/' do
+    @peeps = Peep.all
+    erb :home
+  end
 
   run! if app_file == $0
 end
