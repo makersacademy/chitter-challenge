e4db0ae6ca1f86aba96943ad39eec1d5aca87ca1
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-14 23:32:17 +0100

    08: User | #self.create & #self.all & spec created

--- lib/user.rb
+++ lib/user.rb
@@ -13,4 +13,22 @@ class User
     @password = options[:password]
   end
 
+  def self.create(options)
+    con = Database::connect
+    con.exec "INSERT INTO users (name, email, username, password) VALUES ('#{options[:name]}', '#{options[:email]}', '#{options[:username]}', '#{options[:password]}')"
+  end
+
+  def self.all
+    con = Database::connect
+    con.exec('SELECT * FROM users').map do |user|
+      User.new(
+        id: user['id'],
+        name: user['name'],
+        email: user['email'],
+        username: user['username'],
+        password: user['password'],
+      )
+    end
+  end
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -24,4 +24,27 @@ describe User do
     expect(subject.password).to eq '12345'
   end
 
+  describe '#self.create' do
+    it 'should create the new data into database' do
+      User.create(name: 'createname', email: 'def@gmail.com', username: 'selfcreate', password: '45678')
+      con = PG.connect(dbname: 'chitter_test')
+      result = con.exec('SELECT * FROM users')
+      expect(result.to_a.last['name']).to eq 'createname'
+      expect(result.to_a.last['email']).to eq 'def@gmail.com'
+      expect(result.to_a.last['username']).to eq 'selfcreate'
+      expect(result.to_a.last['password']).to eq '45678'
+    end
+  end
+
+  describe '#self.all' do
+    it 'should create new user objects for each user from database ' do
+      User.create(name: 'name', email: 'ghi@gmail.com', username: 'selfall', password: '66666')
+      result = User.all.last
+      expect(result.name).to eq 'name'
+      expect(result.email).to eq 'ghi@gmail.com'
+      expect(result.username).to eq 'selfall'
+      expect(result.password).to eq '66666'
+    end
+  end
+
 end
