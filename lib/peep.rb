584f3a7a037a279253a4c64b543aed67e871deb6
Shih Han <swang05@mail.bbk.ac.uk>
2018-04-14 18:13:32 +0100

    06: Peep | #self.create & #self.all & spec created

--- lib/peep.rb
+++ lib/peep.rb
@@ -12,4 +12,27 @@ class Peep
     @time = options[:time]
   end
 
+  def self.create(options)
+    con = Database::connect
+    con.exec "INSERT INTO peeps (username, message, time) VALUES ('#{options[:username]}', '#{options[:message]}', '#{current_time}')"
+  end
+
+  def self.all
+    con = Database::connect
+    con.exec('SELECT * FROM peeps').map do |peep|
+      Peep.new(
+        id: peep['id'],
+        username: peep['username'],
+        message: peep['message'],
+        time: peep['time'])
+    end
+  end
+
+  private
+
+  def self.current_time
+    Time.now.strftime "%H:%M:%S | %a | %d-%b-%y"
+  end
+
+
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,23 +1,43 @@
 require 'peep'
+require 'pg'
 
 describe Peep do
 
   let(:subject){ described_class.new(id: 1, username: 'user', message: 'testing', time: '22:00 08/08/18' )}
 
-  it 'should stores the id' do
+  it 'should store the id' do
     expect(subject.id).to eq 1
   end
 
-  it 'should stores the username' do
+  it 'should store the username' do
     expect(subject.username).to eq 'user'
   end
 
-  it 'should stores the message' do
+  it 'should store the message' do
     expect(subject.message).to eq 'testing'
   end
 
-  it 'should stores the time' do
+  it 'should store the time' do
     expect(subject.time).to eq '22:00 08/08/18'
   end
-  
+
+  describe '#self.create' do
+    it 'should create the new data into database' do
+      Peep.create(username: 'selfcreate test', message: 'hello')
+      con = PG.connect(dbname: 'chitter_test')
+      result = con.exec('SELECT * FROM peeps')
+      expect(result.to_a.last['username']).to eq 'selfcreate test'
+      expect(result.to_a.last['message']).to eq 'hello'
+    end
+  end
+
+  describe '#self.all' do
+    it 'should create new peep objects for each peep from database ' do
+      Peep.create(username: 'selfall test', message: 'hi')
+      result = Peep.all.last
+      expect(result.username).to eq 'selfall test'
+      expect(result.message).to eq 'hi'
+    end
+  end
+
 end
