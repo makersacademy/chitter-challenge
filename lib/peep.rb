0ffc689207ca687bbc174db16936d8f268dac2aa
Gadiza Zerari <gadizaz@hotmail.com>
2018-04-15 20:42:07 +0100

    Refactor and self.create Peep with username, message and time

--- lib/peep.rb
+++ lib/peep.rb
@@ -5,21 +5,24 @@ require_relative './database_connection'
 class Peep
   attr_reader :id, :username, :message, :time
 
-  def initialize(id, username, message, time)
-    @id = id
-    @username = username
-    @message = message
-    @time = time
+  def initialize(options)
+    @id = options[:id]
+    @username = options[:username]
+    @message = options[:message]
+    @time = options[:time]
   end
   def self.all
     result = DatabaseConnection.query("SELECT * FROM peeps")
     result.map { |peep| peep['message'] }
   end
 
-  def self.create(params)
-    result = DatabaseConnection.query("INSERT INTO peeps(message) VALUES('#{params[:peep]}')")
-    # DatabaseConnection.query("INSERT INTO peeps(id, username, message, time) VALUES('#{params[:id]}', '#{params[:username]}', '#{params[:peep]}', '#{Time.now}')")
-    # Peep.new(result[0]['id'], result[0]['username'], result[0]['message'], result[0]['time'])
-    result.map { |peep| peep['message']}
+  def self.create(options)
+
+    DatabaseConnection.query("INSERT INTO peeps(username, message, time) VALUES('#{options[:username]}', '#{options[:peep]}', '#{Time.now}')")
+  end
+  private
+
+  def self.time
+    Time.now.strftime "%H:%M:%S | %a | %d-%b-%y"
   end
 end
