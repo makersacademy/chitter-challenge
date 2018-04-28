f68b4a7f1158d3feb74bf75875b3be98d9904e37
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-28 21:49:54 +0100

    fix: added additional db enviroment

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -2,6 +2,8 @@ require './lib/database_connection'
 
 if ENV['RACK_ENV'] == 'test'
   DatabaseConnection.setup('blahblah_test')
-else
+elsif ENV['RACK_ENV'] == 'development'
   DatabaseConnection.setup('blahblah')
+elsif ENV['RACK_ENV'] == 'production'
+  20.times { p "AHHHHHHHHHHHHHHHHHHHHHHHHH REAL MONSTERSSSSS" }
 end
