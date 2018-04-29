13ca2bdfa1f35ef8fc6a5ca818e9b6d0da784dff
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-29 12:06:36 +0100

    fix: db env comma fix

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -13,10 +13,10 @@ elsif ENV['RACK_ENV'] == 'development'
 elsif ENV['RACK_ENV'] == 'production'
   db_connection = {
     host: ENV['RDS_HOSTNAME'],
-    port: ENV['RDS_PORT']
+    port: ENV['RDS_PORT'],
     dbname: ENV['RDS_DB_NAME'],
     user: ENV['RDS_USERNAME'],
-    password: ENV['RDS_PASSWORD'],
+    password: ENV['RDS_PASSWORD']
   }
   DatabaseConnection.setup(db_connection)
 end
