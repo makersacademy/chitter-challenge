bf0d311b08658806458a844bfc3f5c88640c0a71
Hemesh-Unka <hemeshunka@gmail.com>
2018-04-29 20:09:58 +0100

    tests: updating tests to reflect db changes

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -4,14 +4,14 @@ describe DatabaseConnection do
 
   describe '.setup' do
     it 'sets up a connection through PG' do
-      expect(PG).to receive(:connect).with(dbname: 'blahblah_test')
-      DatabaseConnection.setup('blahblah_test')
+      expect(PG).to receive(:connect).with({ dbname: 'blahblah_test' })
+      DatabaseConnection.setup(dbname: 'blahblah_test')
     end
   end
 
   describe '.query' do
     it 'takes a SQL string and executes it' do
-      connection = DatabaseConnection.setup('blahblah_test')
+      connection = DatabaseConnection.setup(dbname: 'blahblah_test')
 
       expect(connection).to receive(:exec).with('SELECT * FROM messages')
       expect(DatabaseConnection.query('SELECT * FROM messages'))
