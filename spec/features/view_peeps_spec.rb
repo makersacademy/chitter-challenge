de64270c95704702bd7524972d0c735f38827eaa
Charly Mannion <charlottemannion100@gmail.com>
2018-05-12 16:57:38 +0100

    tests refactored

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -4,16 +4,18 @@ require 'peep'
 
 feature 'Peep Homepage' do
   scenario 'Can see peeps' do
-    p  peep_1 = Peep.create(:content => "Hello World", :username => "First the worst", :created_at => Time.now)
-    p  Peep.create(:content => "Second Peep", :username => "Second the best", :created_at => Time.now)
-    p  Peep.create(:content => "Peep Peep", :username => "Peeping Tom", :created_at => Time.now)
-    p  peep_1.content
-
+    Peep.create(:content => "Hello World", :username => "First the worst", :created_at => "21:00")
     visit '/peeps'
 
     expect(page).to have_content("Hello World")
-    expect(page).to have_content("Second the best")
-    expect(page).to have_content("Peep Peep")
-
+    expect(page).to have_content("First the worst")
   end
+
+  scenario 'Peeps have a time' do
+    Peep.create(:content => "Hello World", :username => "First the worst", :created_at => "21:00")
+    visit '/peeps'
+
+    expect(page).to have_content "21:00"
+  end
+
 end
