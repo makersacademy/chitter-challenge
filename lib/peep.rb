2ce0b8ed620455b6e76da94652010b98e19f80a2
William Powell <william.powell92@gmail.com>
2018-05-12 13:23:13 +0100

    almost implemented storing and returning of time on peeps

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,20 +1,21 @@
 require 'pg'
 
 class Peep
-  attr_reader :id, :text
+  attr_reader :id, :text, :time
 
-  def initialize(id, text)
+  def initialize(id, text, time)
     @id = id
     @text = text
+    @time = time
   end
 
   def self.all(peep_class = Peep, connection = connect_to_database)
     result = connection.exec 'SELECT * FROM peeps'
-    result.map { |row| peep_class.new(row['id'], row['text']) }
+    result.map { |row| peep_class.new(row['id'], row['text'], row['time']) }
   end
 
   def self.create(text, connection = connect_to_database)
-    connection.exec "INSERT INTO peeps(text) VALUES('#{text}')"
+    connection.exec "INSERT INTO peeps(text, time) VALUES('#{text}', '#{Time.now}')"
   end
 
   private
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,14 +1,15 @@
 require 'peep'
 
 fdescribe Peep do
+  let(:dummy_time) { Time.now }
   let(:peep1) { 'My first peep yoooo' }
   let(:peep2) { 'Just got this peep app! unbelievable!' }
   let(:peep3) { 'Peep is kicking off!' }
-  subject { described_class.new('1', peep1) }
+  subject { described_class.new('1', peep1, dummy_time) }
   let(:connection) { double :database_connection, exec: [
-    { 'id' => '1', 'text' => peep1 },
-    { 'id' => '1', 'text' => peep2 },
-    { 'id' => '1', 'text' => peep3 }
+    { 'id' => '1', 'text' => peep1, 'time' => dummy_time },
+    { 'id' => '1', 'text' => peep2, 'time' => dummy_time },
+    { 'id' => '1', 'text' => peep3, 'time' => dummy_time }
     ] }
   let(:peep_class) { double :peep, new: nil }
 
@@ -21,8 +22,9 @@ fdescribe Peep do
 
   describe '#self.create' do
     it 'calls insert command in SQL' do
+      allow(Time).to receive(:now).and_return(dummy_time)
       described_class.create(peep1, connection)
-      expect(connection).to have_received(:exec).with("INSERT INTO peeps(text) VALUES('#{peep1}')")
+      expect(connection).to have_received(:exec).with("INSERT INTO peeps(text, time) VALUES('#{peep1}', '#{dummy_time}')")
     end
   end
 
@@ -35,6 +37,12 @@ fdescribe Peep do
   describe '#text' do
     it 'returns text' do
       expect(subject.text).to eq peep1
+    end
+  end
+
+  describe '#time' do
+    it 'returns a time' do
+      expect(subject.time).to eq dummy_time
     end
   end
 end
