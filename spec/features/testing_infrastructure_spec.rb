c06364ccbddb0c44022be3f1832f03d0e5b65450
Ellie Keen <elliekeen95@gmail.com>
2018-05-13 16:12:18 +0100

    Remove redundant testing_infrastructure feature test

--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,9 +0,0 @@
-require './app.rb'
-require 'spec_helper'
-
-feature Chitter do
-  scenario 'Testing infrastructure' do
-    visit '/'
-    expect(page).to have_content 'Testing infrastructure working'
-  end
-end
