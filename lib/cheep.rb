f583e467bcf901975cf1c51bb017c83405d4571f
Ellie Keen <elliekeen95@gmail.com>
2018-05-13 22:17:47 +0100

    Add id property to Cheep

--- lib/cheep.rb
+++ lib/cheep.rb
@@ -5,32 +5,29 @@ class Cheep
   TEST_DB = 'chitter_test'
   PROD_DB = 'chitter'
 
-  attr_reader :name, :handle, :message, :time
+  attr_reader :name, :handle, :message, :time, :id
 
-  def initialize(name, handle, message, time)
+  def initialize(name, handle, message, time, id)
     @name = name
     @handle = handle
     @message = message
     @time = time
+    @id = id
   end
 
   def self.create(name, handle, message, time = Time.now)
     connection = PG.connect(dbname: "#{self.set_database}")
     result = connection.exec(self.add_to_database(name, handle, message, time))
     Cheep.new(result.first['name'], result.first['handle'],
-              result.first['message'], result.first['time'])
+              result.first['message'], result.first['time'], result.first['id'])
   end
 
   def self.all
     connection = PG.connect(dbname: "#{self.set_database}")
     result = connection.exec(self.return_data)
     result.map do |cheep|
-      Cheep.new(cheep["name"], cheep["handle"], cheep["message"], cheep["time"])
+      Cheep.new(cheep["name"], cheep["handle"], cheep["message"], cheep["time"], cheep["id"])
     end
-  end
-
-  def ==(other)
-    @handle = other.handle
   end
 
   private
@@ -40,9 +37,8 @@ class Cheep
   end
 
   def self.add_to_database(name, handle, message, time)
-    "INSERT INTO cheeps (name, handle, message, time)
-    VALUES('#{name}','#{handle}','#{message}','#{time}')
-    RETURNING name, handle, message, time"
+    "INSERT INTO cheeps VALUES('#{name}','#{handle}','#{message}','#{time}', DEFAULT)
+    RETURNING name, handle, message, time, id"
   end
 
   def self.return_data
--- spec/cheep_spec.rb
+++ spec/cheep_spec.rb
@@ -6,7 +6,8 @@ describe Cheep do
   let(:name) { 'Celine Dione' }
   let(:message) { 'My heart will go on' }
   let(:time) { Time.now }
-  let(:cheep_instance) { Cheep.new(name, handle, message, time) }
+  let(:id) { 20 }
+  let(:cheep_instance) { Cheep.new(name, handle, message, time, id) }
 
   it "should respond to handle" do
     expect(cheep_instance.handle).to eq handle
@@ -24,27 +25,22 @@ describe Cheep do
     expect(cheep_instance.time).to eq time
   end
 
+  it "should respond to id" do
+    expect(cheep_instance.id).to eq id
+  end
+
   describe ".create" do
     it "should create a new Cheep" do
-      cheep = Cheep.create(name, handle, message, time)
-      expect(Cheep.all).to include cheep
+      expect{ Cheep.create(name, handle, message, time) }.to change{ Cheep.all.length }.from(0).to(1)
     end
   end
 
   describe ".all" do
     it "should return all Cheeps" do
       cheep = Cheep.create(name, handle, message, time)
-      cheep_2 = Cheep.create('Whitney', '@houston', 'I wanna dance with somebody', time)
-      expected_result = [cheep, cheep_2]
-      expect(Cheep.all).to eq(expected_result)
+      cheep_2 = Cheep.create(name, handle, message, time)
+      expect(Cheep.all.length).to eq 2
     end
   end
 
-  describe '#==' do
-    it 'two Cheeps are equal if their handles match' do
-      cheep_1 = Cheep.new(name, handle, message, time)
-      cheep_2 = Cheep.new(name, handle, message, time)
-      expect(cheep_1).to eq cheep_2
-    end
-  end
 end
