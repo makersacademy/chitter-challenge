95f69c2fd9908507416ce08e1ee9d0bc6e7c0e26
Ellie Keen <elliekeen95@gmail.com>
2018-05-13 16:09:51 +0100

    Add Create and All class methods to Cheep

--- lib/cheep.rb
+++ lib/cheep.rb
@@ -1,12 +1,43 @@
+require 'pg'
+
 class Cheep
 
-  attr_reader :handle, :name, :message, :time
+  TEST_DB = 'chitter_test'
+  PROD_DB = 'chitter'
 
-  def initialize(handle, name, message)
-    @handle = handle
+  attr_reader :name, :handle, :message, :time
+
+  def initialize(name, handle, message, time)
     @name = name
+    @handle = handle
     @message = message
-    @time = Time.now
+    @time = time
   end
 
+  def self.create(name, handle, message, time = Time.now)
+    connection = PG.connect(dbname: "#{self.set_database}")
+    result = connection.exec("INSERT INTO cheeps (name, handle, message, time)
+                              VALUES('#{name}','#{handle}','#{message}','#{time}')
+                              RETURNING name, handle, message, time")
+    Cheep.new(result.first['name'], result.first['handle'],
+              result.first['message'], result.first['time'])
+  end
+
+  def self.all
+    connection = PG.connect(dbname: "#{self.set_database}")
+    result = connection.exec("SELECT * FROM cheeps")
+    result.map do |cheep|
+      Cheep.new(cheep["name"], cheep["handle"], cheep["message"], cheep["time"])
+    end
+  end
+
+  def ==(other)
+    @handle = other.handle
+  end
+
+  private
+
+  def self.set_database
+    ENV['ENVIRONMENT'] == 'test' ? TEST_DB : PROD_DB
+  end
 end
--- spec/cheep_spec.rb
+++ spec/cheep_spec.rb
@@ -1,30 +1,50 @@
 require 'cheep'
-require 'pg'
-require 'timecop'
 
 describe Cheep do
 
   let(:handle) { '@celinedione' }
   let(:name) { 'Celine Dione' }
   let(:message) { 'My heart will go on' }
-  let(:cheep) { Cheep.new(handle, name, message) }
+  let(:time) { Time.now }
+  let(:cheep_instance) { Cheep.new(name, handle, message, time) }
 
-  it "should return the user's handle" do
-    expect(cheep.handle).to eq handle
+  it "should respond to handle" do
+    expect(cheep_instance.handle).to eq handle
   end
 
-  it "should return the user's name" do
-    expect(cheep.name).to eq name
+  it "should respond to name" do
+    expect(cheep_instance.name).to eq name
   end
 
-  it "should return the user's message" do
-    expect(cheep.message).to eq message
+  it "should respond to message" do
+    expect(cheep_instance.message).to eq message
   end
 
-  it "should return the time of the user's cheep" do
-    time = Time.now
-    Timecop.freeze(time)
-    expect(cheep.time).to eq time
+  it "should respond to time" do
+    expect(cheep_instance.time).to eq time
   end
 
+  describe ".create" do
+    it "should create a new Cheep" do
+      cheep = Cheep.create(name, handle, message, time)
+      expect(Cheep.all).to include cheep
+    end
+  end
+
+  describe ".all" do
+    it "should return all Cheeps" do
+      cheep = Cheep.create(name, handle, message, time)
+      cheep_2 = Cheep.create('Whitney', '@houston', 'I wanna dance with somebody', time)
+      expected_result = [cheep, cheep_2]
+      expect(Cheep.all).to eq(expected_result)
+    end
+  end
+
+  describe '#==' do
+    it 'two Cheeps are equal if their handles match' do
+      cheep_1 = Cheep.new(name, handle, message, time)
+      cheep_2 = Cheep.new(name, handle, message, time)
+      expect(cheep_1).to eq cheep_2
+    end
+  end
 end
