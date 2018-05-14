bbf7c3c0bcb456b923f269d2d1ee7eb1a765c266
Asia Antczak <aasiaantczak@gmail.com>
2018-05-14 22:13:40 +0100

    changed spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,6 @@ end
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
@@ -31,6 +30,3 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # Want a nice code coverage website? Uncomment this next line!
   SimpleCov::Formatter::HTMLFormatter
 ])
-SimpleCov.start do
-  add_filter './Rakefile'
-end
