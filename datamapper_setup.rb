c2f471df12b6f9ee5eac60f34a31dca424385f74
George Sykes <georgesykes86@gmail.com>
2018-05-14 20:41:55 +0100

    change travis.yml for postgres

--- datamapper_setup.rb
+++ datamapper_setup.rb
@@ -4,9 +4,9 @@ require 'pry'
 require 'pry-byebug'
 
 if ENV['RACK_ENV'] == 'test'
-  DataMapper.setup(:default, 'postgres://georgesykes@localhost/chitter_test')
+  DataMapper.setup(:default, 'postgres://localhost/chitter_test')
 else
-  DataMapper.setup(:default, 'postgres://georgesykes@localhost/chitter')
+  DataMapper.setup(:default, 'postgres://localhost/chitter')
 end
 
 DataMapper.auto_migrate!
