f384f1259b89fe033f277d08e5c4af840bdb7cb7
Nick <nicholasjohnrichardson@gmail.com>
2018-06-09 18:51:08 +0100

    User Story 3
    As a Maker
    So that I can better appreciate the context of a peep
    I want to see the time at which it was made

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,23 +1,25 @@
 require 'pg'
+require 'time'
 require_relative './database_connection'
 
 
 class Peep
-  attr_reader :id, :peep
+  attr_reader :id, :peep, :time
 
-  def initialize(id, peep)
+  def initialize(id, peep, time)
     @id = id
     @peep = peep
+    @time = time
   end
 
   def self.save(options)
-    result = DatabaseConnection.query("INSERT INTO peep_log (peep) VALUES('#{options[:peep]}') RETURNING id, peep;")
-    Peep.new(result[0]['id'], result[0]['peep'])
+    result = DatabaseConnection.query("INSERT INTO peep_log (peep, time) VALUES('#{options[:peep]}', '#{Time.new.strftime("%H:%M %d/%m/%y")}') RETURNING id, peep;")
+    Peep.new(result[0]['id'], result[0]['peep'], result[0]['time'])
   end
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM peep_log ORDER BY id DESC;")
-    result.map { |peep| Peep.new(peep['id'], peep['peep']) }
+    result.map { |peep| Peep.new(peep['id'], peep['peep'], peep['time']) }
   end
 
 end
