7fbe524217039fe527c2e2d8017cc35c2a19cef5
Robert Faldo <robertfaldo@gmail.com>
2018-06-09 12:03:51 +0100

    green tests and refactored for Film.create and Film.all

--- lib/film.rb
+++ lib/film.rb
@@ -1,26 +1,40 @@
 class Film
 
+  attr_reader :title
+
   def initialize(options)
     @title = options[:title]
     @rating = options[:rating]
   end
 
-  def self.all
-    if ENV['ENVIRONMENT'] = 'test'
-      connection = PG.connect(dbname: 'faldo_movie_ratings_test')
-    else
-      connection = PG.connect(dbname: 'faldo_movie_ratings')
-    end
-
-    result = connection.exec("SELECT * FROM films")
-    result.map{ |film| film }
-    # result.map{ |film| Film.new(title: film['title'], rating: film['rating']) }
+  def ==(other)
+    @title == other.title
   end
 
-  # def self.create(options)
-  #   connection = PG.connect(dbanme: 'faldo_movie_ratings')
-  #
-  #   result =
-  # end
+  def self.all
+    connection = connect_to_correct_database
+    result = connection.exec("SELECT * FROM films")
+    result.map{ |film| Film.new(title: film['title'], rating: film['rating']) }
+  end
+
+  def self.create(options)
+    connection = connect_to_correct_database
+
+    result = connection.exec("INSERT INTO films (title, rating)
+      VALUES('#{options[:title]}', '#{options[:rating]}')
+      RETURNING title, rating;")
+    Film.new(title: result.first['title'],
+      rating: result.first['rating'])
+  end
+
+  private
+
+  def self.connect_to_correct_database
+    if ENV['ENVIRONMENT'] = 'test'
+      PG.connect(dbname: 'faldo_movie_ratings_test')
+    else
+      PG.connect(dbname: 'faldo_movie_ratings')
+    end
+  end
 
 end
--- spec/film_spec.rb
+++ spec/film_spec.rb
@@ -1,26 +1,45 @@
 describe Film do
-  it 'User can see a list of films' do
+
+  it 'returns all films in an array' do
     connection = PG.connect(dbname: 'faldo_movie_ratings_test')
 
-    connection.exec("INSERT INTO films (title, rating) VALUES('Film Title', '7');")
+    film_1 = Film.create(title: 'Film 1 Title', rating: '10')
+    film_2 = Film.create(title: 'Film 2 Title', rating: '10')
+    film_3 = Film.create(title: 'Film 3 Title', rating: '10')
 
-    film = {
-      "title" => "Film Title",
-      "rating" => "7"
-    }
+    films = [
+      film_1,
+      film_2,
+      film_3
+    ]
 
-    expect(Film.all).to include(film)
+    expect(Film.all).to eq(films)
   end
 
-
-
-  xit 'User can add a film' do
-    # Setup
-    film = {title: "Movie Title",rating: "8"}
-    # Exercise
-    Film.create(film)
-    # Verification
+  it 'creates a new film' do
+    film = Film.create(title: 'Shawshank Redemption', rating: "10")
     expect(Film.all).to include film
+  end
+
+  xit 'user gets error if enter rating over 10' do
+
+  end
+
+  xit 'user gets error if they enter negative number' do
+
+  end
+
+  xit 'user gets error if they enter title over 60 chars' do
+
+  end
+
+  describe '#==' do
+    it 'two bookmarks are equal if their ids match' do
+      bookmark_1 = Film.new(title: "Pulp Fiction", rating: '9')
+      bookmark_2 = Film.new(title: "Pulp Fiction", rating: '9')
+
+      expect(bookmark_1).to eq(bookmark_2)
+    end
   end
 
 end
