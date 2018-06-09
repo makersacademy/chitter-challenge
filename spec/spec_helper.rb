8263c6d732f40f646aeffcd533f3800b99248698
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-09 20:44:43 +0100

    edited spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,21 @@
+require 'simplecov'
+require 'simplecov-console'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
+
+
+
+
 ENV['ENVIRONMENT'] = 'test'
 
 require 'rspec'
 require 'capybara'
 require 'capybara/rspec'
-require 'simplecov'
-require 'simplecov-console'
 require_relative './setup_test_database'
 
 require File.join(File.dirname(__FILE__), '..', './app.rb')
@@ -17,12 +28,8 @@ RSpec.configure do |config|
 	end
 end
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+
+
 
 RSpec.configure do |config|
   config.after(:suite) do
