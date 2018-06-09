fe568a76835ab63fc131fcb33f5384a22df1bddb
Simone <simonecaplin@gmail.com>
2018-06-09 12:10:16 +0100

    Post a peep and see it displayed on the screen

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
+  end
+
+  post '/posts' do
+    session[:peep] = params['peep']
+    redirect '/posts'
+  end
+
+  get '/posts' do
+    @peeps = session[:peep]
+    erb :posts
   end
 
   run! if app_file == $0
---
+++ spec/features/post_message_feature_spec.rb
@@ -0,0 +1,13 @@
+# User Story 1
+# As a Maker
+# So that I can let people know what I am doing
+# I want to post a message (peep) to chitter
+
+feature 'Post a peep to Chitter' do
+  scenario 'user can write a peep and post it' do
+    visit ('/')
+    fill_in('peep', with: "My first peep")
+    click_button('Post')
+    expect(page).to have_content ("My first peep")
+  end
+end
