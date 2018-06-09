967310babdd89aa8a7dd491aae337d0d60ac7fcb
Robert Faldo <robertfaldo@gmail.com>
2018-06-09 15:10:50 +0100

    added feature test for user putting in rating over 10

--- spec/features/post_a_film_spec.rb
+++ spec/features/post_a_film_spec.rb
@@ -16,25 +16,34 @@ feature 'Add a film' do
     expect(page).to have_content("8/10")
   end
 
-  scenario 'User must complete rating field' do
-    visit("/")
-    click_button("addfilm")
-    fill_in("title", with: "Example Film Title")
-    click_button("Submit")
-    # Verify
-    expect(page).to have_content("You must enter all fields to submit")
-    expect(page).not_to have_content("Example Film Title")
+  feature 'Validates user input' do
+    scenario 'user doesnt enter rating' do
+      visit("/")
+      click_button("addfilm")
+      fill_in("title", with: "Example Film Title")
+      click_button("Submit")
+      # Verify
+      expect(page).to have_content("You must enter all fields to submit")
+      expect(page).not_to have_content("Example Film Title")
+    end
+    scenario 'user doesnt enter title' do
+      visit("/")
+      click_button("addfilm")
+      fill_in("rating", with: "7")
+      click_button("Submit")
+      # Verify
+      expect(page).to have_content("You must enter all fields to submit")
+      expect(page).not_to have_content("7")
+    end
+    scenario 'user puts in rating over 10' do
+      visit("/")
+      click_button("addfilm")
+      fill_in("title", with: "Example Film Title")
+      fill_in("rating", with: "11")
+      click_button("Submit")
+      # Verify
+      expect(page).to have_content("You must enter all fields to submit")
+      expect(page).not_to have_content("11")
+    end
   end
-
-  # scenario 'User must complete title field' do
-  #   visit("/")
-  #   click_button("addfilm")
-  #   fill_in("rating", with: "7")
-  #   click_button("Submit")
-  #   # Verify
-  #   expect(page).to have_content("You must enter all fields to submit")
-  #   expect(page).not_to have_content("7")
-  # end
-
-
 end
