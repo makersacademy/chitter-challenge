ac92b4d066d791f785308dbf70f03ceee4fb2394
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-09 16:00:28 +0100

    Set up Sinatra framework
    * Gemfile set up.
    * spec_helper.rb configured for Capybara and test environment variables
    * Controller tested with infrastructure feature test.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
+ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+require 'features/feature_web_helpers'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -14,4 +24,5 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+  # add in before clause to truncate the data base. 
 end
