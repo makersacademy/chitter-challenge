9945c61cd7dc94ffabb31480910a2cfbe78385d4
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-10 21:58:51 +0100

    formatting fix

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -2,9 +2,9 @@ require 'pg'
 
 def setup_test_database!
 
-	# p 'Resetting test database...'
-	 connection = PG.connect(dbname: 'chitter_challenge_test')
-	 connection.exec("TRUNCATE chitter RESTART IDENTITY;")
-     connection.exec("TRUNCATE users RESTART IDENTITY;")
+    # p 'Resetting test database...'
+    connection = PG.connect(dbname: 'chitter_challenge_test')
+    connection.exec("TRUNCATE chitter RESTART IDENTITY;")
+    connection.exec("TRUNCATE users RESTART IDENTITY;")
 
 end
