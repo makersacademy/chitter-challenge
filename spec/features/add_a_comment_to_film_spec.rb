daf608ac04e8f046f9101e9a85d83866a5f26fe4
Robert Faldo <robertfaldo@gmail.com>
2018-06-10 12:58:38 +0100

    added feature test for adding comments to film

---
+++ spec/features/add_a_comment_to_film_spec.rb
@@ -0,0 +1,19 @@
+# As a user
+# So that I can let people know what I think of a film
+# I want to add a comment to the film rating
+
+feature 'Add a comment to a film' do
+  scenario 'user can add a comment to a film' do
+    visit("/")
+    add_film_with_rating("Inception", "9")
+    # Setup
+    comment = "This is a comment about the film that has been reviewed,
+      it's got a bunch of text and some numbers!! 012345"
+    # Exercise
+    click_button("comment-Inception")
+    fill_in('add_comment', with: comment)
+    click_button("Submit")
+    # Verify
+    expect(page).to have_content(comment)
+  end
+end
--- spec/teardown_test_database.rb
+++ spec/teardown_test_database.rb
@@ -1,4 +1,4 @@
-def setup_test_database!
+def teardown_test_database
   connection = PG.connect(dbname: 'faldo_movie_ratings_test')
   connection.exec("TRUNCATE films, comments;")
 end
