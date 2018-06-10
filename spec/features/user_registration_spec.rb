ba2d97a82b8a2d8f1dfb4cb40de4a59cd9200c6f
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-10 18:39:53 +0100

    removed redundant test from user_registration_spec

--- spec/features/user_registration_spec.rb
+++ spec/features/user_registration_spec.rb
@@ -6,7 +6,6 @@ feature 'registration' do
         fill_in('email', with: 'test@example.com')
         fill_in('password', with: 'password123')
         click_button('Submit')
-        save_and_open_page
         expect(page).to have_content 'welcome, @myname'
 
     end
