7f44155788379134e42da28f57e24df86a64e7fe
Benjamin Tomkins <bentomkins@hotmail.com>
2018-06-10 14:39:59 +0100

    rubocop can bite me

--- app.rb
+++ app.rb
@@ -5,24 +5,28 @@ require 'uri'
 
 class ChitterClallenge < Sinatra::Base
     
-  register Sinatra::Flash
-  enable :sessions
+    register Sinatra::Flash
+    enable :sessions
 
-  get '/' do
-    @peeps = Peep.read
-    erb :index
-  end
 
-  post '/' do
-    oops = 'you peeped too long!'
-    flash[:notice] = oops unless Peep.create(peep: params['peep'])
-    redirect '/'
-  end
+    get '/' do
+        @peeps = Peep.read
+        erb :index
+    end
 
-  error 404 do
-    '404'
-  end
+    
+    post '/' do
+        oops = 'you peeped too long!'
+        flash[:notice] = oops unless Peep.create(peep: params['peep'])
+        redirect '/'
+    end
 
-  run! if app_file == $PROGRAM_NAME
+
+    error 404 do
+        '404'
+    end
+
+
+    run! if app_file == $PROGRAM_NAME
 
 end
--- lib/peep.rb
+++ lib/peep.rb
@@ -2,51 +2,58 @@ require 'pg'
 
 class Peep
     
-  attr_reader :id, :peep, :timestamp
+    attr_reader :id, :peep, :timestamp
 
     
-  def initialize(id, peep, timestamp)
-    @id = id
-    @peep = peep
-    @timestamp = timestamp
-  end
+    def initialize(id, peep, timestamp)
+        @id = id
+        @peep = peep
+        @timestamp = timestamp
+    end
 
-  def ==(peep)
-    @id == peep.id
-  end
 
-  def self.create(text)
-    return false unless long?(text[:peep])
+    def ==(peep)
+        @id == peep.id
+    end
 
-    sql_query = "INSERT INTO chitter (peep)
-                 VALUES('#{text[:peep]}')
-                 RETURNING id, peep, time_stamp"
 
-    result = database.exec(sql_query)
-    Peep.new(result.first['id'], 
-             result.first['peep'], 
-             result.first['time_stamp'])
-  end
+    def self.create(text)
+        return false unless long?(text[:peep])
 
-  def self.read
-    sql_query = "SELECT * FROM chitter ORDER BY time_stamp DESC"
-    database.exec(sql_query).map { |peep|
-      Peep.new(peep['id'], peep['peep'], peep['time_stamp'])
-    }        
-  end
+        sql_query = "INSERT INTO chitter (peep)
+                     VALUES('#{text[:peep]}')
+                     RETURNING id, peep, time_stamp"
 
-  def time
-    Time.parse(timestamp).strftime('%B %d')
-  end
+        result = database.exec(sql_query)
+        Peep.new(result.first['id'], 
+                 result.first['peep'], 
+                 result.first['time_stamp'])
+    end
 
-  private_class_method def self.long?(peep)
-    peep.length <= 140
-  end
 
-  private_class_method def self.database
-    ENV['ENVIRONMENT'] == 'test'  \
-        ? PG.connect(dbname: 'chitter_challenge_test')  \
-        : PG.connect(dbname: 'chitter_challenge')
-  end
+    def self.read
+        sql_query = "SELECT * FROM chitter 
+                     ORDER BY time_stamp DESC"
+
+        database.exec(sql_query).map { |peep|
+            Peep.new(peep['id'], peep['peep'], peep['time_stamp']) }        
+    end
+
+
+    def time
+        Time.parse(timestamp).strftime('%B %d')
+    end
+
+
+
+    private_class_method def self.long?(peep)
+        peep.length <= 140
+    end
+
+    private_class_method def self.database
+        ENV['ENVIRONMENT'] == 'test'  \
+            ? PG.connect(dbname: 'chitter_challenge_test')  \
+            : PG.connect(dbname: 'chitter_challenge')
+    end
 
 end
