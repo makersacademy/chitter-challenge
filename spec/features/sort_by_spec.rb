196bf914ac425884ab635d71bcd68b5e5d1843d4
Robert Faldo <robertfaldo@gmail.com>
2018-06-11 10:38:13 +0100

    fixed bug in test for sorting by spec where it was hardcoded with the date

--- spec/features/sort_by_spec.rb
+++ spec/features/sort_by_spec.rb
@@ -1,16 +1,16 @@
 feature 'Sort by' do
   scenario 'user can sort alphabetically' do
     visit("/")
-    allow_any_instance_of(Time).to receive(:now).and_return("2018-06-10 11:25:40 +0100")
+    allow_any_instance_of(Time).to receive(:now).and_return(Time.now.strftime("%Y-%m-%d"))
     add_film_with_rating("Cars", "5")
     add_film_with_rating("A Bugs Tale", "8")
     add_film_with_rating("Back to the Future", "9")
 
     click_button("sort-alphabetically")
 
-    expect(page).to have_content("A Bugs Tale 8/10 2018-06-10 Comment
-      Back to the Future 9/10 2018-06-10 Comment
-      Cars 5/10 2018-06-10 Comment")
+    expect(page).to have_content("A Bugs Tale 8/10 #{Time.now.strftime("%Y-%m-%d")} Comment
+      Back to the Future 9/10 #{Time.now.strftime("%Y-%m-%d")} Comment
+      Cars 5/10 #{Time.now.strftime("%Y-%m-%d")} Comment")
   end
 
   xscenario 'user can sort by rating' do
