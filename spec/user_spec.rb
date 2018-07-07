e4515a11a8bf54fd21e32968c3e081dbd7f71d04
Elishka Flint <elishkaf@gmail.com>
2018-07-07 17:31:36 +0100

    TEST Add unit tests to prevent duplicate emails and usernames

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -20,20 +20,15 @@ describe User do
       expect(names).to include 'Chewbacca'
       expect(usernames).to include 'chewbacca'
     end
+    it 'returns false if the database already includes the given username' do
+      expect(User.create('Han Solo', 'hansolo', 'han_solo@gmail.com', 'pa$$w0rd1')).to be false
+    end
+    it 'returns false if the database already includes the given email' do
+      expect(User.create('Han Solo', 'han_solo', 'hansolo@gmail.com', 'pa$$w0rd1')).to be false
+    end
+    it 'returns false if an email is not valid' do
+      expect(User.create('Darth Vader', 'darth_vader', 'some email', 'pa$$w0rd1')).to be false
+    end
   end
-
-  # describe '.current_user' do
-  #   context 'when a new user is created' do
-  #     it 'flags that user as the current user' do
-  #       user = User.create('Chewbacca', 'chewbacca', 'chewbacca@gmail.com', 'pa$$w0rd4')
-  #       expect(User.current_user).to eq user
-  #     end
-  #     it 'switches from one user to another' do
-  #       user1 = User.create('Chewbacca', 'chewbacca', 'chewbacca@gmail.com', 'pa$$w0rd4')
-  #       user2 = User.create('Darth Vader', 'darth_vader', 'darthvader@gmail.com', 'pa$$w0rd5')
-  #       expect(User.current_user).to eq user2
-  #     end
-  #   end
-  # end
 
 end
