3cb9f4388613ceec3f4d28fa0ca43b7a95304941
Elishka Flint <elishkaf@gmail.com>
2018-07-07 13:26:49 +0100

    MODEL Add .create and .all methods to the Peep class

---
+++ lib/peep.rb
@@ -0,0 +1,31 @@
+class Peep
+
+  attr_reader :id, :content, :username
+
+  def initialize(id, content, username)
+    @id = id
+    @content = content
+    @username = username
+  end
+
+  def self.create(user_id, content)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: "chitter_test")
+    else
+      connection = PG.connect(dbname: "chitter")
+    end
+    result = connection.exec("INSERT INTO peeps (user_id, content) VALUES('#{user_id}','#{content}') RETURNING id, user_id, content")
+    User.new(result.first['id'], result.first['user_id'], result.first['content'])
+  end
+
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: "chitter_test")
+    else
+      connection = PG.connect(dbname: "chitter")
+    end
+    result = connection.exec("SELECT peeps.id, peeps.content, users.username FROM peeps INNER JOIN users ON peeps.user_id = users.id")
+    result.map { |peep| Peep.new(peep['id'], peep['content'], peep['username']) }
+  end
+
+end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,29 +3,27 @@ require 'peep'
 describe Peep do
 
   before(:each) do
-    star_wars_fan = User.create('Star Wars Fan', 'star_wars_fan', 'star_wars_fan@gmail.com','pa$$w0rd')
+    @star_wars_fan = User.create('Star Wars Fan', 'star_wars_fan', 'star_wars_fan@gmail.com','pa$$w0rd')
   end
 
-  describe '.all/.create' do
-    it 'shows all peeps' do
-      Peep.create(star_wars_fan.id,'Laugh it up, fuzzball')
-      peeps = Peeps.all
+  describe '.create' do
+    it 'creates a peep with a user id' do
+      Peep.create(@star_wars_fan.id,'Laugh it up, fuzzball')
+      peeps = Peep.all
       contents = peeps.map(&:content)
       expect(contents).to include 'Laugh it up, fuzzball'
     end
   end
 
+  describe '.all' do
+    it 'shows all peeps and their associated users' do
+      Peep.create(@star_wars_fan.id,'Laugh it up, fuzzball')
+      peeps = Peep.all
+      usernames = peeps.map(&:username)
+      expect(usernames).to include 'star_wars_fan'
+    end
+  end
 end
 
-#   describe '.create' do
-#     it 'creates a new peep in the database' do
-#       Peep.create(star_wars_fan.id,'Laugh it up, fuzzball')
-#       peeps = Peep.all
-#       contents = peeps.map(&:content)
-#       expect(contents).to include 'Laugh it up, fuzzball'
-#     end
-#   end
-#
-#
 # 'I\'m Luke Skywalker and I\'m here to rescue you!'
 # 'Help me Obi Wan Kenobi, you\'re my only hope'
