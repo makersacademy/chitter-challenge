1154ee6aed4bbd534d7f10442387dc62ca07746e
Jay Khan <jkhan992@gmail.com>
2018-07-08 12:26:33 +0100

    Feature tets: creating a new message (peep)

---
+++ spec/features/create_message_spec.rb
@@ -0,0 +1,16 @@
+require './chitter_app.rb'
+
+# As a Maker
+# So that I can let people know what I am doing
+# I want to post a message (peep) to chitter
+
+feature 'Creating a new message to Chitter' do
+  scenario 'a user can create a message' do
+    visit('/messages/new')
+    fill_in('message', with: 'This is a test message')
+    click_button('Add meesage')
+
+    expect(current_path).to eq '/messages'
+    expect(page).to have_content 'This is a test message'
+  end
+end
