6b28896790dfed8b9312edbdbbf8941221980733
Elishka Flint <elishkaf@gmail.com>
2018-07-08 08:21:05 +0100

    FEATURE Add log in form to a sessions/new route

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ class Chitter < Sinatra::Base
     erb :sign_up
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
   post '/sign-up' do
     if User.invalid_username?(params[:username])
       flash[:notice]="Sorry that username or email is already taken. Please try again."
