da269decd9a70da359f094ea9dfdb794ab14402b
limcuili <limcuili@hotmail.com>
2018-08-04 14:49:35 +0100

    Testing infrastructure works

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,18 @@
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# Require all the testing gems
+require 'capybara'
+require 'capybara/rspec'
+require 'pg'
+require 'rspec'
+
+# Tell Capybara to talk to BookmarkManager
+Capybara.app = Chitter
+
+
 require 'simplecov'
 require 'simplecov-console'
 
@@ -10,7 +25,6 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
