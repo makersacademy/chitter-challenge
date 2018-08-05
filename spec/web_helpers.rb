4958462b8e707e20478154fa078d2fa54bf9c1bf
Angela Wolff <warriorwomenblog@gmail.com>
2018-08-05 23:28:40 +0100

    Code refactoring

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,7 +1,7 @@
 def insert_test_peeps  
-  peep_1 = Peep.create(username: 'whatapalaver', peep: 'Test peep 1 - Deep')
-  peep_2 = Peep.create(username: 'whatapalaver', peep: 'Test peep 2 - Deeper')
-  peep_3 = Peep.create(username: 'whatapalaver', peep: 'Test peep 3 - Deepest')
+  Peep.create(username: 'whatapalaver', peep: 'Test peep 1 - Deep')
+  Peep.create(username: 'whatapalaver', peep: 'Test peep 2 - Deeper')
+  Peep.create(username: 'whatapalaver', peep: 'Test peep 3 - Deepest')
 end
 
 def signup_test_user
