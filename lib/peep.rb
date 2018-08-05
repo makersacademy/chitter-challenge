90dab85c718846060f567e0fe2327337a469b1c1
Tim Blanchard <tim.blanchard@hotmail.co.uk>
2018-08-05 11:13:19 +0100

    added code which outputs peep text into box

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,19 +2,20 @@ require 'pg'
 class Peep
   def self.all
     connection = if ENV['ENVIRONMENT'] == 'test'
-                   PG.connect(dbname: 'bookmark_manager_test')
+                   PG.connect(dbname: 'chitter_test')
                  else
-                   PG.connect(dbname: 'bookmark_manager')
+                   PG.connect(dbname: 'chitter')
                  end
-    result = connection.exec('SELECT * FROM bookmarks')
-    result.map { |bookmark| bookmark['url'] }
+    result = connection.exec('SELECT * FROM peeps')
+      result.map{ |peep| peep['peep'] }
+    
   end
 
   def self.create(options)
     connection = if ENV['ENVIRONMENT'] == 'test'
-                   PG.connect(dbname: 'bookmark_manager_test')
+                   PG.connect(dbname: 'chitter_test')
                  else
-                   PG.connect(dbname: 'bookmark_manager')
+                   PG.connect(dbname: 'chitter')
     end
     connection.exec("INSERT INTO bookmarks (url) VALUES ('#{options[:url]}')")
   end
