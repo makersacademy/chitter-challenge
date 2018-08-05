8f69844e42fa9a3362a789d15d95e10eeb8b6615
Marek <MarekSchmidt@outlook.com>
2018-08-05 19:43:39 +0100

    Changed how peep dates display, updated to true sorting methods, not just reverse. Added charlimit to peep textfield

--- models/peep.rb
+++ models/peep.rb
@@ -10,7 +10,22 @@ class Peep
       connection = PG.connect(dbname: 'chitter_test')
     end
     result = connection.exec "TABLE peeps"
-    result.map { |peep| { :name => peep['name'], :peep => peep['peep'], :time => peep['time'], :date => peep['date']} }
+    resarray = result.map { |peep| { :name => peep['name'].gsub(/[†]/,"'"), :peep => peep['peep'].gsub(/[†]/,"'"), :time => peep['time'], :date => peep['date']} }
+    all = Array.new
+    resarray.each do |hash|
+      pushed_hash = false
+      all.each do |new_hash|
+        if new_hash.has_key?(hash[:date])
+          new_hash[hash[:date]].push(hash)
+          pushed_hash = true
+          new_hash[hash[:date]] = sortbytime(new_hash[hash[:date]])
+        end
+      end
+      if pushed_hash == false
+        all.push({hash[:date] => [hash]})
+      end
+    end
+    return sortbydate(all)
   end
 
   def self.add(name, peep_text)
@@ -19,7 +34,24 @@ class Peep
     elsif ENV['RACK_ENV'] == 'test'
       connection = PG.connect(dbname: 'chitter_test')
     end
-    connection.exec "INSERT INTO peeps (name, peep, time, date) VALUES ('#{name.gsub(/[']/,"\\\\'")}', '#{peep_text.gsub(/[']/,"\\\\'")}', '#{Time.now.strftime('%H:%M:%S')}', '#{Time.now.strftime('%Y:%m:%d')}')"
+    connection.exec "INSERT INTO peeps (name, peep, time, date) VALUES ('#{name.gsub(/[']/,"†")}', '#{peep_text.gsub(/[']/,"†")}', '#{Time.now.strftime('%H:%M:%S')}', '#{Time.now.strftime('%Y-%m-%d')}')"
+  end
+
+  def self.convert_date(date)
+    return "Today" if date == Date.today.to_s
+    return "Yesterday" if date == Date.today.prev_day.to_s
+    return date
+  end
+
+  private
+
+  def self.sortbytime(array)
+    # Why do I not have to call
+    return array.sort_by {|hash| hash[:time]}
+  end
+
+  def self.sortbydate(array)
+    return array.sort_by {|hash| hash.keys[0]}.reverse
   end
 
 end
