9eef07addc0c2bce65b9c4505d5e1fb6bc7e2505
Daniel den Hartog <daniel.mista@gmail.com>
2018-08-06 01:40:29 +0100

    finished

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -2,17 +2,28 @@ require 'peep.rb'
 
 describe Peep do
 
-  subject(:peep) { described_class.new }
+  #subject(:peep) { described_class.new }
 
   describe '.all' do
     it 'should return all messages' do
+      Peep.add("Daniel", 'In')
+      Peep.add("Salimatou", 'Love')
+      Peep.add("Thomas", 'My twin')
 
+      expected_peeps = [
+        "#{Time.new.strftime("%m/%d/%Y")} at #{Time.new.strftime("%I:%M%p")} : Daniel - In",
+        "#{Time.new.strftime("%m/%d/%Y")} at #{Time.new.strftime("%I:%M%p")} : Salimatou - Love",
+        "#{Time.new.strftime("%m/%d/%Y")} at #{Time.new.strftime("%I:%M%p")} : Thomas - My twin"
+      ]
+
+      expect(Peep.all).to eq expected_peeps
     end
   end
 
   describe '.add' do
     it 'should let a user add a message' do
-
+      Peep.add("Daniel", 'Hi')
+      expect(Peep.all).to include "#{Time.new.strftime("%m/%d/%Y")} at #{Time.new.strftime("%I:%M%p")} : Daniel - Hi"
     end
   end
 
