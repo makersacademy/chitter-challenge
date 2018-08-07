8ca6b2f184a27ae1af53d56a9ca85fa2613ba067
Kirtiman Singh <kirt93@hotmail.com>
2018-08-07 11:09:46 +0100

    user story 1 complete: can post a peep

--- app.rb
+++ app.rb
@@ -9,12 +9,13 @@ register Sinatra::Flash
     erb :index
   end
   post '/peep' do
-    Peep.post(params['name'],params['username'], params['peep'])
+    Peep.post(params[:name], params[:username], params[:peep])
     redirect '/peeps'
   end
   get '/peeps' do
     @peeps = Peep.view_peeps
     erb :peeps
+    
   end
     run! if app_file == $0
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,12 +1,15 @@
 require 'peep'
 
 describe Peep do
+
   describe '.post' do
     it "posts a peep" do
-      name = "luke"
-      username = "skywalker"
-      peep = "This is great fun!"
-      subject.post(name, username, peep)
+      # name = "luke"
+      # username = "skywalker"
+      # peep = "This is great fun!"
+      # Peep.post('#{name}', '#{username}', '#{peep}')
+      Peep.post('luke', 'skywalker', 'This is great fun!')
+      expect(Peep.view_peeps).to include 'This is great fun!'
     end
   end
 
@@ -14,7 +17,7 @@ describe Peep do
     it 'shows all peeps' do
       connection = PG.connect(dbname: 'chitter_test')
       connection.exec("INSERT INTO peeps (name, username, peep) VALUES ('darth', 'sith', 'join the dark side');")
-      expect(peeps.view_peeps).to include 'join the dark side'
+      expect(Peep.view_peeps).to include 'join the dark side'
     end
   end
 
