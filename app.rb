86ee3665179ccd6be40ff201ef6f0380a048f891
Freddie <fredjlawson@gmail.com>
2018-09-01 15:45:02 +0100

    adds rendering the message on the view

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require 'sinatra/base'
+require './lib/messages'
 
 class Chitter < Sinatra::Base
 
   get '/' do
+    @messages = Messages.show
     erb :index
   end
 
   post '/' do
-    erb :index
+    p params
+    Messages.add(params['content'])
+    redirect '/'
   end
   
   run! if app_file == $0
