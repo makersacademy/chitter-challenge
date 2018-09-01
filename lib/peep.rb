fc88bb693d0639bd70b8f7b67383ec12e1d668f8
leekarho <k-h_lee@hotmail.co.uk>
2018-09-01 16:02:41 +0100

    orm

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,13 +2,21 @@ require 'pg'
 
 class Peep
 
+  attr_reader :id, :peep, :time
+
+  def initialize(id, peep, time = Time.now)
+    @id = id
+    @peep = peep
+    @time = time
+  end
+
   def self.all
     result = connect.exec("SELECT * FROM chits;")
-    result.map { |row| row['peep'] }
+    result.map { |row| Peep.new(row['id'], row['peep'], row['time']) }
   end
 
   def self.create(peep, time = Time.now)
-    connect.exec("INSERT INTO chits (peep, time) VALUES ('#{peep}', '#{time}');")
+    result = connect.exec("INSERT INTO chits (peep, time) VALUES ('#{peep}', '#{time}');")
   end
 
   def self.reverse
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -1,11 +1,24 @@
 require 'peep'
 
 describe 'all' do
-  it 'shows a list of all chits' do
+  it 'shows a list of all peeps' do
     Peep.create("hey!")
     Peep.create("bye!")
-    peeps = Peep.all
+    peeps = Peep.all.map { |object| object.peep }
     expect(peeps).to include("hey!")
     expect(peeps).to include("bye!")
   end
+
+  it 'tests ORM' do
+    Peep.create('pie')
+    Peep.create('destroy')
+
+    peeps = Peep.all
+    test_peep = Peep.all.first
+
+    expect(peeps.length).to eq 2
+    expect(test_peep).to respond_to(:id)
+    expect(test_peep.peep).to eq("pie")
+  end
+
 end
