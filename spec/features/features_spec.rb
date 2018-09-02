016caa7e19c557943662fa5e991a051d93c446a0
jdm79 <james.malvern@gmail.com>
2018-09-02 18:49:38 +0100

    adds a users_test spec table for registering new users

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -11,16 +11,18 @@ feature 'Adding peeps' do
   scenario 'user can create a peep' do
     visit('peeps/new')
     fill_in :peep_input, with: 'I do hope this works'
-    click_button 'Submit'
+    click_button 'Peep!'
     expect(page).to have_content 'I do hope this works'
   end
 end
 
 feature 'Registering users' do
   scenario 'user can register' do
-    visit('register')
-    fill_in :peep_input, with: 'I do hope this works'
-    click_button 'Submit'
-    expect(page).to have_content 'I do hope this works'
+    visit('/register')
+    fill_in :registration_email, with: 'james.malvern@gmail.com'
+    fill_in :password, with: 'password'
+
+    click_button 'Register'
+    expect(page).to have_content 'james.malvern@gmail.com', 'james malvern'
   end
 end
