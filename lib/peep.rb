8d47055dccf2a1fc5c02332631c864661fb33b2c
Esam Al-Dabagh <esam.aldabagh@gmail.com>
2018-09-02 10:53:10 +0100

    display author of peep

--- lib/peep.rb
+++ lib/peep.rb
@@ -3,13 +3,14 @@ require "./lib/database_connection"
 
 class Peep 
 
-  attr_reader :id, :body, :posted_at
+  attr_reader :id, :body, :posted_at, :author
 
   def initialize(id, body, date, user_id)
     @id = id
     @body = body
     @posted_at = date
-    @author = user_id
+    @user_id = user_id
+    @author = find_author
   end
 
   def self.all
@@ -31,6 +32,14 @@ class Peep
                               RETURNING id, body, posted_at, user_id;"
                             )
     Peep.new(result[0]["id"], result[0]["body"], result[0]["posted_at"], result[0]["user_id"])
+  end
+
+  def find_author 
+    select_database
+
+    result = DatabaseConnection.query("SELECT username FROM users WHERE id = '#{@user_id}';")
+
+    author = result[0]["username"]
   end
 
 end
