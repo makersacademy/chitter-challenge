6809f3f69b57da1176c9a987090be0262a8585f1
Samir Soormally <samirsoormally@gmail.com>
2018-09-02 09:51:12 +0100

    added connection to database

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,12 +1,26 @@
-require pg
+require 'pg'
 
 class Peep
+  attr_reader :content
+
+  def initialize(id, content)
+    @id = id
+    @content = content
+  end
 
   def self.all
-
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec("SELECT * FROM feed;")
+    result.map do |peep|
+      Peep.new(peep['id'], peep['content'])
+    end
   end
 
   def self.create(content)
 
   end
+
+  # def connect_to_database
+
+  # end
 end
