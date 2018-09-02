82bd6f7c558e1888444cd324f2027535137fe521
Lucien Najev <luciennajev@hotmail.com>
2018-09-02 10:23:32 +0100

    first commit environment set up

--- Gemfile
+++ Gemfile
@@ -9,4 +9,8 @@ group :test do
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'sinatra'
+  gem 'capybara'
+  gem 'pg'
+  
 end
---
+++ app.rb
@@ -0,0 +1,18 @@
+require 'sinatra/base'
+
+class Chitter < Sinatra::Base
+
+
+
+  get '/' do
+    'Testing infrastructure working'
+  end
+
+
+
+
+
+
+run! if app_file == $0
+
+end
