34b4e04cdf87037606917fc57b66f0ad93557191
jdm79 <james.malvern@gmail.com>
2018-09-02 17:33:28 +0100

    add unit test for registering a new user

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 describe '.all' do
-  it 'returns a list of peeps' do
+  it 'returns a list of peeps in reverse chronological order' do
     connection = PG.connect(dbname: 'chitter_test')
     connection.exec("INSERT INTO peeps (peep)VALUES('hello world!');")
     peeps = Peep.all
@@ -10,9 +10,14 @@ describe '.all' do
 end
 
 describe '.create' do
-  it 'it creates a new peep' do
+  it 'creates a new peep' do
     peep = Peep.create('i am the peep_spec test')
     expect(peep).to be_a Peep
     expect(peep.peep).to eq "i am the peep_spec test"
+  end
+end
+
+describe '.register' do
+  it 'stores a registered user' do
   end
 end
