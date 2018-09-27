a0ee9d0756b12d75b8e09a4bb45ffa2a0691ef57
Cameron Whitehead <camjw119@gmail.com>
2018-09-27 21:20:02 +0100

    Added 404 page

--- Gemfile
+++ Gemfile
@@ -2,8 +2,13 @@ source 'https://rubygems.org'
 
 ruby '2.5.0'
 
+gem 'bcrypt'
+gem 'capybara'
+gem 'pg'
+gem 'pry'
 gem 'rake'
 gem 'rubocop', '0.56.0'
+gem 'sinatra'
 
 group :test do
   gem 'rspec'
--- app.rb
+++ app.rb
@@ -2,8 +2,14 @@ require 'sinatra/base'
 
 class ChitterApp < Sinatra::Base
 
+  not_found do
+    status 404
+    erb :oops
+  end
+
   get '/' do
     "Hello, Chitter!"
+    erb :oops
   end
 
   run! if app_file == $0
