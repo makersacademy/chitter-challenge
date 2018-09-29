ef80eb71d229ecec2aa58cca08865c84a30bd386
Andres Faraone <andresfaraone@outlook.com>
2018-09-29 21:38:52 +0100

    add test for time

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -4,11 +4,6 @@ feature 'add peeps' do
     expect(page).to have_button('Make Peep')
   end
 
-  scenario 'add peep button on the index page' do
-    visit('/')
-    expect(page).to have_button('Make Peep')
-  end
-
   scenario 'click button links to /peep/new' do
     visit('/')
     click_button('Make Peep')
@@ -29,15 +24,10 @@ feature 'add peeps' do
     expect(current_path).to eq('/peeps')
   end
 
-  scenario 'added peep message appears on /peeps' do
-    visit('/')
-    click_button('Make Peep')
-    fill_in('message', with: "Test")
-    click_button('Post!')
+  scenario 'added peep message appears on top of /peeps' do
+    Timecop.freeze(Time.new(2018, 9, 20, 15, 0, 0, 0))
+    add_peep("Test")
     expect(page).to have_content("Test")
+    expect(page).to have_content("Posted at 20-09-18 15:00")
   end
-end
-
-def current_path
-  URI.parse(current_url).path
 end
