7d6100a2b1ad71ef9882918133989c6255cbfbed
Andres Faraone <andresfaraone@outlook.com>
2018-09-29 21:39:50 +0100

    tests for #format_time, .all_sorted

--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -1,8 +1,5 @@
-require 'timecop'
-require './models/peep'
-
 describe Peep do
-  subject { described_class }
+  Timecop.freeze(Time.new(2018, 9, 20, 15, 0, 0, 0))
   describe 'On creation' do
     it '.all returns empty array' do
       expect(Peep.all).to be_empty
@@ -10,20 +7,34 @@ describe Peep do
   end
 
   describe '.add' do
-    it { is_expected.to respond_to(:add).with(1).argument }
+    let(:a) { Peep.add("hello") }
+    let(:b) { Peep.add("goodbye") }
     it 'takes a message argument and saves to instance in .all' do
-      a = Peep.add("hello")
-      expect(Peep.all.first).to eq(a)
+      expect(Peep.all).to eq([a, b])
     end
 
-    # it 'instance sets date to Time.now' do
-    #   a = Peep.add("hello")
-    #   expect(a.time).to eq(Time.now)
-    # end
+    it 'instance sets date to Time.now' do
+      expect(a.time).to eq(Time.parse("2018-09-20T15:00:00+00:00"))
+    end
 
     it 'instance sets text to argument' do
-      a = Peep.add("hello")
       expect(a.text).to eq("hello")
+    end
+  end
+
+  describe 'time_format' do
+    let(:a) { Peep.add("hello") }
+    it 'returns user-friendly format for DateTime' do
+      expect(a.time_format).to eq("Posted at 20-09-18 15:00")
+    end
+  end
+
+  describe '.add_sorted' do
+    it 'returns arrays in reverse order' do
+      a = Peep.add("1")
+      b = Peep.add("2")
+      c = Peep.add("3")
+      expect(Peep.all_sorted).to eq([c, b, a])
     end
   end
 end
