3e4ae9a301d65397721b6cb50520c3858b72480e
NadiaAiraf <aidanfaria@gmail.com>
2018-09-29 20:11:54 +0100

    test and added ability to see what the username of whoever posted it is

--- spec/features.rb/add_peep_spec.rb
+++ spec/features.rb/add_peep_spec.rb
@@ -15,7 +15,6 @@ feature 'adding peeps-' do
   scenario 'I want to be able to log in then add a peep' do
     User.create user: 'GIMLITHEGREAT', firstname: 'gimli', lastname: 'son of gloin', email: 'outscoredpointyear@gmail.com', password: 'erebor'
     visit '/'
-    binding.pry
     fill_in 'username', with: 'GIMLITHEGREAT'
     fill_in 'password', with: 'erebor'
     click_button 'sign in'
@@ -38,5 +37,15 @@ feature 'adding peeps-' do
     fill_in 'peep', with: "My PRECIOUS!"
     click_button 'Peep'
     expect(page).to have_content "12:00PM 2 Feb 18"
+  end
+
+  scenario 'I want to see the username of whoever posted it' do
+    visit '/'
+    fill_in 'username', with: 'GIMLITHEGREAT'
+    fill_in 'password', with: 'erebor'
+    click_button 'sign in'
+    fill_in 'peep', with: "My PRECIOUS!"
+    click_button 'Peep'
+    expect(page).to have_content "GIMLITHEGREAT"
   end
 end
