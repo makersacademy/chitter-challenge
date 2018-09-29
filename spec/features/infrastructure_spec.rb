aa730b3789120c86bf9cb6075f0eb0a00a80700f
NadiaAiraf <aidanfaria@gmail.com>
2018-09-29 22:11:08 +0100

    added an actual test for making sure the peeps come back in reverse chronological order

--- spec/features/infrastructure_spec.rb
+++ spec/features/infrastructure_spec.rb
@@ -3,4 +3,15 @@ feature 'Homepage' do
     visit '/'
     expect(page).to have_content 'Welcome to Chitter:'
   end
+
+  scenario 'has peeps which are in the reverse order of which they are posted' do
+    login_as_gimli
+    fill_in 'peep', with: "Legolas! I'm on two already!"
+    click_button 'Peep'
+    fill_in 'peep', with: "I'm on seventeen!"
+    click_button 'Peep'
+    fill_in 'peep', with: "What? I'll have no pointy ear outscoring me!"
+    click_button 'Peep'
+    expect(page.first('div')).to have_content "What? I'll have no pointy ear outscoring me!" 
+  end
 end
