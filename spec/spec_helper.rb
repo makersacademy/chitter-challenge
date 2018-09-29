db254cbfb7a12fb6e7afce753e29933806f771df
djstephenson7 <dan_0502@hotmail.co.uk>
2018-09-29 11:14:26 +0100

    Setup Gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,25 @@
 require 'simplecov'
 require 'simplecov-console'
+ENV['RACK_ENV'] = 'test'
+
+# require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+require 'pry'
+require 'rspec'
+# require './spec/features/web_helpers'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+    # puts
+    # puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    # puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
