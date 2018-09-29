6f45bc45d4bcefd0294d4b08c2e94338d3c05ed0
Marianne Rachid <marianne.rachid@gmail.com>
2018-09-29 13:06:05 +0100

    user gets flash notification when password does not match the confirmation

--- app.rb
+++ app.rb
@@ -22,21 +22,22 @@ class Chitter < Sinatra::Base
   end
 
   get '/signup' do
+    @user = User.new
     erb :sign_up
   end
 
   post '/signup' do
     @user = User.new(name: params[:name], username: params[:username],
-      email: params[:email], password: params[:password],
-      password_confirmation: params[:password_confirmation])
+                     email: params[:email], password: params[:password],
+                     password_confirmation: params[:password_confirmation])
 
     if @user.save
-        session[:user_id] = @user.id
-        redirect '/'
-      else
-        flash.now[:alert] = @user.errors.full_messages
-        erb :sign_up
-      end
+      session[:user_id] = @user.id
+      redirect '/'
+    else
+      flash.now[:alert] = @user.errors.full_messages
+      erb :sign_up
+    end
   end
 
   helpers do
--- spec/feature/sign_up_spec.rb
+++ spec/feature/sign_up_spec.rb
@@ -9,6 +9,12 @@ feature 'sign up' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
   end
 
+  scenario 'with a password that does not match the confirmation' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/signup')
+    expect(page).to have_content 'Password does not match the confirmation'
+  end
+
   scenario 'user cannot sign up if username is already in use' do
     sign_up(username: 'AlbusD')
     expect { sign_up(username: 'AlbusD') }.not_to change(User, :count)
@@ -17,7 +23,7 @@ feature 'sign up' do
 
   scenario 'user cannot sign up if email is already in use' do
     sign_up(email: 'albus_dumbledore@hogwarts.co.uk')
-    expect { sign_up(email: 'albus_dumbledore@hogwarts.co.uk')}.not_to change(User, :count)
+    expect { sign_up(email: 'albus_dumbledore@hogwarts.co.uk') }.not_to change(User, :count)
     expect(page).to have_content "Email is already taken"
   end
 end
