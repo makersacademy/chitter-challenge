e76363425609d71aefbbccd899d27a1a14e3e6d2
Andres Faraone <andresfaraone@outlook.com>
2018-09-29 23:18:22 +0100

    gave helper methods and made it skinnier

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'data_mapper'
+require './controllers/controller_helper'
 require './models/peep'
 
 class Chitter < Sinatra::Base
@@ -8,24 +9,19 @@ class Chitter < Sinatra::Base
   end
 
   get '/peeps' do
-    set_up unless ENV['ENVIRONMENT']
+    set_up
     @peeps = Peep.all_sorted
     erb :index
   end
 
   post '/peeps' do
-    Peep.add(params[:message]) unless params[:message].empty?
+    Peep.add(params[:message])
     redirect('/peeps')
   end
 
   get '/peeps/:id' do
     @peep = Peep.get(params[:id])
     erb :peep
-  end
-
-  def set_up
-    DataMapper::setup(:default, "postgres://andres@localhost/chitter")
-    DataMapper.finalize
   end
 
   run! if app_file == $0
