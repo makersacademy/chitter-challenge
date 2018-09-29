2c4e55453a6f1df806d71e8354a355f490f4b8a2
Andres Faraone <andresfaraone@outlook.com>
2018-09-29 15:30:13 +0100

    Controller route for adding peep

--- app.rb
+++ app.rb
@@ -2,8 +2,21 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
   get '/' do
-    'Hello World!'
+    redirect('/peeps')
   end
+
+  get '/peeps' do
+    erb :index
+  end
+
+  post '/peeps' do
+    redirect('/peeps')
+  end
+  
+  get '/peeps/new' do
+    erb :create_peep
+  end
+
 
   run! if app_file == $0
 end
