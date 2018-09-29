36987ed21f98e1ea69bcb3dff7f4890ee8d86b62
Andres Faraone <andresfaraone@outlook.com>
2018-09-29 22:52:10 +0100

    Add_peep test linted

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,27 +1,19 @@
 feature 'add peeps' do
-  scenario 'add peep button on the index page' do
+  scenario 'textbox and submit present in /peeps' do
     visit('/')
-    expect(page).to have_button('Make Peep')
-  end
-
-  scenario 'click button links to /peep/new' do
-    visit('/')
-    click_button('Make Peep')
-    expect(current_path).to eq('/peeps/new')
-  end
-
-  scenario 'textbox and submit present in /peep/new' do
-    visit('/')
-    click_button('Make Peep')
     expect(page).to have_css('#add_peep')
     expect(page).to have_css('#submit_peep')
   end
 
   scenario 'route sent to POST /peep' do
     visit('/')
-    click_button('Make Peep')
     click_button('Post!')
     expect(current_path).to eq('/peeps')
+  end
+
+  scenario 'no peep added if text is empty' do
+    add_peep("")
+    expect(page).not_to have_selector('.peep')
   end
 
   scenario 'added peep message appears on top of /peeps' do
