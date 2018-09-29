5b40d37d84e0b8db0ed76b274fd66a9d137f3f7b
Andres Faraone <andresfaraone@outlook.com>
2018-09-29 19:13:45 +0100

    RSpec uses and links to test database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'pry'
+require 'data_mapper'
+require './models/peep'
+require './models/user'
 
 ENV['RACK_ENV'] = 'test'
 ENV['ENVIRONMENT'] = 'test'
@@ -20,6 +24,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  DataMapper::setup(:default, "postgres://andres@localhost/test_chitter")
+  DataMapper.finalize
+
+  config.before(:each) do
+    DataMapper.auto_migrate!
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
