8e66270f8b8af8f554c7613f1dc055ffb62bed89
Florence Hapgood <flohapgood@icloud.com>
2018-09-30 19:39:41 +0100

    Empty file deleted

--- database_connection_setup.rb
+++
@@ -1,7 +0,0 @@
-# require_relative 'lib/database_connection'
-#
-# if ENV['ENVIRONMENT'] == 'test'
-#   DatabaseConnection.setup('chitter_test')
-# else
-#   DatabaseConnection.setup('chitter')
-# end
