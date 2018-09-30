5c2d83ae70d1e9776639c0a9f066e857cb95ac67
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-09-30 15:13:58 +0100

    Test for encrypted password

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -9,4 +9,9 @@ describe User do
     expect(user.username).to eq 'Test'
     expect(user.email).to eq 'test@example.com'
   end
+
+  it 'hashes the password using BCrypt' do
+    expect(BCrypt::Password).to receive(:create).with('password123')
+    User.create(name: 'Test test', username: 'Test', email: 'test@example.com', password: 'password123')
+  end
 end
