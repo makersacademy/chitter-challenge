0f455feffd5ee18701cdad0129c7f8f92ddd8c67
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-09-30 12:55:50 +0000

    refactored

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,8 +3,8 @@ require 'peep'
 describe Peep do
   context ".create" do
     it "should store peep in database" do
-      Peep.create(text: "Test Peep")
-      expect(Peep.get(1).text).to eq "Test Peep"
+      peep = Peep.create(text: "Test Peep")
+      expect(peep.text).to eq "Test Peep"
     end
   end
 end
