10e98bd8770e27dcfee11c05a73ea0dd510650fc
Andres Faraone <andresfaraone@outlook.com>
2018-09-30 21:26:14 +0100

    controller now handles account actions

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require 'data_mapper'
 require './controllers/controller_helper'
 require './models/peep'
+require './models/user'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+
   get '/' do
     redirect('/peeps')
   end
 
   get '/peeps' do
     set_up
+    @user = User.instance
     @peeps = Peep.all_sorted
     erb :index
   end
@@ -24,15 +30,33 @@ class Chitter < Sinatra::Base
     erb :peep
   end
 
-  get '/user/login' do
-  end
-
   get '/user/register' do
     erb :register
   end
 
+  get '/user/login' do
+    erb :login
+  end
+
+  post '/user/login' do
+    User.login(params["user"], params["password"])
+    flash[:login] = User.instance ? "successful" : "unsuccessful"
+    redirect('/')
+  end
+
   post '/user/register' do
-    p params
+    details = { name: params["name"], user: params["user"],
+      email: params["email"], password: params["password"] }
+    User.add(details)
+    User.login(details[:user], details[:password])
+    flash[:registration] = User.instance ? "successful" : "unsuccessful"
+    redirect('/')
+  end
+
+  post '/user/logout' do
+    User.logout
+    flash[:logout] = true
+    redirect('/')
   end
 
   run! if app_file == $0
