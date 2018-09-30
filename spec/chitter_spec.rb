0eea0f91c1602f3531d708480b52030591e46e06
Florence Hapgood <flohapgood@icloud.com>
2018-09-30 10:15:00 +0100

    More unit tests for all method, but still testing actual database

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -2,10 +2,23 @@ require "./lib/chitter"
 
 describe Chitter do
   describe "#all" do
-    it "Returns all a chitter object, which we can extract a name instance from" do
+    it "Returns a chitter object, which we can extract a name instance from" do
       array = Chitter.all
       chitter_object = array[0]
       expect(chitter_object.message).to eq("Test 1")
+    end
+
+    it "Returns a chitter object, which we can extract the time from" do
+      array = Chitter.all
+      chitter_object = array[0]
+      expect(chitter_object.created_at).to be_an_instance_of(DateTime)
+    end
+
+    it "Returns a chitter object, which we can extract the author from" do
+      array = Chitter.all
+      chitter_object = array[0]
+      expect(chitter_object.posted_by).to eq("Florence")
+
     end
   end
 end
