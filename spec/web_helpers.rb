d2c1cce9159ed371acb476b76561cb95b89f937c
Andres Faraone <andresfaraone@outlook.com>
2018-09-30 21:24:25 +0100

    added additional login methods

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -1,5 +1,4 @@
 def add_peep(message)
-  visit('/')
   fill_in('message', with: message)
   click_button('Post!')
 end
@@ -10,9 +9,16 @@ end
 
 def register_details
   [{ field: 'name', field_input: 'John Thompson' },
-  { field: 'username', field_input: 'jthompson' },
+  { field: 'user', field_input: 'jthompson' },
   { field: 'email', field_input: 'jt@gmail.com' },
   { field: 'password', field_input: 'password123' }]
+end
+
+def register_details_different
+  [{ field: 'name', field_input: 'Andy' },
+  { field: 'user', field_input: 'jthompson' },
+  { field: 'email', field_input: 'jt@gmail.com' },
+  { field: 'password', field_input: 'password456' }]
 end
 
 def register
@@ -22,4 +28,45 @@ def register
     fill_in(details[:field], with: details[:field_input])
   end
   find('#register').find('#submit').click
+end
+
+def register_same_user
+  visit('/')
+  find('#register-form').find('#register').click
+  register_details_different.each do |details|
+    fill_in(details[:field], with: details[:field_input])
+  end
+  find('#register').find('#submit').click
+end
+
+def login_details
+  [{ field: 'user', field_input: "jthompson" },
+  { field: 'password', field_input: 'password123' }]
+end
+
+def login_details_wrong
+  [{ field: 'user', field_input: "jthompson" },
+  { field: 'password', field_input: 'password456' }]
+end
+
+def login
+  visit('/')
+  find('#register-form').find('#login').click
+  login_details.each do |details|
+    fill_in(details[:field], with: details[:field_input])
+  end
+  find('#login').find('#submit').click
+end
+
+def login_wrong
+  visit('/')
+  find('#register-form').find('#login').click
+  login_details_wrong.each do |details|
+    fill_in(details[:field], with: details[:field_input])
+  end
+  find('#login').find('#submit').click
+end
+
+def logout
+  click_button('Log out')
 end
