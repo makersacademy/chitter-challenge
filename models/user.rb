8cf5c89b480d86137594c738e780e1f77eedda62
Andres Faraone <andresfaraone@outlook.com>
2018-09-30 15:10:06 +0100

    .all only creates if email and user unique

--- models/user.rb
+++ models/user.rb
@@ -6,4 +6,21 @@ class User
   property :user, String
   property :email, String
   property :password, String
+
+  def self.add(name, user, email, password)
+    User.create(name: name, user: user,
+      email: email, password: password) if User.unique?(user, email)
+  end
+
+  def self.unique?(user, email)
+    User.unique_user?(user) && User.unique_email?(email)
+  end
+
+  def self.unique_user?(user)
+    !User.all.map { |entry| entry.user }.include?(user)
+  end
+
+  def self.unique_email?(email)
+    !User.all.map { |entry| entry.email }.include?(email)
+  end
 end
