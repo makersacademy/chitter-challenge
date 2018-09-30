e3c8fa9e23319cd0562e615808f8e7785c1564cc
Nathan Quayle <nathan.quayle@gmail.com>
2018-09-30 22:00:59 +0100

    added iLIKE - case insensitive - search on PSQL input;

--- lib/user.rb
+++ lib/user.rb
@@ -3,8 +3,9 @@ require_relative './db_helper.rb'
 class User
   def self.add(username, email)
     conn = DBHelper.connect_to_db
-    if (conn.exec("SELECT username FROM users WHERE username = '#{username}'").first != nil ||
-        conn.exec("SELECT email FROM users WHERE email = '#{email}'").first != nil)
+    # check if username or email is already on the db.
+    if (conn.exec("SELECT username FROM users WHERE username iLIKE '#{username}'").first != nil ||
+        conn.exec("SELECT email FROM users WHERE email iLIKE '#{email}'").first != nil)
       return 'fail'
     else
       conn.exec("INSERT INTO users(username, email) VALUES ('#{username}', '#{email}')")
