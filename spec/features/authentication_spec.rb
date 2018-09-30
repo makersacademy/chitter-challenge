2a29ed83ceea8a6243cef3bdbf45bdf0f39cc1c5
Cristina Maria Ocana Manzano <cristinaocana1@gmail.com>
2018-09-30 20:06:42 +0100

    User login - incorrect password test

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -18,4 +18,15 @@ feature 'authentication' do
     expect(page).not_to have_content "Welcome to Chitter, Test"
     expect(page).to have_content "Your email or your password is incorrect."
   end
+
+  scenario 'a user sees an error if they get their email wrong' do
+    visit '/'
+    click_button('Log in')
+    fill_in('email', with: 'test@example.com')
+    fill_in('password', with: 'vivaelbetis')
+    click_button('Log in')
+
+    expect(page).not_to have_content "Welcome to Chitter, Test"
+    expect(page).to have_content "Your email or your password is incorrect."
+  end
 end
