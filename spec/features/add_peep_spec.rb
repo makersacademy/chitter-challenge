2f147b99735ef06035e00a910c051f253a9b7b74
Andres Faraone <andresfaraone@outlook.com>
2018-09-30 21:25:43 +0100

    adde register to add_peep test

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,23 +1,31 @@
 feature 'add peeps' do
-  scenario 'textbox and submit present in /peeps' do
-    visit('/')
+  scenario 'textbox and submit present in /peeps if logged in' do
+    register
     expect(page).to have_css('#add_peep')
     expect(page).to have_css('#submit_peep')
   end
 
-  scenario 'route sent to POST /peep' do
+  scenario 'textbox and submit not present in /peeps if not logged in' do
     visit('/')
+    expect(page).not_to have_css('#add_peep')
+    expect(page).not_to have_css('#submit_peep')
+  end
+
+  scenario 'route sent to POST /peep' do
+    register
     click_button('Post!')
     expect(current_path).to eq('/peeps')
   end
 
   scenario 'no peep added if text is empty' do
+    register
     add_peep("")
     expect(page).not_to have_selector('.peep')
   end
 
   scenario 'added peep message appears on top of /peeps' do
     Timecop.freeze(Time.new(2018, 9, 20, 15, 0, 0, 0))
+    register
     add_peep("Test")
     expect(page).to have_content("Test")
     expect(page).to have_content("Posted at 20-09-18 15:00")
