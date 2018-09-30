36cdefb119e021cfc8765ca915aa23db183d8588
Florence Hapgood <flohapgood@icloud.com>
2018-09-30 10:05:42 +0100

    Added unit test for all (message)

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -1,9 +1,11 @@
 require "./lib/chitter"
 
-# describe Chitter do
-#   describe "#" do
-#     it "returns all peeps" do
-#       expect(Chitter.all).to eq
-#     end
-#   end
-# end
+describe Chitter do
+  describe "#all" do
+    it "Returns all a chitter object, which we can extract a name instance from" do
+      array = Chitter.all
+      chitter_object = array[0]
+      expect(chitter_object.message).to eq("Test 1")
+    end
+  end
+end
