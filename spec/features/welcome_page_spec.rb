7545808c4e49b0330d93ef3810acd3f334c793db
Damion Hardy <damionhardy@Damos-MBP-8.home>
2018-09-30 11:46:03 +0100

    added functionality to resign name, and general tidying of other files

--- spec/features/welcome_page_spec.rb
+++ spec/features/welcome_page_spec.rb
@@ -1,9 +1,13 @@
+# As a Maker
+# So that I can post messages on Chitter as me
+# I want to sign up for Chitter
+
 feature 'welcome page' do
 
   scenario 'welcome message appears' do
     visit('/')
     expect(page).to have_content "Welcome to Chitter!!!"
-    # expect(page).to have_content "Please submit your first messsage"
+    expect(page).to have_content "Please enter your name and press start to begin"
   end
 
 end
