7dc002397ba91adb44a391f2e004b7d96599246d
Andres Faraone <andresfaraone@outlook.com>
2018-09-30 13:15:55 +0100

    add register function to webhelper

--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -7,3 +7,19 @@ end
 def current_path
   URI.parse(current_url).path
 end
+
+def register_details
+  [{ field: 'name', field_input: 'John Thompson' },
+  { field: 'username', field_input: 'jthompson' },
+  { field: 'email', field_input: 'jt@gmail.com' },
+  { field: 'password', field_input: 'password123' }]
+end
+
+def register
+  visit('/')
+  find('#register-form').find('#register').click
+  register_details.each do |details|
+    fill_in(details[:field], with: details[:field_input])
+  end
+  find('#register').find('#submit').click
+end
