065567e9e1075a1bd004294c4d961bebd8b539f1
Oliver Baum <oliver@baums.org.uk>
2018-10-01 12:14:28 +0100

    Creating peeps feature test passes. Implemented correctly with Datamapper

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'data_mapper'
+require './lib/peep'
 
 if ENV['ENVIRONMENT'] == 'test'
   DataMapper.setup(:default, 'postgres://localhost/chitter_test')
@@ -7,16 +8,25 @@ else
   DataMapper.setup(:default, 'postgres://localhost/chitter')
 end
 
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
 class ChitterApp < Sinatra::Base
   enable :sessions
   set :session_secret, 'something secret'
 
   get '/' do
+    @peeps = Peep.all
     erb(:peeps)
   end
 
   get '/add' do
     erb(:add)
+  end
+
+  post '/add' do
+    Peep.create(text: params[:peep_text])
+    redirect '/'
   end
 
 end
