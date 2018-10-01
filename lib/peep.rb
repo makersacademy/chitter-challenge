c47df3c54718264ca806ea4647a64b7081c8202f
Darcie <darciewalsh@hotmail.com>
2018-10-01 11:06:46 +0100

    remove unnecessary methods

--- lib/peep.rb
+++ lib/peep.rb
@@ -4,23 +4,14 @@ require 'pg'
 
 class Peep
 
-include DataMapper::Resource
+  include DataMapper::Resource
 
 property :id,           Serial
 property :peep,         String
 property :created_at,   DateTime
+# property :created_by,   String
+# property :username,     String
 
-  def self.all
-    result = ConnectDatabase.query("SELECT * FROM peeps")
-    result.map do |peep|
-      Peep.new(id: peep['id'], peep: peep['peep'], created_at: peep['created_at'])
-    end
-  end
-
-  def self.create(peep:)
-    result = ConnectDatabase.query("INSERT INTO peeps (peep) VALUES('#{peep}') RETURNING id, peep, created_at;")
-    Peep.new(id: result[0]['id'], peep: result[0]['peep'], created_at: result[0]['created_at'] )
-  end
 
 end
 
