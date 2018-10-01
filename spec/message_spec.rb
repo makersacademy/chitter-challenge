c3557a476e17396a189e044737fa890730e46f72
Cristina María Ocaña Manzano <42174960+cristinaocanamanzano@users.noreply.github.com>
2018-10-01 10:18:28 +0100

    Delete message_spec.rb

--- spec/message_spec.rb
+++
@@ -1,9 +0,0 @@
-require './lib/message'
-
-describe Message do
-  it "creates a new message" do
-    message = Message.create(text: 'This is a message')
-    expect(message).to be_a Message
-    expect(message.text).to eq 'This is a message'
-  end
-end
