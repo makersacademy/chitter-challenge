2ffaa4c7faaca467614fbe3b2385ed198fdc17b6
Alex Kharouk <akharoukpp@gmail.com>
2018-10-03 12:35:57 +0100

    add heroku database

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -5,7 +5,7 @@ env = ENV['RACK_ENV'] || 'development'
 require './lib/message.rb'
 require './lib/user.rb'
 
-DataMapper.setup(:default, "postgres://localhost/twittarr_#{env}")
+DataMapper.setup(:default, "postgres://localhost/twittarr_#{env}" || ENV['DATABASE_URL'])
 
 DataMapper.finalize
 
