abe7348fafd7683b520bf35289a72fb97978e54a
Mathilde Ferrand Coeurderoy <mathilde.ferrand.coeurderoy@gmail.com>
2018-10-13 13:44:56 +0100

    tests: fix test for chronological order

--- spec/features/retrieve_peeps_spec.rb
+++ spec/features/retrieve_peeps_spec.rb
@@ -2,17 +2,16 @@ feature 'Features - Retrieve peeps' do
   # As a maker
   # So that I can see what others are saying
   # I want to see all peeps in reverse chronological order
-  scenario 'user wants to see all peeps' do
-    Peep.create(peep: 'my first peep, hurray!')
-    Peep.create(peep: 'my second peep, hurray!')
+  scenario 'user wants to see all peeps in reverse chronological order' do
+    Peep.create(peep: 'my first peep')
+    Peep.create(peep: 'my second peep')
     visit('/all_peeps')
-    expect(page).to have_content('my first peep, hurray!')
-    expect(page).to have_content('my second peep, hurray!')
+    expect(page.body.index('my first peep')).to be < (page.body.index('my second peep'))
   end
 
   scenario 'user wants to see when a peep was sent' do
     visit('/all_peeps')
     peep = Peep.create(peep: 'my first peep, hurray!')
-    expect(page).to have_content('sent at')
+    expect(peep.created_at).to be_within(2.seconds).of(Time.now)
   end
 end
