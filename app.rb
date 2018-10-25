7e02091e85ba3c2d422a5d2f8ed2af0345f8e77a
Chris Cooksley <christopher.cooksley@gmail.com>
2018-10-25 09:41:54 +0100

    can view peeps after adding

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'sinatra/base'
+require './lib/peep'
 
 class Chitter < Sinatra::Base
+  USERNAME = "TestUser"
+
   set :port, 6789
 
   get '/' do
@@ -8,11 +11,17 @@ class Chitter < Sinatra::Base
   end
 
   get '/peeps' do
+    @peeps = Peep.all
     erb(:"peeps/index")
   end
 
   get '/peeps/new' do
     erb(:"peeps/new")
+  end
+
+  post '/peeps' do
+    Peep.create(username: USERNAME, content: params[:content])
+    redirect '/peeps'
   end
 
   run! if app_file == $0
--- spec/features/new_peep_spec.rb
+++ spec/features/new_peep_spec.rb
@@ -7,4 +7,10 @@ feature "adding peeps" do
   scenario "user can view a form for adding peeps" do
     expect(page).to have_field("content")
   end
+
+  scenario "user can add a peep and see it on the peeps page" do
+    fill_in("content", with: "FAKE NEWS!")
+    click_button 'Submit'
+    expect(page).to have_content 'FAKE NEWS!'
+  end
 end
