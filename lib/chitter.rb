d98335b53992dd23525c9fdb5b3996a0ed2c403f
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-10-25 09:58:53 +0100

    updated setup test database

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -3,11 +3,13 @@ require 'pg'
 class Chitter
 
   def self.all
-    begin
+    if ENV['ENVIRONMENT'] = 'test'
+      con = PG.connect :dbname => 'chitter_test'
+    else
       con = PG.connect :dbname => 'chitter'
+    end
       rs = con.exec "SELECT * FROM peeps"
       rs.map { |row| row['text'] }
-    end
   end
 
 end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,3 +1,4 @@
 require 'pg'
-con = PG.connect :dbname => 'chitter'
+con = PG.connect :dbname => 'chitter_test'
 rs = con.exec "TRUNCATE TABLE peeps"
+rs = con.exec "INSERT INTO peeps (id, text, name, username) VALUES(1, 'My first peep', 'Imogen Misso', 'imogenmisso123')"
