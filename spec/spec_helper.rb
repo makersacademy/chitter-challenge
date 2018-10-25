c139455deba97ba0c8bcdfdb57aa7d4b7473f741
Clare Jolly <clarejollyuk@gmail.com>
2018-10-25 12:11:33 +0100

    updated readme

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,8 +10,8 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+    # puts
+    # puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
+    # puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
