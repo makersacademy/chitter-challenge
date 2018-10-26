02fb48b2a2325ef463b60296f702da27b116291f
Aimée Craig <aimeecraig1@gmail.com>
2018-10-26 14:38:50 +0100

    feature test added

--- spec/features/posting_a_peep_spec.rb
+++ spec/features/posting_a_peep_spec.rb
@@ -5,4 +5,12 @@ feature 'Posting peeps' do
     click_button 'Post'
     expect(page).to have_content 'This is a test peep!'
   end
+
+  scenario 'user does not see a form to post a peep without being signed in' do
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'username', with: 'Mittens'
+    click_button 'Register'
+    expect(page).not_to have_button 'Post'
+  end
 end
