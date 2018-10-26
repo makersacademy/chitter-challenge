c458c9b052a6fb3e0ebfe9e063bac8d5a6fb366d
hurc87 <zoe1987@btinternet.com>
2018-10-26 11:21:04 +0100

    setting up database connection in self.all

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,8 +1,9 @@
+require 'pg'
+
 class Peep
   def self.all
-    ['message one',
-     'message two',
-     'message three'
-     ]
+    connection = PG.connect(dbname: 'peep_manager_2')
+    result = connection.exec("SELECT * FROM peeps;")
+    result.map { |peep| peep['message'] }
   end
 end
