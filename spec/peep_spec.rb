4c3f6a3f0dd46a60b7ca8653c76fb0175575d590
Aimée Craig <aimeecraig1@gmail.com>
2018-10-26 09:54:52 +0100

    unit test added

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,5 +1,10 @@
 require 'peep'
 
 describe Peep do
-
+  describe '#all' do
+    it 'returns a list of all peeps' do
+      peeps = Peep.all
+      expect(peeps).to include 'This is a test peep!'
+    end
+  end
 end
