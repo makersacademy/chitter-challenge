4048857ace1b7815df0ec76acd6d3ac8e67a489c
Chris Cooksley <christopher.cooksley@gmail.com>
2018-10-26 15:39:56 +0100

    Sign Out button

--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -1,5 +1,10 @@
 feature "signing out" do
   before do
+    visit '/'
+    sign_up
+  end
 
+  scenario 'can see Sign Out button' do
+    expect(page).to have_selector(:button, "Sign Out")
   end
 end
