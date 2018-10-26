00e2d3b92e82163a2ec040a55c2b56e07b4a3805
Aimée Craig <aimeecraig1@gmail.com>
2018-10-26 10:06:30 +0100

    code written to pass unit tests

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,3 +1,15 @@
-class DatabaseConnection
+require 'pg'
 
+class DatabaseConnection
+  def self.setup(database)
+    @connection = PG.connect(dbname: database)
+  end
+
+  def self.connection
+    @connection
+  end
+
+  def self.query(string)
+    @connection.exec(string)
+  end
 end
