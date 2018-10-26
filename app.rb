449a9785d36151ce0e5094bba56c1822dd8f58c7
jebax <joel.eliot.baxter@gmail.com>
2018-10-26 11:49:33 +0100

    can view empty list of peeps without signing up

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
 
   get '/' do
     erb :index
+  end
+
+  get '/peeps' do
+    flash.now[:message] = 'No peeps posted yet!'
+    erb :peeps
   end
 
   run! if app_file == $PROGRAM_NAME
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,6 +1,15 @@
 feature 'index page' do
-  scenario 'displays the title Chitter on the page' do
+  before do
     visit '/'
+  end
+
+  scenario 'displays the title Chitter on the page' do
     expect(page).to have_content 'Chitter'
+  end
+
+  scenario 'has a link to view the list of peeps' do
+    click_button 'View all peeps'
+    expect(page).to have_current_path '/peeps'
+    expect(page).to have_content 'No peeps posted yet!'
   end
 end
