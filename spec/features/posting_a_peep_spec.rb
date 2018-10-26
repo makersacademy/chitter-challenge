9673efa937c0a62028853928dff17bc6e670cdc2
Aimée Craig <aimeecraig1@gmail.com>
2018-10-26 14:40:13 +0100

    code amended to pass feature tests

--- spec/features/posting_a_peep_spec.rb
+++ spec/features/posting_a_peep_spec.rb
@@ -1,6 +1,9 @@
 feature 'Posting peeps' do
   scenario 'user posts a peep' do
     visit '/'
+    click_link 'Sign Up'
+    fill_in 'username', with: 'Mittens'
+    click_button 'Register'
     fill_in 'peep_content', with: 'This is a test peep!'
     click_button 'Post'
     expect(page).to have_content 'This is a test peep!'
@@ -8,9 +11,6 @@ feature 'Posting peeps' do
 
   scenario 'user does not see a form to post a peep without being signed in' do
     visit '/'
-    click_link 'Sign Up'
-    fill_in 'username', with: 'Mittens'
-    click_button 'Register'
     expect(page).not_to have_button 'Post'
   end
 end
