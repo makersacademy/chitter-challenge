cf2a7d7fab45ae58a127e54c52fe8b0aaf360d69
Aimée Craig <aimeecraig1@gmail.com>
2018-10-26 10:02:33 +0100

    unit tests added

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -1,5 +1,18 @@
 require 'database_connection'
 
 describe DatabaseConnection do
+  describe '#setup' do
+    it 'connects to the test database using PG' do
+      expect(PG).to receive(:connect).with(dbname: 'chitter_test')
+      DatabaseConnection.setup('chitter_test')
+    end
+  end
 
+  describe '#query' do
+    it 'executes a query via PG' do
+      connection = DatabaseConnection.setup('chitter_test')
+      expect(connection).to receive(:exec).with("SELECT * FROM peeps;")
+      DatabaseConnection.query("SELECT * FROM peeps;")
+    end
+  end
 end
