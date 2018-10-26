7b57ceddd61fa53e45fa141c02812b0ac9d6e494
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-26 16:03:12 +0100

    Added email field to Sign up

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,6 +1,13 @@
 feature "Sign up" do
+
   scenario "I see a sign up form with a username field" do
     visit('/sign_up')
     expect(page).to have_field("Username")
   end
+
+  scenario "I see a sign up form with a email field" do
+    visit('/sign_up')
+    expect(page).to have_field("email")
+  end
+  
 end
