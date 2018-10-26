8f8603625662e558d05b0cb6afb4229eb2fb0f83
Aimée Craig <aimeecraig1@gmail.com>
2018-10-26 16:49:34 +0100

    feature test amended

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -1,8 +1,8 @@
 feature 'Signing up' do
   scenario 'a user signs up for Chitter' do
-    visit '/'
-    click_link 'Sign Up'
+    visit '/new-user'
     fill_in 'username', with: 'Mittens'
+    fill_in 'password', with: 'password'
     click_button 'Register'
     expect(page.find('.user')).to have_content 'Hi, Mittens!'
     expect(page).not_to have_content 'Sign Up'
