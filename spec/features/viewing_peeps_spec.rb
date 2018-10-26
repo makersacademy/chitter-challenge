756be0fef1ad8516208df87199b35faa9ce36247
Aimée Craig <aimeecraig1@gmail.com>
2018-10-26 11:31:29 +0100

    feature test added

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -4,4 +4,11 @@ feature 'Viewing peeps' do
     visit '/'
     expect(page).to have_content 'This is a test peep!'
   end
+
+  scenario 'peeps are shown in reverse chronological order' do
+    Peep.post('This is a test peep!')
+    Peep.post('This is also a test peep!')
+    visit '/'
+    expect(page.find('.peep:nth-of-type(1)')).to have_content 'This is also a test peep!'
+  end
 end
