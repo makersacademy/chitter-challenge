4d691b05b77c8ab97507cb439c93ef4f3816557c
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-26 14:54:45 +0100

    Tested and added Sign up button to homepage

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,4 +5,8 @@ feature "Welcome to Chitter!" do
     expect(page).to have_content "Welcome to Chitter!"
   end
 
+  scenario "I see a welcome page with a sign up button" do
+    visit('/')
+    expect(page).to have_button "Sign Up"
+  end
 end
