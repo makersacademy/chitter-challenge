bc061d444e58867af99dbf10064afcc048cee3c2
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-27 21:01:14 +0100

    Removed duplicated code

--- lib/user.rb
+++ lib/user.rb
@@ -9,16 +9,20 @@ class User
   end
 
   def self.sign_up(name, email, username, password)
-    connection = PG.connect(dbname: 'chitter_test')
-    connection.exec("INSERT INTO users (name, email, username, password) VALUES ('#{name}', '#{email}', '#{username}', '#{password}')")
+    self.connection.exec("INSERT INTO users (name, email, username, password) VALUES ('#{name}', '#{email}', '#{username}', '#{password}')")
   end
 
   def self.log_in(username, password)
-    connection = PG.connect(dbname: 'chitter_test')
-    user_data = connection.exec("SELECT * FROM users WHERE username = '#{username}' AND password = '#{password}'").first
+    user_data = self.connection.exec("SELECT * FROM users WHERE username = '#{username}' AND password = '#{password}'").first
 
     raise 'Invalid credentials' if user_data.nil?
 
     User.new(user_data["id"].to_i, user_data["name"], user_data["email"], user_data["username"])
   end
+
+private
+  def self.connection
+      PG.connect(dbname: 'chitter_test')
+  end
+
 end
