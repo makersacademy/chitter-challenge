9bbf67a77165489ef0b0369b2a48e3ba09befccd
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-27 17:26:46 +0100

    Added a buton in Post Peep

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -1,11 +1,15 @@
 feature 'Post a peep to chitter' do
 
-  scenario 'I can write a peep' do
+  scenario 'I can write a peep in a text field' do
     visit('/')
     visit('/post_peep')
     expect(page).to have_field("New Peep")
   end
 
-
+  scenario 'I post a peep to chitter by clicking a button' do
+    visit('/')
+    visit('/post_peep')
+    expect(page).to have_button("Post Peep")
+  end
 
 end
