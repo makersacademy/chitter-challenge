86a8ffd20eb95421c5d87ceeb76ed6b845c17ed3
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-27 20:07:54 +0100

    Simplify test

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,28 +1,13 @@
 
 feature "Sign up" do
 
-  scenario "I see a sign up form with a username field" do
+  scenario "I see a sign up form with fields" do
     visit('/sign_up')
     expect(page).to have_field("Username")
-  end
-
-  scenario "I see a sign up form with a name field" do
-    visit('/sign_up')
     expect(page).to have_field("Name")
-  end
-
-  scenario "I see a sign up form with a email field" do
-    visit('/sign_up')
     expect(page).to have_field("email")
-  end
-
-  scenario "I see a sign up form with a password field" do
-    visit('/sign_up')
     expect(page).to have_field("Password")
-  end
-
-  scenario "I see a sign up button I can click on" do
-    visit('/sign_up')
     expect(page).to have_button("Sign Up")
   end
+
 end
