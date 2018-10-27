ccaded9e8800127fc9f7ced0d96db719440ed054
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-27 11:53:26 +0100

    added filter for Rakefile

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,12 +9,17 @@ require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+Capybara.app = Chitter
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
 ])
-SimpleCov.start
+
+SimpleCov.start do
+  add_filter "Rakefile"
+end
 
 Rake.application.load_rakefile
 
