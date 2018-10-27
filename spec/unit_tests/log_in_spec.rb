642ec62eade3082ae0a1bd076cf2a32d2b0178cd
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-27 20:34:36 +0100

    Log in with wrong password fails

--- spec/unit_tests/log_in_spec.rb
+++ spec/unit_tests/log_in_spec.rb
@@ -1,7 +1,13 @@
 describe 'Log in' do
 
-  it 'it fails when user does not exist' do
+  it 'fails when user does not exist' do
     expect { User.log_in('inexistent username', 'a password') }.to raise_error "Invalid credentials"
   end
-  
+
+  it 'fails with non matching password' do
+    User.sign_up('a name', 'an existing username', 'an email', 'correct password')
+
+    expect { User.log_in('an existing username', 'wrong password') }.to raise_error "Invalid credentials"
+  end
+
 end
