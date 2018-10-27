fc740331a89068477f10eedd3011be29fbf782db
Edyta Bednarz <edy.b.bednarz@gmail.com>
2018-10-27 20:09:25 +0100

    Improved field names

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,10 +3,10 @@ feature "Sign up" do
 
   scenario "I see a sign up form with fields" do
     visit('/sign_up')
-    expect(page).to have_field("Username")
-    expect(page).to have_field("Name")
+    expect(page).to have_field("username")
+    expect(page).to have_field("name")
     expect(page).to have_field("email")
-    expect(page).to have_field("Password")
+    expect(page).to have_field("password")
     expect(page).to have_button("Sign Up")
   end
 
