dbac1f8648711e096206095babcd0744ac044eae
Tolvic <terence.h.mace@gmail.com>
2018-10-28 18:35:27 +0000

    Tests written and passed for not showing peep form when user not logged in

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -1,9 +1,14 @@
 feature 'Feature - Posting a Peep' do
-  scenario 'A user can post a Peep to Chitter' do
+  scenario 'A non-logged in user cannot post a Peep to Chitter' do
     visit('/')
-    fill_in('message', with: 'A Peep posted by a user')
-    click_button('Peep')
-
-    expect(page).to have_content 'A Peep posted by a user'
+    expect(page).not_to have_selector("#peep-form")
   end
+
+  # scenario 'A user can post a Peep to Chitter' do
+  #   visit('/')
+  #   fill_in('message', with: 'A Peep posted by a user')
+  #   click_button('Peep')
+  #
+  #   expect(page).not_to have_content 'A Peep posted by a user'
+  # end
 end
