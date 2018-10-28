33d3c5958a04f8e5c2b225e89331dbcb60072b2d
shannongamby <shannongamby@hotmail.com>
2018-10-28 18:20:46 +0000

    excluded rakefile from simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
 ])
-SimpleCov.start
+SimpleCov.start do
+  add_filter "Rakefile"
+end
 
 Rake.application.load_rakefile
 
