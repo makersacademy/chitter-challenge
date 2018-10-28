89f8ee0eb730524a83a66004c706cc5c87ba4ee0
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-10-28 09:26:00 +0000

    added #login_user method

--- spec/support/web_helper.rb
+++ spec/support/web_helper.rb
@@ -7,5 +7,13 @@ def register_a_user
   fill_in :password, with: 'pikachu123'
   fill_in :confirm_password, with: 'pikachu123'
 
-  click_button 'Register'  
+  click_button 'Register'
+end
+
+def login_user
+  visit '/'
+  click_link 'Login'
+  fill_in :email, with: 'ash@pallet.com'
+  fill_in :password, with: 'pikachu123'
+  click_button 'Login'
 end
