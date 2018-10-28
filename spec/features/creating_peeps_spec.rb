d6d1c1da927322cd4674feb9576647d9ab2e48d2
Tolvic <terence.h.mace@gmail.com>
2018-10-28 18:39:07 +0000

    Tests written and passed for logged in user being able to post a peep

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -1,14 +1,23 @@
 feature 'Feature - Posting a Peep' do
-  scenario 'A non-logged in user cannot post a Peep to Chitter' do
+  scenario 'A non-logged in user cannot post a Peep' do
     visit('/')
     expect(page).not_to have_selector("#peep-form")
   end
 
-  # scenario 'A user can post a Peep to Chitter' do
-  #   visit('/')
-  #   fill_in('message', with: 'A Peep posted by a user')
-  #   click_button('Peep')
-  #
-  #   expect(page).not_to have_content 'A Peep posted by a user'
-  # end
+  scenario 'A logged in user can post a Peeprspec' do
+    User.create(first_name: 'Test',
+      last_name: 'McTest',
+      username: 'Testannosaurus',
+      email: 'test@example.com',
+      password: 'password123')
+
+    visit '/sessions/new'
+    fill_in(:email, with: 'test@example.com')
+    fill_in(:password, with: 'password123')
+    click_button('Sign in')
+    fill_in('message', with: 'A Peep posted by a user')
+    click_button('Peep')
+
+    expect(page).torpe have_content 'A Peep posted by a user'
+  end
 end
