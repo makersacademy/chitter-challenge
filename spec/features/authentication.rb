affb833a12e48b020481e4069ff4420b8f75a822
Tolvic <terence.h.mace@gmail.com>
2018-10-28 14:35:22 +0000

    Tests written and passed for signing out

--- spec/features/authentication.rb
+++ spec/features/authentication.rb
@@ -39,4 +39,21 @@ feature 'Feature - authentication' do
     expect(page).not_to have_content 'Welcome, test@example.com'
     expect(page).to have_content 'Please check your email or password.'
   end
+
+  scenario 'a user can sign out' do
+    # Create the user
+    User.create(email: 'test@example.com', password: 'password123')
+
+    # Sign in as that user
+    visit '/sessions/new'
+    fill_in(:email, with: 'test@example.com')
+    fill_in(:password, with: 'password123')
+    click_button('Sign in')
+
+    # Sign out
+    click_button('Sign out')
+
+    expect(page).not_to have_content 'Welcome, Testannosaurus'
+    expect(page).to have_content 'You have signed out.'
+  end
 end
