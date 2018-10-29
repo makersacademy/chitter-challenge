783a06bda9fdf5e15728eb79685395c3b162c26e
Irina Baldwin <irinabaldwin@irinas-air.lan>
2018-10-29 07:35:39 +0000

    Alter spec helper, trying to pass Travis build

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,10 @@ Rake.application.load_rakefile
 RSpec.configure do |config|
 
   config.before(:suite) do
-    Rake::Task['setup'].execute
+    # Rake::Task['setup'].execute
+    conn = PG.connect(dbname: 'chitter_test')
+    conn.exec("CREATE TABLE users(first_name VARCHAR, last_name VARCHAR, email VARCHAR UNIQUE, username VARCHAR UNIQUE, password VARCHAR, user_id SERIAL PRIMARY KEY);")
+    conn.exec("CREATE TABLE peeps(user_id INT4, content VARCHAR, time TIMESTAMP, post_id SERIAL PRIMARY KEY);")
   end
 
   config.before(:each) do
