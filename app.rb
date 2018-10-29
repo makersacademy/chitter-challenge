fc8ba06adf7981fa3de7ce585c3c3f8c1c5efd70
becc-mu <becc.mulugeta@gmail.com>
2018-10-29 08:53:21 +0000

    added db helpers

--- app.rb
+++ app.rb
@@ -14,10 +14,14 @@ class Chitter < Sinatra::Base
   end
 
   post '/signup' do
-    user = User.create(name: params[:name],
-    email: params[:email],
-    username: params[:username], password: params[:password])
-    session[:user_id] = user.id
+    user = params['user']
+  connection = PG.connect(dbname: 'chitter_test')
+  connection.exec("INSERT INTO user (name) VALUES('#{name}')")
+  redirect '/peeps'
+    # user = User.create(name: params[:name],
+    # email: params[:email],
+    # username: params[:username], password: params[:password])
+    # session[:user_id] = user.id
     end
 
   run! if app_file == $0
