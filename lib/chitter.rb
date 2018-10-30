82ffc120f9224bd506acd0ce45bcf8f0c774908f
Spectrumz <spectrumz@Kwames-MacBook-Pro.local>
2018-10-30 16:31:11 +0000

    Adding page view routes

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -7,5 +7,26 @@ class ChitterApp < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/sign_up' do
+    erb :sign_up
+  end
+
+  get '/login_in' do
+    erb :login_in
+  end
+
+  get '/feed' do
+    erb :feed
+  end
+
+  get '/post' do
+    erb :post
+  end
+
+  get '/unauthorized' do
+    erb :unauthorized
+  end
+
   run! if app_file == $0
 end
