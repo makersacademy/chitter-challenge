163366d35331e322e943d98526cc35933b9ef810
Irina Baldwin <irinabaldwin@irinas-air.lan>
2018-11-07 21:41:33 +0000

    Refactor rake tasks

--- Rakefile
+++ Rakefile
@@ -1,22 +1,32 @@
 require 'pg'
+require_relative './lib/database_connection'
+
+if ENV['RACK_ENV'] != 'production'
+  require 'rspec/core/rake_task'
+
+  RSpec::Core::RakeTask.new :spec
+
+  task default: [:spec]
+end
 
 task :test_database_setup do
   conn = PG.connect(dbname: 'chitter_test')
-  conn.exec("TRUNCATE users;")
-  conn.exec("TRUNCATE peeps;")
+  conn.exec("TRUNCATE users, peeps;")
 end
 
+# Sets up production and test databases if they don't exist
 task :setup do
-  p "Creating databases..."
-
   ['chitter', 'chitter_test'].each do |database|
-    conn = PG.connect
-    conn.exec("CREATE DATABASE #{database};")
     conn = PG.connect(dbname: database)
-    conn.exec("CREATE TABLE users(first_name VARCHAR, last_name VARCHAR, \
-      email VARCHAR UNIQUE, username VARCHAR UNIQUE, password VARCHAR, \
-      user_id SERIAL PRIMARY KEY);")
-    conn.exec("CREATE TABLE peeps(user_id INT4, content VARCHAR, \
-      time TIMESTAMP, post_id SERIAL PRIMARY KEY);")
+    if !conn
+      conn = PG.connect
+      conn.exec("CREATE DATABASE #{database};")
+      conn = PG.connect(dbname: database)
+      conn.exec("CREATE TABLE users(first_name VARCHAR, last_name VARCHAR, \
+        email VARCHAR UNIQUE, username VARCHAR UNIQUE, password VARCHAR, \
+        user_id SERIAL PRIMARY KEY);")
+      conn.exec("CREATE TABLE peeps(user_id INT4, content VARCHAR, \
+        time TIMESTAMP, post_id SERIAL PRIMARY KEY);")
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,17 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
 require 'rake'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Rake.application.load_rakefile
+
+Capybara.app = ChitterApp
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -9,17 +20,14 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
-Rake.application.load_rakefile
-
 RSpec.configure do |config|
 
-  # config.before(:suite) do
-  #   Rake::Task['setup'].execute
-  #   conn = PG.connect(dbname: 'chitter_test')
-  #   conn.exec("CREATE TABLE users(first_name VARCHAR, last_name VARCHAR, email VARCHAR UNIQUE, username VARCHAR UNIQUE, password VARCHAR, user_id SERIAL PRIMARY KEY);")
-  #   conn.exec("CREATE TABLE peeps(user_id INT4, content VARCHAR, time TIMESTAMP, post_id SERIAL PRIMARY KEY);")
-  # end
+  # Sets up necessary databases if they don't exist
+  config.before(:suite) do
+    Rake::Task['setup'].execute
+  end
 
+  # Truncates test database tables before each test
   config.before(:each) do
     Rake::Task['test_database_setup'].execute
   end
@@ -30,14 +38,3 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
-
-require 'capybara'
-require 'capybara/rspec'
-require 'rspec'
-
-ENV['RACK_ENV'] = 'test'
-
-# require our Sinatra app file
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
-
-Capybara.app = ChitterApp
