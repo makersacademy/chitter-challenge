122b40a8cdfb8fd959f4b552f6c581aa853d8689
Obsiye <mrabdirizak@gmail.com>
2018-11-21 13:46:49 +0000

    Initial test to create peep

---
+++ spec/features/create_peep_spec.rb
@@ -0,0 +1,8 @@
+feature 'create peep post' do
+  scenario 'submit create peep form' do
+    visit('/')
+    fill_in('peep', with: 'I am a peep.')
+    click_button('submit')
+    expect(page).to have_content('I am a peep.')
+  end
+end
---
+++ spec/peeps_spec.rb
@@ -0,0 +1,2 @@
+describe Peeps do
+end
