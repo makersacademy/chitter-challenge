fc06997cc2f270fc65b1d2a8985a4f2dbc53b79e
Obsiye <mrabdirizak@gmail.com>
2018-11-22 10:33:59 +0000

    Create peep and see peep on page

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra/base'
 require './lib/peep'
 
 class Chitter < Sinatra::Base
-
+  enable :sessions
+  
   get '/' do
     erb(:index)
   end
 
-  get '/peep/new' do
-    Peep.create(description: params[:peep])
+  get '/peeps' do
+   
+     p session[:peep]
+    @peep = session[:peep]
+    erb(:peeps)
+  end
+
+  post '/peeps/new' do
+    session[:peep] = Peep.create(description: params[:peep]).first
+    redirect '/peeps'
   end
 
 end
