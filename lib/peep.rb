c4d7cf9d35dd763dd14e48781f1467ba0db32982
Obsiye <mrabdirizak@gmail.com>
2018-11-22 12:17:28 +0000

    Use creation_time to store Timestamp and return as string HH:MM in 24 hour clock mode:

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,26 +1,23 @@
 require 'pg'
 
 class Peep
-  attr_reader :id, :description, :creation_date, :creation_time
+  attr_reader :id, :description, :creation_time
 
-  def initialize(id:, description:, creation_date:, creation_time:)
+  def initialize(id:, description:,creation_time:)
     @id = id
     @description = description
-    @creation_date = creation_date
     @creation_time = creation_time
   end
 
   def self.create(description:)
       db = connect()
-      db.exec("INSERT INTO peeps(description,user_id) VALUES('#{description}',0) RETURNING id,description,current_date")
+      db.exec("INSERT INTO peeps(description,user_id) VALUES('#{description}',0) RETURNING id,description")
   end
 
   def self.all
     db = connect()
-
-    result = db.exec("SELECT * FROM peeps ORDER BY id DESC;")
-
-    result.map { |peep| Peep.new(id: peep['id'], description: peep['description'], creation_date: peep['creation_date'], creation_time: peep['creation_time'])}
+    result = db.exec("SELECT id, description, COALESCE(to_char(creation_time, 'HH24:MI'), '') AS creation_time FROM peeps ORDER BY id DESC;")
+    result.map { |peep| Peep.new(id: peep['id'], description: peep['description'], creation_time: peep['creation_time'])}
   end
 
   def self.connect
