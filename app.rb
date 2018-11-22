e2ae0b7742f0ed426631a91250cd7252d5396509
Will Wayman <will.jdw@gmail.com>
2018-11-22 12:52:35 +0000

    updated app.rb with post route '/new'

--- app.rb
+++ app.rb
@@ -3,11 +3,17 @@ require 'sinatra/base'
 class Chitter < Sinatra::Base
 
   get '/' do
+    @peeps = Peep.all
     erb(:index)
   end
 
   get '/new' do
     erb(:new)
+  end
+
+  post '/new' do
+    Peep.create(text: params[:text], time: Time.now)
+    redirect '/'
   end
 
   run! if app_file == $0
