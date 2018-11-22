6de755e0dc2eb793b2ea916b98c270e0da2abb3a
Will Wayman <will.jdw@gmail.com>
2018-11-22 11:54:48 +0000

    feature test for posting a peep

---
+++ spec/features/post_peep_spec.rb
@@ -0,0 +1,16 @@
+feature 'post a peep' do
+  scenario 'can post a peep' do
+    visit('/new')
+    fill_in('text', with: 'test peep')
+    click_button('Submit')
+    expect(current_path).to eq('/')
+  end
+
+  scenario 'view peeps in reverse chronological order' do
+
+  end
+
+  scenario 'peeps show time posted' do
+
+  end
+end
