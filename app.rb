f2471fdb63b76ea014c4e8162da382e24fc03ebc
Obsiye <mrabdirizak@gmail.com>
2018-11-22 10:43:46 +0000

    Test that users can view peeps

--- app.rb
+++ app.rb
@@ -3,15 +3,13 @@ require './lib/peep'
 
 class Chitter < Sinatra::Base
   enable :sessions
-  
+
   get '/' do
     erb(:index)
   end
 
   get '/peeps' do
-   
-     p session[:peep]
-    @peep = session[:peep]
+    @peep = session[:peep]['description']
     erb(:peeps)
   end
 
--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,2 +1,12 @@
 feature 'view peeps' do
+
+  scenario 'view peeps' do
+    Peep.create(description: 'test information')
+    visit('/')
+    expect(page).to have_content('test information')
+  end
+
+  scenario 'view peeps in reverse chronological order' do
+  end
+
 end
