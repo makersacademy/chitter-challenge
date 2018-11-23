2f1814c775abd53ddf666e67e688ede767fc4db6
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-11-23 16:53:33 +0000

    Added file to set up test database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,6 +19,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
