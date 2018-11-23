f814d2b2321ea66a9ec168c25663c97c9d992ceb
Will Wayman <will.jdw@gmail.com>
2018-11-23 15:42:09 +0000

    updated sql

--- db/migrations/create_peeps_table.sql
+++ db/migrations/create_peeps_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE peeps (
   id SERIAL PRIMARY KEY,
   user_id INTEGER NOT NULL,
-  time_stamp TIMESTAMP NOT NULL,
+  time_stamp TIMESTAMP DEFAULT NOW(),
   peep varchar(140) NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
