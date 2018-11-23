b5197e0417c6a94d770f6208adff1f3ed6a9d283
Max Stevenson <maxstevenson@msn.com>
2018-11-23 11:57:51 +0000

    - Continued working on the DataMapper setup. - Now have a Peep model that is used to create a peep table in the database. - Manual feature testing confirms possible to add records to either table via direct ruby interaction via irb.

--- lib/dm.rb
+++ lib/dm.rb
@@ -11,15 +11,25 @@ class User
 
 	property :id, 						Serial
 	property :username, 			String
-	property :email, 					String
+	property :email, 					String, format: :email_address
 	property :first_name, 		String
 	property :last_name,			String
-	property :password, 			String
-	property :Date_of_birth, 	Integer
+	property :password, 			String, length: 8..16
+	property :date_of_birth, 	String, length: 8
+
+	has n, :peeps
 end
 
-class Peeps
+class Peep
 	include DataMapper::Resource
+
+	property :message_id,				Serial
+	property :message_content, 	Text, required: true, lazy: :false, length: 1..180
+	property :created_at,			 	DateTime
+	property :author,						String
+
+	belongs_to :user
 end
 
+DataMapper.finalize
 DataMapper.auto_upgrade!
