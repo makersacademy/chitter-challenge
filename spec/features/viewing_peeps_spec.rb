22d3c483a97c7e51e228c59b0661db4b2d373226
Max Stevenson <maxstevenson@msn.com>
2018-11-23 15:36:16 +0000

    - Completed first user story: Users can view peeps in reverse chronological order. - Tests passing and test coverage at 100%.

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -25,5 +25,29 @@ feature 'Viewing peeps' do
   end
 
   scenario 'Peeps should be shown in reverse chronological order' do
+  	User.create(
+  		username: 			"Test Usrname",
+  		email: 					"test@test.com",
+  		first_name: 		"Test",
+  		last_name: 			"Account",
+  		date_of_birth: 	"010191"
+  		)
+  	Peep.create(
+  		message_content: 	"I am the oldest peep",
+  		created_at: 			Time.new,
+  		author: 					"Test Account",
+  		user_id: 					1
+  		)
+  	Peep.create(
+  		message_content: 	"I am the newest peep",
+  		created_at: 			Time.new,
+  		author: 					"Test Account",
+  		user_id: 					1
+  		)
+    visit('/')
+    within ".timeline" do
+    	expect(find("li:first-child").text).to have_content("I am the oldest peep")
+      expect(find("li:last-child").text).to have_content("I am the newest peep")
+    end
   end
 end
