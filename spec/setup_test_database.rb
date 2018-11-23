ee8ac5f167c14a08e88948e379cf68bd0bfe9535
Ajay <ajaymungur@hotmail.com>
2018-11-23 10:15:51 +0000

    set up databases and test environment

---
+++ spec/setup_test_database.rb
@@ -0,0 +1,11 @@
+require 'pg'
+
+def setup_test_database
+  p "setting up test data base......"
+  tables = ["users","peeps"]
+  connection = PG.connect(dbname: 'chitter_test')
+
+  tables.each do |table|
+  connection.exec("TRUNCATE #{table};")
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,14 @@
-require 'simplecov'
-require 'simplecov-console'
-ENV['RACK_ENV'] = 'test'
-
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
+require 'simplecov'
+require 'simplecov-console'
+require_relative './setup_test_database'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
+ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
 
 Capybara.app = Chitter
 
@@ -18,6 +20,11 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+
+  config.before(:each) do
+    setup_test_database
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
