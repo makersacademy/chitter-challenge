686ae84bf21546ebdd87fe89a15b71483d2d80c5
Obsiye <mrabdirizak@gmail.com>
2018-11-24 19:26:23 +0000

    Feature Test login functionality and fail

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,16 @@
+feature 'log in' do
+  scenario 'login with existing account' do
+    # web_helper signup method
+    sign_up
+
+    click_link('login_signup')
+
+    fill_in('email', with: 'abdi@gmail.com')
+    fill_in('pwd', with: 'password123')
+    expect(page).to have_field("email")
+    expect(page).to have_field("pwd")
+    click_button('login')
+
+     expect(page).to have_field("Welcome back, Abdi!")
+  end
+end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -8,3 +8,16 @@ def setup_database
   db = db_connection
   db.exec('TRUNCATE users;')
 end
+
+def sign_up
+  visit('/')
+  click_link('login_signup')
+  click_link('create_account')
+  fill_in('fname', with: 'Abdi')
+  fill_in('lname', with: 'Abdi')
+  fill_in('email', with: 'abdi@gmail.com')
+  fill_in('confirm_email', with: 'abdi@gmail.com')
+  fill_in('pwd', with: 'password123')
+  fill_in('confirm_pwd', with: 'password123')
+  click_button('signup')
+end
