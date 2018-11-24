624210fa0c9aac56d44ed546e11663966090b7c2
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-24 11:34:33 +0000

    setup test database method implemented

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'simplecov-console'
 require 'rspec'
 require 'capybara'
 require 'capybara/rspec'
+require 'setup_test_database'
 
 Capybara.app = Chitter
 
@@ -18,9 +19,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+
 end
