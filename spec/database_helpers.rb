2e5f9496bf44c2810905e3c0ff100a661a710bac
Chris Gilbert <chrisgilbert4@icloud.com>
2018-11-25 10:51:54 +0000

    user double intoduced into Peep tests. persist user data helper method added

--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -5,3 +5,9 @@ def persisted_data(table:, id:)
   result = connection.query("SELECT * FROM #{table} WHERE id = #{id};")
   result.first
 end
+
+def persist_user_data(name:, username:, email:, password:)
+  connection = PG.connect(dbname: 'chitter_test')
+  result = connection.query("INSERT INTO users (name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{password}') RETURNING id, name, username, email;")
+  result.first
+end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -4,34 +4,43 @@ describe Peep do
 
   describe '.create' do
     it 'creates a new peep' do
-      user = User.create(
+      user = double(
         name: 'Test User',
         email: 'test@email.com',
         username: 'test username',
         password: 'password123'
       )
-      peep = Peep.create(text: 'This is a test peep', user_id: user.id)
+
+      persisted_user_data = persist_user_data(name: user.name, username: user.username, email: user.email, password: user.password)
+
+      user_id = persisted_user_data['id']
+
+      peep = Peep.create(text: 'This is a test peep', user_id: user_id)
 
       persisted_data = persisted_data(table: 'peeps', id: peep.id )
 
       expect(peep).to be_a Peep
       expect(peep.id).to eq persisted_data['id']
       expect(peep.text).to eq 'This is a test peep'
-      expect(peep.user_id).to eq user.id
+      expect(peep.user_id).to eq user_id
       expect(peep.tstz).to eq persisted_data['tstz']
     end
   end
 
   describe '.all' do
     it 'returns all the peeps' do
-      user = User.create(
+      user = double(
         name: 'Test User',
         email: 'test@email.com',
         username: 'test username',
         password: 'password123'
       )
 
-      peep = Peep.create(text: 'This is a test peep', user_id: user.id)
+      persisted_user_data = persist_user_data(name: user.name, username: user.username, email: user.email, password: user.password)
+
+      user_id = persisted_user_data['id']
+
+      peep = Peep.create(text: 'This is a test peep', user_id: user_id)
 
       persisted_data = PG.connect(dbname: 'chitter_test').query("SELECT * FROM peeps WHERE id = #{peep.id}")
       expect(peep.id).to eq persisted_data.first['id']
