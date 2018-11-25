67279439f214ab8d642eade20f116f9a6fc6bc44
luisatheodoro <theodoro.luisa@gmail.com>
2018-11-25 16:54:04 +0000

    Finishing third user story with showing time and date message was created

--- lib/message.rb
+++ lib/message.rb
@@ -7,30 +7,25 @@ class Message
     @connection = PG.connect(dbname: 'chitter_chatter')
   end
 
-  attr_reader :id, :title, :body
+  attr_reader :id, :title, :body, :created_at
 
-  def initialize(id:, title:, body:)
+  def initialize(id:, title:, body:, created_at:)
     @id = id
     @title = title
     @body = body
+    @created_at = created_at
   end
 
   def self.add(body:, title:)
-    result = @connection.exec("INSERT INTO messages (body, title) VALUES('#{body}', '#{title}') RETURNING id, title, body;")
-    Message.new(id: result[0]['id'], title: result[0]['title'], body: result[0]['body'])
+    result = @connection.exec("INSERT INTO messages (body, title) VALUES('#{body}', '#{title}') RETURNING id, title, body, created_at;")
+    Message.new(id: result[0]['id'], title: result[0]['title'], body: result[0]['body'], created_at: result[0]['created_at'])
   end
 
   def self.all
     result = @connection.exec("SELECT * FROM messages ORDER BY ID DESC;")
     result.map do |message|
-      Message.new(id: message['id'], title: message['title'], body: message['body'])
+      Message.new(id: message['id'], title: message['title'], body: message['body'], created_at: message['created_at'])
     end
   end
 
-  # def self.all_in_reverse_order
-  #   result = @connection.exec("SELECT * FROM messages ORDER BY ID DESC;")
-  #   result.map do |message|
-  #     Message.new(id: message['id'], title: message['title'], body: message['body'])
-  #   end
-  # end
 end
