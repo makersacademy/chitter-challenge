2c0373d2e76ae1a3d132f535f82a923d75e6d91b
cesare camurani <cesare.camurani@gmail.com>
2018-11-25 12:02:56 +0000

    added user - session views

--- app.rb
+++ app.rb
@@ -15,9 +15,40 @@ class Controller < Sinatra::Base
     erb :home
   end
 
-  get '/peeps' do
+  get '/peeps_page' do
     @user = User.find(id: session[:user_id])
     erb :peeps
+  end
+
+  get '/users/user_new' do
+    erb :"users/user_new"
+  end
+
+  post '/users' do
+    user = User.create(email: params['email'], password: params['password'])
+    session[:user_id] = user.id
+    redirect '/peeps_page'
+  end
+
+  get '/sessions/session_new' do
+    erb :"sessions/session_new"
+  end
+
+  post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect('/peeps_page')
+    else
+      flash[:notice] = 'Please check your email or password.'
+      redirect('/sessions/session_new')
+    end
+  end
+
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = 'You have signed out.'
+    redirect('/peeps_page')
   end
 
   run! if app_file == $0
