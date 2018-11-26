a3dc35123235d0952edf79dc966a85aef7c15583
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-26 10:56:11 +0000

    Viewing messages from others

--- spec/features/viewing_chitter_spec.rb
+++ spec/features/viewing_chitter_spec.rb
@@ -3,4 +3,14 @@ feature 'Viewing Chitter' do
 		visit('/')
 		expect(page).to have_content('Welcome to Chitter!')
 	end
+
+	scenario 'Seeing messages from others' do
+		time = Time.now
+		user = User.create(name: 'test', u_name: 'u_test12', email: 'test12@test.com', password: 'test1234')
+		message = Message.create(text: 'testing', m_date: time.strftime("%Y-%m-%d"), user_id: user.id)
+		visit('/')
+		expect(page).to have_content('Welcome to Chitter!')
+		expect(page).to have_content message.text
+		expect(page).to have_content user.name
+	end
 end
