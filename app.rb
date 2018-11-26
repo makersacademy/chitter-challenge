03ff53e1e254f12c745f83b6792cc641d5b15620
Stephanie Parker <stephanieparker@MacBook-Pro-3.local>
2018-11-26 10:34:06 +0000

    after sign up, the users name is displayed

--- app.rb
+++ app.rb
@@ -5,17 +5,21 @@ require './database_connection_setup'
 
 class Chitter < Sinatra::Base
 
+    enable :sessions
+
   get '/' do
     erb(:login)
   end
 
   get '/peeps' do
     @peeps = Peep.all
+    @name = session[:name]
     erb(:index)
   end
 
   post '/peep' do
     Peep.post(message: params['message'])
+    session[:name] = params[:name]
     redirect '/peeps'
   end
 
