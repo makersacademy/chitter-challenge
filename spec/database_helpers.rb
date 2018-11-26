0246138ae1f6fd505774883cdbcee0873c1c43b8
Cristopher Palacios <criszelaya24@hotmail.com>
2018-11-26 10:49:59 +0000

    Fixing test for class message

--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -3,3 +3,8 @@ def persisted_data(id:, table: 'users')
 	connection = PG.connect(dbname: 'chitter_test')
   	connection.query("SELECT * FROM #{table} WHERE id = '#{id}';")
 end
+
+def persisted_data_message(id:, table: 'messages')
+	connection = PG.connect(dbname: 'chitter_test')
+  	connection.query("SELECT * FROM #{table} WHERE user_id = '#{id}';")
+end
