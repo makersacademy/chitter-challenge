548755857809a1584b9b317bf38603b952c77a96
Ajay <ajaymungur@hotmail.com>
2019-01-02 13:25:38 +0000

    Change made to Database_connection_setup

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -3,7 +3,7 @@ require './lib/database_connection'
 if ENV['ENVIRONMENT'] == 'Test'
   DatabaseConnection.setup('chitter_test')
 elsif ENV['RACK_ENV'] == 'Production'
-  DatabaseConnection.setup ENV['DATABASE_URI']
+  DatabaseConnection.setup(ENV['DATABASE_URL'])
 else
   DatabaseConnection.setup('chitter')
 end
