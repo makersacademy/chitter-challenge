a4d7a59f1ef81b8033ff837f85add9a944d32492
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-12 19:17:58 +0000

    Add test for signing up and redirecting to profile page

--- spec/features/displaying_peeps_spec.rb
+++ spec/features/displaying_peeps_spec.rb
@@ -7,14 +7,13 @@ feature 'Displaying Peeps' do
   end
 
   scenario 'A user can see a history of all previous peeps' do
-    fill_in :peep, with: 'Second test peep'
-    click_button 'Submit'
+    second_peep
 
     expect(page).to have_content 'Test peep'
     expect(page).to have_content 'Second test peep'
   end
 
-  scenario 'A user can see the the peeps in reverse chronological order' do
+  scenario 'A user can see the peeps in reverse chronological order' do
     second_peep
     expect('Second test peep').to appear_before 'Test peep'
   end
