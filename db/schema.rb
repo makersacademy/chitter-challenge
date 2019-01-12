99de85432cf2e0a4c1693cde7279daccbf79543c
stonefarmer9 <g.p.levaillant@gmail.com>
2019-01-12 15:12:42 +0000

    Database cleaner gem activated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_112_112_102) do
+ActiveRecord::Schema.define(version: 2019_01_12_112102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
