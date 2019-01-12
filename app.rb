d0e69d8a6a6d4241617e057f2e64e0c372427511
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-01-12 21:02:59 +0000

    Added name in the model, view and controller

--- app.rb
+++ app.rb
@@ -18,7 +18,7 @@ class Chitter < Sinatra::Base
   end
 
   post '/sign-up' do
-    user = User.create(username: params[:username], email: params[:email], password: params[:password])
+    user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
   if user
       session[:user_id] = user.id
       redirect '/profile'
