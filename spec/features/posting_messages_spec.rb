5b4b28ba2404f7e830dbf52afb24ae848b7320c0
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-01-12 14:50:24 +0000

    updated post message form

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -1,2 +1,11 @@
 RSpec.feature 'posting messages' do
+
+    context ' a user is logged in' do 
+        before {
+            generic_signup
+        }
+        it 'displays the post message form if user is logged in' do 
+            expect(page).to have_selector("input[name = message]")
+        end
+    end
 end
