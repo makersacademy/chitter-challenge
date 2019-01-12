c62179f55c3806df62563643162f1a99efc2d215
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-12 18:03:43 +0000

    Add feature test for peep time display

--- spec/features/displaying_peeps_spec.rb
+++ spec/features/displaying_peeps_spec.rb
@@ -1,5 +1,5 @@
 feature 'Displaying Peeps' do
-  let!(:peep) { Peep.create(text: 'Test peep') }
+  let!(:peep) { Peep.create(text: 'Test peep', created_at: '2000-01-01 00:00:00') }
   background { visit '/' }
 
   scenario 'A user can see a peep' do
@@ -17,5 +17,9 @@ feature 'Displaying Peeps' do
   scenario 'A user can see the the peeps in reverse chronological order' do
     second_peep
     expect('Second test peep').to appear_before 'Test peep'
+  end
+
+  scenario 'A user can see the time that a peep was created at' do
+    expect(page).to have_content '2000-01-01 00:00:00'
   end
 end
