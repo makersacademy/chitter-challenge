025fbdbbf17618d067d8b5a877618b39c227492e
James Hotblack <james.c.hotblack@gmail.com>
2019-01-12 16:40:41 +0000

    Added Config.ru and DataMapper config. Updated Gemfile.

--- Gemfile
+++ Gemfile
@@ -1,9 +1,14 @@
 source 'https://rubygems.org'
 
-ruby '2.5.0'
+ruby '2.5.3'
 
+gem 'capybara'
+gem "sinatra"
 gem 'rake'
+gem 'pry'
 gem 'rubocop', '0.56.0'
+gem 'dm-postgres-adapter'
+gem 'data_mapper', '1.2.0'
 
 group :test do
   gem 'rspec'
---
+++ config/data_mapper.rb
@@ -0,0 +1,12 @@
+require 'data_mapper'
+
+
+DataMapper.setup(:default, "postgres://localhost/MessengerApp_#{ENV['RACK_ENV']}")
+
+DataMapper.finalize
+
+if ENV['RACK_ENV'] == 'development'
+  DataMapper.auto_upgrade!
+elsif ENV['RACK_ENV'] == 'test'
+  DataMapper.auto_migrate!
+end
