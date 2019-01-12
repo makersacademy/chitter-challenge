610f02fff356b2130313508a0cdf86a857464db8
Jethro Whaley <jethro.whaley@gmail.com>
2019-01-12 13:01:33 +0000

    Updated gemfile and spec_helper - DB cleaner added

--- Gemfile
+++ Gemfile
@@ -2,10 +2,14 @@ source 'https://rubygems.org'
 
 ruby '2.5.0'
 
+gem 'pry'
 gem 'rake'
 gem 'rubocop', '0.56.0'
+gem 'sinatra'
 
 group :test do
+  gem 'capybara'
+  gem 'database_cleaner'  
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,4 +14,15 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 end
