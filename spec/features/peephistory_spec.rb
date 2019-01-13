aa82ae7482b109ae29c8a3c29d71139a43c06d3a
Simon Cass <scass1991@gmail.com>
2019-01-13 16:00:42 +0000

    further historic tests added

--- spec/features/peephistory_spec.rb
+++ spec/features/peephistory_spec.rb
@@ -1,6 +1,13 @@
 feature "Peep History is visible" do
-  scenario "A Maker can see previously posted peeps" do
+  scenario "A Maker can see their first posted peep" do
     createpeep
     expect(page).to have_content("Hello, world")
+  end
+
+  scenario "A Maker is able to see a history of peeps" do
+    createpeep
+    createsecondpeep
+    expect(page).to have_content("Hello, world")
+    expect(page).to have_content("Hello again, world")
   end
 end
--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -3,3 +3,9 @@ def createpeep
   fill_in :peep, with: "Hello, world"
   click_button "Submit"
 end
+
+def createsecondpeep
+  visit "/"
+  fill_in :peep, with: "Hello again, world"
+  click_button "Submit"
+end
