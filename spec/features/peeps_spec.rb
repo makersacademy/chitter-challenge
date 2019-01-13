3d08f5c8b45517a33037de82530adb6c97c767ea
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-01-13 14:52:33 +0000

    Most recent peeps are at the top of the Chitter feed

--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -1,3 +1,11 @@
+RSpec::Matchers.define :appear_before do |later_content|
+  match do |earlier_content|
+    begin
+      page.body.index(earlier_content) < page.body.index(later_content)
+    end
+  end
+end
+
 feature 'User peeps' do
 
   scenario 'can be written' do
@@ -16,14 +24,19 @@ feature 'User peeps' do
 end
 
 
-# feature 'All peeps' do
+ feature 'All peeps' do
 
-#   scenario 'are displayed in reverse chronological order' do
-  
-#   end
+   scenario 'are displayed in reverse chronological order' do
+    sign_up
+    fill_in :content, with: 'Oldest!'
+    click_button 'Post'
+    fill_in :content, with: 'Newest!'
+    click_button 'Post'
+    expect('Newest').to appear_before('Oldest')
+   end
 
 #   scenario 'show time created' do
   
 #   end
 
-# end
+ end
