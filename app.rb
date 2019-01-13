ae1c4b5013dcc534307291f9aef2a0e739eef67b
Carol Gardiner <carolan.gardiner@gmail.com>
2019-01-13 20:14:25 +0000

    corrected linter error

--- app.rb
+++ app.rb
@@ -22,12 +22,12 @@ class Chitter < Sinatra::Base
     user = User.create({ firstname: params[:firstname], 
       surname: params[:surname], username: params[:username], 
       email: params[:email], password: params[:password] })
-      if user.valid?
-        session[:id] = user.id
-        redirect "/profile/#{session[:id]}"
-      else
-        redirect '/signin'
-      end
+    if user.valid?
+      session[:id] = user.id
+      redirect "/profile/#{session[:id]}"
+    else
+      redirect '/signin'
+    end
   end
 
   get '/signin' do
