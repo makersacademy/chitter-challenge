a80674723dc17b80f381c18ab0fd6323a24809a0
Jacques <jacques.laffy@gmail.com>
2019-01-13 17:16:43 +0000

    Linted and Loaded + Feature for Sign Up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,10 +17,10 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
-  #config.before(:suite) do
-    #DatabaseCleaner.strategy = :transaction
-    #DatabaseCleaner.clean_with(:truncation)
-  #end
+  # config.before(:suite) do
+    # DatabaseCleaner.strategy = :transaction
+    # DatabaseCleaner.clean_with(:truncation)
+  # end
 
   config.around(:each) do |test|
     DatabaseCleaner.cleaning do
