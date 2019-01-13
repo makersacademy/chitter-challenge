c2aa8ad1233a08ddd1065180a13ec72c53badf53
James Hotblack <james.c.hotblack@gmail.com>
2019-01-13 12:28:40 +0000

    Added Sign up and Sign in and log out. Sessions persisting data.

--- app.rb
+++ app.rb
@@ -11,7 +11,7 @@ class Chitter < Sinatra::Base
 
   get '/' do
     @messages = Message.all
-    erb(:index)
+    erb :index
   end
 
   post '/message' do
@@ -26,16 +26,59 @@ class Chitter < Sinatra::Base
   post '/signup' do
     user = User.authenticate(params[:email], params[:password])
     if user
-      redirect '/'
+      redirect '/error'
     else  
-      user = User.create(email: params[:email], password: params[:password])
+      user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
       if user
         session[:user_id] = user.id
-        redirect '/profile'
+        redirect ("/profile")
       else
         redirect '/'
       end
     end
+  end
+
+  get '/signin' do
+    erb :signin
+  end
+
+  post '/signin' do
+    user = User.authenticate(params[:email], params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect ("/profile")
+    else
+      redirect '/'
+    end
+  end
+
+  get '/profile' do
+    if signed_in?
+      @messages = Message.all
+      erb :profile
+    else
+      redirect 'signin'
+    end
+  end
+
+  get '/error' do
+    erb :error
+  end
+
+  delete '/sessions' do
+    session.delete(:user_id)
+    redirect '/'
+  end
+
+
+  private
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.get(session[:user_id])
   end
 
   run! if app_file == $0
--- spec/features/user_auth_spec.rb
+++ spec/features/user_auth_spec.rb
@@ -1,13 +1,15 @@
 RSpec.feature 'User Authentication' do
-  context 'Sign Up' do
+  context '4. Sign Up' do
     scenario 'A user can signup' do
       visit '/'
-      click_on 'Sign up'
+      click_on 'Sign Up'
+      fill_in :name, with: 'test'
+      fill_in :username, with: '@test'
       fill_in :email, with: 'test@test.com'
       fill_in :password, with: 'secret123'
-      click_button 'Sign up'
+      click_button 'Sign Up'
 
-      expect(page).to have_content 'Welcome, test@test.com'
+      expect(page).to have_content 'Welcome to the Peep Show @test'
     end
   end
 end
