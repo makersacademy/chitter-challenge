9a11ed911f13ab49c27a6ff45e618b28ba2a8e5e
Erin Stevenson <estevenson_94@live.com>
2019-01-30 18:34:17 +0000

    3. Adding the first peeps

--- spec/features/index_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Viewing peeps' do 
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content 'Welcome to Chitter'
-  end
-end
---
+++ spec/features/peeps_spec.rb
@@ -0,0 +1,13 @@
+feature 'Viewing peeps' do 
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content 'Welcome to Chitter'
+  end
+  scenario 'viewing peeps' do
+    visit('/')
+    click_button('see peeps')
+
+    expect(page).to have_content "Hello"
+    expect(page).to have_content "This is my second peep"
+  end
+end
