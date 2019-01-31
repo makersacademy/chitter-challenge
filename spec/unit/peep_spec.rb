841c4b88870ac4407ce4c91fc68f6edf55ed396d
Simon Green <simon.green@thielsen.co.uk>
2019-01-31 15:46:53 +0000

    Added test for peep

--- spec/unit/peep_spec.rb
+++ spec/unit/peep_spec.rb
@@ -2,4 +2,13 @@ require './app/models/peep'
 
 RSpec.describe Peep do
 
+  before(:each) do
+    test_db_initialise
+  end
+
+  it 'should return all peeps' do
+    expect(Peep.all[0].message).to include('first peep in the test database')
+    expect(Peep.all[5].message).to include('sixth peep in the test database')
+  end
+
 end
