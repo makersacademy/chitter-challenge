87bd2c26461f76fb72062cd49aac9ffc12dd820c
Simon Green <simon.green@thielsen.co.uk>
2019-01-31 15:47:08 +0000

    configured to load db connection pool

--- chitter.rb
+++ chitter.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require './app/models/peep'
+require './app/models/user'
+require './app/models/db_connection'
 
 class Chitter < Sinatra::Base
 
   get '/' do
+    ENV['RAILS_ENV'] = 'development' if ENV['RAILS_ENV'] != 'test'
+    @peeps = Peep.all
     erb :index
   end
+
+ DatabaseConnection.setup
 
 end
