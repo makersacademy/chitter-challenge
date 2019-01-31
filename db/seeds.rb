8f2e016773521584c8749570cbb4359210121908
Simon Green <simon.green@thielsen.co.uk>
2019-01-31 16:47:33 +0000

    Updated database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require './app/models/user'
 require './app/models/peep'
 
-User.create(id: 1, username: 'al123', forename: 'Alice', surname: 'Lice', email: 'alice.lice@gmail.com', password: 'password123')
-User.create(id: 2, username: 'bo123', forename: 'Bob', surname: 'Ob', email: 'bob.ob@gmail.com', password: 'password321')
+User.create(id: 1, username: 'al123', forename: 'Alice', surname: 'Smith', email: 'alice.smith@gmail.com', password: 'password123')
+User.create(id: 2, username: 'bo123', forename: 'Bob', surname: 'Smith', email: 'bob.smith@gmail.com', password: 'password321')
 Peep.create(id: 1, message: 'The first peep in the test database', user: 1)
 Peep.create(id: 2, message: 'The second peep in the test database', user: 1)
 Peep.create(id: 3, message: 'The third peep in the test database', user: 1)
