16adc811a4026515762e245f43baf103eb87e3af
Debora <debora.piu92@gmail.com>
2019-02-01 22:44:56 +0000

    empty peeps post attempts are ignored

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -17,8 +17,10 @@ class Peeps
 
   def self.post(text)
     database_connection
-    new_peep  = sanitize(text)
-    @connection.exec("INSERT INTO peeps (peep, time) VALUES ('#{new_peep}', 'NOW');")
+    if text.length > 0
+      new_peep  = sanitize(text)
+      @connection.exec("INSERT INTO peeps (peep, time) VALUES ('#{new_peep}', 'NOW');")
+    end
   end
 
   private
