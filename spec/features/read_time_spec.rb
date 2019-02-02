b65d969af8442354ba4c771ef45e2682b6837276
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-02 13:45:48 +0000

    3. Feature test for displaying time

---
+++ spec/features/read_time_spec.rb
@@ -0,0 +1,13 @@
+# As a Maker
+# So that I can better appreciate the context of a peep
+# I want to see the time at which it was made
+
+feature 'users should see the time peeps were posted' do
+
+  scenario 'peeps display created timestamp' do
+    post_one_message
+    time = "#{Time.now.hour}" + ":" + "#{Time.now.min}"
+    expect(page).to have_content(time)
+  end
+
+end
