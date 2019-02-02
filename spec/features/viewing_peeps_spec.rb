d81563af47e1790cdcc11581043aaa0fef8fcaee
Szilvi Horvath <szil.horvath@gmail.com>
2019-02-02 20:25:42 +0000

    peeps are displayed in reverse chronological order

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,13 +1,13 @@
 require 'pg'
 
 feature 'Viewing peeps' do
-  scenario 'A user can see previous peeps' do
+  scenario 'A user can see previous peeps in reverse chronological order' do
     Peeps.create(peep: 'First peep')
     Peeps.create(peep: 'Second peep')
 
     visit('/peeps')
 
-    expect(page).to have_content "First peep"
     expect(page).to have_content "Second peep"
+    expect(page).to have_content "First peep"
   end
 end
