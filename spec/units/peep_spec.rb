a09edbcd30973f6131cd7b58af34c9ab9eac17a9
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-02 13:59:37 +0000

    3. Unit test for displaying time

--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -4,11 +4,20 @@ describe Peep do
 
   describe 'creation of a new peep' do
 
-    it 'should contain a message body' do
+    it 'contains a message body' do
       message = "I'm a peep message body"
       peep = Peep.create(message: message)
       expect(peep.message).to eq message
     end
+
+    it 'knows when it was created' do
+      message = "I'm a peep message body"
+      time = "#{Time.now.hour}" + ":" + "#{Time.now.min}"
+      peep = Peep.create(message: message)
+      expect(peep.created_at.to_s).to include(time)
+    end
   end
+
+
 
 end
