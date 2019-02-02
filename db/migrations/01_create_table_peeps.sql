c4cf1877c6e6fff37a3bf829a16fa6c281540d08
Debora <debora.piu92@gmail.com>
2019-02-02 00:29:10 +0000

    username added to table peeps

--- db/migrations/01_create_table_peeps.sql
+++ db/migrations/01_create_table_peeps.sql
@@ -1,1 +1,1 @@
- CREATE TABLE peeps(peep VARCHAR(200) NOT NULL CHECK(peep <> ''), time TIMESTAMP);
+ CREATE TABLE peeps(peep VARCHAR(200) NOT NULL CHECK(peep <> ''), time TIMESTAMP, username VARCHAR(20) NOT NULL CHECK(username <> ''));
