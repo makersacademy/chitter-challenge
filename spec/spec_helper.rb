d24bc15c90d67e29e95e6b0fc1f56c02026188df
Brooke Woolley <brooke.woolley@gmail.com>
2019-02-02 11:07:51 +0000

    Project set up: spec files, app.rb and config.ru

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,25 @@
+ENV['ENVIRONMENT'] = 'test'
+
 require 'simplecov'
 require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'sinatra'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# tell Capybara about our app class
+Capybara.app = Chitter
 
 RSpec.configure do |config|
   config.after(:suite) do
