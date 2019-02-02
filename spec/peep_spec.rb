c02d35ab2bc720b08178dbbe3a1a2c168b57d843
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-02 17:19:18 +0000

    modify peep_spec.rb with tests for '.add' and '.create'

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -6,7 +6,18 @@ RSpec.describe Peep do
     it 'returns all peeps' do
       # connection = PG.connect(dbname: 'chitter_test')
       Peep.create(peep: 'I ate some cake')
+      p Peep.all
       expect(Peep.all.first.peep).to eq('I ate some cake')
+      expect(Peep.all.length).to eq(1)
+    end
+  end
+
+  describe '.create' do
+    it 'creates a new peep' do
+      first_peep = Peep.create(peep: 'I ate some cake')
+      expect(first_peep).to be_a(Peep)
+      expect(first_peep.peep).to eq('I ate some cake')
+      expect(first_peep.id).to eq('1')
     end
   end
 end
