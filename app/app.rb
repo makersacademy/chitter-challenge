d9496f8be53ec53bfaba7e10f20d2c89facb7009
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-02 13:52:12 +0000

    add post route '/process-peep'

--- app/app.rb
+++ app/app.rb
@@ -1,8 +1,19 @@
+require 'pg'
 require 'sinatra/base'
+require_relative './lib/peep'
 
 class Chitter < Sinatra::Base
   get '/' do
+    @peeps = Peep.all
+    p @peeps
+    p @peeps[0]
+    # p @peeps[0]['peep']
     erb :index
+  end
+
+  post '/process-peep' do
+    Peep.create(peep: params[:peep])
+    redirect '/'
   end
 
   run! if app_file == $0
