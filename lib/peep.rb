a21f723d33d006fc107761f811756a3a765adc99
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-02 16:29:40 +0000

    4. Add validation to database classes

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,7 +2,7 @@ require 'active_record'
 require 'pg'
 
 class Peep < ActiveRecord::Base
-
+  validates :message, presence: true
 
 
 end
--- lib/user.rb
+++ lib/user.rb
@@ -2,5 +2,9 @@ require 'active_record'
 require 'pg'
 
 class User < ActiveRecord::Base
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :username, presence: true
+  validates :password, presence: true
 
 end
