edb66a57ce615692292eebb40f5d03280ca5c626
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-02 14:10:47 +0000

    Changes to controller which will be using Message class

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
+require './lib/message'
 
 class Chitter < Sinatra::Base
 
   get '/' do
-    'Chitter'
     erb(:index)
+  end
+
+  get '/messages' do
+    @tweets = Message.display
+    erb(:list_of_messages)
   end
 
 end
