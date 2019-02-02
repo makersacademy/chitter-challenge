aae9eeb8e79df5b296bf9413aa5650e6531f3042
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-02 17:10:22 +0000

    Test: User can see the time at which message was posted

--- spec/features/chitter_feature_spec.rb
+++ spec/features/chitter_feature_spec.rb
@@ -1,5 +1,6 @@
 require './app'
 require 'orderly'
+require 'time'
 
 feature 'posting a message' do
   scenario 'user can post a message to chitter' do
@@ -22,6 +23,17 @@ feature 'messages are arranged' do
     click_button 'Submit'
     visit('/messages')
     expect("I love beer").to appear_before("I love Sinatra")
+
+  end
+end
+
+feature 'messages get a time stamp' do
+  scenario 'User can see the time at which message was posted' do
+    visit('/')
+    fill_in 'message', with: "I love Sinatra"
+    click_button 'Submit'
+    visit('/messages')
+    expect(page).to have_content("I love Sinatra #{Time_Calculation.calculation}")
 
   end
 end
