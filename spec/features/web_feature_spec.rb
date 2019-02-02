2a0a3b78c7e4495c3eb542a5bfae4c3304141ac0
Aiden Gleave <aidengleave@gmail.com>
2019-02-02 22:50:51 +0000

    Refactored feature test to write a peep

--- spec/features/web_feature_spec.rb
+++ spec/features/web_feature_spec.rb
@@ -1,6 +1,8 @@
 feature 'user can write peeps and they display on the page' do
   scenario 'user can write a peep' do
     visit('/')
+    fill_in('peep', with: 'Test peep')
+    click_button('Submit')
     expect(page).to have_content('Test peep')
   end
 end
