29fac7bc4023bee90b058ada241fe39702341ce6
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-02 17:20:03 +0000

    add truncate_test_database method to setup_test_database.rb'

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,5 +3,12 @@ require 'pg'
 def setup_test_database
   p "Setting up Chitter's test database..."
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec("TRUNCATE peeps;")
+  connection.exec("TRUNCATE peeps RESTART IDENTITY;")
 end
+
+def truncate_test_database
+  p "Truncating Chitter's test database..."
+  connection = PG.connect(dbname: 'chitter_test')
+  connection.exec("TRUNCATE peeps RESTART IDENTITY;")
+end
+
