7a2cddbf87607deec68b7799805b354e3b01837b
James <james.rudrum@hyperisland.se>
2019-02-02 17:32:57 +0000

    Added feature test to add post

---
+++ spec/feature/add_new_post_spec.rb
@@ -0,0 +1,12 @@
+feature 'Add posts' do
+  scenario 'user can add post' do
+
+    visit('/posts/new')
+
+    fill_in('chit', with: 'Test chit 04')
+    click_button('Add Chit!')
+
+    expect(page).to have_content 'Test chit 04'
+
+  end
+end
