029e5a786c423ecb6174eef71b748174848b25a9
Debora <debora.piu92@gmail.com>
2019-02-02 01:14:32 +0000

    web-helper enters username with peeps

--- spec/web-helper.rb
+++ spec/web-helper.rb
@@ -1,9 +1,9 @@
 def test_configure
   @connection = PG.connect(dbname: 'chitter_test')
   @connection.exec "TRUNCATE TABLE peeps;"
-  @connection.exec "INSERT INTO peeps (peep, time) VALUES ('Hello there', 'NOW()')"
-  @connection.exec "INSERT INTO peeps (peep, time) VALUES ('Another peep below', 'NOW()')"
-  @connection.exec "INSERT INTO peeps (peep, time) VALUES ('This is a longer peep to see how it will be accepted in the test enviroment', 'NOW()')"
+  @connection.exec "INSERT INTO peeps (peep, time, username) VALUES ('Hello there', 'NOW()', 'Debora')"
+  @connection.exec "INSERT INTO peeps (peep, time, username) VALUES ('Another peep below', 'NOW()', 'Paul')"
+  @connection.exec "INSERT INTO peeps (peep, time, username) VALUES ('This is a longer peep to see how it will be accepted in the test enviroment', 'NOW()', 'Simon')"
   @connection.exec "TRUNCATE TABLE users;"
   @connection.exec "INSERT INTO users (username, email, password) VALUES ('Debora38', 'debora@email.com', 'secretpassword')"
 end
