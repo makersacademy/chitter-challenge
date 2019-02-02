e4f21ecbe37f9c6d099782f0be395513286ee3d7
Aiden Gleave <aidengleave@gmail.com>
2019-02-02 22:53:38 +0000

    Refactored app to use form to write peep to page - passing the test

--- app/app.rb
+++ app/app.rb
@@ -2,8 +2,12 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
   get '/' do
-    'Test peep'
+    erb(:peep)
   end
-  
+
+  post '/peep' do
+    @peeps = params[:peep]
+  end
+
   run! if app_file == $0
 end
