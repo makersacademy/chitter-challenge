f85e7a81000c2d558606f2db5160abe250a3f00a
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-02 18:02:38 +0000

    6. Logout feature test

---
+++ spec/features/logout_spec.rb
@@ -0,0 +1,18 @@
+# As a Maker
+# So that I can avoid others posting messages on Chitter as me
+# I want to log out of Chitter
+
+feature 'users can logout' do
+
+  scenario 'logout button clears user session' do
+    visit_site_and_click_sign_up
+    fill_in_signup_form
+    expect(page).to have_content 'Welcome, Ivan the Terrible'
+    expect(page).to_not have_button('Login')
+    expect(page).to_not have_button('Sign Up')
+    click_button 'Logout'
+    expect(page).to_not have_content 'Welcome, Ivan the Terrible'
+    expect(page).to have_button('Login')
+    expect(page).to have_button('Sign Up')
+  end
+end
