c23271fc75d360c1e36e1242626e506ac3f2861f
Colin Hulme <17675453+ch359@users.noreply.github.com>
2019-02-02 10:04:36 +0000

    2. Feature tests for viewing messages in reverse chronological order

---
+++ spec/features/read_messages_spec.rb
@@ -0,0 +1,32 @@
+# As a maker
+# So that I can see what others are saying
+# I want to see all peeps in reverse chronological order
+
+feature 'all peeps can be seen in reverse chronological order' do
+
+  scenario 'all peeps can be seen' do
+    message = 'I am a first peep!'
+    message_2 = 'I am a second peep!'
+    visit '/'
+    fill_in 'peep', with: message
+    click_button 'Submit'
+    fill_in 'peep', with: message_2
+    click_button 'Submit'
+    expect(page).to have_content message
+    expect(page).to have_content message_2
+  end
+
+  scenario 'all peeps are in reverse chronological order' do
+    message = 'I am a first peep!'
+    message_2 = 'I am a second peep!'
+    visit '/'
+    fill_in 'peep', with: message
+    click_button 'Submit'
+    fill_in 'peep', with: message_2
+    click_button 'Submit'
+    expect(page).to have_selector("li#peeps li:nth-child(1)", text: message_2)
+    expect(page).to have_selector("li#peeps li:nth-child(2)", text: message)
+  end
+
+
+end
