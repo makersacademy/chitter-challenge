f5cd72b3df4ba0aa960d7fd64698aa013930990b
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-02 15:43:28 +0000

    Test: user can see all messages in reverse chronological order

--- spec/features/chitter_feature_spec.rb
+++ spec/features/chitter_feature_spec.rb
@@ -1,4 +1,5 @@
 require './app'
+require 'orderly'
 
 feature 'posting a message' do
   scenario 'user can post a message to chitter' do
@@ -7,6 +8,20 @@ feature 'posting a message' do
     click_button 'Submit'
     visit('/messages')
     expect(page).to have_content("I love Sinatra")
+
+  end
+end
+
+feature 'messages are arranged' do
+  scenario 'user can see all messages in reverse chronological order' do
+    visit('/')
+    fill_in 'message', with: "I love Sinatra"
+    click_button 'Submit'
+    visit('/')
+    fill_in 'message', with: "I love beer"
+    click_button 'Submit'
+    visit('/messages')
+    expect("I love beer").to appear_before("I love Sinatra")
 
   end
 end
