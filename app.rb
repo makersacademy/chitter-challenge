c672a8ad742264071f7de89747564e38a5a042d9
James <james.rudrum@hyperisland.se>
2019-02-02 16:12:22 +0000

    Updated gem file. passed unit to for all method using post class

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
+require './lib/post'
 
 class Chitter < Sinatra::Base
 
   get '/' do
     "Hello World"
+  end
+
+  get '/posts' do
+    @posts = Post.all
+
+    erb(:"posts/index")
+
   end
 
   run! if app_file == $0
---
+++ lib/post.rb
@@ -0,0 +1,9 @@
+class Post
+  def self.all
+    [
+      "Test chit 01",
+      "Test chit 02",
+      "Test chit 03"
+    ]
+  end
+end
