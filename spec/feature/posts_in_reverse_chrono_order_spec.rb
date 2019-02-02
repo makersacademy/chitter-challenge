a409206857bb0f685aadf7b7d11d65a235f31cf1
James <james.rudrum@hyperisland.se>
2019-02-02 18:46:13 +0000

    Added feature test, checks posts are displayed in revers chrono order

---
+++ spec/feature/posts_in_reverse_chrono_order_spec.rb
@@ -0,0 +1,13 @@
+feature 'Test post order' do
+  scenario 'check posts are in reverse chronological order' do
+    Post.create(chit: "Test chit 01")
+    Post.create(chit: "Test chit 02")
+    Post.create(chit: "Test chit 03")
+
+    visit ('/posts')
+
+    expect(page.find('li:nth-child(1)')).to have_content 'Test chit 03'
+    expect(page.find('li:nth-child(2)')).to have_content 'Test chit 02'
+    expect(page.find('li:nth-child(3)')).to have_content 'Test chit 01'
+  end
+end
