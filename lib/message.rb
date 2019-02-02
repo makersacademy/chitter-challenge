2c8169c751c08373914f5b06e702949bbbbbbf94
Brooke Woolley <brooke.woolley@gmail.com>
2019-02-02 13:55:34 +0000

    Connected message model with the database

--- lib/message.rb
+++ lib/message.rb
@@ -1,7 +1,20 @@
+require 'pg'
+
 class Message
 
   def self.all
-    peeps = ["Hello!", "Today is Saturday", "Goodbye!"]
+    @result = self.connection.exec("SELECT * FROM peeps;")
+    @result.map { |peep| peep['peep']}
   end
+
+  private
+
+  def self.connection
+  if ENV['ENVIRONMENT'] == 'test'
+    PG.connect(dbname: 'chitter_peeps_test')
+  else
+    PG.connect(dbname: 'chitter_peeps')
+  end
+end
 
 end
