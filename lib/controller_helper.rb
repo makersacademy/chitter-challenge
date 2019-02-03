c7ad5fbc9af55e3545c6ff80682d660bf63dde40
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-03 22:20:04 +0000

    Minor changes - rubocop report

--- lib/controller_helper.rb
+++ lib/controller_helper.rb
@@ -1,6 +1,6 @@
 # keeping controllers slim
 def condition(email, password, user)
-  redirect '/' if email == ""
-  redirect '/' if password == ""
-  redirect '/' if user == ""
+  redirect '/start' if email == ""
+  redirect '/start' if password == ""
+  redirect '/start' if user == ""
 end
