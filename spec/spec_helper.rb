8a411f69a5c5d685382e2f8f76e907bdf7a87003
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-02-03 17:43:37 +0000

    PASSED VIEWING HOMEPAGE TEST added rspec files and setup basing homepage feature test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
+require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require './app'
+
+
+Capybara.app = Chitter
+
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -9,6 +15,7 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
