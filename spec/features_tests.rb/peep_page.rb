88aa51b5e48556080b7bbabab39ae6ad6f837542
Jake Atkin <jakeatkin@jakes-air.home>
2019-02-03 19:30:45 +0000

    further testing coverage

---
+++ spec/features_tests.rb/peep_page.rb
@@ -0,0 +1,13 @@
+require_relative '/Users/jakeatkin/projects/weekend_challenges/Chitter/chitter-challenge/app.rb'
+
+
+feature 'Viewing peep page' do
+  feature 'viewing peep page' do
+    scenario 'should have id number' do
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO peep VALUES(1, 'testing')")
+      visit ('/peeps')
+      expect(page).to have_content '1'
+    end
+  end
+end
