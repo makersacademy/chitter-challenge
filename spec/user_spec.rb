7ef5bf228f70bf3cbfef26b032afce41680f33e8
Dan Groze <dangroze@gmail.com>
2019-02-03 20:31:32 +0000

    20. Added user unit test

---
+++ spec/user_spec.rb
@@ -0,0 +1,12 @@
+require 'user'
+
+describe User do
+  it 'Can create a new user' do
+    user = User.add(username: 'Username', password: 'password')
+    expect(user.username).to eq 'Username'
+  end
+
+  it 'Can see a list of users' do
+    expect { User.add(username: 'Username', password: 'password') }.to change { User.all.count }.by 1
+  end
+end
