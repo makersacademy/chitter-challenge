9743a2d91a8b90390d823f132eccbc5b54f03cb9
habakkukyong <habakkukyong@gmail.com>
2019-02-03 18:46:24 +0000

    Pass tests by passing test data to test table

--- spec/cheets_spec.rb
+++ spec/cheets_spec.rb
@@ -3,6 +3,12 @@ require 'cheet'
 describe Cheet do
   describe '#all' do
     it 'returns all cheets' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO cheets (cheet) VALUES('To get much done in little time');")
+      connection.exec("INSERT INTO cheets (cheet) VALUES('I stick to the pomodoro technique');")
+      connection.exec("INSERT INTO cheets (cheet) VALUES('Being methodical and sensible');")
+
       cheets = Cheet.all
       expect(cheets).to include("Being methodical and sensible")
       expect(cheets).to include("I stick to the pomodoro technique")
@@ -10,6 +16,10 @@ describe Cheet do
     end
 
     it 'gets the cheets from a database' do
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO cheets (cheet) VALUES('To get much done in little time');")
+      connection.exec("INSERT INTO cheets (cheet) VALUES('I stick to the pomodoro technique');")
+      connection.exec("INSERT INTO cheets (cheet) VALUES('Being methodical and sensible');")
       expect(Cheet.all).to include("Being methodical and sensible")
     end
   end
