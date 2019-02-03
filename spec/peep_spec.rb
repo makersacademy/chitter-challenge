0959b3fefb00303347382764457a0a4e5ecc79f0
Dan Groze <dangroze@gmail.com>
2019-02-03 17:56:41 +0000

    10. Added unit test for posting peep

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -2,10 +2,14 @@ require 'peep'
 require 'web_helpers'
 
 describe Peep do
-  it 'returns a list of cheets' do
+  it 'Returns a list of cheets' do
     add_test_peeps
     peeps = Peep.all
     expect(peeps.first.peep).to include "First peep"
+  end
+  it 'Can post a new peep' do
+    new_peep = Peep.post(peep: 'New peep')
+    expect(new_peep.peep).to eq 'New peep'
   end
 
 end
