9026401b89618b1299fb87f0b596d0307afd974b
Dan Groze <dangroze@gmail.com>
2019-02-03 17:58:01 +0000

    11. Added post peep functionality

--- lib/peep.rb
+++ lib/peep.rb
@@ -18,4 +18,14 @@ class Peep
     result.map { |row| Peep.new(id: row['id'], peep: row['peep']) }
   end
 
+  def self.post(peep:)
+    if ENV['ENVIRONMENT'] == 'test'
+      conn = PG.connect(dbname: 'chitter_test')
+    else
+      conn = PG.connect(dbname: 'chitter')
+    end
+    result = conn.exec( "INSERT INTO peeps(peep) VALUES('#{peep}' ) RETURNING id, peep;")
+    Peep.new(id: result[0]['id'], peep: result[0]['peep'])
+  end
+
 end
