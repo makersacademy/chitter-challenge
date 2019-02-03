fbfe8cfe7c2b6cc5659f4a396ad90d30881da98a
habakkukyong <habakkukyong@gmail.com>
2019-02-03 19:26:13 +0000

    Pass feature test for second user story

--- app.rb
+++ app.rb
@@ -11,5 +11,12 @@ class Chitter < Sinatra::Base
     erb :peeps
   end
 
+  post '/peeps' do
+    peep = params[:peep]
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (peep) VALUES('#{peep}');")
+    redirect '/peeps'
+  end
+
   run! if app_file == $0
 end
