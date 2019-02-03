763882f3c79e44c660db67736bdb0b4012486c31
Aiden Gleave <aidengleave@gmail.com>
2019-02-03 16:27:03 +0000

    Refactored peep_spec

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -2,10 +2,16 @@ require 'peep'
 
 describe '.all' do
   it 'returns a list of peeps' do
+    connection = PG.connect(dbname: 'Chitter_test')
+
+    connection.exec("INSERT INTO peeps (peep) VALUES('Test peep 1');")
+    connection.exec("INSERT INTO peeps (peep) VALUES('Test peep 2');")
+    connection.exec("INSERT INTO peeps (peep) VALUES('Test peep 3');")
+
     peeps = Peeps.all
 
-    expect(peeps).to include "Test peep 1"
-    expect(peeps).to include "Test peep 2"
-    expect(peeps).to include "Test peep 3"
+    expect(peeps[0].peep).to include "Test peep 1"
+    expect(peeps[1].peep).to include "Test peep 2"
+    expect(peeps[2].peep).to include "Test peep 3"
   end
 end
