05b84566a90a545219c7582f29b0b07cbe3d46c3
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-03 22:21:05 +0000

    Major changes to controllers to accomodate new views and handle new routes - all tests passing

--- app.rb
+++ app.rb
@@ -6,11 +6,16 @@ require './lib/controller_helper'
 class Chitter < Sinatra::Base
 
   get '/' do
+    erb(:page_one)
+  end
+
+  get '/start' do
     erb(:sign_up)
   end
 
   post '/storage_users' do
-    User.create(email: params[:your_email], password: params[:your_password], name: params[:user_name])
+    User.create(email: params[:your_email], password: params[:your_password],
+      name: params[:user_name])
     condition(params[:your_email], params[:your_password], params[:user_name])
     redirect '/create'
   end
@@ -29,6 +34,11 @@ class Chitter < Sinatra::Base
   get '/messages' do
     @tweets = Message.display
     erb(:list_of_messages)
+  end
+
+  get '/messages2' do
+    @tweets = Message.display
+    erb(:list_of_messages_two)
   end
 
 end
