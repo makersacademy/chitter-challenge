8a8a91704efb030772d8139288614d88f971ed2e
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-03 15:38:38 +0000

    Test refactoring. User needs to provide a name

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,9 +2,10 @@ require 'user'
 
 describe '.create' do
   it 'creates a new user' do
-    user = User.create(email: 'test@test.com', password: 'password')
+    user = User.create(email: 'test@test.com', password: 'password', name: 'name')
     expect(user).to be_a User
     expect(user.email).to eq('test@test.com')
     expect(user.password).to eq('password')
+    expect(user.name).to eq('name')
   end
 end
