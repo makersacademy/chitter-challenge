c42ec0d013f7f62e27f397b69e91b5651257d067
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-03 13:21:15 +0000

    add 'users' table instruction to db file

--- db/migrations/create_chitter_table.sql
+++ db/migrations/create_chitter_table.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE chitter;
 CREATE TABLE peeps(id SERIAL PRIMARY KEY, peeps VARCHAR(60));
--- ALTER TABLE peeps ADD COLUMN title VARCHAR(60);
+ALTER TABLE peeps ADD COLUMN timestamp timestamp with time zone;
+CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(140));
