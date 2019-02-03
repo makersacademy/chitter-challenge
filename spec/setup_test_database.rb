f34a8fefea5dc66cfdf31a98f296774a34bb5da2
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-03 13:20:53 +0000

    add 'users' table to spec/setup_test_database.rb file

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,12 +3,12 @@ require 'pg'
 def setup_test_database
   p "Setting up Chitter's test database..."
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec("TRUNCATE peeps RESTART IDENTITY;")
+  connection.exec("TRUNCATE peeps, users RESTART IDENTITY;")
 end
 
 def truncate_test_database
   p "Truncating Chitter's test database..."
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec("TRUNCATE peeps RESTART IDENTITY;")
+  connection.exec("TRUNCATE peeps, users RESTART IDENTITY;")
 end
 
