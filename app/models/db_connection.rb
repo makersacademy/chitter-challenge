c90ec77d7063d7914197a3bedcf5afce2b4b2d52
Simon Green <simon.green@thielsen.co.uk>
2019-02-03 10:36:19 +0000

    Refactor of db connectiomn to decouple

--- app/models/db_connection.rb
+++ app/models/db_connection.rb
@@ -1,9 +1,9 @@
 class DatabaseConnection
 
-  def self.setup
-    @environment = ENV['RAILS_ENV'] || 'development'
-    @dbconfig = YAML.load(File.read('db/config.yml'))
-    @db_connection = ActiveRecord::Base.establish_connection @dbconfig[@environment]
+  def self.setup(yaml_file = 'db/config.yml')
+    environment = ENV['RAILS_ENV'] || 'development'
+    dbconfig = YAML.load(File.read(yaml_file))
+    ActiveRecord::Base.establish_connection dbconfig[environment]
   end
 
 end
