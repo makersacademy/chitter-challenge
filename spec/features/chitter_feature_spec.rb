222b21570974516215f9ebd2842b7cbd8b6f4210
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-02-03 16:43:13 +0000

    DRY - Major feature tests refactoring

--- spec/features/chitter_feature_spec.rb
+++ spec/features/chitter_feature_spec.rb
@@ -1,43 +1,32 @@
 require './app'
 require 'orderly'
 require 'time'
+require_relative 'web_helper'
 
 feature 'posting a message' do
   scenario 'user can post a message to chitter' do
-    visit('/create')
-    fill_in 'message', with: "I love Sinatra"
-    click_button 'Submit'
-    visit('/messages')
+    messages_log
     expect(page).to have_content("I love Sinatra")
-
   end
 end
 
 feature 'messages are arranged' do
   scenario 'user can see all messages in reverse chronological order' do
     visit('/create')
-    fill_in 'message', with: "I love Sinatra"
-    click_button 'Submit'
-    visit('/create')
     fill_in 'message', with: "I love beer"
     click_button 'Submit'
-    visit('/messages')
-    expect("I love beer").to appear_before("I love Sinatra")
+    messages_log
+    expect("I love Sinatra").to appear_before("I love beer")
 
   end
 end
 
 feature 'messages get a time stamp' do
   scenario 'user can see the time at which message was posted' do
-    visit('/create')
-    fill_in 'message', with: "I love Sinatra"
-    click_button 'Submit'
-    visit('/messages')
-    expect(page).to have_content("I love Sinatra #{Time_Calculation.calculation}")
-
+    messages_log
+    expect(page).to have_content("@ #{Time_Calculation.calculation} ~ I love Sinatra")
   end
 end
-
 
 feature 'messages get a user name' do
   scenario 'user can see his name displayed by the message' do
@@ -46,11 +35,7 @@ feature 'messages get a user name' do
     fill_in 'your_email', with: 'Kasia@o2.pl'
     fill_in 'your_password', with: 'password1'
     click_button 'Sign up'
-    visit('/create')
-    fill_in 'message', with: "I love databases"
-    click_button 'Submit'
-    visit('/messages')
+    messages_log
     expect(page).to have_content("Kasia")
   end
-
 end
