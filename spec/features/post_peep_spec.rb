d38620f443fd65791cbab94956a10440d86b8c5f
Dan Groze <dangroze@gmail.com>
2019-02-03 19:34:30 +0000

    15. Feature test for seeing peep time

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -1,4 +1,4 @@
-feature 'As a user' do
+feature 'As a Maker' do
   scenario 'You can post a peep to Newsfeed' do
     visit '/peeps'
     fill_in 'peep', with: 'New peep'
@@ -6,3 +6,11 @@ feature 'As a user' do
     expect(page).to have_content 'New peep'
   end
 end
+feature 'As a Maker' do
+    scenario 'You can see the time a peep was posted' do
+      visit '/peeps'
+      fill_in 'peep', with: 'New peep'
+      click_button 'Post'
+      expect(page).to have_content "New peep posted at #{DateTime.now}"
+    end
+  end
