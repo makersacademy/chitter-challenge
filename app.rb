1e25672ceb5a1e714bd2be9a75297d99322e23df
Dan Groze <dangroze@gmail.com>
2019-02-03 17:13:06 +0000

    6. Added list all peeps form to controller

--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra/base'
+require_relative 'lib/peep'
+require_relative 'spec/web_helpers'
 
 class Chitter < Sinatra::Base
 
   get '/' do
-    erb :index
+    erb(:index)
+  end
+
+  get '/peeps' do
+    @peeps = Peep.all
+    erb(:peeps)
   end
 
   run! if app_file == $0
