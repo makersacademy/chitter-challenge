76d0940ec3bf818fb8be744df8e465ef63238e9e
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-02-03 22:30:04 +0000

    created a sign in button on the register page to return to the index

--- spec/features/guest_view_spec.rb
+++ spec/features/guest_view_spec.rb
@@ -14,3 +14,23 @@ feature 'You can create an account and go to the registration page' do
       expect(page).to have_content 'Register to create a Chitter account'
     end
 end
+
+feature 'You can click on Sign in to return to the homepage' do
+    scenario do
+    visit '/'
+    click_button 'Register'
+    click_button 'Sign in'
+    expect(page).to have_content 'Sign in to Chitter'
+  end
+end
+
+
+feature 'You can click on Sign in to return to the homepage after being in guest view' do
+    scenario do
+    visit '/'
+    click_button 'Continue as guest'
+    click_button 'Create an account'
+    click_button 'Sign in'
+    expect(page).to have_content 'Sign in to Chitter'
+  end
+end
