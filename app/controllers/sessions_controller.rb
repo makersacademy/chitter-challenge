10951379473a7bcd878c1ce1168e6ff915a197f0
William Dunk <williamdunk@gmail.com>
2019-02-03 17:11:50 +0000

    added access to session variables handle and name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
+      session[:user_name] = user.name
+      session[:user_handle] = user.username
       redirect_to root_url, notice: "Logged in!"
     else
       flash.now[:alert] = "Email or password is invalid"
@@ -14,6 +16,8 @@ class SessionsController < ApplicationController
   end
   def destroy
     session[:user_id] = nil
+    session[:user_name] = nil
+    session[:user_handle] = nil
     redirect_to root_url, notice: "Logged out!"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
