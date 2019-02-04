2c35a282f8ea8d8138cba06cba866c73b58c7a0d
Katie Cornish <katielcornish@gmail.com>
2019-02-04 10:42:18 +0000

    fixed test

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -1,6 +1,6 @@
 feature 'Index page' do
   scenario 'Index page loads correctly' do
     visit '/'
-    expect(page).to have_content 'Welcome to Chitter'
+    expect(page).to have_content 'Chitter'
   end
 end
--- spec/features/peep_feed_spec.rb
+++ spec/features/peep_feed_spec.rb
@@ -7,7 +7,7 @@ feature 'Peep feed' do
     click_button 'New Peep'
     fill_in 'new_peep', with: 'New test peep'
     click_button 'Peep'
-    expect(page).to have_content 'New test peep Test User at ' + Time.now.to_s + ' Test peep Test User at ' + Time.now.to_s
+    expect(page).to have_content 'New test peep Test User at ' + Time.now.to_s + "\nTest peep Test User at " + Time.now.to_s
   end
 
   scenario 'Each new peep has the name and the user handle of who posted it' do
