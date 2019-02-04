46c20d19b003c55ba88374c0b0f4347bacf8ad25
Erin Stevenson <estevenson_94@live.com>
2019-02-04 19:25:38 +0000

    45. Updated setup test database file

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def setup_test_database
-  connection = PG.connect(dbname: 'Chitter_Manager_Test')
+  connection = PG.connect(dbname: 'chitter_manager_test')
   connection.exec("TRUNCATE peeps, users;")
   connection.exec("INSERT INTO peeps (peep) VALUES('HELLO');")
   connection.exec("INSERT INTO peeps (peep) VALUES('This is my second peep');")
