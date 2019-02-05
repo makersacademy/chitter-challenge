750ab5b6158b523315381cc3d6800120d5717d71
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-05 00:05:54 +0000

    update create_chitter_table.sql

--- db/migrations/create_chitter_table.sql
+++ db/migrations/create_chitter_table.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE chitter;
 CREATE TABLE peeps(id SERIAL PRIMARY KEY, peeps VARCHAR(60));
 ALTER TABLE peeps ADD COLUMN timestamp timestamp with time zone;
+ALTER TABLE peeps ALTER COLUMN peep TYPE VARCHAR(140);
 CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(140));
