e3450c8e94088e466acce1e0b82bce18674fee1b
matt <mwhitaker1006@gmail.com>
2019-03-01 18:25:22 +0000

    added test to check for timestamp of each peep on homepage - timestamp already passed through so test passes

--- spec/features/add_new_peep_spec.rb
+++ spec/features/add_new_peep_spec.rb
@@ -19,6 +19,18 @@ feature 'adding new peeps' do
     p page.body.index('just writing some peeps...')
     expect('writing another peep').to appear_before('just writing some peeps...')
   end
+
+  scenario 'users can see the time at which a peep was created' do
+    clean_test_database
+    test_time = Time.local(2008, 9, 1, 10, 5, 0)
+    Timecop.freeze(test_time)
+    visit('/')
+    click_button('Add Peep')
+    fill_in 'chitter_user', with: 'Gary'
+    fill_in 'new_chitter_message', with: 'Time testing peep'
+    click_button('Create Peep')
+    expect(page).to have_content('Time testing peep Gary at 2008-09-01 10:05:00')
+  end
 end
 
 
