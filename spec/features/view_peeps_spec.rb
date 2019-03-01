0d2454d8e5dae92807104b10ccafcb4f1b5f9b4a
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-01 10:24:13 +0000

    refactor view test to use .create

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,6 +1,10 @@
 feature 'viewing peeps feed' do
   scenario 'shows a list of peeps in reverse date order' do
     
+    Peep.create(text: 'Peep #3')
+    Peep.create(text: 'Peep #2')
+    Peep.create(text: 'Peep #1')
+
     visit '/peeps'
 
     expect(page).to have_content 'Peep #3'
