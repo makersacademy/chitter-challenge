e3cd88c603b6f6baa21a8306bc0c3e6d3c969a15
matt <mwhitaker1006@gmail.com>
2019-03-01 10:54:50 +0000

    refactored tests - creating first peep in helper method

--- spec/features/add_new_peep_spec.rb
+++ spec/features/add_new_peep_spec.rb
@@ -1,11 +1,7 @@
 feature 'adding new peeps' do
   scenario 'users can add a new peep and see it on the homepage' do
     clean_test_database
-    visit('/')
-    click_button('Add Peep')
-    fill_in 'chitter_user', with: 'Mark'
-    fill_in 'new_chitter_message', with: 'just writing some peeps...'
-    click_button('Create Peep')
+    create_first_peep
     expect(page).to have_content('just writing some peeps...')
   end
 end
--- spec/helper_methods.rb
+++ spec/helper_methods.rb
@@ -2,3 +2,11 @@ def clean_test_database
   @conn = PG.connect(dbname: 'chitter_test')
   @conn.exec("TRUNCATE peeps;")
 end
+
+def create_first_peep
+  visit('/')
+  click_button('Add Peep')
+  fill_in 'chitter_user', with: 'Mark'
+  fill_in 'new_chitter_message', with: 'just writing some peeps...'
+  click_button('Create Peep')
+end
