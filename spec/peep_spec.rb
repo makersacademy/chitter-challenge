9d9fbf1be62a8b6537302cfb1933f01ebae3d801
Sarah Byrne <sarah.byrne@johnlewis.co.uk>
2019-03-01 22:00:46 +0000

    remove p's and use to b < syntax in feature test to check for reverse chronological order

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,9 +3,7 @@ require 'peep'
 describe 'Peep' do
   it 'returns all peeps in reverse chronological order' do
     peeps = Peep.all
-    p "peeps: #{peeps}"
     peep = peeps[0]
-    p "peep: #{peep}"
     expect(peep.message).to eq('The second ever peep')
   end
 end
