188e541e4b672918c3c9cad676ca5b8b7aee8636
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-01 11:22:43 +0000

    add failing test to shwo peep time

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,14 +1,14 @@
 feature 'viewing peeps feed' do
   scenario 'shows a list of peeps in reverse date order' do
     
-    Peep.create(text: 'Peep #3')
-    Peep.create(text: 'Peep #2')
-    Peep.create(text: 'Peep #1')
+    @peep3 = Peep.create(text: 'Peep #3')
+    @peep2 = Peep.create(text: 'Peep #2')
+    @peep1 = Peep.create(text: 'Peep #1')
 
     visit '/peeps'
 
-    expect(page).to have_content 'Peep #3'
-    expect(page).to have_content 'Peep #2'
-    expect(page).to have_content 'Peep #1'
+    expect(page).to have_content "Peep #3 (#{@peep3.created_at})"
+    expect(page).to have_content "Peep #2 (#{@peep2.created_at})"
+    expect(page).to have_content "Peep #1 (#{@peep1.created_at})"
   end
 end
