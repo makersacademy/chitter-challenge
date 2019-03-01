b629a8a9045fc8df5b97026693c752bf896f7fa3
matt <mwhitaker1006@gmail.com>
2019-03-01 20:16:36 +0000

    added failing feature test to check if user can sign up for chitter

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,9 @@
+feature 'user login' do
+  scenario 'user creates an account' do
+    visit('/user/signup')
+    fill_in('username', with: 'chitter_tester')
+    fill_in('password', with: 'chitter_test_password')
+    click_button('Sign up')
+    expect(page).to have_content("Welcome to Chitter, chitter_tester!")
+  end
+end
