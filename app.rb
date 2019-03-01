31a6262dafdcae074e328bc478d48327bd91aca2
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-01 10:02:14 +0000

    pass test for adding a peep

--- app.rb
+++ app.rb
@@ -10,6 +10,17 @@ class Chitter < Sinatra::Base
     @peeps = Peep.all
     erb :'peeps/index'
   end
+
+  post '/peeps' do
+    text = params['text']
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("insert into peeps (text) values ('#{text});")
+    redirect '/peeps'
+  end
+
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
   
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -2,9 +2,11 @@ feature 'adding a peep' do
   scenario 'the new peep appears on the page' do
     
     visit '/peeps'
-    click_button 'Make a peep'
+    click_button 'Make a Peep'
 
     fill_in :text, with: 'Peep dis!'
+
+    click_button 'Peep!'
 
     expect(page).to have_content 'Peep dis!'
   end
