79fdd45c688966101cbdee7103a2b9ca1dfba7b5
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 11:49:32 +0000

    add passing test for signing-in with bad creds

--- spec/features/sign_in_user_spec.rb
+++ spec/features/sign_in_user_spec.rb
@@ -18,4 +18,24 @@ feature 'signing in' do
     expect(page).to have_content 'Hello, testuser'
 
   end
+
+  scenario 'user is shown a flash error when using bad credentials' do
+
+    # sign-up first
+    visit '/accounts/new'
+    fill_in :email, with: 'user@example.com'
+    fill_in :name, with: 'Test User'
+    fill_in :username, with: 'testuser'
+    fill_in :password, with: '123456'
+    click_button 'Sign-up!'
+    
+    # log in with bad creds
+    visit '/accounts/login'
+    fill_in :email, with: 'user@example.com'
+    fill_in :password, with: '12345'
+    click_button 'Log-in!'
+
+    expect(page).to have_content 'Bad username and password. Please try again.'
+
+  end
 end
