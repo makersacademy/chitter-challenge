cb27331a8cc38dfad7eabf384085c9edfdba45bc
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 08:54:54 +0000

    update to order results by date desc

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,21 +1,22 @@
-require 'pg'
+require './connect_to_database'
 
 class Peep
-  attr_reader :text
+  attr_reader :text, :created_at
 
-  def initialize(text:)
+  def initialize(text:, created_at:)
     @text = text
+    @created_at = created_at
   end
 
   def self.all
-    result = DatabaseConnection.query('select * from peeps')
-    result.map { |peep| Peep.new(text: peep['text']) }
+    result = DatabaseConnection.query('select * from peeps order by created_at desc')
+    result.map { |peep| Peep.new(text: peep['text'], created_at: peep['created_at']) }
   end
 
   def self.create(text:)
     
     result = DatabaseConnection.query("insert into peeps (text) values ('#{text}') returning id, text, created_at;")
-    Peep.new(text: result[0]['text'])
+    Peep.new(text: result[0]['text'], created_at: DateTime.parse(result[0]['created_at']))
   end
   
 end
