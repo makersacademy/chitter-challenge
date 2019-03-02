b73e9d9d97c2684a13386fe84022b8fa26434b74
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 10:19:03 +0000

    pass test for creating a new user account

--- lib/user.rb
+++ lib/user.rb
@@ -9,6 +9,11 @@ class User
     @username = username
   end
 
+  def self.all
+    result = DatabaseConnection.query('select * from users;')
+    result.map { |user| User.new(id: user['id'], email: user['email'], name: ['name'], username: user['username']) }
+  end
+
   def self.create(email:, name:, username:, password:)
     result = DatabaseConnection.query("insert into users (email, name, username, password) values ('#{email}', '#{name}', '#{username}', '#{password}') returning id, email, username, name;")
     User.new(id: result[0]['id'], email: result[0]['email'], name: result[0]['name'], username: result[0]['username'])
