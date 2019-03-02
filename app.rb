1dae4e9f54024332cb87d7fc0171e72e9e9e01df
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-02 10:34:21 +0000

    feature test for second user story

--- app.rb
+++ app.rb
@@ -4,6 +4,10 @@ require_relative './lib/peep.rb'
 class Chitter < Sinatra::Base
   enable :sessions
 
+  get '/peeps' do
+    peeps = ["First test peep", "Second test peep", "Third test peep"]
+  end
+
   post '/peeps' do
     session[:new_peep] = params[:body]
     @new_peep = Peep.create(body: session[:new_peep])
@@ -12,7 +16,6 @@ class Chitter < Sinatra::Base
 
   get '/peeps/new' do
     erb :'peeps/create_peeps'
-
   end
 
 run! if app_file == $0
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,6 +1,8 @@
 feature 'displays peeps' do
   scenario 'user can view peeps in reverse chronological order' do
     visit('/peeps')
-    expect(page). to have_content "These are all the peeps"
+    expect(page).to have_content "First test peep"
+    expect(page).to have_content "Second test peep"
+    expect(page).to have_content "Third test peep"
   end
 end
