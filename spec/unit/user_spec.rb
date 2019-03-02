2b730827ef88b6ef0febc92a1b170d8fb1b73254
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 10:01:16 +0000

    update to use all db fields

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,9 +1,9 @@
-require '.lib/user'
+require './lib/user'
 
 describe User do
   it 'creates an account' do
     
-    User.create(email: 'user@example.com', password: '123456')
+    User.create(email: 'user@example.com', name: 'Test User', username: 'testuser', password: '123456')
 
     expect(User.all.count).to eq 1
   end
