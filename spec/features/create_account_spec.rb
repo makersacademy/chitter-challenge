35ca7007e9a329d65809be35d672f01f988ade5a
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 10:02:53 +0000

    update to fill-in all fields for db

--- spec/features/create_account_spec.rb
+++ spec/features/create_account_spec.rb
@@ -3,6 +3,8 @@ feature 'creating accounts' do
     
     visit '/accounts/new'
     fill_in :email, with: 'user@example.com'
+    fill_in :name, with: 'Test User'
+    fill_in :username, with: 'testuser'
     fill_in :password, with: '123456'
     click_button 'Sign-up!'
 
