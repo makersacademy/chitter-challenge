58da8bad475d230e0293941c031cba4afc8cb5b5
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 22:49:19 +0000

    change to use DatabaseConnection

--- spec/db_helpers.rb
+++ spec/db_helpers.rb
@@ -1,7 +1,6 @@
 def setup_test_database
-
-  connection = PG.connect(dbname: 'chitter_test')
-  # connection.query('truncate peeps, users')
+  connection = DatabaseConnection.setup('chitter_test')
+  connection.query('truncate peeps, users')
   connection.query('alter sequence users_id_seq restart with 1')
   connection.query('alter sequence peeps_id_seq restart with 1')
 end
