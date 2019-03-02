766fe308e0d6489526fa46effefb79dc3361f3de
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-02 17:41:25 +0000

    adding and viewing peeps

--- app.rb
+++ app.rb
@@ -9,17 +9,21 @@ class Chitter < Sinatra::Base
 
   get '/peeps' do
     @peeps = Peep.all
-    erb :'peeps/all_peeps' 
+    erb :'peeps/all_peeps'
   end
   post '/peeps' do
-    session[:new_peep] = params[:body]
-    @peeps = Peep.all
-    @new_peep = Peep.create(body: session[:new_peep], time: Time.now)
-    erb :'peeps/view_peeps'
+    peep = params[:body]
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps1 (url) VALUES('#{peep}');")
+    redirect'/peeps'
+    # session[:new_peep] = params[:body]
+    # @peeps = Peep.all
+    # @new_peep = Peep.create(body: session[:new_peep], time: Time.now)
+    # erb :'peeps/view_peeps'
   end
 
   get '/peeps/new' do
-    erb :'peeps/create_peeps'
+    erb :'peeps/new'
   end
 
 run! if app_file == $0
