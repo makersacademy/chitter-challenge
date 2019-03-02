b0ab0b3e9228123c88a8162ea9cf9e72ad539ee7
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 19:13:36 +0000

    change order of config section

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,13 +18,13 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
-
-  config.before(:each) do
-    setup_test_database
   end
 end
