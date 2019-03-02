2bab2f371b656395d07d9a5afad2b8733bb3ef84
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 11:22:21 +0000

    pass test for logging-in a user

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,7 @@
 require './connect_to_database'
 
 class User
+  attr_reader :email
 
   def initialize(id:, email:, name:, username:)
     @id = id
@@ -17,5 +18,10 @@ class User
   def self.create(email:, name:, username:, password:)
     result = DatabaseConnection.query("insert into users (email, name, username, password) values ('#{email}', '#{name}', '#{username}', '#{password}') returning id, email, username, name;")
     User.new(id: result[0]['id'], email: result[0]['email'], name: result[0]['name'], username: result[0]['username'])
+  end
+
+  def self.login(email:, password:)
+    result = DatabaseConnection.query("select * from users where email = '#{email}' and password = '#{password}';")
+    User.new(id: result[0]['id'], email: result[0]['email'], name: result[0]['name'], username: result[0]['username']) if result.count == 1
   end
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,10 +1,21 @@
 require './lib/user'
 
 describe User do
+  
   it 'creates an account' do
     
     User.create(email: 'user@example.com', name: 'Test User', username: 'testuser', password: '123456')
 
     expect(User.all.count).to eq 1
+  end
+
+  it 'signs a user in' do
+
+    User.create(email: 'user@example.com', name: 'Test User', username: 'testuser', password: '123456')
+
+    user = User.login(email: 'user@example.com', password: '123456')
+
+    expect(user).to be_a User
+    expect(user.email).to eq 'user@example.com'
   end
 end
