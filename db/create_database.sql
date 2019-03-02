13cc2bd083c6aa9597397f5263eaebb887ecd9f6
matt <mwhitaker1006@gmail.com>
2019-03-02 16:06:58 +0000

    travis build failed, swapped order of user and peeps tables in sql file

--- db/create_database.sql
+++ db/create_database.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE chitter;
-CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep_content VARCHAR(280), timestamp TIMESTAMP, user_id INTEGER, constraint fk_test foreign key (user_id) REFERENCES users (id));
 CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(100), password VARCHAR(200));
+CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep_content VARCHAR(280), timestamp TIMESTAMP, user_id INTEGER, constraint fk_test foreign key (user_id) REFERENCES users (id));
