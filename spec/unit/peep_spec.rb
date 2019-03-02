3adfe0cc0b2c97cba61cbcbe7eafb22aced42b8a
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 18:11:51 +0000

    add expectation to check actual entered peep

--- spec/unit/peep_spec.rb
+++ spec/unit/peep_spec.rb
@@ -1,11 +1,11 @@
 require './lib/peep'
-require './lib/user'
+# require './lib/user'
 
 describe Peep do
 
   let(:user) { { id: 1, email: 'test@example.com', username: 'testuser', name: 'Test User' } }
 
-  it 'returns all peeps, with times' do
+  it 'returns all peeps' do
     
     Peep.create(text: 'Peep #1', user_id: user[:id])
     Peep.create(text: 'Peep #2', user_id: user[:id])
@@ -14,25 +14,24 @@ describe Peep do
     peeps = Peep.all
 
     expect(peeps.count).to eq 3
+    expect(peeps.first.title).to eq 'Peep #1'
   end
 
   it 'creates a new peep' do
 
-    user = User.create(email: 'test@example.com', name: 'Test User', username: 'testuser', password: '123456')
+    Peep.create(text: 'Peep dis, yo!', user_id: user[:id])
 
-    Peep.create(text: 'Peep dis, yo!', user_id: user.id)
-
+    expect(Peep.all.first.text).to be_a Peep
     expect(Peep.all.first.text).to eq 'Peep dis, yo!'
   end
 
   it 'can get info on its poster' do
 
-    user = User.create(email: 'test@example.com', name: 'Test User', username: 'testuser', password: '123456')
-    peep = Peep.create(text: 'Peep dis, yo!', user_id: user.id)
+    peep = Peep.create(text: 'Peep dis, yo!', user_id: user[:id])
     peep_creator = peep.user
     
     expect(peep_creator).to be_a User
-    expect(peep_creator.id).to eq user.id
+    expect(peep_creator.id).to eq user[:id]
   end
 
 end
