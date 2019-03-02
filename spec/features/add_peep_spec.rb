d653062557a12b8fe76b0faada986be71d1ee486
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 12:05:21 +0000

    pass test for hiding new peep button for logged-out user

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,4 +1,13 @@
 feature 'adding a peep' do
+
+  scenario 'logged-out, the new peep button is not available' do
+
+    visit '/peeps'
+
+    expect(page).to_not have_button 'Make a Peep'
+    
+  end
+
   scenario 'the new peep appears on the page' do
     
     visit '/peeps'
