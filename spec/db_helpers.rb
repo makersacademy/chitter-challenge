54c56aecb93956624166a8ffda75d090a3b093b2
Andrew <24258342+acodeguy@users.noreply.github.com>
2019-03-02 23:21:33 +0000

    edit to truncate users table first

--- spec/db_helpers.rb
+++ spec/db_helpers.rb
@@ -1,6 +1,6 @@
 def setup_test_database
   connection = DatabaseConnection.setup('chitter_test')
-  connection.query('truncate peeps, users')
+  connection.query('truncate users, peeps')
   connection.query('alter sequence users_id_seq restart with 1')
   connection.query('alter sequence peeps_id_seq restart with 1')
 end
