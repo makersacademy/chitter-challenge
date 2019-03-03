7081abe59e274084b47c0a08add36d13d1c32f23
David Thai <davidthai@dafuloth.me>
2019-03-03 03:10:45 +0000

    Peep TDD, failing test

---
+++ spec/peep_spec.rb
@@ -0,0 +1,13 @@
+require 'peep'
+
+describe Peep do
+  describe '.all' do
+    it 'list peeps in reverse chronological order' do
+      peeps = Peep.all
+
+      expect(peeps).to include('2019-03-03; 08:52; The peep at the top of the page is the most recent')
+      expect(peeps).to include('2019-03-02; 10:45; Homepage shows peeps in reverse chronological order')
+      expect(peeps).to include('2019-03-02; 10:34; On Chitter, we post messages called "Peeps"')
+    end
+  end
+end
