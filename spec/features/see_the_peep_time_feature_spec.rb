e0c1fb5f5626eeed5d20fa456a8b3758786fbf52
James Ault <james.ault@johnlewis.co.uk>
2019-03-03 16:55:25 +0000

    Passing feature test for displaying the peep time

---
+++ spec/features/see_the_peep_time_feature_spec.rb
@@ -0,0 +1,15 @@
+feature 'See the time of the Peep' do
+
+  scenario "Visit the home page and post a peep and see it's peep time" do
+    visit '/'
+    fill_in('user_name', with: 'James Ault')
+    fill_in('handle', with: 'tso402')
+    fill_in('peep_text', with: 'I am the best')
+    @time = Time.now.strftime('%T').to_s
+    click_button 'PEEP'
+
+    expect(page).to have_content @time
+
+  end
+
+end
