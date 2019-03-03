32d2600b2ea5d559eeefc4445583daca0be64fa3
Sureka Stoeckigt <sureka.stoeckigt@gmail.com>
2019-03-03 07:28:58 +0000

    refactoring featured test

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,8 +2,10 @@ feature 'displays peeps' do
   scenario 'user can view peeps in reverse chronological order' do
     connection = PG.connect(dbname: 'chitter_test')
 
-    connection.exec("INSERT INTO peeps1 VALUES(1, 'Test peep from database');")
-    connection.exec("INSERT INTO peeps1 VALUES(2, 'Second test peep from database');")
+    Peep.create(body: 'Test peep from database')
+    Peep.create(body: 'Second test peep from database')
+    # connection.exec("INSERT INTO peeps1 VALUES(1, 'Test peep from database');")
+    # connection.exec("INSERT INTO peeps1 VALUES(2, 'Second test peep from database');")
 
     visit('/peeps')
 
