d4677a3b538202a4ee2aee00b04d0a798f9e107c
David Thai <davidthai@dafuloth.me>
2019-03-03 02:58:35 +0000

    controller–view interaction follows MVC convention: index.erb introduced

--- app.rb
+++ app.rb
@@ -2,13 +2,13 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
   get '/' do
-    peeps = [
-      '2019-03-03; 14:52; The peep at the top of the page is the most recent', 
+    @peeps = [
+      '2019-03-03; 08:52; The peep at the top of the page is the most recent', 
       '2019-03-02; 10:45; Homepage shows peeps in reverse chronological order', 
       '2019-03-02; 10:34; On Chitter, we post messages called "Peeps"'
     ]
 
-    peeps.join
+    erb :index
   end
 
   run! if app_file == $0
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,7 +2,7 @@ feature 'Viewing peeps' do
   scenario 'User sees peeps in reverse chronological order' do
     visit('/')
 
-    expect(page).to have_content '2019-03-03; 14:52; The peep at the top of the page is the most recent'
+    expect(page).to have_content '2019-03-03; 08:52; The peep at the top of the page is the most recent'
     expect(page).to have_content '2019-03-02; 10:45; Homepage shows peeps in reverse chronological order'
     expect(page).to have_content '2019-03-02; 10:34; On Chitter, we post messages called "Peeps"'
   end
