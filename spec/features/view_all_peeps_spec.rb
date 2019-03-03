5bb9f6acbcd16eb6fa75e7e28d02fce0793293f4
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-03 12:35:16 +0000

    feauture test listspeeps

--- spec/features/view_all_peeps_spec.rb
+++ spec/features/view_all_peeps_spec.rb
@@ -1,6 +1,6 @@
 require File.join(File.dirname(__FILE__), '..', 'test_database_helpers.rb')
 feature 'View all peeps' do
-  scenario 'view all peeps in reverse chronological order' do
+  scenario 'view all peeps' do
     build_database
     visit '/chitter/peep/all'
     expect(page).to have_content 'This is a peep created by the test database helper' and 'This is a second peep created by the test database helper'
