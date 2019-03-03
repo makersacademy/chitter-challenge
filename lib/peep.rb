171ee1d9bf73b5dc849eb6a32284c06c4a216308
David Thai <davidthai@dafuloth.me>
2019-03-03 03:13:13 +0000

    tests passing

---
+++ lib/peep.rb
@@ -0,0 +1,9 @@
+class Peep
+  def self.all
+    [
+      '2019-03-03; 08:52; The peep at the top of the page is the most recent',
+      '2019-03-02; 10:45; Homepage shows peeps in reverse chronological order',
+      '2019-03-02; 10:34; On Chitter, we post messages called "Peeps"'
+     ]
+  end
+end
