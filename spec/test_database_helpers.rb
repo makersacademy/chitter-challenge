5f8fcc828a80e812f3bb65eec1629da6e91e20bb
Lima Vadakara <lima.vadakara@gmail.com>
2019-03-03 10:33:42 +0000

    View all peeps

--- spec/test_database_helpers.rb
+++ spec/test_database_helpers.rb
@@ -8,4 +8,5 @@ end
 def build_database
   conn = PG.connect(dbname: 'chitter_test')
   conn.exec("INSERT INTO peep (message, posted_by) values('This is a peep created by the test database helper', 1);")
+  conn.exec("INSERT INTO peep (message, posted_by) values('This is a second peep created by the test database helper', 1);")
 end
