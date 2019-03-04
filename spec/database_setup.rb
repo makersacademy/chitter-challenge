988f6e93dc2925c0f4456ea74a2455ed759161fb
David Thai <davidthai@dafuloth.me>
2019-03-04 08:25:41 +0000

    remember to PR

--- spec/database_setup.rb
+++ spec/database_setup.rb
@@ -17,6 +17,6 @@ def build_database
   
   conn.exec("INSERT INTO peeps VALUES (DEFAULT, '2019-03-02 10:45:55', 'Homepage shows peeps in reverse chronological order')")
     
-   conn.exec("INSERT INTO peeps VALUES (DEFAULT, '2019-03-03 08:52:30', 'The peep at the top of the page is the most recent')")
+  conn.exec("INSERT INTO peeps VALUES (DEFAULT, '2019-03-03 08:52:30', 'The peep at the top of the page is the most recent')")
     
 end
