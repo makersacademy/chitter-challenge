5857fde9d0ead1cd575f9c1076cf357994fa3113
debora <debora.piu92@gmail.com>
2019-03-04 12:42:35 +0000

    travis setup database

--- db/migrations/02_create_users_table.sql
+++ db/migrations/02_create_users_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE users (username VARCHAR(20) NOT NULL CHECK(username <> ''), email VARCHAR(20) NOT NULL CHECK(email <> ''), password VARCHAR(20) NOT NULL CHECK(LENGTH(password) >4));
+CREATE TABLE users (username VARCHAR(20) NOT NULL CHECK(username <> ''), email VARCHAR(60) NOT NULL CHECK(email <> ''), password VARCHAR(20) NOT NULL CHECK(LENGTH(password) >4));
