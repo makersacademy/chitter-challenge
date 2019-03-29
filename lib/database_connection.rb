9ddc3c7a0c6686c9925897e381d98301f02a82ff
Wardah <wlt_93@hotmail.com>
2019-03-29 15:28:14 +0000

    checks if connection is persistent

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -5,4 +5,7 @@ class DatabaseConnection
     @connection = PG.connect(dbname: dbname)
   end
 
+  def self.connection
+    @connection
+  end
 end
--- spec/units/database_connection_spec.rb
+++ spec/units/database_connection_spec.rb
@@ -6,5 +6,20 @@ describe DatabaseConnection do
       expect(PG).to receive(:connect).with(dbname: 'chitter')
       DatabaseConnection.setup('chitter')
     end
+    it 'checks this connection is persistent' do
+      connection = DatabaseConnection.setup('chitter_test')
+
+      expect(DatabaseConnection.connection).to eq connection
+    end
   end
+
+  # describe '.query' do
+  #   it 'executes a query via PG' do
+  #     connection = DatabaseConnection.setup('bookmark_manager_test')
+
+  #     expect(connection).to receive(:exec).with("SELECT * FROM bookmarks;")
+
+  #     DatabaseConnection.query("SELECT * FROM bookmarks;")
+  #   end
+  # end
 end
