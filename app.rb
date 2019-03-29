df7efc1f4da38c03c59eed1b8f278a5dfe5fc388
Harry Galliano <harrygalliano97@gmail.com>
2019-03-29 12:13:54 +0000

    made and passed a second test for being able to see peeps

--- app.rb
+++ app.rb
@@ -5,5 +5,14 @@ class Chitter < Sinatra::Base
         'Chitter'
     end 
 
+    get '/peepfeed' do
+        peeps = [
+        "peep1",
+       "peep2",
+       "peep3"
+        ]
+        peeps.join(" ")
+    end
+
     run! if app_file == $0
 end
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -4,3 +4,13 @@ feature 'Viewing Peeps' do
         expect(page).to have_content "Chitter"
     end 
 end 
+
+feature 'Viewing peeps' do
+    scenario 'A user can see bookmarks' do
+      visit('/peepfeed')
+  
+      expect(page).to have_content "peep1"
+      expect(page).to have_content "peep2"
+      expect(page).to have_content "peep3"
+    end
+  end
