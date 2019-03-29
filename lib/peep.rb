d5f8b49d1a2cfd1f242092f49ed488291cf55fc7
Wardah <wlt_93@hotmail.com>
2019-03-29 16:22:02 +0000

    Added .create method into the Peep class. Added entry helper file. Passes Unit and Feature test

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,3 +1,17 @@
-class Peep
+require_relative 'database_connection'
 
+class Peep
+  attr_reader :id, :peep, :timestamp
+
+  def initialize(id: id,peep: peep, timestamp: timestamp)
+    @id = id
+    @peep = peep
+    @timestamp = timestamp
+  end
+
+  def self.create(peep:)
+    result = DatabaseConnection.query("INSERT INTO peeps (peep) VALUES('#{peep}') RETURNING id, peep, timestamp;")
+    Peep.new(id: result[0]['id'], peep: result[0]['peep'], timestamp: Time.parse(result[0]['timestamp']))
+
+  end
 end
