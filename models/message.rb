2750452eb49deb14efb1476aa0de5546c921abf7
alexanderchen6 <alexbaggio315@hotmail.com>
2019-03-30 19:45:33 +0000

    create method

--- models/message.rb
+++ models/message.rb
@@ -3,9 +3,8 @@ require 'sinatra/activerecord'
 class Message < ActiveRecord::Base
 
   def create(content:)
-    Message.new(content:content)
-
-
+    message = Message.new(content:content)
+    message.save
   end 
 
 
