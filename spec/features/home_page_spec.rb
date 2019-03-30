0432647fcb0e31d84df8fc54895bb59be6b1e50e
James Palmer <jamesdcpalmer1@gmail.com>
2019-03-30 17:34:15 +0000

    implemented styling

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,7 +5,7 @@ feature 'Viewing home page' do
   scenario 'Introductory title' do
     visit('/')
     expect(page).to have_content 'Welcome to Chitter'
-    expect(page).to have_content 'Peeps'
+    expect(page).to have_content 'Peepdeck'
   end
 end
 
