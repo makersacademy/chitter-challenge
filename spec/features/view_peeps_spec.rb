f202db70de21f9292e34c4d8c2c492518cd053aa
James Palmer <jamesdcpalmer1@gmail.com>
2019-03-31 13:47:53 +0100

    peeps display in reverse chronological order

---
+++ spec/features/view_peeps_spec.rb
@@ -0,0 +1,21 @@
+# As a maker
+# So that I can see what others are saying
+# I want to see all peeps in reverse chronological order
+
+feature 'View all peeps in reverse chronological order' do
+  scenario 'Post two peeps and last one appears first' do
+    # first peep
+    visit '/message/new'
+    fill_in('peep', with: 'My first peep!')
+    click_button('Submit')
+
+    # second peep
+    # click_button('Compose Peep')
+    visit '/message/new'
+    fill_in('peep', with: 'My second peep!')
+    click_button('Submit')
+
+    page.body.index('My first peep!').should > page.body.index('My second peep!')
+
+  end
+end
