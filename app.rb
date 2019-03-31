10e7fb175b99fd3abd005f85cb47337ffd2189f5
Lauren <laurenqurashi@me.com>
2019-03-31 18:06:49 +0100

    Added a feature that allows additions to the feed.

--- app.rb
+++ app.rb
@@ -12,6 +12,16 @@ class Chitter < Sinatra::Base
     erb :index
   end
 
+  get '/feed/create' do
+    erb :"feed/create"
+  end
+
+  post '/feed' do
+    content = params['content']
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO peeps (content) VALUES('#{content}')")
+    redirect '/feed'
+  end
 
   run! if app_file == $0
 end
