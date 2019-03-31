66729ccc0a57636178e8ca591c1c0763b99b17fb
Lauren <laurenqurashi@me.com>
2019-03-31 17:50:10 +0100

    Added a test DB

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,15 +1,24 @@
 
+require 'pg'
+
 feature 'Viewing Peeps' do
   scenario 'visiting the index page' do
     visit ('/')
     expect(page).to have_content "Chitter"
   end
 
-  scenario 'A user can see a load of Peeps' do
-    visit ('/feed')
+  scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'chitter_test')
+
+    # Add the test data
+    connection.exec("INSERT INTO peeps VALUES(1, 'Peep 1');")
+    connection.exec("INSERT INTO peeps VALUES(2, 'Peep 2');")
+    connection.exec("INSERT INTO peeps VALUES(3, 'Peep 3');")
+
+    visit('/feed')
+
     expect(page).to have_content "Peep 1"
     expect(page).to have_content "Peep 2"
     expect(page).to have_content "Peep 3"
   end
-
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,14 +1,19 @@
 
 require 'peep'
 
-describe Peep do
-  describe '.all' do
-    it 'returns all peeps' do
-      peeps = Peep.all
+describe '.all' do
+  it 'returns a list of peeps' do
+    connection = PG.connect(dbname: 'chitter_test')
 
-      expect(peeps).to include("Peep 1")
-      expect(peeps).to include("Peep 2")
-      expect(peeps).to include("Peep 3")
-    end
+    # Add the test data
+    connection.exec("INSERT INTO peeps (content) VALUES ('Peep 1');")
+    connection.exec("INSERT INTO peeps (content) VALUES ('Peep 2');")
+    connection.exec("INSERT INTO peeps (content) VALUES ('Peep 3');")
+
+    peeps = Peep.all
+
+    expect(peeps).to include('Peep 1')
+    expect(peeps).to include('Peep 2')
+    expect(peeps).to include('Peep 3')
   end
 end
