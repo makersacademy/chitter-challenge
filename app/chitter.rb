662497aa044292c4ac8087b5efef3f1608b63962
Cosmin <mirceacosmin@gmail.com>
2019-04-01 00:57:34 +0100

    #Travis test

--- app/chitter.rb
+++ app/chitter.rb
@@ -45,14 +45,14 @@ class Chitter < Sinatra::Base
       session[:user_id] = user.id
       redirect '/peeps'
     else
-      #flash[:notice] = 'Please check your email or password'
+      flash[:notice] = 'Please check your email or password'
       redirect '/sessions/new/login'
     end
   end
 
   post '/sessions/destroy' do
     session.clear
-    #flash[:notice] = "You have logged out."
+    flash[:notice] = "You have logged out."
     redirect '/peeps'
   end
 
