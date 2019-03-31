ac8899cf713789e8230e21b3b4728c5fba19e926
Paul Martin <communty.hivemind@gmail.com>
2019-03-31 21:36:11 +0100

    Fiddle with Heroku database setup

--- lib/database_connection_setup.rb
+++ lib/database_connection_setup.rb
@@ -7,13 +7,15 @@ development_db = ENV['DEVELOPMENT_DATABASE']
 test_db = ENV['TEST_DATABASE']
 host = ENV['HOST']
 
-if ENV['RACK_ENV'] == 'test'
-  DataMapper.setup(:default,
-                   "postgres://#{user}:#{password}@#{host}/#{test_db}")
-else
-  DataMapper.setup(:default,
-                   "postgres://#{user}:#{password}@#{host}/#{development_db}")
-end
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+# if ENV['RACK_ENV'] == 'test'
+#   DataMapper.setup(:default,
+#                    "postgres://#{user}:#{password}@#{host}/#{test_db}")
+# else
+#   DataMapper.setup(:default,
+#                    "postgres://#{user}:#{password}@#{host}/#{development_db}")
+# end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
