86c6fb74269a1d1f180cab30764fabd1e24a03ff
Amy Jordan <amy_jordan@live.co.uk>
2019-03-31 15:31:44 +0100

    Updated .travis.yml for CI

--- db/migrations/02_create_users_table.sql
+++ db/migrations/02_create_users_table.sql
@@ -1,2 +1,1 @@
-
-chitter_test=# CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(140), username VARCHAR(140), name VARCHAR(140));
+CREATE TABLE users (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(140), username VARCHAR(140), name VARCHAR(140));
