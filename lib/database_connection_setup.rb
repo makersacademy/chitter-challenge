1f1543a2f8acaf6cfedd6b1b1a53b77f4e065f3e
Paul Martin <communty.hivemind@gmail.com>
2019-03-31 21:44:11 +0100

    Set up test, development and production envs

--- lib/database_connection_setup.rb
+++ lib/database_connection_setup.rb
@@ -7,15 +7,15 @@ development_db = ENV['DEVELOPMENT_DATABASE']
 test_db = ENV['TEST_DATABASE']
 host = ENV['HOST']
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
-
-# if ENV['RACK_ENV'] == 'test'
-#   DataMapper.setup(:default,
-#                    "postgres://#{user}:#{password}@#{host}/#{test_db}")
-# else
-#   DataMapper.setup(:default,
-#                    "postgres://#{user}:#{password}@#{host}/#{development_db}")
-# end
+if ENV['RACK_ENV'] == 'test'
+  DataMapper.setup(:default,
+                   "postgres://#{user}:#{password}@#{host}/#{test_db}")
+elsif ENV['DEVELOPMENT'] == 'hellyes'
+  DataMapper.setup(:default,
+                   "postgres://#{user}:#{password}@#{host}/#{development_db}")
+else
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+end
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
