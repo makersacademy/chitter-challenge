c5f35a23437e9d770adce52fe5ca912e579bd65b
Amy Jordan <amy_jordan@live.co.uk>
2019-03-31 12:40:51 +0100

    Use DatabaseConnection class in Peep class

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,4 +1,4 @@
-require 'pg'
+require 'database_connection'
 
 class Peep
 
@@ -11,12 +11,7 @@ class Peep
   end
 
   def self.create(content:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    result = connection.exec("INSERT INTO peeps (peep, time) VALUES ('#{content}', '#{Time.now}') RETURNING id, peep, time;")
+    result = DatabaseConnection.query("INSERT INTO peeps (peep, time) VALUES ('#{content}', '#{Time.now}') RETURNING id, peep, time;")
     Peep.new(id: result[0]['id'], content: result[0]['peep'], time: result[0]['time'])
   end
 
