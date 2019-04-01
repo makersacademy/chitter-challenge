72f3fe0ed2bca5f504cd6a69c1cc8e934d09c275
hybridbad <hybridbad90@gmail.com>
2019-04-01 10:38:23 +0100

    fix travis ci

--- db/migrations/create_chitter.sql
+++ db/migrations/create_chitter.sql
@@ -1,2 +1,3 @@
 CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep VARCHAR(140));
+ALTER TABLE peeps ADD timestamp timestamp with time zone;
 CREATE TABLE users(id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(60));
