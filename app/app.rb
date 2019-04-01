0419927fe33bdbc3c880658069d75ab4cf269674
hybridbad <hybridbad90@gmail.com>
2019-04-01 09:05:06 +0100

    create user erb, form created

--- app/app.rb
+++ app/app.rb
@@ -1,16 +1,30 @@
 require 'sinatra/base'
 require './app/lib/peep'
+require './app/lib/user'
 require './app/lib/database_connection_setup'
 
 class Chitter < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     @peeps = Peep.all
+
     erb :index
   end
 
   post '/create-peep' do
     Peep.create(peep: params[:peep])
+
+    redirect '/'
+  end
+
+  get '/users/new' do
+    erb :'/users/new'
+  end
+
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
 
     redirect '/'
   end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,4 +4,5 @@ def setup_test_database
   connection = PG.connect(dbname: 'chitter_test')
   #Clear the table after use
   connection.exec("TRUNCATE TABLE peeps RESTART IDENTITY;")
+  connection.exec("TRUNCATE TABLE users RESTART IDENTITY;")
 end
