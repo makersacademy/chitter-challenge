0c5406d84914ffd413a3ea4eae7d755f78d3e778
Phil <phil.board90@googlemail.com>
2019-04-12 15:12:20 +0100

    reverse ordered the timeline

--- spec/features/viewing_posts_spec.rb
+++ spec/features/viewing_posts_spec.rb
@@ -8,4 +8,14 @@ feature 'Viewing posts' do
     expect(page).to have_content "this is a test"
     expect(page).to have_content "this is also a test"
   end
+
+  scenario 'The posts should be in reverse order' do
+    old_comment = Post.create(post: "this is also a test")
+    new_comment = Post.create(post: "newest")
+
+    visit('/posts')
+
+    first_child = find("ul li:first-child")
+    expect(first_child).to have_content "newest"
+  end
 end
