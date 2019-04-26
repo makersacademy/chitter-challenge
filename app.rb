95f519a7aa8a7ed0071517481702ec175d0118ad
FrankieBoye <frankie4@btinternet.com>
2019-04-26 11:01:46 +0100

    first story completed, all tests passing

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ require 'sinatra/base'
 
 class ChitterChallenge < Sinatra::Base
   get '/' do
-    'Chitter'
+    erb :index
+  end
+
+  post '/peeps' do
+    @peep = params[:peep]
+    erb :peeps
   end
 
   run! if app_file == $0
