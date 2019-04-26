873061e30cfbcb8ac6c058bd18ba590d2209994e
FrankieBoye <frankie4@btinternet.com>
2019-04-26 11:32:45 +0100

    changed code to include sessions

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require 'sinatra/base'
 
 class ChitterChallenge < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/peeps' do
-    @peep = params[:peep]
+    session[:peep] = params[:peep]
+    redirect '/peep'
+  end
+
+  get '/peep' do
+    @peep = session[:peep]
     erb :peeps
   end
 
