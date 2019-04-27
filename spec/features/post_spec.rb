e1e0e0a53b54bed7078741d6e82a2a23e1719e24
guilhe0756 <guiheurich@gmail.com>
2019-04-27 21:14:37 +0100

    User Story 1
    As a Maker
    So that I can let people know what I am doing
    I want to post a message (peep) to chitter

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,12 @@
+# As a Maker
+# So that I can let people know what I am doing
+# I want to post a message (peep) to chitter
+
+feature 'posting a message' do
+  scenario 'user posts a message on the page' do
+    visit('/')
+    fill_in 'content', with: 'Hello World'
+    click_button 'Post'
+    expect(page).to have_content 'Hello World'
+  end
+end
