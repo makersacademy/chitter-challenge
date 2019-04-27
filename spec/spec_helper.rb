244916ed9960d9752432058dad09190b9f782a59
sarpong <abasimisarpong@rocketmail.com>
2019-04-27 17:43:39 +0100

    spec helper updated with a before hook

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,10 @@ require 'simplecov-console'
 require 'rspec'
 require 'capybara/rspec'
 require_relative '../app'
+require_relative './db_test_setup'
 
 ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test_database'
 Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
@@ -20,5 +22,9 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
-  config.example_status_persistence_file_path  = './spec/failing_test.txt'
+  config.example_status_persistence_file_path = './spec/failing_test.txt'
+
+  config.before(:each) do
+    DataBaseTespSetup.truncating
+  end
 end
