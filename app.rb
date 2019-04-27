c7236f2674c68fcaa67414f24951ab750ad88fe4
Carly <carly.jenkinson65@googlemail.com>
2019-04-27 16:37:00 +0100

    One message can now appear on the message board. Updated controller to include a session for message and created created a message form in index.erb.

--- app.rb
+++ app.rb
@@ -2,10 +2,22 @@ require 'sinatra'
 
 class Chitter < Sinatra::Application
 
+  enable :sessions 
+
   get '/' do
     'Welcome to Chitter!'
+  end
+
+  post '/message' do
+  p  session[:message] = params[:message]
+    redirect '/message'
+  end
+
+  get '/message' do
+   p @message = session[:message]
     erb :index
   end
+
 
   run! if app_file == $0
 end
--- spec/features/create_chitter_home_page_spec.rb
+++ spec/features/create_chitter_home_page_spec.rb
@@ -4,3 +4,12 @@ feature 'loads chitter home page' do
     expect(page).to have_content('Welcome to Chitter!')
   end
 end 
+
+feature 'message board' do
+  scenario 'user posts a message and it is shown on the screen' do
+    visit ('/message')
+    fill_in "message", :with => 'Message has been sent'
+    click_on('Submit')
+    expect(page).to have_content('Message has been sent')
+  end
+end
