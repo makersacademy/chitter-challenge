521840d42ee30aa44c9d2bc19f08c5a60d4696a7
domw30 <domw30@gmail.com>
2019-04-27 13:36:47 +0100

    update peeps table and save step in db/migration

--- spec/features/creating_peep_spec.rb
+++ spec/features/creating_peep_spec.rb
@@ -2,7 +2,7 @@ feature 'Posting peeps' do
   scenario 'the User can add a peep which appears on the page' do
     visit('/peeps')
     click_button('Create a Peep')
-    fill_in(:content, with: 'Love to Peep!')
+    fill_in('content', with: 'Love to Peep!')
     click_button('Peep!')
 
     expect(page).to have_content 'Love to Peep!'
