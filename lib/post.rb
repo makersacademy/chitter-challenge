100770ccdba1db1bb00521e57b855f9877339a0e
Ayelisha Amoah <ayelisha.amoah@gmail.com>
2019-04-27 11:01:50 +0100

    Removed .all unit test

--- lib/post.rb
+++ lib/post.rb
@@ -1,5 +1,9 @@
+require 'pg'
+
 class Post
+
   def self.create(message)
     @post_message = message
   end
+
 end
--- spec/post_spec.rb
+++ spec/post_spec.rb
@@ -7,12 +7,4 @@ describe Post do
       expect(Post.create(message)).to eq message
     end
   end
-
-  describe '.all' do
-    it 'shows a list of all posts' do
-      message = "First peep"
-      Post.create(message)
-      expect(Post.all).to include message
-    end
-  end
 end
