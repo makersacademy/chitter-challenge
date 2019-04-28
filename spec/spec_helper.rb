aadb5be4a63b0520abc4e5f014ce75010ee95e86
yadlra <yadi.sb1@gmail.com>
2019-04-28 15:45:37 +0100

    web project set up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+ENV['RACK_ENV'] = 'test'
+require './app.rb'
+
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
