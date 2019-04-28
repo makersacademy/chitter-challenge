e2c9b38cdc747b568b4338a840bf935f245e2bce
shektor <44544977+shektor@users.noreply.github.com>
2019-04-28 20:41:06 +0100

    Remove refactors to make code more readable

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -3,10 +3,11 @@ require 'database_connection'
 describe DatabaseConnection do
   let(:database) { 'chitter_manager_test' }
   let(:sql) { "SELECT table_name FROM information_schema.tables WHERE table_schema='public'" }
-  let!(:connection) { described_class.setup(database) }
 
   describe '#setup' do
     it 'creates a PG connection object' do
+      connection = DatabaseConnection.setup(database)
+
       expect(connection).to be_a PG::Connection
       expect(connection.db).to eq database
     end
@@ -14,18 +15,23 @@ describe DatabaseConnection do
 
   describe '#connect' do
     it 'returns connection instance' do
-      expect(described_class.connect).to eq connection
+      connection = DatabaseConnection.setup(database)
+
+      expect(DatabaseConnection.connect).to eq connection
     end
   end
 
   describe '#execute' do
     it 'can execute a SQL statement' do
-      expect(described_class.connect).to receive(:exec).with(sql)
-      described_class.execute(sql)
+      DatabaseConnection.setup(database)
+
+      expect(DatabaseConnection.connect).to receive(:exec).with(sql)
+      DatabaseConnection.execute(sql)
     end
 
     it 'returns the result of a SQL statement' do
-      result = described_class.execute(sql)
+      DatabaseConnection.setup(database)
+      result = DatabaseConnection.execute(sql)
 
       expect(result).to be_a PG::Result
       expect(result.fields.first).to eq 'table_name'
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,18 +5,12 @@ describe User do
   let(:username) { 'begeldof' }
   let(:email) { 'its.me@bobgeldof.com' }
   let(:password) { 'bob123' }
-  let!(:user) {
-    described_class.create(
-      name: name,
-      username: username,
-      email: email,
-      password: password
-    )
-  }
 
   describe '#create' do
     it 'creates a user instance' do
-      expect(user).to be_a described_class
+      user = User.create(name: name, username: username, email: email, password: password)
+
+      expect(user).to be_a User
       expect(user.id.to_i).to be_a Integer
       expect(user.name).to eq name
       expect(user.username).to eq username
@@ -24,10 +18,10 @@ describe User do
     end
 
     it 'adds a new user to the database' do
+      user = User.create(name: name, username: username, email: email, password: password)
       sql = "SELECT * FROM users WHERE id = #{user.id};"
       result = DatabaseConnection.execute(sql).first
 
-      expect(user).to be_a described_class
       expect(user.id).to eq result['id']
       expect(user.name).to eq result['name']
       expect(user.username).to eq result['username']
@@ -36,18 +30,14 @@ describe User do
 
     it 'hashes the password with BCrypt' do
       expect(BCrypt::Password).to receive(:create).with(password)
-      described_class.create(
-        name: name,
-        username: username,
-        email: email,
-        password: password
-      )
+      User.create(name: name, username: username, email: email, password: password)
     end
   end
 
   describe '#find' do
     it 'returns a user based on id' do
-      result = described_class.find(user.id)
+      user = User.create(name: name, username: username, email: email, password: password)
+      result = User.find(user.id)
 
       expect(result.id).to eq user.id
       expect(result.name).to eq name
