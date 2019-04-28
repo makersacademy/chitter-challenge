a6bdc539d1357eafdeefafcd2c8883fdcc5eb9f2
sarpong <abasimisarpong@rocketmail.com>
2019-04-28 22:34:20 +0100

    Completed 4th user story

--- db/migrations/01_creating_table.sql
+++ db/migrations/01_creating_table.sql
@@ -13,3 +13,11 @@ CREATE TABLE Users (
   password VARCHAR(180) NOT NULL UNIQUE,
   signed_up_on TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+INSERT INTO Users (firstname, lastname, email, password)
+VALUES ('chris', 'brown', 'c@demo.com', 'password') 
+RETURNING 'id';
+
+INSERT INTO Users (firstname, lastname, email, password)
+VALUES ('sam', 'brown', 's@demo.com', 'passwors') 
+RETURNING 'id';
--- spec/db_test_setup.rb
+++ spec/db_test_setup.rb
@@ -5,6 +5,7 @@ class DataBaseTestSetup
     p 'Clearing test database table......'
     connection = PG.connect(dbname: 'chitter_test')
     connection.exec('TRUNCATE peep_table')
+    connection.exec('TRUNCATE users')
     p 'Done clearing test database table........'
   end
 
