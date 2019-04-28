83256eb4922757d82b4ac23a81d366b0da48661d
guilhe0756 <guiheurich@gmail.com>
2019-04-28 20:47:04 +0100

    Still trying to work with Travis

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,7 +3,7 @@ require 'pg'
 p "Setting up database..."
 
 def setup_test_database
-  connection = PG.connect(dbname: 'chitter_test')
+  connection = PG.connect(dbname: 'travis_ci_test')
   connection.exec("TRUNCATE posts;")
   connection.exec("TRUNCATE users;")
 end
