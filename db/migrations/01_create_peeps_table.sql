dd2b93a2b35827737395d0bd20f17fa532590f24
sejinkay <sejinkay@gmail.com>
2019-04-29 00:15:15 +0100

    now user can see who wrote peep and the time posted, related test all passes

--- db/migrations/01_create_peeps_table.sql
+++ db/migrations/01_create_peeps_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE peeps (id SERIAL PRIMARY KEY, message VARCHAR(280), user_id VARCHAR(60), posted_time TIMESTAMP without time zone NULL,tag VARCHAR(60));
+CREATE TABLE peeps (id SERIAL PRIMARY KEY, message VARCHAR(280), user_id VARCHAR(60), posted_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,tag VARCHAR(60));
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -5,24 +5,21 @@ describe Peep do
     it 'returns the list of peeps' do
       connection = PG.connect :dbname => 'chitter_test'
 
-      connection.exec("INSERT INTO peeps (message, user_id) VALUES ('Hello!', 'Makers');")
       connection.exec("INSERT INTO peeps (message, user_id) VALUES ('Groot?', 'Groot');")
       connection.exec("INSERT INTO peeps (message, user_id) VALUES ('Donut...', 'Homer');")
 
       peeps = Peep.all
-      expect(peeps).to include("Hello!")
-      expect(peeps).to include("Groot?")
-      expect(peeps).to include("Donut...")
+      expect(peeps.first.message).to eq("Groot?")
+      expect(peeps.first.user_id).to include("Groot")
     end
   end
 
   describe '.add_peep' do
     it 'adds a new peep' do
-      connection = PG.connect :dbname => 'chitter_test'
+      peeps = Peep.add_peep('Hello!', 'Makers')
 
-      Peep.add_peep('Hello!', 'Makers')
-      peeps = Peep.all
-      expect(peeps).to include("Makers")
+      expect(peeps.message).to eq("Hello!")
+      expect(peeps.user_id).to eq("Makers")
     end
   end
 end
