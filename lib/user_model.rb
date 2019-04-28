3f8896193c5df0f4facfe18e6f73f6b31379df84
sarpong <abasimisarpong@rocketmail.com>
2019-04-28 19:02:09 +0100

    deleted user

--- lib/user_model.rb
+++ lib/user_model.rb
@@ -1,8 +1,4 @@
 require 'pg'
 
-class User
-  def self.create(fname, lname, email, password)
-    connection = PG.connect(:dbname, 'chitter')
-    new_user = connection.exec('INSERT INTO USERS (firstname, lastname, email, password)')
-  end
+class User 
 end
--- spec/user_model_spec.rb
+++ spec/user_model_spec.rb
@@ -1,10 +1,4 @@
 require_relative '../lib/user_model'
 
 RSpec.describe User do
-  xdescribe '.create' do
-    it 'stores users details in database' do
-      described_class.create('Sarpong','Abasimi','a@demo.com','pass')
-      expect(described_class.all).('hello')
-    end
-  end
 end
