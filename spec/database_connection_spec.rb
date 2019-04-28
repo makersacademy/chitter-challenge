23ae72d0f70c580b7bd5aebcb77b3e1c86d2f244
domw30 <domw30@gmail.com>
2019-04-28 09:39:52 +0100

    add a query method to DatabaseConnection

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -11,6 +11,13 @@ describe DatabaseConnection do
       connection = DatabaseConnection.setup('chitter_manager_test')
       expect(DatabaseConnection.connection).to eq connection
     end
+  end
 
+  describe '.query' do
+    it 'executes a query via PG' do
+      connection = DatabaseConnection.setup('chitter_manager_test')
+      expect(connection).to receive(:exec).with("SELECT * FROM peeps;")
+      DatabaseConnection.query("SELECT * FROM peeps;")
+    end
   end
 end
