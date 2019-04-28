cecb7746dfc068172e0639e99456d3afdd3a6c44
Kintaro-Oe <marcusclairmont@gmail.com>
2019-04-28 16:29:38 +0100

    update travis.yml

--- db/migrations/01_create_peeps_table.sql
+++ db/migrations/01_create_peeps_table.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE chitter;
-\c chitter2;
+\c chitter;
 CREATE TABLE peeps (id serial PRIMARY KEY, message VARCHAR (140), posted_at TIMESTAMP);
 CREATE DATABASE chitter_test;
-\c chitter_test2;
+\c chitter_test;
 CREATE TABLE peeps (id serial PRIMARY KEY, message VARCHAR (140), posted_at TIMESTAMP);
