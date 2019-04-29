5177a42b1cfc55198f4dcab2c69da7e04c98af7e
FrankieBoye <frankie4@btinternet.com>
2019-04-29 09:41:47 +0100

    travis again...

--- spec/features/add_time_spec.rb
+++ spec/features/add_time_spec.rb
@@ -3,6 +3,6 @@ feature 'So that I can appreciate the context of a peep' do
     visit('/peeps/new')
     fill_in('peep', with: 'My first peep')
     click_button 'Submit'
-    expect(page).to have_content (Time.now).strftime("%H:%M:%S")
+    expect(page).to have_content Time.now.strftime("%H:%M:%S")
   end
 end
