f65b6cff5febc3feb1e3f0824e61b8b7f3aca73b
alexanderchen6 <alexbaggio315@hotmail.com>
2019-05-04 16:40:07 +0100

    .

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,16 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2_019_033_112_080_000) do
+ActiveRecord::Schema.define(version: 2019033112080000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,8 +29,8 @@ ActiveRecord::Schema.define(version: 2_019_033_112_080_000) do
     t.string "handle"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.string "password_digest"
+    t.string "username"
   end
 
 end
