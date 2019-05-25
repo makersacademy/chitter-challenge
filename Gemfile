f547e64b48d8d067e16f0b9b3ef2ce342e6577ab
Francesca Chater <francescachater@gmail.com>
2019-05-25 11:42:05 +0100

    Sets up test environment and adds domain model

--- Gemfile
+++ Gemfile
@@ -1,12 +1,16 @@
 source 'https://rubygems.org'
 
-ruby '2.5.0'
+ruby '2.6.0'
 
+gem 'pg', '~> 1.1', '>= 1.1.4'
+gem 'sinatra', '~> 2.0', '>= 2.0.5'
 gem 'rake'
 gem 'rubocop', '0.56.0'
 
 group :test do
-  gem 'rspec'
+  gem 'capybara', '~> 3.20', '>= 3.20.2'
+  gem 'rspec', '~> 3.8'
+  gem 'rubocop', '0.56.0'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'cabybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = Chitter 
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
