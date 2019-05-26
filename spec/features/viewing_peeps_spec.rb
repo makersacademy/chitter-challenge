3d155b93c1f46206a3d6a0476e179cba947ac948
Edina Bolkeny <bolkeny.edina@gmail.com>
2019-05-26 14:05:57 +0100

    Add peep time to view

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -5,15 +5,18 @@ feature 'Viewing peeps' do
     expect(page).to have_content("CHITTER")
   end
 
-  scenario 'user can view peeps' do    
-    Peep.send(message: "First peep")
-    Peep.send(message: "Second peep")
-    Peep.send(message: "Third peep")
+  scenario 'user can view peeps' do
+    first_peep = Peep.send(message: "First peep")
+    second_peep = Peep.send(message: "Second peep")
+    third_peep = Peep.send(message: "Third peep")
 
     visit '/peeps'
 
     expect(page).to have_content("Third peep")
+    expect(page).to have_content("#{third_peep.sent_time}")
     expect(page).to have_content("Second peep")
+    expect(page).to have_content("#{second_peep.sent_time}")
     expect(page).to have_content("First peep")
+    expect(page).to have_content("#{first_peep.sent_time}")
   end
 end
