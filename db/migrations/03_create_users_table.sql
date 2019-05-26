a241ece4271439b93288650c3b6a84037b0af658
Francesca Chater <francescachater@gmail.com>
2019-05-26 17:37:17 +0100

    Create user table sql command

---
+++ db/migrations/03_create_users_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE peeps(id SERIAL PRIMARY KEY,
+                   email VARCHAR(60),
+                   password VARCHAR(60),
+                   name VARCHAR(60),
+                   username VARCHAR(30)
+                 );
