1287eb7d6acac4a88481b72a69ddbeda2697d622
VladC <vlad.chiriac24@gmail.com>
2019-05-27 14:50:50 +0100

    Add first feature test and erb file

--- spec/features/profile_spec.rb
+++ spec/features/profile_spec.rb
@@ -1,9 +1,17 @@
 feature 'viewing and posting peeps' do
 
-  scenario 'it has a peep list and fields to post' do
+  scenario 'it shows page contents' do
     visit('/')
     expect(page.status_code).to eq 200
-    expect(page).to have_content('Hello, Chitter')
+    expect(page).to have_content('Chitter')
+    expect(page).to have_field('content')
+    expect(page).to have_button('Peep')
   end
   
+  scenario 'it posts a peep' do
+  	visit('/')
+  	fill_in :content, with: 'Hello, this is my first peep on Chitter!'
+  	click_button 'Peep'
+  end
+
 end
