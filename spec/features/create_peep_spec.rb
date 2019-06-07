c0e5102d102752c4d8096a53b6d8da4bff74abb9
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-07 13:59:33 +0100

    Refactors to View Peeps test

---
+++ spec/features/create_peep_spec.rb
@@ -0,0 +1,7 @@
+# feature 'Creating new peep' do
+
+#   scenario 'user can add a new peep' do
+#     visit("/")
+#     visit('/peeps')
+#     click_link('Peeps')
+#   end
--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -2,8 +2,9 @@ feature 'On Peeps page' do
   # background { truncates }
 
   scenario 'user can navigate to Peeps page and see all' do
-    visit('/peeps')
-    click_link('Peeps')
+    visit('/')
+    click_link('All Peeps')
     expect(page).to have_content "Hey, this is my first test peep!"
   end
+  
 end
