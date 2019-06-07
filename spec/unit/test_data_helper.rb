b876df75aa2917b6187bf62f0cf1e45e39de9883
Anna <annas.wieclaw@gmail.com>
2019-06-07 13:24:32 +0100

    Now the tests are using the db connection class

--- spec/unit/test_data_helper.rb
+++ spec/unit/test_data_helper.rb
@@ -1,6 +1,7 @@
+require "db_connection"
+
 def connect_delete_and_create_data
-  connection = PG.connect(dbname: 'chitter_chatter_test')
-  connection.exec('TRUNCATE peeps, users RESTART IDENTITY CASCADE;')
+  DbConnection.query('TRUNCATE peeps, users RESTART IDENTITY CASCADE;')
   Peep.create("My first peep")
   Peep.create("I have just added this")
 end
