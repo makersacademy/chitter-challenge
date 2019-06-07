3a2c29ceaede8dc1e3440a21b3f871689dce58cd
Lauren O'Mara <lauren.omara22@yahoo.co.uk>
2019-06-07 11:24:30 +0100

    added login and signup buttons

--- spec/features/app_spec.rb
+++ spec/features/app_spec.rb
@@ -1,5 +1,4 @@
 feature Chitter do 
-
   scenario 'it displays the peeps' do 
     visit '/peeps'
     expect(page).to have_content('Today is a great day! Ask me why...')
@@ -15,10 +14,16 @@ feature Chitter do
     expect(page).to have_field('text')
   end 
 
-  scenario 'it displays new peep back' do
+  scenario 'it displays new peep back as the first peep' do
     visit '/peeps'
     fill_in 'text', with: "cant wait to watch love island tonight"
     click_button 'Post Peep'
     expect(page).to have_content("cant wait to watch love island tonight")
   end
+
+  scenario 'it displays buttons to login or sign up' do 
+    visit '/peeps' 
+    expect(page).to have_button('Login')
+    expect(page).to have_button('Sign up')
+  end 
 end
