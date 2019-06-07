dba1a9bc440fce2a736df3f378fcefec4f31002e
JAYDA KADIR <jydkdr@gmail.com>
2019-06-07 11:16:47 +0100

    Setup databases

---
+++ app.rb
@@ -0,0 +1,5 @@
+require 'sinatra/base'
+require './lib/chitter'
+
+class ChitterManager < Sinatra::Base
+end
---
+++ db/migrations/02_create_peeps_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE peeps(id SERIAL PRIMARY KEY, content VARCHAR(140), peep_id INTEGER REFERENCES users (id));
