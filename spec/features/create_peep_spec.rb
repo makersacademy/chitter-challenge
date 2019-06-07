c664399841f97bdf7afb4cb573728728081d18ca
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-07 15:43:04 +0100

    Formats to HTML for improved css

--- spec/features/create_peep_spec.rb
+++ spec/features/create_peep_spec.rb
@@ -3,6 +3,7 @@ feature 'Creating new peep' do
   scenario 'user can add a new peep' do
     visit("/")
     click_link('All Peeps')
+    click_link('Add a new Peep')
     fill_in('author', with: 'Lin')
     fill_in('handle', with: '@LinnyCoder')
     fill_in('content', with: 'Oh cool! I can add a new peep too!')
