2efee6f29a8f8ed38675aced3297645ea4ae0e40
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-07 15:51:17 +0100

    Corrected RSPec test to Create new Peep

--- spec/features/create_peep_spec.rb
+++ spec/features/create_peep_spec.rb
@@ -4,10 +4,10 @@ feature 'Creating new peep' do
     visit("/")
     click_link('All Peeps')
     click_link('Add a new Peep')
-    fill_in('author', with: 'Lin')
-    fill_in('handle', with: '@LinnyCoder')
-    fill_in('content', with: 'Oh cool! I can add a new peep too!')
-    click_link('Submit a new peep')
+    fill_in('Author1', with: 'Lin')
+    fill_in('Handle1', with: '@LinnyCoder')
+    fill_in('Content1', with: 'Oh cool! I can add a new peep too!')
+    click_button('Submit a new peep')
     expect(page).to have_content('Oh cool! I can add a new peep too!')
   end
 end
