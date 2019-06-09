7f608f573415796d95e554be96c41dd3a6459474
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-09 23:46:25 +0100

    Improvements to HTML view files

--- app.rb
+++ app.rb
@@ -43,11 +43,11 @@ class Chitter < Sinatra::Base
     erb :'sessions/new'
   end
 
-  post 'sessions' do
-    user = User.authenticate(params[:handle], params[:password])
-    session[:user_id] = user.id
-    redirect '/peeps'
-  end
+  # post 'sessions' do
+    # user = User.authenticate(params[:handle], params[:password])
+    # session[:user_id] = user.id
+  #   redirect '/peeps'
+  # end
 
   run! if app_file == $0
 end
