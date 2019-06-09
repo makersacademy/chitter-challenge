9a79f301e163fdd47baa23161769afb0e3cae147
Egle Bernotaite <ebernotaite94@gmail.com>
2019-06-09 21:43:58 +0100

    Some cleanup

--- spec/features/peep_feature_spec.rb
+++ spec/features/peep_feature_spec.rb
@@ -2,7 +2,6 @@ feature 'peep' do
   scenario 'user can submit a peep' do
     visit '/'
     click_button 'View feed'
-    save_and_open_page
     click_on 'Add peep'
     fill_in(:peep, with: 'It is friyay')
     click_button 'Submit'
--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -36,19 +36,16 @@ describe User do
     it 'returns a user given one exists with a correct username and password' do 
       user = User.create(name: 'Test', username: 'test', email: 'test@example.com', password: 'password123')
       authenticated_user = User.authenticate(email: 'test@example.com', password:'password123')
-      p "***************#{authenticated_user.id}"
       expect(authenticated_user.id).to eq user.id
     end
 
     it 'returns nil given an incorrect email address' do
       user = User.create(name: 'Test', username: 'test', email: 'test@example.com', password: 'password123')
-  
       expect(User.authenticate(email: 'nottherightemail@me.com', password: 'password123')).to be_nil
     end
 
     it 'returns nil given an incorrect password' do
       user = User.create(name: 'Test', username: 'test', email: 'test@example.com', password: 'password123')
-  
       expect(User.authenticate(email: 'test@example.com', password: 'wrongpassword')).to be_nil
     end
   end
