7baedf1f66a1fd404718d2a473231964bdeb4cb2
Student <magnusarbuthnott@gmail.com>
2019-06-09 10:24:01 +0100

    Test GREEN. Feature test for log out updated.

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -3,29 +3,28 @@ feature "Log In" do
     User.create(name: 'John Smith', username: 'jsmith', email: 'john@smith.com', password: 'password')
     visit '/'
     click_button 'Log In'
-    expect(page).to have_content "Log In"
   end
 
   scenario 'a user can login with correct details' do
-    fill_in "user",     with: "jsmith"
+    fill_in "username",     with: "jsmith"
     fill_in "password", with: "password"
     click_button "Log In"
     expect(page).to have_content "Welcome jsmith"
   end
 
   scenario 'a user sees an error message if incorrect username entered' do
-    fill_in "user",     with: "wrong username"
+    fill_in "username",     with: "wrong username"
     fill_in "password", with: "password"
     click_button "Log In"
     expect(page).not_to have_content "Welcome jsmith"
-    expect(page).to have_content 'Please check your email or password'
+    expect(page).to have_content 'The username and password that you entered did not match our records. Please double-check and try again.'
   end
 
   scenario 'a user sees an error message if incorrect password entered' do
-    fill_in "user",     with: "jsmith"
+    fill_in "username",     with: "jsmith"
     fill_in "password", with: "wrong password"
     click_button "Log In"
     expect(page).not_to have_content "Welcome jsmith"
-    expect(page).to have_content 'Please check your email or password'
+    expect(page).to have_content 'The username and password that you entered did not match our records. Please double-check and try again.'
   end
 end
--- spec/features/logout_spec.rb
+++ spec/features/logout_spec.rb
@@ -1,19 +1,16 @@
 feature 'Log Out' do
   scenario 'User con log out' do
     User.create(name: 'Bill Smith', username: 'bsmith', email: 'bill@smith.com', password: 'password')
-
     visit '/'
+    expect(page).not_to have_content "Logout"
+
     click_button 'Log In'
-    expect(page).to have_content "Log in section"
-
-    fill_in "user",     with: "bsmith"
+    fill_in "username",     with: "bsmith"
     fill_in "password", with: "password"
-
     click_button "Log In"
 
     expect(page).to have_content "Welcome bsmith"
-
-    expect(page).not_to have_content "Bill Smith"
-    expect(page).to have_content "You have logged out"
+    click_button "Log Out"
+    expect(page).not_to have_content "Welcome bsmith"
   end
 end
