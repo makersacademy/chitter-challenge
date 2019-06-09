c2b42aa542c4b320fc76b6c5b67c24a7a3f7fb60
LinTrieu <36490540+LinTrieu@users.noreply.github.com>
2019-06-09 16:13:57 +0100

    Refactors to Signup feature

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -5,11 +5,11 @@ feature 'Registration' do
   scenario 'a user can sign up to Chitter' do
     visit("/signup")
     fill_in('author', with: 'David')
-    fill_in('handle', with: '@DaveDev')
+    fill_in('handle', with: '@DavetheDev')
     fill_in('email', with: 'dave@gmail.com')
     fill_in('password', with: 'Dave1234')
-    click_button('Sign up to Chitter')
-    expect(page).to have_content('Welcome to Chitter, ...')
+    click_button('Register')
+    expect(page).to have_content('Welcome to Chitter, @DavetheDev')
   end
 
 end
