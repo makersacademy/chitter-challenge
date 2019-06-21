32a04cc5b6cfa75e6b88de01efadd029692b3b9e
Celia <celiafrom12@gmail.com>
2019-06-21 17:34:52 +0100

    connecting to PostgreSQL, retrive peeps from database

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,9 +1,17 @@
+require 'pg'
+
 class Chitter
 
   attr_reader :peeps
 
   def initialize
     @peeps = []
+  end
+
+  def self.all
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec('SELECT * FROM chitters')
+    result.map { |chitter| chitter['peep'] }
   end
 
   def add_message(message)
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -5,4 +5,12 @@ describe Chitter do
   it 'receives a message' do
     expect(subject.add_message(message)).to include(message)
   end
+
+  it 'returns a list of bookmarks' do
+   chitter = Chitter.all
+
+   expect(chitter).to include "First peep"
+   expect(chitter).to include "Second peep"
+   expect(chitter).to include "Third peep"
+ end
 end
