98c72fa88a44af25130d5eec31d4196a4e43738a
Alexis Josephides <ajosephides@gmail.com>
2019-06-22 09:02:04 +0100

    comment out database cleaner until tables are present. added first feature test to add user

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,19 +25,19 @@ Capybara.app = Chitter
 
 RSpec.configure do |config|
 
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
+  # config.before(:each) do
+  #   DatabaseCleaner.start
+  # end
 
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-  
+  # config.after(:each) do
+  #   DatabaseCleaner.clean
+  # end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
