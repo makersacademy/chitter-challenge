1780c9ca3cba0a1831a8229b8940715aa9374463
Alexis Josephides <ajosephides@gmail.com>
2019-06-22 13:14:54 +0100

    added button on homepage to navigate to all peeps

--- spec/features/view_chronological_spec.rb
+++ spec/features/view_chronological_spec.rb
@@ -1,3 +1,4 @@
+require_relative '../helpers/add_users_peeps'
 # As a maker
 # So that I can see what others are saying  
 # I want to see all peeps in reverse chronological order
@@ -5,31 +6,9 @@
 feature 'peeps listed in reverse order (newest first' do
   scenario 'second peep appears before first' do
 
-    visit('/')
-    click_button('New User')
-    fill_in('name', with: 'Jack')
-    fill_in('username', with: 'JD')
-    fill_in('email', with: 'jd@peep.com')
-    fill_in('password', with: 'sshhhhh')
-    click_button('Add Me')    
-    click_button('Post Peep')
-    fill_in('message', with: 'This is my first peep')
-    click_button('Peep')
+    create_users_peeps
 
-    visit('/')
-    click_button('New User')
-    fill_in('name', with: 'Mark')
-    fill_in('username', with: 'MZ')
-    fill_in('email', with: 'mz@smugbook.com')
-    fill_in('password', with: 'secrets')
-    click_button('Add Me')    
-    click_button('Post Peep')
-    fill_in('message', with: 'This is a smug peep')
-    
-    # a potential code smell
-    sleep(1)
-
-    click_button('Peep')
+    visit('/peeps')
 
     peep1 = 'This is my first peep'
     peep2 = 'This is a smug peep'
--- spec/features/view_peep_author_spec.rb
+++ spec/features/view_peep_author_spec.rb
@@ -2,7 +2,7 @@ require_relative '../helpers/add_users_peeps'
 
 # As a Maker
 # So that I can better appreciate the context of a peep
-# I want to see the time at which it was made
+# I want to see the user who posted the peep at which it was made
 
 feature 'created time visible on a peep' do
   scenario 'each peep has a created time ' do
@@ -11,9 +11,8 @@ feature 'created time visible on a peep' do
 
     visit('/peeps')
 
-    expect(page).to have_content(Time.new.year)
-    expect(page).to have_content(Time.new.hour)
+    expect(page).to have_content('Author: JD')
+    expect(page).to have_content('Author: MZ')
 
-  
   end
 end
