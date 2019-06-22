cb28f3e9846e6abb814fd22167c5c0912f90ae9d
Alexis Josephides <ajosephides@gmail.com>
2019-06-22 11:23:56 +0100

    added feature test for presenting time

---
+++ spec/features/view_peep_created_spec.rb
@@ -0,0 +1,34 @@
+# As a Maker
+# So that I can better appreciate the context of a peep
+# I want to see the time at which it was made
+
+feature 'created time visible on a peep' do
+  scenario 'each peep has a created time ' do
+    visit('/')
+    click_button('New User')
+    fill_in('name', with: 'Jack')
+    fill_in('username', with: 'JD')
+    fill_in('email', with: 'jd@peep.com')
+    fill_in('password', with: 'sshhhhh')
+    click_button('Add Me')    
+    click_button('Post Peep')
+    fill_in('message', with: 'This is my first peep')
+    click_button('Peep')
+
+    visit('/')
+    click_button('New User')
+    fill_in('name', with: 'Mark')
+    fill_in('username', with: 'MZ')
+    fill_in('email', with: 'mz@smugbook.com')
+    fill_in('password', with: 'secrets')
+    click_button('Add Me')    
+    click_button('Post Peep')
+    fill_in('message', with: 'This is a smug peep')
+    click_button('Peep')
+
+    expect(page).to have_content(Date.today.year)
+    expect(page).to have_content(Date.today.hour)
+
+  
+  end
+end
