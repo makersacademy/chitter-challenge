40af7ce9ecc0b272cc0c10e170fc223a472513af
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-06-22 15:59:26 +0100

    Unit Test Added to Test Reverse Chronological Order of Peeps

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -11,6 +11,18 @@ describe Peep do
 
       expect(peeps).to include("Hi Chitter!")
     end
+
+    it 'Lists the Peeps in reverse chronological order' do
+      connection = PG.connect(dbname: "Chitter_Test")
+
+      connection.exec("INSERT INTO peep (peep) VALUES('Hi Chitter!');")
+      connection.exec("INSERT INTO peep (peep) VALUES('I love Chitter');")
+
+      peeps = connection.exec("SELECT * FROM peep ORDER BY timestamp DESC;")
+      timestamps = peeps.map { |peep| peep['timestamp'] }
+
+      expect(timestamps[0] > timestamps[1]).to be true
+    end
   end
 
   describe '.create' do
