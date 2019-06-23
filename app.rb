956f2a1a9ba9b7a05e1b0a46eb108d939678610b
Student <student@MAKERSs-MacBook-Pro-6.local>
2019-06-23 19:45:13 +0100

    Signup form added with css + database set up

--- app.rb
+++ app.rb
@@ -1,10 +1,24 @@
 require 'sinatra/base'
 
 class ChitterApp < Sinatra::Base
-
+  enable :sessions
   get '/' do
     erb :homepage
   end
 
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    session[:current_user] = User.create(
+      name: params[:name],
+      username: params[:username],
+      password: params[:password],
+      email: params[:email]
+    )
+    redirect '/'
+  end
+  
   run! if app_file == $0
 end
