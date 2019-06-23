49f047b474fddd7c3fd031c45412e5864845aa4c
Monika Kaczanowska <46656126+monikakaczan@users.noreply.github.com>
2019-06-23 19:05:52 +0100

    Delete otherusers.rb

--- lib/otherusers.rb
+++
@@ -1,9 +0,0 @@
-require 'pg'
-
-class OtherUsers
-
-  def self.all
-    connect_to_database= PG.connect(dbname: "fakepeeps")
-    pull_out_data=connect_to_database.exec("SELECT (text, username) FROM fp;")
-  end
-end
