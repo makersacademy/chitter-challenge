548c669ded6aafbd36ca96d792606d7897cdc620
Arthur Carl Forest-Jones <carlfj@live.co.uk>
2019-06-24 08:08:21 +0100

    Attempt at adding date

--- lib/peep.rb
+++ lib/peep.rb
@@ -10,9 +10,13 @@ class Peep
      connection = PG.connect(dbname: 'Chitter')
     end
      result = connection.exec("SELECT * FROM peeps;")
-     result.map do
+     peep = result.map do
      |peep_entry| peep_entry['peep']
      end
+     # date = result.map do
+     #   |peep_entry| peep_entry['created_at']
+     # end
+     return peep
 
   end
 
