e92e905346f32446407d2f35f0ed0e3c794359d4
Kaja <catcalledkaja@gmail.com>
2019-07-16 11:51:17 +0100

    adding database and connection to Peep class

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Memoization 
+
+
 Chitter Challenge
 =================
 
--- lib/peep.rb
+++ lib/peep.rb
@@ -1,8 +1,18 @@
+require 'pg'
 
 class Peep
 
+  def self.connection
+    @connection ||= PG.connect(dbname: 'chitter')
+
+  # ||= what id does is: first our variable is nill so it is false,
+  # hence our code is looking on the right side. Now if we are going to call
+  # connection anywhere it will only open one connection istead of multiple
+  # and use that one connetion.
+
   def self.message
-    "Your New Peep is now life!"
+    result = connection.exec("SELECT * FROM messages;")
+    result.map{ |message| message['url'] }
   end
 
 end
