5a68fa811875220c9d68818441d4da70eb11ea01
iAquino47 <ianaquino47@gmail.com>
2019-07-22 10:48:33 +0100

    travis file edited

--- db/datamapper_setup.rb
+++ db/datamapper_setup.rb
@@ -3,8 +3,6 @@
 require_relative '../lib/user'
 require_relative '../lib/peep'
 
-# DataMapper.setup(:default, 'postgres:///chitter_development')
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
 DataMapper.finalize
-
 DataMapper.auto_upgrade!
