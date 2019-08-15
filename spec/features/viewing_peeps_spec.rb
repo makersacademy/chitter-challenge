5e6cdaf4b7cbc9e302fae1f94e1916fd39ca0533
Jodi <jodiallysonfoster@gmail.com>
2019-08-15 22:27:25 +0100

    User can see the time each peep was posted

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,5 +1,6 @@
 require 'pg'
 require 'timecop'
+require 'time'
 
 feature 'viewing peeps' do
   scenario 'a user can see peeps on the homepage' do
@@ -26,16 +27,23 @@ feature 'viewing peeps' do
     expect(all('li')[0].text).to have_content "This is the first peep"
   end
 
-  # scenario 'a user can see the time each peep was posted' do
-  # before do
-  #   Timecop.freeze(Time.now)
-  # end
+  feature 'a user can see the time each peep was posted' do
+  before do
+    Timecop.freeze(Time.now)
+  end
   # after do
   #   Timecop.return
   # end
-  #
-  #
-  # end
 
+  scenario 'a user can see the time each peep was posted' do
 
+  connection = PG.connect(dbname: 'peeps_test')
+
+  Peep.create(content: "This is the first peep" )
+
+  visit('/')
+  expect(all('li')[0].text).to have_content Time.now.strftime("%H:%M")
+
+  end
+end
 end
