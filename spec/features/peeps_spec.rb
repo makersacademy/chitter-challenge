6af7c5c2b083fa37db82e9028f76ce6285d741e6
dtrts <djt210@gmail.com>
2019-08-18 20:32:13 +0100

    RED test for seeing a reply to a tweet

--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -1,5 +1,6 @@
 feature 'Peeeeeps' do
 
+
   scenario 'without being logged in i can see peeps' do
     visit('/chitter')
     expect(find(:xpath, '//div[@class="peep"][1]//div[@class="content"]')).to have_content('haha, does that look funny to you?')
@@ -32,6 +33,14 @@ feature 'Peeeeeps' do
     expect(find(:xpath, '//div[@class="peep"][2]//div[@class="username"][1]')).to have_content('AzureDiamond')
     expect(find(:xpath, '//div[@class="peep"][2]//div[@class="display-name"][1]')).to have_content('AD')
 
+  end
+
+  scenario 'after logging in i can reply to a peep' do
+    user_sign_in
+    first('.reply-button').click
+    fill_in("Peep content:", with: 'this is a reply')
+    click_button('Post Peep!')
+    expect(page).to have_content('this is a reply')
   end
 
 end
