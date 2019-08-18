f4b9c7fc160baa9366c4258ffaf117610b2d6bb4
dtrts <djt210@gmail.com>
2019-08-18 10:25:09 +0100

    RED Tests for user log in and signup

---
+++ spec/db_helper.rb
@@ -0,0 +1,7 @@
+def user_sign_in
+  visit('/chitter')
+  click_button("Login")
+  fill_in("Username:",with: "admin")
+  fill_in("Password:",with: "admin")
+  click_button("Login")
+end
--- spec/features/user_accounts_spec.rb
+++ spec/features/user_accounts_spec.rb
@@ -1,13 +1,25 @@
 
 feature 'user accounts' do
 
-  it 'shows a list of users' do
-    visit('/users')
-    expect(page).to have_content('Hello there, Sally')
+  scenario 'user can register a new account' do
+    visit('/chitter')
+    expect(page).to have_xpath('//div[@id="welcome-message"]')
+    expect(page).to have_xpath('//button[@name="user-registration"]')
+    click_button("Sign Up")
+    fill_in("Username:",with: 'simym')
+    fill_in("Display Name:",with: 'ContraP')
+    fill_in("Password:",with: 'password!')
+    click_button("Register")
+    expect(page).to have_content("Hello there, ContraP")
   end
 
-  it 'has user records' do
-    users = User.all
-    p users[0]
+  scenario 'user can login to an account' do
+    visit('/chitter')
+    click_button("Login")
+    fill_in("Username:",with: "admin")
+    fill_in("Password:",with: "admin")
+    click_button("Login")
+    expect(page).to have_content("Hello there, admin")
   end
+
 end
