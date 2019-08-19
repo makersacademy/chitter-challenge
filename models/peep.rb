888cd09948ee720d532aab221d039034a59155a3
dtrts <djt210@gmail.com>
2019-08-19 10:03:50 +0100

    Travis green tick attempt

--- models/peep.rb
+++ models/peep.rb
@@ -14,7 +14,7 @@ class Peep < ActiveRecord::Base
       ref_user = User.where('lower(username) = ?', match[1].downcase).first
       if ref_user
         if PeepUserTag.where(user_id: ref_user.id, peep_id: id).empty?
-          PeepUserTag.new(user_id: ref_user.id, peep_id: id)
+          PeepUserTag.new(user_id: ref_user.id, peep_id: id).save!
         end
         new_html_content = new_html_content.gsub(/(&)(#{match[1]})/, "<a href='/users/profile/#{ref_user.id}'><b>\\1</b>\\2</a>")
       end
