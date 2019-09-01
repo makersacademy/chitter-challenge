0429b76b89fe29cab9198b4a85fd47893ebe9200
tuyetngo1 <tuyetngo136@gmail.com>
2019-09-01 23:22:03 +0100

    sign up successful

--- app.rb
+++ app.rb
@@ -1,7 +1,10 @@
 require 'sinatra/base'
 require './lib/peeps'
 require './lib/timestamp'
+require './lib/users'
+
 class ChitterApp < Sinatra::Base
+  enable :sessions
   get '/chitter' do
     @peeps = Peep.all
     erb :messages
@@ -9,11 +12,38 @@ class ChitterApp < Sinatra::Base
 
   post '/chitter' do
     @time = Inputtime.timestamp
-    @peep = Peep.create(peeps: params['message'],timestamp: @time) 
+    @peep = Peep.create(peeps: params['message'],timestamp: @time)
 
     # connection = PG.connect(dbname: 'chitter_test')
     # connection.exec("INSERT INTO peeps (peeps) VALUES('#{peep}')")
     redirect '/chitter'
   end
+
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    @name = params['name']
+    @email = params['email']
+    @password = params['password']
+    if Users.exist(email:@email).nil?
+    @user = Users.create(name:@name,email:@email,password:@password)
+    redirect '/welcome'
+    else
+    @error = true
+    # puts "Email has already been taken"
+    end
+    erb :signup
+  end
+
+  get '/signin' do
+    erb :signin
+  end
+
+  get '/welcome' do
+    erb :welcome
+  end
+
   run! if app_file == $0
 end
