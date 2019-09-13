230b14e1b1d9eeb44975a41f7294a24c4eb718ff
Dan Gyi <daniel.gyi@gmail.com>
2019-09-13 11:40:12 +0100

    add login form for index, first test passing

--- app.rb
+++ app.rb
@@ -6,12 +6,22 @@ class ChitterApp < Sinatra::Base
   enable :sessions
   register Sinatra::Flash
 
+  get '/' do
+    erb(:index)
+  end
 
-
-
-
-
+  post '/login' do
+    redirect('/peeps')
+  end
   
+  get '/peeps' do
+    erb(:peeps)
+  end
+
+
+
+
+
   run! if app_file == $0
 
 end
--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -1,9 +1,9 @@
 feature 'Log in' do
   scenario 'test login works, redirects to /peeps' do
     visit '/'
-    fill_in 'username', with: "Test987"
-    fill_in 'password', with: "password987"
+    fill_in 'Username', with: "Test987"
+    fill_in 'Password', with: "password987"
     click_button "Log In"
-    expect(current_path).to eq "/peeps"
+    expect(page).to have_content "Chitter - Get your Daily Peeps"
   end
 end
