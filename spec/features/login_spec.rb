f7662d2823d591a9ca4ae7d7eb0ac23928b0a46e
Dan Gyi <daniel.gyi@gmail.com>
2019-09-14 19:26:14 +0100

    feature tests passing

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -5,4 +5,29 @@ feature 'Log in' do
     log_in_test
     expect(page).to have_content "Chitter - Get your Daily Peeps"
   end
+
+  scenario 'incorrect password' do
+    visit '/'
+    fill_in 'Email', with: "Test987@email.com"
+    fill_in 'Password', with: "wrong password"
+    click_button "Log In"
+    expect(current_path).to eq('/')
+    expect(page).to have_content "Please check your email or password."
+  end
+
+  scenario "email doesn't exist" do
+    visit '/'
+    fill_in 'Email', with: "Test100000@email.com"
+    fill_in 'Password', with: "password987"
+    click_button "Log In"
+    expect(current_path).to eq('/')
+    expect(page).to have_content "Please check your email or password."
+  end
+
+  scenario 'cant visit /peeps page without logging in' do
+    visit '/peeps'
+    expect(current_path).to eq('/')
+    expect(page).to have_content "You must log in to see Peeps!"
+  end
+
 end
