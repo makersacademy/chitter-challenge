0466b6f52af0773ed53a030800e16d9e532e6c68
Dan Gyi <daniel.gyi@gmail.com>
2019-09-14 16:37:20 +0100

    tests passing

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -4,7 +4,7 @@ feature 'Post Peep' do
   scenario 'Want new peep to be displayed on /peeps' do
     log_in_test
     expect(page).to have_content "Chitter - Get your Daily Peeps"
-    fill_in 'Peepbox', with: "Hello, this is a really lovely test peep"
+    fill_in 'content', with: "Hello, this is a really lovely test peep"
     click_button "Post Peep"
     expect(page).to have_content("Hello, this is a really lovely test peep")
   end
