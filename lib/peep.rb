79b24ec4863da60eb107de3af924549d988de072
karlo <deguzmankarlo@gmail.com>
2019-09-15 14:28:37 +0100

    Add time to each peep

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,16 +1,18 @@
 class Peep
-  attr_reader :id, :peep
+  attr_reader :id, :peep, :time
 
-  def initialize(id:, peep:)
+  def initialize(id:, peep:, time:)
     @id = id
     @peep = peep
+    @time = time
   end
 
   def self.create(peep)
     conn = connect_database
+    time = Peep.time
 
-    res = conn.exec("INSERT INTO peeps (peep) VALUES ('#{peep}') RETURNING id, peep")
-    Peep.new(id: res[0]['id'], peep: res[0]['peep'])
+    res = conn.exec("INSERT INTO peeps (peep, time) VALUES ('#{peep}', '#{time}') RETURNING id, peep, time")
+    Peep.new(id: res[0]['id'], peep: res[0]['peep'], time: res[0]['time'])
   end
 
   def self.all
@@ -18,7 +20,7 @@ class Peep
 
     res = conn.exec("SELECT * FROM peeps")
     res.map do |peep|
-      Peep.new(id: peep['id'], peep: peep['peep'])
+      Peep.new(id: peep['id'], peep: peep['peep'], time: peep['time'])
     end
   end
 
@@ -30,5 +32,10 @@ class Peep
     else
         conn = PG.connect(dbname: 'chitter')
     end
+  end
+
+  def self.time
+    t = Time.now
+    t.strftime '%d %B %Y - %H:%M'
   end
 end
