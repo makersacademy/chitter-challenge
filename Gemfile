3ecd14c9f42fab0ecbb6744b090b94bd9217e8b2
Rafaela <rafalucy3@gmail.com>
2019-09-15 16:04:16 +0100

    Setup to DataMapper adapter

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'data_mapper'
+gem 'dm-postgres-adapter'
 gem 'rake'
 gem 'rubocop', '0.71.0'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,5 +1,9 @@
 require 'sinatra/base'
 require 'sinatra/flash'
+require 'data_mapper'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, 'postgres://user:rafaela@127.0.0.1/chitter')
 
 class Chitter < Sinatra::Application
 
