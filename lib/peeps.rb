5c56f5328f45e995ad34cfbab4a133951366f64c
ilianameza <ilianameza530@gmail.com>
2019-09-15 23:38:31 +0100

    create private method in peeps class

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -2,13 +2,19 @@ require 'pg'
 
 class Peeps
   def self.all
+    result = self.connect.exec("SELECT * FROM peeps_library;")
+    result = result.map { |peep| peep['peeps'] }
+    result.reverse
+  end
+
+  private_class_method
+
+  def self.connect
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'peeps_test')
     else
       connection = PG.connect(dbname: 'peeps')
     end
-    
-    result = connection.exec("SELECT * FROM peeps_library;")
-    result.map { |peep| peep['peeps'] }
   end
+
 end
--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -3,10 +3,10 @@ require 'peeps'
 describe Peeps do
   describe '.all' do
     it 'returns all peeps' do
-    connection = PG.connect(dbname: 'peeps_test')
+      connection = PG.connect(dbname: 'peeps_test')
 
-    connection.exec("INSERT INTO peeps_library (peeps) VALUES('Good morning!');")
-    connection.exec("INSERT INTO peeps_library (peeps) VALUES('Good evening!');")
+      connection.exec("INSERT INTO peeps_library (peeps) VALUES('Good morning!');")
+      connection.exec("INSERT INTO peeps_library (peeps) VALUES('Good evening!');")
 
       peeps = Peeps.all
 
@@ -14,4 +14,16 @@ describe Peeps do
       expect(peeps).to include("Good evening!")
     end
   end
+
+  describe '.timestamp' do
+    it 'returns the time and date the peep was created' do
+      connection = PG.connect(dbname: 'peeps_test')
+      connection.exec("INSERT INTO peeps_library (peeps) VALUES('Good morning!');")
+      connection.exec("INSERT INTO peeps_library (peeps) VALUES('Good evening!');")
+      peeps = Peeps.all
+      expect(peeps).to include("Good morning!\n 2018-05-03 02:36:54.480")
+      expect(peeps).to include("Good evening!\n 2018-05-03 02:36:54.480")
+    end
+  end
+
 end
