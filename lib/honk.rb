bdb73ec3e39166edab839dcb9e3aae2e04616793
Mike <mjwalshe@gmail.com>
2019-09-15 18:14:32 +0100

    Refactor honk.rb

--- lib/honk.rb
+++ lib/honk.rb
@@ -5,24 +5,14 @@ class Honk
   attr_reader :id, :text
   
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'honker_test')
-    else
-      connection = PG.connect(dbname: 'honker')
-    end
-    result = connection.exec("SELECT * FROM honks;")
+    result = DatabaseConnection.query("SELECT * FROM honks;")
     result.map do |honk| 
       Honk.new(id: honk['id'], text: honk['text'])
     end 
   end
 
   def self.add(text:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'honker_test')
-    else
-      connection = PG.connect(dbname: 'honker')
-    end
-    result = connection.exec("INSERT INTO honks (text) VALUES ('#{text}') RETURNING id, text;")
+    result = DatabaseConnection.query("INSERT INTO honks (text) VALUES ('#{text}') RETURNING id, text;")
     Honk.new(id: result[0]['id'], text: result[0]['text'])
   end
 
