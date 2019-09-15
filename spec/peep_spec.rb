7c84c0144231f4de22bbd37f443b2b0c5a2a35f3
karlo <deguzmankarlo@gmail.com>
2019-09-15 12:24:20 +0100

    Display peeps in reverse chronological order

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -24,5 +24,17 @@ describe Peep do
       expect(peeps.first.peep).to include 'First peep'
       expect(peeps.last.peep).to include 'Second peep'
     end
+
+    it 'returns all peeps in reverse chronological order' do
+      conn = PG.connect(dbname: 'chitter_test')
+
+      conn.exec("INSERT INTO peeps (peep) VALUES ('First peep')")
+      conn.exec("INSERT INTO peeps (peep) VALUES ('Second peep')")
+
+      peeps = Peep.all.reverse
+
+      expect(peeps.last.peep).to include 'First peep'
+      expect(peeps.first.peep).to include 'Second peep'
+    end
   end
 end
