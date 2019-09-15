73a4798da3bc2468de7cb4741e2601b3dcae90ba
Rafaela <rafalucy3@gmail.com>
2019-09-15 20:05:23 +0100

    Add a new path to insert new user into database - Test refactored and passing"

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
 require 'sinatra/base'
 require 'sinatra/flash'
 require 'data_mapper'
+require 'dm-migrations'
+require_relative './lib/model/user'
 
 DataMapper::Logger.new($stdout, :debug)
-DataMapper.setup(:default, 'postgres://user:rafaela@127.0.0.1/chitter')
+DataMapper.setup(:default, 'postgres://rafaela@127.0.0.1/chitter')
+DataMapper.auto_migrate!
 
 class Chitter < Sinatra::Application
 
@@ -13,6 +16,14 @@ class Chitter < Sinatra::Application
 
   get '/register' do
     erb :register
+  end
+
+  post '/register/submit' do
+    name = params[:name]
+    email = params[:user_email]
+    password = params[:password]
+    user = User.create(:name => name, :email => email, :password => password)
+    redirect '/'
   end
 
 end
--- spec/feature/sign_up_spec.rb
+++ spec/feature/sign_up_spec.rb
@@ -1,11 +1,10 @@
 feature 'resgister' do
   scenario 'If user is not register yet' do
     visit '/register'
-    fill_in "Name", with: 'name'
-    fill_in "Last Name", with: 'last_name'
-    fill_in "Email", with: 'user_email'
-    fill_in "Password", with: 'password'
+    fill_in 'name', with: 'name'
+    fill_in 'user_email', with: 'user_email'
+    fill_in 'password', with: 'password'
     click_on 'Submit'
-    expect(current_path).to eql('/register')
+    expect(current_path).to eql('/')
   end
 end
