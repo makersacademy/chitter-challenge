daa7052336cdb5552cb732e6e955292c25e3a395
Sid Mason-Malik <49129863+smasonmalik@users.noreply.github.com>
2019-10-11 14:08:36 +0100

    New Peep button added

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -1,6 +1,7 @@
 feature "Add a peep" do
   scenario "User adds a peep to chitter" do
-    visit '/peeps/new'
+    visit '/peeps'
+    click_button('New Peep')
     fill_in('message', with: 'My first peep')
     click_button('Post')
     expect(page).to have_content("My first peep")
--- spec/features/testing_setup_spec.rb
+++
@@ -1,7 +0,0 @@
-# feature 'viewing peeps' do
-#   scenario 'visiting the index page' do
-#     visit('/')
-#     click_button 'New Peep'
-#     expect(page).to have_content "Peeps"
-#   end
-# end
