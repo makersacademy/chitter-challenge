f01225e2fdc874ca2bcb00db7d89dbea8f2d4d62
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-11 12:31:39 +0100

    Installed 'pg' gem and connected to database

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,11 +1,11 @@
+require 'pg'
+
 class Chitter
 
   def self.all_peeps
-    [
-      "First peep",
-      "Second peep",
-      "Third peep"
-    ]
+    connection = PG.connect(dbname: 'chitter_manager')
+    result = connection.exec("SELECT * FROM peeps;")
+    result.map { |peep| peep['peep'] }
   end
 
 end
