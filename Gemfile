579edbf2ae0ef2db000a45bd18be18ffb79db955
Jay Issuree <Jay_Issuree@hotmail.co.uk>
2019-10-11 11:12:01 +0100

    Set up: gems, config.ru & spec_helper

--- Gemfile
+++ Gemfile
@@ -1,10 +1,9 @@
 source 'https://rubygems.org'
 
+gem 'capybara'
 gem 'rake'
+gem 'rspec'
 gem 'rubocop', '0.71.0'
-
-group :test do
-  gem 'rspec'
-  gem 'simplecov', require: false
-  gem 'simplecov-console', require: false
-end
+gem 'simplecov', require: false
+gem 'simplecov-console', require: false
+gem 'sinatra'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,19 @@
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
 ])
+
 SimpleCov.start
 
 RSpec.configure do |config|
