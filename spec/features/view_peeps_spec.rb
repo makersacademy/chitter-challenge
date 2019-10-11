a6935c7d3e0b6d321fdc6e28b4a162c56dca5423
Jessica Marais <jessica_marais@hotmail.com>
2019-10-11 19:12:37 +0100

    Refactor view_peep test

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,9 +1,7 @@
 feature 'Viewing Peeps' do
   scenario 'user can see all peeps' do
-    connection = PG.connect(dbname: 'peep_manager_test')
-
-    connection.exec("INSERT INTO peeps (message) VALUES ('This is a test peep');")
-    connection.exec("INSERT INTO peeps (message) VALUES ('This is a test peep 2');")
+    Peep.create(message: 'This is a test peep')
+    Peep.create(message: 'This is a test peep 2') 
 
     visit '/peeps'
 
