ab2886624df8d69edb103046032ac7fa9da3f940
Jessica Marais <jessica_marais@hotmail.com>
2019-10-12 18:46:27 +0200

    README update

--- spec/features/create_peep_spec.rb
+++ spec/features/create_peep_spec.rb
@@ -3,10 +3,13 @@ feature 'Add a new peep' do
     visit '/peeps'
     expect(page).to have_button 'New peep'
   end
+
   scenario 'user can write a peep and add it to list of peeps' do
     visit '/peeps/new'
+    fill_in('name', with: 'John Smith')
+    fill_in('handle', with: 'js2019')
     fill_in('message', with: 'This is a test peep')
     click_button 'Submit'
-    expect(page).to have_content 'This is a test peep'
+    expect(page).to have_content('John Smith', 'js2019', 'This is a test peep') 
   end
 end
