5021cde467805d55a88fc98937624d1f6687c152
Dom Tunstill <datunstill@gmail.com>
2019-10-13 21:07:25 +0100

    add error checking if username and email in use

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ class Chitter < Sinatra::Base
   register Sinatra::Flash
 
   get '/' do
+    redirect "/user/#{User.logged_in.id}" if User.logged_in  
     @peeps = Peep.all_in_order
     erb :index
   end
@@ -20,8 +21,16 @@ class Chitter < Sinatra::Base
   end
 
   post '/user' do
-    user = User.create(user_name:params['user_name'], email:params['email'], password:params['password'])
-    redirect "/user/#{user.id}"
+    User.create(user_name:params['user_name'], email:params['email'], password:params['password'])
+    if User.error == nil
+      redirect "/user/#{user.id}"
+    elsif User.error == 'user_name'
+      flash[:notice] = "Username taken"
+      redirect '/user/new'
+    elsif User.error == 'email'
+      flash[:notice] = "email already in use"
+      redirect '/user/new'
+    end
   end
 
   get '/user/:id' do
@@ -33,15 +42,12 @@ class Chitter < Sinatra::Base
   post '/peep' do
     if User.logged_in != nil
       Peep.create(message:params['peep'])
-      user = User.logged_in
-      redirect "/user/#{user.id}"
+      redirect "/user/#{User.logged_in.id}"
     else
       flash[:notice] = "Not logged in, please login or sign up"
       redirect '/'
     end
-    
   end
-
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -6,6 +6,30 @@ feature 'sign up' do
         fill_in 'user_name', with:'test'
         fill_in 'password', with:'test'
         click_button 'Submit'
-        expect(page).to have_content('welcome test')
+        expect(page).to have_content('Welcome to Chitter, test')
+    end
+
+    scenario 'gives error is username taken' do
+        User.create(user_name:'test', email:'test@test.com', password:'test')
+        visit('/')
+        click_link 'Sign up'
+        fill_in 'email', with:'test2@test.com'
+        fill_in 'user_name', with:'test'
+        fill_in 'password', with:'test'
+        click_button 'Submit'
+        expect(page).to have_content('Username taken')
+        expect(page).not_to have_content('test')
+    end
+
+    scenario 'gives error is email is taken' do
+        User.create(user_name:'test', email:'test@test.com', password:'test')
+        visit('/')
+        click_link 'Sign up'
+        fill_in 'email', with:'test@test.com'
+        fill_in 'user_name', with:'test2'
+        fill_in 'password', with:'test'
+        click_button 'Submit'
+        expect(page).to have_content('email already in use')
+        expect(page).not_to have_content('test')
     end
 end
