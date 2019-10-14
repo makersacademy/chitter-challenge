d6b369541a582836233343839bff73017054b023
Jessica Marais <jessica_marais@hotmail.com>
2019-10-14 12:22:51 +0100

    Fix issue so user can add single and double quotes in their peep

--- lib/peep.rb
+++ lib/peep.rb
@@ -5,7 +5,7 @@ class Peep
   def initialize(id:, name:, handle:, message:, timestamp:)
     @name = name
     @handle = handle
-    @message = message
+    @message = message.gsub("`apos*") {"'"}
     @id = id
     @timestamp = Time.parse(timestamp).strftime("%d/%m/%Y %H:%M")
   end
@@ -18,7 +18,7 @@ class Peep
   end
 
   def self.create(name:, handle:, message:)
-    result = DatabaseConnection.query("INSERT INTO peeps (name, handle, message) VALUES('#{name}', '#{handle}', '#{message}') RETURNING id, name, handle, message, created_at;")
+    result = DatabaseConnection.query("INSERT INTO peeps (name, handle, message) VALUES('#{name}', '#{handle}', '#{message.gsub("'") {"`apos*"}}') RETURNING id, name, handle, message, created_at;")
 
     Peep.new(id: result[0]['id'], name: result[0]['name'], handle: result[0]['handle'], message: result[0]['message'], timestamp: result[0]['created_at'])
   end
