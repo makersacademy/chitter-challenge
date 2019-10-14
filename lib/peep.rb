20b8365f5bc921d291d41cfa01e551e78db0c109
Jessica Marais <jessica_marais@hotmail.com>
2019-10-14 11:59:43 +0100

    User can see timestamp on each peep

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,24 +1,25 @@
 class Peep
 
-  attr_reader :message, :id, :name, :handle
+  attr_reader :message, :id, :name, :handle, :timestamp
 
-  def initialize(id:, name:, handle:, message:)
+  def initialize(id:, name:, handle:, message:, timestamp: )
     @name = name
     @handle = handle
     @message = message
     @id = id
+    @timestamp = Time.parse(timestamp).strftime("%d/%m/%Y %H:%M")
   end
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM peeps;")
     result.map do |message|
-      Peep.new(id: message['id'], name: message['name'], handle: message['handle'], message: message['message'])
-    end.reverse 
+      Peep.new(id: message['id'], name: message['name'], handle: message['handle'], message: message['message'], timestamp: message['created_at'])
+    end.reverse
   end
 
-  def self.create(name:, handle:, message:)
-    result = DatabaseConnection.query("INSERT INTO peeps (name, handle, message) VALUES('#{name}', '#{handle}', '#{message}') RETURNING id, name, handle, message;")
+  def self.create(name:, handle:, message: )
+    result = DatabaseConnection.query("INSERT INTO peeps (name, handle, message) VALUES('#{name}', '#{handle}', '#{message}') RETURNING id, name, handle, message, created_at;")
 
-    Peep.new(id: result[0]['id'], name: result[0]['name'], handle: result[0]['handle'], message: result[0]['message'])
+    Peep.new(id: result[0]['id'], name: result[0]['name'], handle: result[0]['handle'], message: result[0]['message'], timestamp: result[0]['created_at'])
   end
 end
--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,11 +1,11 @@
 feature 'Viewing Peeps' do
   scenario 'user can see all peeps' do
-    Peep.create(name: 'John Smith', handle: 'js2019', message: 'This is a test peep')
-    Peep.create(name: 'John Smith', handle: 'js2019', message: 'This is a test peep 2')
+    Peep.create(name: 'John Smith', handle: 'js2019', message: 'This is a test peep', timestamp: '14/10/2019 10:30')
+    Peep.create(name: 'John Smith', handle: 'js2019', message: 'This is a test peep 2' timestamp: '14/10/2019 10:30')
 
     visit '/peeps'
 
     expect(page).to have_content ('This is a test peep')
-    expect(page).to have_content ('This is a test peep 2') 
+    expect(page).to have_content ('This is a test peep 2')
   end
 end
