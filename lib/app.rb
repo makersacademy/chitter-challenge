e093949656242294b2cc5a5ce1ce73878751155a
Student <Student@Admins-MacBook-Air.local>
2019-10-25 14:57:30 +0100

    Creating new content to place on the chitter feed (test database)

--- lib/app.rb
+++ lib/app.rb
@@ -11,4 +11,15 @@ class Chitter < Sinatra::Base
     @feed_items = ChitterFeed.content
     erb :feed
   end
+
+  get '/chitter_feed/new' do
+    erb :new
+  end
+
+  post '/chitter_feed' do
+  content = params['content']
+  connection = PG.connect(dbname: 'chitter_test')
+  connection.exec("INSERT INTO feed (content) VALUES('#{content}')")
+  redirect '/chitter_feed'
+  end
 end
