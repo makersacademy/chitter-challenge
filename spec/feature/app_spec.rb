654129458aa364c5412d87b3b0344b77e5dab25f
bradley <bradley.plater1@gmail.com>
2019-11-08 09:26:28 +0000

    Added test to check if user can add new peep

--- spec/feature/app_spec.rb
+++ spec/feature/app_spec.rb
@@ -6,3 +6,15 @@ feature "Page loads" do
     expect(page).to have_content "Hello World"
   end
 end
+
+
+feature "Peeps" do
+  scenario "Can store new peeps" do
+    visit '/new_peep'
+    fill_in('title', with: 'New Peep')
+    fill_in('body', with: 'This is a test peep')
+    click_button("submit")
+    expect(page).to have_content "New Peep"
+    expect(page).to have_content "This is a test peep"
+  end
+end
