67ff20c02a5d5cfed1703c480cbeb85364de43f5
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-08 10:23:05 +0000

    created test database and test to check that users are returned

--- spec/chitter_app_spec.rb
+++ spec/chitter_app_spec.rb
@@ -1,5 +1,17 @@
-describe ChitterApp do
-    it 'returns an instance of the ChitterApp class' do
-        expect(subject).to be_an_instance_of(ChitterApp)
+require 'chitter.rb'
+
+describe Chitter do
+    it 'returns instanc of class' do
+        expect(subject).to be_a Chitter
+    end
+
+    describe '#return_users' do
+        it 'returns a list of users' do
+            users = Chitter.return_users
+
+            expect(users).to include "Kanye"
+            expect(users).to include "Kendrick"
+            expect(users).to include "Jay Z"
+        end
     end
 end
