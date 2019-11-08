712a097a6059303b82293fc49ae0fac623bafeda
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2019-11-08 14:19:25 +0000

    set up ready for more work

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'sinatra/base'
+
+class TBD < Sinatra::Base
+  get '/' do
+    'Hello World'
+  end
+
+  run! if app_file == $0
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,6 @@ require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
   SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
@@ -15,3 +14,13 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = TBD
