f386d23e8c858f15ec6d0dca9fef32bffba3b4f1
Rob Baile <robbaile12@outlook.com>
2019-11-08 15:47:59 +0000

    test for adding post

--- spec/feature/chitter_feature_spec.rb
+++ spec/feature/chitter_feature_spec.rb
@@ -31,4 +31,20 @@ feature 'Chitter' do
         click_button "Sign up"
         expect(page).to have_content "You have successfully signed up!"
     end
+
+    scenario 'Create post' do 
+        log_in
+        fill_in('text', :with => "This is my friday challenge")
+        click_button "Post"
+        expect(page).to have_content "This is my friday challenge"
+    end
+end
+
+
+def log_in
+    User.create('rob@rob.com', "rob")
+    visit('/login')
+    fill_in('email', :with => "rob@rob.com")
+    fill_in('password', with: 'rob')
+    click_button "Log in"
 end
