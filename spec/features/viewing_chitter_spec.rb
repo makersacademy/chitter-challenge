c5c902f05b06dcbc7662ded162df06db99490f7d
Marc Blenkinsop <marcblenky@hotmail.com>
2019-11-08 10:04:48 +0000

    wrote feature test for viewing 'homepage' with test peeps - currently failing

--- spec/features/viewing_chitter_spec.rb
+++ spec/features/viewing_chitter_spec.rb
@@ -5,4 +5,10 @@ feature "Viewing Chitter" do
     visit("/")
     expect(page).to have_content "Welcome to Chitter!"
   end
+
+  scenario "user can view 'Peeps' on Chitter" do
+    visit("/homepage")
+    expect(page).to have_content "This is a test Peep! Peep Peep Peep!"
+    expect(page).to have_content "This is a second, even longer, test Peep! Peep Peep Peep!"
+  end
 end
