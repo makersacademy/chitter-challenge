10628e8a95fba8a4bcc1134f360ea70a6066c7fa
Rob Baile <robbaile12@outlook.com>
2019-11-08 09:51:12 +0000

    created initial test for user class

---
+++ spec/user_spec.rb
@@ -0,0 +1,16 @@
+require 'user'
+require 'pg'
+
+describe User do
+    it 'get a user' do 
+        connection = PG.connect(dbname: 'chitter_test')
+        email = 'rob@rob.com'
+        password = 'rob'
+
+        connection.exec("INSERT INTO users (email, password) VALUES ($1, crypt($2, gen_salt('bf')));", [email, password])
+
+        user = User.find(email, password)
+
+        expect(user.first.email).to eq 'rob@rob.com'
+    end
+end
