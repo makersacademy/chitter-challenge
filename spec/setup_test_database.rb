088eb6e1a1b18ab5b6a7fb1c25c13ffd2b04300e
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2019-11-08 16:00:36 +0000

    Fix CI tests, create tables if not exist

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,6 +3,20 @@ require_relative 'login_helper'
 
 def setup_test_database
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec("TRUNCATE peep, chitter_user;")
+  connection.exec("DROP TABLE IF EXISTS peep, chitter_user;")
+  connection.exec("CREATE TABLE chitter_user (
+    email varchar(255) PRIMARY KEY,
+    password varchar(255) NOT NULL,
+    username varchar(255) UNIQUE,
+    name varchar(255) NOT NULL,
+    image text
+  );")
+  connection.exec("CREATE TABLE peep (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255),
+    text TEXT NOT NULL,
+    date timestamp NOT NULL,
+    FOREIGN KEY (username) REFERENCES chitter_user(username)
+  );")
   register
 end
