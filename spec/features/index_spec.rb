f73bf21b1c83fbd15d1cc69cdab0a86d65d69d3a
Student <student@Makerss-MacBook-Pro.local>
2019-11-08 15:03:16 +0000

    showing multiple peeps, no order or db yet

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -6,12 +6,12 @@ feature 'index page' do
     expect(page).to have_content 'Peeps: test'
   end
 
-  scenario 'shows peeps in reverse order' do
+  scenario 'shows multiple peeps' do
     visit '/'
     fill_in 'peep', with: '1st test'
     click_button 'submit'
     fill_in 'peep', with: '2nd test'
     click_button 'submit'
-    expect(page).to have_content 'Peeps: 2nd test, 1st test'
+    expect(page).to have_content 'Peeps:' && '2nd test' && '1st test'
   end
 end
