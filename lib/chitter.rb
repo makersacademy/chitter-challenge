50bc8ffc46f064ce3301e730cc6f5e01794c7934
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-08 10:32:23 +0000

    added method to return names of users

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,5 +1,9 @@
 require 'pg'
 
 class Chitter
-    
+    def self.return_users
+        connection = PG.connect(dbname: 'chitter')
+        result = connection.exec("SELECT * FROM users;")
+        result.map { |user| user['name'] }
+    end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,16 @@ RSpec.configure do |config|
   end
 end
 
+require_relative './setup_test_database'
+
+ENV['ENVIRONMENT'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
+
 ENV['RACK_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
