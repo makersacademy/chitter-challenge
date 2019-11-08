e5abbea64e0e1701679e160b8dbad0ea731243b4
Rob Baile <robbaile12@outlook.com>
2019-11-08 10:15:37 +0000

    set up views and public directory as well as defined some basic routes

--- app.rb
+++ app.rb
@@ -5,6 +5,14 @@ class Chitter < Sinatra::Base
     enable :sessions
     
     get '/' do 
-        "Hello World"
+        session[:login] == true ? redirect('/home') : redirect('/login')
+    end
+
+    get '/login' do 
+        erb(:login)
+    end
+
+    get '/signup' do 
+        erb(:signup)
     end
 end
