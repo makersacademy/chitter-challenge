d08dbea9087466a50f0da9f55db44439c2c39153
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-08 11:16:45 +0000

    added functionality for creating a user

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -11,4 +11,14 @@ class Chitter
         result = connection.exec("SELECT * FROM users;")
         result.map { |user| user['name'] }
     end
+
+    def self.create_user(name:, username:, email:, password:)
+        if ENV['ENVIRONMENT'] == 'test'
+            connection = PG.connect(dbname: 'chitter_test')
+        else
+            connection = PG.connect(dbname: 'chitter')
+        end
+
+        connection.exec("INSERT INTO users (name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{password}')")
+    end
 end
