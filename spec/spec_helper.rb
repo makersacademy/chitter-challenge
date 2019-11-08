9ae67a33f0541d27cbe6b4ddebd5e921a83231a8
Rob Baile <robbaile12@outlook.com>
2019-11-08 10:09:22 +0000

    got create user and find user test to work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,16 @@
 require 'simplecov'
 require 'simplecov-console'
 
+require_relative './setup_test_database'
+
+ENV['ENVIRONMENT'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -7,7 +7,9 @@ describe User do
         email = 'rob@rob.com'
         password = 'rob'
 
-        connection.exec("INSERT INTO users (email, password) VALUES ($1, crypt($2, gen_salt('bf')));", [email, password])
+        # connection.exec("INSERT INTO users (email, password) VALUES ($1, crypt($2, gen_salt('bf')));", [email, password])
+
+        User.create(email, password)
 
         user = User.find(email, password)
 
