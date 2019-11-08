1fef2f799aca91733dc57aa5984da802ea6fa1c7
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-08 11:34:24 +0000

    added view for creating a new user account

--- app.rb
+++ app.rb
@@ -11,6 +11,15 @@ class ChitterApp < Sinatra::Base
         @users = Chitter.return_users
         erb :'users/users-names'
     end
+
+    get '/users/new' do
+        erb :'/users/new'
+    end
+
+    post '/users' do
+        Chitter.create_user(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
+        redirect '/users'
+    end
     
     run! if app_file == $0
 end
--- spec/features/create_a_new_user_spec.rb
+++ spec/features/create_a_new_user_spec.rb
@@ -1,11 +1,12 @@
 feature 'Creating a new user' do
     scenario 'A user can create an account' do
         visit('/users/new')
+
         fill_in('name', with: 'Vince')
         fill_in('username', with: 'vince')
         fill_in('email', with: 'vince@gmail.com')
         fill_in('password', with: 'password456')
-        click_button('Submit')
+        click_button('Create Account')
 
         expect(page).to have_content('Vince')
     end
