4ddb0c30dbd3761989f2bc6d7fe5dde54139f48a
Shahrukh Khan <Shahrukh.Khan.2597@gmail.com>
2019-11-08 14:14:00 +0000

    added styling

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,7 @@
 feature 'Check that infrastructure is working' do
     scenario 'View is returned' do
         visit('/')
-        expect(page).to have_content 'Welcome to Chitter'
+        expect(page).to have_content 'Chitter'
+        expect(page).to have_content 'Enter site'
     end
 end
