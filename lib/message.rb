cd226a50bdb1b848cce5588d6b195594e4c6008e
Rob Baile <robbaile12@outlook.com>
2019-11-08 11:51:52 +0000

    test and code for adding messages

--- lib/message.rb
+++ lib/message.rb
@@ -1,7 +1,31 @@
 require 'pg'
 
 class Message
-    def initialize
-        
+    attr_reader :id, :text, :time
+
+    def initialize(id:, text:, time:)
+        @id = id
+        @text = text
+        @time = time
+    end
+
+    def self.create(user_id, text, time)
+        connection = connect
+        connection.exec("INSERT INTO message (user_id, text, time) VALUES ($1, $2, $3);", [user_id, text, time])
+    end
+
+    def self.all 
+        connection = connect
+        result = connection.exec("SELECT * from message")
+        result.map do |message|
+            Message.new(id: message['id'], text: message['text'], time: message['time'])
+        end
+    end
+
+    private
+
+    def self.connect
+        db_name = ENV['ENVIRONMENT'] == 'test' ? 'chitter_test' : 'chitter'
+        return PG.connect(dbname: db_name) 
     end
 end
