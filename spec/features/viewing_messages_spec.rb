be29ff8bae2bb1399336f17c9833f8bb492591db
Migle Kubiliute <migle.kubiliute@gmail.com>
2019-11-08 12:28:14 +0000

    added a test for viewing the messages - edited

--- spec/features/viewing_messages_spec.rb
+++ spec/features/viewing_messages_spec.rb
@@ -1,9 +1,9 @@
 feature 'Viewing messages' do
     scenario 'A user can see the messages of others' do
-      visit('/messages')
+      visit('/chits')
   
       expect(page).to have_content "Hey you! this is Chitter"
       expect(page).to have_content "Have groundbreaking ideas?"
-      expect(page).to have_content "Chit it now!
+      expect(page).to have_content "Chit it now!"
     end
   end
