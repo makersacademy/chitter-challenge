47d290bf60874b5a38ae9a1f5518ca59af983d6a
Robert <bibbycodes@gmail.com>
2019-11-09 00:09:36 +0000

    used ORM

--- lib/beet.rb
+++ lib/beet.rb
@@ -1,19 +1,28 @@
 require_relative './setup_db'
 
 class Beet
+  attr_reader :text, :timestamp
+
+  def initialize(text, timestamp)
+    @text = text
+    @timestamp = timestamp
+  end
+
   def self.all
-    response = DatabaseConnection.query("SELECT * FROM beets ORDER BY id;").values
+    response = DatabaseConnection.query("SELECT * FROM beets ORDER BY timestamp DESC;").values
     beets = []
 
     for value in response
-      p value
-      beets.push(value[1])
+      beets.push(Beet.new(value[1], value[2]))
     end
 
     return beets
   end
 
   def self.add(beet)
-    request = DatabaseConnection.query("INSERT INTO beets (beet_text) VALUES ('#{beet}');")
+    result = DatabaseConnection.query("INSERT INTO beets (beet_text, timestamp) VALUES ('#{beet}', '#{Time.now}');")
   end
+
+  # def self.edit(beet_id, new_text)
+  # end
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -2,8 +2,8 @@ def truncate_and_load_beets
   connection = PG::Connection.open(:dbname => 'bitter_test')
   result  = connection.exec(
     "TRUNCATE beets;
-    INSERT INTO beets (beet_text) VALUES ('No Brexit!');
-    INSERT INTO beets (beet_text) VALUES ('SJWs make me sick!');
-    INSERT INTO beets (beet_text) VALUES ('I love Coding!');
+    INSERT INTO beets (beet_text, timestamp) VALUES ('No Brexit!', '#{Time.now}');
+    INSERT INTO beets (beet_text, timestamp) VALUES ('SJWs make me sick!', '#{Time.now}');
+    INSERT INTO beets (beet_text, timestamp) VALUES ('I love Coding!', '#{Time.now}');
     ")
 end
