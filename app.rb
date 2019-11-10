f8d17eda0a4dd2b546080296e74cebdc73ab5c93
Student <student@Makerss-MBP.connect>
2019-11-10 15:34:38 +0000

    User can enter name and proceed to signed in page. Runs in browser, tests pass, 100%, no offences

--- app.rb
+++ app.rb
@@ -1,13 +1,17 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
-  
+
   get '/' do
-    erb :home
+    erb :splash
   end
   
   get '/register' do
     erb :register
+  end
+
+  post '/home' do
+    erb :home
   end
 
   run! if app_file == $0
