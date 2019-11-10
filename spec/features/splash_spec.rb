a47507b6b1240153ad80c4a52989c96cf735e11a
Student <student@Makerss-MBP.connect>
2019-11-10 18:51:50 +0000

    Added log in screen. Runs in browser, tests pass, 100%, no offences

--- spec/features/splash_spec.rb
+++ spec/features/splash_spec.rb
@@ -6,4 +6,20 @@ feature 'Home page' do
     expect(page).to have_content('Already registered?')
     expect(page).to have_button('Log in')
   end
+
+  scenario 'allows user to proceed to registration screen' do
+    visit('/')
+    click_button('Register')
+    expect(page).to have_content("Please enter your details.")
+    expect(page).to have_field("name")
+    expect(page).to have_field("password")
+  end
+
+  scenario 'allows user to proceed to log in screen' do
+    visit('/')
+    click_button('Log in')
+    expect(page).to have_content("Please log in below.")
+    expect(page).to have_field("name")
+    expect(page).to have_field("password")
+  end
 end
