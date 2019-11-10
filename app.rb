252f215924187785073d11380b2094cc267185c3
Donald Newholm <newholm1@hotmail.com>
2019-11-10 14:51:16 +0000

    User can register - no backend yet. Works in browser, tests pass, 100%, no offences

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+  
   get '/' do
     erb :home
   end
   
+  get '/register' do
+    erb :register
+  end
+
   run! if app_file == $0
 end
--- spec/home_spec.rb
+++ spec/home_spec.rb
@@ -1,5 +1,5 @@
 feature 'Home page' do
-  scenario 'greets user and offers Sign Up and Log In options' do
+  scenario 'greets user and offers Register and Log In options' do
     visit('/')
     expect(page).to have_content('Howdy, Stranger!')
     expect(page).to have_button('Register')
