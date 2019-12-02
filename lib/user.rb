35829a885d3995dec78e4e4860930820965be5d7
Sam King <samuelking94@gmail.com>
2019-12-02 16:08:52 +0000

    Implement #last method

--- lib/user.rb
+++ lib/user.rb
@@ -8,4 +8,24 @@ class User
     @email = email
     @password = password
   end
+
+  def self.create(name:, username:, email:, password:)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    user = connection.exec "INSERT INTO users (name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{password}') RETURNING id, name, username, email, password;"
+    User.new(id: user[0]['id'], name: user[0]['name'], username: user[0]['username'], email: user[0]['email'], password: user[0]['password'])
+  end
+
+  def self.last
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    last_user = connection.query("SELECT * FROM users ORDER BY id DESC limit 1")
+    User.new(id: last_user[0]['id'], name: last_user[0]['name'], username: last_user[0]['username'], email: last_user[0]['email'], password: last_user[0]['password'])
+  end
 end
