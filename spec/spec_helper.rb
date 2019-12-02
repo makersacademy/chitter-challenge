6a5c99ab2941bfdbb7ee891219cb18696b59cdd2
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-12-02 10:45:40 +0000

    /homepage created. Simple feature test passed. Spec helper, Gemfile, config set up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
+ENV['RACK_ENV'] = 'test'
+
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'sinatra'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
---
+++ spec/view_peeps_spec.rb
@@ -0,0 +1,8 @@
+feature 'User can visit the home page and read peeps' do
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content "Welcome to Chitter"
+    # within(:css, '#buttons') do
+    #       click_link 'Enter'
+  end
+end
