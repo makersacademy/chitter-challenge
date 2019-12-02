00c372882c236bd168874061d15b96ada55b70f2
djlonboy <djlonboy@gmail.com>
2019-12-02 16:28:21 +0000

    Added and passed test for Create Post button linking to new post page

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ class Chitter < Sinatra::Base
     erb :index
   end
 
+  get '/create' do
+    erb :create
+  end
+
   run! if app_file == $0
 
 end
--- spec/feature/feature_spec.rb
+++ spec/feature/feature_spec.rb
@@ -1,11 +1,20 @@
 #Feature tests for the web app
 
-feature 'Add bookmarks' do
+feature 'Peeps page:' do
   scenario 'index shows all peeps' do
     visit('/')
     expect(page).to have_content "Post from 2019-12-02 @ 10:00"
     expect(page).to have_content "Post from 2019-11-17 @ 10:00"
     expect(page).to have_content "Post from 2019-08-16 @ 10:00"
     expect(page).to have_content "Post from 2019-12-02 @ 08:00"
+  end
+end
+
+feature 'Peeps page:' do
+  scenario 'Create Post button goes to create post page' do
+    visit('/')
+    click_button "Create Post"
+    expect(page).to have_content "Enter a post"
+    expect(page).to have_current_path("/create")
   end
 end
