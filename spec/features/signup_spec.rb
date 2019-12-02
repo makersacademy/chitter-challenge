f0acd470c16b4a6317430e868fb81a442dfe8c98
Sam King <samuelking94@gmail.com>
2019-12-02 15:14:59 +0000

    Failing feature test

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -8,4 +8,19 @@ feature 'signup' do
     expect(page).to have_field('password')
     expect(page).to have_button('Sign up!')
   end
+
+  scenario 'user details are shown on page' do
+    visit('/')
+    click_button('Sign up!')
+    fill_in('name', :with => 'Charlie')
+    fill_in('username', :with => 'charlie123')
+    fill_in('email', :with => 'charlie@emailhost.com')
+    fill_in('password', :with => 'password321')
+    click_button('Sign up!')
+    expect(page).to have_content("Thank you for signing up!")
+    expect(page).to have_content("Name: Charlie")
+    expect(page).to have_content("Username: charlie123")
+    expect(page).to have_content("Email: charlie@emailhost.com")
+    expect(page).to have_content("Password: password321")
+  end
 end
