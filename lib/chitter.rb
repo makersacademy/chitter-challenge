d4c3b4b37c5eb2659adeec40646751c953c90d5d
Selina Erhabor <selinaerhabor@gmail.com>
2019-12-02 23:19:20 +0000

    Created method to display peeps from chitter table. Test is now passing

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,4 +1,14 @@
 require 'pg'
 
 class Chitters
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_manager_test')
+    else
+      connection = PG.connect(dbname: 'chitter_manager')
+    end
+
+    result = connection.exec("SELECT * FROM chitter;")
+    result.map { |chitter| chitter['peep'] }
+  end
 end
