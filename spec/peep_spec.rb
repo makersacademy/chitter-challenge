b35879da3ab188b1d69e16e43ca83828fd7e8cc8
Marianne OConnor <marianneoco@gmail.com>
2019-12-02 11:53:20 +0000

    All tests passing for peep spec

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,29 +3,30 @@ require './lib/peep'
 
 describe Peep do
 
+  subject(:peep) { described_class.new(1, "Hello World!", "Marianne", "marianneoco", "09:00") }
+
   it "can create a new peep" do
-    peep = Peep.new("Hello World!")
     expect(peep).to be_instance_of(Peep)
   end
 
+  it "can create a new peep with an id" do
+    expect(peep.id).to eq(1)
+  end
+
   it "can create a new peep with content" do
-    peep = Peep.new("Hello World!")
     expect(peep.content).to eq("Hello World!")
   end
 
-  it "has a default name of the peep poster" do
-    peep = Peep.new("Hello World!")
+  it "has name of peep poster" do
     expect(peep.author_name).to eq("Marianne")
   end
 
-  it "has a default user handle of the peep poster" do
-    peep = Peep.new("Hello World!")
+  it "has user handle of the peep poster" do
     expect(peep.author_handle).to eq("marianneoco")
   end
 
-  #it "shows the time the peep was posted" do
-
-  #end
-
+  it "shows the time the peep was posted" do
+    expect(peep.time).to eq("09:00")
+  end
 
 end
