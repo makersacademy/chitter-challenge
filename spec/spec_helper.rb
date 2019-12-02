2a1d5f294ece107a8b9a96e13fca54b916b5da79
Richard <aagbajdos@gmail.com>
2019-12-02 14:56:45 +0000

    DB & Table added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
