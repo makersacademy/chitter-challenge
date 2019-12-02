1e87240db2dc76abc326f1a1c8da74f5cdf5f75f
Sam King <samuelking94@gmail.com>
2019-12-02 10:59:32 +0000

    Implement #create self method
    Returns instance of peep containing persisted data that has been added to database

--- lib/peep.rb
+++ lib/peep.rb
@@ -5,4 +5,14 @@ class Peep
     @id = id
     @body = body
   end
+
+  def self.create(body:)
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    peep = connection.exec "INSERT INTO peeps (body) VALUES('#{body}') RETURNING id, body;"
+    Peep.new(id: peep[0]['id'], body: peep[0]['body'])
+  end
 end
