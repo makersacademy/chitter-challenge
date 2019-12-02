9dbe6cfe077bc6ce80c0adef07468826219a86b4
Gabby Sanghera <gabby.sanghera@gmail.com>
2019-12-02 12:38:23 +0000

    database connected in peeps class, test not updated ran through irb- works

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'rake'
 gem 'rubocop', '0.71.0'
+gem 'pg'
 
 group :test do
   gem 'capybara'
--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,5 +1,12 @@
+require 'pg'
+
 class Peeps
   def self.all
-  @peeps = peeps 
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec("SELECT * FROM peeplist;")
+    result.map { |peep| p peep}
+
+
+  @peeps = peeps
   end
 end
