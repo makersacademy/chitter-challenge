355197802d7ae515a90fe2014bd891157a95da9c
Selina Erhabor <selinaerhabor@gmail.com>
2019-12-02 23:44:00 +0000

    Created failing test for the create method to be introduced in Chitters class

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -2,10 +2,20 @@ require './lib/chitter'
 require 'pg'
 
 describe Chitters do
+
   describe '.all' do
     it 'returns a list of chitters' do
       connection = PG.connect(dbname: 'chitter_manager_test')
       peeps = Chitters.all
     end
   end
+
+  describe '.create' do
+    it 'creates a new peep' do
+      peep = Chitters.create(user_name: 'user', peep: 'This is a test peep')
+      expect(peep['user_name']).to eq 'user'
+      expect(peep['peep']).to eq 'This is a test peep'
+    end
+  end
+
 end
