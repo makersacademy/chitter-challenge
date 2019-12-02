21f6ca844a58a2be0c198dc2db7953c46c1d92c1
Sam King <samuelking94@gmail.com>
2019-12-02 16:08:16 +0000

    Failing unit test
    Assert that #last returns most recently added user as an instance of User

--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -11,4 +11,33 @@ describe User do
       expect(user.password).to eq("password321")
     end
   end
+
+  describe '#create' do
+    it 'creates a new user in the Chitter database' do
+      user = User.create(name: "Charlie", username: "charlie123", email: "charlie@emailhost.com", password: "password321")
+      db = PG.connect(dbname: 'chitter_test')
+      users = db.query("SELECT * FROM users WHERE id = #{user.id};")
+
+      expect(user.id).to eq(users.first['id'])
+      expect(user.name).to eq("Charlie")
+      expect(user.username).to eq("charlie123")
+      expect(user.email).to eq("charlie@emailhost.com")
+      expect(user.password).to eq("password321")
+    end
+  end
+
+  describe '#last' do
+    it 'should return most recently added instance of User' do
+      user = User.create(name: "Charlie", username: "charlie123", email: "charlie@emailhost.com", password: "password321")
+
+      new_user = User.last
+
+      expect(new_user).to be_instance_of(User)
+      expect(new_user.id).to eq(user.id)
+      expect(new_user.name).to eq(user.name)
+      expect(new_user.username).to eq(user.username)
+      expect(new_user.email).to eq(user.email)
+      expect(new_user.password).to eq(user.password)
+    end
+  end
 end
