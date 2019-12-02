bf48db6d7de311ff22d8429d8fce82099a388919
Sam King <samuelking94@gmail.com>
2019-12-02 11:10:00 +0000

    #all method implemented
    Connect to correct database
    
    Query to extract all rows
    
    Map rows to Peep instances, return as array

--- lib/peep.rb
+++ lib/peep.rb
@@ -15,4 +15,14 @@ class Peep
     peep = connection.exec "INSERT INTO peeps (body) VALUES('#{body}') RETURNING id, body;"
     Peep.new(id: peep[0]['id'], body: peep[0]['body'])
   end
+
+  def self.all
+    if ENV['RACK_ENV'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    peeps = connection.query("SELECT * FROM peeps")
+    peeps.map { |peep| Peep.new(id: peep['id'], body: peep['body'])}
+  end
 end
