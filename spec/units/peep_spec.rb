c42da002846ba6c8f8e987bb111ab28bb627904e
Sam King <samuelking94@gmail.com>
2019-12-02 10:44:13 +0000

    Refactor failing unit test

--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -3,9 +3,9 @@ require 'peep'
 describe Peep do
   describe '#initialize' do
     it 'stores id and body' do
-      peep = Peep.new(:id, :body)
-      expect(peep.id).to eq(:id)
-      expect(peep.body).to eq(:body)
+      peep = Peep.new(id: 1, body: "first peep")
+      expect(peep.id).to eq(1)
+      expect(peep.body).to eq("first peep")
     end
   end
 end
