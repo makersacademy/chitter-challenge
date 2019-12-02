8274f896d5f79caf768ac5bf2ac2306570d2f17d
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2019-12-02 10:19:15 +0000

    Set up testing infrastructure

---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,7 @@
+feature "Testing infrastructure" do
+  scenario "Can run app and check page content" do
+    visit("/")
+
+    expect(page).to have_content "Welcome to Chitter!"
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require "capybara/rspec"
 require 'simplecov'
 require 'simplecov-console'
 
@@ -7,6 +8,11 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+ENV["RACK_ENV"] = "test"
+
+require_relative "../app"
+Capybara.app = Chitter
 
 RSpec.configure do |config|
   config.after(:suite) do
