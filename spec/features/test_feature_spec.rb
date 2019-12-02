208ab16f68ed7676654566f73863acb0a72b9480
Anh Nguyen <anhnguyenis@gmail.com>
2019-12-02 10:11:48 +0000

    add initial setup files and run smoke test

---
+++ spec/features/test_feature_spec.rb
@@ -0,0 +1,6 @@
+feature 'visit homepage' do
+  scenario 'smoke test to see if test passes' do
+    visit '/'
+    expect(page).to have_content "Hello World!"
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,16 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
+ENV['RACK_ENV'] = 'test'
+
+require(File.join(File.dirname(__FILE__), '..', 'app.rb'))
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Chitter
+
 RSpec.configure do |config|
   config.after(:suite) do
     puts
