60a92490240d2c0df33cee38a4d4595d5cf2c699
Sam King <samuelking94@gmail.com>
2019-12-02 10:33:56 +0000

    Failing feature test
    Assert that contents of peep is shown once submitted

--- spec/features/post_message_spec.rb
+++ spec/features/post_message_spec.rb
@@ -8,4 +8,11 @@ feature 'post message' do
     visit('/')
     expect(page).to have_button('Post Peep!')
   end
+
+  scenario 'peep is posted and shown' do
+    visit('/')
+    fill_in('peep', :with => 'my first peep!')
+    click_button('Post Peep!')
+    expect(page).to have_content('my first peep!')
+  end
 end
