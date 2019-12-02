0d7a3d59a679473648e67f1d60f180c3f26ab96d
Sam King <samuelking94@gmail.com>
2019-12-02 10:29:40 +0000

    Failing feature test
    Assert that page contains post peep button

--- spec/features/post_message_spec.rb
+++ spec/features/post_message_spec.rb
@@ -3,4 +3,9 @@ feature 'post message' do
     visit('/')
     expect(page).to have_field("peep")
   end
+
+  scenario 'submit button exists' do
+    visit('/')
+    expect(page).to have_button('Post Peep!')
+  end
 end
