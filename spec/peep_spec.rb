3b3084347e702e67ffb466dc155f6c0bfeeebabd
pavidshetty <pavitrashetty23@gmail.com>
2019-12-03 12:26:39 +0000

    id not recognised error

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,5 +1,5 @@
 require './lib/peep'
-
+require 'database_helper'
 describe Peep do
   describe '#initialize' do
     it 'stores id and message' do
@@ -11,11 +11,10 @@ describe Peep do
 
   describe '#create' do
     it 'creates a new peep entry in the Chitter database' do
-      peeps = Peep.create(message: 'Hello!!This is my first peep!')
-      connection = PG.connect(dbname: 'chitter_test')
-      peeps = connection.query("SELECT * FROM peep WHERE id = #{peeps.id};")
-
-      expect(peeps.id).to eq(peep.first['id'])
+      peeps = Peep.create(id: 1, message: 'Hello!!This is my first peep!')
+      persisted_data = PG.connect(dbname: 'chitter_test').query("SELECT * FROM peep WHERE id = #{peeps.id};")
+      expect(peeps).to be_a Peep
+      expect(peeps.id).to eq persisted_data['id']
       expect(peeps.message).to eq('Hello!!This is my first peep!')
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require_relative './set_up_database'
+require 'database_helper'
 require 'simplecov'
 require 'simplecov-console'
 
