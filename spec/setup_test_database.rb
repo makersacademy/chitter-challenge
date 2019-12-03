7104be5cd510e73883829f9eafedc5c55ae32d6b
Rebecca Varley <rebecca.varley@gmail.com>
2019-12-03 21:23:14 +0000

    Failing test for registering a user

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -5,5 +5,5 @@ p "setting up test database"
 def setup_test_database
 connection = PG.connect(dbname: 'chitter_challenge_test')
 p "setting up test database"
-connection.exec("Truncate chitters;")
+connection.exec("Truncate chitters, users;")
 end
