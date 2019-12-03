c915baa41101a1bcd182bcbc560c602724abd3b2
Josh Hill <jamesjoshuahill@users.noreply.github.com>
2019-12-03 10:48:44 +0000

    Avoid DRY connection for this example

--- lib/peep.rb
+++ lib/peep.rb
@@ -7,6 +7,12 @@ class Peep
   end
 
   def self.all
+    if ENV["ENVIRONMENT"] == "test"
+      connection = PG.connect(dbname: "chitter_test")
+    else
+      connection = PG.connect(dbname: "chitter")
+    end
+
     result = connection.exec("SELECT * FROM peeps")
 
     result.map do |peep|
@@ -15,18 +21,16 @@ class Peep
   end
 
   def self.create(message:)
+    if ENV["ENVIRONMENT"] == "test"
+      connection = PG.connect(dbname: "chitter_test")
+    else
+      connection = PG.connect(dbname: "chitter")
+    end
+
     result = connection.exec("INSERT INTO peeps (message)
       VALUES('#{message}')
       RETURNING id, message;")
 
     Peep.new(id: result[0]["id"], message: result[0]["message"])
-  end
-
-  def self.connection
-    if ENV["ENVIRONMENT"] == "test"
-      return PG.connect(dbname: "chitter_test")
-    end
-
-    return PG.connect(dbname: "chitter")
   end
 end
