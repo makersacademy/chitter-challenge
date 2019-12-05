211db08ff5a7d0d9e17b7f7fb1b3b2ab1b5f8000
Sonia Quintiao <soniaquintiao@gmail.com>
2019-12-05 09:20:54 +0000

    Refactor feature test for: -Newbie & Member buttons. -Failing feature test for posting a peep.

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,34 +3,31 @@ feature 'homepage' do
   scenario 'a user can visit the homepage and see content' do
     visit '/'
     expect(page).to have_content "Welcome to Chitter!"
-    expect(page).to have_content "Sign up or login below."
+    expect(page).to have_content "Register or login below:"
   end
 
-  scenario 'a user can see peep form and submit button' do
+  scenario 'a user can see the peep form and submit button' do
     visit '/'
     expect(page).to have_field('enter peep')
     expect(page).to have_selector(:link_or_button, 'Peep')
   end
 
-  scenario 'a user can fill in form with a peep that becomes visible on page' do
+  scenario 'a user can post a peep that appears on the page' do
     visit '/'
     fill_in('enter peep', with: 'My first peep!')
     click_button('Peep')
     expect(page).to have_content 'My first peep!'
   end
 
-  scenario 'a user can click the Newbie button' do
+  scenario 'a user can click the Newbie or Member buttons' do
     visit '/'
     within(:css, '#button_1') do
-    click_button 'Newbie'
-    # save_and_open_page
-    end
-  end
-
-  scenario 'a user can click the Member button' do
-    visit '/'
-    within(:css, '#button_2') do
-      click_button 'Member'
+      click_button 'Newbie'
+      visit '/'
+      within(:css, '#button_2') do
+        click_button 'Member'
+        # save_and_open_page
+      end
     end
   end
 
