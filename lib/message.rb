3f44cfe0864dc770724563d0f01efe3e3a11b370
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-07 20:18:33 +0000

    Check ts error

--- lib/message.rb
+++ lib/message.rb
@@ -19,7 +19,24 @@ class Message
     end
 
     result = connection.exec("SELECT * FROM messages;")
-    result.map { |message| Message.new(id: message ['id'], msg: message['msg']) }
+    result.map { |message| Message.new(id: message ['id'], msg: message['msg'], ts: message['ts']) }
   end 
+
+  def self.create(msg:, ts:)
+
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+  
+    result = connection.exec("INSERT INTO messages (msg, ts) VALUES('#{msg}') RETURNING id, msg, ts")
+    Message.new(id: result[0]['id'], msg: result[0]['msg'], ts: result[0]['ts'])
+ 
+  
+  end
+
+
+
   
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,18 +2,41 @@ require 'message'
 
 describe Message do
 
-  describe ".create" do
+  describe '.all' do
 
-    # it "creates a new chitter message" do
-    #   bookmark = Bookmark.create(msg: "hello world")
-    #   persisted_data = persisted_data(id: message.id)      
+    it 'returns all messages' do
+      connection = PG.connect(dbname: 'chitter_test')
 
-    #   expect(bookmark).to be_a Bookmark
-    #   expect(bookmark.id).to eq persisted_data['id']
-    #   expect(bookmark.url).to eq 'https://www.bbcgoodfood.com/'
-    #   expect(bookmark.title).to eq 'BBC Good Food'
-    # end
+      # Add the test data
 
+      message = Message.create(msg: "hello world", ts: "2019-12-07 19:29:31.493764")
+      Message.create(msg: "i love kimchi", ts: "2019-12-07 19:30:55.905084")
+      Message.create(msg: "donuts, amirite?", ts: "2019-12-07 19:31:22.503385")
+      
+      messages = Message.all
+
+      #this tests that the Message object returns the same data we input into the database, 
+      #basically, that our object behaves the way we want it to
+      expect(message.length).to eq 3
+      expect(message.first).to be_a Message
+      expect(message.first.id).to eq message.id
+      expect(message.first.msg).to eq 'hello world'
+      expect(message.first.ts).to eq '2019-12-07 19:29:31.493764'
+    end
   end
+
+  # describe ".create" do
+
+  #   it "creates a new chitter message" do
+  #     bookmark = Bookmark.create(msg: "hello world")
+  #     persisted_data = persisted_data(id: message.id)      
+
+  #     expect(bookmark).to be_a Bookmark
+  #     expect(bookmark.id).to eq persisted_data['id']
+  #     expect(bookmark.url).to eq 'https://www.bbcgoodfood.com/'
+  #     expect(bookmark.title).to eq 'BBC Good Food'
+  #   end
+
+  # end
 
 end
