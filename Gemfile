1366fcbec19f534a2d5d63a3b026c98c820ab465
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-12-07 12:16:51 +0000

    initial commit, set up active record with postgres adapter, create database

--- Gemfile
+++ Gemfile
@@ -1,7 +1,16 @@
 source 'https://rubygems.org'
 
+gem 'activerecord'
+gem 'capybara'
+gem 'pg'
 gem 'rake'
 gem 'rubocop', '0.71.0'
+gem 'sinatra'
+gem 'sinatra-activerecord'
+
+group :development do
+  gem 'tux'
+end
 
 group :test do
   gem 'rspec'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,20 @@ require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require 'selenium-webdriver'
+require_relative '../app'
+
+Capybara.app = Chitter
 
 RSpec.configure do |config|
   config.after(:suite) do
