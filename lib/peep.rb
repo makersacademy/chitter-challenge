b6cc161361dd2e38b92d4cc201a3ec278d90cc3a
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2019-12-08 11:17:09 +0000

    Separate connecting to databases from modifying data

--- lib/peep.rb
+++ lib/peep.rb
@@ -10,26 +10,15 @@ class Peep
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-
-    result = connection.exec("SELECT * FROM peeps;")
+    result = DatabaseConnection.query("SELECT * FROM peeps;")
     result.map do |peep|
       Peep.new(id: peep['id'], content: peep['content'], time: peep['time'])
     end
   end
 
   def self.create(content:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-
-    result = connection.exec("INSERT INTO peeps (content, time) VALUES ('#{content}', '#{Time.now}') returning id, content, time;")
+    sql = "INSERT INTO peeps (content, time) VALUES ('#{content}', '#{Time.now}') returning id, content, time;"
+    result = DatabaseConnection.query(sql)
     Peep.new(id: result[0]['id'], content: result[0]['content'], time: result[0]['time'])
   end
 end
