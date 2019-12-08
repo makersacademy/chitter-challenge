25cf055f8bad7428a79bca5da2d6452a18f34efc
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-08 20:25:28 +0000

    Creating test environment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
+# Set the environment to "test"
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file
+require File.join(File.dirname(__FILE__), '..', 'app', 'chitter_app.rb')
+
+# Require all the testing gems
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# Tell Capybara to talk to BookmarkManager
+Capybara.app = ChitterApp
+
 require 'simplecov'
 require 'simplecov-console'
 
