836466804e496d7e5015998ff26ea6a16140253e
kealanheena <kealanheena@gmail.com>
2019-12-08 22:16:39 +0000

    add styling

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -3,15 +3,14 @@ require "user"
 describe User do 
   describe ".all" do 
     it "contains all the users infomation after it's passed" do
-      User.create(email: 'maker@gmail.com', password: 'roadeo', username: 'enthusiastcowboy', name: 'jeff')
+      User.create(email: 'maker@gmail.com', password: 'coding101', username: 'curiouscoder', name: 'bobby')
 
       user = User.all
-      p user
 
       expect(user[0]).to include("maker@gmail.com")
-      expect(user[0]).to include("roadeo")
-      expect(user[0]).to include('enthusiastcowboy')
-      expect(user[0]).to include('jeff')
+      expect(user[0]).to include("coding101")
+      expect(user[0]).to include('curiouscoder')
+      expect(user[0]).to include('bobby')
     end
   end
 
