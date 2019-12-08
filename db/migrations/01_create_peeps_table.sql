deea246e7a1fd350ddc488b57bd1ecb4ccd3730b
Ellie <elliemmanifold@gmail.com>
2019-12-08 11:49:52 +0000

    Add db migrations db setup instructions

---
+++ db/migrations/01_create_peeps_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE peeps (id SERIAL PRIMARY KEY, userid VARCHAR, content TEXT, time TEXT, username TEXT;
---
+++ db/migrations/02_create_users_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE peeps (id SERIAL PRIMARY KEY, username TEXT, email TEXT, password TEXT;
