937eca22348e606f1fadcc5022c6f90ed9b83262
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2019-12-08 11:09:23 +0000

    100% coverage, commit before css

--- lib/user.rb
+++ lib/user.rb
@@ -2,6 +2,8 @@ require_relative 'login_information'
 
 class User < ActiveRecord::Base
   has_many :peeps
+  validates_uniqueness_of :username
+  validates_uniqueness_of :email
 
   def peep message, user_id
     Peep.new_peep message, user_id
