65f3a9096bd6ea9b6cfe3cb50509214b87ca800f
Jonathan Palma <jonathanpalma89@gmail.com>
2019-12-08 21:32:40 +0000

    Modify index design

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,6 +1,6 @@
 feature 'Welcome to Chitter' do
   scenario 'user can see a welcoming message' do
     visit '/'
-    expect(page).to have_content "Welcome to Chitter"
+    expect(page).to have_content "Chitter"
   end
 end
