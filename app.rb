846888de491bbe1a4f2ddf3ecb758f772e64c428
Julie <j49411@gmail.com>
2019-12-08 10:36:46 +0000

    add sign up page and create database

--- app.rb
+++ app.rb
@@ -1,10 +1,29 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+enable :sessions
 
   get '/' do
     erb :index
   end
 
+  get '/sign_up' do
+    erb :sign_up
+  end
+
+  post '/sign_up' do
+    session[:name] = params[:name]
+    redirect '/peeps'
+  end
+
+  get '/peeps' do
+    @name = session[:name]
+    erb :peeps
+  end
+  
+
   run! if app_file ==$0
 end
+
+
+
