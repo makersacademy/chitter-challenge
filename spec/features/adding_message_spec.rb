e129664d2d38fd6fb19d6fc7521fccb71ab2ff60
Raluca Ciucu <raluca.ciucu@gmail.com>
2019-12-08 16:40:47 +0000

    Add 'new peep' link for transition to new msg page

--- spec/features/adding_message_spec.rb
+++ spec/features/adding_message_spec.rb
@@ -1,6 +1,8 @@
 feature "adding new message" do
   scenario "user can add a message" do
-    visit 'messages/new'
+    visit 'messages'
+    click_on("New Peep")
+    expect(current_path).to eq '/messages/new'
     fill_in('msg', with: "hello world")
     click_button("Post")
     expect(current_path).to eq '/messages'
