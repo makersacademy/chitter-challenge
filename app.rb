f0a12d47cf4bf5d3995c57ff4d6745e0ada619cc
kealanheena <kealanheena@gmail.com>
2019-12-08 17:02:30 +0000

    add status and news route

--- app.rb
+++ app.rb
@@ -1,9 +1,20 @@
 require "sinatra"
+require "./lib/status.rb"
 
 class Chitter < Sinatra::Base
 
   get '/' do
     erb(:index)
+  end
+
+  get "/status" do
+    # @statuses = Status.all
+    "What's on your mind?"
+    # erb(:status)
+  end
+
+  get "/news" do
+    "Current Events!"
   end
 
   run! if app_file == $0
