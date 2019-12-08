3248c3c2336c224747a0f6f9b066c282b06dd67f
Ellie <elliemmanifold@gmail.com>
2019-12-08 16:16:07 +0000

    Travis changes

--- db/migrations/01_create_peeps_table.sql
+++ db/migrations/01_create_peeps_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE peeps (id SERIAL PRIMARY KEY, userid VARCHAR, content TEXT, time TEXT, username TEXT;
+CREATE TABLE peeps (id SERIAL PRIMARY KEY, userid VARCHAR, content TEXT, time TEXT, username TEXT;)
