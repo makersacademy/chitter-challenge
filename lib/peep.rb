9f67b3a3c3001c0603d462914595aeeaff672109
Jamie95187 <jamiewong63@googlemail.com>
2019-12-09 01:51:03 +0000

    add time columns to the chitter table and displayed them on the webpage.

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,26 +1,29 @@
+require 'time'
 require 'pg'
 
 class Peep
 
-  attr_reader :message
+  attr_reader :message, :date, :time
 
-  def initialize(id:, message:)
+  def initialize(id:, date:, message:, time:)
     @id = id
+    @date = date
     @message = message
+    @time = time
   end
 
   def self.all
     con = connect_database
     peeps = con.exec('SELECT * FROM chitter')
     peeps.map do |peep|
-      Peep.new(id: peep['id'], message: peep['msg'])
+      Peep.new(id: peep['id'], date: peep['date'], message: peep['msg'], time: peep['time'])
     end
   end
 
   def self.create(message:)
     con = connect_database
-    result = con.exec("INSERT INTO chitter (msg) VALUES('#{message}') RETURNING id, msg;")
-    Peep.new(id: result[0]['id'], message: result[0]['msg'])
+    result = con.exec("INSERT INTO chitter (date, msg, time) VALUES('#{date_of_msg}', '#{message}', '#{time_of_msg}') RETURNING id, date, msg, time;")
+    Peep.new(id: result[0]['id'], date: result[0]['date'], message: result[0]['msg'], time: result[0]['time'])
   end
 
   def self.connect_database
@@ -28,4 +31,13 @@ class Peep
     con = PG.connect(dbname: database)
   end
 
+  def self.time_of_msg
+    time_obj = Time.new
+    time =  "#{time_obj.hour}:#{time_obj.min}:#{time_obj.sec}"
+  end
+
+  def self.date_of_msg
+    time_obj = Time.new
+    date = "#{time_obj.year}-#{time_obj.month}-#{time_obj.day}"
+  end
 end
