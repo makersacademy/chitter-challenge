66ca1e697d8fe0a8911508ffef75c22d411183d4
Jamie95187 <jamiewong63@googlemail.com>
2019-12-09 06:59:34 +0000

    add button that directs user from chitter page to send a new message

---
+++ spec/features/link_to_chitter_page_spec.rb
@@ -0,0 +1,10 @@
+feature 'Send user to a new message page' do
+  scenario 'Allow user to resend another message' do
+    sign_in_and_chatter
+    fill_in 'message', with: 'Henlo World'
+    click_button 'post'
+    click_button 'Send new peep'
+    expect(page).to have_content 'post'
+    expect(current_path).to eq '/peeps/new'
+  end
+end
