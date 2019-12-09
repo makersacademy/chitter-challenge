ebadbe80342095d7590cdd2443ecca6d577fdb71
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-12-09 11:14:02 +0000

    update database connection spec file

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -10,7 +10,7 @@ describe DatabaseConnection do
 
     it 'this connection is persistent' do
       # Grab the connection as a return value from the .setup method
-      connection = DatabaseConnection.setup('bookmark_manager_test')
+      connection = DatabaseConnection.setup('chitter_test')
     
       expect(DatabaseConnection.connection).to eq connection
     end
