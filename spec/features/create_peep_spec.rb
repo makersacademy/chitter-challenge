9daa98439e63c3efa74bba3dcb501497f6d3b67f
Jessica Marais <jessica_marais@hotmail.com>
2019-12-10 12:42:18 +0000

    Fix failing tests

--- spec/features/create_peep_spec.rb
+++ spec/features/create_peep_spec.rb
@@ -1,15 +1,13 @@
 feature 'Add a new peep' do
   scenario 'user sees the new peep button on the /peeps page' do
     visit '/peeps'
-    expect(page).to have_button 'New peep'
+    expect(page).to have_button 'Submit'
   end
 
   scenario 'user can write a peep and add it to list of peeps' do
-    visit '/peeps/new'
-    fill_in('name', with: 'John Smith')
-    fill_in('handle', with: 'js2019')
+    visit '/peeps'
     fill_in('message', with: 'This is a test peep')
     click_button 'Submit'
-    expect(page).to have_content 'This is a test peep' 
+    expect(page).to have_content 'This is a test peep'
   end
 end
