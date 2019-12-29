5ae38058150f64e714f98bf41af95d5b4a55fe95
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-29 17:01:00 +0000

    Test to see that user can see peeps in reverse chronological order

---
+++ spec/features/view_peeps_spec.rb
@@ -0,0 +1,23 @@
+feature "View peeps" do
+  scenario "A maker can view the peeps in reverse cronological order" do
+    sign_up
+    sign_in
+    post_peep
+
+    visit "/peeps/new"
+    fill_in "content", with: "Second peep"
+    click_button "Post"
+
+    expect(current_path).to eq "/peeps"
+    expect("Second peep").to appear_before("First peep")
+  end
+
+  scenario "A maker can view the author's name and username for all the peeps" do
+    sign_up
+    sign_in
+    post_peep
+
+    visit "/peeps"
+    expect(page).to have_content "Ben (@BenSE)"
+  end
+end
