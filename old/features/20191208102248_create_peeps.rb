f7d8cac167dda7b438c3d647e72fd84411db8271
ajbacon <47368135+ajbacon@users.noreply.github.com>
2019-12-29 11:31:48 +0000

    remove defunct files

--- old/features/20191208102248_create_peeps.rb
+++
@@ -1,9 +0,0 @@
-class CreatePeeps < ActiveRecord::Migration[6.0]
-  def change
-    create_table :peeps do |t|
-      t.string :userid
-      t.string :content
-      t.timestamps default: -> { 'CURRENT_TIMESTAMP' }
-    end
-  end
-end
