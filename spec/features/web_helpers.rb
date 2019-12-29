e4e873bcf2733f86bd7f5a51b42e01b71fd0a055
bensheridanedwards <bensheridanedwards@gmail.com>
2019-12-29 16:49:50 +0000

    Add methods for signing up, signing in and posting a peep

---
+++ spec/features/web_helpers.rb
@@ -0,0 +1,21 @@
+def sign_up
+  visit "/"
+  fill_in "email", with: "bensheridanedwards@makers.com"
+  fill_in "password", with: "password"
+  fill_in "name", with: "Ben"
+  fill_in "username", with: "BenSE"
+  click_button "Sign Up"
+end
+
+def sign_in
+  visit '/sessions/new'
+  fill_in "email", with: "bensheridanedwards@makers.com"
+  fill_in "password", with: "password"
+  click_button 'Sign In'
+end
+
+def post_peep
+  visit "/peeps/new"
+  fill_in "content", with: "First peep"
+  click_button "Post"
+end
