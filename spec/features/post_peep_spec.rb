6099810866c7fb66b1d4c89ccbdd55eb9d4219c6
AliciaMerlino <acmerlino1@gmail.com>
2020-01-17 20:18:42 +0000

    user can make a post and view on homepage

---
+++ spec/features/post_peep_spec.rb
@@ -0,0 +1,11 @@
+feature 'create a post' do
+  scenario 'User can write a post and share on webpage' do
+    visit('/peep/new')
+    # click_button 'New peep'
+    fill_in 'user_name', with: 'Harry'
+    fill_in 'user_handle', with: '@thechosenone'
+    fill_in 'peep', with: 'Harry Potter that\'s me'
+    click_button 'Post'
+    expect(page).to have_content "Harry Potter that\'s me"
+  end
+end
