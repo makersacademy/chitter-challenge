3ac74c542ec110bfce92ced3ceafbbb149e5ef90
Meghan <meghan.e.mccue@gmail.com>
2020-01-18 15:05:43 +0000

    Add .all and .create to User class and connect to users database

--- lib/user.rb
+++ lib/user.rb
@@ -1,2 +1,18 @@
 class User
+
+  def self.all
+    DatabaseConnection.setup(environment)
+    result = DatabaseConnection.query("SELECT * FROM users")
+    result.map { |user| user['name'] }
+  end
+
+  def self.create(name:, username:, email:, password:)
+    DatabaseConnection.setup(environment)
+    DatabaseConnection.query("INSERT INTO users (name, username, email, password) VALUES ('#{name}', '#{username}', '#{email}', '#{password}') RETURNING id, name, username, email, password")
+  end
+
+  def self.environment
+    ENV['RACK_ENV'] == 'test' ? 'chitter_app_test' : 'chitter_app'
+  end
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,4 +2,15 @@ require_relative '../lib/user'
 
 describe User do
   let(:subject) { User }
+
+  describe '.all' do
+    it { expect(subject.all).to be_a Array }
+  end
+
+  describe '.create' do
+    it 'creates a new user record in the user databse' do
+      user = subject.create(name: 'name2', username: 'username2', email: 'name2@email.com', password: 'password')
+      expect(subject.all).to include('name2')
+    end
+  end
 end
