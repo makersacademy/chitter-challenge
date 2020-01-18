ab5ec0a3a81679e08f7c5c6d0c921e9b23150040
Umberto Valente <uvalente89@gmail.com>
2020-01-18 18:17:52 +0000

    Implemented log in functionality

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require_relative 'database_connection_setup'
 require './lib/user'
 require './lib/peep'
 
 class Chitter < Sinatra::Base
   enable :sessions
+  register Sinatra::Flash
 
   get '/' do
     redirect '/peeps'
@@ -24,6 +26,21 @@ class Chitter < Sinatra::Base
     user = User.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
     session[:user_id] = user.id
     redirect '/peeps'
+  end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect '/peeps'
+    else
+      flash[:notice] = 'Please check your email or password.'
+      redirect '/sessions/new'
+    end
   end
 
   run! if app_file == $PROGRAM_NAME
---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,36 @@
+feature 'authentication' do
+  scenario 'a user can sign in with the correct credentials' do
+    user = User.create(name: 'Umberto', username: 'uValente', email: 'mock@gmail.com', password: 'psw123')
+    visit '/sessions/new'
+
+    fill_in 'email', with: 'mock@gmail.com'
+    fill_in 'password', with: 'psw123'
+    click_button 'Sign In'
+
+    expect(page).to have_content 'Welcome uValente!'
+  end
+
+  scenario 'a user sees an error if signing in with the email password' do
+    user = User.create(name: 'Umberto', username: 'uValente', email: 'mock@gmail.com', password: 'psw123')
+
+    visit '/sessions/new'
+    fill_in 'email', with: 'wrong@gmail.com'
+    fill_in 'password', with: 'psw123'
+    click_button 'Sign In'
+
+    expect(page).not_to have_content 'Welcome uValente'
+    expect(page).to have_content 'Please check your email or password.'
+  end
+
+  scenario 'a user sees an error if signing in with the wrong password' do
+    user = User.create(name: 'Umberto', username: 'uValente', email: 'mock@gmail.com', password: 'psw123')
+
+    visit '/sessions/new'
+    fill_in 'email', with: 'mock@gmail.com'
+    fill_in 'password', with: 'wrong123'
+    click_button 'Sign In'
+
+    expect(page).not_to have_content 'Welcome uValente'
+    expect(page).to have_content 'Please check your email or password.'
+  end
+end
