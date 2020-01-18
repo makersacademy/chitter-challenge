1168bfaf1e98018a99c73e699c9638e66a643e82
nicolasraffray <nraffray@gmail.com>
2020-01-18 19:14:29 +0000

    setting up a DatabaseConnection class to which will allow queries to the database

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -3,7 +3,14 @@ require 'pg'
 class DatabaseConnection 
 
   def self.setup(database)
-    @connection = PG.connection(dbname: "#{database}")
+    @connection = PG.connect(dbname: database)
   end 
 
+  def self.query(sql_string)
+    @connection.exec(sql_string)
+  end
+
+  def self.connection
+    @connection
+  end 
 end 
--- spec/unit/database_connection_spec.rb
+++ spec/unit/database_connection_spec.rb
@@ -4,10 +4,24 @@ describe DatabaseConnection do
   
   describe '.setup' do 
     it 'makes a connection to the database' do 
-      expect(PG).to receive(:connection).with(dbname: 'chitter_test')
+      expect(PG).to receive(:connect).with(dbname: 'chitter_test')
       DatabaseConnection.setup('chitter_test')
+    end
+  end
+
+  describe '.conncetion' do 
+    it 'gets the value of the conncetion we have and lets us determine if its persistent' do 
+      connection = DatabaseConnection.setup('chitter_test')
+      expect(DatabaseConnection.connection).to eq connection 
     end 
   end 
 
 
+  describe '.query' do 
+    it 'takes in a sql string and passes it the the database' do 
+      connection = DatabaseConnection.setup('chitter_test')
+      expect(connection).to receive(:exec).with("INSERT INTO peeps(peep) VALUES ('Hey, pal, how be the world');")
+      DatabaseConnection.query("INSERT INTO peeps(peep) VALUES ('Hey, pal, how be the world');")
+    end 
+  end 
 end 
