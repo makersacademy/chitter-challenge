a38648f8d8324a923066f5d69a2d8819c292fc2a
Meghan <meghan.e.mccue@gmail.com>
2020-01-18 12:32:20 +0000

    Set up database and add DatabaseConnection

---
+++ lib/database_connection.rb
@@ -0,0 +1,12 @@
+require 'pg'
+
+class DatabaseConnection
+  def self.setup(database_name)
+    @connection = PG.connect :dbname => database_name
+  end
+  
+  def self.query(query_string)
+    @connection.exec(query_string)
+  end
+  
+end
