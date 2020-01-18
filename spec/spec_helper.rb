aae996f2b0e0b0fcf6abda6bcdb21c3a927cf980
Umberto Valente <uvalente89@gmail.com>
2020-01-18 12:38:59 +0000

    Created TRUNCATE script for test database

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'simplecov'
 require 'simplecov-console'
 require 'capybara/rspec'
 require './app'
+require './setup_test_database'
 
 ENV['ENVIRONMENT'] = 'test'
 
@@ -15,6 +16,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
   # config.after(:suite) do
   #   puts
   #   puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
