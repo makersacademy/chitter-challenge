6b77d628c9475809e75b4b5a52384cd022132baa
niki-01 <niki.manoledaki@gmail.com>
2020-01-18 18:51:15 +0000

    Truncates database at the end of rspec test, refactored

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,14 @@
 require 'simplecov'
 require 'simplecov-console'
+require_relative './setup_test_database'
 
 ENV['ENVIRONMENT'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
