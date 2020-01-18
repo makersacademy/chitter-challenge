2de29b62b1a2f20587b6c3f87f40db62cb23205e
David Papamichael <davidpapamichael@gmail.com>
2020-01-18 15:11:42 +0000

    test database set up, set to truncate after each test, and connection optimised for the test environment

--- lib/peep.rb
+++ lib/peep.rb
@@ -3,7 +3,13 @@ require 'pg'
 class Peep
 
   def self.view
-    connection = PG.connect(dbname: 'chitter')
+
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else 
+      connection = PG.connect(dbname: 'chitter')
+    end
+
     result = connection.exec("SELECT * FROM peeps")
     result.map do |post|
       post["body"]
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,11 +3,16 @@ require 'peep'
 describe Peep do
   
   describe ".view" do
+
     it "returns all peeps" do
-      peeps = Peep.view.join(" ")
-      expect(peeps).to include("First post!")
-      expect(peeps).to include("Second post!")
-      expect(peeps).to include("Third post!")
+
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO peeps (body) VALUES ('Test peep one');")
+      connection.exec("INSERT INTO peeps (body) VALUES ('Test peep two');")
+
+      peeps = Peep.view
+      expect(peeps).to include("Test peep one")
+      expect(peeps).to include("Test peep two")
     end
   end
 
