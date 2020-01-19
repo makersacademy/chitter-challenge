46b487a4e68f9af08284494ab888113add6a765a
shaunlsy <shaunlowshuennyuh@hotmail.com>
2020-01-19 21:31:29 +0000

    writing test for reverse chronological order

--- spec/features/see_peeps_reverse_chronological_spec.rb
+++ spec/features/see_peeps_reverse_chronological_spec.rb
@@ -1,18 +1,14 @@
 require 'timecop'
 
 feature 'See peeps in reverse chronological order' do
-  before do 
-    Chitter.create(peep: "Hi")
-  end
-
   scenario 'See latest peep first' do
-    sign_up
-    fill_in "peeps", with: "I'm Shaun"
-    click_button "Tweet"
-    Timecop.freeze(Time.now)
-    expect(current_path).to eq "/peeps"
-
-    expect(page).to have_content "I'm Shaun"
-    # expect(page).to have_content Time.now.to_s
+    Timecop.freeze(Time.now) do
+      sign_up
+      fill_in "peeps", with: "I'm Shaun"
+      click_button "Tweet"
+      expect(current_path).to eq "/peeps"
+      expect(page).to have_content "I'm Shaun"
+      # expect(page).to have_content (Time.now.strftime('%Y-%m-%d %H:%M:%s')).to_s
+    end
   end
 end
