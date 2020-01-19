2541a1b7cbd3c9a4e30cb44d2cf3147c19b35c62
Umberto Valente <uvalente89@gmail.com>
2020-01-19 12:50:26 +0000

    Implemented displaying peep's author username

--- spec/features/shows_peep_spec.rb
+++ spec/features/shows_peep_spec.rb
@@ -17,4 +17,15 @@ feature 'shows peeps' do
     expect(page).to have_content("#{peep1.created_on}")
     expect(page).to have_content("#{peep2.created_on}")
   end
+
+  scenario 'peeps have the author displayed' do
+    user1 = User.create(name: 'Umberto', username: 'uValente', email: 'mock@gmail.com', password: 'psw123')
+    user2 = User.create(name: 'Trump', username: 'MockingTrump', email: 'mock@trump.com', password: 'psw123')
+    Peep.create(user_id: user1.id, content: 'Peep 1')
+    Peep.create(user_id: user2.id, content: 'Peep 2')
+
+    visit '/'
+    expect(page).to have_content("uValente")
+    expect(page).to have_content("MockingTrump")
+  end
 end
