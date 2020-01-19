fb3048873cbfdda070f4e0a92c416298a2dc2cf6
AliciaMerlino <acmerlino1@gmail.com>
2020-01-19 21:16:36 +0000

    update to views to show timestamp

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -14,14 +14,15 @@ describe 'Peep' do
     end
   end
 
-  it 'creates a new peep' do
-    connection = PG.connect(dbname: 'peep_manager_test')
-    peep = Peep.create(user_name: 'Ron', user_handle: '@notthechosenone', peep: 'Ron, Ron, Ron Weasley', timestamp: '2020-01-19')
-  
-    expect(peep).to be_a Peep
-    expect(peep.user_name).to eq 'Ron'
-    expect(peep.user_handle).to eq '@notthechosenone'
-    expect(peep.timestamp).to eq '2020-01-19'
-
+  describe '.create' do
+    it 'creates a new peep' do
+      connection = PG.connect(dbname: 'peep_manager_test')
+      peep = Peep.create(user_name: 'Ron', user_handle: '@notthechosenone', peep: 'Ron, Ron, Ron Weasley', timestamp: '2020-01-19')
+    
+      expect(peep).to be_a Peep
+      expect(peep.user_name).to eq 'Ron'
+      expect(peep.user_handle).to eq '@notthechosenone'
+      expect(peep.timestamp).to eq '2020-01-19'
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+ENV['ENVIRONMENT'] = 'test'
+
+require_relative './setup_test_database'
+
 require './app.rb'
 require 'capybara'
 require 'capybara/rspec'
@@ -5,9 +9,15 @@ require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
-Capybara.app = Peep
+Capybara.app = Chitter
 
-ENV['RACK_ENV']='test'
+ENV['RACK_ENV'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
