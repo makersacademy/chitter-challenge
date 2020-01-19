3e04363628bf6db643281224073ee5f2790ab32a
ria patel <ria.amy.patel@gmail.com>
2020-01-19 14:46:43 +0000

    refactored feature test

--- spec/features/displaying_peeps_spec.rb
+++ spec/features/displaying_peeps_spec.rb
@@ -1,8 +1,9 @@
 feature 'Displaying peeps' do
   scenario 'user posts a peep which is displayed' do
-    visit '/'
-    fill_in 'peep', with: 'My first peep'
-    click_on 'Peep'
-    expect(page).to have_content 'My first peep'
+    Peep.create(peep: 'This is a peep')
+    Peep.create(peep: 'This is another peep')
+    visit '/peeps'
+    expect(page).to have_content 'This is a peep'
+    expect(page).to have_content 'This is another peep'
   end
 end
