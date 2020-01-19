6537f32a506eb68ba38414da1ca9f02c43d4993f
nicolasraffray <nraffray@gmail.com>
2020-01-19 21:46:17 +0000

    database_helper modified to truncate test user table also

--- spec/database_helper.rb
+++ spec/database_helper.rb
@@ -2,7 +2,7 @@ require 'pg'
 
 def test_database_setup
   connection = PG.connect(:dbname => 'chitter_test')
-  connection.exec('TRUNCATE TABLE peeps')
+  connection.exec('TRUNCATE peeps, users')
   connection = PG.connect(:dbname => 'chitter_test')
   connection.exec("INSERT INTO peeps(peep,time) VALUES ('Hey, how it do?','19/01/2020 00:00');")
 end 
