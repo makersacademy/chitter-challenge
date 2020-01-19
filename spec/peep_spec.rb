c5b8538f0009ce5c0cf8445e9a2a2ad100a694fe
David Papamichael <davidpapamichael@gmail.com>
2020-01-19 18:19:34 +0000

    peep_spec refactored to use .create method

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -5,14 +5,10 @@ describe Peep do
   describe ".view" do
 
     it "returns all peeps" do
-
-      connection = PG.connect(dbname: 'chitter_test')
-      connection.exec("INSERT INTO peeps (body) VALUES ('Test peep one');")
-      connection.exec("INSERT INTO peeps (body) VALUES ('Test peep two');")
-
-      peeps = Peep.view
-      expect(peeps).to include("Test peep one")
-      expect(peeps).to include("Test peep two")
+      Peep.create("Test peep one")
+      Peep.create("Test peep two")
+      expect(Peep.view).to include("Test peep one")
+      expect(Peep.view).to include("Test peep two")
     end
   end
 
