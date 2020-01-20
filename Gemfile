041c23cf2848a1118554a8160c3b1f8aa26e0824
Rhodine <rorleans_lindsay@yahoo.co.uk>
2020-01-20 11:54:22 +0000

    Failing test as I have not created a form for a message to be created.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 group :test do
   gem 'rspec'
   gem 'rubocop', '0.71.0'
+  gem 'sinatra'
+  gem 'capybara'
+  gem 'pg'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,13 @@
+ENV['ENVIRONMENT'] = 'test'
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
-
+# tell Capybara about our app class
+Capybara.app = Chitter
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
