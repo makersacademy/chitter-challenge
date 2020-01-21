97b9d55f5e032750d759fe4b165c48ff97a0dd85
Carmen-Alina <carmen.panu@gmail.com>
2020-01-21 11:53:38 +0000

    Simple code to pass the test

--- chitter.rb
+++ chitter.rb
@@ -1,9 +1,27 @@
 require 'sinatra/base'
+require 'pg'
 
 class Chitter < Sinatra::Base
+  enable :sessions
 
   get '/' do
     'Welcome to Chitter!'
+  end
+
+  get '/peeps' do
+    @message = $message
+    erb :'peeps/index'
+  end
+
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  post '/peeps' do
+    $message = params['message']
+    connection = PG.connect(dbname: 'chitter_manager_test')
+    connection.exec("INSERT INTO peeps (message) VALUES('#{$message}');")
+    redirect '/peeps'
   end
 
   run! if app_file == $0
