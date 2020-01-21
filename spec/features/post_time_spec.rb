65c53e0f28dca6f17da249cf023425cf46bb663a
Fergus Corcoran <fergus.corcoran@gmail.com>
2020-01-21 14:46:41 +0000

    feature test for timestamp

---
+++ spec/features/post_time_spec.rb
@@ -0,0 +1,8 @@
+feature 'peeps with timestamps' do
+  scenario 'user can see peeps and the time they were posted' do
+    visit '/chitter'
+    fill_in :post_peep, with: 'Peep The Time'
+    click_button 'Submit'
+    expect(page).to have_content(DateTime)
+  end
+end
