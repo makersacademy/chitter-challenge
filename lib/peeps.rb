4d3c07d1dd75077d6bf6ec9659e090f9dd4bedab
Masha N <marianetchaeva@hotmail.com>
2020-01-21 11:11:47 +0000

    decided to test for Model .create method first

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,2 +1,8 @@
 class Peeps
+  attr_reader :id, :message
+
+  def initialize(id:, message:)
+    @id = id
+    @message = message
+  end
 end
--- spec/units/peeps_spec.rb
+++ spec/units/peeps_spec.rb
@@ -3,12 +3,15 @@ require 'peeps'
 describe Peeps do
   it 'creates a peep with an id and message body' do
       my_peep = Peeps.new(id:1, message: 'This is a test')
-      expect(my_peep).to be Peeps
+      expect(my_peep).to be_instance_of Peeps
+      expect(my_peep.id).to eq 1
+      expect(my_peep.message).to eq 'This is a test'
   end
 
-  it 'can view peeps via .all class method' do
-    my_peep = Peeps.new(id:1, message: 'This is a test')
-    expect(Peeps.all).not_to be_empty
-    expect(Peeps.all.message).to eq 'This is a test'
+  it 'can create a peep via .create class method' do
+    my_peep = Peeps.create(id:1, message: 'This is a test')
+    expect(my_peep).to be_instance_of Peeps
+    expect(my_peep.id).to eq 1
+    expect(my_peep.message).to eq 'This is a test'
   end
 end
