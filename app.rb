0142bd0a18fa67aeeb1972e5eaf944ab909d10d0
Aaron Redden <aaron.redden01@vodafone.com>
2020-01-21 15:57:10 +0000

    second commit

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ require 'sinatra/base'
 class Chitter < Sinatra::Base
 
   get '/' do
-    'Chitter'
+    'chitter'
+  end
+
+  get '/peeps' do
+    peeps = [
+              "peep 1",
+              "peep 2",
+             ]
+  
+    peeps.join
   end
 
   run! if app_file == $0
--- spec/features/viewing_chitter_spec.rb
+++ spec/features/viewing_chitter_spec.rb
@@ -1,6 +1,8 @@
-feature 'Viewing chitter' do
-    scenario 'visiting the chitter page' do
-      visit('/')
-      expect(page).to have_content "Chitter"
+feature 'Viewing bookmarks' do
+    scenario 'A user can see bookmarks' do
+      visit('/peeps')
+  
+      expect(page).to have_content "peep 1"
+      expect(page).to have_content "peep 2"
     end
   end
