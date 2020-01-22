d81e4e3934610b898240a332973f72f8325ade39
Masha N <marianetchaeva@hotmail.com>
2020-01-22 08:12:24 +0000

    fixed Users unit .find test and implemented code

--- lib/users.rb
+++ lib/users.rb
@@ -17,4 +17,9 @@ class Users
     result = DatabaseConnection.query("INSERT INTO users (username, email, password) VALUES('#{username}', '#{email}', '#{password}') RETURNING id, username, email;")
     Users.new(id: result[0]['id'], email: email, username: username)
   end
+
+  def self.find(id:)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id='#{id}';")
+    Users.new(id: id, email: result[0]['email'], username: result[0]['username'])
+  end
 end
