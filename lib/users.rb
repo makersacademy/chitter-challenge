e7ca92f33207b6f6bcb3b045bec650a096ca0f05
Masha N <marianetchaeva@hotmail.com>
2020-01-22 08:12:24 +0000

    fixed Users unit .find test and implemented code

--- lib/users.rb
+++ lib/users.rb
@@ -7,14 +7,13 @@ class Users
     @username = username
   end
 
-  def self.all
-    result = DatabaseConnection.query("SELECT * FROM users;")
-    result.map { |user| Users.new(id: user['id'], email: user['email'], username: user['username']) }
-
-  end
-
   def self.create(username:, email:, password:)
     result = DatabaseConnection.query("INSERT INTO users (username, email, password) VALUES('#{username}', '#{email}', '#{password}') RETURNING id, username, email;")
     Users.new(id: result[0]['id'], email: email, username: username)
+  end
+
+  def self.find(id:)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id='#{id}';")
+    Users.new(id: id, email: result[0]['email'], username: result[0]['username'])
   end
 end
--- spec/units/users_spec.rb
+++ spec/units/users_spec.rb
@@ -3,6 +3,9 @@ require 'users'
 describe Users do
   it 'can find a user by id' do
     user = Users.create(username: 'name1', email: 'test@me.com', password: 'password123')
-    expect(Users.all.find(1)).to eq user
+    found_user = Users.find(id: '1')
+    expect(found_user.id).to eq user.id
+    expect(found_user.email).to eq user.email
+    expect(found_user.username).to eq user.username
   end
 end
