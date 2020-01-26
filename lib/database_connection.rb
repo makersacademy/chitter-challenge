5dcf20ecb769dbc11e0328b44d5e5100d461f24d
Robert <bibbycodes@gmail.com>
2020-01-26 14:28:50 +0000

    debugging heorku

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -7,6 +7,7 @@ class DatabaseConnection
   def self.setup(db_name)
     if ENV['DATABSE_URL']
       uri = URI.parse(ENV['DATABASE_URL'])
+      print(uri)
       @connection = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
     else
       @connection = PG::Connection.open(:dbname => db_name )
