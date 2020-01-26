09ea75075c25437d77750412e33724a628c20d90
Robert <bibbycodes@gmail.com>
2020-01-26 14:18:01 +0000

    added heroku uri

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -4,7 +4,12 @@ class DatabaseConnection
   @connnection
 
   def self.setup(db_name)
-    @connection = PG::Connection.open(:dbname => db_name )
+    if ENV['DATABSE_URL']
+      uri = URI.parse(ENV['DATABASE_URL'])
+      @connection = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+    else
+      @connection = PG::Connection.open(:dbname => db_name )
+    end
   end
 
   def self.query(query)
