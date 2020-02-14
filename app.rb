f447de55288f17bf08addc17b0b929f50631c086
BarriF13 <info@bdesignstudio.co.uk>
2020-02-14 15:21:25 +0000

    User can post

--- app.rb
+++ app.rb
@@ -4,6 +4,12 @@ class Chitter < Sinatra::Base
   enable :sessions
 
     get'/' do
-      "This is my new message"
+      "This is my homepage"
+    end
+    get '/messages' do
+    erb :'index'
+  end
+    post '/messages/index' do
+      redirect '/messages'
     end
 end
