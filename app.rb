5d2cc367ea0eecf4a7c2d542c4ab1e796b8b4f5f
Student <emilywright212@gmail.com>
2020-02-15 20:00:17 +0000

    view peeps page created

--- app.rb
+++ app.rb
@@ -4,4 +4,13 @@ class Chitter < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  get '/make_a_peep' do
+    erb :make_a_peep
+  end
+
+  get '/view_peeps' do
+    erb :view_peeps
+  end
+
 end
---
+++ spec/features/view_peeps_spec.rb
@@ -0,0 +1,11 @@
+feature 'has a page for posting peeps' do
+  scenario '#can enter a peep' do
+    visit('/make_a_peep')
+    fill_in :new_peep, with: "This is my first peep"
+    fill_in :user, with: "Flounder"
+    click_button('Submit')
+    visit('/view_peeps')
+    expect(page).to have_content("Flounder")
+    expect(page).to have_content("This is my first peep")
+  end
+end
