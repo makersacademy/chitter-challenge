f20a70d6b0f4055fdfb384072ab1379123222701
Josh Blumberg <jlblumberg@gmail.com>
2020-02-15 10:44:41 +0000

    First user story passed

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ class Chitter < Sinatra::Base
     "Welcome to Chitter!"
   end
 
+  get '/peeps' do
+    erb(:'peeps/index')
+  end
+
+  get '/peeps/new' do
+    erb(:'peeps/new')
+  end
+
+  post '/peeps' do
+    params[:peep_text]
+  end
+
 end
