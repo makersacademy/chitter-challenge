2202d8080bae466ea89fc7713cd96016d7cab17b
Liz Daly <liz7daly@gmail.com>
2020-02-15 16:10:34 +0000

    Remove testing infrastructure test

--- spec/features/testing_infrastructure_spec.rb
+++
@@ -1,7 +0,0 @@
-#can delete this test when start building - just to test file set up
-feature 'testing infrastructure' do
-  scenario 'shows a webpage' do
-    visit '/'
-    expect(page).to have_content "Hello World"
-  end
-end
