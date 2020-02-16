f34411bd7394c41912f83652e4b819742e6887cf
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-02-16 11:55:57 +0000

    Add sign out feature

--- app/app.rb
+++ app/app.rb
@@ -53,5 +53,11 @@ class Chitter < Sinatra::Base
     end
   end
 
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = 'You have signed out.'
+    redirect('/peeps/index')
+  end
+
   run! if app_file == $0
 end
