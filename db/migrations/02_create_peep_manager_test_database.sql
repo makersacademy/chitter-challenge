c3e300c1eedff4f162672dc038181309bfc0cc07
Jason <jasylwong@gmail.com>
2020-02-16 21:24:07 +0000

    update migrations sql files

---
+++ db/migrations/02_create_peep_manager_test_database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE peep_manager_test;
--- db/migrations/02_create_peeps_test_table.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE peep_manager_test;
-CREATE TABLE peeps(id SERIAL PRIMARY KEY, name VARCHAR(30), username VARCHAR(30), content VARCHAR(150), time TIMESTAMP);
