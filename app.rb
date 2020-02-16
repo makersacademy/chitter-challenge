10851c52d6bad95c10140da1a9d7e07e5683183b
Student <student@makerss-air.home>
2020-02-16 16:02:43 +0000

    creating a database test that comunivate with the form

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/peeps'
 
 class Chitter < Sinatra::Base
     
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,14 @@ require 'simplecov-console'
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+
 # Set the environment to "test"
 ENV['RACK_ENV'] = 'test'
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+#require_relative './setup_test_database'
 
 
 
