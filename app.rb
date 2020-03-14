122a58326d7ec45c350b131317b6ab2fd6e88a32
Sophia Bell <Kittaru87@gmail.com>
2020-03-14 23:12:05 +0000

    css styles

--- app.rb
+++ app.rb
@@ -49,7 +49,7 @@ class Chitter < Sinatra::Base
       session[:user_id] = user.id
       redirect('/chitter')
     else
-      flash.next[:notice] = 'Please check your email or password.'
+      flash[:notice] = 'Please check your email or password.'
       redirect('/sessions/new')
     end
   end
