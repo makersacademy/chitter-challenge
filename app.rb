950d0765760c65bbcd47af48fe12de5d2fb5240a
plkujaw <jawor.jakub@gmail.com>
2020-03-14 16:46:47 +0000

    fixed tests

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require 'sinatra/base'
+require './lib/peep'
 
 class Chitter < Sinatra::Base
-  get '/' do
-    'Chitter'
 
-    erb :index
+  get '/' do
+
+    peeps = [
+    'peep test 1',
+    'peep test 2',
+    'peep test 3'
+    ]
+
   end
 
   run! if app_file == $0
+
 end
--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,7 +1,8 @@
 feature 'viewing a list of peeps' do
   scenario 'user can see a list of peeps' do
     visit('/')
-    expect(page).to have_content('Chitter')
-    expect(page).to have_content('Peeps:')
+    expect(page).to have_content('peep test 1')
+    expect(page).to have_content('peep test 2')
+    expect(page).to have_content('peep test 3')
   end
 end
