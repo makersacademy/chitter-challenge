7fd55238dfaea321ae8b426e0b7db2624b87526c
Sophia Bell <Kittaru87@gmail.com>
2020-03-14 15:27:34 +0000

    making sure you cannot tweet unless signed in

--- spec/features/reversing_peep_order_spec.rb
+++ spec/features/reversing_peep_order_spec.rb
@@ -3,7 +3,12 @@
 feature 'reversing the order of the peeps' do
 
   scenario 'A user can view the newest peep first' do
-    visit '/chitter'
+    visit '/users/new'
+    fill_in('email', with: 'test@example.com')
+    fill_in('password', with: 'password123')
+    fill_in('name', with: 'Jane Doe')
+    fill_in('username', with: '@username')
+    click_button('Submit')
     fill_in('text', with: '1st peep')
     click_button('Submit')
     fill_in('text', with: '2nd peep')
