70ed008624d6a4d6e37e14e18b17925b3b84eaec
Gareth Harris <gdharris1000@gmail.com>
2020-03-15 22:45:36 +0000

    can add messages

--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra/base'
 require 'pg'
 require_relative './database_connection_setup'
 require_relative './lib/user'
+require_relative './lib/message'
+
 
 class Chitter < Sinatra::Base
 
@@ -17,6 +19,17 @@ class Chitter < Sinatra::Base
     
     post '/register' do
         User.create(user_id: params[:user_id], name: params[:name], email: params[:email], password: params[:password])
+
+        redirect('/')
+    end
+
+    get '/message' do
+        erb :message
+    end
+
+    post '/message' do
+        user = User.current
+        Message.create(user_id: user.user_id, time: Time.now, content: params[:message])
 
         redirect('/')
     end
