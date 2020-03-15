d99fac70407dbd8a09ec2a8d982fdf63934339af
plkujaw <jawor.jakub@gmail.com>
2020-03-15 19:33:01 +0000

    sort peeps in desc order

--- db/dbsetup.md
+++ db/dbsetup.md
@@ -2,11 +2,11 @@ Production Database
 $ psql
 $ student=# CREATE DATABASE "chitter";
 $ student=# \c chitter;
-$ chitter=# CREATE TABLE peeps(id SERIAL PRIMARY KEY, message VARCHAR(140) NOT NULL, time TIME NOT NULL, date DATE NOT NULL);
+$ chitter=# CREATE TABLE peeps(id SERIAL PRIMARY KEY, message VARCHAR(140) NOT NULL, time VARCHAR(30) NOT NULL);
 
 Test Database
 
 $ psql
 $ student=# CREATE DATABASE "chitter_test";
 $ student=# \c chitter_test;
-$ chitter=# CREATE TABLE peeps_test(id SERIAL PRIMARY KEY, message VARCHAR(140) NOT NULL, time TIME NOT NULL, date DATE NOT NULL);
+$ chitter=# CREATE TABLE peeps_test(id SERIAL PRIMARY KEY, message VARCHAR(140) NOT NULL, time VARCHAR(30) NOT NULL);
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,29 +1,38 @@
 require 'peep'
+require 'database_helpers'
 
 describe Peep do
+
   describe '.all' do
     it 'returns the list of peeps' do
       connection = PG.connect(dbname: 'chitter_test')
 
       # add the test data
-      Peep.create(message:'peep test 1')
+      peep = Peep.create(message:'peep test 1')
       Peep.create(message:'peep test 2')
       Peep.create(message:'peep test 3')
 
       peeps = Peep.all
 
-      expect(peeps).to include('peep test 1')
-      expect(peeps).to include('peep test 2')
-      expect(peeps).to include('peep test 3')
+      expect(peeps.length).to eq(3)
+      expect(peeps.first).to be_a Peep
+      expect(peeps.first.id).to eq peep.id
+      expect(peeps.first.message).to eq('peep test 1')
 
     end
   end
 
   describe '.create' do
     it 'creates a new peep' do
-      Peep.create(message: 'test peep message')
+      #time = Time.now
+      peep = Peep.create(message: 'test peep message')
+      persisted_data = persisted_data(id: peep.id)
 
-      expect(Peep.all).to include('test peep message')
+      expect(peep).to be_a Peep
+      expect(peep.id).to eq(persisted_data['id'])
+      expect(peep.message).to eq('test peep message')
+      #expect(peep['message']).to eq('test peep message')
+      #expect(peep['time']).to eq(time)
     end
   end
 end
