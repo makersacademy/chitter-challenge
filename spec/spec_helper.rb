63dae99e6a3f4903488202456d94df2129eb5cad
Sophia Bell <Kittaru87@gmail.com>
2020-03-15 17:16:40 +0000

    again with the travis attempts

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,8 +13,6 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
