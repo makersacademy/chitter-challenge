1a4fae4b2c22ebff96c365fe3cd12b5f2f2a2d65
srikaanthtb <srikaanth.t.b@gmail.com>
2020-03-16 11:43:05 +0000

    chitter challenge

---
+++ lib/database_connection.rb
@@ -0,0 +1,15 @@
+require 'pg'
+
+class DatabaseConnection
+  def self.setup(dbname)
+    @connection = PG.connect(dbname: dbname)
+  end
+
+  def self.connection
+    @connection
+  end
+
+  def self.query(sql)
+    @connection.exec(sql)
+  end
+end
