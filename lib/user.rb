63d05726facaa5c55221efec76092148242a778a
Edward Phillips <edd0ddi0@gmail.com>
2020-04-10 13:37:23 +0100

    test - Users: signing up: should be able to tell me the details of the user - failing

--- lib/user.rb
+++ lib/user.rb
@@ -2,5 +2,8 @@ class User
   attr_reader :username, :password, :email_address
 
   def initialize(name: , pw: , email: )
+    @username = name
+    @password = pw
+    email_address = email
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,7 +2,13 @@ require './lib/user'
 describe User do
   context "signing up: " do
     it " should be able to create a new user with email address, username & password" do
-    expect { User.new(name: 'eddoddo', pw: 'Secure_PW', email:'edd0ddi0@gmail.com') }.not_to raise_error
+    expect { User.new(name: 'eddoddo', pw: 'Secure_PW', email:'example@examples.com') }.not_to raise_error
+    end
+    it " should be able to tell me the details of the user" do
+      eddo = User.new(name: 'eddoddo', pw: 'Secure_PW', email:'example@examples.com')
+      expect(eddo.username).to eq('eddoddo')
+      expect(eddo.password).to eq('Secure_PW')
+      expect(eddo.email_address).to eq('example@examples.com')
     end
   end
 end
