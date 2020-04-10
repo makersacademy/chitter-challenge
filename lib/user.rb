5af46d868ec3b65841c0259004706f0bf60fef7e
Edward Phillips <edd0ddi0@gmail.com>
2020-04-10 13:24:04 +0100

    u nit test - User: Signing up: should be able to create a new user with email address, username & password - passing

---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+class User
+  attr_reader :username, :password, :email_address
+
+  def initialize(name: , pw: , email: )
+  end
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,7 +2,7 @@ require './lib/user'
 describe User do
   context "signing up: " do
     it " should be able to create a new user with email address, username & password" do
-    expect { User.new(name: 'eddoddo',pw: 'Secure_PW', email:'edd0ddi0@gmail.com') }.not_to raise_error
+    expect { User.new(name: 'eddoddo', pw: 'Secure_PW', email:'edd0ddi0@gmail.com') }.not_to raise_error
     end
   end
 end
