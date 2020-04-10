1e2911cc4bca0d13067a8d494d9bf7715972448b
Alex L <macbookpro@macbooks-mbp.home>
2020-04-10 09:51:58 +0100

    add pg, sinatra, capybara gem & set up chitter.rb with chitter class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,4 @@
+require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
@@ -10,8 +11,11 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'chitter.rb')
+
+Capybara.app = Chitter
