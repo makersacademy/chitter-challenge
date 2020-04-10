cedcbf08653385422b857e1c279d4d1e614c4c09
Faye Carter <fayecarter333@gmail.com>
2020-04-10 19:15:21 +0100

    tests - Peep.create and Peep.all now working

--- lib/peep.rb
+++ lib/peep.rb
@@ -12,4 +12,15 @@ class Peep
     connection.exec "INSERT INTO peeps(peep, peeper) VALUES('#{peep}', 'none')"
   end
 
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    
+    result = connection.exec "SELECT * FROM peeps"
+    result.map { |peep| peep['peep'] }
+  end
+
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -5,7 +5,15 @@ describe Peep do
   describe '.create' do
     it 'A peep can be created' do
       Peep.create("First Peep")
-      expect(Peep.peeps).to include "First Peep"
+      expect(Peep.all).to include "First Peep"
+    end
+  end
+
+  describe '.all' do
+    it 'peeps can be views' do
+      Peep.create("First Peep")
+      Peep.create("Second Peep")
+      expect(Peep.all).to eq ["First Peep", "Second Peep"]
     end
   end
 
