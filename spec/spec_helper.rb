f451d43cb21a023360c750037858b91e31833d55
Harry Turnbull <hturnbull@live.co.uk>
2020-04-10 23:15:47 +0100

    added gems and capybara config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,17 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
+# require app.rb
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# testing gems
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# configure capybara app
+Capybara.app = Chitter
+
 RSpec.configure do |config|
   config.after(:suite) do
     motivation = [
@@ -16,6 +27,7 @@ RSpec.configure do |config|
       'Focus on the simplest problem!',
       'Only change code to pass tests!',
     ].sample
+    puts
     puts "\e[33m#{motivation}\e[0m"
   end
 end
