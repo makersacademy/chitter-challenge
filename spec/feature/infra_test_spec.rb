150fad1a929ff97659a619f0a3e6945ac8c84f75
Alex Lewis <alexlewis374@gmail.com>
2020-04-10 12:03:59 +0100

    fix merge conflicts

--- spec/feature/infra_test_spec.rb
+++ spec/feature/infra_test_spec.rb
@@ -3,11 +3,5 @@ feature "Home page loads" do
   scenario "has content on the home page" do
     visit '/'
     expect(page).to have_content "Welcome to Chitter"
-=======
-feature "test infrastructure working" do
-  scenario "has content on the home page" do
-    visit '/'
-    expect(page).to have_content "Hello world"
->>>>>>> ea38a010acdb864682f138155a8a1e3ee542ca8d
   end
 end
