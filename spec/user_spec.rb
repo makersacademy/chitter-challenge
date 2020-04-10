86d6a4af0d88168e5e7f4c281be087f03129be69
Edward Phillips <edd0ddi0@gmail.com>
2020-04-10 13:22:11 +0100

    unit test - User: Signing up: should be able to create a new user with email address, username and password - failing

---
+++ spec/user_spec.rb
@@ -0,0 +1,8 @@
+require './lib/user'
+describe User do
+  context "signing up: " do
+    it " should be able to create a new user with email address, username & password" do
+    expect { User.new(name: 'eddoddo',pw: 'Secure_PW', email:'edd0ddi0@gmail.com') }.not_to raise_error
+    end
+  end
+end
