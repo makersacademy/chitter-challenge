8c68a44c165ed600416c613780a34888e561d77d
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-10 10:25:09 +0100

    Refactor peep list to use database accessed from Peep class

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,5 +1,10 @@
+require 'pg'
+
 class Peep
   def self.all
-    ['First peep', 'Second peep']
+    database = ENV['ENVIRONMENT'] == 'test' ? 'chitter_test' : 'chitter'
+    conn = PG.connect(dbname: database)
+    result = conn.exec("SELECT * FROM peeps;")
+    result.map { |row| row['text']}
   end
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,12 +1,17 @@
 require 'peep'
+require 'pg'
 
 describe Peep do
   describe '.all' do
     it 'returns a list of all peeps' do
+      conn = PG.connect(dbname: 'chitter_test')
+      conn.exec("INSERT INTO peeps (text, time) VALUES ('My first peep', NOW());")
+      conn.exec("INSERT INTO peeps (text, time) VALUES ('My second peep', NOW());")
+      conn.close
       peeps = Peep.all
       expect(peeps.length).to eq 2
-      expect(peeps[0]).to eq 'First peep'
-      expect(peeps[1]).to eq 'Second peep' 
+      expect(peeps[0]).to eq 'My first peep'
+      expect(peeps[1]).to eq 'My second peep' 
     end
   end
 end
