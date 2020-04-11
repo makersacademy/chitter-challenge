410c612241eb661d46a254d2ee26339b707a3012
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-11 13:12:36 +0100

    Implement all_for_maker method on Tag class

--- lib/tag.rb
+++ lib/tag.rb
@@ -17,4 +17,12 @@ class Tag
     peep = Peep.find_by_id(peep_id)
     Tag.new(result[0]['id'], maker, peep)
   end
+
+  def self.all_for_maker(id)
+    DBConnection.connect
+    result = DBConnection.run_query("SELECT * FROM tags WHERE maker_id = #{id}")
+    DBConnection.disconnect
+    
+    result.map { |row| Tag.new(row['id'], Maker.find_by_id(row['maker_id']), Peep.find_by_id(row['peep_id'])) }
+  end
 end
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -16,4 +16,19 @@ describe Tag do
       expect(tag_in_db[0]['peep_id']).to eq tag.peep.id
     end
   end
+
+  describe '.all_for_maker' do
+    it 'returns all tags tagging the maker with the specified id' do
+      maker = Maker.create('Phil', 'Squirrel', 'here@there.com', '1234')
+      peep = Peep.create('A new peep, tagging #Squirrel', Time.now, maker.id)
+      Tag.create(maker.id, peep.id)
+      Tag.create(maker.id, peep.id)
+
+      tags = Tag.all_for_maker(maker.id)
+
+      expect(tags.length).to eq 2
+      expect(tags[0].maker.id).to eq maker.id
+      expect(tags[0].peep.id).to eq peep.id
+    end
+  end 
 end
