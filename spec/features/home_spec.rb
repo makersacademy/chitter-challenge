1505655537484d5c56af39ab0c8de025c7cea8be
Faye Carter <fayecarter333@gmail.com>
2020-04-11 11:15:43 +0100

    test - Update to navigation - add button, failing

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,7 +1,7 @@
 feature 'User can create a peep' do
   
   scenario 'User can enter a peep' do
-    visit '/add'
+    click_button 'add'
     fill_in 'message', with: 'This is the first Peep!'
     click_button 'Peep'
     expect(page).to have_content 'This is the first Peep!'
