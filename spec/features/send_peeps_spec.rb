50fc81ea8daf741ed26bd54113203a4ac6f139df
Harry Turnbull <hturnbull@live.co.uk>
2020-04-11 14:03:52 +0100

    added for test to be back at homepage

--- spec/features/send_peeps_spec.rb
+++ spec/features/send_peeps_spec.rb
@@ -1,9 +1,11 @@
 feature 'Sending a Peep' do
-  scenario 'user can send a peep and see it' do
+  scenario 'user can send a peep and see it back on the homepage' do
     visit '/'
     click_on 'Compose Peep'
     fill_in 'content', with: 'This is a test peep'
+    click_on 'Peep'
 
+    expect(current_path).to eq '/'
     expect(page).to have_content 'This is a test peep'
   end
 end
