8e1a6f5db71f8a45175c72fd375679e3a26c9ffa
Harry Turnbull <hturnbull@live.co.uk>
2020-04-11 13:46:50 +0100

    R: Send Peep feature

---
+++ spec/features/send_peeps_spec.rb
@@ -0,0 +1,9 @@
+feature 'Sending a Peep' do
+  scenario 'user can send a peep and see it' do
+    visit '/'
+    click_on 'Send a Peep'
+    fill_in 'content', with: 'This is a test peep'
+
+    expect(page).to have_content 'This is a test peep'
+  end
+end
