1fccc6a097431c153a7a7bba04f4352289d5dd18
Alex Lewis <alexlewis374@gmail.com>
2020-04-11 12:34:54 +0100

    User can send peep and display on home page - tests pass

--- spec/features/send_peep_spec.rb
+++ spec/features/send_peep_spec.rb
@@ -7,7 +7,7 @@ feature "User can send a peep" do
 
   scenario "User can type their peep, submit and view it" do
     visit '/compose_peep'
-    fill_in 'peep_text', with: "Sunny sunny day!"
+    fill_in :peep_text, with: "Sunny sunny day!"
     click_button "Submit Peep"
 
     expect(page).to have_content "Sunny sunny day!"
