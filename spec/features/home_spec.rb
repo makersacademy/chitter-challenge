a573ac925ac4a1b214e47a5216ac51cee7bdda1f
Faye Carter <fayecarter333@gmail.com>
2020-04-11 11:33:13 +0100

    test - Tests updated to request date visability - failing

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,4 +1,4 @@
-feature 'User can create a peep' do
+feature 'Homepage loads' do
   
   scenario 'User can enter a peep' do
     visit '/home'
@@ -8,13 +8,13 @@ feature 'User can create a peep' do
     expect(page).to have_content 'This is the first Peep!'
   end
 
-  scenario 'User can enter a peep' do
+  scenario 'Home displays peeps' do
     peep_1 = Peep.create("Peep 1", '2020-04-09 10:52:57.960784')
     peep_2 = Peep.create("Peep 2", '2020-04-10 10:52:57.960784')
     visit '/add'
     fill_in 'message', with: 'This is the Newest Peep!'
     click_button 'Peep'
-    expect(page).to have_content 'This is the Newest Peep! Peep 2 Peep 1'
+    expect(page).to have_content 'This is the Newest Peep! 2020-04-11 Peep 2 2020-04-10 Peep 1 2020-04-09'
   end
 
 end
