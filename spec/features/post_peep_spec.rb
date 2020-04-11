91bc2fad19e6e182324371c82544c48bfa1c94c4
Faye Carter <fayecarter333@gmail.com>
2020-04-11 14:53:27 +0100

    Test - Peep.create test updated to include peeper - passing

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -3,8 +3,8 @@ require 'peep'
 feature 'Homepage loads with peeps' do
   
   scenario 'home has peeps' do
-    peep_1 = Peep.create("Peep 1", '2020-04-10 10:52:57.960784')
-    peep_2 = Peep.create("Peep 2", '2020-04-11 10:52:57.960784')
+    peep_1 = Peep.create("Peep 1", '2020-04-10 10:52:57.960784', 'Bruce123')
+    peep_2 = Peep.create("Peep 2", '2020-04-11 10:52:57.960784', 'Bruce123')
     visit '/home'
     expect(page).to have_content 'Peep 2 2020-04-11 Peep 1 2020-04-10'
   end
