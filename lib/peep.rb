8a5d3e59a91bd46690693e3406839a0c50352688
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 00:40:05 +0100

    stripped out old ode and replaced with activerecord

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,41 +1,12 @@
 require 'pg'
 require 'sinatra/activerecord'
 
+# disable logging
+# old_logger = ActiveRecord::Base.logger
+ActiveRecord::Base.logger = nil
 
-class Peep # < ApplicationRecord
-  def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    result = connection.exec('SELECT * FROM peeps ORDER BY time DESC;')
-    result.map do |peep|
-      Peep.new(id: peep['id'], content: peep['content'], time: peep['time'])
-    end
-  end
-
-  def self.create(content:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    result = connection.exec("INSERT INTO peeps (content)
-                                   VALUES ('#{content}')
-                                RETURNING id, content, time;")
-    Peep.new(id: result[0]['id'], content: result[0]['content'], time: result[0]['time'])
-  end
-
-  def initialize(id:, content:, time:)
-    @id = id
-    @content = content
-    @time = time
-  end
-
-  attr_reader :id, :content
-
+class Peep < ActiveRecord::Base
   def time
-    Time.parse(@time).strftime('%b %e %I:%M%P')
+    self.created_at.strftime('%b %e %I:%M%P')
   end
 end
