54fcce5d5de8cd7053fb9c81a4a7b00aa4f5fe68
Alexa <alexakearns@outlook.com>
2020-04-11 17:14:29 +0100

    Test database set up and tests pass - green

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -4,6 +4,12 @@ describe Peep do
 
   describe '.all' do
     it 'returns all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (content) VALUES ('900 People die from Coronavirus');")
+      connection.exec("INSERT INTO peeps (content) VALUES ('Lockdown is lifted');")
+      connection.exec("INSERT INTO peeps (content) VALUES ('Coronavirus is over!');")
+      
       peeps = Peep.all
       
       expect(peeps).to include("900 People die from Coronavirus")
