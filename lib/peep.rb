724aae13d02ed7bc054741a95c895dadc2958582
Alex Lewis <alexlewis374@gmail.com>
2020-04-11 13:28:44 +0100

    test -display a list of all the Peeps -pass

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,4 +1,9 @@
 class Peep
   def self.all
+    [
+      "I miss the football!",
+      "Time for some socially distanced excercise",
+      "I think I deserve some chocolate"
+    ]
   end
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -2,10 +2,15 @@ require 'peep'
 
 describe Peep do
   it "Can call #all method on the Peep class" do
-    expect(Self).to respond_to :all
+    expect(Peep).to respond_to :all
   end
+end
 
-  it "shows all the peeps" do
-    expect(self.all).to include "I miss the football!"
+describe ".all" do
+  it "displays a list of the peeps" do
+    peeps = Peep.all
+    expect(peeps).to include "I miss the football!"
+    expect(peeps).to include "Time for some socially distanced excercise"
+    expect(peeps).to include "I think I deserve some chocolate"
   end
 end
