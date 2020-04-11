09d570dbc853d1a011c17491dea5141eb97140ef
Faye Carter <fayecarter333@gmail.com>
2020-04-11 12:12:46 +0100

    test - User can log in from homescreen

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -8,6 +8,16 @@ feature 'Homepage loads' do
     expect(page).to have_content 'This is the first Peep!'
   end
 
+  scenario 'User can Login' do
+    visit '/home'
+    click_button 'Login'
+    ill_in 'username', with: "Bruce"
+    fill_in 'password', with: "password"
+    click_button 'log_in'
+    expect(page).to have_content "Peeper: Bruce"
+    expect(page).not_to have_button "Login"
+  end
+
   scenario 'Home displays peeps' do
     peep_1 = Peep.create("Peep 1", '2020-04-09 10:52:57.960784')
     peep_2 = Peep.create("Peep 2", '2020-04-10 10:52:57.960784')
