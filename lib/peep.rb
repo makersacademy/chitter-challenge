39eb28b5bbac39d9647e1ae7e7aa222a8f4f0a3f
Faye Carter <fayecarter333@gmail.com>
2020-04-11 08:35:06 +0100

    refactor - Peep.all refactored to use database connection

--- lib/peep.rb
+++ lib/peep.rb
@@ -14,13 +14,7 @@ class Peep
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    
-    result = connection.exec "SELECT * FROM peeps"
+    result = DbConnection.query("SELECT * FROM peeps")
     result.map { |peep| peep['peep'] }
   end
 
