cc3e896ab6f66af76a94273275e8f119aa7ef172
Faye Carter <fayecarter333@gmail.com>
2020-04-11 08:11:33 +0100

    test - Test to move databse connection to a class - failing

---
+++ spec/db_connection_spec.rb
@@ -0,0 +1,13 @@
+require 'database_connection'
+
+describe DatabaseConnection do
+  
+  describe '.setup' do
+    it 'sets up a connection to a database through PG' do
+      expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
+
+      DatabaseConnection.setup('bookmark_manager_test')
+    end
+  end
+
+end
