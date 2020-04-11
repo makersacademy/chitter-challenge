26b59971d802577ccd51576ff760da0ddfe4a6e5
Faye Carter <fayecarter333@gmail.com>
2020-04-11 12:21:12 +0100

    test - signup page - failing

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,13 @@
+feature 'Signup Page Loads' do
+
+  scenario 'Page loads with content' do
+    visit '/signup' 
+    fill_in 'name', with: "Bruce"
+    fill_in 'email', with: "Bruce@Bruceiscool.com"
+    fill_in 'username', with: "Bruce123"
+    fill_in 'password', with: "password"
+    click_button 'sign_up'
+    expect(page).to have_content "Login to your Peeps"
+  end
+
+end
