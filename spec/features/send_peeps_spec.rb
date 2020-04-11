80d3ea8fc9617c519a865e59824de169d2b64dcc
Harry Turnbull <hturnbull@live.co.uk>
2020-04-11 13:48:18 +0100

    reworded to use Compose Peep

--- spec/features/send_peeps_spec.rb
+++ spec/features/send_peeps_spec.rb
@@ -1,7 +1,7 @@
 feature 'Sending a Peep' do
   scenario 'user can send a peep and see it' do
     visit '/'
-    click_on 'Send a Peep'
+    click_on 'Compose Peep'
     fill_in 'content', with: 'This is a test peep'
 
     expect(page).to have_content 'This is a test peep'
