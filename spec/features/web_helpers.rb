d75366347305f3570c706a24d037d4a2457fe950
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-11 12:53:17 +0100

    Remove unused log_in web helper

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -6,11 +6,3 @@ def sign_up
   fill_in 'password', with: '1234'
   click_button 'Submit'
 end
-
-def log_in
-  click_button 'Log out'
-  click_button 'Log in'
-  fill_in 'username', with: 'Squirrel'
-  fill_in 'password', with: '1234'
-  click_button 'Log in'
-end
