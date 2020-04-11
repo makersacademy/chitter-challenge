03908af1fa52049bc35516e8e54221ec559e4328
Faye Carter <fayecarter333@gmail.com>
2020-04-11 08:37:13 +0100

    refactor - Peep.create refactored to use database connection

--- lib/peep.rb
+++ lib/peep.rb
@@ -3,14 +3,8 @@ require 'db_connection'
 
 class Peep
 
-  def self.create(peep)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    
-    connection.exec "INSERT INTO peeps(peep, peeper) VALUES('#{peep}', 'none')"
+  def self.create(peep)    
+    DbConnection.query("INSERT INTO peeps(peep, peeper) VALUES('#{peep}', 'none')")
   end
 
   def self.all
