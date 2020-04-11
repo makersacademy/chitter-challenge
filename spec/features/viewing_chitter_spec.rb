c1c23b946f4c52f3bbc462efcada473506e2afe9
Alexa <alexakearns@outlook.com>
2020-04-11 14:58:46 +0100

    create index.erb for '/' route. add peep feature - red

--- spec/features/viewing_chitter_spec.rb
+++ spec/features/viewing_chitter_spec.rb
@@ -1,6 +1,14 @@
 feature 'Viewing peeps' do
   scenario 'visit the index page' do
     visit('/')
-    expect(page).to have_content "Hello World"
+    expect(page).to have_content "Chitter"
+  end
+
+  scenario 'user can see peeps' do
+    visit('/')
+    click_button "Add Peep"
+
+    expect(page).to have_content "What a day to peep!"
+    expect(page).to have_content "Happy Easter"
   end
 end
