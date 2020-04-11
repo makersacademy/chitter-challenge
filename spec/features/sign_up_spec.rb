7aa89c92e15bb0d3f130717b8b48b8d3e4fe0f6a
Phil Vigus <philipvigus@yahoo.co.uk>
2020-04-11 09:59:49 +0100

    Add missing feature tests for bad sign-up and log-in

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,4 +8,26 @@ feature 'you can sign up to chitter' do
     click_button 'Submit'
     expect(page).to have_content 'Hello Squirrel!'
   end
+
+  scenario 'I should be notified when my username is already taken' do
+    sign_up
+    visit '/users/new'
+    fill_in 'name', with: 'Phil'
+    fill_in 'username', with: 'Squirrel'
+    fill_in 'email', with: 'somewhere@there.com'
+    fill_in 'password', with: '1234'
+    click_button 'Submit'
+    expect(page).to have_content 'Your username or email are already in use. Please try again'
+  end
+
+  scenario 'I should be notified when my email is already taken' do
+    sign_up
+    visit '/users/new'
+    fill_in 'name', with: 'Phil'
+    fill_in 'username', with: 'Fox'
+    fill_in 'email', with: 'here@there.com'
+    fill_in 'password', with: '1234'
+    click_button 'Submit'
+    expect(page).to have_content 'Your username or email are already in use. Please try again'
+  end
 end
