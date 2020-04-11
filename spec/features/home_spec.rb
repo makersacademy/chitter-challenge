391fe5dcda706aa0b6e1c547f72d5dea60c1b448
Faye Carter <fayecarter333@gmail.com>
2020-04-11 10:36:03 +0100

    test - Peeps display most recent first - failing

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -7,4 +7,13 @@ feature 'User can create a peep' do
     expect(page).to have_content 'This is the first Peep!'
   end
 
+  scenario 'User can enter a peep' do
+    peep_1 = Peep.create("Peep 1")
+    peep_2 = Peep.create("Peep 2")
+    visit '/add'
+    fill_in 'message', with: 'This is the Newest Peep!'
+    click_button 'Peep'
+    expect(page).to have_content 'This is the Newest Peep! Peep 2 Peep 1'
+  end
+
 end
