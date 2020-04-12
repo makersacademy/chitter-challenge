f43707eea6d77a7a62128f1674449813334125b0
tommyrharper <tommyrharper@gmail.com>
2020-04-12 19:46:56 +0100

    old depreciated tests and code removed

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -2,12 +2,6 @@ require 'pg'
 
 class Peeps
 
-  attr_reader :messages
-
-  def initialize
-    @messages = []
-  end
-
   def self.all
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_test')
@@ -27,18 +21,6 @@ class Peeps
     end
 
     connection.exec("INSERT INTO peeps (peep) VALUES('#{peep}');")
-  end
-
-  def self.create
-    @peeps = Peeps.new
-  end
-
-  def self.instance
-    @peeps
-  end
-  
-  def add(peep)
-    @messages << peep
   end
 
 end
--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -2,14 +2,14 @@ require 'peeps'
 
 describe Peeps do
 
-  it 'peeps to be able to store a peep' do
-    peeps = Peeps.new
-    peeps.add('1st peep')
-    peeps.add('2nd peep')
+  # it 'peeps to be able to store a peep' do
+  #   peeps = Peeps.new
+  #   peeps.add('1st peep')
+  #   peeps.add('2nd peep')
 
-    expect(peeps.messages).to include '1st peep'
-    expect(peeps.messages).to include '2nd peep'
-  end
+  #   expect(peeps.messages).to include '1st peep'
+  #   expect(peeps.messages).to include '2nd peep'
+  # end
 
   it 'stores in the database a new peep' do
     Peeps.add("why hello there")
