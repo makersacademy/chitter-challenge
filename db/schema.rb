afb376dd6fa565d62a4effabd74634b2718fb6fa
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 14:12:47 +0100

    password to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_12_105431) do
+ActiveRecord::Schema.define(version: 2020_04_12_130224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 2020_04_12_105431) do
     t.string "last_name"
     t.string "email"
     t.string "user_name"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
