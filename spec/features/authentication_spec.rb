f6c6b888039d7c23caae79b9cfe2d462b34703a1
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 15:37:46 +0100

    fixed test

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,14 +1,16 @@
 feature 'authentication' do
-  it 'a user can sign in' do
+  it 'a user can log in' do
     # Create a test user
     user_create_dave
+
     # Then sign in as them
+    visit '/'
     click_on 'Log in'
-    fill_in(:email, with: 'test@example.com')
+    fill_in(:email, with: 'davedude@example.com')
     fill_in(:password, with: 'password123')
     click_button('Log in')
 
     expect(current_path).to eq '/'
-    expect(page).to have_content '@'
+    expect(page).to have_content '@dave'
   end
 end
