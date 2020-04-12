8d233836d3426f16bb6f8b098f4b976b7ce01fc5
Harry Turnbull <hturnbull@live.co.uk>
2020-04-13 00:31:48 +0100

    Updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
-5.times do |i|
-  Peep.create(content: "Test peep #{i}")
-end
+user1 = User.create(
+  first_name: 'Dave',
+  last_name: 'Dude',
+  email: 'davedude@example.com',
+  user_name: 'davedude',
+  password: 'password123'
+)
+
+user2 = User.create(
+  first_name: 'Jim',
+  last_name: 'Guy',
+  email: 'jimguy@example.com',
+  user_name: 'jimguy',
+  password: 'password123'
+)
+
+Peep.create(content: "I love to peep!", user_id: user1.id)
+Peep.create(content: "My name is Dave and I am a cool dude!", user_id: user1.id)
+Peep.create(content: "Peeping is great!", user_id: user2.id)
+Peep.create(content: "I am Jim and I am an alright guy.", user_id: user2.id)
