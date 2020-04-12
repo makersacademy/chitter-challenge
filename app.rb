1a981b02b06d20567867cf5759bbc39008361d69
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 15:37:59 +0100

    sessions routes

--- app.rb
+++ app.rb
@@ -39,4 +39,14 @@ class Chitter < Sinatra::Base
     redirect '/'
   end
 
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+    user = User.find_by(email: params[:email])
+    auth = user.authenticate(params[:password])
+    session[:user_id] = auth.id
+    redirect '/'
+  end
 end
