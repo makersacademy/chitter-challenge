9c76a8079705876771c07d62d150f0b6d8742461
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 19:25:09 +0100

    R: email already in use

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -6,4 +6,21 @@ feature 'Registration' do
     expect(page).to have_content '@davedude'
     expect(page).to_not have_content 'Sign up'
   end
+
+  scenario 'Attempting to sign up with an email that already exists will raise a notice' do
+    user_create_dave
+
+    visit '/'
+    click_on 'Sign up'
+    
+    fill_in('first_name', with: 'Jim')
+    fill_in('last_name', with: 'Guy')
+    fill_in('email', with: 'davedude@example.com')
+    fill_in('user_name', with: 'jimguy')
+    fill_in('password', with: 'password123')
+    click_on 'Submit'
+
+    expect(current_path).to eq '/users/new'
+    expect(page).to have_content 'There is already a user with that email'
+  end
 end
