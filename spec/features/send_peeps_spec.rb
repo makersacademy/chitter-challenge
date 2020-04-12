98aa9abb833652ff1f3a5c43ed54034e0793117d
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 14:34:47 +0100

    R: user can only peep when signed in

--- spec/features/send_peeps_spec.rb
+++ spec/features/send_peeps_spec.rb
@@ -1,6 +1,15 @@
+require './spec/capybara_helpers'
+
 feature 'Sending a Peep' do
-  scenario 'user can send a peep and see it back on the homepage' do
+  scenario 'compose peep button not present when user not signed in' do
     visit '/'
+
+    expect(page).to_not have_content 'Compose Peep'
+  end
+
+  scenario 'signed in user can send a peep and see it back on the homepage' do
+    dave_sign_up
+    
     click_on 'Compose Peep'
     fill_in 'content', with: 'This is a test peep'
     click_on 'Peep'
