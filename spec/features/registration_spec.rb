f94cbaa7cf5b135ae2a8d9a81278564100b84bfc
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 14:34:35 +0100

    RF to use capybara_helpers

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,16 +1,9 @@
+require './spec/capybara_helpers'
+
 feature 'Registration' do
   scenario 'a user can sign up' do
-    visit '/'
-    click_on 'Sign up'
+    dave_sign_up
     
-    fill_in 'first_name', with: 'Dave'
-    fill_in 'last_name', with: 'Dude'
-    fill_in 'email', with: 'davedude@example.com'
-    fill_in 'user_name', with: 'davedude'
-    fill_in 'password', with: 'password123'
-
-    click_on 'Submit'
-
     expect(current_path).to eq '/'
     expect(page).to have_content '@davedude'
     expect(page).to_not have_content 'Sign up'
