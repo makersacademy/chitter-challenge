ecd5499069f41246b772b9267be7c27c1e53c597
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 12:06:52 +0100

    added migration for users table

---
+++ db/migrate/20200412105431_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :user_name
+      t.string :password
+      t.timestamps
+    end
+  end
+end
