af91ac56585df6aa079a771f5aeb21a7c6e314ae
Harry Turnbull <hturnbull@live.co.uk>
2020-04-12 19:26:15 +0100

    R: username already in use

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -23,4 +23,21 @@ feature 'Registration' do
     expect(current_path).to eq '/users/new'
     expect(page).to have_content 'There is already a user with that email'
   end
+
+  scenario 'Attempting to sign up with a username that already exists will raise a notice' do
+    user_create_dave
+
+    visit '/'
+    click_on 'Sign up'
+    
+    fill_in('first_name', with: 'Jim')
+    fill_in('last_name', with: 'Guy')
+    fill_in('email', with: 'jimguy@example.com')
+    fill_in('user_name', with: 'davedude')
+    fill_in('password', with: 'password123')
+    click_on 'Submit'
+
+    expect(current_path).to eq '/users/new'
+    expect(page).to have_content 'There is already a user with that email or username'
+  end
 end
