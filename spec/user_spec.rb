f90f559c729c9ba2287cf6097eb6c66efeaeed29
Alexa <alexakearns@outlook.com>
2020-04-13 21:48:11 +0100

    test for new user with username and passwork - red

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,4 +2,12 @@ require 'user'
 
 describe User do
 
+  let (:johnsmith) {User.new(username: 'John Smith', password: "password123")}
+
+  it 'creates new user with username and password' do
+    
+    expect(subject.username).to eq 'John Smith'
+    expect(subject.password).to eq 'password123'
+  end
+
 end
