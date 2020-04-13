5c4cb510df3e7915e348e6b8812498f2180f5fdc
Faye Carter <fayecarter333@gmail.com>
2020-04-13 15:37:44 +0100

    travis update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require_relative './setup_test_database'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
@@ -26,5 +27,4 @@ end
 
 ENV['ENVIRONMENT'] = 'test'
 
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
 Capybara.app = Chitter
