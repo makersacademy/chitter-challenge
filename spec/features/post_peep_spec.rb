6717fc7afb19f92efeefc53f26de4215e52d9c14
Angelica <Angelica137@users.noreply.github.com>
2020-04-13 20:56:14 +0100

    post peep feature test, red

---
+++ spec/features/post_peep_spec.rb
@@ -0,0 +1,9 @@
+feature 'Post peep' do
+  scenario 'A user wants to post to Chitter' do
+    visit('/')
+    fill_in('peep', with:'My first peep')
+    click_button('Post')
+
+    expect(page).to have_content 'My first peep'
+  end
+end
