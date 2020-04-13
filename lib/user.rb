ca7600f2ea10ce443de94596c460adbfdda1809c
tommyrharper <tommyrharper@gmail.com>
2020-04-13 15:18:20 +0100

    passwords now encrypted

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,15 @@
 require_relative 'database_connection'
+require 'bcrypt'
 
 class User
 
   attr_reader :id, :email, :username, :name
 
   def self.create(email:, password:, username:, name:)
+    encrypted_password = BCrypt::Password.create(password)
+
     result = DatabaseConnection.query("INSERT INTO users (email, password, username, name)
-    VALUES('#{email}', '#{password}', '#{username}', '#{name}') 
+    VALUES('#{email}', '#{encrypted_password}', '#{username}', '#{name}') 
     RETURNING id, email, password, username, name;")
 
     User.new(id: result[0]['id'], email: result[0]['email'], username: result[0]['username'], 
@@ -15,7 +18,7 @@ class User
 
   def self.find(id)
     return nil unless id
-    
+
     result = DatabaseConnection.query("SELECT * FROM users WHERE id = #{id}")
     User.new(id: result[0]['id'], email: result[0]['email'], username: result[0]['username'], 
       name: result[0]['name'])
