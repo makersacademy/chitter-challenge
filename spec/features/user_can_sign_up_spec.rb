62dea060e6193e8c78f7aefb5f330c3ac5d5a88c
lilawalker <lilawlker@gmail.com>
2020-04-19 17:06:07 +0100

    user is prompted for sign up details

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -4,4 +4,12 @@ feature 'sign up' do
     click_button 'Sign Up'
     expect(current_path).to eq '/register'
   end
+
+  scenario 'user is prompted to provide username, email and password' do
+    visit '/register'
+    expect(page).to have_content 'Username'
+    expect(page).to have_content 'Email'
+    expect(page).to have_content 'Password'
+    expect(page).to have_button 'Register'
+  end
 end
