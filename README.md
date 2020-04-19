d313cee0c0d3837a422a95f193969fdd1eed7fce
lilawalker <lilawlker@gmail.com>
2020-04-19 16:34:17 +0100

    create and test index

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 Chitter Challenge
 =================
+
+## User Stories
+
+```
+As a user
+So that I can post messages on Chitter as me
+I want to sign up for Chitter
+```
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,6 +1,6 @@
 feature "Infrastructure Test" do
   scenario "can run app and check page content" do
     visit '/'
-    expect(page).to have_content "Hello world"
+    expect(page).to have_content "Welcome to Chitter!"
   end
 end
