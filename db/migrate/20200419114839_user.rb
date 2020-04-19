b76f69a08cc420b4bb77cb392360f17343ef66c4
lilawalker <lilawlker@gmail.com>
2020-04-19 14:28:30 +0100

    setup user database

---
+++ db/migrate/20200419114839_user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
+      t.string :username,           null: false
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,24 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_19_114839) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "username", null: false
+  end
+
+end
