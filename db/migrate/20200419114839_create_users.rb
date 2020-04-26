69861deb9d7154fbbf4fa437420b8759a48b4539
lilawalker <lilawlker@gmail.com>
2020-04-26 11:03:38 +0100

    Update migration class name

--- db/migrate/20200419114839_create_users.rb
+++ db/migrate/20200419114839_create_users.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Migration[6.0]
+class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :email,              null: false
