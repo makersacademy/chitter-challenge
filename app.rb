95c72f1a02ea155c7f93663778022d2990544507
Andrew Hulme <andrewhulme1993@gmail.com>
2020-05-08 16:30:43 +0100

    Adds functionality to add a singluar peep

--- app.rb
+++ app.rb
@@ -1,8 +1,26 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+
   get '/' do
     'Peeps'
+  end
+
+  get '/peeps' do
+    @peep_name = session[:name]
+    @peep_text = session[:text]
+    erb :"index"
+  end
+
+  post '/peeps' do
+    session[:name] = params[:name]
+    session[:text] = params[:text]
+    redirect '/peeps'
+  end
+
+  get '/peeps/new' do
+    erb :"peeps/new"
   end
 
   run! if app_file == $0
---
+++ spec/features/posting_peep_spec.rb
@@ -0,0 +1,22 @@
+# User Story:
+# As a Maker
+# So that I can let people know what I am doing
+# I want to post a message (peep) to chitter
+
+feature 'Posting peeps' do
+  scenario 'Posting a peep from the new page' do
+    visit('/peeps/new')
+    fill_in('text', with: 'Feeling those summer vibes today')
+    fill_in('name', with: 'Andrew')
+    click_button('Peep')
+
+    expect(current_path).to eq '/peeps'
+    expect(page).to have_content('Feeling those summer vibes today')
+  end
+
+  scenario 'Can access new post page from /peeps' do
+    visit('/peeps')
+    click_button('New Peep')
+    expect(current_path).to eq '/peeps/new'
+  end
+end
