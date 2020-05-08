363f4bba47398f49b0b1d365bdbaa2679f3b67f0
mbrad26 <mbrad26@gmail.com>
2020-05-08 19:05:25 +0100

    implemented visiting home page feature

--- app.rb
+++ app.rb
@@ -1,6 +1,14 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
-  
+
+  get '/' do
+    erb :home
+  end
+
+  get '/peeps' do
+    erb :'/peeps/index'
+  end
+
   run! if app_file == $0
 end
