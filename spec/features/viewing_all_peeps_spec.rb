bb7ac8b4f215c8ffe5c8cf0b366ba6a807f4b575
Joanne Young <joanneyoung_@hotmail.com>
2020-05-08 13:59:56 +0100

    refactor feature test to create bookmark during test

--- spec/features/viewing_all_peeps_spec.rb
+++ spec/features/viewing_all_peeps_spec.rb
@@ -1,9 +1,12 @@
 require 'spec_helper'
 
-
-
 feature 'Viewing peeps' do
   scenario 'A user can see peeps' do
+
+    Peep.create(message: "Message 1")
+    Peep.create(message: "Message 2")
+    Peep.create(message: "Message 3")
+
     visit('/peeps')
 
     expect(page).to have_content "Message 1"
