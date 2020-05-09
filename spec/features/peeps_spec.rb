1e5ed279f8cd7b7aab055c092e0812bfad7f7af7
dpwdec <declankolakowski@gmail.com>
2020-05-09 19:48:38 +0100

    user can now only add a peep if logged in

--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -1,11 +1,20 @@
 require 'orderly'
 
 feature 'peeps' do
-  scenario 'user can add a peep' do
-    visit('/peeps')
+  scenario 'user can add a peep if logged in' do
+    User.new(name: 'Dec', user_name: 'dk', email: 'dec@dec.com', password: 'pa55word').save
+    visit('/log-in')
+    fill_in('username', with: 'dk')
+    fill_in('password', with: 'pa55word')
+    click_button('log in')
     fill_in('body', with: '#waffles')
     click_button('post')
     expect(page).to have_content('#waffles')
+  end
+
+  scenario 'user cannot add a peep if not logged in' do
+    visit('/peeps')
+    expect(page).to_not have_content("What's up?")
   end
 
   scenario 'user can see peeps in chronological order' do
