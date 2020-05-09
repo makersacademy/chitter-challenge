8b6bd163cd5d29e4bc89f5292139a5788b25ab14
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-09 11:12:11 +0100

    Cleans up commented code

--- db/migrate/01-create_databases.sql
+++ db/migrate/01-create_databases.sql
@@ -1,7 +1,5 @@
--- DROP DATABASE chitter_test;
-DROP DATABASE chitter_development;
-DROP DATABASE chitter_production;
+-- DROP DATABASE chitter_development;
+-- DROP DATABASE chitter_production;
 
--- CREATE DATABASE chitter_test;
 CREATE DATABASE chitter_development;
 CREATE DATABASE chitter_production;
