371fd0652f73e98c573bf20ecca31248b34f4828
lilawalker <lilawlker@gmail.com>
2020-05-09 18:50:17 +0100

    Peeps show the date they were made

---
+++ spec/features/shows_date_of_peep_spec.rb
@@ -0,0 +1,13 @@
+feature 'date of peep' do
+
+  before do
+    @time_now = Time.parse('01/01/2020 12:12')
+    allow(Time).to receive(:now).and_return(@time_now)
+  end
+
+  scenario 'shows the date the peep was made' do
+    login
+    add_peep('This is a peep')
+    expect(first('.peep')).to have_content '01/01/2020 12:12'
+  end
+end
