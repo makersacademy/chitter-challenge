9ecf5222c54533666d525e259e2563fbc180d0bd
GarethWoodman <gareth.woodman92@gmail.com>
2020-05-09 11:18:27 +0100

    Updated spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,18 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+# Set the environment to "test"
+ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
+
+# Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# Require all the testing gems
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# Tell Capybara to talk to ChitterApp
+Capybara.app = ChitterApp
