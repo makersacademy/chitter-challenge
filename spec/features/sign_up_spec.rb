951f147dd17b4142ce502c4a0746efe4e6f9e235
dpwdec <declankolakowski@gmail.com>
2020-05-09 20:47:21 +0100

    added sign up link if not logged in

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -8,4 +8,9 @@ feature 'sign up' do
     click_button('sign up')
     expect(User.all(User.user_name => 'dk').first).to be_a_kind_of(User)
   end
+
+  scenario 'sign up link is dispalyed if user is not logged in' do
+    visit('/peeps')
+    expect(page).to have_link('Sign up')
+  end
 end
