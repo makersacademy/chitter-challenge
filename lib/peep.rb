b529b336479bbf2b71b0bba7908c4e42ab768140
Catriona <catrionamairi@blueyonder.co.uk>
2020-05-10 10:55:02 +0100

    connected databse to app

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,5 +1,9 @@
+require 'pg'
+
 class Peep
   def self.all
-    ["A lovely day", "An interesting day"]
+    connection = PG.connect(dbname: 'Chitter_Manager')
+    result = connection.exec("SELECT * FROM peeps;")
+    result.map { |peep| peep['peep'] }
   end
 end
