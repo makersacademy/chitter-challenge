c8e94269b46b7ac18ba41590b8650b7ab4a3af4c
@willhead <60652390+will-head@users.noreply.github.com>
2020-05-10 10:13:17 +0100

    Removes commented code from config/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -2,27 +2,3 @@ require 'bundler'
 Bundler.require
 
 require 'sinatra'
-
-configure :development do
-  set :database, {
-    adapter: 'postgresql',
-    encoding: 'unicode', 
-    database: 'chitter_development', 
-    # pool: 2, 
-    # username: 'your_username', 
-    # password: 'your_password'
-  }
-
-  set :show_exceptions, true
-end
-
-configure :production do
-  set :database, {
-    adapter: 'postgresql',
-    encoding: 'unicode', 
-    database: 'chitter_production', 
-    # pool: 2, 
-    # username: 'your_username', 
-    # password: 'your_password'
-  }
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_05_09_090837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.text "fullname"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
