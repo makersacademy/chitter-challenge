ee70c3105bda4cfe015d0ddd895f51a2a0c591a0
Tim <TimCPB@github.com>
2020-06-06 22:39:58 +0100

    passing 'Contains @id, @peep, @peeped_at, @peeped_by instance variables' unit test in peep_spec.rb

---
+++ lib/peep.rb
@@ -0,0 +1,12 @@
+class Peep
+
+  attr_reader :id, :peep, :peeped_at, :peeped_by
+
+  def initialize(id:, peep:, peeped_at:, peeped_by:)
+    @id = id
+    @peep = peep
+    @peeped_at = peeped_at
+    @peeped_by = peeped_by
+  end
+
+end
