5dff55a8bcf9c9acbc2e418065e86909f74ba39e
Rachael <rachaelewins@hotmail.com>
2020-06-06 17:13:11 +0100

    /chitter, peeps have the time they were made

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,16 +2,17 @@ require 'pg'
 
 class Peep
 
-  attr_reader :text
+  attr_reader :text, :time
 
-  def initialize(text)
+  def initialize(text, time)
     @text = text
+    @time = time
   end
 
   def self.all
-    rs = sql_query("SELECT text FROM peeps ORDER BY time DESC")
+    rs = sql_query("SELECT text, time FROM peeps ORDER BY time DESC")
 
-    rs.map { |peep| Peep.new(peep['text']) }
+    rs.map { |peep| Peep.new(peep['text'], peep['time']) }
   end
 
   def self.create(text)
--- spec/features/chitter_homepage_spec.rb
+++ spec/features/chitter_homepage_spec.rb
@@ -1,6 +1,10 @@
 feature '/chitter' do
   scenario 'homepage shows peeps' do
     visit '/chitter'
-    expect(page).to have_content('peep1')
+    expect(page).to have_content('peep2')
+  end
+  scenario 'peeps have time they were made' do
+    visit '/chitter'
+    expect(page).to have_content('peep1 2020-06-06 11:10:25')
   end
 end
