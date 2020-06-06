12b1ceb87cb81de71ca192777ce442a0adbd6e4d
nadinedelia <nadine.delia@yahoo.com>
2020-06-06 15:29:23 +0100

    updated controller and view, feature test passing

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require './lib/peep'
+require 'pg'
 
 class Chitter < Sinatra::Base
 
@@ -7,5 +8,20 @@ class Chitter < Sinatra::Base
     @peeps = Peep.all
     erb :index
   end
+
+  get '/peeps/new' do
+   erb :'peeps/new'
+ end
+
+  post '/peeps' do
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter_test')
+    end
+    connection.exec("INSERT INTO peeps (content) VALUES ('#{params[:content]}');")
+    redirect '/'
+  end
+
 
 end
--- spec/features/adding_peeps_spec.rb
+++ spec/features/adding_peeps_spec.rb
@@ -1,9 +1,11 @@
 feature 'Adding a new peep' do
   scenario 'A user can add a peep to the chitter feed' do
     visit('/')
+    click_on 'New Peep'
     fill_in('content', with: 'my bonnie lies over the ocean')
     click_button('Peep')
 
+    expect(current_path).to eq '/'
     expect(page).to have_content 'my bonnie lies over the ocean'
   end
 end
