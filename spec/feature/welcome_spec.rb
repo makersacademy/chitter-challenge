73cdc05ac2abd5bb215c3f8c289f03e3df81c649
Sumner1185 <alastair.sumner@yahoo.com>
2020-06-06 13:15:23 +0100

    Buttons added to add and view peeps on homepage

--- spec/feature/welcome_spec.rb
+++ spec/feature/welcome_spec.rb
@@ -4,4 +4,14 @@ feature 'welcome page' do
     visit('/')
     expect(page).to have_content "Welcome to Chitter"
   end
+
+  scenario 'it should have a link to add peep' do
+    visit('/')
+    expect(page).to have_button "Post a Peep"
+  end
+
+  scenario 'it should have a link to view peeps' do
+    visit('/')
+    expect(page).to have_button "View Peeps"
+  end
 end
