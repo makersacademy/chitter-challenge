5f6926b5cd79954abe375a1767d66fd19112aa02
Tim <TimCPB@github.com>
2020-06-06 21:51:34 +0100

    Now passing 'posts a peep' test in post_peep_spec.rb

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -5,6 +5,6 @@ feature "/post-peep" do
     visit('/post-peep')
     fill_in 'peep', with: 'my first peep'
     click_on 'Submit'
-    expect(page).to have_content("View all peeps!")
+    expect(page).to have_content("View all peeps here!")
   end
 end
