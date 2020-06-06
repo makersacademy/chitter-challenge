17f72700a214abd2b848c88e16979db9aa6e22ae
Tim <TimCPB@github.com>
2020-06-06 15:37:25 +0100

    index.erb now passes 'Displays a link to view-peeps' and 'Displays a link to post a Peep' features tests

--- app.rb
+++ app.rb
@@ -5,7 +5,15 @@ class Chitter < Sinatra::Base
   get '/' do
     erb(:index)
   end
- 
+
+  get '/post-peep' do
+    "post here"
+  end
+
+  get '/view-peeps' do
+    "View all peeps here"
+  end
+  
   run! if app_file == $PROGRAM_NAME
 
 end
--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -5,4 +5,14 @@ feature 'Chitter Homepage' do
     visit('/')
     expect(page).to have_content("Welcome to Chitter")
   end
+
+  scenario "Displays a link to view-peeps" do
+    visit('/')
+    expect(page).to have_link('Click here to view all Peeps', href: 'http://localhost:9292/view-peeps')
+  end
+
+  scenario "Displays a link to post a Peep" do
+    visit('/')
+    expect(page).to have_link('Click here to post a peep', href: 'http://localhost:9292/post-peep')
+  end
 end
