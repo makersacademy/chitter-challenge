6eeb7408051c70528048c110eb9245c6a8c7f0e8
Anthony <a.a.donovan@outlook.com>
2020-06-06 17:54:36 +0100

    testing infrastructure set up

--- Gemfile
+++ Gemfile
@@ -1,6 +1,12 @@
 source 'https://rubygems.org'
 
+group :test, :development do
+  gem 'sinatra'
+  gem 'pg'
+end
+
 group :test do
+  gem 'capybara'
   gem 'rspec'
   gem 'rubocop', '0.79.0'
   gem 'simplecov', require: false
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+ENV['RACK_ENV'] = 'test'
+
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+require_relative '../app'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
