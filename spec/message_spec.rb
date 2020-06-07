ae696ee4567162deb69c70b4b05d60161b815188
victorvallet <victorvallet8@gmail.com>
2020-06-07 22:22:09 +0200

    Added test for #.all method

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -7,4 +7,13 @@ describe "Message" do
     expect(peep.body).to eq 'Hello Chitter!'
     expect(peep.date).to eq '2020-06-07 21:54:00'
   end
+  it '#.all returns all peeps' do
+    peep = Message.new('Victor', 'Hello Chitter!', '2020-06-07 21:54:00')
+    peep2 = Message.new('John', 'Just got hired!', '2020-02-19 10:00:10')
+
+    peeps = Message.all
+
+    expect(peeps).to include( { :author => 'John', :body => 'Just got hired!', :date => '2020-02-19 10:00:10' } )
+    expect(peeps).to include( { :author => 'Victor', :body => 'Hello Chitter!', :date => '2020-06-07 21:54:00' } )
+  end
 end
