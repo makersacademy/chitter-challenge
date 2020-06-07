e49d495259c57834d070390bb7d140f3e49ef447
Rachael <rachaelewins@hotmail.com>
2020-06-07 18:02:01 +0100

    db setup changes

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE chitter
-\c chitter
+CREATE DATABASE chitter;
+\c chitter;
 CREATE TABLE pieps (id SERIAL PRIMARY KEY, text TEXT, time TIMESTAMP, username VARCHAR(20));
 INSERT INTO pieps (text, time, username) VALUES ('peep1', '2020-06-06 11:10:25', 'someone'),
  ('peep2', '2020-06-06 11:15:25', 'someone');
