f3ba0ffbd20e9ab08212c284a0e597dcccde8703
victorvallet <victorvallet8@gmail.com>
2020-06-07 20:02:43 +0200

    Added post route and peeps view to display peep

--- app.rb
+++ app.rb
@@ -1,10 +1,16 @@
 require 'sinatra/base'
+require './lib/message'
 
 class Chitter < Sinatra::Base
 
   get '/' do
     erb :index
+
   end
 
+  post '/peeps' do
+    @peep = Message.new(params[:peep])
+    erb :peeps
+  end
   run! if app_file == $0
 end
---
+++ lib/message.rb
@@ -0,0 +1,7 @@
+class Message
+  
+  def initialize(message)
+    @message = message
+  end
+    
+end
