117039f8ba598f9f6e485e287a727aec226c6cf9
victorvallet <victorvallet8@gmail.com>
2020-06-07 19:19:02 +0200

    Added feature test to post message

---
+++ spec/features/posting_messages_spec.rb
@@ -0,0 +1,8 @@
+feature 'posting messages' do
+  scenario 'post a message' do
+    visit('/')
+    fill_in('peep', with : 'Hello world!')
+    click_button('post')
+    expect(page).to have_content('Hello world!')
+  end
+end
