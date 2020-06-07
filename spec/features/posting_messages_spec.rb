c3b9904eb941a0f600d3bfbfaaab8704e638511b
victorvallet <victorvallet8@gmail.com>
2020-06-07 19:19:47 +0200

    Fixed typo in fill_in

--- spec/features/posting_messages_spec.rb
+++ spec/features/posting_messages_spec.rb
@@ -1,7 +1,7 @@
 feature 'posting messages' do
   scenario 'post a message' do
     visit('/')
-    fill_in('peep', with : 'Hello world!')
+    fill_in('peep', with: 'Hello world!')
     click_button('post')
     expect(page).to have_content('Hello world!')
   end
