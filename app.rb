1ecd1d5a1f512679c1162cb7d6912d1731dd2da3
J Perrin <perrinjack96@gmail.com>
2020-06-07 19:16:02 +0100

    refactored app.rb to include sign in page at / route

--- app.rb
+++ app.rb
@@ -4,14 +4,30 @@ require 'sinatra/base'
 require './lib/chitter.rb'
 
 class ChitterBox < Sinatra::Base
+  
   get '/' do
+    erb :sign_in
+  end
+  
+  
+ 
+
+
+  
+  
+  
+  get '/posts' do
     @posts = Chitter.all
     erb :index
   end
 
+  post '/posts' do
+    redirect '/posts'
+  end
+
   post '/new' do
     @new = Chitter.create(params[:content])
-    redirect '/'
+    redirect '/posts'
   end
 
   run! if app_file == $PROGRAM_NAME
--- spec/feature_tests/infrastructure_tests_spec.rb
+++ spec/feature_tests/infrastructure_tests_spec.rb
@@ -6,7 +6,7 @@ feature 'index page' do
   scenario 'shows content of posts' do
   
    add_test_data
-    visit('/')
+    visit('/posts')
     expect(page).to have_content('CNN is FAKE NEWS')
   end
 end
@@ -14,7 +14,7 @@ end
 feature 'index page' do
   scenario 'new post added' do
 
-    visit('/')
+    visit('/posts')
     fill_in 'content', with: 'We MUST BUILD THE WALL NOW!'
     click_button('post')
     expect(page).to have_content('We MUST BUILD THE WALL NOW!')
@@ -24,7 +24,7 @@ end
 feature 'index page' do
   scenario 'new post added' do
 
-    visit('/')
+    visit('/posts')
     fill_in 'content', with: 'We MUST BUILD THE WALL NOW!'
     click_button('post')
     expect(page).to have_content('We MUST BUILD THE WALL NOW!')
