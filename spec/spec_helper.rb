0cf6a6443db74341c54f7c29348c299d1cab7846
J Perrin <perrinjack96@gmail.com>
2020-06-07 14:50:10 +0100

    spec helper setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,18 @@
 require 'simplecov'
 require 'simplecov-console'
 
+
+
+
+require 'capybara/rspec'
+require 'rspec'
+require 'capybara'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+
+ENV['RACK_ENV'] = 'test'
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
@@ -10,8 +22,6 @@ SimpleCov.start
 
 RSpec.configure do |config|
   config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+    
   end
 end
