600807442d77e2cfefd8d5f6d80f75d604cfa5a6
minsi yang <yangminsi@gmail.com>
2020-06-07 17:37:55 +0100

    Couldn't pass both .all and .create method. After running .create rspec, data remains, affecting rspec .all method

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,6 +1,14 @@
 require "pg"
 
 class Peep
+
+  attr_reader :id, :message
+
+  def initialize(id:, message:)
+    @id = id
+    @message = message
+  end
+
   def self.all
     if ENV['ENVIROMENT'] == 'test'
       connection = PG.connect(dbname: 'peeps_manager_test')
@@ -8,17 +16,20 @@ class Peep
       connection = PG.connect(dbname: 'peeps_manager')
     end
    
-    result = connection.exec("SELECT * FROM peeps;")
-    result.map {|peep| peep['message']}
+    result = connection.exec("SELECT * FROM peeps ORDER BY id DESC;")
+    result.map do |peep|
+      Peep.new(id: peep['id'], message:peep['message'])
+    end
   end
 
   def self.create(message:)
     if ENV['ENVIROMENT'] == 'test'
-        connection = PG.connect(dbname: 'peeps_manager_test')
-      else
-        connection = PG.connect(dbname: 'peeps_manager')
-      end
+      connection = PG.connect(dbname: 'peeps_manager_test')
+    else
+      connection = PG.connect(dbname: 'peeps_manager')
+    end
 
-      connection.exec("INSERT INTO peeps (message) VALUES ('#{message}');")
+    result = connection.exec("INSERT INTO peeps (message) VALUES ('#{message}') RETURNING id, message;")
+    Peep.new(id: result[0]['id'], message: result[0]['message'])
   end
 end
--- spec/peep_class_spec.rb
+++ spec/peep_class_spec.rb
@@ -2,22 +2,34 @@ require "peep"
 
 describe Peep do
   describe ".all" do
+    connection = PG.connect(dbname: 'peeps_manager_test')
+
+    peep = Peep.create(message: "Howdy!")
+    Peep.create(message: "Anyone there?")
+    Peep.create(message: "Party time!")
+
+    peeps = Peep.all
+    p peeps
+
     it "returns a list of peeps" do
-      connection = PG.connect(dbname: 'peeps_manager_test')
-
-      Peep.create(message: "Hello everyone!")
-   
-      peeps = Peep.all
-
-      expect(peeps).to include("Hello everyone!")
+      expect(peeps.length).to eq 3
+      expect(peeps.last).to be_a Peep
+      expect(peeps.last.id).to eq peep.id
+      expect(peeps.last.message).to eq "Howdy!"
     end
   end
 
   describe ".create" do
     it "creates a new peep" do
-      Peep.create(message: "Hello everyone!")
+      peep = Peep.create(message: "Hello everyone!")
+      persisted_data = PG.connect(dbname: 'peeps_manager_test').query("SELECT * FROM peeps WHERE id = #{peep.id};")
+      
+      p peep
+      p persisted_data.first
 
-      expect(Peep.all).to include("Hello everyone!")
+      expect(peep).to be_a Peep
+      expect(peep.id).to eq persisted_data.first['id']
+      expect(peep.message).to eq "Hello everyone!"
     end
   end
 
