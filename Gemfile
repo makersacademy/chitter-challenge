5084514363f8440238c441412e3d191bb048aa4d
Alessa <alessa_hardwick@live.co.uk>
2020-07-04 14:36:32 +0100

    added pg gem and connected to database

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'pg'
 gem 'sinatra'
 
 group :test do
--- lib/peep.rb
+++ lib/peep.rb
@@ -1,9 +1,14 @@
+require 'pg'
+
 class Peep
   def self.all
-    [
-      "Hello this is a peep",
-      "also a peep",
-      "another peep"
-     ]
+    # [
+    #   "Hello this is a peep",
+    #   "also a peep",
+    #   "another peep"
+    #  ]
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec("SELECT * FROM chitter;")
+    result.map { |peep| peep['peeps'] }
   end
 end
