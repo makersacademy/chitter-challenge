459f2a2f50cbdd470311e440e0461b1686e83192
Katie Jones <katieljones29@gmail.com>
2020-07-04 11:37:36 +0100

    Added Welcome text on homepage, index erb created, testing infrastructure passing

--- app.rb
+++ app.rb
@@ -2,4 +2,8 @@ require 'sinatra/base'
 require './lib/chitter'
 class Chitter < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
 end
--- spec/features/posting_peep_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'User posts a peep' do
-  scenario 'user can post a message' do
-    visit('/')
-    fill_in "peep", :with => "Hello, Twitter!"
-    click_button('Peep')
-    expect(page).to have_content('Hello, Twitter!')
-  end
-end
