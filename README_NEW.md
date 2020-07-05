1f87c6a7d4c3e6150b60f829a6757dab5d69e20f
India Rex <face940@gmail.com>
2020-07-05 21:49:21 +0100

    first commit incl db setup

---
+++ README_NEW.md
@@ -0,0 +1,7 @@
+# Chitter
+
+Using PostgreSQL for the DBMS
+Sinatra and Capybara for the web and testing 
+Rspec for ruby testing
+Written in Ruby
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
@@ -15,3 +15,4 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
