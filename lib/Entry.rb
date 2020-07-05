f91081e63dd03e3664d16f27140bdf8510f54f65
Matthew Lock <matthew_lock20@hotmail.com>
2020-07-05 12:39:40 +0100

    Display peeps functionality working fully with updated timestamp, duplicate database now created but not yet connected for testing purposes

--- lib/Entry.rb
+++ lib/Entry.rb
@@ -1,4 +1,10 @@
 class Entry
+  attr_reader :text, :posting_date
+
+  def initialize(text, posting_date)
+    @text = text
+    @posting_date = posting_date
+  end
 
   def self.add(peep)
     connection = PG.connect(dbname: 'chitter')
@@ -8,7 +14,9 @@ class Entry
   def self.all 
     connection = PG.connect(dbname: 'chitter')
     result = connection.exec("SELECT * FROM entry;")
-    result.map { |peep| peep['text'] }
+    result.map do |peep| Entry.new(peep['text'], peep['posting_date'])
+    #result.map do |peep| peep['text']
+    end
   end
 
 end
