ed1cf4ed5c5f01316f7edb6e1dd8bedf20f3be2f
sam landman <sam landman>
2020-07-06 10:24:51 +0100

    DRY principle in rspec files

--- spec/features/addcheet_spec.rb
+++ spec/features/addcheet_spec.rb
@@ -1,9 +1,7 @@
 feature 'Add Peep' do
   scenario 'User Logs in, clicks Add Cheet. Fill in details. Visits page and Peep is there' do
     logintopage
-    click_button('Add Peep')
-    fill_in'peep_text', with: "This is a first Peep"
-    click_button('Add Peep')
+    addpeep
     expect(page).to have_content("This is a first Peep")
   end
 end
--- spec/features/deletecheet_spec.rb
+++ spec/features/deletecheet_spec.rb
@@ -1,9 +1,7 @@
 feature 'Delete Peep' do
   scenario 'User Logs in, clicks Delete Cheet. Visits page and Peep is not there' do
     logintopage
-    click_button('Add Peep')
-    fill_in'peep_text', with: "This is a first Peep"
-    click_button('Add Peep')
+    addpeep
     click_button('Delete Peep')
     click_button('Delete Peep')
     expect(page).not_to have_content("This is a first Peep")
