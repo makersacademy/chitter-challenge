52b529b18ecce9c649e86b2ac13d5c0fc8641d24
Alexis Simonian <alexis792.as@gmail.com>
2020-08-01 14:03:12 +0200

    futher environment setups

--- db/migrations/01_create_mandatory_tables.sql
+++ db/migrations/01_create_mandatory_tables.sql
@@ -1,2 +1,2 @@
 CREATE TABLE users(id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(140));
-CREATE TABLE messages(id SERIAL PRIMARY KEY, user_id NUMERIC, content TEXT, time TIMESTAMP);
+CREATE TABLE messages(id SERIAL PRIMARY KEY, user_id NUMERIC, content TEXT, time TIMESTAMP DEFAULT clock_timestamp());
