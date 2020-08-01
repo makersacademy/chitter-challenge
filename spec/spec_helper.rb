01ea7c808b69fbcb5ddb08dd3a42d005d1b947b0
Alexis Simonian <alexis792.as@gmail.com>
2020-08-01 13:52:48 +0200

    corrected typos

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,9 @@ require 'capybara/rspec'
 require 'simplecov'
 require 'simplecov-console'
 
-ENV['ENVIRONEMENT'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
 
-require FILE.join(File.dirname(__FILE__), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 Capybara.app = Chitter
 
