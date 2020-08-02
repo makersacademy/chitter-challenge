d2cf2b20495075fdd2a839ce3b59887083fe24ab
Student <student@Makerss-MacBook-Air.local>
2020-08-02 10:54:08 +0100

    First test added and passed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara/rspec'
+require './app.rb'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
+Capybara.app = Chitter
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
