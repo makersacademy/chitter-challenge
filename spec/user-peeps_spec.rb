3c7fe5611baa236a343ac2f4305ad938b1c5ac7a
Daniel M <danielim@hotmail.co.uk>
2020-08-03 02:54:37 +0100

    spec fix

--- spec/user-peeps_spec.rb
+++ spec/user-peeps_spec.rb
@@ -7,7 +7,7 @@ describe 'user-peeps' do
     generate_example_users
     generate_example_peeps
     generate_example_threads
-    p user_peep = UserPeep.add(user_id:1, peep_id:1, thread_id:1)
+    user_peep = UserPeep.add(user_id:1, peep_id:1, thread_id:1)
     expect(user_peep['user_id']).to eq '1'
     expect(user_peep['peep_id']).to eq '1'
   end
