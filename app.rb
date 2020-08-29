ebceea32d8af83a9cfcb14e50d15d18941155f86
Iliyan-Y <maximizebg@gmail.com>
2020-08-29 17:44:19 +0300

    User can post and see all previouse posts

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra/base"
 require 'sinatra/activerecord'
 require './db_connection_setup'
+require "./lib/chitter"
 
 class App < Sinatra::Base
 
@@ -9,7 +10,13 @@ class App < Sinatra::Base
   end
 
   get "/chitter" do 
+    @posts = Chitter.all
     erb :create
+  end
+
+  post "/chitter/create" do
+    Chitter.create(params[:text])
+    redirect "/chitter" 
   end
   
 end
