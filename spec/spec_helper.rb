29cf116404c5d4bdee80586e0d2e12457acf7287
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-29 14:54:07 +0100

    Built initial project infrastructure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,21 @@
+ENV['RACK_ENV'] = 'test'
+
+require_relative '../app.rb'
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+Capybara.app = Chitter
 
 RSpec.configure do |config|
   config.after(:suite) do
