c64b85cb1451a6ac9509db2dd648f3a0a4a768bc
Iliyan-Y <maximizebg@gmail.com>
2020-08-29 17:31:33 +0300

    Methods for getting and creating posts added to the monedel

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,4 +1,14 @@
 require_relative 'database'
 
 class Chitter
+
+  def self.all 
+    get_all_posts = DataBase.query("SELECT * FROM posts;")
+    get_all_posts.entries
+  end
+
+  def self.create(text)
+    DataBase.query("INSERT INTO posts (post) VALUES ('#{text}') RETURNING id, post, created_at;")
+  end
+
 end
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -2,4 +2,18 @@ require "chitter"
 
 describe Chitter do 
 
+  describe "#all" do 
+    it "Gets all entries from the database" do 
+      DataBase.query("INSERT INTO posts (post) VALUES ('My first post');")
+      expect(Chitter.all.length).to be > 0 
+    end
+  end
+
+  describe "#create" do 
+    it "create a new entry in the database" do 
+      new_post = Chitter.create("New post in chitter")
+      expect(Chitter.all).to eq new_post.entries
+    end
+  end
+
 end
