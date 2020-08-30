c4b8970974322f5ac77882a05350c49fbdcbbf30
Iliyan-Y <maximizebg@gmail.com>
2020-08-30 11:45:11 +0300

    test for creating user passed

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,9 @@
 require "database"
 
 class User
+
+  def self.create(name, email, password)
+    DataBase.query("INSERT INTO users (name,email,password) VALUES ('#{name}', '#{email}','#{password}') RETURNING id, name, email;")
+  end
+
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -4,8 +4,8 @@ describe User do
   describe "#create" do 
     it "Create a new user" do 
       new_user = User.create("Test User", "test@test.com", "pass123")
-      expect(new_user.entries['name']).to eq "Test User"
-      expect(new_user.entries['email']).to eq "test@test.com"
+      expect(new_user.entries[0]['name']).to eq "Test User"
+      expect(new_user.entries[0]['email']).to eq "test@test.com"
     end
   end
 end
