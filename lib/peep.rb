fae9d83322c28c28d3f841df75aa0404eb7c36b4
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-30 18:14:17 +0100

    unit test for Peep class to return messsages passed

---
+++ lib/peep.rb
@@ -0,0 +1,13 @@
+require 'pg'
+
+class Peep
+
+  def self.all
+    connection = PG.connect :dbname => 'chitter_manager'
+
+    peeps = connection.exec "SELECT * FROM peeps"
+
+    peeps.map { |peep| peep['message'] }
+  end
+
+end
