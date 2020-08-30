b7913caee2ef991af1f881e78118fe9a7e8c7a8a
Iliyan-Y <maximizebg@gmail.com>
2020-08-30 12:58:24 +0300

    Log in test for class user added

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -8,4 +8,12 @@ describe User do
       expect(new_user.entries[0]['email']).to eq "test@test.com"
     end
   end
+
+  describe "#log_in" do 
+    it "Log in user as instance of class user" do 
+      User.create("Test User", "test@test.com", "pass123")
+      user = User.log_in("test@test.com", "pass123")
+      expect(user).to be_an_instance_of(User)
+    end
+  end
 end
