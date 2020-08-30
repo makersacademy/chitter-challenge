39eaf91e94013e4988fbd291fbc01f71a2cadcdb
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-30 18:30:22 +0100

    create in Peep class test passed

--- lib/peep.rb
+++ lib/peep.rb
@@ -10,4 +10,10 @@ class Peep
     peeps.map { |peep| peep['message'] }
   end
 
+  def self.create(message)
+    connection = PG.connect :dbname => 'chitter_manager'
+
+    connection.exec "INSERT INTO peeps (message) VALUES ('#{message}');"
+  end
+
 end
