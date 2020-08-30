90964a359548f9be659529c1826e8c865f7fac87
Iliyan-Y <maximizebg@gmail.com>
2020-08-30 18:25:56 +0300

    Feature test for log out added

--- spec/features/user_menu_spec.rb
+++ spec/features/user_menu_spec.rb
@@ -14,4 +14,16 @@ feature "Youser can choose options" do
     expect(page).to have_field("password")
   end
 
+  scenario "User login and out" do 
+    User.create("Test", "test@test.com", "test123")
+    visit("/")
+    click_link "Log In"
+    fill_in("email", with: "test@test.com")
+    fill_in("password", with: "test123" )
+    click_button "GO"
+    expect(page).to have_content "Welcome to Chitter Test"
+    click_link "Log Out"
+    expect(page).not_to have_content "Welcome to Chitter Test"
+  end
+
 end
