a1df5036b62101aef8cabbfbea5ef0eea06df1c0
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-31 11:26:33 +0100

    Updated a test to sign a user in, for travis

--- spec/features/creating_peeps_spec.rb
+++ spec/features/creating_peeps_spec.rb
@@ -1,5 +1,13 @@
 feature 'Posting a new Peep' do
   scenario 'user can post a new Peep' do
+    User.create(handle: 'tester', email: 'test@email.com', password: 'password123', name: 'Mr Test')
+
+    visit '/sessions/new'
+    fill_in :email, with: 'test@email.com'
+    fill_in :password, with: 'password123'
+
+    click_button 'Sign in'
+    
     visit '/peeps/new'
     
     fill_in "peep", with: "This is my test peep"
