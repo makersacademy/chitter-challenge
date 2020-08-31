0c2b25ad12c47682be9f6e7d7425e1fb489ee7ed
Yasmin Underdown <yunderdown1@gmail.com>
2020-08-31 11:01:03 +0100

    unit test for showing reverse order failing

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -12,6 +12,17 @@ describe Peep do
       expect(peeps).to include('second peep test')
       expect(peeps).to include('third peep test')
     end
+
+    it 'returns peeps in reverse order' do
+      Peep.create('first peep test')
+      Peep.create('second peep test')
+      Peep.create('third peep test')
+      peeps = Peep.all
+      expect(peeps.first).to eq('third peep test')
+      expect(peeps[1]).to eq('second peep test')
+      expect(peeps.last).to eq('first peep test')
+
+    end
   end
 
   describe '#.create' do
