0cc733c182acfd705500cbc09dbd36102ce577d0
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-08-31 10:40:10 +0100

    added path for new user registration and posting of user details to app.rb, created users/new.erb view with form to enter email and password

--- app.rb
+++ app.rb
@@ -3,11 +3,15 @@ require './lib/peep'
 require './database_connection_setup.rb'
 
 class Chitter < Sinatra::Base
-enable :sessions
+  enable :sessions
 
   get '/' do
     @peeps = Peep.peeps
     erb :index
+  end
+
+  get '/peep/new' do
+    erb :'/peep/new'
   end
 
   post '/' do
@@ -15,8 +19,13 @@ enable :sessions
     redirect '/'
   end
 
-  get '/peep/new' do
-    erb :'/peep/new'
+  get '/users/new' do
+    erb :'/users/new'
+  end
+
+  post '/users/new' do
+    # create new user
+    redirect '/'
   end
 
   run! if app_file == $0
