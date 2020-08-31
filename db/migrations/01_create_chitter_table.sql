157a6f2c9d02a7032c90b65ba3f36abdc5c9eb92
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-31 11:18:18 +0100

    Travis pls

--- db/migrations/01_create_chitter_table.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep VARCHAR(140), peep_time timestamptz DEFAULT NOW(), poster VARCHAR(60));
---
+++ db/migrations/01_peeps_chitter_table.sql
@@ -0,0 +1,1 @@
+CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep VARCHAR(140), peep_time timestamptz DEFAULT NOW(), poster VARCHAR(60), poster_name VARCHAR(60));
