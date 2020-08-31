6cb9974acd510e171bce075f5b7b73b3cc0fe800
JRG <47917431+JR-G@users.noreply.github.com>
2020-08-31 11:11:35 +0100

    Updated sql file to help travis

--- db/migrations/01_create_chitter_table.sql
+++ db/migrations/01_create_chitter_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE chitter(id SERIAL PRIMARY KEY, peep VARCHAR(140), peep_time timestamptz DEFAULT NOW(), poster VARCHAR(60));
+CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep VARCHAR(140), peep_time timestamptz DEFAULT NOW(), poster VARCHAR(60));
