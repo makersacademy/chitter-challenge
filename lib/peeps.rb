a494a1e9c9bd714a4d386cea438632f1cf625c6c
Connor Worthington <worthington.connor@gmail.com>
2020-09-26 14:07:45 +0100

    adds method for setting test environment

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -3,14 +3,24 @@ require 'pg'
 class Peeps 
 
   def self.all
-    connection = PG.connect(dbname: 'chitter_test')
-    result = connection.exec("SELECT * FROM peeps;")
+    set_environment
+    result = @connection.exec("SELECT * FROM peeps;")
     result.map { |peep| peep['peep'] }
   end
 
   def self.post(peep)
-    connection = PG.connect(dbname: 'chitter_test')
-    connection.exec("INSERT INTO peeps (peep) VALUES ('#{peep}')")
+    set_environment
+    @connection.exec("INSERT INTO peeps (peep) VALUES ('#{peep}')")
+  end 
+
+private 
+
+  def self.set_environment
+    if ENV['ENVIRONMENT'] == 'test'
+      @connection = PG.connect(dbname: 'chitter_test')
+    else 
+      @connection = PG.connect(dbname: 'chitter')
+    end 
   end 
 
 end 
