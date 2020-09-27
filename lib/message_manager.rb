beecf74d952b8ca1db40b2eb271f5fcedbc814f3
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-27 10:55:59 +0100

    Created store and all methods in MessageManager.
    Created the methods that store a message to a database, and return the entire database.

--- lib/message_manager.rb
+++ lib/message_manager.rb
@@ -1,4 +1,26 @@
+require "pg"
+
 class MessageManager
+  def self.all
+    if ENV["ENVIRONMENT"] == "test"
+      connection = PG.connect(dbname: "chitter_test")
+    else
+      connection = PG.connect(dbname: "chitter")
+    end
+
+    result = connection.exec("SELECT * FROM messages;")
+    result.map { |message|
+      Message.new(message["content"])
+    }
+  end
+
   def self.store(message)
+    if ENV["ENVIRONMENT"] == "test"
+      connection = PG.connect(dbname: "chitter_test")
+    else
+      connection = PG.connect(dbname: "chitter")
+    end
+
+    result = connection.exec("INSERT INTO messages (content) VALUES ('#{message}');")
   end
 end
