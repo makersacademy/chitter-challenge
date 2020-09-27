6d61708338fe480431ec714022549121dd4300f1
Veronica Lee <veronicavlee@icloud.com>
2020-09-28 00:50:02 +0100

    Add travis config for db setup

--- db/migrations/02_create_peeps_table.sql
+++ db/migrations/02_create_peeps_table.sql
@@ -1,9 +1,8 @@
-CREATE TABLE peeps(
+CREATE TABLE IF NOT EXISTS peeps(
     id SERIAL PRIMARY KEY,
     message VARCHAR(120) NOT NULL,
     username VARCHAR(120) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     CONSTRAINT fk_username
       FOREIGN KEY(username)
-	  REFERENCES users(username)
-);
+	  REFERENCES users(username));
