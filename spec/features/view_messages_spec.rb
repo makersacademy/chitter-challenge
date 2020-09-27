1ac332b44c15910334e3336aec639600b52fb1b5
Charlotte Brandhorst-Satzkorn <catzkorn@gmail.com>
2020-09-27 15:09:09 +0100

    Timestamps are displayed.
    Timestamps are now displayed alongside posts.

--- spec/features/view_messages_spec.rb
+++ spec/features/view_messages_spec.rb
@@ -11,13 +11,13 @@ feature "view posted messages" do
     fill_in :content, with: "Can I list a world of warcraft rank 1 under achievements on a cv? Asking for a friend."
     click_button "Post"
     third_button_click = Time.now
-    posts = page.find(".posts").all("div")
+    posts = page.find(".posts").all(".post")
     expect(posts.length).to eq 3
-    expect(posts[0].text).to eq "Can I list a world of warcraft rank 1 under achievements on a cv? Asking for a friend."
-    expect(posts[0]).to have_content first_button_click.strftime("%H:%m")
-    expect(posts[1].text).to eq "If Kubernetes is based off of Borg. Is it called Kubernetes because the Borg ship is a Kube?"
-    expect(posts[1]).to have_content second_button_click.strftime("%H:%m")
-    expect(posts[2].text).to eq "If the aliens in Independence Day used Kubernetes; the movie would have turned out a lot differently."
-    expect(posts[2]).to have_content third_button_click.strftime("%H:%m")
+    expect(posts[0].text).to have_content "Can I list a world of warcraft rank 1 under achievements on a cv? Asking for a friend."
+    expect(posts[0]).to have_content first_button_click.strftime("%H:%M")
+    expect(posts[1].text).to have_content "If Kubernetes is based off of Borg. Is it called Kubernetes because the Borg ship is a Kube?"
+    expect(posts[1]).to have_content second_button_click.strftime("%H:%M")
+    expect(posts[2].text).to have_content "If the aliens in Independence Day used Kubernetes; the movie would have turned out a lot differently."
+    expect(posts[2]).to have_content third_button_click.strftime("%H:%M")
   end
 end
