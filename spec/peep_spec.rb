9a23e3fdaa5cd46d75d0d7d0d23fceddffd37e15
samcolson4 <samcolson4@gmail.com>
2020-09-28 09:24:56 +0100

    Added tests for peep class

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -8,31 +8,16 @@ describe Peep do
     add_test_users
   end
 
-=begin
   it 'creates a new peep' do
+    truncates
+    user = Users.create(name: 'test name', email: 'test@email.com', password: 'pword123', username: 'test_username')
 
-    peep = Peep.create(text: 'This is a test peep', )
+    peep = Peep.create(text: 'This is a test peep', user_id: user.id)
 
     persisted_data = persisted_data(table: :peeps, id: peep.id)
 
     expect(peep).to be_a Peep
     expect(peep.text).to eq 'This is a test peep'
   end
-
-  describe '.all' do
-    let(:id) { 1 }
-
-    it 'returns a list of peeps' do
-      
-      Peep.create(text: "This issss a test peep", user_id: :id)
-
-      peep = Peep.all
-      
-      expect(peep.first).to be_a Peep
-      expect(peep.first.id).to eq peep.id
-      expect(peep.first.text).to eq "This issss a test peep"
-    end
-  end
-=end
 
 end
