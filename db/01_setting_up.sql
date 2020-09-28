a28c25d6d14267223e00000fceb34bf6a509c569
ollie beney <ombeney@gmail.com>
2020-09-28 10:11:48 +0100

    didn't save

--- db/01_setting_up.sql
+++ db/01_setting_up.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE chitter
-CREATE TABLE chitter_users(username VARCHAR(15), password VARCHAR(20));
+CREATE TABLE chitter_users(username VARCHAR(15), password TEXT);
 CREATE TABLE peeps(username VARCHAR(15), peep VARCHAR(120), time DATE);
-
+CREATE EXTENSION pgcrypt; 
 You must also complete the same steps for a second database called chitter_test
