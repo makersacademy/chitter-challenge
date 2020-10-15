e5348063790fa9789431294e7c02846a64dc035b
elsstrange <ellenabstrange@gmail.com>
2020-10-15 21:01:07 +0100

    Unit tests (not passing) for DatabaseConnection.query

--- spec/units/database_connection_spec.rb
+++ spec/units/database_connection_spec.rb
@@ -15,4 +15,19 @@ describe DatabaseConnection do
       expect(connection.db).to eq test_db
     end
   end
+
+  describe '#query' do
+    let(:test_query) { "INSERT INTO users(name, username, email, password) VALUES ('a','b','c','d') RETURNING name;" }
+
+    it 'passes a SQL query to the connected database' do
+      connection = DatabaseConnection.setup(test_db)
+      expect(connection).to receive(:exec).with(test_query)
+      DatabaseConnection.query(test_query)
+    end
+
+    it 'returns a Result object' do
+      DatabaseConnection.setup(test_db)
+      expect(DatabaseConnection.query(test_query)).to be_an_instance_of(PG::Result)
+    end
+  end
 end
