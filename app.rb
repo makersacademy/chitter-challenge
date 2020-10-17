97122d0d5c6a8a3300d48763099774287f2e66e2
elsstrange <ellenabstrange@gmail.com>
2020-10-17 11:21:53 +0100

    Passing feature test for user sign up

--- app.rb
+++ app.rb
@@ -4,10 +4,22 @@ require 'sinatra/base'
 require './lib/peep'
 require './lib/formats'
 
-
 class Chitter < Sinatra::Base
+  enable :sessions
+
   get '/chitter' do
+    @user = session[:username]
     @peeps = Peep.all
     erb :chitter
+  end
+
+  get '/chitter/user/new' do
+    erb :new_user
+  end
+
+  post '/chitter/user' do
+    session[:username] = params[:username]
+    DatabaseConnection.query("INSERT INTO users(name, username, email, password) VALUES ('#{params[:name]}', '#{params[:username]}', '#{params[:email]}', '#{params[:password]}');")
+    redirect '/chitter'
   end
 end
