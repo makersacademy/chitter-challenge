a5248aac483ee5999d2b1a7240749b420868dffc
elsstrange <ellenabstrange@gmail.com>
2020-10-17 16:55:44 +0100

    Fixed use of incorrect variable for filling in name in form

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -5,7 +5,7 @@ end
 def sign_up(username:, name:, email:, password:)
   click_button 'Sign Up'
   fill_in 'username', with: username
-  fill_in 'name', with: user
+  fill_in 'name', with: name
   fill_in 'email', with: email
   fill_in 'password', with: password
   click_button 'Sign Up'
