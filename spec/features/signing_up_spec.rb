6992ad5eb176eeb0ef64f9035406e8b91dd42144
elsstrange <ellenabstrange@gmail.com>
2020-10-17 11:20:49 +0100

    Fixed test wording to specify clicking buttons not links

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -4,16 +4,16 @@ feature 'user sign up' do
   let(:email) { 'email' }
   let(:password) { 'password' }
 
-  scenario 'a new user signs up' do
+  scenario 'a new user signs up with valid details' do
     visit '/chitter'
-    click_on 'Sign Up'
+    click_button 'Sign Up'
     expect(current_path).to eq '/chitter/user/new'
     fill_in 'username', with: username
     fill_in 'name', with: user
     fill_in 'email', with: email
     fill_in 'password', with: password
-    click_on 'Sign Up'
+    click_button 'Sign Up'
     expect(current_path).to eq '/chitter'
-    expect(page).to have_content 'Hello #{username}'
+    expect(page).to have_content "Hello #{username}"
   end
 end
