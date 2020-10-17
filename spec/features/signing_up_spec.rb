a0dec86fbdfc63d505e9384580ba3f8e11af1344
elsstrange <ellenabstrange@gmail.com>
2020-10-17 17:52:13 +0100

    Amended spec for duplicate sign ups to distinguish error handling for emails and usernames

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -3,6 +3,8 @@ feature 'user sign up' do
   let(:user) { 'Test Name' }
   let(:email) { 'email' }
   let(:password) { 'password' }
+  let(:username2) { 'tester2' }
+  let(:email2) { 'email2' }
 
   scenario 'a new user signs up with valid details' do
     visit '/chitter'
@@ -18,12 +20,21 @@ feature 'user sign up' do
     expect(page).not_to have_button "Sign Up"
   end
 
-  scenario 'a new user signs up with duplicate details' do
+  scenario 'a new user signs up with duplicate username' do
     DatabaseConnection.query("INSERT INTO users(name, username, email, password) VALUES('#{user}','#{username}','#{email}','#{password}')")
     visit '/chitter'
-    sign_up(name: user, username: username, email: email, password: password)
-    expect(current_path).to eq '/chitter'
-    expect(page).to have_content "Sorry, something went wrong with your sign-up."
-    expect(page).to have_button "Sign Up"
+    sign_up(name: user, username: username, email: email2, password: password)
+    expect(current_path).to eq '/chitter/user/new'
+    expect(page).to have_content 'That username is already in use.'
+    expect(page).to have_button 'Sign Up'
+  end
+
+  scenario 'a new user signs up with duplicate email' do
+    DatabaseConnection.query("INSERT INTO users(name, username, email, password) VALUES('#{user}','#{username}','#{email}','#{password}')")
+    visit '/chitter'
+    sign_up(name: user, username: username2, email: email, password: password)
+    expect(current_path).to eq '/chitter/user/new'
+    expect(page).to have_content 'That email address is already in use.'
+    expect(page).to have_button 'Sign Up'
   end
 end
