aaee0ab0a492be5538872ca12d1efea368a432f0
elsstrange <ellenabstrange@gmail.com>
2020-10-17 17:40:27 +0100

    Passing tests for error handling duplicate email and username

--- lib/user.rb
+++ lib/user.rb
@@ -2,8 +2,16 @@ class User
   attr_reader :id, :username
   
   def self.create(name:, username:, email:, password:)
-    persisted = DatabaseConnection.query("INSERT INTO users(name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{password}') RETURNING id, username;").first
-    new(id: persisted['id'], username: persisted['username'])
+    begin
+      persisted = DatabaseConnection.query("INSERT INTO users(name, username, email, password) VALUES('#{name}', '#{username}', '#{email}', '#{password}') RETURNING id, username;").first
+      new(id: persisted['id'], username: persisted['username'])
+    
+    rescue PG::UniqueViolation => error
+      return :duplicate_username if error.message.include?('users_username_key')
+      return :duplicate_email if error.message.include?('users_email_key')
+      
+      :error
+    end    
   end
 
   private
