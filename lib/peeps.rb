1b2d755ee34c4b495bf49c9dc88bd92c64c53728
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-23 19:50:38 +0100

    Refactored the app and updated the view. User can now view peeps.

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -5,7 +5,7 @@ class Peeps
   def self.all
     connection = PG.connect(dbname: 'chitter')
     result = connection.exec('SELECT * FROM peeps;')
+    
     result.map { |peep| peep['peep'] }
-     #[ 'Happy Friday!', 'Does chitter remind you of anything? Tweet Tweet']
   end
 end
