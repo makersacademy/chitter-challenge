316b41b2ce4e4468bf7cf04eb6a4ff72b278cf9a
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-23 11:35:50 +0100

    Required gems to spec helper and linked the sinatra application

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,7 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
@@ -15,3 +15,15 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+ENV['ENVIRONMENT'] = 'test'
+
+require 'capybara'
+require 'rspec'
+require 'capybara/rspec'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
+
+
