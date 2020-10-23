2e9d6c7431b3f680a0aa851345320f597ed5d2cb
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-23 12:16:00 +0100

    Updated unit test to test .all method of ChitterManager and passed in most basic way

---
+++ lib/chitter.rb
@@ -0,0 +1,7 @@
+class ChitterManager
+
+  def self.all
+    'I ran 8 miles today, #winning'
+  end
+
+end
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../lib/chitter.rb'
-describe Chitter do
+describe ChitterManager do
   describe '.all' do
     it 'returns all the peeps' do
       #set up to add peep to db
-      expect(Chitter.all).to include('I ran 8 miles today, #winning')    
+      expect(ChitterManager.all).to include('I ran 8 miles today, #winning')    
     end
   end
 end
