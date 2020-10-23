9ad399c7b3bc6884393fbeee19afc844c08534bf
Kiril <kiril@olgas-air.mynet>
2020-10-23 17:31:23 +0100

    feature test for sign up and staying signed in

--- spec/features/registrations/user_can_sign_up_and_in_spec.rb
+++ spec/features/registrations/user_can_sign_up_and_in_spec.rb
@@ -1,12 +1,13 @@
-# feature "signup" do
-#   scenario "user clicks button from homepage and signs up" do
-#     visit('/')
-#     click_button('Sign Up')
-#     fill_in(:name, with: "Kiril D")
-#     fill_in(:email, with: "kiriarf@gmail.com")
-#     fill_in(:username, with: "kiriarf")
-#     fill_in(:password, with: "password123")
-#     click_button('Register')
-#     expect(page).to have_content("Hi, kiriarf")
-#   end  
-# end
+feature 'registrations' do
+  scenario 'user can sign up and be signed in' do
+    visit('registrations/new')
+    fill_in "name",	with: "testuser" 
+    fill_in "email",	with: "test@test.com" 
+    fill_in "username",	with: "testusername"
+    fill_in "password",	with: "Qwerty12345!" 
+    click_button("Register")
+
+    expect(page.current_path).to eq('/')
+    expect(page).to have_content("Hi, testuser")
+  end
+end
