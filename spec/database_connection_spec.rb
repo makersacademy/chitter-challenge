108f8153415a27fb72e2c0ce8b864d8aa6377403
jotkafomat <k-j@xl.wp.pl>
2020-10-24 09:49:18 +0100

    add test for .connection method

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -7,5 +7,11 @@ describe DatabaseConnection do
 
       DatabaseConnection.setup('chitter_db_test')
     end
+    it 'this connection is persistent' do
+      # Grab the connection as a return value from the .setup method
+      connection = DatabaseConnection.setup('chitter_db_test')
+
+      expect(DatabaseConnection.connection).to eq connection
+    end
   end
 end
