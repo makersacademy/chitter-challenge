6451b3b5e52fbb2b900a73926e92fd45a069f1fb
Cameron Logie <cameronlogie24@gmail.com>
2020-10-24 11:58:59 +0100

    feature tests and unit tests passing for adding a new peep

--- chitter.rb
+++ chitter.rb
@@ -1,7 +1,14 @@
 class Chitter 
 
+  @peeps = ["this is the first message ever posted to chitter", "this is the second", "okay now i've posted three"]
+
   def self.all
-    ["this is the first message ever posted to chitter", "this is the second", "okay now i've posted three"]
+    @peeps
+  end
+
+  def self.add(peep)
+    @peeps = @peeps.push(peep)
+    peep
   end
 
 end
--- spec/unit/chitter_spec.rb
+++ spec/unit/chitter_spec.rb
@@ -4,8 +4,7 @@ describe Chitter do
   end
 
   it 'responds to .add by adding a peep to the array of all peeps' do
-    Chitter.add("This is a new peep")
+    Chitter.add("this is a new peep")
     expect(Chitter.all).to eq ["this is the first message ever posted to chitter", "this is the second", "okay now i've posted three", "this is a new peep"]
   end
-  
 end
