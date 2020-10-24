534da956df1fec50702c68766c4901521f7cd11d
jotkafomat <k-j@xl.wp.pl>
2020-10-24 09:45:56 +0100

    test pass

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -3,7 +3,7 @@ require 'database_connection'
 describe DatabaseConnection do
   describe '.setup' do
     it 'sets up a connection to a database through PG' do
-      expext(PG).to receive(:connect).with(dbname: 'chitter_db_test')
+      expect(PG).to receive(:connect).with(dbname: 'chitter_db_test')
 
       DatabaseConnection.setup('chitter_db_test')
     end
