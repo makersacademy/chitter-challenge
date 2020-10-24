6dd62d0fc6bcfecc2a8781b480032387a348670d
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-24 16:32:45 +0100

    created table and added first feature test for user

---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,12 @@
+feature 'user registration' do
+  scenario 'user can sign up' do
+    visit('/users/new')
+    fill_in :name, with: "Leo D"
+    fill_in :username, with: "Leothelion"
+    fill_in :email, with: "newuser@umail.com"
+    fill_in :password, with: "lovemai"
+    click_button "Sign Up"
+
+    expect(page).to have_content "Leo D"
+  end
+end 
