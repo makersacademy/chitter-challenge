98ba7b3daf402f97beac9d19f8e4291e59bb0043
Kiril <kiril@olgas-air.mynet>
2020-10-24 16:54:23 +0100

    refactor credential checking

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,21 +12,7 @@ class User
   end
 
   def self.create(username:, email:, name:, password:)
-    check1 = DatabaseConnection.query("SELECT * FROM users WHERE username='#{username}'")
-    check2 = DatabaseConnection.query("SELECT * FROM users WHERE email='#{email}'")
-
-    check1 = check1.map do |row|
-      row['id']
-    end.first
-
-    check2 = check2.map do |row|
-      row['id']
-    end.first
-
-    puts check1
-    puts check2
-
-    if !check1 || !check2
+    if credentials_check(username, email)
       DatabaseConnection.query("INSERT INTO users (username, email, full_name, pwd) 
       VALUES ('kiriarf', 'kiriarf@chitter.com', 'kiril', '123');")
       User.new(username: username, email: email, name: name, password: password)
@@ -35,4 +21,24 @@ class User
     end
   end
 
+  private
+  def self.credentials_check(username, email)
+    check_email(email) || check_username(username)
+  end
+
+  def self.check_email(email)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE email='#{email}'")
+    check = result.map do |row|
+      row['id']
+    end.first
+    check.nil?
+  end
+
+  def self.check_username(username)
+    result = DatabaseConnection.query("SELECT * FROM users WHERE username='#{username}'")
+    check = result.map do |row|
+      row['id']
+    end.first
+    check.nil?
+  end
 end
