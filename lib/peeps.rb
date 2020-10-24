a7bb289fbdf232f4a343fd707c95ca13d3b3a182
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-24 15:31:03 +0100

    Wrapped the datatbase data into the program, updated table names to have better descriptions, updated view to correspond to the model.

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,9 +1,11 @@
 require 'pg'
 
 class Peeps
-
-  def initialize
-
+  attr_reader :id, :body, :post_time
+  def initialize(id:, body:, post_time:)
+    @id = id
+    @body = body
+    @post_time = post_time
   end
 
   def self.all
@@ -13,18 +15,19 @@ class Peeps
       connection = PG.connect(dbname: 'chitter')
     end
     
-    result = connection.exec('SELECT * FROM peeps
-      ORDER BY posted_at DESC;')
-    result.map { |peep| peep['peep'] }
+    result = connection.exec('SELECT * FROM peeps ;')
+    result.map do |peep| 
+      Peeps.new(id: peep['id'], body: peep['body'], post_time: peep['post_time'])
+    end
   end
 
-  def self.add(peep:)
+  def self.add(body:)
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_test')
     else
       connection = PG.connect(dbname: 'chitter')
     end
-
-    connection.exec("INSERT INTO peeps (peep) VALUES('#{peep}') RETURNING peep, posted_at")
+    result = connection.exec("INSERT INTO peeps (body) VALUES ('#{body}') RETURNING id, body, post_time")
+    Peeps.new(id: result[0]['id'], body: result[0]['body'], post_time: result[0]['post_time'])
   end
 end
--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -1,8 +1,8 @@
 feature 'viewing a peep' do
   scenario 'a user can view a peep on chitter' do
-    Peeps.add(peep: 'Weekend vibes')
-    Peeps.add(peep: 'insert funny peep here')
-    Peeps.add(peep: 'Happy Friday!')
+    Peeps.add(body: 'Weekend vibes')
+    Peeps.add(body: 'insert funny peep here')
+    Peeps.add(body: 'Happy Friday!')
     
     visit '/chitter'
 
