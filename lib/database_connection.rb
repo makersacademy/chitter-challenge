955ea762fae9830b562ee76e9a35175ade760ae5
jotkafomat <k-j@xl.wp.pl>
2020-10-24 09:51:08 +0100

    test pass

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -2,6 +2,10 @@ require 'pg'
 
 class DatabaseConnection
   def self.setup(dbname)
-    PG.connect(dbname: dbname)
+    @connection = PG.connect(dbname: dbname)
+  end
+
+  def self.connection
+    @connection
   end
 end
