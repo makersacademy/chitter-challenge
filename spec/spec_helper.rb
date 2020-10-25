e0ba6f4b59fdce2d056db49a0ef460a5ad42b9c4
Otis <otisvickersgraver@gmail.com>
2020-10-25 15:58:37 +0000

    RSpec config clears database when rspec is executed

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require_relative './setup_test_database.rb'
+
 ENV['RACK_ENV'] = 'test'
 
 ENV['ENVIRONMENT'] = 'test'
@@ -8,7 +10,7 @@ require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
 
-Capybara.app = Chitter
+
 
 
 require 'simplecov'
@@ -18,13 +20,14 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
   # SimpleCov::Formatter::HTMLFormatter
-])
-SimpleCov.start
+  ])
+  SimpleCov.start
+  Capybara.app = Chitter
 
-RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+
+  RSpec.configure do |config|
+    config.before(:each) do
+      setup_test_database
+    end
+
   end
-end
