766b30aeebe8ff07cd8ccd87f32eb73c3f4cdc92
KC Thomas <thisiskcthomas@gmail.com>
2020-10-25 16:36:40 +0000

    Added passing feature test for users to add peeps

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require 'sinatra/base'
+require_relative './models/peep'
 
 class ChitterApp < Sinatra::Base 
+
+  enable :sessions
 
   get '/' do
     @peeps = Peep.list_ordered_peeps
     p @peeps
     erb :index
+  end
+
+  post '/create-peep' do
+    connection = PG.connect :dbname => 'peeps_test_manager'
+    connection.exec "INSERT INTO peeps (message, creator) VALUES ('#{params[:message]}', '#{params[:username]}');"
+    redirect to '/'
   end
 
   run! if app_file == $0
---
+++ spec/features/create_peep_spec.rb
@@ -0,0 +1,11 @@
+feature 'Create Peep' do
+  scenario 'a user can create a peep' do 
+    visit '/'
+
+    fill_in(:message, with: "THE BEST PEEP EVER!")
+    fill_in(:username, with: "Peepz4Dayz")
+    click_button 'Peep It!'
+
+    expect(page).to have_content "THE BEST PEEP EVER! @Peepz4Dayz"
+  end
+end
