5b7a62333f2f1ed2c2e17d3b4648be6221b7cc22
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-25 13:44:57 +0000

    Wrapping id, peep and time data retrieved from database in instance of Peep and refactoring tests to pass

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -2,6 +2,14 @@ require 'pg'
 #rename file
 class Peep
 
+  attr_reader :id, :peep, :time
+
+  def initialize(id:, peep:, time:)
+    @time = time
+    @peep = peep
+    @id = id
+  end
+
   def self.all
     if  ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect( dbname: 'chitter_manager_test' )
@@ -9,8 +17,8 @@ class Peep
       connection = PG.connect( dbname: 'chitter_manager' )
     end
     rs = connection.exec("SELECT * FROM peeps;")
-    rs.map do |peep|
-      peep['peep']
+    rs.map do |post|
+      Peep.new(id: post['id'], peep: post['peep'], time: post['post_time'])
     end
   end
 
@@ -20,7 +28,8 @@ class Peep
     else
       connection = PG.connect( dbname: 'chitter_manager' )
     end
-    connection.exec("INSERT INTO peeps (peep, post_time) VALUES('#{peep}', '#{time}') RETURNING peep, post_time")
+    result = connection.exec("INSERT INTO peeps (peep, post_time) VALUES('#{peep}', '#{time}') RETURNING id, peep, post_time")
+    Peep.new(id: result[0]['id'], peep: result[0]['peep'], time: result[0]['post_time'])
   end
 end
 
--- spec/units/chitter_spec.rb
+++ spec/units/chitter_spec.rb
@@ -6,18 +6,34 @@ describe Peep do
     it 'returns all the peeps' do
       Peep.create(peep: 'I saw twelve chickens in the park earlier', time: time)
       Peep.create(peep: 'There are 9 million bicycles in Beijing', time: time)
-      expect(Peep.all).to include('There are 9 million bicycles in Beijing')  
-      expect(Peep.all).to include('I saw twelve chickens in the park earlier')    
+      chitter_feed = Peep.all
+      # expect(chitter_feed.first.peep).to include('There are 9 million bicycles in Beijing')  
+      expect(chitter_feed.first.peep).to include('I saw twelve chickens in the park earlier')    
+    end
+
+    it 'can access the id and time of the peep' do
+      posted_peep = Peep.create(peep: 'I saw twelve chickens in the park earlier', time: time)
+      Peep.create(peep: 'There are 9 million bicycles in Beijing', time: time)
+
+      peeps = Peep.all
+
+      expect(peeps.length).to eq 2
+      expect(peeps.first).to be_a Peep
+      expect(peeps.first.id).to eq posted_peep.id
+      expect(peeps.first.time).to eq time #may need re thinking
+      expect(peeps.first.peep).to eq 'I saw twelve chickens in the park earlier'
     end
   end
+
   describe '.create' do
     it 'returns the inputted peep' do
-      new_peep = Peep.create(peep: 'Hey, how are ya?', time: time).first
-      expect(new_peep['peep']).to eq('Hey, how are ya?')
+      new_peep = Peep.create(peep: 'Hey, how are ya?', time: time)
+      expect(new_peep.peep).to eq('Hey, how are ya?')
     end
+
     it 'returns the time the peep was created' do
-      new_peep = Peep.create(peep: 'Hey, how are ya?', time: time).first
-      expect(new_peep['post_time']).to eq(time)
+      new_peep = Peep.create(peep: 'Hey, how are ya?', time: time)
+      expect(new_peep.time).to eq(time)
     end
   end
 end
