8857857bf5920e2fc4d906293251d188318e790b
Otis <otisvickersgraver@gmail.com>
2020-10-25 15:58:09 +0000

    refactor to use database_connetion class

--- spec/features/list_peeps_spec.rb
+++ spec/features/list_peeps_spec.rb
@@ -1,6 +1,14 @@
 feature 'user can see list of peeps' do
-  scenario 'user visits home page' do
-    visit '/'
-    expect(page).to have_content('my first peep')
+  scenario 'user create some peeps, and they are displayed on homepage' do
+
+    DatabaseConnection.query("INSERT INTO peeps VALUES(1, 'my first peep');")
+    DatabaseConnection.query("INSERT INTO peeps VALUES(2, 'my second peep');")
+    DatabaseConnection.query("INSERT INTO peeps VALUES(3, 'my third peep');")
+
+    visit('/')
+
+    expect(page).to have_content "my first peep"
+    expect(page).to have_content "my second peep"
+    expect(page).to have_content "my third peep"
   end
 end
