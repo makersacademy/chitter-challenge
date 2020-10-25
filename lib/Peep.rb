c696db8ca3cfb97507c8cb688ced291a3c4d47fc
Otis <otisvickersgraver@gmail.com>
2020-10-25 15:13:14 +0000

    rename peep unit test file

--- lib/Peep.rb
+++ lib/Peep.rb
@@ -5,7 +5,9 @@ class Peep
   def self.list
     result = DatabaseConnection.query("SELECT * FROM peeps")
     result.map do |peep|
+      p peep
       {:id => peep['id'], :message => peep['message']}
     end
   end
+
 end
