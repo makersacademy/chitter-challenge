988cd2c9d6f919c771908bd7b6fa7c77cd2b3fe9
Otis <otisvickersgraver@gmail.com>
2020-10-25 18:53:17 +0000

    in progress: users able to sign up

--- chitter.rb
+++ chitter.rb
@@ -1,19 +1,32 @@
 require 'sinatra/base'
-require_relative './lib/peep.rb'
+require './lib/peep.rb'
+require './lib/database_connection.rb'
 require './database_connection_setup'
 
 class Chitter < Sinatra::Base
+  enable :sessions
+
   get '/' do
+    @username = session[:username]
     @peeps = Peep.list
     erb :index
+  end
+
+  post '/' do
+    Peep.create(message: params[:message])
+    redirect '/'
   end
 
   get '/create_peep' do
     erb :create_peep
   end
 
-  post '/' do
-    Peep.create(message: params[:message])
+  get '/signup' do
+    erb :signup
+  end
+
+  post '/signup' do
+    session[:username] = params[:username]
     redirect '/'
   end
   run! if app_file == $0
