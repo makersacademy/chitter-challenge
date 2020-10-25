9d0c6a696ec34d2e610bd95025384419b7a07b67
Otis <otisvickersgraver@gmail.com>
2020-10-25 15:59:11 +0000

    refactor to use database_connection class

--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -1,8 +1,17 @@
+require 'pg'
+
 describe Peep do
   describe '.list' do
     it 'lists all peeps in database' do
+      DatabaseConnection.query("INSERT INTO peeps VALUES (1, 'my first peep');")
+      DatabaseConnection.query("INSERT INTO peeps VALUES (2, 'my second peep');")
+      DatabaseConnection.query("INSERT INTO peeps VALUES (3, 'my third peep');")
+
       peeps = Peep.list
-      expect(peeps[0][:message]).to eq("my first peep")
+
+      expect(peeps[0][:message]).to include('my first peep')
+      expect(peeps[1][:message]).to include('my second peep')
+      expect(peeps[2][:message]).to include('my third peep')
     end
   end
 end
