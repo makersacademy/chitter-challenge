3e8fa80022d04baf2252bd027e80c34f58ece572
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-25 11:08:15 +0000

    Created new route for posting a peep on the app file

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ class Chitter < Sinatra::Base
   end
 
   get '/chitter_feed' do
-    @peeps = ChitterManager.all
+    @peeps = Peep.all
     erb :chitter_feed
     #pass information from database to views 
+  end
+
+  get '/chitter_feed/new_peep' do
+    erb :new_peep
   end
 end
