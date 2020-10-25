27267fdf872e1fa5541520d8b119a33eaf54f547
Emily Sesto <emilyalicesesto@gmail.com>
2020-10-25 14:14:13 +0000

    Refactored code

--- lib/peep.rb
+++ lib/peep.rb
@@ -14,26 +14,27 @@ class Peep
 
   def self.create(content, user_class = User)
     account = Peep.user(user_class)
-    if account == "Anonymous"
-      time = Time.now.strftime("%d/%m/%Y, %I:%M %p")
-      result = DatabaseConnection.query("INSERT INTO peeps (content, time) VALUES('#{content}', '#{time}') RETURNING id, content, time;")
-      @peep = Peep.new(result[0]['content'], result[0]['id'], result[0]['time'])
-    else 
-      account_id = account.id
-      time = Time.now.strftime("%d/%m/%Y, %I:%M %p")
-      result = DatabaseConnection.query("INSERT INTO peeps (content, time, user_id) VALUES('#{content}', '#{time}', '#{account_id}') RETURNING id, content, time, user_id;")
-      @peep = Peep.new(result[0]['content'], result[0]['id'], result[0]['time'], result[0]['user_id'])
-    end
+    time = current_time
+    result = account == "Anonymous" ? create_anon_peep(content, time) : create_user_peep(content, time, account)
+    @peep = Peep.new(result[0]['content'], result[0]['id'], result[0]['time'], result[0]['user_id'])
+  end
+
+  def self.create_anon_peep(content, time)
+    DatabaseConnection.query("INSERT INTO peeps (content, time) VALUES('#{content}', '#{time}') RETURNING id, content, time;")
+  end
+
+  def self.create_user_peep(content, time, account)
+    DatabaseConnection.query("INSERT INTO peeps (content, time, user_id) VALUES('#{content}', '#{time}', '#{account.id}') RETURNING id, content, time, user_id;")
+  end
+
+  def self.current_time
+    Time.now.strftime("%d/%m/%Y, %I:%M %p")
   end
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM peeps;")
     result.map do |peep|
-      if peep['user_id'].nil?
-        Peep.new(peep['content'], peep['id'], peep['time'])
-      else 
         Peep.new(peep['content'], peep['id'], peep['time'], peep['user_id'])
-      end
     end
   end
 
