674340efba8ffb104b5f74cba9cf6f3b56270566
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-25 18:21:45 +0000

    Added signup feature, created users table on the chitter database, connected to the database from the controller.

--- app.rb
+++ app.rb
@@ -13,5 +13,22 @@ class Chitter < Sinatra::Base
     redirect '/chitter'
   end
 
+  get '/users/signup' do
+    erb :'/users/signup'
+  end
+
+  post '/users/new' do
+    @new_user = params['username']
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec('SELECT * FROM users;')
+    connection.exec("INSERT INTO users (username) VALUES('#{@new_user}')")
+    redirect '/chitter'
+  end
+
+
+  #   get '/users/login' do
+  #   erb :'/users/login'
+  # end
+
   run! if app_file == $0
 end
