ee0ba973a2e3e505da359ecff39bb33454e59a23
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-25 23:05:11 +0000

    Updated database_helper to work with more than one table.

---
+++ spec/units/peeps_spec.rb
@@ -0,0 +1,33 @@
+require './lib/peeps'
+require 'database_helper'
+
+describe Peeps do
+  describe '#.all' do
+    it 'returns a list of peeps' do
+      PG.connect(dbname: 'chitter_test')
+
+      Peeps.add(body: 'Happy Friday!')
+      Peeps.add(body: 'Does chitter remind you of anything? Tweet Tweet')
+      newest_peep = Peeps.add(body: 'testing newest tweet')
+
+      peeps = Peeps.all
+      
+      expect(peeps.length).to eq 3
+      expect(peeps.first).to be_a Peeps
+      expect(peeps.first.id).to eq newest_peep.id
+      expect(peeps.first.body).to eq 'testing newest tweet'
+      expect(peeps.first.post_time).to eq newest_peep.post_time
+    end
+  end
+
+  describe '#.add' do
+    it 'adds a new peep to the peep feed' do
+      new_peep = Peeps.add(body: 'new peep')
+      persisted_data = persisted_data(id: new_peep.id, table: :peeps)
+
+      expect(new_peep).to be_a Peeps
+      expect(new_peep.id).to eq persisted_data['id']
+      expect(new_peep.body).to eq 'new peep'
+    end
+  end
+end
