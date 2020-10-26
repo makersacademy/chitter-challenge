00615380a6de15ac3552780108b970840985d497
Arabella Knowles <arabellaknowles@hotmail.co.uk>
2020-10-26 00:49:01 +0000

    refactored peep class

--- lib/peep.rb
+++ lib/peep.rb
@@ -13,12 +13,8 @@ class Peep
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_manager_test')
-    else
-      connection = PG.connect(dbname: 'chitter_manager')
-    end
-    rs = connection.exec("SELECT * FROM peeps;")
+    database_connection
+    rs = @connection.exec("SELECT * FROM peeps;")
     feed = rs.map do |post| 
       Peep.new(id: post['id'], peep: post['peep'], time: post['post_time'], name: post['name'], username: post['name'])
     end
@@ -26,14 +22,18 @@ class Peep
   end
 
   def self.create(peep:, time:, name:, username:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_manager_test')
-    else
-      connection = PG.connect(dbname: 'chitter_manager')
-    end
-    result = connection.exec("INSERT INTO peeps (peep, post_time, name, username) VALUES('#{peep}', '#{time}', '#{name}', '#{username}') RETURNING id, peep, post_time, name, username")
+    database_connection
+    result = @connection.exec("INSERT INTO peeps (peep, post_time, name, username) VALUES('#{peep}', '#{time}', '#{name}', '#{username}') RETURNING id, peep, post_time, name, username")
     Peep.new(id: result[0]['id'], peep: result[0]['peep'], time: result[0]['post_time'], name: result[0]['name'], username: result[0]['username'])
   end
+
+  private 
+    
+  def self.database_connection
+    if ENV['ENVIRONMENT'] == 'test'
+      @connection = PG.connect(dbname: 'chitter_manager_test')
+    else
+      @connection = PG.connect(dbname: 'chitter_manager')
+    end
+  end  
 end
-# need to return name and username
-# organising posts by reverse chronological order 
