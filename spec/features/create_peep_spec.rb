5ab8623273d820711a0f0326393bf8a85b7054e6
KC Thomas <thisiskcthomas@gmail.com>
2020-10-26 01:21:17 +0000

    Added feature test for timestamps

--- spec/features/create_peep_spec.rb
+++ spec/features/create_peep_spec.rb
@@ -1,12 +1,6 @@
 feature 'Create Peep' do
   scenario 'a user can create a peep' do 
-    visit '/'
-    fill_in(:username, with:"Peepz4Dayz")
-    click_button 'Sign In'
-
-    fill_in(:message, with: "THE BEST PEEP EVER!")
-    click_button 'Peep It!'
-
+    sign_in_and_peep
     expect(page).to have_content "THE BEST PEEP EVER!\n-Peepz4Dayz"
   end
 end
--- spec/web_helpers.rb
+++ spec/web_helpers.rb
@@ -5,3 +5,17 @@ def add_peeps
   Peep.create(message: 'This is my second peep!', creator: 'USER_2')
   Peep.create(message: 'This is my third peep!', creator: 'USER_3')
 end
+
+def parsed_time_now
+  time = DateTime.now
+  time.strftime('%d/%m/%Y %I:%M %p')
+end
+
+def sign_in_and_peep
+  visit '/'
+  fill_in(:username, with:"Peepz4Dayz")
+  click_button 'Sign In'
+
+  fill_in(:message, with: "THE BEST PEEP EVER!")
+  click_button 'Peep It!'
+end
