28af6d4932f70c0cd14aecaa9997239fe36869b1
Mace <maceplus@gmail.com>
2020-10-26 00:23:46 +0000

    add 3 peeps and show them in chronological order

--- spec/features/create_peep_spec.rb
+++ spec/features/create_peep_spec.rb
@@ -6,4 +6,24 @@ feature 'User can make add a peep' do
 
     expect(page).to have_content 'This is my first peep!'
   end
+
+  feature 'peeps are shown in reverse chronological order' do
+    scenario 'visits webpage, adds multiple peeps' do
+      visit('/')
+      fill_in('peep', with: 'This is my first peep!')
+      click_button('PEEP')
+      fill_in('peep', with: 'This is my second peep!')
+      click_button('PEEP')
+      fill_in('peep', with: 'This is my third peep!')
+      click_button('PEEP')
+  
+      expect(page).to have_content('This is my third peep! This is my second peep! This is my first peep!')
+    end
+  end
+  
+
+
+
+
+
 end
