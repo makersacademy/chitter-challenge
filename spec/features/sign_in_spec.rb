0c736ed4aaea597a880cf83e2d40d5435aa0de74
KC Thomas <thisiskcthomas@gmail.com>
2020-10-26 00:56:55 +0000

    Amended feature test for sign in feature.

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -3,7 +3,9 @@ feature 'Sign In' do
     visit '/'
     fill_in(:username, with:"Peepz4Days")
     click_button 'Sign In'
-    expect(page).to have_content "Welcome, Peepz4Days!"
+    
+    expect(page).to have_css('input[name="message"]')
+    expect(page).to_not have_css('input[name="username"]')
   end
 
 
