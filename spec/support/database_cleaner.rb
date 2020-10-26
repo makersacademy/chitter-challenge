3f43994267e8e82f83da65301665043ce8fa531b
Dan Holmes <dh@danielholmes.com>
2020-10-26 17:43:20 +0000

    fix issue causing login test false positive

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -4,6 +4,6 @@ RSpec.configure do |config|
   config.before(:each) do
     DBConnection.exec("TRUNCATE TABLE users RESTART IDENTITY;")
     DBConnection.exec("TRUNCATE TABLE peeps RESTART IDENTITY;")
-    User.create(username: "ds.danielh", full_name: "Dan Holmes", email: "dan@gmail.com", password: "hunter2")
+    User.register(username: "ds.danielh", full_name: "Dan Holmes", email: "dan@gmail.com", password: "hunter2")
   end
 end
