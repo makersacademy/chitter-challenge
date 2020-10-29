27dadaebc5ac363735720a45fe278fd49a0aae36
Ralph Mallett <rmallett12@gmail.com>
2020-10-29 11:31:24 +0000

    Refactor: add database connection query into peep methods

--- lib/peep.rb
+++ lib/peep.rb
@@ -19,19 +19,8 @@ class Peep
   end
 
   def self.create(message:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    result = connection.exec("INSERT INTO peeps (message) VALUES('#{message}')
+    result = DatabaseConnection.query("INSERT INTO peeps (message) VALUES('#{message}')
     RETURNING id, message, time;")
     Peep.new(id: result[0]['id'], message: result[0]['message'], time: result[0]['time'])
-  end
-
-  def self.test
-    connection = PG.connect(dbname: 'chitter')
-    peeps = connection.exec("SELECT * FROM peeps")
-    peeps.map { |peep| peep['message'] }
   end
 end
