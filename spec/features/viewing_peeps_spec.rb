29f7bb3fe3274e417de1b9d7911dc4b434829908
Ralph Mallett <rmallett12@gmail.com>
2020-11-02 11:50:43 +0000

    Test: update viewing peeps test, two peeps added

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,15 +2,13 @@ feature 'viewing peeps' do
   scenario 'user visits peeps page and sees a list of peeps' do
     visit '/peeps'
     sign_up
-    fill_in('message', with: 'Wow it was hot yesterday...')
-    fill_in('message', with: 'Busy doing some coding today')
+    click_link 'Create new peep'
     fill_in('message', with: 'Heading to the pub later!')
     click_button('Peep')
-
-    visit '/peeps'
-
-    expect(page).to have_content 'Wow it was hot yesterday...'
-    expect(page).to have_content 'Busy doing some coding today'
+    click_link 'Create new peep'
+    fill_in('message', with: "Can't belive its Novemeber!")
+    click_button('Peep')
     expect(page).to have_content 'Heading to the pub later!'
+    expect(page).to have_content "Can't belive its Novemeber!"
   end
 end
