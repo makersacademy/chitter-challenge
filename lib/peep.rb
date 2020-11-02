8272a75e3d6549c71e8564a4052b057679b8e6c1
Ralph Mallett <rmallett12@gmail.com>
2020-11-02 11:27:21 +0000

    Test: peep displays author details

--- lib/peep.rb
+++ lib/peep.rb
@@ -16,14 +16,16 @@ class Peep
   def self.all
     result = DatabaseConnection.query("SELECT * FROM peeps ORDER BY time DESC;")
     result.map do |peep|
-      Peep.new(id: peep['id'], message: peep['message'], time: peep['time'], user_id: peep['user_id'])
+      Peep.new(id: peep['id'], message: peep['message'], time: peep['time'],
+        user_id: peep['user_id'])
     end
   end
 
   def self.create(message:, user_id:)
-    result = DatabaseConnection.query("INSERT INTO peeps (message, user_id) VALUES('#{message}', #{user_id})
-    RETURNING id, message, time, user_id;")
-    Peep.new(id: result[0]['id'], message: result[0]['message'], time: result[0]['time'], user_id: result[0]['user_id'])
+    result = DatabaseConnection.query("INSERT INTO peeps (message, user_id) VALUES('#{message}',
+    #{user_id}) RETURNING id, message, time, user_id;")
+    Peep.new(id: result[0]['id'], message: result[0]['message'],
+      time: result[0]['time'], user_id: result[0]['user_id'])
   end
 
   def find_author
