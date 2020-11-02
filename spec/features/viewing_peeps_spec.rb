caf09a085f435248ea89b0ec907f724e9720a294
Ralph Mallett <rmallett12@gmail.com>
2020-11-02 11:42:15 +0000

    Test: update viewing peeps test, remove create peep method

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,9 +2,10 @@ feature 'viewing peeps' do
   scenario 'user visits peeps page and sees a list of peeps' do
     visit '/peeps'
     sign_up
-    Peep.create(message: 'Wow it was hot yesterday...', user_id: 1)
-    Peep.create(message: 'Busy doing some coding today', user_id: 1)
-    Peep.create(message: 'Heading to the pub later!', user_id: 1)
+    fill_in('message', with: 'Wow it was hot yesterday...')
+    fill_in('message', with: 'Busy doing some coding today')
+    fill_in('message', with: 'Heading to the pub later!')
+    click_button('Peep')
 
     visit '/peeps'
 
