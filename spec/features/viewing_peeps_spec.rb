0e51f858fed3f5f5d54e3d1a277da8e1ab4571fc
Ralph Mallett <rmallett12@gmail.com>
2020-11-02 11:34:42 +0000

    Test: update viewing peeps test, sign in required

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -1,6 +1,7 @@
 feature 'viewing peeps' do
   scenario 'user visits peeps page and sees a list of peeps' do
-
+    visit '/peeps'
+    sign_up
     Peep.create(message: 'Wow it was hot yesterday...', user_id: 1)
     Peep.create(message: 'Busy doing some coding today', user_id: 1)
     Peep.create(message: 'Heading to the pub later!', user_id: 1)
