1f6cc0754da96cbedcd23f9563e6dbad9d0abf2c
Symion Edwards <symion.edwards@gmail.com>
2020-11-21 10:23:51 +0000

    First commit. Environment setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,12 @@
+ENV['ENVIRONMENT'] = 'test'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
