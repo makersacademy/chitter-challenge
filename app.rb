20007997a6ce5a2050fd92333ba0e5b8697776d6
Alex Withington-Smith <alex@awithsmith.com>
2020-11-21 11:44:43 +0000

    added error message if account creation fails

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require './database_connection_setup.rb'
 require './lib/user'
 
 class ChitterController < Sinatra::Base
+  enable :sessions
+  register Sinatra::Flash
+
   get '/' do
     erb(:login_page)
   end
@@ -12,8 +16,13 @@ class ChitterController < Sinatra::Base
   end
 
   post '/create-account' do
-    User.create(username: params[:username], email: params[:email], password: params[:password])
-    redirect('/')
+    if User.create(username: params[:username], email: params[:email], password: params[:password])
+      flash[:notice] = "Account created succesfully, log in below!"
+      redirect('/')
+    else
+      flash[:notice] = "Account name/email exist, try again!"
+      redirect('/create-account')
+    end
   end
 
   run! if app_file == $0
