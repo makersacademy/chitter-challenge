783e4c91f51f6890b63ae9436124ed89c53417ca
Adam G <adamwizzy@hotmail.co.uk>
2020-11-21 13:47:54 +0000

    Begin adding views

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require 'sinatra/base'
 class Chitter < Sinatra::Base
   enable :sessions, :method_override # for session variables & _method hack
 
-
+  get '/' do
+    erb(:index)
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
