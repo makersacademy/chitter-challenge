893a331bf64b59f761bccbec9e4277f7d97aa2a4
Margarida Martelo <margmartelo@gmail.com>
2020-11-21 20:54:36 +0000

    Writing and passing second feature test: Being able to submit a peep

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require 'sinatra/base'
 
 class Chitter < Sinatra::Base
   get '/' do
-    erb :index 
+    erb :index
   end
+
+  get '/post_peep' do
+    erb :'post_peep'
+  end
+
 
   run! if app_file == $0
 end
--- spec/features/posting_peeps_spec.rb
+++ spec/features/posting_peeps_spec.rb
@@ -2,6 +2,14 @@ feature 'Posting peeps' do
   scenario 'Have a link to post a peep' do
     visit('/')
 
-    expect(page).to have_link("Peep it!")
+    expect(page).to have_link('Peep it!')
   end
+
+  scenario 'Being able to submit a peep' do
+    visit('/')
+    click_link('Peep it!')
+    expect(page).to have_field('peep')
+    expect(page).to have_button('Post')
+  end
+
 end
