01afbd2d9e97f9eb4646a4e7713c07ff6b48f7e8
Alex Withington-Smith <alex@awithsmith.com>
2020-11-22 16:32:18 +0000

    added documentation

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -3,5 +3,7 @@ require './lib/database_connection.rb'
 if ENV['ENVIRONMENT'] == 'test'
   DatabaseConnection.setup('chitter_test')
 else
+  # use below line if you'd like to run locally
+  # DatabaseConnection.setup('chitter')
   DatabaseConnection.setup_production
 end
