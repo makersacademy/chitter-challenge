6d352da99b3073c8f8c192033d4e95794195b56b
Michael Linford <linford.mike@gmail.com>
2020-11-23 08:32:11 +0000

    my boi travis coming in hot

---
+++ db/migrations/03_creating_peep_table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE chitter(id SERIAL PRIMARY KEY, peep VARCHAR(140), lastmodified TIMESTAMP, username VARCHAR(60));
+ALTER TABLE mytable ALTER COLUMN lastmodified SET DEFAULT CURRENT_TIMESTAMP;
+UPDATE chitter SET lastmodified=CURRENT_TIMESTAMP;
+CREATE OR REPLACE FUNCTION update_lastmodified_column()
+        RETURNS TRIGGER AS '
+  BEGIN
+    NEW.lastmodified = NOW();
+    RETURN NEW;
+  END;
+' LANGUAGE 'plpgsql';
+CREATE TRIGGER update_lastmodified_modtime BEFORE UPDATE
+  ON chitter FOR EACH ROW EXECUTE PROCEDURE
+  update_lastmodified_column();
