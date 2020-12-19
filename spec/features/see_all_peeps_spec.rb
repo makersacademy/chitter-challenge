60c281721d8ed99994a1aaf8b9cf1c0a358c0792
Chris Whitehouse <chris.whitehouse@hotmail.com>
2020-12-19 18:42:11 +0000

    User Story 2: Failing Feature Test

---
+++ spec/features/see_all_peeps_spec.rb
@@ -0,0 +1,13 @@
+feature 'So that I can see what others are saying,' do
+  scenario 'I want to see all peeps in reverse chronological order' do
+    Peep.create(message: 'Peep 1')
+    Peep.create(message: 'Peep 2')
+    Peep.create(message: 'Peep 3')
+
+    visit('/peeps')
+
+    expect(page).to_not have_content 'Sinatra'
+    first('.peep').to have_content 'Peep 3'
+    last('.peep').to have_content 'Peep 1'
+  end
+end
