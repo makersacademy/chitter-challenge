09e2c3f12aa91d139c6995f089ccd1e991ba3356
Chris Whitehouse <chris.whitehouse@hotmail.com>
2020-12-19 15:17:30 +0000

    User Story 1: Feature Test Pass

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 require 'sinatra/base'
 
 class Chitter < Sinatra::Base
-  get '/' do
-    "Hello Chitter"
+  get '/peeps' do
+    erb :'peeps/index'
+  end
+
+  post '/peeps' do
+    $message = params[:message]
+    redirect '/peeps/1'
+  end
+
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  get '/peeps/:id' do
+    $message
+    erb :'peeps/show'
   end
 
   # establish server if file run directly
---
+++ spec/features/new_peep_spec.rb
@@ -0,0 +1,8 @@
+feature 'So that I can let people know what I am doing,' do
+  scenario 'I want to post a message (peep) to chitter' do
+    visit('/peeps/new')
+    fill_in('message', with: 'Test message')
+    click_button('Peep')
+    expect(page).to have_content('Test message')
+  end
+end
