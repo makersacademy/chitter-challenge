3f5f7582df7c259c0cee3ac3605dfe0abcc815ec
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2020-12-22 13:55:00 +0000

    update databse instructions in readme

--- db/migrations/peeps_table.sql
+++ db/migrations/peeps_table.sql
@@ -2,6 +2,7 @@
 CREATE DATABASE chitter;
 -- To create peeps table with id and content columns:
 CREATE TABLE peeps (id SERIAL PRIMARY KEY, content VARCHAR(60));
+ALTER TABLE peeps ADD time VARCHAR(60);
 
 -- To create the test database, follow the above steps exactly but name
 -- the database "chitter_test"
