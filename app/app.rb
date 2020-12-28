e4ce57b97e7d10b449006e3d3a4458236452c32f
Ian Fleming <>
2020-12-28 14:34:52 +0000

    user details appear on all pages

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 require './lib/chitter'
 
 class Chitter_Main < Sinatra::Base
+  enable :sessions
 
   get '/' do
     'Howdy'
@@ -10,21 +11,31 @@ class Chitter_Main < Sinatra::Base
 
 
   post '/create_user' do
-    @email_1 = params[:email]
-    @password_1 = params[:password]
-    @name_1 = params[:name]
-    @user_1 = params[:username]
-    # redirect '/logged_in'
-    erb :logged_in
+    # @email_1 = params[:email]
+    # @password_1 = params[:password]
+    # @name_1 = params[:name]
+    # @user_1 = params[:username]
+    session[:email_1] = params[:email]
+    session[:password_1] = params[:password]
+    session[:name_1] = params[:name]
+    session[:user_1] = params[:username]
+    # erb :logged_in
+    redirect '/logged_in'
   end
 
   get '/logged_in' do
+    @email_1 = session[:email_1]
+    @password_1 = session[:password_1]
+    @name_1 = session[:name_1]
+    @user_1 = session[:user_1]
     erb :logged_in
   end
 
 
   get '/postboard' do
     @chitter = Chitter.all
+    @name_1 = session[:name_1]
+    @user_1 = session[:user_1]
     erb :postboard
   end
 
