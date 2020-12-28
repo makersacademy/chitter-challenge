cd30cb05e4b7287fa18dafd4c840955035cb74f6
jotkafomat <k-j@xl.wp.pl>
2020-12-28 10:17:24 +0000

    add helpers methods to registration spec

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,31 +1,30 @@
+require_relative '../helpers.rb'
+
+include Helpers
+
 feature 'registration' do
+
+  before(:each) do
+    @user = instance_double("Book",
+        :email => 'test@example.com',
+        :password => 'password123',
+        :name => 'rubberduck'
+      )
+  end
+
   scenario 'a maker can sign up' do
-    visit '/peeps'
-    click_button("Sign up")
-    fill_in('email', with: 'test@example.com')
-    fill_in('password', with: 'password123')
-    fill_in('name', with: 'rubberduck')
-    click_button('Submit')
+    sign_up(@user)
 
     expect(page).to have_content "Welcome, rubberduck"
   end
 
   scenario 'if username/email are already in use user get info to try again' do
 
-    visit '/peeps'
-    click_button("Sign up")
-    fill_in('email', with: 'test@example.com')
-    fill_in('password', with: 'password123')
-    fill_in('name', with: 'rubberduck')
-    click_button('Submit')
+    sign_up(@user)
 
     click_button('Sign out')
 
-    click_button("Sign up")
-    fill_in('email', with: 'test@example.com')
-    fill_in('password', with: 'password123')
-    fill_in('name', with: 'rubberduck')
-    click_button('Submit')
+    sign_up(@user)
 
     expect(page).to have_content "Username or email currently in use, please try again"
   end
--- spec/helpers.rb
+++ spec/helpers.rb
@@ -7,4 +7,13 @@ module Helpers
     fill_in(:password, with: user.password)
     click_button('Sign in')
   end
+
+  def sign_up(user)
+    visit '/peeps'
+    click_button('Sign up')
+    fill_in(:email, with: user.email)
+    fill_in(:password, with: user.password)
+    fill_in('name', with: 'rubberduck')
+    click_button('Submit')
+  end
 end
