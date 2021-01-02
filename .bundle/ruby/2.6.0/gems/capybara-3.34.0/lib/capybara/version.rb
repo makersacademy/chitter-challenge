49ab7b50ba978e5d8cab5186be2640d9cd88b754
Ara <hello.ara@pm.me>
2021-01-02 20:02:44 +0100

    remove bundle

--- .bundle/ruby/2.6.0/gems/capybara-3.34.0/lib/capybara/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Capybara
-  VERSION = '3.34.0'
-end
--- .bundle/ruby/2.6.0/gems/rspec-support-3.9.2/lib/rspec/support/version.rb
+++
@@ -1,7 +0,0 @@
-module RSpec
-  module Support
-    module Version
-      STRING = '3.9.2'
-    end
-  end
-end
