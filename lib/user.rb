b7a1888f791e857984fc6ffa141fd016080bb999
Chris Abdo <christophermabdo@icloud.com>
2021-01-02 18:20:21 +0000

    adding unit tests

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,7 @@
 require 'pg'
 
 class User
+attr_reader :id, :name, :email, :username, :password
 
     def initialize(id:, name:, email:, username:, password:)
         @id = id
@@ -13,7 +14,7 @@ class User
     def self.newuser(id:, name:, email:, username:, password:)
       connection = PG.connect(dbname: 'chitter')
       user = connection.exec("INSERT INTO users (name, email, username, password) VALUES('#{name}', '#{email}', '#{username}','#{password}') RETURNING id, name, email, username, password;")
-      User.new(id: user[0]['id'], name: user[0]['name'], email: user[0]['email'], username: user[0]['username'], password: password[0]['password'])
+      User.new(id: user[0]['id'], name: user[0]['name'], email: user[0]['email'], username: user[0]['username'], password: user[0]['password'])
     end
 
 end
