5a3860828387f384927eef00a6ff4c4062ea91fc
Ara <hello.ara@pm.me>
2021-01-02 23:28:42 +0100

    update travis file

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,7 +3,7 @@ p "Setting up the test database..."
 
 def wipe_clean
   @connection = PG.connect(dbname: 'chitter_test')
-  @connection.exec("TRUNCATE messages CASCADE;")
-  @connection.exec("TRUNCATE users CASCADE;")
-  @connection.exec("TRUNCATE tag;")
+  # @connection.exec("TRUNCATE messages CASCADE;")
+  # @connection.exec("TRUNCATE users CASCADE;")
+  # @connection.exec("TRUNCATE tag;")
 end
