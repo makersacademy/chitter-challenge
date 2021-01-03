49d71e7bb8d2568a85f500932c4e878d63ca28ed
Sujeevan Rajagopalan <rsujee@MA307.local>
2021-01-03 17:30:24 +0000

    Chitter Challenge in progress

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,15 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+ENV['ENVIRONMENT'] = 'test'
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
