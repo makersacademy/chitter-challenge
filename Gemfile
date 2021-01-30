aae913b1f37b05e0a4e20a9c3df0a59acd550f11
Karsten Finlay <karstenfinlay@hotmail.com>
2021-01-30 08:53:07 +0000

    Setup for Chitter project

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ source 'https://rubygems.org'
 group :test do
   gem 'rspec'
   gem 'rubocop', '0.79.0'
+  gem 'capybara'
+  gem 'pg'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'rspec-sinatra'
+  gem 'sinatra'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,17 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
+
+ENV['ENVIRONMENT'] = 'test'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
