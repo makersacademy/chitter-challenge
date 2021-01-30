40833ac19469a4365ca608e70ffb5c086633ac34
twigz826 <thomastwigden@gmail.com>
2021-01-29 21:24:41 -0300

    Updates to setup_test_database file

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,7 +3,7 @@ require 'pg'
 p "Setting up test database..."
 
 def setup_test_database
-  connection = PG.connect(dbname: 'bookmark_manager_test')
-  #Clear the bookmarks table
-  connection.exec("TRUNCATE bookmarks;")
+  connection = PG.connect(dbname: 'chitter_test')
+  #Clear the peeps table
+  connection.exec("TRUNCATE peeps;")
 end
