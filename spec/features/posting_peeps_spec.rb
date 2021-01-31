865157ce2bebb20e0cc19765d7bf0efecc1ddae6
twigz826 <thomastwigden@gmail.com>
2021-01-31 12:05:54 -0300

    Wrote a feature test for posting a peep

---
+++ spec/features/posting_peeps_spec.rb
@@ -0,0 +1,9 @@
+feature 'Posting a new peep' do
+  scenario 'A user can post a peep to Chitter' do
+    visit('/peeps/new')
+    fill_in('peep', with: 'God I love coding so much I could cry')
+    click_button('Peep')
+
+    expect(page).to have_content 'God I love coding so much I could cry'
+  end
+end
