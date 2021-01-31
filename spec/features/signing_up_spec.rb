08d0ed7992dd58fd3c254c690d6ee4c5d39ad34c
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-31 21:00:29 +0000

    Not showing sign up button when you have signed up

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -7,6 +7,9 @@ feature 'signing up as a user' do
     scenario 'it shows a welcome message' do
       expect(page).to have_content("Welcome to Chitter Steve Mandanda")
     end
+    scenario 'it no longer shows the sign up button' do
+      expect(page).not_to have_selector('.go_to_signup')
+    end
   end
 
 end
