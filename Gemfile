d85fa03e11907746a1f9baecc7fc1b29b43e3720
Katy Day <day.katy@gmail.com>
2021-01-31 17:56:48 +0000

    updates

--- Gemfile
+++ Gemfile
@@ -1,11 +1,15 @@
 source 'https://rubygems.org'
 
+
+  gem 'sinatra'
+  gem 'capybara'
+  gem 'pg'
+  gem 'bcrypt'
+  gem 'sinatra-flash'
+
 group :test do
   gem 'rspec'
   gem 'rubocop', '0.79.0'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
-  gem 'sinatra'
-  gem 'capybara'
-  gem 'pg'
 end
--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 require './lib/peep'
 require './database_connection_setup'
+require 'sinatra/flash'
+require './lib/user'
+require_relative './session_helper'
 
 class Chitter < Sinatra::Base 
+  enable :sessions, :method_override
+  register Sinatra::Flash
+  include SessionHelper
 
   get '/' do
     redirect '/peeps'
   end
 
   get '/peeps' do
+    @user = current_user
     @peeps = Peep.all
     erb :'peeps/index'
   end
@@ -23,10 +30,31 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    User.create(email: params[:email], password: params[:password])
-    redirect '/bookmarks'
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect '/peeps'
   end
 
+  get '/sessions/new' do
+    erb :"sessions/new"
+  end
+
+  post '/sessions' do
+    user = User.authenticate(email: params[:email], password: params[:password])
+    if user
+      session[:user_id] = user.id
+      redirect '/peeps'
+    else
+      flash[:notice] = "Please check your email or password."
+      redirect '/sessions/new'
+    end
+  end
+
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = "You have signed out."
+    redirect '/peeps'
+  end
 
   run! if app_file == $0
 
