00a5e52507fb37b31ba03809077b57a2956e69d5
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-31 11:10:19 +0000

    Refactoring peep to use DBConnection class

--- lib/peep.rb
+++ lib/peep.rb
@@ -10,16 +10,12 @@ class Peep
   end
 
   def self.new_peep(content)
-    database = ENV['Rack ENV'] == 'test' ? 'chitter_test' : 'chitter'
-    connection = PG.connect(dbname: database)
-    results = connection.exec("insert into peep (content) values ('#{content}') returning id, content").first
+    results = DBConnection.query("insert into peep (content) values ('#{content}') returning id, content").first
     Peep.new(results['id'].to_i, results['content'])
   end
 
   def self.all
-    database = ENV['Rack ENV'] == 'test' ? 'chitter_test' : 'chitter'
-    connection = PG.connect(dbname: database)
-    results = connection.exec 'select * from peep'
+    results = DBConnection.query('select * from peep')
     results.map { |row| Peep.new(row['id'].to_i, row['content'])}
   end
 
