c0aa228c9ec88ef79598da647937ebd575af522b
CodeRed30 <katrinaharradine@gmail.com>
2021-01-31 23:18:58 +0000

    Sign out

--- lib/user.rb
+++ lib/user.rb
@@ -13,7 +13,6 @@ class User
   end
 
   def self.create(name, username, email, password)
-    # encrypt the password
     encrypted_password = BCrypt::Password.create(password)
 
     result = DatabaseConnection.query("INSERT INTO users (name, username, email, password) VALUES ('#{name}', '#{username}', '#{email}', '#{encrypted_password}') RETURNING id, email, password, name, username;")
@@ -22,7 +21,7 @@ class User
 
   def self.find(id)
     return nil unless id
-    result = DatabaseConnection.query("SELECT * FROM users WHERE id = id")
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = '#{id}'")
     User.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
   end
 
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -35,4 +35,19 @@ feature 'authentication' do
    expect(page).not_to have_content 'Welcome, test@example.com'
    expect(page).to have_content 'Please check your email or password.'
  end
+
+ scenario 'a user can sign out' do
+  User.create('John Doe', 'JDoe', 'test@example.com', 'password123')
+
+  visit '/sessions/new'
+  fill_in(:email, with: 'test@example.com')
+  fill_in(:password, with: 'wrongpassword')
+  click_button('Sign in')
+
+  # Sign out
+  click_button('Sign out')
+
+  expect(page).not_to have_content 'Welcome, test@example.com'
+  expect(page).to have_content 'You have signed out.'
+ end
 end
