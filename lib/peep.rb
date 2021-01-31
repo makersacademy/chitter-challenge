033f2ecdd5a056e66738fbe1d60c1aae5dce3343
Oscar Bertrand <oscarb1989@yahoo.co.uk>
2021-01-31 10:58:37 +0000

    Passed tests user story 3

--- lib/peep.rb
+++ lib/peep.rb
@@ -17,10 +17,11 @@ class Peep
     end
 
     result = connection.exec('SELECT * FROM peeps')
-    result.map do |peep|
+    x = result.map do |peep|
       Peep.new(peep: peep['peep'], created_at: peep['created_at'])
     end
     .reverse
+    p x
   end
 
   def self.create(peep:, created_at: Time.now)
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,24 +1,30 @@
 describe '.all' do
   it 'returns a list of peeps' do
-  
+    connection = PG.connect(dbname: 'peep_manager_test')
+
     Peep.create(peep: 'Salut!')
-    Peep.create(peep: 'Ca farte?!')
+    Peep.create(peep: 'Ca farte?')
 
     peeps = Peep.all
 
-    expect(peeps).to include "Salut!"
-    expect(peeps).to include "Ca farte?"
+    expect(peeps.last.peep).to eq "Salut!"
+    expect(peeps.first.peep).to eq "Ca farte?"
   end
 
   it 'returns a list of reversed peeps' do
-    
-    Peep.create(peep: 'Salut!')
-    Peep.create(peep: 'Ca farte?!')
+    connection = PG.connect(dbname: 'peep_manager_test')
+
+    Peep.create(peep: 'Salut!', created_at: '2021-01-31 10:42:42+00')
+    Peep.create(peep: 'Ca farte?', created_at: '2021-01-31 10:43:42+00')
 
     peeps = Peep.all
 
-    expect(peeps.first).to eq "Ca farte?"
-    expect(peeps.last).to eq "Salut!"
+    expect(peeps.length).to eq 2
+    expect(peeps.first.peep).to eq 'Ca farte?'
+    expect(peeps.last.peep).to eq 'Salut!'
+    expect(peeps.first.created_at).to eq '2021-01-31 10:43:42+00'
+    expect(peeps.last.created_at).to eq '2021-01-31 10:42:42+00'
+
   end
 end
 
@@ -26,6 +32,6 @@ describe '.create' do
   it 'creates a new peep' do
     Peep.create(peep: 'Kikoo!')
 
-    expect(Peep.all).to include 'Kikoo!'
+    expect(Peep.all.first.peep).to eq 'Kikoo!'
   end
 end
