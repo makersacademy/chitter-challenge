e6ee6d07e084755214d61f5df29894a8d4a087c7
bateso88 <bateso@hotmail.co.uk>
2021-01-31 16:21:34 +0000

    100% coverage and (almost) all functionality. Still need to put in checks to ensure unique user names and emails

--- spec/features/feed_page_spec.rb
+++ spec/features/feed_page_spec.rb
@@ -2,5 +2,7 @@ feature 'Viewing peeps' do
   scenario 'shows all peeps' do
     create_new_user_and_write_a_peep
     expect(page).to have_content('Hello this is a peep')
+    expect(page).to have_content('test')
+    expect(page).to have_content('testing')
   end
 end
