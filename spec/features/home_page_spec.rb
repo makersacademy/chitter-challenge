0bd97ef19b67d89f45a4c601f367c9f9288b9b2e
RTurney <r.turney93@gmail.com>
2021-01-31 12:02:17 +0000

    added feature test for posting a peep

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,4 +4,11 @@ feature 'Homepage' do
     visit('/')
     expect(page).to have_content 'Welcome to Chitter'
   end
+
+  scenario 'User posts a Peep' do
+    visit('/')
+    fill_in :peep, with: 'My first peep!'
+    click_button('Peep')
+    expect(page).to have_content('My first peep!')
+  end
 end
