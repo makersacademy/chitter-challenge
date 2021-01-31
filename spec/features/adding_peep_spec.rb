c51f0ab7070887ade4719799f7639a11556a614f
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-31 14:39:47 +0000

    Listing peeps by reverse created order

--- spec/features/adding_peep_spec.rb
+++ spec/features/adding_peep_spec.rb
@@ -1,11 +1,21 @@
 feature 'Adding a peep' do
 
-  scenario 'Adding a peep shows it on the home page' do
+  scenario 'Shows it on the home page' do
     visit('/')
     fill_in('content', with: "peep peep, I'm in me mum's car")
     click_button('Share')
     expect(page.status_code).to eq 200
     expect(page).to have_content("peep peep, I'm in me mum's car")
+  end
+
+  scenario 'lists peeps by reverse chronological order' do
+    visit('/')
+    fill_in('content', with: "peep peep, I'm in me mum's car")
+    click_button('Share')
+    fill_in('content', with: "I'm no longer in me mum's car")
+    click_button('Share')
+    expect(page.status_code).to eq 200
+    expect(page.body.index("I'm no longer in me mum's car")).to be < page.body.index("peep peep, I'm in me mum's car")
   end
 
 end
