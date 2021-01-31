e3c80c33100e33e01686620041035bec2796a952
twigz826 <thomastwigden@gmail.com>
2021-01-31 20:49:56 -0300

    All tests passing

--- spec/unit/peep_spec.rb
+++ spec/unit/peep_spec.rb
@@ -1,4 +1,5 @@
 require 'peep'
+require 'database_helpers'
 
 describe Peep do
   describe '.all' do
@@ -22,10 +23,10 @@ describe Peep do
     it 'creates a new peep' do
 
       peep = Peep.create(peep: 'Red bull gives you wings', username: 'timmy_toes')
-      persisted_data = PG.connect(dbname: 'chitter_test').query("SELECT * FROM peeps WHERE id = #{peep.id};")
+      persisted_data = persisted_data(id: peep.id)
 
       expect(peep).to be_a Peep
-      expect(peep.id).to eq persisted_data.first['id']
+      expect(peep.id).to eq persisted_data['id']
       expect(peep.peep).to eq 'Red bull gives you wings'
       expect(peep.username).to eq 'timmy_toes'
     end
