b4c133a10ccf30fa8961f2ffdc089919adf4193f
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-31 13:35:44 +0000

    Refactoring insert method of db connection to add prepare sql string private class method

--- lib/db_connection.rb
+++ lib/db_connection.rb
@@ -15,10 +15,16 @@ class DBConnection
   end
 
   def self.insert(table:, columns:, values:)
-    value_subs = values.each_with_index.map { |val, ind| "$#{ind + 1}"}
-    @connection.prepare('insert_statement', "insert into #{table} (#{columns.join(',')}) values (#{value_subs.join(',')}) returning id, #{columns.join(',')}")
+    @connection.prepare('insert_statement', prepare_sql_string(table, columns, values))
     @connection.exec_prepared('insert_statement', values)
   end
 
+  def self.prepare_sql_string(table, columns, values)
+    value_subs = values.each_with_index.map { |val, ind| "$#{ind + 1}"}.join(',')
+    columns_string = columns.join(',')
+    return_columns = columns.unshift('id').join(',')
+    "insert into #{table} (#{columns_string}) values (#{value_subs}) returning #{return_columns}"
+  end
 
+  private_class_method :prepare_sql_string
 end
