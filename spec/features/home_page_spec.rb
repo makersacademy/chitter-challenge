d2d71a57bfe178f46e122c042ec0028bd0fc5a5b
Hugh Cavanagh <hacavanagh@gmail.com>
2021-01-31 19:12:30 +0000

    Adding Button for signup

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,5 +4,11 @@ feature 'Home Page' do
     visit('/')
     expect(page).to have_content('Welcome to Chitter!')
   end
-  
+
+  scenario 'Has a button to sign up when you first visit' do
+    visit('/')
+    click_button('Sign up for Chitter')
+    expect(page.current_path).to eq '/users/new'
+  end
+
 end
