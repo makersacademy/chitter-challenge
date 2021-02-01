8a2af573f9ccc5e18561595e979ed9ed4c5403fe
PetarSimonovic <petar@simonovic.com>
2021-02-01 09:37:48 +0000

    Travis table fix

--- db/migrations/01_create_peeps_table.sql
+++ db/migrations/01_create_peeps_table.sql
@@ -1,3 +1,1 @@
 CREATE TABLE peeps(id SERIAL PRIMARY KEY, date DATE, time TIME(0), message VARCHAR, sender VARCHAR);
-
-CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR, password VARCHAR, email VARCHAR);
