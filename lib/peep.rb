b05ae96edeab16fbb7cb45d8b7f1c9172f933125
olga <itsglimerry@gmail.com>
2021-02-26 18:00:11 +0000

    Added timestamp to each new peep

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,11 +1,12 @@
 require 'pg'
 
 class Peep
-  attr_reader  :id, :content
+  attr_reader  :id, :content, :created_at
 
-  def initialize(id:, content:)
+  def initialize(id:, content:, created_at:)
     @id = id
     @content = content
+    @created_at = created_at
   end
 
   def self.all
@@ -17,7 +18,7 @@ class Peep
     end
 
     result = connection.exec("SELECT * FROM messages;")
-    result.map {|message| Peep.new(id: message['id'], content: message['content'] )}
+    result.map {|message| Peep.new(id: message['id'], content: message['content'], created_at: message['created_at'] )}
   end
 
 
@@ -30,8 +31,8 @@ class Peep
     end
 
 
-    result = connection.exec("INSERT INTO messages (content) VALUES ( '#{content}') RETURNING id, content")
-    Peep.new( id: result[0]['id'], content: result[0]['content'] )
+    result = connection.exec("INSERT INTO messages (content) VALUES ( '#{content}') RETURNING id, content, created_at")
+    Peep.new( id: result[0]['id'], content: result[0]['content'], created_at: result[0]['created_at'] )
   end
 
 end
