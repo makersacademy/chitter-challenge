edf286eb0cabc8b86f1c8ae756f625618f8ce802
Raf Swiderski <rafswid@icloud.com>
2021-02-27 21:12:48 +0000

    post spec and class

---
+++ lib/post.rb
@@ -0,0 +1,35 @@
+require 'pg'
+
+class Post
+
+    attr_reader :id, :name, :username, :content
+
+    def initialize(id:, name:, username:, content:)
+        @id = id
+        @name = name
+        @username = username
+        @content = content
+    end
+    
+    def self.all
+        if ENV['ENVIRONMENT'] == 'test' 
+            connection = PG.connect :dbname => 'chitter_test'
+        else 
+            connection = PG.connect :dbname => 'chitter'
+        end
+        result = connection.exec("SELECT * FROM posts;")
+        result.map do |post|
+        Post.new(id: post['id'], name: post['name'], username: post['username'], content: post['content'])
+        end
+    end
+        
+    def self.create(name:, username:, content:)
+        if ENV['ENVIRONMENT'] == 'test' 
+            connection = PG.connect :dbname => 'chitter_test'
+        else 
+            connection = PG.connect :dbname => 'chitter'
+        end
+        result = connection.exec("INSERT INTO posts (name, username, content) VALUES('#{name}', '#{username}', '#{content}') RETURNING id, name, username, content;")
+        Post.new(id: result[0]['id'], name: result[0]['name'], username: result[0]['username'], content: result[0]['content'])
+    end    
+end
---
+++ spec/unit/post_spec.rb
@@ -0,0 +1,13 @@
+describe Post do
+    describe '#self.create' do
+        it 'stores the post details on the chitter database' do
+            Post.create(name: 'Raf Swiderski', username: 'rafswid', content: 'Hello Chitter')
+            posts = Post.all
+            expect(posts.length).to eq 1
+            expect(posts.first).to be_a Post
+            expect(posts.first.name).to eq 'Raf Swiderski'
+            expect(posts.first.username).to eq 'rafswid'
+            expect(posts.first.content).to eq 'Hello Chitter'
+        end
+    end
+end
