d07932816444b84378bed9d2609ebd5bcb609f43
Raf Swiderski <rafswid@icloud.com>
2021-02-27 13:30:59 +0000

    create database

---
+++ db/migrations/01_create_db_of_details.sql
@@ -0,0 +1,1 @@
+CREATE TABLE details (id SERIAL PRIMARY KEY, email VARCHAR(60), password VARCHAR(60), name VARCHAR(60), username VARCHAR(60));
--- spec/features/signup_button_spec.rb
+++ spec/features/signup_button_spec.rb
@@ -1,13 +1,11 @@
 feature 'signup option' do
     scenario 'i can see a button called signup' do
         visit('/')
+        expect(page).to have_content 'Sign Up'
+    end
+    scenario 'i can visit the signup page' do
+        visit('/')
         click_link 'Sign Up'
         expect(page).to have_content 'Sign Up Page'
     end
-    # scenario 'i can fill in my email, password, name and a username' do
-    #     visit('/signup')
-    #     fill_in :email, with: 'rafswid@icloud.com'
-    #     fill_in :password, with: 'cabbage'
-    #     fill_in :name, with: 'Raf Swiderski'
-    #     fill_in :username, with: 'rafswid'
 end
