79018c11666d955604b66db84b52e92eb5c2f51f
Jack Gumoes <j.gumoes@gmail.com>
2021-02-28 17:42:59 +0000

    visitors can now post anonymously

--- app.rb
+++ app.rb
@@ -9,5 +9,13 @@ class Chitter < Sinatra::Base
     erb :'main'
   end
 
+  post '/postPeep' do
+    anon_names = ['Mr Mystery Man', 'Anonababes', 'The silent one', "Who Dis?"]
+    params.each { |p| puts p}
+    params['username'] == nil ? user = anon_names.sample : user = params['username']
+    Peep.create(peep: params['peepMessage'], username: user, timestamp: Time.now.to_i)
+    redirect '/'
+  end
+
   run! if app_file == $0
 end
