36036d75d6d9674d5fd07a82b8cda5d853038087
Adam Jonas <adamjonas25@gmail.com>
2021-03-04 16:39:40 +0000

    change pg connection in db connection

--- app/lib/database_connection.rb
+++ app/lib/database_connection.rb
@@ -3,16 +3,16 @@ require 'pg'
 class DatabaseConnection
   class << self
     def setup(database)
-      if ENV['DATABASE_HOST']
+      # if ENV['DATABASE_HOST']
         self.connection = PG.connect(
         host: ENV['DATABASE_HOST'],
         dbname: ENV['DATABASE_NAME'],
         user: ENV['DATABASE_USER'],
         password: ENV['DATABASE_PASSWORD']
       )
-      else
-        self.connection = PG.connect(dbname: database)
-      end
+      # else
+      #   self.connection = PG.connect(dbname: database)
+      # end
     end
 
     def query(sql_query)
