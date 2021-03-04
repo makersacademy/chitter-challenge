a6ee0f86f22d00e5737b71468b82ce216b01c2ac
Adam Jonas <adamjonas25@gmail.com>
2021-03-04 18:20:56 +0000

    refactors setup method, removes comments

--- app/lib/database_connection.rb
+++ app/lib/database_connection.rb
@@ -3,7 +3,6 @@ require 'pg'
 class DatabaseConnection
   class << self
     def setup(database)
-
       self.connection = PG.connect(
         host: 'ec2-54-242-43-231.compute-1.amazonaws.com',
         port: '5432',
@@ -11,7 +10,7 @@ class DatabaseConnection
         user: 'gxhgqxcjdyefqi',
         password: '35c1be836d9ea060b87e618f0a05d1bef3d7de2daa775ba39f8963ee3e0d6590'
       )
-      end
+    end
 
     def query(sql_query)
       connection.exec(sql_query)
