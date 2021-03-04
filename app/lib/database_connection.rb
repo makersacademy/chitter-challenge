2ade0cdd49b33f32f03cf5f3d5a2349d67e943ab
Adam Jonas <adamjonas25@gmail.com>
2021-03-04 18:14:11 +0000

    adds puts statements for env vars in setup method

--- app/lib/database_connection.rb
+++ app/lib/database_connection.rb
@@ -3,18 +3,31 @@ require 'pg'
 class DatabaseConnection
   class << self
     def setup(database)
-      # if ENV['DATABASE_HOST']
-        self.connection = PG.connect(
+      puts ENV['DATABASE_HOST']
+      puts ENV['DATABASE_NAME']
+      puts ENV['DATABASE_USER']
+      puts ENV['DATABASE_PASSWORD']
+
+           # if ENV['DATABASE_HOST']
+      self.connection = PG.connect(
         host: ENV['DATABASE_HOST'],
         port: 5432,
         dbname: ENV['DATABASE_NAME'],
         user: ENV['DATABASE_USER'],
         password: ENV['DATABASE_PASSWORD']
       )
-      # else
-      #   self.connection = PG.connect(dbname: database)
-      # end
-    end
+        # else
+        #   self.connection = PG.connect(dbname: database)
+        # end
+      end
+
+      # if production - specify host / port
+      # if ENV['DATABASE_HOST']
+        # host = ...
+        # user = ....
+        # ^^
+        # %w[host port options tty dbname user password]
+        # https://www.rubydoc.info/gems/pg/PG/Connection
 
     def query(sql_query)
       connection.exec(sql_query)
@@ -25,11 +38,3 @@ class DatabaseConnection
     attr_accessor :connection
   end
 end
-
-      # if production - specify host / port
-      # if ENV['DATABASE_HOST']
-        # host = ...
-        # user = ....
-        # ^^
-        # %w[host port options tty dbname user password]
-        # https://www.rubydoc.info/gems/pg/PG/Connection
