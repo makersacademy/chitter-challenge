851237bb5819c2ddee8a107219ddfc1c758022d3
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-25 13:23:58 +0000

    updated spec_helper and added class spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,9 +17,13 @@ ENV['ENVIRONMENT'] = 'test'
 Capybara.app = Chitter
 
 RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  
+  config.shared_context_metadata_behavior = :apply_to_host_groups
 end
