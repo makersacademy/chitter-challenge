6a765c0ec36e0a421bb47251da45f5373869359e
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-27 15:29:26 +0000

    updated user to introduce new_user class method

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,16 @@
 class User
-  attr_reader :user_id, :username, :password, :email
 
-  def initialize(user_id:, username:, password:, email:)
+  def self.new_user(username, password, email)
+    new_user = DbConnection.query("INSERT INTO users(username, password, email) VALUES ('#{username}', '#{password}', '#{email}') 
+                                   RETURNING user_id, username, email;")
+    User.new(user_id: new_user[0]['user_id'], username: new_user[0]['username'], email: new_user[0]['email'])
+  end
+
+  attr_reader :user_id, :username, :email
+
+  def initialize(user_id:, username:, email:)
     @user_id = user_id
     @username = username
-    @password = password
     @email =  email
   end
 end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,10 +1,25 @@
 require 'user'
 
-describe Peep do
+describe User do
+  describe 'self.new_user' do
+    it 'creates a new user on the database and a instance of User' do
+      user = User.new_user('littletrout', 'password1', 'littletrout@notadomain.com')
+
+      stored_data = stored_user(table: 'users', user_id: user.user_id)
+      
+      expect(user).to be_a(User)
+      expect(user.user_id).to eq(stored_data.first['user_id'])
+      expect(user.username).to eq(stored_data.first['username'])
+      expect(user.email).to eq(stored_data.first['email'])
+    end
+  end
+
   describe 'Contains the data for a user' do
-    it "has an id, username, password and email address" do
-      user = User.new(user_id: '1', username: 'snowy', password: 'password1', email: 'snowy@gmail.com')
+    it "has an id, username, and email address" do
+      user = User.new(user_id: '1', username: 'snowy', email: 'snowy@gmail.com')
       expect(user).to respond_to(:username)
+      expect(user).to respond_to(:user_id)
+      expect(user).to respond_to(:email)
     end
   end
 end
