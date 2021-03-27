282d4167f450adeae8929c9735c0bbe82f450919
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-03-27 11:57:11 +0000

    sign up link, form and confirmation

--- app.rb
+++ app.rb
@@ -5,4 +5,16 @@ class Chitter < Sinatra::Base
   get '/' do
     erb :chitter
   end
-end
+
+  get '/sign_up' do
+    erb :sign_up
+  end
+
+  post '/peeps' do
+    redirect '/peeps'
+  end
+
+  get '/peeps' do
+    erb :peeps
+  end
+ end
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,6 +4,26 @@ require 'spec_helper'
 feature 'Chitter homepage' do
   scenario 'homepage has a chitter header' do
     visit '/'
-    expect(page).to have_content "Chitter"
+    expect(page).to have_content 'Chitter'
   end
+end
+
+feature 'sign up to chitter' do
+  scenario 'a user can click a link to sign up' do
+    visit '/'
+    click_link 'Sign Up'
+    expect(page).to have_content 'Sign up to Chitter'
+  end
+
+  scenario 'user can fill in user information to sign up' do
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'name', with: 'Lou Reade'
+    fill_in 'username', with: 'loushark'
+    fill_in 'email', with: 'loushark@gmail.com'
+    fill_in 'password', with: 'Sharks@reC00l'
+    click_button 'sign_up'
+    expect(page).to have_content 'you are signed in'
+  end
+
 end
