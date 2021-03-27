7b77379bcd9b49f564ec84a75a0d356a03dd2b99
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-27 15:30:04 +0000

    refined chitter for removal of peep and user methods

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,26 +1,23 @@
 require './lib/db_connection'
 require './lib/peep'
+require './lib/user'
 
 class Chitter
   
-  def self.send_peep(message, user_id)
-    DbConnection.query("INSERT INTO peeps(message, user_id) VALUES ('#{message}', '#{user_id}');")
-  end
-
   def self.all_peeps
     @peeps = []
     DbConnection.query("SELECT * FROM peeps ORDER BY created_at DESC;").each do |peep|
-      @peeps << Peep.new(id: peep['id'], message: peep['message'], user_id: peep['user_id'], 
-                created_at: peep['created_at'])
+      @peeps << Peep.new(peep_id: peep['peep_id'], message: peep['message'], 
+                         user_id: peep['user_id'], created_at: peep['created_at'])
     end
     return @peeps
   end
 
   def self.all_users
-    @users
-    DbConnection.query("SELECT * FROM users ORDER BY created_at ASC;").each do |peep|
-      @users << User.new(id: peep['id'], message: peep['message'], user_id: peep['user_id'], 
-                created_at: peep['created_at'])
+    @users = []
+    DbConnection.query("SELECT * FROM users;").each do |user|
+      @users << User.new(user_id: user['user_id'], username: user['username'], 
+                         email: user['email'])
     end
     return @users
   end
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -1,10 +1,23 @@
 require 'chitter'
 
 describe Chitter do
-  describe '#send_peep' do
-    it 'records the peep in the chitter db table peeps' do
-      Chitter.send_peep('My first peep!', '1')
-      expect(Chitter.all_peeps.last.message).to eq('My first peep!')
+  describe 'self.all_peeps' do
+    it 'returns all peeps as an array of peeps' do
+      peeps = Chitter.all_peeps
+      
+      expect(peeps.last).to be_a(Peep)
+      expect(peeps.first.peep_id).to eq('3')
+      expect(peeps.first.message).to eq('How do you do this?')
     end
-  end
+  end    
+
+  describe 'self.all_users' do
+    it 'returns all users as an array of users' do
+      users = Chitter.all_users
+      
+      expect(users.last).to be_a(User)
+      expect(users.last.user_id).to eq('3')
+      expect(users.first.email).to eq('captainfrog@notadomain.com')
+    end
+  end  
 end
