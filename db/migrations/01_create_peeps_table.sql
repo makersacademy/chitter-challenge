675fd8b445df9320c436e8d05998644215dd7b27
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-03-28 15:39:27 +0100

    Update 01_create_peeps_table.sql

--- db/migrations/01_create_peeps_table.sql
+++ db/migrations/01_create_peeps_table.sql
@@ -1,2 +1,4 @@
 CREATE TABLE peeps(id SERIAL PRIMARY KEY, body VARCHAR(140), date VARCHAR, username VARCHAR);
 CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(50), username VARCHAR(20), email VARCHAR(50), password VARCHAR(20));
+                                                                                                                      
+                                                                                                                      
