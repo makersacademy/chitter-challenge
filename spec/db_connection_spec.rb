fc4be9ecc287438464b07a224e539d5dd85c0282
StuBehan <33100049+StuBehan@users.noreply.github.com>
2021-03-28 00:09:43 +0000

    continuity

--- spec/db_connection_spec.rb
+++ spec/db_connection_spec.rb
@@ -1,14 +1,14 @@
 require 'db_connection'
 
 describe DbConnection do
-  describe '#setup' do
+  describe 'self.setup' do
     it 'sets up the database connection to the specified database' do
       expect(PG).to receive(:connect).with(:dbname => 'chitter_test')
       DbConnection.setup('chitter_test')
     end
   end
 
-  describe '#query' do
+  describe 'self.query' do
     it 'takes a string and actions it on the current server' do
       DbConnection.setup('chitter_test')
       expect(DbConnection.connected).to receive(:exec).with("SELECT * FROM peeps")
