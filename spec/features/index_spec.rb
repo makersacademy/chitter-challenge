c1a8b9f3bfc0a2920dbda1b2b017fe53d356d70d
Emmapr123 <emmapriester@hotmail.com>
2021-03-28 13:50:08 +0200

    users can now log out

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -1,4 +1,4 @@
-feature 'homepage' do
+feature '/sign_up' do
   scenario 'A user can sign up' do 
     visit '/'
     click_on 'Sign up'
@@ -8,7 +8,9 @@ feature 'homepage' do
     click_on 'Sign up'
     expect(page).to have_content 'What is on your mind Test User?'
   end
+end 
 
+feature '/log_in' do
   scenario 'A user can log in' do 
     User.create(name: 'Test User', email: 'test@example.com', password: 'password123')
 
@@ -38,10 +40,23 @@ feature 'homepage' do
     visit '/log_in'
     fill_in 'username', with: 'Test User'
     fill_in 'password', with: 'password125'
-    click_button('Log in')
+    click_on 'Log in'
 
     expect(page).not_to have_content 'What is on your mind Test User?'
     expect(page).to have_content 'Please check your username or password.'
+  end
+end 
+
+feature '/log_out' do 
+  scenario 'a user can log out at the end of their session' do 
+    User.create(name: 'Test User', email: 'test@example.com', password: 'password123')
+
+    
+    visit '/log_in'
+    fill_in 'username', with: 'Test User'
+    fill_in 'password', with: 'password123'
+    click_on 'Log in'
+    click_on 'Log out'
   end
 end 
 
