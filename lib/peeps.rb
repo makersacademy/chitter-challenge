75fbc704b8b15b490fa075123233622a9604289d
Will Spencer <willspencer16@gmail.com>
2021-03-29 08:59:46 +0100

    Created database for users

---
+++ lib/peeps.rb
@@ -0,0 +1,11 @@
+require 'pg'
+
+class Peeps
+  def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+  end
+end
