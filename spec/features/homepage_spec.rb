d1570d1fc4c0921b98f8759c30ad5743640290ad
Abdur Rehman <wabdulrehman@yahoo.com>
2021-04-24 21:47:39 +0100

    Add a database class for setup connection and execute querys

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,7 @@
 feature 'Home page' do
   scenario 'has a feild to write a peep' do
-    visit('/chitter')
+    visit('/')
+    expect(current_path).to eq('/chitter')
     click_button('New Peep')
     fill_in 'peep', with: 'my first peep'
     click_button('Submit')
