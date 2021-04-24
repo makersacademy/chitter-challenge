9c800e8268e2485dde7ad8e86da8e0af66ef28ce
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-24 22:59:00 +0100

    Test database added to unit and feature tests, setup_test_database.rb added to clear test database after every rspec

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -4,6 +4,9 @@ describe Chitter do
 
   describe '#.all method' do
     it 'can return all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO peeps (message) VALUES ('Test Peep');")
+      connection.exec("INSERT INTO peeps (message) VALUES ('Peep Testing!');")
       peeps = Chitter.all
       expect(peeps).to include "Test Peep"
       expect(peeps).to include "Peep Testing!"
