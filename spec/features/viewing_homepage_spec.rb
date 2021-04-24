2c83264f27aa436a709044b85be6b3f9666003a7
Tiffany Vallo <tiffanyvallo@tiffanys-mbp.home>
2021-04-24 19:44:17 +0100

    Homepage has view peeps button

--- spec/features/viewing_homepage_spec.rb
+++ spec/features/viewing_homepage_spec.rb
@@ -1,6 +1,6 @@
 feature 'Testing infrastucture' do
   scenario 'Can run app and check page content' do
     visit('/')
-    expect(page).to have_content 'Hello World!'
+    expect(page).to have_selector("input[type=submit][value='View Peeps']")
   end
 end
