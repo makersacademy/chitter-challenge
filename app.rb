190fbd3223320b478ef2cf3ca405a85fc42ca6a7
Kerri McMahon <kerrimcm25@gmail.com>
2021-04-24 16:01:09 +0100

    Users can post a peep and see it on the peepstream

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require './database_connection_setup'
+require './lib/peep'
 
 class Chitter < Sinatra::Base
   configure :development do
@@ -8,8 +9,18 @@ class Chitter < Sinatra::Base
   end
 
   get '/' do
-    'Hello World'
+    erb(:home)
   end
+
+  get '/peepstream' do
+    @peeps = Peep.all
+    erb(:peepstream)
+  end
+
+  post '/peepstream' do
+    Peep.create(peep: params[:peep])
+    redirect '/peepstream'
+  end 
 
   run! if app_file == $0
 end
---
+++ lib/peep.rb
@@ -0,0 +1,22 @@
+require 'pg'
+
+class Peep
+  attr_reader :id, :peep
+
+  def initialize(id:, peep:)
+    @id = id
+    @peep = peep
+  end 
+
+  def self.create(peep:)
+    result = DatabaseConnection.query("INSERT INTO peeps (peep) VALUES('#{peep}') RETURNING id, peep;")
+    Peep.new(id: result[0]['id'], peep: result[0]['peep'])
+  end
+
+  def self.all
+    result = DatabaseConnection.query("SELECT * FROM peeps;")
+    result.map do |post|
+      Peep.new(id: post['id'], peep: post['peep'])
+    end 
+  end
+end
