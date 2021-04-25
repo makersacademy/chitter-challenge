7436e8fa20752bb3f6479d7a9711adf14f60bed2
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-04-25 17:09:35 +0100

    Testing environment and basic app infrastructure set up - basic feature test passed

--- Gemfile
+++ Gemfile
@@ -1,8 +1,16 @@
 source 'https://rubygems.org'
 
+gem 'pg'
+gem 'sinatra'
+
 group :test do
+  gem 'capybara'
+  gem 'launchy'
   gem 'rspec'
   gem 'rubocop', '0.79.0'
+  gem 'selenium-webdriver'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'sinatra-reloader'
+  gem 'webrick'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,29 @@ require 'simplecov-console'
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+  SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
+
+# For accurate test coverage measurements, require your code AFTER 'SimpleCov.start'
+
+ENV['RACK_ENV'] = 'test'
+# ENV['ENVIRONMENT'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+# require_relative './setup_test_database'
+
+Capybara.app = Chitter
+
+# RSpec.configure do |config|
+#   config.before(:each) do
+#     setup_test_database
+#   end
+# end
 
 RSpec.configure do |config|
   config.after(:suite) do
