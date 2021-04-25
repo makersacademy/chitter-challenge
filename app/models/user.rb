a610288f6754e1b903b42137c408f72f8fb1a311
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-25 01:55:47 +0100

    ienforcing email uniquenuess

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
+  before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 15 }
   validates :email, presence: true, length: { maximum: 280 },
                     format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
+                    uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -49,7 +49,6 @@ class UserTest < ActiveSupport::TestCase
 
   test "email addresses should be unique" do
     duplicate_user = @user.dup
-    duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
