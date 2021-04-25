26308f7076cddd7ab4947af68b617db2ff66ab28
Muhammad Nasr Ahmad <79932064+ahmad047@users.noreply.github.com>
2021-04-25 20:05:03 +0100

    refactor tests using web helper

--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -1,31 +1,14 @@
 feature 'peeps' do
   scenario 'user can post a peep' do
-    sign_in
-
-    click_button 'Post a peep'
-    expect(current_path).to eq '/peeps/new'
-
-    fill_in :'peep', with: 'Hi, I am a new student at makers'
-    click_button 'Peep!'
+    post_first_peep
 
     expect(current_path).to eq '/peeps'
-    expect(page).to have_content 'Hi, I am a new student at makers'
+    expect(page).to have_content 'this is the first peep'
   end
 
   scenario 'user can view all peeps listed newest to oldest' do
-    sign_in
-
-    click_button 'Post a peep'
-    expect(current_path).to eq '/peeps/new'
-
-    fill_in :'peep', with: 'this is the first peep'
-    click_button 'Peep!'
-
-    click_button 'Post a peep'
-    expect(current_path).to eq '/peeps/new'
-
-    fill_in :'peep', with: 'this is the second peep'
-    click_button 'Peep!'
+    post_first_peep
+    post_second_peep
 
     expect(current_path).to eq '/peeps'
     expect(page).to have_content 'this is the second peep'
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -3,3 +3,24 @@ def sign_in
   click_button 'Sign in'
   expect(current_path).to eq '/peeps'
 end
+
+def open_new_peep_page
+  sign_in
+
+  click_button 'Post a peep'
+  expect(current_path).to eq '/peeps/new'
+end
+
+def post_first_peep
+  open_new_peep_page
+
+  fill_in :peep, with: 'this is the first peep'
+  click_button 'Peep!'
+end
+
+def post_second_peep
+  open_new_peep_page
+
+  fill_in :peep, with: 'this is the second peep'
+  click_button 'Peep!'
+end
