fe814d9a08b1d6dc32e30b590fdcb244351d0621
ian-1 <iandavidwalton@mail.com>
2021-04-25 16:46:04 +0100

    update spec_helper for Capybara and Sinatra

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,16 @@
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
 
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
-  SimpleCov::Formatter::Console,
-])
+console = [SimpleCov::Formatter::Console, SimpleCov::Formatter::HTMLFormatter]
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(console)
 SimpleCov.start
 
-RSpec.configure do |config|
-  config.after(:suite) do
-    puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
-  end
-end
+p 'Setting up test environment...'
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+Capybara.app = Chitter
