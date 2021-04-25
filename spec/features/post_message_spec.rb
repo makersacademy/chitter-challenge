dc25d6585aef185f262a3591fb99df15161cdf07
ian-1 <iandavidwalton@mail.com>
2021-04-25 21:31:37 +0100

    add feature see peeps in reverse chonological order

--- spec/features/post_message_spec.rb
+++ spec/features/post_message_spec.rb
@@ -6,4 +6,15 @@ feature 'Post message' do
     expect(page).to have_current_path('/')
     expect(page).to have_content 'my first peep!'
   end
+
+  scenario 'shows latest posted message first' do
+    visit('/')
+    fill_in 'message', with: 'first message'
+    click_button 'peep'
+    fill_in 'message', with: 'second message'
+    click_button 'peep'
+    first_index = page.body.index('first message')
+    second_index = page.body.index('second message')
+    expect(first_index).to be > second_index
+  end
 end
