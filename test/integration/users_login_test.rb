55c00fdeacc48c8896841d4c5f27421779e10736
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-25 15:41:34 +0100

    Added safe navigation operator

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,30 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-  test "login with invalid information" do
+  test "login with invalid email and password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { 
+      session: { 
+        email: "", 
+        password: "" 
+      } 
+    }
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
+  test "login with valid email and invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { 
+      session: { 
+        email: 'bobross@mail.com',
+        password: "" 
+      } 
+    }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
