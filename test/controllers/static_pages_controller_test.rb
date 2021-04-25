ef34e4e092c45f3800e3cc8e7bc4a496b4b94c64
jasonrowsell <jasonrowsell@hotmail.co.uk>
2021-04-25 01:04:33 +0100

    integration test passing

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,25 +1,21 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @title = "Chitter"
-  end
-
   test "should get home" do
     get root_path
     assert_response :success #abstract representation of status 200
-    assert_select "title", "Home | #{@title}"
+    assert_select "title", "Home"
   end
 
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@title}"
+    assert_select "title", "Contact"
   end
 
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | #{@title}"
+    assert_select "title", "About"
   end
 end
