b2f8e4f2bd2d50d24b4e27563db0dfedd8fd1b63
ian-1 <iandavidwalton@mail.com>
2021-04-25 16:41:11 +0100

    Set up Sinatra and Rack

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Chitter < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  run! if app_file == $PROGRAM_NAME
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,8 +3,6 @@ require 'simplecov-console'
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
