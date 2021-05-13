59534e8782f6f5e1aaee82f43304eb74ae389000
Pete <peter_james_allen@hotmail.com>
2021-05-13 12:06:08 +0100

    updated anonymous icon

--- db/migrations/01_create_chitter_db.sql
+++ db/migrations/01_create_chitter_db.sql
@@ -3,4 +3,4 @@ CREATE TABLE tags (id SERIAL PRIMARY KEY, name VARCHAR);
 CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR, username VARCHAR, picture VARCHAR, email VARCHAR, password VARCHAR);
 CREATE TABLE peeps (id SERIAL PRIMARY KEY, FOREIGN KEY (user_id) REFERENCES users(id), user_id INT, "text" VARCHAR, date VARCHAR); 
 CREATE TABLE peep_tags (id SERIAL PRIMARY KEY, FOREIGN KEY (peep_id) REFERENCES peeps(id), FOREIGN KEY (tag_id) REFERENCES tags(id), tag_id INT, peep_id INT); 
-INSERT INTO users (id, name, username) VALUES(0, 'Anonymous', 'anon');
+INSERT INTO users (id, name, username) VALUES(0, 'Anonymous', 'anon', 'https://www.iconbolt.com/iconsets/streamline-emoji/speaking-head.svg');
