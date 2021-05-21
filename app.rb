d8321b512765787eaf14b2a0f4581ddd6d1f194f
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-05-21 09:27:23 +0100

    solved first user story

--- app.rb
+++ app.rb
@@ -2,12 +2,27 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Chitter < Sinatra::Base
+  enable :sessions
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
     'Welcome to chitter'
+  end
+
+  get '/chitter' do
+    p session
+    session[:message]
+  end
+
+  get '/chitter/new' do
+    erb(:'chitter/new')
+  end
+
+  post '/chitter' do
+    session[:message] = params[:message] 
+    redirect ('/chitter')
   end
 
   run! if app_file == $0
---
+++ spec/features/post_peep_spec.rb
@@ -0,0 +1,12 @@
+# As a Maker
+# So that I can let people know what I am doing  
+# I want to post a message (peep) to chitter
+
+feature 'posting a message' do
+  scenario 'user can post a message to chitter' do
+    visit('/chitter/new')
+    fill_in('message', with: 'This is my first peep!')
+    click_button('Submit')
+    expect(page).to have_content('This is my first peep!')
+  end
+end
