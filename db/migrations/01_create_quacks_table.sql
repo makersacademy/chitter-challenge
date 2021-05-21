31b8a59a10134a2d142f03734d84e674ab5d710a
BecaLParker <becaparker@gmail.com>
2021-05-21 16:52:48 +0100

    Adds timestamp and display_name to table

--- db/migrations/01_create_quacks_table.sql
+++ db/migrations/01_create_quacks_table.sql
@@ -1,2 +1,6 @@
 
-CREATE TABLE quacks(id SERIAL PRIMARY KEY, message VARCHAR(140), display_name VARCHAR(60), time_stamp VARCHAR(60));
+CREATE TABLE quacks(id SERIAL PRIMARY KEY, message VARCHAR(140), display_name VARCHAR(60), time_stamp timestamp default now());
+
+INSERT INTO quacks (message, display_name) VALUES ('Love is strength', 'Quackie');
+INSERT INTO quacks (message, display_name) VALUES ('Dream it, be it', 'Quackie');
+INSERT INTO quacks (message, display_name) VALUES ('The last word', 'Quackie');
