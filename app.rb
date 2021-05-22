831dd3c182ed953d660513b67728198dc0885ae9
Gunel Mehdiyeva-Chau <gunel.mehdiyev@gmail.com>
2021-05-22 11:30:35 +0100

    user story 1 - pass in the simplest way

--- app.rb
+++ app.rb
@@ -16,5 +16,18 @@ class ChitterApp < Sinatra::Base
     'Chitter Application'
   end
 
+  get '/peeps/new' do
+    erb :'peeps/new'
+  end
+
+  post '/peeps' do
+    @peep = params[:peep]
+    redirect '/peeps'
+  end
+
+  get '/peeps' do
+    erb :'peeps/index'
+  end 
+
   run! if app_file == $0
 end
--- spec/features/posting_message_spec.rb
+++ spec/features/posting_message_spec.rb
@@ -2,3 +2,13 @@
 # So that I can let people know what I am doing  
 # I want to post a message (peep) to chitter
 
+feature 'post peep' do
+  scenario 'a user can post a new peep' do
+    visit ('/peeps/new')
+    fill_in('peep', with: 'This is my first peep!')
+    click_button('Post peep')
+
+    expect(current_path).to eq('/peeps')
+    expect(page).to have_content('This is my first peep!')
+  end
+end 
