7032b40580ec28b8336644cb610d9214810237d1
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-22 16:37:48 +0100

    peeps display in reverse chron order

--- spec/features/viewing_peep_spec.rb
+++ spec/features/viewing_peep_spec.rb
@@ -16,4 +16,20 @@ feature 'Viewing peeps' do
     expect(page).to have_content("test peep 2 - #{now}")
     expect(page).to have_content("test peep 3 - #{now}")
   end
+
+  scenario 'Peeps should be shown in reverse chronological order' do
+    now = Time.now
+    before = Time.now - 3600
+    yesterday = Time.now - 86400
+    Peep.create(text: 'test peep 1', posted: now.strftime("%F %T"))
+    Peep.create(text: 'test peep 3', posted: yesterday.strftime("%F %T"))
+    Peep.create(text: 'test peep 2', posted: before.strftime("%F %T"))
+
+    visit '/peeps'
+
+    peeps = page.all('.peep')
+    expect(peeps[0]).to have_content(now.strftime("%F %T"))
+    expect(peeps[1]).to have_content(before.strftime("%F %T"))
+    expect(peeps[2]).to have_content(yesterday.strftime("%F %T"))
+  end
 end
