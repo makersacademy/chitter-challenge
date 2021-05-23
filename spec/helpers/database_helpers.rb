a96ff6e68603e6f159fecd8ff601d6d32dfe3140
Robert Dupplaw <dupplawrobert@gmail.com>
2021-05-23 16:05:27 +0100

    test setup truncates users table

--- spec/helpers/database_helpers.rb
+++ spec/helpers/database_helpers.rb
@@ -2,7 +2,7 @@ require 'pg'
 
 def truncate_test_tables
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec('TRUNCATE TABLE peeps;')
+  connection.exec('TRUNCATE TABLE peeps, users;')
 end
 
 def persisted_data(id:)
