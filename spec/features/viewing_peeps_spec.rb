43f384515a7db2fc2ce86a6389baf0728dfecc5b
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-05-24 06:46:50 +0100

    third user story complete

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,6 +2,10 @@
 # So that I can see what others are saying  
 # I want to see all peeps in reverse chronological order
 
+# As a Maker
+# So that I can better appreciate the context of a peep
+# I want to see the time at which it was made
+
 feature 'viewing messages' do
   scenario 'a user can see messages' do
     Message.create("This is my first peep!", "Bob", "13:00 01/01/2021")
@@ -10,13 +14,8 @@ feature 'viewing messages' do
 
     visit('/chitter')
 
-    # first_message = first('.message')
-    # expect(first_message).to eq("Third peep peeps!")
-
-    expect(page).to have_content "This is my first peep!"
-    expect(page).to have_content "Second peep!"
-    expect(page).to have_content "Third peep peeps!"
-    
+    first_message = find('/html/body/ul/li[1]').text
+    expect(first_message).to eq("15:00 03/03/2021 - Bob - Third peep peeps!")  
   end
   
 end
