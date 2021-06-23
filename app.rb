f2ae9b7b87ea75456f2144501db0b4535d9a3d02
Ashley Slaney <ashleyslaney96@gmail.com>
2021-06-23 11:59:30 +0100

    initial setup

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Chitter < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb(:index)
+  end
+end
