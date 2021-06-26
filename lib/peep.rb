7e306d9b5c73f7962831c2673b7f6c13873c0964
Cynthia Fu <yinnycfu@gmail.com>
2021-06-26 01:18:15 +0100

    extracted DatabaseConnection object

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,5 +1,4 @@
-require 'pg'
-require 'time'
+require_relative 'database_connection'
 
 class Peep
   attr_reader :peep_id, :peep_text, :peeped_on
@@ -11,27 +10,18 @@ class Peep
   end
 
   def self.all
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-
-    result = connection.exec('SELECT * FROM peeps;')
-
+    result = DatabaseConnection.query("SELECT * FROM peeps")
     result.map do |peep| 
-      Peep.new(peep_id: peep['peep_id'], peep_text: peep['peep_text'], peeped_on: peep['peeped_on'])
+      Peep.new(
+        peep_id: peep['peep_id'], 
+        peep_text: peep['peep_text'], 
+        peeped_on: peep['peeped_on']
+      )
     end
   end
 
   def self.create(peep_text:)
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-
-    result = connection.exec("INSERT INTO peeps (peep_text) VALUES('#{peep_text}') RETURNING peep_id, peep_text, peeped_on;")
+    result = DatabaseConnection.query("INSERT INTO peeps (peep_text) VALUES('#{peep_text}') RETURNING peep_id, peep_text, peeped_on;")
     Peep.new(peep_id: result[0]['peep_id'], peep_text: result[0]['peep_text'], peeped_on: result[0]['peeped_on'])
   end
 
