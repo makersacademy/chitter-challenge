1036a01555e8dbd2a294e7c49b7ce4f39ca6d004
Masako Gray <masakogray@hotmail.co.uk>
2021-06-26 15:10:05 +0100

    gems updated

--- Gemfile
+++ Gemfile
@@ -1,8 +1,12 @@
 source 'https://rubygems.org'
 
 group :test do
+  gem 'sinatra'
+  gem 'sinatra-contrib'
+  gem 'capybara'
   gem 'rspec'
   gem 'rubocop', '0.79.0'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'pg'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 ])
 SimpleCov.start
 
+# You can see your test coverage when you run your tests. 
+# If you want this in a graphical form, uncomment the `HTMLFormatter` line and see what happens!
+
 RSpec.configure do |config|
   config.after(:suite) do
     puts
