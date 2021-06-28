584a41b739e28f03f4b0db038895fe9ca7cf9eea
JooMin Choi <joominchoi23@gmail.com>
2021-06-28 10:39:55 +0100

    first user story works now

--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -2,6 +2,6 @@ require 'pg'
 
 def persisted_data(id:)
   connection = PG.connect(dbname: 'chitter_peeps_test')
-  result = connection.query("SELECT * FROM news_feed WHERE id = #{id};")
+  result = connection.query("SELECT * FROM news_feed WHERE id = #{id.to_i};")
   result.first
 end
