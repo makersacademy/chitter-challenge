f43deea93ed2e4fe0d727de29647b6d2517f42e3
Else Meijerink <61120785+SipofTea@users.noreply.github.com>
2021-07-26 00:11:36 +0100

    Implemented display of peeps on homepage

--- lib/peep.rb
+++ lib/peep.rb
@@ -3,6 +3,18 @@ require 'pg'
 class Peep
 attr_reader :id, :name, :content
 
+  def self.all
+    connection = if ENV['RACK_ENV'] == 'test'
+      PG.connect(dbname: 'chitter_test')
+    else
+      PG.connect(dbname: 'chitter')
+    end
+    peeps = connection.exec("SELECT * FROM peeps")
+    peeps.map do |peep|
+      Peep.new(id: peep['id'], name: peep['name'], content: peep[ 'content'])
+    end
+  end
+
   def self.create(name:, content:)
     connection = if ENV['RACK_ENV'] == 'test'
       PG.connect(dbname: 'chitter_test')
