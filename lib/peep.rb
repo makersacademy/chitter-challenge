180337cb3467ef12763e4a9da114adaf42904c97
Brett Karlson <brettjkarlson@gmail.com>
2021-07-26 09:23:07 +0100

    setting up test environment

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,7 +2,12 @@ require 'pg'
 
 class Peep
   def self.all
-    connection = PG.connect(dbname: 'peep_manager')
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'peep_manager_test')
+    else
+      connection = PG.connection(dbname: 'peep_manager')
+    end
+    
     result = connection.exec("SELECT * FROM peeps;")
     result.map { |peep| peep['peep'] }
   end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,6 +3,12 @@ require 'peep'
 RSpec.describe Peep do
   describe '.all' do
     it 'returns all the peeps' do
+      connection = PG.connect(dbname: 'peep_manager_test')
+
+      connection.exec("INSERT INTO peeps (peep) VALUES ('Hi, I am peep three');")
+      connection.exec("INSERT INTO peeps (peep) VALUES('Hi, I am peep two');")
+      connection.exec("INSERT INTO peeps (peep) VALUES('Hi, I am peep one');")
+
       peeps = Peep.all
 
       expect(peeps).to include('Hi, I am peep three')
