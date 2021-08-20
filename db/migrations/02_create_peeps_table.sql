e46ae785930c45694702841f3b972f47d0df5e60
edpackard <edpackard81@gmail.com>
2021-08-20 22:25:40 +0100

    Added database table setup queries

---
+++ db/migrations/02_create_peeps_table.sql
@@ -0,0 +1,11 @@
+CREATE TABLE peeps(
+  id INT GENERATED ALWAYS AS IDENTITY,
+  content VARCHAR(140) NOT NULL,
+  time TIME NOT NULL,
+  user_id INT,
+  PRIMARY KEY(id),
+  CONSTRAINT fk_user
+    FOREIGN KEY(user_id)
+      REFERENCES users(id)
+      ON DELETE CASCADE
+);
