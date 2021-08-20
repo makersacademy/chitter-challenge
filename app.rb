945a2c9aa51db4c9f36e9d000ab12915699c5082
Hotu Nga Morehu Elkington <hotunz@gmail.com>
2021-08-20 23:11:19 +0100

    Refactoring peeps/index to use Peep class

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require './lib/peep.rb'
 
 class Chitter < Sinatra::Base
   configure :development do
@@ -7,9 +8,14 @@ class Chitter < Sinatra::Base
   end
 
   get '/peeps/index' do
+    @peep = Peep.all
     erb(:'peeps/index')
   end
 
+  post '/peeps/create' do
+    Peep.create(message: params[:peep])
+    redirect('/peeps/index')
+  end
 
   run! if app_file == $0
 end
--- spec/features/adding_peep_spec.rb
+++ spec/features/adding_peep_spec.rb
@@ -2,7 +2,7 @@ feature 'Allows a user to post a message to chitter' do
   scenario 'User can post a message by filling out the message box then post it' do
     visit('/peeps/index')
     find_field('peep')
-    fill_in 'peep', with: "How's everyone?"
+    fill_in 'peep', with: "How is everyone?"
     click_button('Peep it')
   end
 
