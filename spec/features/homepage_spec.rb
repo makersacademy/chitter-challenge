d91811f2de533e5819333196f8369a658f9b4a26
Graeme Stirling <graemejstirling@gmail.com>
2021-08-21 10:59:17 +0100

    Added new button to add post, test failing as expected

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,6 +1,15 @@
-feature "Prints home page" do 
+feature "Displats home page with usr options" do 
   scenario "prints string" do 
     visit "/"
-    expect(page).to have_content "Hello, World!"
+    expect(page).to have_content "Welcome to Chitter ! What would you like to do ?"
   end 
 end 
+
+feature "Redirects user to add a post page" do 
+  scenario "User clicks add post button" do 
+    visit "/"
+    click_button('Add a post')
+    expect(page).to have_current_path('/add_post')
+  end 
+end 
+
