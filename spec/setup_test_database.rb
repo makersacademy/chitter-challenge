a9e7490901f514b744839c2f6badbca9c3bb525b
Fiona Apps <wikipanyd@gmail.com>
2021-08-22 10:55:49 +0100

    Now can I merge this version?

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -5,8 +5,8 @@ def setup_test_database
   p 'Setting up test database...'
 
   conn = PG.connect(dbname: 'chitter_test')
-  conn.exec("TRUNCATE user;")
-  conn.exec("TRUNCATE post;")
+  conn.exec("TRUNCATE users;")
+  conn.exec("TRUNCATE posts;")
   # set up table id as well?
   
 end
