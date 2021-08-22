afb9fb27e92238443c2b75e8be0bad880fdce80c
Nicola Carroll <nicolaccarroll@gmail.com>
2021-08-22 18:20:46 +0100

    home page prints user names from db

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,13 @@
-ENV['RACK_ENV'] = 'test'
+ENV['SINATRA_ENV'] = 'test'
 
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require 'capybara'
 require 'capybara/rspec'
-require 'pg'
 require 'rspec'
 require 'simplecov'
 require 'simplecov-console'
+require 'active_record'
 
 Capybara.app = Chitter
 
@@ -24,4 +24,5 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+
 end
