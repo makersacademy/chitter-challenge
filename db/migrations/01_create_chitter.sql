592318e8c24b9ea81c29f5086abc78d8f569c11b
Corinne <corinne.boesch@gmail.com>
2021-08-22 17:50:57 +0100

    set up db migrations instructions

---
+++ db/migrations/01_create_chitter.sql
@@ -0,0 +1,7 @@
+psql
+CREATE DATABASE "chitter_db";
+CREATE TABLE users_db(id SERIAL PRIMARY KEY, name VARCHAR(15), email VARCHAR(50), password VARCHAR(12));
+CREATE TABLE peeps_db(id SERIAL PRIMARY KEY, time integer, peep VARCHAR(140), user_id integer);
+
+CREATE DATABASE "chitter_db_test"; 12
+
