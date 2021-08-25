94565c117b1525d7dd4b4a644f063eda9d53d99a
danroche10 <danielroche10@gmail.com>
2021-08-25 22:02:44 +0100

    refactored tests

--- spec/units/peep_spec.rb
+++ spec/units/peep_spec.rb
@@ -1,34 +1,29 @@
 require 'peep'
 
 describe Peep do
-  
+  let(:peep) { described_class.create('hello this is a practice peep') }
   describe '#self.all' do
     it 'returns all peeps' do
-      Peep.create('hello this is a practice peep')
-
+      peep
       expect(Peep.all.length).to eq 1
     end
   end
 
   describe "#self.add" do
     it "should add a peep to the database" do
-      peep = Peep.create('hello this is a practice peep')
-
       expect(peep).to be_a Peep
     end
   end
 
   describe '#comments' do
     it 'returns a list of comments on the bookmark' do
-      new_peep = Peep.create('hello this is a practice peep')
-      peep_id = new_peep.id
+      peep_id = peep.id
       # peep below is peep_id within comments table
       DatabaseConnection.connection.exec_params(
         "INSERT INTO comments (id, comment, peep) VALUES(1, 'Test comment', $1)",
         [peep_id]
       )
-      comment = new_peep.comments.first
-
+      comment = peep.comments.first
       expect(comment['comment']).to eq 'Test comment'
     end
 
--- spec/units/user_spec.rb
+++ spec/units/user_spec.rb
@@ -2,9 +2,9 @@ require 'user'
 require 'bcrypt'
 
 describe User do
+  let (:user) { described_class.create(email: 'test@example.com', password: 'password123', name: 'testname', username: 'username') }
   describe '.create' do
     it 'creates a new user' do
-      user = described_class.create(email: 'test@example.com', password: 'password123', name: 'testname', username: 'username')
       expect(user.id).not_to eq nil
       expect(user.email).to eq "test@example.com"
       expect(user.name).to eq "testname"
@@ -23,7 +23,6 @@ describe User do
     end
 
     it 'finds a user by ID' do
-      user = described_class.create(email: 'test@example.com', password: 'password123', name: 'testname', username: 'username')
       result = described_class.find(user.id)
       expect(result.email).to eq user.email
       expect(result.id).to eq user.id
@@ -35,19 +34,16 @@ describe User do
     it 'returns a user given a correct username and password, if one exists' do
       user = described_class.create(email: 'test@example.com', password: 'password123', name: 'testname', username: 'username')
       authenticated_user = described_class.authenticate(email: 'test@example.com', password: 'password123')
-
       expect(authenticated_user.id).to eq user.id
     end
 
     it 'returns nil given an incorrect email address' do
-      User.create(email: 'test@test.com', password: 'password123', name: 'testname', username: 'username')
-      
+      user      
       expect(User.authenticate(email: 'nottherightemail@me.com', password: 'password123')).to be_nil
     end
 
     it 'returns nil given an incorrect password' do
-      User.create(email: 'test@test.com', password: 'password123', name: 'testname', username: 'username')
-
+      user
       expect(User.authenticate(email: 'test@test.com', password: 'wrongpassword')).to be_nil
     end
   end
