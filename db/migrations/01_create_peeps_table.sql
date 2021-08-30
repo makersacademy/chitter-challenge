a2fe3f0788566079522f9e175f3a632971b08b08
danroche10 <danielroche10@gmail.com>
2021-08-30 17:59:30 +0100

    altered peeps table script

--- db/migrations/01_create_peeps_table.sql
+++ db/migrations/01_create_peeps_table.sql
@@ -1,1 +1,2 @@
-CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep VARCHAR(200), maker integer REFERENCES users(id) );
+CREATE TABLE peeps(id SERIAL PRIMARY KEY, peep VARCHAR(200), created_at TIMESTAMP, maker integer REFERENCES users(id) );
+ALTER TABLE peeps ALTER COLUMN created_at SET DEFAULT now();
