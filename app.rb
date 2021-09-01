1a22fcfdd0f1f7ca8c198fa3a714fdeb6433619c
Jenny Moon <jenny0moon@gmail.com>
2021-09-01 08:55:22 +0100

    Add: signing out

--- app.rb
+++ app.rb
@@ -46,5 +46,11 @@ class Chitter < Sinatra::Base
     end
   end
 
+  post '/sessions/destroy' do
+    session.clear
+    flash[:notice] = 'You have signed out.'
+    redirect('/')
+  end
+
   run! if app_file == $0
 end
