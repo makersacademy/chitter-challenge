cda39022f7336dc2dd7eebc96879c264b9718bcd
Osneil Drakes <osneil-drakes@hotmail.com>
2021-09-17 23:41:49 +0100

    set up database

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source 'https://rubygems.org'
 
-ruby '3.0.2'
 
 group :test do
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+
 end
 
 group :development, :test do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,19 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+
+require_relative './setup_test_database'
+
+ENV['ENVIRONMENT'] = 'test'
+
+RSpec.configure do |config|
+  config.before(:each) do
+    setup_test_database
+  end
+end
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -15,3 +29,17 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
+
+# Set the environment to "test"
+ENV['RACK_ENV'] = 'test'
+
+# Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+# Require all the testing gems
+
+
+
+# Tell Capybara to talk to BookmarkManager
+Capybara.app = BookmarkManager
