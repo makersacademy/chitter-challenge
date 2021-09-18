c3e025a375dded5fcf1eb41ccc9c0725652d7467
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-18 20:47:19 +0100

    Interacting with PostgreSQL from Ruby, for the .all method

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -2,10 +2,13 @@ require 'pg'
 
 class Chitter
   def self.all
-    [
-      "This is my first Chitter post!",
-      "This is my second Chitter post!", 
-      "This is my third Chitter post!"
-    ]
+    connection = PG.connect(dbname: 'chitter_app')
+    result = connection.exec("SELECT * FROM chitter;")
+    result.map { |bookmark| bookmark['post'] }
+    # [
+    #   "This is my first Chitter post!",
+    #   "This is my second Chitter post!", 
+    #   "This is my third Chitter post!"
+    # ]
   end 
 end
