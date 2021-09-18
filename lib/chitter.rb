dfa197b156676ea111291d2c380214ecb6cf5c10
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-18 16:39:47 +0100

    time is now parsed to look user friendly using Time library

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -1,16 +1,27 @@
 require_relative '../db/queries/postgres_db'
 require_relative './message'
+require 'Time'
 class Chitter
 
   def self.show_all_messages
     result = PGDatabase.con.exec_params("SELECT * FROM message ORDER BY createdate DESC;")
     result.map { |row |
-      Message.new(id: row['id'], message: row['message'] ,create_date: row['createdate'] )
+      
+      Message.new(id: row['id'], message: row['message'] ,create_date: parse_date(row['createdate']))
     }
   end
 
   def self.create_message(message: )
     PGDatabase.create_message(user_id: 1, message: message)
   end
+
+  def self.parse_date(date)
+    if date 
+      ruby_date = Time.parse(date)
+      ruby_date.strftime("%Y-%m-%d %H:%M")
+    end
+  end
+
+  private_class_method :parse_date
 
 end
