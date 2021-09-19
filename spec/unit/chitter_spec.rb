e2e3df4db2eaf244a1fbe04d06e84ef0d8e8b6e8
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-09-19 18:55:06 +0100

    Making the unit test pass again by inserting values from the test database

--- spec/unit/chitter_spec.rb
+++ spec/unit/chitter_spec.rb
@@ -3,7 +3,13 @@ require 'chitter'
 describe Chitter do
   describe '.all' do
     it 'returns all chitter posts in reverse chronological order' do
-      chitter = Chitter.all
+        connection = PG.connect(dbname: 'chitter_app_test')
+      
+        connection.exec("INSERT INTO chitter (post) VALUES ('This is my first Chitter post!');")
+        connection.exec("INSERT INTO chitter (post) VALUES ('This is my second Chitter post!');")
+        connection.exec("INSERT INTO chitter (post) VALUES ('This is my third Chitter post!');")
+
+        chitter = Chitter.all
 
       expect(chitter).to include("This is my first Chitter post!")
       expect(chitter).to include("This is my second Chitter post!")
