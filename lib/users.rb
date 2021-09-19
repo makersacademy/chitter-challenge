d51a20aa31155d1b8e6e10256603243d0a54b294
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-19 11:15:13 +0100

    passwords are now encrypted

--- lib/users.rb
+++ lib/users.rb
@@ -1,9 +1,13 @@
 require_relative '../db/queries/postgres_db'
+require 'bcrypt'
+require 'dotenv'
 
 class User
 
+  Dotenv.load
+
   def self.add(username:, email:, password:)
-    result = PGDatabase.create_user(username: username, email: email, password: password)
+    result = PGDatabase.create_user(username: username, email: email, password: encrypt(password))
     User.new(id: result['id'], username: result['id'], email:result['id'])
   end
 
@@ -12,6 +16,12 @@ class User
     result = PGDatabase.get_user(id)
     User.new(id: result['id'], username: result['user_name'], email:result['email'])
   end
+
+  def self.encrypt(password)
+    BCrypt::Password.create(password)
+  end
+
+  private_class_method :encrypt
 
   def initialize(id:, username:, email: )
     @id = id
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -10,6 +10,11 @@ describe User do
       expect(user.email).to eq 'username@gmail.com'
     end
 
+    it 'hashes the password using BCrypt' do
+      expect(BCrypt::Password).to receive(:create).with('password123')
+      User.add(username: 'encrypted', email: 'encrypted@example.com', password: 'password123')
+    end
+
   end
 
 end
