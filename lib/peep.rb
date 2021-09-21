9a7e6453028aa39fb2b04f823924e09893b46336
Gianluca <gianluca.ansaldi.1989@gmail.com>
2021-09-22 00:33:28 +0100

    Used bootstrap card for peep styling

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,14 +2,22 @@ require 'pg'
 require_relative './database_connection'
 
 class Peep 
+  attr_reader :user_id, :create_time, :body
+
+  def initialize(user_id:, create_time:, body:)
+    @user_id = user_id
+    @create_time = create_time
+    @body = body
+  end
 
   def self.all
-    # if ENV['RACK_ENV'] == 'test'
-    #   connection = PG.connect(dbname: 'chitter_test')
-    # else
-    #   connection = PG.connect(dbname: 'chitter')
-    # end
     peeps = DatabaseConnection.query('SELECT * FROM peeps ORDER BY create_time DESC;')
-    peeps.map { |peep| peep['body'] }
+    peeps.map do |peep|
+      Peep.new(
+        user_id: peep['user_id'],
+        create_time: peep['create_time'],
+        body: peep['body']
+      )
+    end
   end
 end
