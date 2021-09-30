877a3881144804c20f17035919b02cc7e284543a
Else Meijerink <p.e.meijerink@gmail.com>
2021-09-30 15:05:23 +0100

    Added display of peeps in reverse order

---
+++ spec/features/peep_list_spec.rb
@@ -0,0 +1,18 @@
+feature 'Peep list' do
+  before(:each) do
+    visit('/peeps/new')
+    fill_in 'user_name', with: 'Twitter'
+    fill_in 'content', with: 'Lawsuit!!'
+    click_button('submit')
+    visit('/peeps/new')
+    fill_in 'user_name', with: 'Second'
+    fill_in 'content', with: 'Potatoes!!'
+    click_button('submit')
+  end
+  scenario 'displays peeps in reverse chronological order' do
+    peeps = page.all("div.peep")
+
+    expect(peeps[0]).to have_content("Second Potatoes!!")
+    expect(peeps[1]).to have_content("Twitter Lawsuit!!")
+  end
+end
--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,7 +1,6 @@
 require 'pg'
 
 def setup_test_database!
-  p "Clearing!"
   con = PG.connect(dbname: 'chitter_test')
   con.exec("TRUNCATE peeps;")
 end
