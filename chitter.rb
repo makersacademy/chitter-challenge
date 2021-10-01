7f5d73bdb50af610557c38699f75303dbf67b9e7
Else Meijerink <p.e.meijerink@gmail.com>
2021-10-01 16:35:29 +0100

    Implemented sign in on sign up

--- chitter.rb
+++ chitter.rb
@@ -1,13 +1,17 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require './lib/peep'
+require './lib/user'
 
 class Chitter < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
+    @username = session[:username]
     erb(:index)
   end
 
@@ -25,7 +29,8 @@ class Chitter < Sinatra::Base
   end
 
   post '/users' do
-    
+    user = User.create(username: params[:user_name], email: params[:email], password: params[:password])
+    session[:username] = user.username
     redirect to('/')
   end
 end
