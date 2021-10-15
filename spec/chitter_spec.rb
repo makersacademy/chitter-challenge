85d3e2ebc702a1d068c3a19ffa3ad47cb3e29e59
Marianne Azzopardi <marianneazzopardi@Mariannes-MacBook-Air.local>
2021-10-15 11:44:07 +0100

    tests are connected to test enviroment and passing

--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -3,6 +3,11 @@ require 'chitter'
 describe Chitter do
   describe '.all' do
     it 'returns all peeps' do
+      connection = PG.connect(dbname: 'chitter_test')
+
+      connection.exec("INSERT INTO peeps (name,peep) VALUES ('Voldemort', 'I am a Pull Stack Developer. I pull things off the internet and put it in my code.');")
+      connection.exec("INSERT INTO peeps (name,peep) VALUES('Gregorovitch', 'Debugging is like an onion. There are multiple layers to it, and the more you peel them back, the more likely you will cry.');")
+
       peeps = Chitter.all
       expect(peeps).to include("I am a Pull Stack Developer. I pull things off the internet and put it in my code.")
       expect(peeps).to include("Debugging is like an onion. There are multiple layers to it, and the more you peel them back, the more likely you will cry.")
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,7 @@ require 'rspec'
 
 # Set the environment to "test"
 ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
