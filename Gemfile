ad49c71a77c20e20fd82d7ea3c53c7e0d7f7b461
ConorButler <cwdbutler@gmail.com>
2021-10-15 20:11:29 +0100

    First 4 user stories

--- Gemfile
+++ Gemfile
@@ -2,8 +2,15 @@ source 'https://rubygems.org'
 
 ruby '3.0.2'
 
+gem 'pg'
+gem 'sinatra'
+gem 'sinatra-contrib'
+# gem launchy
+
 group :test do
+  gem 'capybara', group: :test
   gem 'rspec'
+  gem 'timecop'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
+ENV["RACK_ENV"] = "test"
+ENV["ENVIRONMENT"] = "test"
+
+require File.join(File.dirname(__FILE__), "..", "app.rb")
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+require 'setup_test_database'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -9,6 +18,9 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+  config.before(:each) do 
+    setup_test_database
+  end
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
