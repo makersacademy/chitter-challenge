d661c2901565598616920ff55a714178f3c2dc66
Arthur Fincham <arthurfincham@me.com>
2021-10-16 10:29:50 +0100

    toasts are working

--- Gemfile
+++ Gemfile
@@ -9,7 +9,7 @@ gem 'sinatra-reloader'
 gem 'webrick'
 gem 'sinatra-flash'
 gem 'bcrypt'
-gem 'bootstrap'
+gem 'bootstrap', '~> 5.0.2'
 gem 'relative_time'
 
 group :test do
--- public/script.js
+++ public/script.js
@@ -1,5 +1,3 @@
-$( document ).ready(function() {
-   $('body').on('click','.close',function(){
-          $(this).closest('.toast').toast('hide')
-        })
+$(document).ready(function(){
+  $('.toast').toast('show');
 });
