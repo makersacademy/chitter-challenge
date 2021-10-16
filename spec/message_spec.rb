e7488a120aa786f008042253bcc2155bf3f55229
luke <luke44@gmail.com>
2021-10-16 21:18:03 +0100

    Deleted redundant tests

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -3,15 +3,5 @@ describe Message do
     it 'has a Message class' do
       expect(Message).to respond_to(:new)
     end
-    it 'uses class instance variables' do 
-      expect(Message).to respond_to(:instance)
-      expect(Message).to respond_to(:create)
-    end
-
-    it 'can store many messages' do 
-      expect(Message).to respond_to(:instance)
-      expect(Message).to respond_to(:create)
-    end
-
   end
 end
