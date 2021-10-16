63e7a9315fff32e9954a1b7275511338c5c1eb26
Bernard Leanse <bernardleanse96@gmail.com>
2021-10-16 17:24:08 +0100

    peeps now have date

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,20 +1,26 @@
 class Peep
-  attr_reader :content
-  def initialize(content: nil)
+  attr_reader :content, :date
+
+  def initialize(content: nil, date: nil)
     @content = content
+    @date = date
   end
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM peeps")
 
     result.map do |peep| 
-      Peep.new(content: peep['content'])
+      Peep.new(content: peep['content'], date: peep['date_posted'])
     end
 
   end
 
   def self.create(content: )
-    DatabaseConnection.query("INSERT INTO peeps(content) VALUES ($1)", [content])
+    DatabaseConnection.query("INSERT INTO peeps(content, date_posted) VALUES ($1, $2)", [content, Date.today.to_s])
   end
+
+
+
+
 
 end
