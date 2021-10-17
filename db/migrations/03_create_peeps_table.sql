d19b0c6cc42b4e2c0604e63e2b02416bc849a405
Sergei Palmer <zigpalmer@gmail.com>
2021-10-17 22:03:50 +0100

    Added images for stack

--- db/migrations/03_create_peeps_table.sql
+++ db/migrations/03_create_peeps_table.sql
@@ -1,5 +1,1 @@
 CREATE TABLE peeps (peep_id SERIAL PRIMARY KEY, content VARCHAR (140) NOT NULL, posttime TIMESTAMPTZ, author_id INTEGER REFERENCES users (user_id));
-
-
-
--- CREATE TABLE peeps (peep_id SERIAL PRIMARY KEY, content VARCHAR (140) NOT NULL, posttime TIME(HH:MI), postdate TO_CHAR(NOW() :: DATE, 'dd/mm/yyyy'), author_id INTEGER REFERENCES users (user_id));
