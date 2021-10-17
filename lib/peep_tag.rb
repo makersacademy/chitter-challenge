057ba3702b7e46fa606705234685be6e776afcd8
Arthur Fincham <arthurfincham@me.com>
2021-10-17 12:51:33 +0100

    add PeepTag class as join table, add spec

--- lib/peep_tag.rb
+++ lib/peep_tag.rb
@@ -7,8 +7,10 @@ class PeepTag
     @tag_id = tag_id
   end
 
-   def self.create(peep_id:, tag_id:)
-      result = DatabaseConnection.query("INSERT INTO peep_tags (peep_id, tag_id) VALUES($1, $2) RETURNING peep_id, tag_id;", [peep_id, tag_id])
-      PeepTag.new(peep_id: result[0]['peep_id'], tag_id: result[0]['tag_id'])
-    end
+  def self.create(peep_id:, tag_id:)
+    result = DatabaseConnection.query(
+"INSERT INTO peep_tags (peep_id, tag_id) VALUES($1, $2) RETURNING peep_id, tag_id;", [peep_id, 
+tag_id])
+    PeepTag.new(peep_id: result[0]['peep_id'], tag_id: result[0]['tag_id'])
+  end
 end
