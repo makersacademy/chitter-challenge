9b7fa66b4d113c677520892aa57fda9e75bf575d
Sergei Palmer <zigpalmer@gmail.com>
2021-10-17 12:26:31 +0100

    Test updates

--- db/migrations/02_create_user_table.sql
+++ db/migrations/02_create_user_table.sql
@@ -1,1 +1,3 @@
 CREATE TABLE users (user_id SERIAL PRIMARY KEY, username VARCHAR(20) NOT NULL UNIQUE, name VARCHAR (40) NOT NULL UNIQUE, email_address VARCHAR(255) NOT NULL UNIQUE, password VARCHAR(50) NOT NULL);
+
+INSERT INTO users (username, name, email_address) VALUES (test, test, test@gmail.com);
--- spec/features/peeps_spec.rb
+++ spec/features/peeps_spec.rb
@@ -7,8 +7,8 @@ feature 'User can add a peep' do
 
   scenario 'User can add a new peep on the peep page' do
     go_to_new_peep_page
-    fill_in('content', with: 'This is a test peep')
+    fill_in('content', with: 'This is my first peep')
     click_button('Peep It')
-    expect(page).to have_content('This is a test peep')
+    expect(page).to have_content('This is my first peep')
   end
 end
