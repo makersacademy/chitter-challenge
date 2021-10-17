624dea73eb4e6838c669a8f9f25055d39ba7d8de
Luke Blackburn <75164517+blue-luke@users.noreply.github.com>
2021-10-18 00:16:23 +0100

    Update user.rb

--- lib/user.rb
+++ lib/user.rb
@@ -13,12 +13,7 @@ class User
     connection = PG.connect(dbname: 'chitter')
     result = connection.exec("INSERT INTO users (email, password) VALUES('#{email}', '#{password}') RETURNING id, email;")
     User.new(id: result[0]['id'], email: result[0]['email'])
-    # Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
 
   end
-
-  # def self.find(id:)
-
-  # end
-
+  
 end
