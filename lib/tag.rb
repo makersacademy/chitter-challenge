cfa69f3a6865fff3d830a5579fc6a41ad8f092a9
Arthur Fincham <arthurfincham@me.com>
2021-10-17 12:51:58 +0100

    extract Tag class

--- lib/tag.rb
+++ lib/tag.rb
@@ -3,16 +3,38 @@ require 'pg'
 
 class Tag
 
-  def self.create(text:)
-    result = DatabaseConnection.query(
-      "INSERT INTO tags (text) VALUES ($1) RETURNING id, text;",
-      [text]
-    )
-    Tag.new(
-      id: result[0]['id'],
-      text: result[0]['text'],
-    )
+  def self.hashtag
+    query = DatabaseConnection.query("SELECT text FROM peeps ORDER BY timestamp DESC LIMIT 1")
+    peep_id = DatabaseConnection.query("SELECT id FROM peeps ORDER BY timestamp DESC LIMIT 1")
+    query.map do |qr|
+      arr = qr['text'].scan(/#\w+/).flatten
+      arr.each do |tag|
+        tagga = Tag.create(text: tag)
+        peep_id.map do |pid|
+          PeepTag.create(peep_id: pid['id'], tag_id: tagga.id)
+        end
+      end
+    end
   end
+
+  def self.create(text:)
+    begin
+      result = DatabaseConnection.query(
+        "INSERT INTO tags (text) VALUES ($1) RETURNING id, text;",
+        [text]
+      )
+      rescue PG::UniqueViolation
+        existing_tag = DatabaseConnection.query("SELECT * FROM tags WHERE text = ($1)", [text])
+        existing_tag.map do |tag|
+          return Tag.new(id: tag['id'], text: tag['text'])
+        end
+    else
+      Tag.new(
+        id: result[0]['id'],
+        text: result[0]['text'],
+      )
+    end
+  end 
 
   def self.all
     result = DatabaseConnection.query("SELECT * FROM tags")
@@ -23,7 +45,6 @@ class Tag
 
   
 
-
   attr_reader :id, :text
 
   def initialize(id:, text:)
@@ -31,6 +52,4 @@ class Tag
     @text = text
   end
 
-
 end
-
