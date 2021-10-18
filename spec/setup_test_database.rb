c61643818f564d67b2e604de5231248ce31b495a
Sergei Palmer <zigpalmer@gmail.com>
2021-10-18 11:06:09 +0100

    Remove feed truncate

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -6,6 +6,6 @@ def setup_test_database
   p 'Setting up test database...'
 
   connection = PG.connect(dbname: 'chitter_test')
-  connection.exec('TRUNCATE peeps, users, feed;')
+  connection.exec('TRUNCATE peeps, users;')
   connection.exec("INSERT INTO users (user_id, username, name, email_address, password) VALUES (1, 'sergei', 'Sergei', 'sergei@test.com', 'password')");
 end
