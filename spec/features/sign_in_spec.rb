b0229903a6c87374b08887fa6c345e9b57a9d3e4
Arthur Fincham <arthurfincham@me.com>
2021-10-18 14:01:55 +0100

    add features tests back in travis

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -1,38 +1,38 @@
-# feature 'Signing IN' do
-#   scenario 'user can sign in' do
-#     User.create(username: 'test_user', email: 'test@example.com', password: 'password123', name: 'test name')
+feature 'Signing IN' do
+  scenario 'user can sign in' do
+    User.create(username: 'test_user', email: 'test@example.com', password: 'password123', name: 'test name')
 
-#     # Then sign in as them
-#     visit '/'
-#     click_button 'Sign In'
-#     fill_in('username', with: 'test_user')
-#     fill_in('password', with: 'password123')
-#     click_button('Submit')
+    # Then sign in as them
+    visit '/'
+    click_button 'Sign In'
+    fill_in('username', with: 'test_user')
+    fill_in('password', with: 'password123')
+    click_button('Submit')
 
-#     expect(page).to have_selector(:link_or_button, 'Sign Out')
-#   end
+    expect(page).to have_selector(:link_or_button, 'Sign Out')
+  end
 
-#   scenario 'user sees an error if they get their username wrong' do
-#     User.create(username: 'test_user', email: 'test@example.com', password: 'password123', name: 'test name')
+  scenario 'user sees an error if they get their username wrong' do
+    User.create(username: 'test_user', email: 'test@example.com', password: 'password123', name: 'test name')
 
-#     visit '/'
-#     click_button 'Sign In'
-#     fill_in(:username, with: 'test_aasuser')
-#     fill_in(:password, with: 'password123')
-#     click_button('Submit')
+    visit '/'
+    click_button 'Sign In'
+    fill_in(:username, with: 'test_aasuser')
+    fill_in(:password, with: 'password123')
+    click_button('Submit')
 
-#     expect(page).not_to have_selector(:link_or_button, 'Sign Out')
-#   end
+    expect(page).not_to have_selector(:link_or_button, 'Sign Out')
+  end
 
-#   scenario 'user sees an error if they get their password wrong' do
-#     User.create(username: 'test_user', email: 'test@example.com', password: 'password123', name: 'test name')
+  scenario 'user sees an error if they get their password wrong' do
+    User.create(username: 'test_user', email: 'test@example.com', password: 'password123', name: 'test name')
 
-#     visit '/'
-#     click_button 'Sign In'
-#     fill_in(:username, with: 'test_user')
-#     fill_in(:password, with: 'passwo23')
-#     click_button('Submit')
+    visit '/'
+    click_button 'Sign In'
+    fill_in(:username, with: 'test_user')
+    fill_in(:password, with: 'passwo23')
+    click_button('Submit')
 
-#     expect(page).not_to have_selector(:link_or_button, 'Sign Out')
-#   end
-# end
+    expect(page).not_to have_selector(:link_or_button, 'Sign Out')
+  end
+end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,13 +1,13 @@
-# feature 'Registration' do
-#   scenario 'a user can sign up' do
-#     visit '/'
-#     click_button 'Sign Up'
-#     fill_in('username', with: 'test_user')
-#     fill_in('name', with: 'Test')
-#     fill_in('email', with: 'test@example.com')
-#     fill_in('password', with: 'password123')
-#     click_button('Submit')
+feature 'Registration' do
+  scenario 'a user can sign up' do
+    visit '/'
+    click_button 'Sign Up'
+    fill_in('username', with: 'test_user')
+    fill_in('name', with: 'Test')
+    fill_in('email', with: 'test@example.com')
+    fill_in('password', with: 'password123')
+    click_button('Submit')
 
-#     expect(page).to have_selector(:link_or_button, 'Sign Out')
-#   end
-# end
+    expect(page).to have_selector(:link_or_button, 'Sign Out')
+  end
+end
