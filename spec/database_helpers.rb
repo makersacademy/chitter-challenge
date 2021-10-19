2fa01eb0eff466f94d9158e7512bfd2adbad5bff
ConorButler <cwdbutler@gmail.com>
2021-10-19 13:31:57 +0100

    Typo

--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -2,6 +2,6 @@ require 'pg'
 
 def persisted_data(table:, id:)
   connection = PG.connect(dbname: 'chitter_test')
-  result = connection.query("SELECT * FROM #{table} WHERE id = #{id};")
+  result = connection.exec("SELECT * FROM #{table} WHERE id = #{id};")
   result.first
 end
