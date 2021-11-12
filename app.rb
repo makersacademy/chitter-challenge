3adb8b818d8f1251918ad7bb4933b0ab967c521d
ginnythomas <ginnynjon@gmail.com>
2021-11-12 19:28:36 +0100

    changed all method to pull from database, test still passes

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader' if development?
+require_relative './lib/peep'
 
 class Chitter < Sinatra::Base
   configure :development do
--- lib/peep.rb
+++ lib/peep.rb
@@ -1,11 +1,14 @@
+require 'pg'
+
 class Peep
 
   def self.all
-    [
-    'this is a test, should be Ginny as user',
-    'this is another test message, Ginny as user',
-    'test message, Jon as user',
-    'Grace as user'
-    ]
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec('SELECT * FROM peeps;')
+    result.map { |peep| peep['content'] }
+    #   'this is a test, should be Ginny as user',
+    # 'this is another test message, Ginny as user',
+    # 'test message, Jon as user',
+    # 'Grace as user'
   end
 end
