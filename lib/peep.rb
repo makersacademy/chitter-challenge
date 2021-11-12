1a0b2f571da49d9479df3dfa1202571c439637ee
ginnythomas <ginnynjon@gmail.com>
2021-11-12 19:52:17 +0100

    connected to database for test environment in rb and spec rb files - tests passing

--- lib/peep.rb
+++ lib/peep.rb
@@ -3,12 +3,8 @@ require 'pg'
 class Peep
 
   def self.all
-    connection = PG.connect(dbname: 'chitter')
+    connection = PG.connect(dbname: "chitter#{'_test' if ENV['ENVIRONMENT'] == 'test'}")
     result = connection.exec('SELECT * FROM peeps;')
     result.map { |peep| peep['content'] }
-    #   'this is a test, should be Ginny as user',
-    # 'this is another test message, Ginny as user',
-    # 'test message, Jon as user',
-    # 'Grace as user'
   end
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,14 +1,22 @@
+require 'pg'
+
 require_relative '../lib/peep'
 
 describe Peep do
   describe '.all' do
-    it 'returns all peeps (regardless of log in status' do
-    peeps = Peep.all
-    expect(peeps).to include 'this is a test, should be Ginny as user'
-    expect(peeps).to include 'this is another test message, Ginny as user'
-    expect(peeps).to include 'test message, Jon as user'
-    expect(peeps).to include 'Grace as user'
+    it 'returns all peeps (regardless of log in status)' do
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO 'peeps' (content) VALUES ('this is a test, should be Ginny as user');")
+      connection.exec("INSERT INTO 'peeps' (content) VALUES ('this is another test message, Ginny as user');")
+      connection.exec("INSERT INTO 'peeps' (content) VALUES ('test message, Jon as user');")
+      connection.exec("INSERT INTO 'peeps' (content) VALUES ('Grace as user');")
+
+      peeps = Peep.all
+
+      expect(peeps).to include 'this is a test, should be Ginny as user'
+      expect(peeps).to include 'this is another test message, Ginny as user'
+      expect(peeps).to include 'test message, Jon as user'
+      expect(peeps).to include 'Grace as user'
     end
   end
 end
-
