b1354193c5327529adab4c03feee8f41bce7c60f
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-13 18:48:48 +0100

    Trying to add time stamp to peeps, added some CSS styling

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,12 +2,13 @@ require 'pg'
 
 class Peep 
   
-  attr_reader :id, :username, :message
+  attr_reader :id, :username, :message, :time
 
-  def initialize(id:, username:, message:)
+  def initialize(id:, username:, message:, time:)
     @id = id
     @username = username
     @message = message
+    @time = time
   end
 
   def self.all
@@ -16,9 +17,9 @@ class Peep
     else
       connection = PG.connect(dbname: 'chitter_data')
     end
-    result = connection.exec "SELECT * FROM peeps;"
+    result = connection.exec "SELECT * FROM peeps ORDER BY id DESC;"
     result.map do |peep|
-      Peep.new(id: peep['id'], username: peep['username'], message: peep['message'])
+      Peep.new(id: peep['id'], username: peep['username'], message: peep['message'], time:['time'])
     end
   end
 
@@ -29,8 +30,10 @@ class Peep
       connection = PG.connect(dbname: 'chitter_data')
     end
 
-    result = connection.exec_params("INSERT INTO peeps (username, message) VALUES($1, $2) RETURNING id, username, message;", [username, message] )
-    Peep.new(id: result[0]['id'], username: result[0]['username'], message: result[0]['message'])
+    time = Time.now
+
+    result = connection.exec_params("INSERT INTO peeps (username, message) VALUES($1, $2) RETURNING id, username, message, time;", [username, message] )
+    Peep.new(id: result[0]['id'], username: result[0]['username'], message: result[0]['message'], time: result[0]["#{Time.now}"])
   end
 
 end
---
+++ spec/peep_spec.rb
@@ -0,0 +1,24 @@
+require 'peep'
+
+describe Peep do
+  describe '#all' do
+    it 'returns all peeps' do
+      connection = PG.connect(dbname: 'chitter_data_test')
+      Peep.add("RyanGrimes", "Test peep")
+
+      peep = Peep.all
+      expect(peep.length).to eq 1
+      expect(peep.first.message).to eq "Test peep"
+    end
+  end
+
+  describe '#add' do
+    it 'adds a new peep' do
+        connection = PG.connect(dbname: 'chitter_data_test')
+        Peep.add("RyanGrimes", "Test peep")
+        Peep.add("GrimesRyan", "Pest teep")
+        peep = Peep.all
+        expect(peep.length).to eq 2
+    end
+  end
+end
