cb7e852f8150220aca212159143fd181c65135cc
Ryan Grimes <ryanpaulgrimes@gmail.com>
2021-11-13 16:53:35 +0100

    Fixed test that was broken

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -4,10 +4,10 @@ feature 'add new peep' do
   scenario 'user can add new peeps to the page' do
     connection = PG.connect(dbname: 'chitter_data_test')
 
-    visit('/')
+    visit('/peeps')
     fill_in :username, with: 'RyanGrimes'
-    fill_in :message, with: 'This is my first peep :D'
-    click_button('Submit')
-    expect(page).to have_content 'This is my first peep :D'
+    fill_in :message, with: 'Testing 1, 2, 3'
+    click_button('Post Peep')
+    expect(page).to have_content 'Testing 1, 2, 3'
   end
 end
