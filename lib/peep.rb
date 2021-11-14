18af6416620e33814c85687f24ce62a47af8a792
ginnythomas <ginnynjon@gmail.com>
2021-11-14 12:42:16 +0100

    initialized instance veriables to be able to map the database to an object

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,17 +1,23 @@
 require 'pg'
 
 class Peep
+  attr_reader :name, :user_name, :timestamp, :content
+
+  def initialize(name, user_name, timestamp, content)
+    @name = name
+    @user_name = user_name
+    @timestamp = timestamp
+    @content = content
+  end
 
   def self.all
     connection = PG.connect(dbname: "chitter#{'_test' if ENV['ENVIRONMENT'] == 'test'}")
-    result = connection.exec('SELECT peeps."content", chit_user."name",
-      chit_user.user_name, peeps."timestamp"
+    result = connection.exec('SELECT chit_user."name",
+      chit_user.user_name, peeps."timestamp", peeps."content"
       FROM peeps
         INNER JOIN chit_user ON chit_user.chit_user_id = PEEPS.chit_user_id
       ORDER BY timestamp DESC;')
-    result.map { |peep| peep}
-# (peep['content'], peep["'chit_user.'name'"], peep['chit_user.user_name'], peep['timestamp']) }
-
+    result.map { |dbrow| Peep.new(dbrow['name'], dbrow['user_name'], dbrow['timestamp'], dbrow['content']) }
   end
 end
 
