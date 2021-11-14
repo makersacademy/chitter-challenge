4c1523774b9cb484c0334989624fc9f70c595174
ginnythomas <ginnynjon@gmail.com>
2021-11-14 20:19:25 +0000

    sign_up feature test passing & data hits the database

--- app.rb
+++ app.rb
@@ -6,6 +6,7 @@ require_relative './lib/peep'
 class Chitter < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+    enable :sessions
   end
 
   get '/' do
@@ -21,5 +22,23 @@ class Chitter < Sinatra::Base
   get '/sign_up' do
     erb :sign_up
   end
+
+  post '/confirm' do
+    txt_chit_name = params['txt_chit_name']
+    txt_chit_user_name = params['txt_chit_user_name']
+    session[:user_name] = txt_chit_user_name
+    txt_chit_email = params['txt_chit_email']
+    txt_password = params['txt_password']
+
+    connection = PG.connect(dbname: 'chitter_test')
+    connection.exec("INSERT INTO chit_user (name, user_name, email, password) VALUES('#{txt_chit_name}', '#{txt_chit_user_name}', '#{txt_chit_email}', '#{txt_password}');")
+    redirect '/confirm_sign_up'
+  end
+
+  get '/confirm_sign_up' do
+    @user_name = session[:user_name]
+    erb :confirm_sign_up
+  end
+
   run! if app_file == $PROGRAM_NAME
 end
--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,6 +2,7 @@ feature 'user can sign_up for chitter account' do
   scenario 'so that user can post to chitter, they can sign up for an account' do
     visit '/'
     click_button 'Sign up for a Chitter Account'
+
     expect(page).to have_button 'Sign Up!'
   end
 end
