680fea4eca559b4db208f076ea0671b30d334107
Mark McLeod <markmcleod45@gmail.com>
2021-11-14 13:29:28 +0000

    Update viewing peeps feature test to use new create method

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -4,12 +4,12 @@ feature 'View peeps' do
   scenario 'A user can see multiple peeps' do
     connection = PG.connect(dbname: 'chitter_test')
 
-    connection.exec("INSERT INTO peeps (message) VALUES('This is my peep');")
-    connection.exec("INSERT INTO peeps (message) VALUES('Another peep');")
-    connection.exec("INSERT INTO peeps (message) VALUES('Third peep');")
+    Peep.create(message: "This is my peep!")
+    Peep.create(message: "Another peep")
+    Peep.create(message: "Third peep")
 
     visit('/peeps')
-    
+
     expect(page).to have_content 'This is my peep'
     expect(page).to have_content 'Another peep'
     expect(page).to have_content 'Third peep'
