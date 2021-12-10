fc46b07a1b5e043a0c4015bb6a27fb06fa476ab3
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-10 18:29:28 +0000

    Write test for adding messages to database

--- spec/unit/message_spec.rb
+++ spec/unit/message_spec.rb
@@ -1,6 +1,13 @@
 require 'message'
 
 describe Message do
+  describe '.create' do
+    connection = PG.connect :dbname => 'chitter_test'
+    connection.exec("INSERT INTO messages VALUES")
+    Message.create(text: 'Hello World!')
+    messages = Message.all
+    expect(messages.first.text).to eq 'Hello World!'
+  end
   describe '.all' do
     it 'returns an array of messages' do
       messages = Message.all
