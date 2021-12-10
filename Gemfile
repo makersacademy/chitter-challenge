cfcba00d197b603720e2ff8c1235c9a3cb55ea64
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-10 17:46:51 +0000

    Setup Sinatra web project with RSpec and Capybara

--- Gemfile
+++ Gemfile
@@ -2,10 +2,15 @@ source 'https://rubygems.org'
 
 ruby '3.0.2'
 
+gem 'pg'
+gem 'sinatra'
+
 group :test do
+  gem 'capybara'
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'sinatra-contrib'
 end
 
 group :development, :test do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Chitter
+
 require 'simplecov'
 require 'simplecov-console'
 
@@ -15,3 +25,4 @@ RSpec.configure do |config|
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
 end
+
