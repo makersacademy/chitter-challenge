d886e25f0f3b46adefc35901585bd91acf418794
riky moroni <riky5.moroni@gmail.com>
2021-12-10 20:23:29 +0000

    add feature test: post message

--- app.rb
+++ app.rb
@@ -2,13 +2,24 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Chitter < Sinatra::Base 
+  enable :sessions
 
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    'Hello World'
+    erb :index
+  end
+
+  post '/post_message' do
+    session[:message] = params[:message]
+    redirect '/home'
+  end
+
+  get '/home' do
+    @message = session[:message]
+    erb :messages
   end
 
   run! if app_file == $0
--- spec/features/post_message_spec.rb
+++ spec/features/post_message_spec.rb
@@ -1,7 +1,9 @@
 feature 'Post a message' do
   scenario 'it should add a message to chitter' do
     visit('/')
-    click_on('login')
+    click_on('Login')
+    fill_in('message', with: 'My first message')
+    click_on('Post')
     expect(page).to have_content('My first message')
   end
 end
