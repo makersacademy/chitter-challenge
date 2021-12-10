e3dd5634d814bb85d35ae0876c6b402aefda732a
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-10 23:00:09 +0000

    Implement User class

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,17 @@
+require_relative './database_connection'
+
 class User
+  attr_reader :id, :username, :password
+
+  def initialize(id:, username:, password:)
+    @id = id
+    @username = username
+    @password = password
+  end
+
+  def self.create(username:, password:)
+    result = DatabaseConnection.query("INSERT INTO users (username, password) VALUES($1, $2) RETURNING username, password", [username, password])
+    User.new(id: result[0]['id'], username: result[0]['username'], password: result[0]['password'])
+  end
 
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -3,8 +3,9 @@ require 'user'
 describe User do
   describe '.create' do
     it 'creates a user with a name and password' do
-      user = User.create(name: 'Sherwin', password: 'test123')
-      expect(user.name).to eq 'Sherwin'
+      user = User.create(username: 'Sherwin', password: 'test123')
+      
+      expect(user.username).to eq 'Sherwin'
       expect(user.password).to eq 'test123'
     end
   end
