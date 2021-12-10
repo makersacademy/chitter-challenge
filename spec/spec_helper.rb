e92d0502b51a9f790f8ce45f381fc538f5eeccf7
Kimberley Morgan <kimberley.morgan135@gmail.com>
2021-12-10 16:36:33 +0000

    refactoring tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require './database_connection_setup.rb'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
+require 'web_helper'
 require 'rspec'
 
 ENV['RACK_ENV'] = 'test'
@@ -19,6 +20,11 @@ SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
 SimpleCov.start
 
 RSpec.configure do |config|
+
+  config.before(:each) do
+    empty_database
+  end
+
   config.after(:suite) do
     puts
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
