112ffd48d44df29d551af2f83671bd67da81fef8
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-10 20:45:53 +0000

    Add stores time into Message .create unit test

--- spec/unit/message_spec.rb
+++ spec/unit/message_spec.rb
@@ -2,10 +2,10 @@ require 'message'
 
 describe Message do
   describe '.create' do
-    it 'creates a new message' do
-      message = Message.create(text: 'Hello World!')
-      p message
+    it 'creates a new message with text and time' do
+      message = Message.create(text: 'Hello World!', time: Time.now.localtime)
       expect(message.text).to eq 'Hello World!'
+      expect(message.text).to eq "#{expect(message.text).to eq 'Hello World!'}"
     end
   end
   
