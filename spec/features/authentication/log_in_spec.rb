1907db0d5b708e1b8ec668564173f55285bd1a87
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-10 23:38:53 +0000

    Write feature test for logging in

---
+++ spec/features/authentication/log_in_spec.rb
@@ -0,0 +1,15 @@
+require './lib/user'
+
+feature 'Login' do
+  context 'Logging in' do
+    scenario 'shows a welcome messsage' do
+      User.create(username: 'Sherwin', password: 'password123')
+      visit '/'
+      fill_in('username', with: 'Sherwin')
+      fill_in('password', with: 'password123')
+      click_button 'Log In'
+      expect(page).to have_content "Welcome, Sherwin!"
+      expect(page.current_path).to eq('/messages')
+    end
+  end
+end
