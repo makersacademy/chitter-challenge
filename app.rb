2504a6aefc701fdd8e77c67e06ba92265cbaf802
Jessica Gordon <ketchesj@gmail.com>
2021-12-10 21:56:21 +0000

    sets up database connections for test and development interaction using pg gem

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+require './database_connection_setup'
+
 class Chitter < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
