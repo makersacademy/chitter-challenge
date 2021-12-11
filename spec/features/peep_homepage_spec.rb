56c302aeba3b66d8b009e70b46613f7bde96ab49
Jessica Gordon <ketchesj@gmail.com>
2021-12-11 22:13:25 +0000

    user can view peeps in reverse chronological order

--- spec/features/peep_homepage_spec.rb
+++ spec/features/peep_homepage_spec.rb
@@ -11,7 +11,7 @@ feature 'homepage for chitter' do
     expect(page).to have_content 'Oh hey chitter!'
   end
 
-  scenario 'user can view multiple peeps on the homepage' do
+  scenario 'user can view multiple peeps on the homepage in reverse chronological order' do
     DatabaseConnection.query("INSERT INTO Users(email, password, name, username) VALUES($1, $2, $3, $4);", ['example@gmail.com', '*****', 'Birdy', 'fly_away'])
     user_id = DatabaseConnection.query("SELECT id FROM Users WHERE email = 'example@gmail.com';")
     DatabaseConnection.query("INSERT INTO Peeps(user_id, time, content) VALUES($1, $2, $3);", [user_id[0]["id"].to_i, '1999-01-08 04:05:06', 'Oh hey chitter!'])
@@ -19,8 +19,8 @@ feature 'homepage for chitter' do
 
     visit('/')
     
-    expect(page).to have_content 'Oh hey chitter!'
-    expect(page).to have_content "Man I'm getting old!"
+    expect(all(".peep").first).to have_content "Man I'm getting old!"
+    expect(all(".peep").last).to have_content "Oh hey chitter!"
   end
 
   scenario 'user can add a peep to the homepage' do
