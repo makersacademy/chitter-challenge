3d119d82e4da32ec4ca1515a428f6d58afe62f82
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-11 02:39:37 +0000

    Implement user authentication in User model

--- lib/user.rb
+++ lib/user.rb
@@ -16,7 +16,7 @@ class User
     result = DatabaseConnection.query(
 "INSERT INTO users (username, password) VALUES($1, $2) RETURNING id, username, password", [
   username, encrypted_password])
-    User.new(id: result[0]['id'], username: result[0]['username'])
+    User.new(id: result[0]['id'], username: result[0]['username'], password: result[0]['password'])
   end
 
   def self.find(id)
@@ -27,6 +27,9 @@ class User
 
   def self.authenticate(username:, password:)
     result = DatabaseConnection.query("SELECT * FROM users WHERE username = $1",[username])
+    return unless result.any?
+    return unless BCrypt::Password.new(result[0]['password']) == password
+
     User.new(id: result[0]['id'], username: result[0]['username'], password: result[0]['password'])
   end
 end
