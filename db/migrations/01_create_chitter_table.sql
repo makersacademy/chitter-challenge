304e1dddab4d4f04ea62399b6b804c22dd2e5fdf
Adrienne Galloway <adriennegalloway@hotmail.com>
2021-12-11 22:00:39 +0000

    css on the timeline page

--- db/migrations/01_create_chitter_table.sql
+++ db/migrations/01_create_chitter_table.sql
@@ -1,4 +1,2 @@
--- CREATE TABLE chitter (id SERIAL PRIMARY KEY, name VARCHAR(60), post VARCHAR(280));
-
 CREATE TABLE chitter (id SERIAL PRIMARY KEY, name VARCHAR(240), post VARCHAR(280), author_email VARCHAR(60), author_name VARCHAR(60), author_id INTEGER REFERENCES users (id))
 
