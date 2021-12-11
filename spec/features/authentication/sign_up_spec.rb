f3d74ed714fdd09e8ac3c0f1feba47d3637ff479
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-11 12:52:13 +0000

    Write test for unique username validation

--- spec/features/authentication/sign_up_spec.rb
+++ spec/features/authentication/sign_up_spec.rb
@@ -7,5 +7,11 @@ feature 'Registrations' do
       expect(page).to have_content "Welcome, Sherwin!"
       expect(page.current_path).to eq('/messages')
     end
+
+    scenario 'with a username that already exists' do
+      sign_up
+      sign_up
+      expect(page).to have_content "Username already exists."
+    end
   end
 end
