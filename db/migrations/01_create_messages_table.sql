71267aa93ebc59ae52c83b759f19ac437a3edac1
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-11 17:28:57 +0000

    Add new migrations for replies table

--- db/migrations/01_create_messages_table.sql
+++ db/migrations/01_create_messages_table.sql
@@ -1,8 +1,15 @@
 CREATE TABLE messages(id SERIAL PRIMARY KEY, text VARCHAR(280), created_at TIMESTAMP WITHOUT TIME ZONE);
+
 CREATE TABLE users(
   id SERIAL PRIMARY KEY, 
   username VARCHAR(20), 
   password VARCHAR(140),
-  user_id int,
+  user_id INT,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+CREATE TABLE replies(
+  id SERIAL PRIMARY KEY, 
+  text VARCHAR(280), 
+  created_at TIMESTAMP WITHOUT TIME ZONE, 
+  message_id INT REFERENCES messages (id));
