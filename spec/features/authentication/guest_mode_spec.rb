0abf5d6e2a4a6fbc1a049feb5df224f2a87fbd7c
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-11 02:52:12 +0000

    Write feature tests for guest mode

---
+++ spec/features/authentication/guest_mode_spec.rb
@@ -0,0 +1,14 @@
+feature 'guest mode' do
+  scenario 'when viewing messages without logging in' do
+    visit '/'
+    click_button 'Continue as guest'
+    expect(page).to_not have_content "Welcome"
+    expect(page.current_path).to eq('/messages')
+  end
+
+  scenario 'cannot post' do
+    visit('/messages')
+    fill_in('text', with: 'Hello World!')
+    click_button('Submit')
+    expect(page).to have_content 'Log in or sign up to post!'
+end
