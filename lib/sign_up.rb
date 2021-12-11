5caad2614a2cbb2d103d0df2c7f748d5087d73a7
Chris Clement <chrisclement@Chriss-Air.connect>
2021-12-11 13:41:02 +0000

    create method for sign_up

--- lib/sign_up.rb
+++ lib/sign_up.rb
@@ -2,8 +2,18 @@ require 'database_connection'
 
 class SignUp
 
+  attr_reader :email, :user_name, :handle
+
+  def initialize(email:, password:, user_name:, handle:)
+    @email = email
+    @password = password
+    @user_name = user_name
+    @handle = handle
+  end
+
   
   def self.create(email:, password:, user_name:, handle:)
-    
+    result = DatabaseConnection.query("INSERT INTO sign_up(email, password, user_name, handle) VALUES ($1, $2, $3, $4) RETURNING email, password, user_name, handle;", [email, password, user_name, handle])
+    p SignUp.new(email: result[0]['email'], password: result[0]['password'], user_name: result[0]['user_name'], handle: result[0]['handle'])
   end
 end
--- spec/unit/sign_up_spec.rb
+++ spec/unit/sign_up_spec.rb
@@ -3,9 +3,9 @@ describe SignUp do
   describe '.create' do
     it 'adds a user to the sign_up database' do
       example_user = SignUp.create(email: 'example1@fake_email.com', password: 'Password123', user_name: 'Joe Biden', handle: 'sleepy_joe')
-      expect(example_user.email).to eq 
-      expect(example_user.user_name).to eq 
-      expect(example_user.handle).to eq 
+      expect(example_user.email).to eq 'example1@fake_email.com'
+      expect(example_user.user_name).to eq 'Joe Biden'
+      expect(example_user.handle).to eq 'sleepy_joe'
     end
   end
 end
