c99f62ef0bdb41dd37e151fba77c88a3578cc97d
Chris Clement <chrisclement@Chriss-Air.connect>
2021-12-11 12:59:53 +0000

    Add routing for signing up to Chitter. Sign up info not currently stored

--- app.rb
+++ app.rb
@@ -19,4 +19,12 @@ class Chitter < Sinatra::Base
     Peep.create(text: params[:text], user_name: params[:user_name], handle: params[:handle], time_posted: Time.new)
     redirect '/'
   end
+
+  get '/sign_up' do
+    erb :sign_up
+  end
+
+  post '/confirm_sign_up' do
+    erb :confirm_sign_up
+  end
 end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,12 @@
+feature 'User can sign up for Chitter' do 
+  scenario 'User signs up with email, password, name and username' do 
+    visit '/'
+    click_on 'Sign up'
+    fill_in 'email', with: 'johnsmith@fakeemail.com'
+    fill_in 'password', with: 'Pas$word123'
+    fill_in 'user_name', with: 'John Smith'
+    fill_in 'handle', with: 'the_real_john_smith'
+    click_on 'Submit'
+    expect(page).to have_content 'Thanks for signing up for Chitter John!'
+  end
+end
