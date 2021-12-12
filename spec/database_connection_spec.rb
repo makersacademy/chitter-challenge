5b61b6e871849399a921591f3f982a20e7096519
Sarah <dousse.sarah@gmail.com>
2021-12-12 11:46:34 +0000

    testing it the connection is persistent

--- spec/database_connection_spec.rb
+++ spec/database_connection_spec.rb
@@ -4,9 +4,16 @@ describe DatabaseConnection do
 
   describe '.setup' do
     it 'sets up a connection to a database through PG' do
-      expect(PG).to receive(:connect).with(dbname: 'bookmarks_manager_test')
+      expect(PG).to receive(:connect).with(dbname: 'chitter_app_test')
 
-      DatabaseConnection.setup('bookmarks_manager_test')
+      DatabaseConnection.setup('chitter_app_test')
     end
+  end
+
+  it 'this connection is persistent' do
+    # Grab the connection as a return value from the .setup method
+    connection = DatabaseConnection.setup('chitter_app_test')
+
+    expect(DatabaseConnection.connection).to eq connection
   end
 end
