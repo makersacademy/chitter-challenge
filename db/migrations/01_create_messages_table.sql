a19c94bff0cf27d85a410de7f2f4439d4cb77593
Sherwin Panganiban <sherwingpanganiban@gmail.com>
2021-12-12 23:33:19 +0000

    Setup travis

--- db/migrations/01_create_messages_table.sql
+++ db/migrations/01_create_messages_table.sql
@@ -1,9 +1,3 @@
-CREATE TABLE users(
-  id SERIAL PRIMARY KEY, 
-  username VARCHAR(20), 
-  password VARCHAR(140)
-);
-
 CREATE TABLE messages(
   id SERIAL PRIMARY KEY, 
   text VARCHAR(280), 
@@ -11,6 +5,12 @@ CREATE TABLE messages(
   user_id INT REFERENCES users(id)
 );
 
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY, 
+  username VARCHAR(20), 
+  password VARCHAR(140)
+);
 
 CREATE TABLE replies(
   id SERIAL PRIMARY KEY, 
