6bc878730afbc885b78d73d70a6fa4f955baac14
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2021-12-12 21:32:01 +0000

    added posting test

--- spec/features/posting_message_spec.rb
+++ spec/features/posting_message_spec.rb
@@ -7,4 +7,8 @@ feature "Posting messages" do
 
     expect(page).to have_content("Hi mom")
   end
+
+  scenario "Can't post message if not logged in" do
+    expect(page).to_not have_button("Chitter")
+  end
 end
