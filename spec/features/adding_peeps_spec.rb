eb6a7b5d8a1dc7e236a3611f1a98410c476734b6
Tatiana Nantes <tatiananantes@gmail.com>
2021-12-30 18:18:36 +0000

    create users DB

--- spec/features/adding_peeps_spec.rb
+++ spec/features/adding_peeps_spec.rb
@@ -1,5 +1,22 @@
-require 'pg'
+# require 'pg'
 
+# feature 'Creating peeps' do
+#   scenario 'A user can create a peep' do
+#     visit('/users/new')
+#     fill_in('email', with: 'pepe@mail.com')
+#     fill_in('password', with: 'password123')
+#     click_button('Register')
+#     click_link('New Peep!')
+#     expect(page).to have_field('text')
+#     fill_in('text', with: 'My first peep!')
+#     click_button('Submit')
+#     expect(page).to have_content('My first peep!')
+#     expect(page).to have_current_path('/users')
+#     expect(page).to have_content('My first peep!')
+#   end
+# end
+
+require 'pg'
 feature 'Creating peeps' do
   scenario 'A user can create a peep' do
     visit('/peeps/new')
