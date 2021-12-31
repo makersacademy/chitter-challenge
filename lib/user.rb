63532e319e5dcb44bef28a496145c0bf6527d8e8
Tatiana Nantes <tatiananantes@gmail.com>
2021-12-31 00:46:58 +0000

    find user by ID method tests passing

--- lib/user.rb
+++ lib/user.rb
@@ -11,7 +11,6 @@ class User
   end
   
   def self.create(username:, email:, password:)
-    
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_test')
     else
@@ -21,5 +20,17 @@ class User
     result = connection.exec_params("INSERT INTO users (username, email, password) VALUES($1, $2, $3) RETURNING id, username, email, password;", [username, email, password])
     User.new(id: result[0]['id'], username: result[0]['username'], email: result[0]['email'], password: result[0]['password'])
   end
+
+  def self.find(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end  
+
+    result = connection.exec_params("SELECT * FROM users WHERE id = $1", [id])
+    result[0]['id']
+  end
+
 
 end
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -5,10 +5,20 @@ describe '.create' do
     connection = PG.connect(dbname: 'chitter_test')
     user = User.create(username: 'Hagrid', email: 'hagrid@gmail.com', password: 'hagrid123')
    
-    # expect(user).to be_a User
+    expect(user).to be_a User
     # expect(user.id).to eq(persisted_data['id'])
     expect(user.username).to eq('Hagrid')
     expect(user.email).to eq('hagrid@gmail.com')
     expect(user.password).to eq('hagrid123')
+  end
+end
+
+describe '.find' do
+  it 'finds a user by ID' do
+    connection = PG.connect(dbname: 'chitter_test')
+    user = User.create(username: 'Hagrid', email: 'hagrid@gmail.com', password: 'hagrid123')
+    result = User.find(id: user.id)
+
+    expect(result).to eq(user.id)
   end
 end
