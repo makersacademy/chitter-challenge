abed4975a2b7e9c390c37c15ba281682b297b211
Arthur Fincham <arthurfincham@me.com>
2022-01-11 14:25:51 +0000

    update remote db

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -3,7 +3,7 @@ require "./lib/database_connection"
 if ENV["ENVIRONMENT"] == "test"
   DatabaseConnection.setup("chitter_test")
 elsif ENV["ENVIRONMENT"] == "production"
-  DatabaseConnection.setup("chitter_production")
+  DatabaseConnection.setup("dbbe1lrkgacr5n")
 else
   DatabaseConnection.setup("chitter_development")
 end
