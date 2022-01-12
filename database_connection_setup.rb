5d15b18029ebe9e01d19f86f7d5f0504176b6d98
Arthur Fincham <arthurfincham@me.com>
2022-01-12 16:22:10 +0000

    try again

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -3,7 +3,9 @@ require "./lib/database_connection"
 if ENV["ENVIRONMENT"] == "test"
   DatabaseConnection.setup(dbname: "chitter_test")
 elsif ENV["ENVIRONMENT"] == "production"
-  DatabaseConnection.setup(host: ENV["CHITTER_DB_HOST"], port: ENV["CHITTER_DB_PORT"], dbname: ENV["CHITTER_DB_NAME"], user: ENV["CHITTER_DB_USERNAME"], password: ENV["CHITTER_DB_PASSWORD"])
+  DatabaseConnection.setup(dbname: "chitter_production")
 else
   DatabaseConnection.setup(dbname: "chitter_development")
 end
+
+# host: ENV["CHITTER_DB_HOST"], port: ENV["CHITTER_DB_PORT"], dbname: ENV["CHITTER_DB_NAME"], user: ENV["CHITTER_DB_USERNAME"], password: ENV["CHITTER_DB_PASSWORD"]
