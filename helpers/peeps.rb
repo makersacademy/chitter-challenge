94f2fa36b157b31a2d497a57beea307c099271ad
Arthur Fincham <arthurfincham@me.com>
2022-01-12 16:55:17 +0000

    update image for chitter guide

--- helpers/peeps.rb
+++ helpers/peeps.rb
@@ -1,16 +1,15 @@
 module PeepHelper
-
   def peep_username(id)
-    result = DatabaseConnection.query('SELECT * FROM users WHERE id = $1;', [id])
-    user = User.new(id: result[0]['id'], username: result[0]['username'], email: result[0]['email'], 
-name: result[0]['name'])
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = $1;", [id])
+    user = User.new(id: result[0]["id"], username: result[0]["username"], email: result[0]["email"],
+                    name: result[0]["name"])
     user.username
   end
 
   def peep_name(id)
-    result = DatabaseConnection.query('SELECT * FROM users WHERE id = $1;', [id])
-    user = User.new(id: result[0]['id'], username: result[0]['username'], email: result[0]['email'], 
-name: result[0]['name'])
+    result = DatabaseConnection.query("SELECT * FROM users WHERE id = $1;", [id])
+    user = User.new(id: result[0]["id"], username: result[0]["username"], email: result[0]["email"],
+                    name: result[0]["name"])
     user.name
   end
 
@@ -21,16 +20,16 @@ name: result[0]['name'])
 
   def image_picker(id)
     case id
-      when "1"
-        "/images/neville.png"
-      when "2"
-        "/images/mouse.png"
-      when "3"
-        "/images/tyson.png"
-      when "4"
-        "/images/press.png"
-      else
-        "/images/avatar.png"
+    when "1"
+      "/images/neville.png"
+    when "2"
+      "/images/mouse.png"
+    when "3"
+      "/images/tyson.png"
+    when "4"
+      "/images/favicon.png"
+    else
+      "/images/avatar.png"
     end
   end
 end
