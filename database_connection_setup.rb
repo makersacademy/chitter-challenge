247e5c3988497d5707d9467bd98925c4b6b2c1c0
Arthur Fincham <arthurfincham@me.com>
2022-01-12 11:33:23 +0000

    update prod db name

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -3,7 +3,7 @@ require "./lib/database_connection"
 if ENV["ENVIRONMENT"] == "test"
   DatabaseConnection.setup("chitter_test")
 elsif ENV["ENVIRONMENT"] == "production"
-  DatabaseConnection.setup("dbbe1lrkgacr5n")
+  DatabaseConnection.setup("chitter_production")
 else
   DatabaseConnection.setup("chitter_development")
 end
