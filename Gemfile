c1940f7c01b89caaa2e352cc31bf66fc123a0a6f
Hannah Desmond <desmond.hannah@googlemail.com>
2022-01-16 13:39:01 +0000

    Set up Ruby project

--- Gemfile
+++ Gemfile
@@ -6,8 +6,11 @@ group :test do
   gem 'rspec'
   gem 'simplecov', require: false
   gem 'simplecov-console', require: false
+  gem 'capybara'
 end
 
 group :development, :test do
   gem 'rubocop', '1.20'
+  gem 'sinatra'
+  gem 'sinatra-contrib'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,13 @@
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'capybara/rspec'
+
+Capybara.app = Chitter
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
