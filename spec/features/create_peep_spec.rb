32526c9b51a41dc1b441ffe88dcc926395762dd1
OrganicAC <anthonychaplin17@gmail.com>
2022-01-16 19:15:09 +0000

    fixed 1 test, 1 test failing

--- spec/features/create_peep_spec.rb
+++ spec/features/create_peep_spec.rb
@@ -2,7 +2,7 @@ feature 'Create a new peep' do
   scenario 'Adding a peep' do 
     visit '/peeps/new'
 
-    fill_in('peep', with: 'Test peep')
+    fill_in('message', with: 'Test peep')
     click_button 'submit'
 
     expect(page).to have_content 'Test peep'
