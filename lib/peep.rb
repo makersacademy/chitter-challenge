28d22d6737875cba85e298e69be78db26df5b35a
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-17 05:00:57 +0000

    Refactor - extract from Peep to DatabaseConnection

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,5 +1,3 @@
-require 'pg'
-
 class Peep
   attr_reader :peep, :peeped_at
 
@@ -10,21 +8,11 @@ class Peep
 
   def self.create(peep)
     time = Time.now
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    peeps = connection.exec_params("INSERT INTO peeps(peep, peeped_at, nanosecs) VALUES ($1, $2, $3);", [peep, time, time.nsec])
+    peeps = DatabaseConnection.query("INSERT INTO peeps(peep, peeped_at, nanosecs) VALUES ($1, $2, $3);", [peep, time, time.nsec])
   end
 
   def self.show
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    peeps = connection.exec("SELECT * FROM peeps ORDER BY peeped_at DESC, nanosecs DESC;")
+    peeps = DatabaseConnection.query("SELECT * FROM peeps ORDER BY peeped_at DESC, nanosecs DESC;")
     peeps.map { |peep| Peep.new(peep['peep'], peep['peeped_at']) }
   end
 end
