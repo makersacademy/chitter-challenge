0ab62376144479bdf7b0b6d20a4234b47a2fc0a8
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2022-02-02 10:45:53 +0000

    readded logic to view

--- app/controllers/peeps_controller.rb
+++ app/controllers/peeps_controller.rb
@@ -4,7 +4,6 @@ class PeepsController < Sinatra::Base
   
   get '/peeps' do
     @user = User.find(id: session[:user_id])
-    @user_name = User.find(id: chitter.user_id).username
     @chitters = Peep.all
     erb(:index)
   end
