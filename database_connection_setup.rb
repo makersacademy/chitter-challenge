dea98e3e3c41cdf7136b6a6c73ebb7095b5b020c
Tatiana Nantes <tatiananantes@gmail.com>
2022-02-03 19:31:34 +0000

    add DB env for heroku

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -2,7 +2,8 @@ require './lib/database_connection'
 
 if ENV['ENVIRONMENT'] == 'test'
   DatabaseConnection.setup('chitter_test')
+elsif ENV['RACK_ENV'] == 'production'
+  DatabaseConnection.setup(ENV['DATABASE_URL'])
 else
   DatabaseConnection.setup('chitter')
 end
-
--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -1,11 +1,3 @@
-# require './lib/database_connection'
-
-# def persisted_data(table:, id:)
-#   DatabaseConnection.query(
-#     "SELECT * FROM $1 WHERE id = $2;",
-#     [table, id]
-#   )
-# end
 
 require 'pg'
 
@@ -14,8 +6,3 @@ def persisted_data(id:, table:)
   connection.query("SELECT * FROM #{table} WHERE id = '#{id}';")
 end
 
-# def persisted_data(id:)
-#   connection = PG.connect(dbname: 'chitter_test')
-#   result = connection.query("SELECT * FROM peeps WHERE id = #{id};")
-#   result.first
-# end
