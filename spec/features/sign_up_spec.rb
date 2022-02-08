e9f507b2973bd1eb0b3474717c1a9920b93dada5
MattW <mattnwarnock@yahoo.co.uk>
2022-02-08 15:25:06 +0000

    Create sign up helper

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,12 +2,7 @@
 
 feature 'signing up' do
   scenario 'a user can signup' do
-    visit('/chitter')
-    click_button 'sign-up'
-
-    fill_in 'email', with: 'example@email.com'
-    fill_in 'password', with: 'password123'
-    click_button 'submit'
+    sign_up_user
 
     expect(page).to have_content 'Welcome, example@email.com'
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@ ENV['ENVIRONMENT'] = 'test'
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require_relative './clear_test_database'
+require_relative './sign_up_user'
 
 Capybara.app = Chitter
 
