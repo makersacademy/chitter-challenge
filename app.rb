95963b33bd9a8dac288915b3b657718ba993a8e7
Adrienne Galloway <aasgalloway@gmail.com>
2022-02-09 15:48:40 +0000

    fixing bug that didn't save the new user email address in the database

--- app.rb
+++ app.rb
@@ -40,7 +40,7 @@ class ChitterApp < Sinatra::Base
   end
   
   post '/users' do
-    user = User.create(name: params[:name], email: params[:email], password: params[:password])
+    user = User.create(email: params[:email], name: params[:name], password: params[:password])
     session[:user_id] = user.id
     redirect '/chitter'
   end
