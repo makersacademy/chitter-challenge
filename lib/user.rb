3aa5b85f49d117df5dfed5960a822e9d8d662843
MattW <mattnwarnock@yahoo.co.uk>
2022-02-11 18:17:44 +0000

    Add user class

---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+class User
+  def self.create
+  end
+end
---
+++ spec/units/user_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'user'
+
+RSpec.describe User do
+  describe '.Create' do
+
+  end
+end
