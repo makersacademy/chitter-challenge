401a4322fc20b298f04ee4826aa75e94235e7699
Ledders26 <benlhall584@hotmail.co.uk>
2022-02-18 13:59:56 +0000

    added test database

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -3,9 +3,9 @@ require 'pg'
 class Peeps
 
   def self.all
+    set_env
     peep_feed = []
-    connection = PG.connect(dbname: 'chitter')
-    all_peeps = connection.exec( "SELECT * FROM peeps;") do |all|
+    all_peeps = @conn.exec( "SELECT * FROM peeps;") do |all|
       all.each do |message|
         peep_feed << message['peep']
       end
@@ -14,7 +14,16 @@ class Peeps
   end
 
   def self.create(peep:)
-    connection = PG.connect(dbname: 'chitter')
-    connection.exec("INSERT INTO peeps(peep) VALUES('#{peep}');")
+    set_env
+    @conn.exec("INSERT INTO peeps(peep) VALUES('#{peep}');")
   end
 end
+
+private
+
+  def set_env
+    if ENV['ENVIRONMENT'] == 'test'
+      @conn = PG.connect( dbname: 'bookmark_manager_test')
+    else 
+      @conn = PG.connect( dbname: 'bookmark_manager')
+  end
