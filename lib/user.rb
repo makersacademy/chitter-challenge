d044d530275446b4b04d6de18e1a1214b583df40
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-02-18 17:35:36 +0000

    quotes now allowed in the username

--- lib/user.rb
+++ lib/user.rb
@@ -2,11 +2,12 @@ require 'bcrypt'
 
 class User
   def self.create(email:, password:, name:, user_name:)
-    return nil if email_or_username_exists?(email, user_name)
+    escaped_user_name = DatabaseConnection.escape_string(user_name)
+    return nil if email_or_username_exists?(email, escaped_user_name)
 
     encrypted_password = BCrypt::Password.create(password)
     result = DatabaseConnection.query(
-      "INSERT INTO users (email, password, name, user_name) VALUES($1, $2, $3, $4) RETURNING user_id, email, name, user_name;",[email, encrypted_password, name, user_name]
+      "INSERT INTO users (email, password, name, user_name) VALUES($1, $2, $3, $4) RETURNING user_id, email, name, user_name;",[email, encrypted_password, name, escaped_user_name]
     )
     result.map do |user|
       User.new(id: user['user_id'], email: user['email'], name: user['name'], user_name: user['user_name'])
