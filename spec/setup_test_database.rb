94703a54a753a4bbda9677a586d4b48a418c949a
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-02-19 14:04:17 +0000

    Tweaked presentation of peeps, updated test data base script to restart identity column

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,5 +4,5 @@ def setup_test_database
   connection = PG.connect(dbname: 'chitter_test')
   # Clear the tables used in the program
   # N.B This will clear peeps due to the foreign key constraint to users
-  connection.exec("TRUNCATE TABLE users CASCADE;")
+  connection.exec("TRUNCATE users RESTART IDENTITY CASCADE;")
 end
