0c956973c42b19a8acb93a85cc44aeb0911b876a
Jack <winterschladen.j@gmail.com>
2022-02-20 14:32:07 +0000

    Completed User Story 2
    As a maker
    So that I can see what others are saying
    I want to see all peeps in reverse chronological order

--- lib/chitter.rb
+++ lib/chitter.rb
@@ -2,18 +2,26 @@ require 'pg'
 
 class Chitter
 
+  attr_reader :id, :content
+
+  def initialize(id:, content:)
+    @id = id
+    @content = content
+  end
+
   def self.post(peep)
     database_connect
     result = @connection.exec_params("INSERT INTO peeps(peep) 
                                            VALUES($1) 
-                                        RETURNING peep", [peep])
-    peep
+                                        RETURNING id, peep", [peep])
+    Chitter.new(id: result[0]['id'], content: result[0]['peep'])
   end
 
   def self.list
     database_connect
     result = @connection.exec("SELECT * FROM peeps")
-    result.map { |peep| peep['peep'] }
+    result.map { |peep| Chitter.new(id: peep['id'], content: peep['peep']) }
+
   end
 
   private
--- spec/chitter_spec.rb
+++ spec/chitter_spec.rb
@@ -1,19 +1,24 @@
 require_relative '../lib/chitter'
+require 'database_helpers'
 
 describe Chitter do
   describe '#post' do
     it 'saves and returns the posted peep' do
       peep = Chitter.post("Hello, chitter!")
-      expect(peep).to eq ("Hello, chitter!")
+      expect(peep.content).to eq ("Hello, chitter!")
     end
   end
 
   describe '#list' do
     it 'lists all the posted peeps' do
-      Chitter.post("First post")
-      Chitter.post("Second post")
-      expect(Chitter.list).to include("First post")
-      expect(Chitter.list).to include("Second post")
+      
+      peep = Chitter.post("Test peep")
+      
+      persisted_data = persisted_data(id: peep.id)
+
+      expect(peep).to be_a Chitter
+      expect(peep.id).to eq persisted_data['id']
+      expect(peep.content).to eq 'Test peep'
     end
   end
 end
