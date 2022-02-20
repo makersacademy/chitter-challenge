65c77410bfdab3318578413eac98fb7146b4213e
Ledders26 <benlhall584@hotmail.co.uk>
2022-02-20 00:19:49 +0000

    db connections to be made at boot

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,4 +1,4 @@
-require 'pg'
+require 'database_connection'
 
 class Peeps
 
@@ -11,26 +11,14 @@ class Peeps
   end
 
   def self.all
-    set_env
-    all_peeps = @conn.exec( "SELECT * FROM peeps ORDER BY id DESC;")
+    all_peeps = DatabaseConnection.query( "SELECT * FROM peeps ORDER BY id DESC;")
     all_peeps.map { |post| Peeps.new(id: post['id'], peep: post['peep'], created_at: post['created_at']) }
   end
 
   def self.create(peep:)
-    set_env
-    result = @conn.exec_params(
+    result = DatabaseConnection.query(
       "INSERT INTO peeps(peep) VALUES($1) RETURNING id, peep, created_at;",[peep]
     )
     Peeps.new(id: result[0]['id',], peep: result[0]['peep'], created_at: result[0]['created_at'])
   end
 end
-
-private
-
-  def set_env
-    if ENV['ENVIRONMENT'] == 'test'
-      @conn = PG.connect( dbname: 'chitter_test')
-    else 
-      @conn = PG.connect( dbname: 'chitter')
-    end
-  end
