6e625af606c63cd5721d8bcf5f6a56511b3079b6
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-02-20 09:43:31 +0000

    Extracted UserController

--- app.rb
+++ app.rb
@@ -7,12 +7,14 @@ require './database_connection_setup'
 require 'sinatra/flash'
 require 'action_view'
 require_relative './controllers/peep_controller'
+require_relative './controllers/user_controller'
 
 include ActionView::Helpers::DateHelper
 
 class Chitter < Sinatra::Base
 
   use PeepController
+  use UserController
 
   configure :development do
     register Sinatra::Reloader
@@ -28,24 +30,6 @@ class Chitter < Sinatra::Base
   get '/reply' do
     @peep = Peep.find_by_id(id: params[:peep_id])
     erb :reply
-  end
-
-  get '/users/new' do
-    erb :"users/new"
-  end
-  
-  post '/users' do
-    user = User.create(
-      email: params['email'], password: params['password'], 
-      name: params['name'], user_name: params['user_name']
-    )
-    if user
-      session[:user_id] = user.id
-      redirect('/peeps')
-    else
-      flash[:notice] = 'Email or Username already in use!'
-      redirect('/users/new')
-    end
   end
 
   get '/sessions/new' do
