34d9a6b4bdabff91fd76ffe29dde442565cc8b88
Francesco <francesco@monzo.com>
2022-02-20 16:44:40 +0100

    added unit test for reverse chronological order of peeps

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,7 +1,8 @@
 require 'peep'
 
 describe Peep do
-  describe '.all' do
+
+  describe '#all' do
     it 'returns all peeps' do
       connection = PG.connect(dbname: 'chitter_test')
       connection.exec("INSERT INTO peeps (message, time) VALUES('Hello', '2022-02-18 19:43:00');")
@@ -11,6 +12,23 @@ describe Peep do
       expect(peeps).to include("Hello")
       expect(peeps).to include("Ciao ragazzi")
       expect(peeps).to include("I love coding weekends")
+    end
+
+    it 'sorts peeps in reverse chronological order' do
+      connection = PG.connect(dbname: 'chitter_test')
+      connection.exec("INSERT INTO peeps (message, time) VALUES('Very old peep', '2022-02-15 08:43:00');")
+      connection.exec("INSERT INTO peeps (message, time) VALUES('Newest peep', '2022-02-17 18:33:00');")
+      connection.exec("INSERT INTO peeps (message, time) VALUES('This peep is between old and new', '2022-02-17 18:32:33');")
+      peeps = Peep.all
+      expect(peeps.first).to eq "Newest peep"
+      expect(peeps.last).to eq "Very old peep"
+    end
+  end
+
+  describe '#new' do
+    it 'creates a new peep' do
+      Peep.new(message: 'This is a newly created peep')
+      expect(Peep.all).to include 'newly created peep'
     end
   end
 end
