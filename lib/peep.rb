e5048c51786d953cd4d118948e2fe45534bc3c13
PhilBligh <philbligh@hotmail.com>
2022-03-16 22:31:42 +0000

    changing display to desc order and formatting time

--- lib/peep.rb
+++ lib/peep.rb
@@ -18,7 +18,7 @@ class Peep
     else
       connection = PG.connect(dbname: 'chitter')
     end
-    result = connection.exec("SELECT * FROM peeps;")
+    result = connection.exec("SELECT * FROM peeps ORDER BY id DESC;")
     result.map do |peep|
       Peep.new(id: peep['id'], peep_text: peep['peep_text'], user_id: peep['user_id'], time: peep['time'])
     end
@@ -32,7 +32,7 @@ class Peep
       connection = PG.connect(dbname: 'chitter')
     end
     result = connection.exec_params(
-      "INSERT INTO peeps (peep_text, user_id, time) VALUES($1, $2, '#{Time.now.strftime("%H:%M")}' ) RETURNING id, peep_text, user_id, time;", [peep_text, user_id])
+      "INSERT INTO peeps (peep_text, user_id, time) VALUES($1, $2, '#{Time.now.strftime("%d-%b - %H:%M")}' ) RETURNING id, peep_text, user_id, time;", [peep_text, user_id])
     Peep.new(id: result[0]['id'], peep_text: result[0]['peep_text'], user_id: result[0]['user_id'], time: result[0]['time'])
   end
 
@@ -42,7 +42,7 @@ class Peep
     else
       connection = PG.connect(dbname: 'chitter')
     end
-    result = connection.exec("SELECT * FROM peeps WHERE user_id = '#{user_id}';")
+    result = connection.exec("SELECT * FROM peeps WHERE user_id = '#{user_id}' ORDER BY id DESC;")
     result.map do |peep|
       Peep.new(id: peep['id'], peep_text: peep['peep_text'], user_id: peep['user_id'], time: peep['time'])
     end
