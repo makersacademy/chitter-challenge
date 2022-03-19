f91500e5451c3b1de15276773eb8faa6904ea93a
Samuel Raducan <samuel.thembp@gmail.com>
2022-03-19 14:28:34 +0000

    Create db and add migrations folder

--- spec/feature/peeps_spec.rb
+++ spec/feature/peeps_spec.rb
@@ -1,7 +1,7 @@
 feature 'User can see all the peeps' do
   scenario 'can see peep' do
     visit '/peeps'
-    expect(page).to have_content 'Peeps!'
     expect(page).to have_button 'Add Peep'
+    expect(page).to have_content 'Peeps!'
   end
 end 
