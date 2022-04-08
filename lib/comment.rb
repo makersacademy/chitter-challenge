5960b7bcd41f68d22aaba688d42276c69c545765
clovellbsc <clovellbsc@gmail.com>
2022-04-08 12:45:38 +0100

    refactored comment.rb to remove unnecessary active rocord connection code

--- lib/comment.rb
+++ lib/comment.rb
@@ -1,23 +1,5 @@
 require 'active_record'
 
-if ENV['ENVIRONMENT'] == 'test'
-  ActiveRecord::Base.establish_connection(
-    adapter: 'postgresql',
-    host: 'localhost',
-    username: ENV["USERNAME"],
-    password: ENV["SQL_PASSWORD"],
-    database: 'chitter_test'
-  )
-else 
-  ActiveRecord::Base.establish_connection(
-    adapter: 'postgresql',
-    host: 'localhost',
-    username: ENV["USERNAME"],
-    password: ENV["SQL_PASSWORD"],
-    database: 'chitter'
-  )
-end
-
 class Comment < ActiveRecord::Base
   belongs_to :post
 
