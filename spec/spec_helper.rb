e00c4240046c719a4b9b9a11f471706dc4f89092
monenavarro <me.navarro.97@outlook.com>
2022-04-17 13:09:44 +0100

    Initial commit setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,16 @@
 require 'simplecov'
 require 'simplecov-console'
 
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Chitter
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
