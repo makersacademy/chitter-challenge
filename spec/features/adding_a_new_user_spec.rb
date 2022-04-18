8b5526b29e39769498a77ad64f3ea18171077fc3
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-18 18:55:13 +0100

    test fixed

--- spec/features/adding_a_new_user_spec.rb
+++ spec/features/adding_a_new_user_spec.rb
@@ -3,7 +3,7 @@ feature 'Adding user' do
     visit('/users/new')
     fill_in('email', with: 'test@example.com')
     fill_in('password', with: 'password123')
-    click_button('Submit')
+    click_button('Register')
     expect(page).to have_content 'Welcome, test@example.com'
   end
 end
