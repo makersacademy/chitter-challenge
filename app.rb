e3ad03e36f5d226bd1ad6a5d42ee70eba474fe55
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-18 11:31:43 +0100

    Disengaged ActiveRecord as I couldn't get it to work

--- app.rb
+++ app.rb
@@ -1,12 +1,14 @@
-
+require 'sinatra'
 require 'sinatra/base'
 require 'sinatra/reloader'
-require 'sinatra/activerecord'
+require 'pg'
+#require 'sinatra/activerecord'
 
-set :database, { adapter: "sqlite3", database: "foo.sqlit3" }
+#set :database_file, '/config/database.yml'
 
 class Chitter < Sinatra::Base
-  registeer Sinatra::ActiveRecordExtension
+  #register Sinatra::ActiveRecordExtension
+
   configure :development do
     register Sinatra::Reloader
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'simplecov-console'
 
 ENV['RACK_ENV'] = 'test'
 
-require File.join(File.dirname(__FILE), '..', 'app.rb')
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
