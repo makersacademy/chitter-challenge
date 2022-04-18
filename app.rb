c7f93a38cabecef18693f7808d62b91403231029
almorcrette <26277447+almorcrette@users.noreply.github.com>
2022-04-18 16:39:57 +0100

    Registration, with password made secure

--- app.rb
+++ app.rb
@@ -6,12 +6,15 @@ require 'pg'
 require './database_connection_setup'
 
 require_relative './lib/peep'
+require_relative './lib/user'
 # require 'sinatra/activerecord'
 
 # set :database_file, '/config/database.yml'
 
 class Chitter < Sinatra::Base
   # register Sinatra::ActiveRecordExtension
+
+  enable :sessions
 
   configure :development do
     register Sinatra::Reloader
@@ -22,6 +25,7 @@ class Chitter < Sinatra::Base
   end
 
   get '/peeps' do
+    @user = User.find(session[:user_id])
     @peeps = Peep.all
     @peeps_sorted = @peeps.sort_by { |peep| peep.time_created }.reverse
     erb :"/peeps/index"
@@ -34,6 +38,21 @@ class Chitter < Sinatra::Base
 
   get '/peeps/new' do
     erb(:"/peeps/new")
+  end
+
+  get '/users/new' do
+    erb(:"/users/new")
+  end
+
+  post '/users' do
+    user = User.create(
+      name: params[:name],
+      username: params[:username],
+      email: params[:email],
+      password: params[:password]
+    )
+    session[:user_id] = user.id
+    redirect '/peeps'
   end
 
   run! if app_file == $0
