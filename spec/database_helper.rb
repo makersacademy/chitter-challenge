7b48d37ef0af290e9fd934ecf7ffa4923ee745cc
Baljit Rakhra <baljit.rakhra@gmail.com>
2022-04-18 16:55:06 +0100

    we can add a peep, view a peep, see the time of the peep

---
+++ spec/database_helper.rb
@@ -0,0 +1,11 @@
+require 'pg'
+
+def truncate_database 
+  connection = PG.connect(dbname: 'chitter_test')
+  connection.exec("TRUNCATE TABLE peeps;")
+end
+
+def persisted_data(table:, id:)
+  connection = PG.connect(dbname: 'chitter_test')
+  connection.query("SELECT * FROM #{table} WHERE id = '#{id}';")
+end
--- spec/unit/chitter_spec.rb
+++ spec/unit/chitter_spec.rb
@@ -7,13 +7,14 @@ describe Chitter do
     DatabaseConnection.query("INSERT INTO peeps (peep, time) VALUES($1, $2);", ['how are you?', current_timestamp ])
     DatabaseConnection.query("INSERT INTO peeps (peep, time) VALUES($1, $2);", ['Let us discuss', current_timestamp ])
     peeps = Chitter.all
-    expect(peeps[0]).to include "how are you?" 
-    expect(peeps[1]).to eq "Let us discuss"
+    expect(peeps.first.peep).to include "how are you?" 
+    # expect(peeps[1]).to eq "Let us discuss"
   end
   context 'adding a new peep' do
     it 'lets a user to post a new peep on chitter' do
-      peep = Chitter.create(peep: 'Hello')
-      expect(Chitter.all).to include 'Hello'
+      Chitter.create(peep: 'Hello')
+      peep = Chitter.all
+      expect(peep.first.peep).to include 'Hello'
     end
   end 
 
