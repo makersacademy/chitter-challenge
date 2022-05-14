ebe19acd57a510abf6100ad1be0db076e2a397fa
George Dainton <gcdainton94@gmail.com>
2022-05-14 21:09:46 +0100

    Created chitter database and test database, recorded setup instructions

---
+++ db/migrations/01_create_peeps_table.sql
@@ -0,0 +1,23 @@
+1.
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated_at = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+2.
+
+CREATE TABLE peeps (
+  id SERIAL NOT NULL PRIMARY KEY,
+  peep TEXT,
+  posted TIMESTAMPTZ NOT NULL DEFAULT NOW()
+  );
+
+3. CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON peeps
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
