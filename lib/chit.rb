18c6145edbe6b6c5aa263aa8cc390e51f4b132b9
StevenSpiegl <steven.spiegl@gmail.com>
2022-05-15 22:50:43 +0100

    here is another example of it working in irb but not in rackup

--- lib/chit.rb
+++ lib/chit.rb
@@ -4,10 +4,12 @@ class Chit
 
 attr_reader :content, :handle, :timestamp
 
-  def initialize(content:, handle:)
+TIMESTAMP = Time.new.strftime "%H:%M:%S %d-%m-%Y"
+
+  def initialize(content:, handle:, timestamp:)
     @content = content
     @handle = handle
-    @timestamp = Time.new.strftime "%H:%M:%S %d-%m-%Y"
+    @timestamp = timestamp
   end
 
 
@@ -30,12 +32,12 @@ attr_reader :content, :handle, :timestamp
     else
       connection = PG.connect(dbname: 'chitter')
     end
-      result = connection.exec_params("INSERT INTO chits (handle, content, timestamp) VALUES ($1, $2, '#{@timestamp}') RETURNING handle, content;", [handle, content])
+      result = connection.exec_params("INSERT INTO chits (handle, content, timestamp) VALUES ($1, $2, '#{TIMESTAMP}') RETURNING handle, content, timestamp;", [handle, content])
       #is the above safe? it uses interpolation but timestamp isn't accessible to the web user...
       #I do not understand why this isn't sending timestamp to the timestamp column . Instead, when I calls timestamp, it seems to initialize time
       # then and there, so it's not saving the time a post was made
       #I've tried generating the timestamp in the table, as well as generating it here and inserting it into the table, but whenever I've got either
       #of those to work it's broken rackup, even though it works in irb
-      Chit.new(content: result[0]['content'], handle: result[0]['handle'])
+      Chit.new(content: result[0]['content'], handle: result[0]['handle'], timestamp: result[0]['timestamp'])
   end
 end
