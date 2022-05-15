3c1e177108793fbf4ccb4a45046a2601ba00bcc8
Anish <anishkakaiya@outlook.com>
2022-05-15 23:19:32 +0100

    add init method to wrap database data

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,6 +1,16 @@
 require 'pg'
 
 class Peeps
+
+  attr_reader :id, :first_name, :handle, :peep
+
+  def initialize(id, first_name, handle, peep)
+    @id = id
+    @first_name = first_name
+    @handle = handle
+    @peep = peep
+  end
+
   def self.all
     if ENV['ENVIRONMENT'] == 'test'
       connection = PG.connect(dbname: 'chitter_test')
@@ -8,8 +18,10 @@ class Peeps
       connection = PG.connect(dbname: 'chitter')
     end
 
-    result = connection.exec("SELECT handle, first_name, peep FROM peeps;")
-    result.map { |peep| "#{peep['first_name']} (#{peep['handle']}): #{peep['peep']}"}
+    result = connection.exec("SELECT * FROM peeps;")
+    result.map { |peep| 
+      Peeps.new(peep['id'], peep['first_name'], peep['handle'], peep['peep'])
+    }
   end
 
   def self.new_peep(first_name, handle, peep)
@@ -18,6 +30,7 @@ class Peeps
     else
       connection = PG.connect(dbname: 'chitter')
     end
-    connection.exec("INSERT INTO peeps (handle, first_name, peep) VALUES ('#{handle}','#{first_name}', '#{peep}');")
+    result = connection.exec("INSERT INTO peeps (first_name, handle, peep) VALUES ('#{first_name}', '#{handle}', '#{peep}') RETURNING id, handle, first_name, peep;")
+    Peeps.new(result[0]['id'], result[0]['first_name'], result[0]['handle'], result[0]['peep'])
   end
 end
