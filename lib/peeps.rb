3d6fb58970b329ba7cda720301df67f300d70298
Anish <anishkakaiya@outlook.com>
2022-05-15 20:50:42 +0100

    add new_peep method

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -11,4 +11,13 @@ class Peeps
     result = connection.exec("SELECT handle, first_name, peep FROM peeps;")
     result.map { |peep| "#{peep['first_name']} (#{peep['handle']}): #{peep['peep']}"}
   end
+
+  def self.new_peep(first_name, handle, peep)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'chitter_test')
+    else
+      connection = PG.connect(dbname: 'chitter')
+    end
+    connection.exec("INSERT INTO peeps (handle, first_name, peep) VALUES ('#{handle}','#{first_name}', '#{peep}');")
+  end
 end
