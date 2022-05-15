03554d2a545d0d8d8746ec9e11b3e41b439bda95
Anish <anishkakaiya@outlook.com>
2022-05-15 20:48:49 +0100

    add new_peep test

--- spec/unit/peeps_spec.rb
+++ spec/unit/peeps_spec.rb
@@ -1,12 +1,28 @@
 require 'peeps.rb'
+require 'pg'
+
 
 describe 'messages' do
   describe '.all' do
-    it 'returns list of peeps' do
-      peeps = Peeps.all
+    
+  connection = PG.connect(dbname: 'chitter_test')
 
+    connection.exec("INSERT INTO peeps (handle, first_name, peep) VALUES ('@KLamar', 'Kendrick', 'New album coming soon :D');")
+    connection.exec("INSERT INTO peeps (handle, first_name, peep) VALUES ('@Ronaldo', 'Cristiano', 'Messi is better tbh.');")
+
+    peeps = Peeps.all
+
+    it 'returns list of peeps' do
       expect(peeps).to include("Kendrick (@KLamar): New album coming soon :D")
-      expect(peeps).to include("Cristiano Ronaldo (@Ronaldo): Messi is better tbh.")
+      expect(peeps).to include("Cristiano (@Ronaldo): Messi is better tbh.")
+    end
+  end
+  
+  describe '.new_peep' do
+    it 'adds a peep to the website via the database' do
+
+      Peeps.new_peep("Leo", "@Messi10", "I miss Barcelona :(")
+      expect(Peeps.all).to include("Leo (@Messi10): I miss Barcelona :(")
     end
   end
 end
