24d91764152cbf9b53a0bb5d5768ce7e1355b986
Luke Storey <lukemstorey@gmail.com>
2022-05-15 22:25:39 +0100

    connection

--- database_connection_setup.rb
+++ database_connection_setup.rb
@@ -2,6 +2,8 @@ require './lib/database_connection'
 
 if ENV['RACK_ENV'] == 'test'
   DatabaseConnection.setup('chitter_test')
+elsif ENV['RACK_ENV'] == 'development'
+  DatabaseConnection.setup('chitter')
 else
-  DatabaseConnection.setup('postgres://xxazaatnwnezpi:6bcf17beedd1646d930ce66b0b312f889173e15aafa1d71af749fafc71ff27fc@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/d8vg8rk0k05gnf')
+  DatabaseConnection.setup("postgres://xxazaatnwnezpi:6bcf17beedd1646d930ce66b0b312f889173e15aafa1d71af749fafc71ff27fc@ec2-63-35-156-160.eu-west-1.compute.amazonaws.com:5432/d8vg8rk0k05gnf")
 end
