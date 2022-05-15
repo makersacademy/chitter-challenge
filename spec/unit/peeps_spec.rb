a752d3af9637daf4a64bb7fa7afcb786a507551e
Anish <anishkakaiya@outlook.com>
2022-05-15 23:21:26 +0100

    update test for wrapping database data

--- spec/unit/peeps_spec.rb
+++ spec/unit/peeps_spec.rb
@@ -7,22 +7,34 @@ describe 'messages' do
     
   connection = PG.connect(dbname: 'chitter_test')
 
-    Peeps.new_peep('Kendrick', '@KLamar', 'New album coming soon :D')
-    Peeps.new_peep('Cristiano', '@Ronaldo', 'Messi is better tbh.')
+    it 'returns a list of peeps' do
 
-    peeps = Peeps.all
-
-    it 'returns list of peeps' do
-      expect(peeps).to include("Kendrick (@KLamar): New album coming soon :D")
-      expect(peeps).to include("Cristiano (@Ronaldo): Messi is better tbh.")
+      peep = Peeps.new_peep('Dave', '@Dave', 'Hi Im Dave')
+      Peeps.new_peep('Kendrick', '@KLamar', 'New album coming soon :D')
+      Peeps.new_peep('Cristiano', '@Ronaldo', 'Good morning everyone, SUIIIII')
+  
+      peeps = Peeps.all
+      
+      expect(peeps.length).to eq 3
+      expect(peeps.first).to be_a Peeps
+      expect(peeps.first.id).to eq peep.id
+      expect(peeps.first.first_name).to eq 'Dave'
+      expect(peeps.first.handle).to eq '@Dave'
+      expect(peeps.first.peep).to eq 'Hi Im Dave'
     end
   end
   
   describe '.new_peep' do
     it 'adds a peep to the website via the database' do
 
-      Peeps.new_peep("Leo", "@Messi10", "I miss Barcelona :(")
-      expect(Peeps.all).to include("Leo (@Messi10): I miss Barcelona :(")
+      peep = Peeps.new_peep("Leo", "@Messi10", "I miss Barcelona :(")
+      persisted_data = PG.connect(dbname: 'chitter_test').query("SELECT * FROM peeps WHERE id = #{peep.id};")
+
+      expect(peep).to be_a Peeps
+      expect(peep.id).to eq persisted_data.first['id']
+      expect(peep.first_name).to eq 'Leo'
+      expect(peep.handle).to eq '@Messi10'
+      expect(peep.peep).to eq 'I miss Barcelona :('
     end
   end
 end
