400de3f338dcd23a51e7eb8541b3e534f41f9edc
Anish <anishkakaiya@outlook.com>
2022-05-15 20:50:08 +0100

    test for new peep

--- spec/feature/make_peep_spec.rb
+++ spec/feature/make_peep_spec.rb
@@ -2,10 +2,10 @@ feature "Peep Creation & Viewing" do
   scenario "Make a peep (without a login)" do
     visit('/')
     fill_in :handle, with: "@user_anish"
-    fill_in :name, with: "Anish"
+    fill_in :first_name, with: "Anish"
     fill_in :peep, with: "First peep on Chitter! :D"
-    click_button 'Submit Peep'
-    expect(page).to have_content "Anish, (@user_anish): First peep on Chitter! :D"
+    click_button 'Peep'
+    expect(page).to have_content "Anish (@user_anish): First peep on Chitter! :D"
   end
 end
 
