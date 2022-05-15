6b9edaff9961da8dbf74b77a92d0324020a19d71
Anish <anishkakaiya@outlook.com>
2022-05-15 20:50:57 +0100

    add post_peep route

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/peeps'
 
 class Chitter < Sinatra::Base
   configure :development do
@@ -11,7 +12,14 @@ class Chitter < Sinatra::Base
   end
 
   get '/home' do
-    'Chitter Home.'
+    @peeps = Peeps.all
+    erb :home
+  end
+
+  post '/post_peep' do
+
+    Peeps.new_peep(params['first_name'], params['handle'], params['peep'])
+    redirect '/home'
   end
 
   run! if app_file == $0
