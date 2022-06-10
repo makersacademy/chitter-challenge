7cbfc8489ccf176b3f4ebbc6ed534db6275c3f8d
Kay <kard@hotmail.co.uk>
2022-06-10 21:22:13 +0100

    set up web dev environment with hello world page

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Chitter < Sinatra::Base
+  configure :develpment do
+    register Sinatra::reloader
+  end
+
+  get '/' do
+    'Hello world'
+  end
+
+  run if app_file == $0
+end
