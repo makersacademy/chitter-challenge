5258bd163180a500e1f7363cf0eb7f6a7ca56233
Kay <kard@hotmail.co.uk>
2022-06-10 22:26:37 +0100

    .read shows single tweet

---
+++ lib/peep.rb
@@ -0,0 +1,8 @@
+class Peep
+  def initialize(post)
+  end
+
+  def read
+    'my wife just left me'
+  end
+end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,1 +1,8 @@
 require 'peep'
+
+RSpec.describe Peep do
+  it 'constructs' do
+    peep = Peep.new('my wife just left me')
+    expect(peep.read).to eq 'my wife just left me'
+  end
+end
