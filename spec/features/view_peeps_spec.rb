b26d76477aace37177aa4757bd61104ca584600b
Kay <kard@hotmail.co.uk>
2022-06-11 19:25:26 +0100

    shows multiple peeps in a list

--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -3,4 +3,11 @@ feature 'homepage' do
     visit '/chitter'
     expect(page).to have_content 'Chitter'
   end
+
+  scenario 'shows multiple peeps not in an array' do
+    add_two_peeps
+    expect(page).to have_content "What's peeping with YOU!?"
+    expect(page).to have_content "I'm loving to peep"
+    expect(page).not_to have_content ["What's peeping with YOU!?", "I'm loving to peep"]
+  end
 end
--- spec/test_helpers.rb
+++ spec/test_helpers.rb
@@ -2,3 +2,13 @@ def truncate_table
   controller = PG.connect :dbname => 'chitter_test'
   controller.exec ("TRUNCATE TABLE peeps")
 end
+
+def add_two_peeps
+  visit '/chitter'
+      click_on 'Compose a peep'
+      fill_in 'peep', with: "What's peeping with YOU!?"
+      click_on 'Share'
+      click_on 'Compose a peep'
+      fill_in 'peep', with: "I'm loving to peep"
+      click_on 'Share'
+end
