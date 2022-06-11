89b70a3e71877248840696c8e2df713d31199e76
Kay <kard@hotmail.co.uk>
2022-06-11 19:52:33 +0100

    shows a second hardcoded username

--- spec/features/join_chitter_spec.rb
+++ spec/features/join_chitter_spec.rb
@@ -10,4 +10,16 @@ feature 'form to sign up to chitter' do
     expect(page).to have_content 'Welcome to Chitter, penelopeheartsyou'
     expect(page).to have_link 'Compose a peep'
   end
+
+  scenario 'can sign up and receive personalised confirmation' do
+    visit '/chitter'
+    click_on 'Join Chitter'
+    fill_in 'Name', with: 'John Heart'
+    fill_in 'Email', with: 'john<3@makers.com'
+    fill_in 'username', with: 'johnjohn!'
+    fill_in 'password', with: 'SweetJohn321'
+    click_on 'Submit'
+    expect(page).to have_content 'Welcome to Chitter, johnjohn!'
+    expect(page).to have_link 'Compose a peep'
+  end
 end
