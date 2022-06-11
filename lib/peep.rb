cb819ae6d3fa1d5e37d67add5633feda7fe9fc2b
Kay <kard@hotmail.co.uk>
2022-06-11 12:47:54 +0100

    hard-coded one peep that is passed through the database and returning. 2 failing tests

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,6 +1,17 @@
+require 'pg'
+
 class Peep
   attr_accessor :post
   def initialize(post)
-    @post = post
+    @controller = PG.connect :dbname => 'chitter'
+    @controller.exec ("TRUNCATE TABLE peeps")
+    @controller.exec (
+      "INSERT INTO peeps (post) VALUES ('my wife just left me') returning post;"
+      )
+  end
+
+  def post
+    result = @controller.exec ("SELECT post FROM peeps")
+    result.map {|rows| rows['post']}
   end
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,11 +3,11 @@ require 'peep'
 RSpec.describe Peep do
   it 'constructs' do
     peep = Peep.new('my wife just left me')
-    expect(peep.post).to eq 'my wife just left me'
+    expect(peep.post).to eq ['my wife just left me']
   end
 
   it 'shows a different peep' do
     peep = Peep.new('my wife came back!')
-    expect(peep.post).to eq 'my wife came back!'
+    expect(peep.post).to eq ['my wife came back!']
   end
 end
