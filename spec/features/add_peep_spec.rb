fa6d565baf54accd7220f591214e5e26d1190cbe
Kay <kard@hotmail.co.uk>
2022-06-11 13:06:47 +0100

    passed feature to enter and view a peep without an apostrophe

--- spec/features/add_peep_spec.rb
+++ spec/features/add_peep_spec.rb
@@ -2,8 +2,8 @@ feature 'add a peep' do
   scenario 'adds a peep and then shows it on the homepage' do
     visit '/chitter'
     click_on 'Compose a peep'
-    fill_in 'peep', with: "I never thought I'd fall in love with a utensil."
+    fill_in 'peep', with: "I never thought I would fall in love with a utensil."
     click_on 'Share'
-    expect(page).to have_content "I never thought I'd fall in love with a utensil."
+    expect(page).to have_content "I never thought I would fall in love with a utensil."
   end
 end
