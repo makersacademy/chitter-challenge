5c65ff20daa0371abe58b9a9f94494707c7b96f2
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-12 14:11:29 +0100

    Add sign up feature

--- app.rb
+++ app.rb
@@ -1,20 +1,42 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require './lib/peeps'
+require './lib/account'
 require 'pg'
+require 'sinatra/flash'
 
 class Chitter < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     'Hello World'
   end
 
-  get '/chitter' do 
-    p ENV
+  get '/chitter' do # LIST data
     @peeps = Peeps.all
+    erb :index
+  end 
+
+  get '/chitter/signup' do 
+    erb :sign_up
+  end 
+
+  post '/chitter' do # CREATE data
+    p params 
+    Account.create(name: params[:name], username: params[:username], email: params[:email], password: params[:password])
+    # @username = params[:username]
+    # p @username
+    session[:success_message] = "You've successfully signed up to chitter!"
+    redirect '/chitter/member'
+  end 
+
+  get '/chitter/member' do # home page for someone who has just signed up
+    @peeps = Peeps.all
+    @success_message = session[:success_message]
     erb :index
   end 
 
