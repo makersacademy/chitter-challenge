e1c9ef727b702056dbf421096ee636d25e6028c1
Kay <kard@hotmail.co.uk>
2022-06-12 17:47:44 +0100

    when user has not signed up name is nil and username is an empty string

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -1,16 +1,16 @@
 require 'peep'
 
 RSpec.describe Peep do
-  xit 'constructs' do
+  it 'constructs' do
     time = double :time, now: Time.new(2022, 0o6, 12, 15, 49, 0o2)
     peep = Peep.add('my wife just left me', time)
-    expect(Peep.all).to eq [['my wife just left me', '15:49:00']]
+    expect(Peep.all).to eq [[nil, '', 'my wife just left me', '15:49:00']]
   end
 
-  xit 'shows a different peep' do
+  it 'shows a different peep' do
     time = double :time, now: Time.new(2022, 0o6, 12, 17, 30, 0o2)
     peep = Peep.add('my wife came back!', time)
-    expect(Peep.all).to eq [['', '', 'my wife came back!', '17:30:00']]
+    expect(Peep.all).to eq [[nil, '', 'my wife came back!', '17:30:00']]
   end
 
   describe ".double_apostrophe" do
@@ -24,20 +24,20 @@ RSpec.describe Peep do
   end
 
   describe 'self.all' do
-    xit 'shows a peep that has been created' do
+    it 'shows a peep that has been created' do
       time = double :time, now: Time.new(2022, 0o6, 12, 17, 30, 0o2)
       Peep.add("What's your favourite colour?", time)
-      expect(Peep.all).to eq [["What's your favourite colour?", "17:30:00"]]
+      expect(Peep.all).to eq [[nil, "", "What's your favourite colour?", "17:30:00"]]
     end
 
-    xit 'shows mutliple peeps that have been created, in reverse chronological order' do
+    it 'shows mutliple peeps that have been created, in reverse chronological order' do
       time = double :time, now: Time.new(2022, 0o6, 12, 17, 30, 0o2)
       second_time = double :time, now: Time.new(2022, 0o6, 12, 17, 45, 0o2)
       Peep.add("What's your favourite colour?", time)
       p Peep.all
       Peep.add("Loving the sunshine!!!", second_time)
       p Peep.all
-      expect(Peep.all).to eq [["", "", "Loving the sunshine!!!", "17:45:00"], ["What's your favourite colour?", "17:30:00"]]
+      expect(Peep.all).to eq [[nil, "", "Loving the sunshine!!!", "17:45:00"], [nil, "", "What's your favourite colour?", "17:30:00"]]
     end
 
     context 'user has signed up' do
