0db057c81a8f14f32ae654e7a4ba32622efc304b
fridayshoes <one_amp@hotmail.com>
2022-06-12 20:15:39 +0100

    chitter app now get peeps from database

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,16 +1,15 @@
-# require 'pg'
+require 'pg'
 
 class Peeps
   def self.all
-    [
-      "hello everyone",
-      "Still got this bloomin lurgy and high temperature.",
-      "Why is he sniffing so much?",
-      "I am in the curry house",
-    ]
+    # [
+    #   "hello everyone",
+    #   "Still got this bloomin lurgy and high temperature.",
+    #   "Why is he sniffing so much?",
+    #   "I am in the curry house",
+    # ]
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec("SELECT * FROM peeps;")
+    result.map { |message| message['peep'] }
   end
-#     connection = PG.connect(dbname: 'chitter')
-#     connection.exec("SELECT * FROM peeps;")
-#     result.map { |message| message['peep'] }
-#   end
 end
