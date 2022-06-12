e43b3e566fc20665a89571950b5bdb61ebbc3ac1
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-12 19:38:12 +0100

    Add unit test for .create method

--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -1,19 +1,37 @@
 require 'peeps'
 require 'pg'
 
-describe '.all' do 
-  it 'returns a list of peeps' do 
-    conn = PG.connect(dbname: 'chitter_test')
+describe Peeps do 
+  
+  describe '.all' do 
+    it 'returns a list of peeps' do 
+      conn = PG.connect(dbname: 'chitter_test')
 
-    # Add test data
-    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 1');")
-    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 2');")
-    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 3');")
+      # Add test data
+      peep = Peeps.create(message: "Test Example peep 1")
+      Peeps.create(message: "Test Example peep 2")
+      Peeps.create(message: "Test Example peep 3")
+  
+      peeps = Peeps.all
 
-    peeps = Peeps.all
-
-    expect(peeps).to include("Test Example peep 1")
-    expect(peeps).to include("Test Example peep 2")
-    expect(peeps).to include("Test Example peep 3")
+      expect(peeps[0].peep_id).to eq peep.peep_id
+      expect(peeps[0]).to be_a Peeps
+      expect(peeps[0].message).to eq "Test Example peep 1"
+      expect(peeps[1].message).to eq "Test Example peep 2"
+      expect(peeps[2].message).to eq "Test Example peep 3"
+    end 
   end 
-end 
+
+  describe '.create' do 
+    it "creates a new peep" do 
+      peep = Peeps.create(message: "I love chitter more than twitter!")
+      persisted_data = PG.connect(dbname: 'chitter_test').query("SELECT * FROM peeps WHERE peep_id = #{peep.peep_id};")
+
+      p persisted_data
+
+      expect(peep).to be_a Peeps
+      expect(peep.peep_id).to eq persisted_data.first['peep_id']
+      expect(peep.message).to eq "I love chitter more than twitter!"
+    end 
+  end 
+end
