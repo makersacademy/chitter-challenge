07de70254c19e32edc17502c25890cd3e3d0d040
Sophie Gilder <sophieachannon@hotmail.co.uk>
2022-06-12 22:15:28 +0100

    sign out working

--- spec/feature/authentication_spec.rb
+++ spec/feature/authentication_spec.rb
@@ -37,4 +37,18 @@ feature 'authentication' do
     expect(page).to have_content 'Please check your email or password.'
   end
 
+  scenario 'user can sign out' do
+    User.create(first_name: "Sophie", last_name: "Gilder", email: "test@gmail.com", handle: "sophieg", password: "password123")
+
+    visit 'sessions/new'
+    fill_in(:email, with: "test@gmail.com")
+    fill_in(:password, with: "password123")
+    click_button('Sign in')
+    expect(page).to have_content 'Welcome, Sophie!'
+
+    click_button('Sign out')
+    expect(page).not_to have_content 'Welcome, Sophie!'
+    expect(page).to have_content 'You have signed out.'
+  end
+
 end
