bd571c5b69e6c17d28c3561be61f45eab4e445cd
benjamin <b.james.neustadt@gmail.com>
2022-06-12 23:51:58 +0200

    Created two databases
    created two databases, one test, and one for production. The test
    database is empty, the production one has the three entries. The columns
    in the databases are: id, content, created-at (generated automatically
    using a function created with psql; CF. db/migrations/01), and user_id
    (name).

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,12 +1,21 @@
+require 'pg'
+
 class Peep
 
   def self.all
-    @peeps = [
-      "This is my first peep",
-      "This is my second peep",
-      "This is my third peep"
-    ]
-    @peeps.reverse
+
+    connection = PG.connect(dbname: 'chitter_chatter')
+    result = connection.exec("SELECT * FROM peeps;")
+    peeps = result.map { |peep| peep['content'] }
+    peeps.reverse
   end
 
 end
+
+
+ # @peeps = [
+    #   "This is my first peep",
+    #   "This is my second peep",
+    #   "This is my third peep"
+    # ]
+    # @peeps.reverse
