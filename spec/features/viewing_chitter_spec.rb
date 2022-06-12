d6df8cfe6c753e23e26e3689acedd4fb6d97e36b
benjamin <b.james.neustadt@gmail.com>
2022-06-12 21:48:57 +0200

    USER STORY
    Changed user story 2 to user story 1, and broken down this user story
    into two, in order to first show list of peeps, and then work on list f
    peeps in reverse order.

--- spec/features/viewing_chitter_spec.rb
+++ spec/features/viewing_chitter_spec.rb
@@ -1,6 +1,17 @@
-feature 'Viewing bookmarks' do
+feature 'Viewing peeps' do
   scenario 'visiting the index page' do
     visit('/')
     expect(page).to have_content "Chitter Chatter"
   end
+
+  scenario 'A user can see peeps' do
+    visit('/peeps')
+    expect(page).to have_content "This is my first peep"
+    expect(page).to have_content "This is my second peep"
+    expect(page).to have_content "This is my third peep"
+  end
 end
+
+
+
+
