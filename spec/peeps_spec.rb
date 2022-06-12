f6c14fea282897e82cbf3b0be9639c2ea1130bc3
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-12 02:19:00 +0100

    add unit test for view all peeps feature

--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -1,10 +1,19 @@
 require 'peeps'
+require 'pg'
 
 describe '.all' do 
   it 'returns a list of peeps' do 
+    conn = PG.connect(dbname: 'chitter_test')
+
+    # Add test data
+    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 1');")
+    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 2');")
+    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 3');")
+
     peeps = Peeps.all
 
-    expect(peeps).to include "This is my first peep on Chitter!"
-    expect(peeps).to include "I am an example."
+    expect(peeps).to include("Test Example peep 1")
+    expect(peeps).to include("Test Example peep 2")
+    expect(peeps).to include("Test Example peep 3")
   end 
 end 
