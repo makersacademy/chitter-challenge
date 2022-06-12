0595d653551a4b245d268ae08d7dccaa5001ccef
Hafiz <hafizalimi77@gmail.com>
2022-06-12 20:31:42 +0100

    Chit to now using PG

--- lib/chit.rb
+++ lib/chit.rb
@@ -1,8 +1,8 @@
+require 'pg'
 class Chit
   def self.all
-    [
-      "Hey my first chit",
-      "Look pops, I'm chitting"
-    ]
+    connection = PG.connect(dbname: 'chitter')
+    result = connection.exec("SELECT * FROM chit;")
+    result.map { |chit| chit['text']}
   end
 end
--- spec/chit_spec.rb
+++ spec/chit_spec.rb
@@ -4,8 +4,9 @@ describe Chit do
   describe '.all' do
     it 'should show all posted chits' do
       chits = Chit.all
-      expect(chits).to include("Hey my first chit")
-      expect(chits).to include("Look pops, I'm chitting")
+      expect(chits).to include "Hey my first chit"
+      expect(chits).to include "Look pops, I am chitting"
+      expect(chits).to include "Wassup!"
     end
   end
 end
