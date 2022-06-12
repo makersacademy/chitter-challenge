809da3982e1cc037980344c8278c3ec3acade756
Kay <kard@hotmail.co.uk>
2022-06-12 16:59:03 +0100

    user class tested for different usernames

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe User do
     expect(User.username).to eq 'penelopeheartsyou'
   end
 
+  it 'contructs and returns a different username' do
+    User.create('John Heart', 'penel<3@makers.com', 'john333', 'SweetHeart321')
+    expect(User.username).to eq 'john333'
+  end
+
   it 'returns a name' do
     User.create('Penelope Heart', 'penel<3@makers.com', 'penelopeheartsyou', 'SweetHeart321')
     expect(User.name).to eq 'Penelope Heart'
