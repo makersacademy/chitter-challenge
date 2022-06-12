8f3e172981dd9d210b4fe45c1ccbe8bb7642822d
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-06-12 19:34:19 +0100

    change file name

--- spec/features/viewing_peeps_spec.rb
+++ spec/features/viewing_peeps_spec.rb
@@ -2,12 +2,12 @@ require 'pg'
 
 feature "View all peeps" do 
   scenario "A user can see all the peeps" do 
-    conn = PG.connect(dbname: 'chitter_test')
 
-    # Add test data
-    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 1');")
-    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 2');")
-    conn.exec("INSERT INTO peeps (message) VALUES ('Test Example peep 3');")
+    # Add test data to peeps table
+    Peeps.create(message: "Test Example peep 1")
+    Peeps.create(message: "Test Example peep 2")
+    Peeps.create(message: "Test Example peep 3")
+  
 
     visit('/chitter')
 
