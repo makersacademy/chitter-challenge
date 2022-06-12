5e29ced825eb4c51d8255f2f2b9633a3d2902f7a
Robi <robi.maliqi@hotmail.co.uk>
2022-06-12 20:51:53 +0100

    added a second test where we have 3 peeps on the page for a user to view

--- app.rb
+++ app.rb
@@ -10,7 +10,14 @@ class Chitter < Sinatra::Base
     erb :index
   end
 
-
+  get '/peeps' do
+    peeps = [
+      "This is my first peep",
+      "This is my second peep",
+      "This is my third peep"
+    ]
+    peeps.join
+  end 
 
   run! if app_file == $0
 end
--- spec/features/view_peeps_spec.rb
+++ spec/features/view_peeps_spec.rb
@@ -4,4 +4,11 @@ feature 'Viewing peeps' do
 
     expect(page).to have_content "First Peep"
   end
+
+  scenario 'A user can see peeps' do
+    visit('/peeps')
+    expect(page).to have_content "This is my first peep"
+    expect(page).to have_content "This is my second peep"
+    expect(page).to have_content "This is my third peep"
+  end
 end
