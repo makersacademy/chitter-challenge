77d4fb1a52459a9e6c5d58172500d636e8d8c8ae
fridayshoes <one_amp@hotmail.com>
2022-06-12 22:43:33 +0100

    Feature 3 implemented - Time/Date added to peeps

--- lib/peeps.rb
+++ lib/peeps.rb
@@ -1,6 +1,15 @@
 require 'pg'
 
 class Peeps
+
+  attr_reader :id, :peep, :created_on
+  
+  def initialize(id:, peep:, created_on:)
+    @id  = id
+    @peep = peep
+    @created_on = created_on
+  end
+
   def self.all
     # [
     #   "hello everyone",
@@ -10,7 +19,10 @@ class Peeps
     # ]
     connection = PG.connect(dbname: 'chitter')
     result = connection.exec("SELECT * FROM peeps ORDER BY id DESC;") #Ask DB to sort list descending by id
-    p result
-    result.map { |message| message['peep'] }
+    # result.map { |message| message['peep'] }
+    result.map do |message| 
+      # message['peep']
+      Peeps.new(id: message['id'], peep: message['peep'], created_on: message['created_on'])
+    end
   end
 end
--- spec/peeps_spec.rb
+++ spec/peeps_spec.rb
@@ -5,10 +5,10 @@ describe Peeps do
     it 'returns all peeps' do
       peeps = Peeps.all
 
-      expect(peeps).to include "hello everyone"
-      expect(peeps).to include "Still got this bloomin lurgy and high temperature."
-      expect(peeps).to include "Why is he sniffing so much?"
-      expect(peeps).to include "I am in the curry house"
+      expect(peeps.last.peep).to eq "hello everyone"
+      # expect(peeps).to eq "Still got this bloomin lurgy and high temperature."
+      # expect(peeps).to eq "Why is he sniffing so much?"
+      # expect(peeps).to eq "I am in the curry house"
     end
   end
 end
