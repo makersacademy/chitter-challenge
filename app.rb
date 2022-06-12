2308d5edc4ba9ef85884b11187c185a286d37886
benjamin <b.james.neustadt@gmail.com>
2022-06-12 21:48:57 +0200

    USER STORY : Changed user story 2 to user story 1, and broken down this user story into two, in order to first show list of peeps, and then work on list f peeps in reverse order.

--- app.rb
+++ app.rb
@@ -7,8 +7,63 @@ class ChitterApp < Sinatra::Base
   end
 
   get '/' do
-    'Chitter Chatter'
+    erb :index
   end
+
+  get '/peeps' do
+    peeps = [
+      "This is my first peep",
+      "This is my second peep",
+      "This is my third peep"
+    ]
+    peeps.join
+  end 
+
 
   run! if app_file == $0
 end
+
+
+
+
+# USER STORIES:
+
+
+# user story 1 (was 2 originally)
+# As a maker
+# So that I can see what others are saying  
+# I want to see all peeps ////////\\\\\\\\\\\\\
+
+# user story 1.5 
+# As a maker
+# So that I can see what others are saying  
+# I want to see all peeps in reverse chronological order 
+
+
+
+# user story 2
+# As a Maker
+# So that I can let people know what I am doing  
+# I want to post a message (peep) to chitter
+
+
+# user story 3
+# As a Maker
+# So that I can better appreciate the context of a peep
+# I want to see the time at which it was made
+
+# user story 4
+# As a Maker
+# So that I can post messages on Chitter as me
+# I want to sign up for Chitter
+
+
+# FUNCTIONALITY: 
+
+# 1. You don't have to be logged in to see the peeps.
+# 2. Makers sign up to chitter with their email, password, name and a username (e.g. samm@makersacademy.com, password123, Sam Morgan, sjmog).
+# 3. The username and email are unique.
+# 4. Peeps (posts to chitter) have the name of the maker and their user handle.
+# 5. Your README should indicate the technologies used, and give instructions on how to install and run the tests.
+
+
