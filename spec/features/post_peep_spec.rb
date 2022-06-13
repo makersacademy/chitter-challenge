12f5ec0616b495f06667c466e91ca5abee537b4a
benjamin <b.james.neustadt@gmail.com>
2022-06-13 10:46:08 +0200

    present timestamp with the peep, wasnt able to get it together : to work on

--- spec/features/post_peep_spec.rb
+++ spec/features/post_peep_spec.rb
@@ -3,9 +3,11 @@ feature "Post peep" do
   scenario "a user can post a new peep" do
     visit '/peeps/new'
     fill_in 'content', with: 'This is my first peep!'
-    click_button 'Post peep'
+    fill_in 'user_id', with: 'Test name'
+    click_button 'Post'
 
     expect(current_path).to eq '/peeps'
+    # expect(page).to have_content('Test name')
     expect(page).to have_content('This is my first peep!')
   end
 end
