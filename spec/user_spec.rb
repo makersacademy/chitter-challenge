10b4f61166b5acfbcfc2d84bc68369a3dffb5fa1
Dave Kempsell <dave_kempsell@hotmail.com>
2022-06-19 18:24:18 +0100

    create user method added

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,4 +1,20 @@
 require 'user'
+require 'database_helpers'
 
 RSpec.describe User do
+  describe '.create method' do
+    it 'creates a new user' do
+      user = User.create(
+        name: 'Dack Jorsey', username: 'dack',
+        email: 'example@example.com', password: 'password123'
+      )
+      persisted_data = persisted_data(id: user.id, table: 'users')
+
+      expect(user).to be_a User
+      expect(user.id).to eq persisted_data.first['id']
+      expect(user.name).to eq 'Dack Jorsey'
+      expect(user.username).to eq 'dack'
+      expect(user.email).to eq 'example@example.com'
+    end
+  end
 end
