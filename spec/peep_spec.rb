899f90d4a58f0b5705d362f1d06c31306f41bc72
benjamin <b.james.neustadt@gmail.com>
2022-06-19 10:53:20 +0200

    Refactored peep_spec.rb unit test
    1. Included .create method inside the _describe .all_ RSpec test;
    2. Deleted one test inside RSpec.describe Peep because was duplpicate
    3. Placed two tests left inside peep_spec.rb under RSPec.describe do
    title

--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -5,23 +5,12 @@ describe Peep do
     it 'returns all peeps' do
       Peep.create("This is my first peep")
       expect(Peep.all).to include ("This is my first peep")
-      # expect(peeps).to include("This is my second peep")
-      # expect(peeps).to include("This is my third peep")
     end
   end
 
   describe '.create' do
-    xit 'creates a new peep' do
+    it 'creates a new peep' do
       Peep.create(content: 'This is a test')
-
-      expect(Peep.all).to include 'This is a test'
-    end
-  end
-
-  describe '.create' do
-    xit 'creates a new peep and shows the timestamp' do
-      Peep.create(content: 'This is a test')
-
       expect(Peep.all).to include 'This is a test'
     end
   end
