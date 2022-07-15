39b2742f18c957662fa81bc3e5c24b7b45431834
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-15 13:10:54 +0100

    testing setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,13 @@
-require 'simplecov'
-require 'simplecov-console'
+require "simplecov"
+require "simplecov-console"
+require "database_connection"
+
+DatabaseConnection.connect("chitter_test")
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
-  # Want a nice code coverage website? Uncomment this next line!
-  # SimpleCov::Formatter::HTMLFormatter
+# Want a nice code coverage website? Uncomment this next line!
+# SimpleCov::Formatter::HTMLFormatter
 ])
 SimpleCov.start
 
