70ba31236ea94f2c0de6d808a89de0d324e417da
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-07-16 08:01:05 +0100

    Form added

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -6,7 +6,7 @@ require 'pg'
 
 class DatabaseConnection
 
-def self.connect
+def self.connect(database_name)
   # If the environment variable (set by Heroku)
   # is present, use this to open the connection.
   if ENV['DATABASE_URL'] != nil
@@ -21,4 +21,9 @@ def self.connect
   end
   @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
 end
+
+def self.exec_params(query, params)
+  @connection.exec_params(query, params)
+end
+
 end
