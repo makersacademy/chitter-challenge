e8bc9314cf95709c46b1bd0f7e296a84b15d5181
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-07-16 10:31:24 +0100

    Update post signup

--- app.rb
+++ app.rb
@@ -23,15 +23,15 @@ class Application < Sinatra::Base
   post '/signup' do
     repo = UserRepository.new
     @new_user = User.new
-    
+    new_user.id = params[:id]
+    new_user.name = params[:name]
+    new_user.username = params[:username]
+    new_user.email = params[:email]
+    new_user.password = params[:password]
+    repo.create(new_user)
     return erb(:post_signup)
   end
 end
 
 
-#     new_user.id = params[:id]
-#     new_user.name = params[:name]
-#     new_user.username = params[:username]
-#     new_user.email = params[:email]
-#     new_user.password = params[:password]
-#     repo.create(new_user)
+    
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -11,19 +11,6 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
-
-  # def reset_users_table
-  #   seed_sql = File.read('spec/chitter_seeds.sql')
-  #   connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_test' })
-  #   connection.exec(seed_sql)
-  # end
-
-  # def reset_peeps_table
-  #   seed_sql = File.read('spec/chitter_seeds.sql')
-  #   connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_test' })
-  #   connection.exec(seed_sql)
-  # end
-
   context 'GET /' do
     it 'returns 200 OK and a form to sign in' do
       response = get('/')
