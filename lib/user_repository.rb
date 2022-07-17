724920f0a66b9300d71ad2801d1dde28e3326ed2
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-17 14:14:23 +0100

    added create method

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -5,16 +5,22 @@ class UserRepository
     sql = "SELECT * FROM users;"
     result_set = DatabaseConnection.exec_params(sql, [])
 
-    users = map_records_to_user_objects(result_set)
+    map_records_to_user_objects(result_set)
   end
 
   def find_by_email(email)
     sql = "SELECT * FROM users WHERE email = $1;"
     result_set = DatabaseConnection.exec_params(sql, [email])
 
-    user = map_records_to_user_objects(result_set)[0]
+    fail "This email does not exist in the database." if result_set.to_a.empty?
 
-    user
+    map_records_to_user_objects(result_set)[0]
+  end
+
+  def create(user)
+    sql = "INSERT INTO users (first_name, last_name, email, username, password) VALUES ($1, $2, $3, $4, crypt($5, gen_salt('bf', 8)));"
+
+    result_set = DatabaseConnection.exec_params(sql, [user.first_name, user.last_name, user.email, user.username, user.password])
   end
 
   private
