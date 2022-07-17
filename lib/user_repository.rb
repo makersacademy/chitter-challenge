50a5aacdf1c04b1c9f7a6a922a9db8d0c645713e
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-17 12:38:26 +0100

    refactored part of all method into a private method & added find_by_email method

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -2,12 +2,27 @@ require_relative "user"
 
 class UserRepository
   def all
-    users = []
-
     sql = "SELECT * FROM users;"
     result_set = DatabaseConnection.exec_params(sql, [])
 
-    result_set.each do |record|
+    users = map_records_to_user_objects(result_set)
+  end
+
+  def find_by_email(email)
+    sql = "SELECT * FROM users WHERE email = $1;"
+    result_set = DatabaseConnection.exec_params(sql, [email])
+
+    user = map_records_to_user_objects(result_set)[0]
+
+    user
+  end
+
+  private
+
+  def map_records_to_user_objects(records)
+    users = []
+
+    records.each do |record|
       user = User.new
       user.user_id = record["user_id"]
       user.first_name = record["first_name"]
