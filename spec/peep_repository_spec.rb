fc5e41a64ad65b7b9cadb30e12a70e04f126b0b5
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-17 19:10:25 +0100

    added tests for create(peep) and delete(peep)

--- spec/peep_repository_spec.rb
+++ spec/peep_repository_spec.rb
@@ -47,4 +47,38 @@ describe PeepRepository do
 
     expect(peep.content).to eq "my first peep!!"
   end
+
+  context "username not in database" do
+    it "fails when trying to find_by_username" do
+      repo = PeepRepository.new
+      expect { repo.find_by_username("Sam") }.to raise_error "This username does not exist in the database."
+    end
+  end
+
+  it "creates a new peep" do
+    repo = PeepRepository.new
+    user_id = UserRepository.new.find_by_email("test2@example.com").user_id
+    repo.create(double(:peep, content: "peeping", user_id: user_id))
+
+    new_peep = repo.all.last
+
+    expect(new_peep.peep_id.length).to eq 36
+    expect(new_peep.content).to eq "peeping"
+    expect(new_peep.user_id).to eq user_id
+    expect(new_peep.created).to include Time.now.strftime("%F")
+    expect(new_peep.updated).to include Time.now.strftime("%F")
+  end
+
+  it "deletes a peep" do
+    repo = PeepRepository.new
+    peep = repo.all.first
+    repo.delete(peep)
+
+    peeps = repo.all
+
+    expect(peeps.length).to eq 4
+    expect(peeps.first.content).to eq "what a great concept for an app"
+    expect(peeps.first.created).to include Time.now.strftime("%F")
+    expect(peeps.first.updated).to include Time.now.strftime("%F")
+  end
 end
