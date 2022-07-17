88da29858f32a1202e61e83adbb9a1d67a1efd0b
Karolina Joksaite <k.joksaite00@gmail.com>
2022-07-17 14:10:07 +0100

    added test for create(user)

--- spec/user_repository_spec.rb
+++ spec/user_repository_spec.rb
@@ -42,4 +42,17 @@ describe UserRepository do
       expect { repo.find_by_email("test") }.to raise_error "This email does not exist in the database."
     end
   end
+
+  it "creates a new user" do
+    repo = UserRepository.new
+    repo.create(double(:user, first_name: "Jane", last_name: "Doe", email: "jane@doe.com", username: "Jane1", password: "password"))
+    new_user = repo.all.last
+
+    expect(new_user.first_name).to eq "Jane"
+    expect(new_user.last_name).to eq "Doe"
+    expect(new_user.email).to eq "jane@doe.com"
+    expect(new_user.username).to eq "Jane1"
+
+    p repo.all
+  end
 end
