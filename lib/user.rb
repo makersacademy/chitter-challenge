6872238a9f59773ecc5ac8a6c6aa68567a49add5
S-Spiegl <steven.spiegl@gmail.com>
2022-08-07 14:28:57 +0100

    chitter_users working?

--- lib/user.rb
+++ lib/user.rb
@@ -21,12 +21,4 @@ class User
     result = connection.exec_params("INSERT INTO chitter_users (chitterer, username, handle, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING chitterer, username, handle, email, password;", [full_name, username, handle, email, password])
     User.new(full_name: result[0]['chitterer'], username: result[0]['username'], handle: result[0]['handle'], email: result[0]['email'], password: result[0]['password'])
   end
-
-  def self.pg_connection
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect("postgres://tjcpisthcdezaj:e8888f176d8c4a9279a0c01b0ab685b3c51d82a97c406f0778ac35dccb85bfb1@ec2-3-213-228-206.compute-1.amazonaws.com:5432/db76t2s6m0vebl")
-    end
-  end
 end
