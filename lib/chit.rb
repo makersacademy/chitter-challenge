da3d4a7b7948758d4635d002424b008366ed3c2a
S-Spiegl <steven.spiegl@gmail.com>
2022-08-07 01:47:50 +0100

    added pg-connect info from heroku

--- lib/chit.rb
+++ lib/chit.rb
@@ -11,29 +11,38 @@ attr_reader :content, :handle, :timestamp
   end
 
 
+  # def self.all
+  #   # research self
+  #   if ENV['ENVIRONMENT'] == "test"
+  #     connection = PG.connect(dbname: 'chitter_test')
+  #   else
+  #     connection = PG.connect(dbname: 'chitter')
+  #   end
+  #   result = connection.exec('SELECT * FROM chits')
+  #   result.map do |chit|
+  #     Chit.new(content: chit['content'], handle: chit['handle'], timestamp: chit['timestamp'])
+  #   end
+  # end
+ 
   def self.all
-    # research self
-    if ENV['ENVIRONMENT'] == "test"
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-    result = connection.exec('SELECT * FROM chits')
+    connection = pg_connection
+    result = connection.exec_params("SELECT * FROM chits")
+    connection.close
     result.map do |chit|
       Chit.new(content: chit['content'], handle: chit['handle'], timestamp: chit['timestamp'])
     end
   end
 
-  def self.post(handle:, content:)
-    date_time = DateTime.now
-    timestamp = date_time.strftime "%Y-%m-%d %H:%M"
-    if ENV['ENVIRONMENT'] == 'test'
-      connection = PG.connect(dbname: 'chitter_test')
-    else
-      connection = PG.connect(dbname: 'chitter')
-    end
-      connection.exec_params("INSERT INTO chits (handle, content, timestamp) VALUES ($1, $2, $3) RETURNING handle, content, timestamp;", [handle, content, timestamp])
-  end
+  # def self.post(handle:, content:)
+  #   date_time = DateTime.now
+  #   timestamp = date_time.strftime "%Y-%m-%d %H:%M"
+  #   if ENV['ENVIRONMENT'] == 'test'
+  #     connection = PG.connect(dbname: 'chitter_test')
+  #   else
+  #     connection = PG.connect(dbname: 'chitter')
+  #   end
+  #     connection.exec_params("INSERT INTO chits (handle, content, timestamp) VALUES ($1, $2, $3) RETURNING handle, content, timestamp;", [handle, content, timestamp])
+  # end
 
   def self.pg_connection
     if ENV['ENVIRONMENT'] == 'test'
