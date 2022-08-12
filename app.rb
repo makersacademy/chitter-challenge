3a7ae22728c488a3d5cdbb7270af285ac27c4d60
ruben wilson <rwilson_connell@yahoo.com>
2022-08-12 19:44:07 +0100

    sinatra added and webrick- 'rack-up' added

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
-require_relative "PostsRepository"
-require_relative 'UsersRepository'
+require_relative "lib/PostsRepository"
+require_relative 'lib/UsersRepository'
+require 'sinatra/reloader'
+require 'sinatra'
 
-class Application
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/album_repository'
+    also_reload 'lib/artist_repository'
+  end
+  #routes 
 
-  
-end 
+end
--- lib/PostsRepository.rb
+++ lib/PostsRepository.rb
@@ -1,4 +1,4 @@
-require "post"
+require_relative "post"
 
 class PostsRepository
   def all
