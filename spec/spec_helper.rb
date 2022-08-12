6fb9c7c84f50f2eba82c4b3a7fcbb3de61e5e687
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-12 12:23:27 +0100

    project setup, rspec, postgres and sinatra

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+require 'database_connection'
+
+# Make sure this connects to your test database
+# (its name should end with '_test')
+DatabaseConnection.connect('chitter_test')
 require 'simplecov'
 require 'simplecov-console'
 
@@ -11,7 +16,7 @@ SimpleCov.start
 RSpec.configure do |config|
   config.after(:suite) do
     puts
-    puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
-    puts "\e[33mTry it now! Just run: rubocop\e[0m"
+    puts "[33mHave you considered running rubocop? It will help you improve your code![0m"
+    puts "[33mTry it now! Just run: rubocop[0m"
   end
 end
