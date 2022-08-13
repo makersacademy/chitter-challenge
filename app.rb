9281840188b6cbdbe99097cfdc8e0784d93fc790
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-08-13 11:06:23 +0100

    database_connection.rb to connect for app.rb

--- app.rb
+++ app.rb
@@ -1,10 +1,22 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/database_connection'
+require_relative 'lib/user_repo'
+require_relative 'lib/peep_repo'
+
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
-  # This allows the app code to refresh
-  # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/user_repo'
+    also_reload 'lib/peep_repo'
   end
+  
+  get '/' do
+    repo = PeepRepository.new
+    peeps = repo.view_all
+    peeps[0]['content']
+  end
+
 end
