96ca1cdeb8531214873a1d35357961153b585546
alexandrashelley <alexbshelley@gmail.com>
2022-08-14 10:37:35 +0100

    added basic functionality

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -2,10 +2,29 @@ require_relative 'user'
 
 class UserRepository
   def all
+    sql = 'SELECT * FROM users;'
+    result_set = DatabaseConnection.exec_params(sql, [])
 
+    users = []
+
+    result_set.each do |row|
+      user = User.new
+      user.id = row['id']
+      user.name = row['name']
+      user.username = row['username']
+      user.email = row['email']
+      user.password = row['password']
+
+      users << user
+    end
+
+    return users
   end
 
   def create(user)
-
+    sql = 'INSERT INTO users (id, name, username, email, password) VALUES ($1, $2, $3, $4, $5);'
+    params = [user.id, user.name, user.username, user.email, user.password]
+    
+    DatabaseConnection.exec_params(sql, params)
   end
 end
