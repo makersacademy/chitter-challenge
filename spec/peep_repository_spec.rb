f151c5d31dbd99fc9e2ff9eb26602a387c6d259d
alexandrashelley <alexbshelley@gmail.com>
2022-08-14 10:38:22 +0100

    fixed name error in table reset

--- spec/peep_repository_spec.rb
+++ spec/peep_repository_spec.rb
@@ -1,14 +1,14 @@
 require 'peep_repository'
 require 'peep'
 
-def reset_students_table
+def reset_peeps_table
     seed_sql = File.read('spec/test_seeds.sql')
     connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter' })
     connection.exec(seed_sql)
   end
   
-  describe StudentRepository do
+  describe PeepRepository do
     before(:each) do 
       reset_peeps_table
     end
-  
+  end
