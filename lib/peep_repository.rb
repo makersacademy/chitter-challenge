2f2f7db07ad2d9b8f62796b263f567ada719945c
alexandrashelley <alexbshelley@gmail.com>
2022-08-14 14:57:10 +0100

    peeps shown in chronoligcal order pass

--- lib/peep_repository.rb
+++ lib/peep_repository.rb
@@ -2,7 +2,23 @@ require_relative 'peep'
 
 class PeepRepository
   def all
+    sql = 'SELECT * FROM peeps ORDER BY timestamp DESC;'
+    result_set = DatabaseConnection.exec_params(sql, [])
 
+    peeps = []
+
+    result_set.each do |row|
+      peep = Peep.new
+      peep.content = row['content']
+      peep.timestamp = row['timestamp']
+      peep.name = row['name']
+      peep.username = row['username']
+      peep.user_id = row['user_id']
+
+      peeps << peep
+    end
+    
+    return peeps
   end
 
   def create(peep)
