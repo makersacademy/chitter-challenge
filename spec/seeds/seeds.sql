39fdece4f7fac8855842562f6fb20026a9a301ee
alexandrashelley <alexbshelley@gmail.com>
2022-08-14 10:37:56 +0100

    reformatted seed files

--- spec/seeds/seeds.sql
+++ spec/seeds/seeds.sql
@@ -17,3 +17,9 @@ CREATE TABLE peeps (
     references users(id)
     on delete cascade
 );
+
+INSERT INTO users (name, email, username, password) VALUES ('Alex', 'alexemail@test.com', 'iloveanimals391', 'password123');
+INSERT INTO users (name, email, username, password) VALUES ('Zeus', 'zeusemail@test.com', 'habanerohotstuff', 'password123');
+
+INSERT INTO peeps (content, timestamp, name, username, user_id) VALUES ('Now I know how lobsters feel :-(', '2022-08-13 12:00:00', 'Alex', 'iloveanimals123', '1');
+INSERT INTO peeps (content, timestamp, name, username, user_id) VALUES ('Just call me the man with a fan! LOL', '2022-08-13 13:00:00', 'Zeus', 'habanerohotstuff', '2');
