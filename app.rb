ea3ed632382a6725b6e12c0b81f062689694d20d
Zac <zac@zacmoss.co.uk>
2022-08-15 09:16:58 +0100

    changed db for heroku

--- app.rb
+++ app.rb
@@ -8,9 +8,16 @@ require_relative 'lib/peep'
 require_relative 'lib/user'
 require_relative 'lib/send_email'
 
+# ActiveRecord::Base.establish_connection(
+#   adapter:  'postgresql',
+#   database: 'postgresql-colorful-10519'
+# )
+
 ActiveRecord::Base.establish_connection(
-  adapter:  'postgresql',
-  database: 'chitter'
+  adapter:  "postgresql",
+  # username: "vkathwugzaldqh",
+  # password: "9621b8bc05f7d0b3c1468ccdb4d6859fb7992fa7b9f53e7fefae604f7af20c03",
+  database: "postgresql-colorful-10519"
 )
 
 class Application < Sinatra::Base
--- spec/seeds/seeds_mixed_tables.sql
+++ spec/seeds/seeds_mixed_tables.sql
@@ -1,25 +1,25 @@
--- -- Table Definition
--- CREATE TABLE users (
---   id SERIAL PRIMARY KEY,
---   name text,
---   username text,
---   email text,
---   password text
--- );
+-- Table Definition
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name text,
+  username text,
+  email text,
+  password text
+);
 
 
--- - CREATE TABLE peeps (
---   id SERIAL PRIMARY KEY,
---   content text,
---   date_time_created timestamp,
--- -- The foreign key name is always {other_table_singular}_id
---   user_id int,
---   constraint fk_user foreign key(user_id)
---     references users(id)
---     on delete cascade
--- );
+CREATE TABLE peeps (
+  id SERIAL PRIMARY KEY,
+  content text,
+  date_time_created timestamp,
+-- The foreign key name is always {other_table_singular}_id
+  user_id int,
+  constraint fk_user foreign key(user_id)
+    references users(id)
+    on delete cascade
+);
 
-TRUNCATE TABLE users, peeps RESTART IDENTITY; -- replace with your own table name.
+-- TRUNCATE TABLE users, peeps RESTART IDENTITY; -- replace with your own table name.
 
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
