1a30784c0f1d931529ed624cd3f816080431adde
benjamin <b.james.neustadt@gmail.com>
2022-08-21 21:55:22 +0200

    WIP: Test: Sign up test working, though route not
    * Uncommenting the test, we find that it passes, however
      the actual browser is not allowing the redirect once the exact same
      info as in test is inputted.

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,6 +1,6 @@
 feature "sign up" do
   context "from the sign up page" do 
-    xscenario "lets new users sign up" do
+    scenario "lets new users sign up" do
       visit '/'
       fill_in 'username', with: 'Godzilla'
       fill_in 'email', with: 'godzilla@email.com'
@@ -10,6 +10,8 @@ feature "sign up" do
       expect(page).to have_content "Welcome to Chitter Chatter"
       expect(current_path).to eq '/users/sign_up'
     end
+
+    # Test above passes but the actual route is not working (??)
 
     scenario "Greets the User" do
       visit '/'
