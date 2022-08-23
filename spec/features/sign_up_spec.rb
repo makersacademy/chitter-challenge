b49a30ab61de5f3c9b19a881f790cabbce8c0134
benjamin <b.james.neustadt@gmail.com>
2022-08-23 16:42:37 +0200

    Modify: spec files to re-configure tests
    * user signup tests passing as route paths sorted out:
    * the user/sign-up path redirects to user_homepage erb with
      welcome message ${name}
    
    NOTE:
       - currently this test does not actually test the database.
       - currently this test will fail if we ask for password verification,
       - suspicion is that password is not given in the attr_reader and therefore we
         cannot check for it outside the actual class.
       - this test is therefore not checking the class, but rather the function of creation
         inside the route.

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -1,27 +1,26 @@
 feature "sign up" do
   context "from the sign up page" do
     scenario "lets new users sign up" do
-      visit '/'
+      visit '/users/sign_up'
       fill_in 'username', with: 'Godzilla'
       fill_in 'email', with: 'godzilla@email.com'
       fill_in 'password', with: '1234'
       click_button 'Sign Up'
 
-      expect(page).to have_content "Welcome to Chitter Chatter"
       expect(current_path).to eq '/users/sign_up'
     end
 
     # Test above passes but the actual route is not working (??)
 
     scenario "Greets the User" do
-      visit '/'
+      visit '/users/sign_up'
       fill_in 'username', with: 'Godzilla'
       fill_in 'email', with: 'godzilla@email.com'
       fill_in 'password', with: '1234'
       click_button 'Sign Up'
 
-      expect(page).to have_content "Welcome Godzilla!"
       expect(current_path).to eq '/users/sign_up'
+      expect(page).to have_content "Welcome Godzilla!"
     end
   end
 end
