1d1a573f491a1dc6e93e8be892c852d532553410
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-08-28 11:43:22 +0100

    updated

--- app.rb
+++ app.rb
@@ -2,19 +2,19 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 
 class Chitter
-#     < Sinatra::Base
-#   configure :development do
-#     register Sinatra::Reloader
-#   end
+  < Sinatra::Base
+    configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
     'chitter!'
   end 
 
-  get '/peeps' do
-    # @peeps = Peeps.all
-    erb :'peeps/index'
-  end 
+  # get '/peeps' do
+  #   # @peeps = Peeps.all
+  #   erb :'peeps/index'
+  # end 
   
   run! if app_file == $0
 end
