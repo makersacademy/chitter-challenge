c9164e312f59695d4d293d561f1d6409ecdaab37
benjamin <b.james.neustadt@gmail.com>
2022-08-31 17:45:36 +0200

    Improves test surety for .all peeps
    * Peep.all test
      - Add suite of tests to cover single and multiple peep entries:
        - verify that it's all inclusive
    
    * Peep.create functionality improvement
      - Modify the existing test to verify a user_id for each peep
      - requires a user_id to be given
        - this is not enforced at the database level

--- lib/peep.rb
+++ lib/peep.rb
@@ -2,25 +2,30 @@ require 'pg'
 
 class Peep
 
-  attr_reader :content, :time_stamp
+  def self.all
+    DatabaseConnection.query("SELECT * FROM peeps;")
+    .map do |peep|
+      new(
+        content: peep['content'],
+        time_stamp: peep['time_stamp'],
+        user_id: peep['user_id'].to_i
+      )
+    end
+  end
 
-  def initialize(content:, time_stamp:)
+  def self.create(content:, user_id:)
+    DatabaseConnection.query("
+      INSERT INTO peeps (content, user_id)
+      VALUES('#{content}', #{user_id})
+    ")
+  end
+
+  def initialize(content:, time_stamp:, user_id:)
     @content = content
     @time_stamp = time_stamp
+    @user_id = user_id
   end
 
-  def self.all
-    peeps = DatabaseConnection.query("SELECT * FROM peeps;")
+  attr_reader :content, :time_stamp, :user_id
 
-    peeps.map { |peep| 
-      Peep.new(content: peep['content'],
-        time_stamp: peep['time_stamp'])
-    }
-  end
-
-  def self.create(content:)
-   peep_create = DatabaseConnection.query(
-    "INSERT INTO peeps (content) VALUES('#{content}')"
-   )
-  end
 end
--- spec/peep_spec.rb
+++ spec/peep_spec.rb
@@ -3,23 +3,49 @@ require 'peep'
 RSpec.describe Peep do
 
   describe '.all' do
-    it 'returns all peeps' do
+
+    it 'has 1 peep' do
       connection = PG.connect(dbname: 'chitter_chatter_test')
       connection.exec("INSERT INTO peeps (content) VALUES ('This is my first peep');")
-  
-      peeps = Peep.all
 
-      expect(peeps.to_s).to include("This is my first peep")
-      
+      expect(Peep.all.size).to eq 1
+    end
+
+    it 'has the content of the first peep' do
+      connection = PG.connect(dbname: 'chitter_chatter_test')
+      connection.exec("INSERT INTO peeps (content) VALUES ('This is my first peep');")
+
+      expect(Peep.all.first.content).to eq 'This is my first peep'
+    end
+
+    it 'has the content of the second peep' do
+      connection = PG.connect(dbname: 'chitter_chatter_test')
+      connection.exec("INSERT INTO peeps (content) VALUES ('This is my first peep');")
+      connection.exec("INSERT INTO peeps (content) VALUES ('This is my second peep');")
+
+      expect(Peep.all.last.content).to eq 'This is my second peep'
+
+    end
+
+    it 'has two peeps' do
+      connection = PG.connect(dbname: 'chitter_chatter_test')
+      connection.exec("INSERT INTO peeps (content) VALUES ('This is my first peep');")
+      connection.exec("INSERT INTO peeps (content) VALUES ('This is my second peep');")
+
+      expect(Peep.all.size).to eq 2
     end
   end
 
   describe '.create' do
-    it 'creates a new peep' do
-      Peep.create(content: 'This is a test')
-      peeps = Peep.all.to_s
-      expect(peeps).to include 'This is a test'
+
+    it 'a new peep assigned to a user' do
+      Peep.create(content: 'This is a test', user_id: 1)
+      peeps = Peep.all
+
+      expect(peeps.first.content).to eq 'This is a test'
+      expect(peeps.first.user_id).to eq(1)
     end
+
   end
 
 end
