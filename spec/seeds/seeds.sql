3a5a0f0050148f600736c4b0b2f579d51053da22
Stephen Fletcher <stephen.fletchtek@gmail.com>
2022-09-03 09:33:01 +0100

    get bcrypt working again

--- spec/seeds/seeds.sql
+++ spec/seeds/seeds.sql
@@ -1,4 +1,4 @@
-TRUNCATE TABLE users RESTART IDENTITY;
+TRUNCATE TABLE users, peeps RESTART IDENTITY;
 
 INSERT INTO users (email, password) VALUES ('duck@makers.com', 'quack!');
 INSERT INTO users (email, password)
@@ -6,7 +6,7 @@ INSERT INTO users (email, password)
 INSERT INTO users (email, password, name, username)
   VALUES ('homer@simpsons.com', '$2a$12$GKyE/JG3VsUfVeaPzfoNu.4U2DLkXo9uPbq1/K2MohAgAC2Qw4sTm','Homer Simpson',	'@homer');
 
-TRUNCATE TABLE peeps RESTART IDENTITY;
+-- TRUNCATE TABLE peeps RESTART IDENTITY;
 
 INSERT INTO peeps (content, timestamp, name, username)
   VALUES ('It is awfully hot today', '2022-08-12 21:55:12 +0100', 'Daffy Duck', '@daffy');
