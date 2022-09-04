dd4ff6323244bf9a3eb7d762410ba7448e747f4e
nickwlong <nickwlong@gmail.com>
2022-09-04 19:51:52 +0100

    Travis testing #7

--- spec/chitter_seeds.sql
+++ spec/chitter_seeds.sql
@@ -6,14 +6,11 @@ CREATE TABLE users (
   password text
 );
 
-
--- Then the table with the foreign key first.
 CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
   content text,
   post_time timestamp,
   tagged_users text,
--- The foreign key name is always {other_table_singular}_id
   user_id int,
   constraint fk_user foreign key(user_id)
     references users(id)
