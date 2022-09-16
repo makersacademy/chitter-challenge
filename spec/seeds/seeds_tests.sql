ef790a5fa2336157ae1f689a5e893016471b05ec
jovi <kwong.jovi@gmail.com>
2022-09-16 09:31:54 +0100

    Updated database .sql files to now have a user's id, email and handle to be unique primary keys.

--- spec/seeds/seeds_tests.sql
+++ spec/seeds/seeds_tests.sql
@@ -1,13 +1,13 @@
 TRUNCATE TABLE peeps, users, peeps_users RESTART IDENTITY;
 
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 1', '2022-01-01 01:01:01', 'Wendy', 'wendy0');
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 2', '2022-02-02 02:02:02', 'Jovi', 'jovi0');
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 3', '2022-03-03 03:03:03', 'Joey', 'joey0');
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 4', '2022-04-04 04:04:04', 'Chandler', 'chandler0');
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 5', '2022-05-05 05:05:05', 'Ross', 'ross0');
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 6', '2022-06-06 06:06:06', 'Phoebe', 'phoebe0');
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 7', '2022-07-07 07:07:07', 'Rachel', 'rachel0');
-INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Peep 8', '2022-08-08 08:08:08', 'Monica', 'monica0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Hello, I am Wendy!', '2022-01-01 01:01:01', 'Wendy', 'wendy0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('I am named after Bon Jovi...', '2022-02-02 02:02:02', 'Jovi', 'jovi0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('How YOU doin?', '2022-03-03 03:03:03', 'Joey', 'joey0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Could I BE more sorry?', '2022-04-04 04:04:04', 'Chandler', 'chandler0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('You at my sandwhich?!', '2022-05-05 05:05:05', 'Ross', 'ross0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Princess Consuela Bananahammock', '2022-06-06 06:06:06', 'Phoebe', 'phoebe0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('I got off the plane.', '2022-07-07 07:07:07', 'Rachel', 'rachel0');
+INSERT INTO peeps (content, date_time, user_f_name, user_handle) VALUES ('Does anybody care what kind of potatoes I want!?', '2022-08-08 08:08:08', 'Monica', 'monica0');
 
 INSERT INTO users (email, password, f_name, handle) VALUES ('wendy0@example.com', 'password123', 'Wendy', 'wendy0');
 INSERT INTO users (email, password, f_name, handle) VALUES ('jovi0@example.com', 'password123', 'Jovi', 'jovi0');
