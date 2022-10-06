f4a533a2d9b187b1d304affbf615ce087a4419d6
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-06 15:28:26 +0100

    Improve spacing for peep_seeds

--- spec/seeds/peep_seeds.sql
+++ spec/seeds/peep_seeds.sql
@@ -2,9 +2,10 @@ CREATE TABLE peeps (
     id SERIAL PRIMARY KEY,
     content text,
     creation_date date,
+    
     user_id int,
     constraint fk_user foreign key (user_id)
-    references users (id)
+    references users(id)
     on delete cascade
 );
 
