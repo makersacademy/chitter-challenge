f3341839edc640e54b84e0cda4d41b6db8ba5e1d
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-06 15:27:13 +0100

    Add foreign key to peeps table

--- spec/seeds/peep_seeds.sql
+++ spec/seeds/peep_seeds.sql
@@ -3,4 +3,8 @@ CREATE TABLE peeps (
     content text,
     creation_date date,
     user_id int,
+    constraint fk_user foreign key (user_id)
+    references users (id)
+    on delete cascade
 );
+
