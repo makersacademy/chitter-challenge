19af36c2494477a720f3ec2fff8a7b737711f210
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-06 17:08:12 +0100

    Add first test for all method in UserRepository class

---
+++ spec/user_repository_spec.rb
@@ -0,0 +1,25 @@
+require 'user'
+require 'user_repository'
+
+def reset_users_table
+  seed_sql = File.read('spec/seeds/seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_database_test'})
+  connection.exec(sql)
+end
+
+describe UserRepository do
+  before(:each) do
+    reset_users_table
+  end
+
+  describe '#all' do
+    it 'finds all users' do
+      repo = UserRepository.new
+
+      users = repo.all
+
+      expect(users.length.to eq(2))
+      expect(users.first.name).to eq('Ruby')
+    end
+  end
+end
