e77b7d8c954934ec31ba4715389ab5454eb94efd
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-06 16:07:34 +0100

    Create #create(user)

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -9,7 +9,8 @@ class UserRepository
 
   end
 
-  def create
-
+  def create(user)
+    sql = 'INSERT INTO users (username, password, name, email) VALUES ($1, $2, $3, $4)'
+    DatabaseConnection.exec_params(sql, [user.username, user.password, user.name, user.email])
   end
 end
