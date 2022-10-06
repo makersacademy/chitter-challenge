b5332ebbc207a6155acc84a80e9f6e9df718dda5
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-06 15:34:54 +0100

    Create user model class

---
+++ lib/user.rb
@@ -0,0 +1,10 @@
+class user
+  def initialize(username, password, name, email)
+    @username = username
+    @password = password
+    @name = name
+    @email = email
+  end
+
+  attr_accessor :username, :password, :name, :email
+end
