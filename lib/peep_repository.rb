35cba28554a185518d7a51dafc5eee7f63830e9f
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-06 16:38:04 +0100

    Add #create(peep)

--- lib/peep_repository.rb
+++ lib/peep_repository.rb
@@ -9,7 +9,8 @@ class PeepRepository
 
   end
 
-  def create
-
+  def create(peep)
+    sql = 'INSERT INTO peeps (content, creation_date, user_id) VALUES ($1, $2, $3)'
+    DatabaseConnection.exec_params(sql, [peep.content, peep.creation_date, peep.user_id])
   end
 end
