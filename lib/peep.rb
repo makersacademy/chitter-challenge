ef3a5b0604399599df30af5a3fc4405532969979
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-07 18:37:53 +0100

    Bring consistency to indentation

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,9 +1,9 @@
 class Peep
-def initialize(content = '', creation_date = 0, user_id = 0)
-  @content = content
-  @creation_date = creation_date
-  @user_id = user_id
-end
+  def initialize(content = '', creation_date = 0, user_id = 0)
+    @content = content
+    @creation_date = creation_date
+    @user_id = user_id
+  end
 
   attr_accessor :content, :creation_date, :user_id
 end
