14a77fc32d1da2e568f7e04c7c5ab36cce75acea
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-07 12:10:40 +0100

    Have creation_date set itself

--- lib/peep_repository.rb
+++ lib/peep_repository.rb
@@ -10,7 +10,7 @@ class PeepRepository
   end
 
   def create(peep)
-    sql = 'INSERT INTO peeps (content, creation_date, user_id) VALUES ($1, $2, $3)'
-    DatabaseConnection.exec_params(sql, [peep.content, peep.creation_date, peep.user_id])
+    sql = 'INSERT INTO peeps (content, creation_date, user_id) VALUES ($1, NOW(), $2)'
+    DatabaseConnection.exec_params(sql, [peep.content, peep.user_id])
   end
 end
