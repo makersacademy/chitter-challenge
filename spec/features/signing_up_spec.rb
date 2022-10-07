c930c424defaa1f7ed07db8f92c37aea8d4b940a
jennieevans <jennie1evans@hotmail.com>
2022-10-07 10:39:31 +0100

    sign up and sign in buttons added to peeps page

--- spec/features/signing_up_spec.rb
+++ spec/features/signing_up_spec.rb
@@ -8,4 +8,20 @@ feature 'user registration' do
 
     expect(page).to have_content 'Welcome test12'
   end
+
+  scenario 'a user can get to sign up page from peeps page' do
+    visit '/peeps'
+    click_link('Sign up')
+    
+    expect(current_path).to eq '/users/new'
+    expect(page).to have_content 'Sign up here to join Chitter'
+  end
+
+  scenario 'a user can get to sign in page from peeps page' do
+    visit '/peeps'
+    click_link('Sign in')
+    
+    expect(current_path).to eq '/sessions/new'
+    expect(page).to have_content 'Sign in here to Chitter'
+  end
 end
