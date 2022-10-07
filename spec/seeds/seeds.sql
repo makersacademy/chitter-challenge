45ecde8b06c586e7105a9119d6db293930e0a522
Patricia Garcia <patricia@MASTUDENT-Patricia-Garcia-C17G44KCQ6L4.local>
2022-10-07 10:09:17 +0100

    Change data type and values for created_at in table peeps

--- spec/seeds/seeds.sql
+++ spec/seeds/seeds.sql
@@ -4,6 +4,6 @@ TRUNCATE TABLE peeps, users RESTART IDENTITY;
 INSERT INTO users (name, username, email, password) VALUES ('Ruby', 'cute-cat', 'ruby1@gmail.com', '12345');
 INSERT INTO users (name, username, email, password) VALUES ('Amethist', 'foxlover', 'ameth22@gmail.com', '123456');
 
-INSERT INTO peeps (content, user_id, created_at) VALUES ('Have you seen my new kitty-cat? She is adorable!', '1', '2022-10-05 17:50:20');
-INSERT INTO peeps (content, user_id, created_at) VALUES ('I am back from the dead and stronger than ever.', '2', '2022-10-03 12:45:20');
-INSERT INTO peeps (content, user_id, created_at) VALUES ('Who thought foxes could be so fascinating? Have you seen the documentary...', '2', '2022-10-01 10:55:20');
+INSERT INTO peeps (content, user_id, created_at) VALUES ('Have you seen my new kitty-cat? She is adorable!', '1', CURRENT_TIMESTAMP);--'2022-10-05 17:50:20');
+INSERT INTO peeps (content, user_id, created_at) VALUES ('I am back from the dead and stronger than ever.', '2', CURRENT_TIMESTAMP); --'2022-10-03 12:45:20'
+INSERT INTO peeps (content, user_id, created_at) VALUES ('Who thought foxes could be so fascinating? Have you seen the documentary...', '2', CURRENT_TIMESTAMP); --'2022-10-01 10:55:20'
--- tables.sql
+++ tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
 CREATE TABLE peeps (
   id SERIAL PRIMARY KEY,
   content text,
-  created_at text,          --date_time Timestamp,
+  created_at timestamp,          --date_time Timestamp,
   user_id int,
   constraint fk_user foreign key(user_id)
     references users(id)
