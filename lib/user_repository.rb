41cc021c11a89a0a4510dd476f30994d8d446efe
imisiaina <imis.aina1@gmail.com>
2022-10-07 19:33:38 +0100

    User Methods added

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -16,7 +16,22 @@ class UserRepository
     end
     return users
   end
-end
 
-def find(id)
-  sql - 'SELECT * FROM users'
+
+  def find(id)
+    sql - 'SELECT * FROM users WHERE id = $1;'
+    result_set = DatabaseConnection.exec_params(sql, [id])
+
+    user = User.new
+    user.id = result_set[0]['id'].to_i
+    user.username = result_set[0]['username']
+    user.password = result_set[0]['password']
+    return artist 
+  end 
+
+  def create(user)
+    sql = 'INSERT INTO users (username, password) VALUES ($1, $2);'
+    result_set = DatabaseConnection.exec_params(sql, [user.username, user.password])
+    return user
+  end
+end
