8486b0f15e22ae938928457bbf375897cf12b0be
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-10-07 19:28:14 +0100

    Move #reset_tables definition to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,12 @@ require 'database_connection'
 
 DatabaseConnection.connect('chitter_test')
 
+def reset_tables
+  seed_sql = File.read('spec/seeds/reset_tables_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_test' })
+  connection.exec(seed_sql)
+end
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
