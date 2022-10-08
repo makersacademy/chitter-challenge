74858c25f8e502ed9011a2084c291371cb0a5790
imisiaina <imis.aina1@gmail.com>
2022-10-08 13:19:45 +0100

    Updated Seeds

--- spec/seeds/peeps_seeds.sql
+++ spec/seeds/peeps_seeds.sql
@@ -11,5 +11,5 @@ TRUNCATE TABLE peeps RESTART IDENTITY;
 INSERT INTO peeps ("content", "user_id") VALUES
 ('Hey all, Im the flash, the fastets man alive!', 1),
 ('Good Morning America, it is I Superman!', 2),
-('Im Batman', 3);
-('Hey yall its your friendly neighbourhood spider-man!', 4)
+('Im Batman', 3),
+('Hey yall its your friendly neighbourhood spider-man!', 4);
--- spec/seeds/users_seeds.sql
+++ spec/seeds/users_seeds.sql
@@ -13,5 +13,5 @@ TRUNCATE TABLE users RESTART IDENTITY;
 INSERT INTO users ("email", "name", "username", "password") VALUES
 ('barrya@gmail.com', 'Barry Allen', 'The_flash', 'fast'),
 ('clarkk@dailyplanet.com', 'Clark Kent', 'Kal-el', 'Super'),
-('brucew@wayneindustries.com', 'Bruce Wayne', 'Batman', 'rich');
-('peterp@esu.org.uk', 'Peter Parker', 'Spider-man', 'webhead')
+('brucew@wayneindustries.com', 'Bruce Wayne', 'Batman', 'rich'),
+('peterp@esu.org.uk', 'Peter Parker', 'Spider-man', 'webhead');
