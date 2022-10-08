79e07f6f708e40ed76c4b4ab6951072811a6525c
imisiaina <imis.aina1@gmail.com>
2022-10-08 10:54:51 +0100

    Signup Added

--- app.rb
+++ app.rb
@@ -8,16 +8,30 @@ class Application < Sinatra::Base
     also_reload ''
   end
 
+    # Homepage
   get '/' do
     return erb(:homepage)
   end
 
+  #Signup page
   get '/signup' do
-    return #erb(:signup)
+    return erb(:signup)
   end
 
+  # Successfully signed up -> link to home page
   post '/signup' do
-    return # Successfully signed up -> link to home page (see if i can redirect page automatically to homepage)
+    if invalid_request_signup?
+      status 400
+      return ''
+    end
+
+    repo = UserRepository.new
+    new_user.id = params[:id]
+    new_user.username = params[:username]
+    new_user.password = params[:password]
+    repo.create(new_user)
+    return erb(:signup_success)
+      #(see if i can redirect page automatically to homepage)
   end
 
   get '/newpeep' do
@@ -27,3 +41,15 @@ class Application < Sinatra::Base
   post '/newpeep' do
     return # New peep added -> see if i can redirect back to home page 
   end
+end
+
+private 
+
+# Error Status methods
+def invalid_request_signup?
+  params[:username] == nil || params[:password] == nil
+end
+
+def invalid_request_peep?
+  params[:content] == nil
+end
