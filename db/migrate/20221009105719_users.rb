ccc1dfd269e883e1d7166b9cf9483657fa44ef70
Na-Rae Kim <narae1096@gmail.com>
2022-10-10 20:59:43 +0100

    add user real name in the table

--- db/migrate/20221009105719_users.rb
+++ db/migrate/20221009105719_users.rb
@@ -1,9 +1,10 @@
 class Users < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      t.stirng :name
       t.string :username
-      t.string :password_digest
       t.string :email
+      t.string :password_digest
     end
   end
 end
