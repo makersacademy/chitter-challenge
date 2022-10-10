03a712c905dbe23074b12033d1ba1aa66dcd7eb7
Na-Rae Kim <narae1096@gmail.com>
2022-10-10 21:12:44 +0100

    update table with first and last name

--- db/migrate/20221010200952_users.rb
+++ db/migrate/20221010200952_users.rb
@@ -1,7 +1,8 @@
 class Users < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.stirng :name
+      t.string :first_name
+      t.string :last_name
       t.string :username
       t.string :email
       t.string :password_digest
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_09_105732) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_10_201003) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,9 +22,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_09_105732) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.string "username"
-    t.string "password_digest"
     t.string "email"
+    t.string "password_digest"
   end
 
 end
