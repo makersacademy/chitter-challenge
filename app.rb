500ce5c26d7f3981df477c568513f1751b2882de
williamjones <willj836@gmail.com>
2022-11-30 16:52:10 +0000

    can log in, log out and create new users

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ class Application < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  def initialize 
+    # initialize the all 3 repos here
+  end
+
   get '/' do 
     peep_repository = PeepRepository.new 
     @peeps = peep_repository.all
@@ -28,19 +32,60 @@ class Application < Sinatra::Base
   end
 
   post '/signup' do 
-    username = params[:username]
-    email = params[:email]
-    password = params[:password]
     new_user = User.new 
-    new_user.username = username
-    new_user.email = email 
-    new_user.password = password
+    new_user.username = params[:username]
+    new_user.email = params[:email] 
+    new_user.password = params[:password]
     repo = UserRepository.new 
-    repo.create(new_user)
+    @user_repository = UserRepository.new 
+    if @user_repository.check_if_username_or_email_taken(params[:username], params[:email])
+      return erb(:taken_details)
+    else 
+      repo.create(new_user)
+    end
+
+
     peep_repository = PeepRepository.new 
     @peeps = peep_repository.all
-    @user_repository = UserRepository.new 
     @comment_repository = CommentRepository.new
     return erb(:homepage_logged_in)
+  end
+
+  post '/credentials_checker' do
+    peep_repository = PeepRepository.new 
+    @peeps = peep_repository.all
+    @comment_repository = CommentRepository.new
+    @user_repository = UserRepository.new
+
+    all_users = @user_repository.all 
+    valid_details = all_users.any? { |user| (user.username == params[:username]) and (BCrypt::Password.new(user.password) == params[:password])}
+    if valid_details
+      return erb(:homepage_logged_in)
+    else
+      return redirect '/incorrect-details'
+    end
+  end
+
+  get '/logged_in' do 
+    return erb(:homepage_logged_in)
+  end
+
+  get '/incorrect-details' do 
+    peep_repository = PeepRepository.new 
+    @peeps = peep_repository.all
+    @comment_repository = CommentRepository.new
+    @user_repository = UserRepository.new
+    return erb(:incorrect_details)
+  end
+end
+
+
+def sign_in(username, submitted_password)
+  user = find_by_username(username)
+  return nil if user.nil?
+  if submitted_password == BCrypt::Password.new(user.password)
+    true
+  else
+    false
   end
 end
