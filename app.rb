9723400b15bb3c937989a734fd1f0bf82f1f6fe9
Marina Papapetrou <contact@marinapapap.com>
2022-12-01 18:11:04 +0000

    repo classes running in app.rb file

--- app.rb
+++ app.rb
@@ -1,4 +1,32 @@
 require_relative 'lib/database_connection'
+require_relative 'lib/user_repository'
+require_relative 'lib/peep_repository'
 
 # We need to give the database name to the method `connect`.
 DatabaseConnection.connect('chitter')
+
+class Application
+
+  def initialize(database_name, io, user_repository, peep_repository)
+    DatabaseConnection.connect(database_name)
+    @io = io
+    @user_repository = user_repository
+    @peep_repository = peep_repository
+  end
+
+
+  def run
+   
+    @peep_repository.all.each do |peep|
+      puts "#{peep.id} #{peep.content} #{peep.date_time} #{peep.user_id}"
+    end
+
+  end
+
+end
+
+peep_repository = PeepRepository.new
+user_repository = UserRepository.new
+app = Application.new('chitter', Kernel, user_repository, peep_repository)
+
+app.run
