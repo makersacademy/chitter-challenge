c3bb3bc8a46de4a581f506a15b8abdcc83ba5ed4
Robbie Kirkbride <robbie_kirkbride@hotmail.co.uk>
2022-12-01 14:24:37 +0000

    First commit, project setup

---
+++ app.rb
@@ -0,0 +1,16 @@
+# file: app.rb
+require 'sinatra'
+require "sinatra/reloader"
+require_relative 'lib/database_connection'
+require_relative 'lib/album_repository'
+require_relative 'lib/artist_repository'
+
+DatabaseConnection.connect
+
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/album_repository'
+    also_reload 'lib/artist_repository'
+  end
+end
