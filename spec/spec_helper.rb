3e44dc7b76b75135439c65da7b49eb07a05c67f7
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-12-01 15:01:40 +0000

    Project setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,14 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'database_connection'
+
+# Make sure this connects to your test database
+# (its name should end with '_test')
+require 'database_connection'
+
+ENV['ENV'] = 'test'
+
+DatabaseConnection.connect
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
