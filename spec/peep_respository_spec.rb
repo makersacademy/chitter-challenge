accc3b4117154eefbe5abb33437f9065a6812f24
Marina Papapetrou <contact@marinapapap.com>
2022-12-01 15:46:34 +0000

    second peep repo test for find(id) method ran red

--- spec/peep_respository_spec.rb
+++ spec/peep_respository_spec.rb
@@ -15,21 +15,30 @@ describe PeepRepository do
   # (your tests will go here).
 
 
-it "returns all peeps" do
-  repo = PeepRepository.new
+  it "returns all peeps" do
+    repo = PeepRepository.new
 
-  peeps = repo.all
+    peeps = repo.all
 
-  expect(peeps.length).to eq 3
+    expect(peeps.length).to eq 3
 
-  expect(peeps[0].id).to eq '1'
-  expect(peeps[0].content).to eq 'peep 1'
-  expect(peeps[0].user_id).to eq '1'
+    expect(peeps[0].id).to eq '1'
+    expect(peeps[0].content).to eq 'peep 1'
+    expect(peeps[0].user_id).to eq '1'
 
-  expect(peeps[1].id).to eq '2'
-  expect(peeps[1].content).to eq 'peep 2'
-  expect(peeps[1].user_id).to eq '2'
-end
+    expect(peeps[1].id).to eq '2'
+    expect(peeps[1].content).to eq 'peep 2'
+    expect(peeps[1].user_id).to eq '2'
+  end
 
+  it "returns one peep based on id" do
+    repo = PeepRepository.new
 
+    peep = repo.find(1)
+
+    expect(peep.id).to eq '1'
+    expect(peep.content).to eq 'peep 1'
+    expect(peep.date_time).to eq '2022-12-01 15:38:09.152426'
+    expect(peeps.user_id).to eq '1'
+  end
 end
