7761839855abda423f82476cc6b34b1df9b8e691
Marina Papapetrou <contact@marinapapap.com>
2022-12-01 23:37:10 +0000

    first get / and get /peeps route tests passed

--- app.rb
+++ app.rb
@@ -1,10 +1,26 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/database_connection'
+require_relative 'lib/peep_repository'
+require_relative 'lib/user_repository'
+
+DatabaseConnection.connect('chitter_test')
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb(:homepage)
+  end
+
+  get '/peeps' do
+    repo = PeepRepository.new
+    @peeps = repo.all.reverse
+
+    erb(:all_peeps)
   end
 end
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,44 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+
+  def reset_both_tables
+    seed_sql = File.read('spec/seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_test' })
+    connection.exec(seed_sql)
+  end
+  
+  
+  before(:each) do 
+    reset_both_tables
+  end
+
+context "GET /" do
+  it "homepage" do
+    response = get('/')
+
+    expect(response.body).to include('<h1>Welcome to Chitter!</h1>')
+    expect(response.body).to include('<a href="/peeps">View Peeps Here</a>')
+  end
+end
+
+context "GET /peeps" do
+  it "returns peeps in reverse chronological order" do
+    response = get('/peeps')
+
+    expect(response.body).to include('<h1>Peeps</h1>')
+    expect(response.body).to include('<p>peep 1</p>')
+    expect(response.body).to include('<p>peep 2</p>')
+  end
+end
+
+end
