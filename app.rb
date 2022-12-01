c3feb35d048b1247d0e2a72539f5b82a828a7081
Marina Papapetrou <contact@marinapapap.com>
2022-12-01 18:15:48 +0000

    sinatra setup and rackup running

--- app.rb
+++ app.rb
@@ -1,32 +1,10 @@
-require_relative 'lib/database_connection'
-require_relative 'lib/user_repository'
-require_relative 'lib/peep_repository'
+require 'sinatra/base'
+require 'sinatra/reloader'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('chitter')
-
-class Application
-
-  def initialize(database_name, io, user_repository, peep_repository)
-    DatabaseConnection.connect(database_name)
-    @io = io
-    @user_repository = user_repository
-    @peep_repository = peep_repository
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
   end
-
-
-  def run
-   
-    @peep_repository.all.each do |peep|
-      puts "#{peep.id} #{peep.content} #{peep.date_time} #{peep.user_id}"
-    end
-
-  end
-
 end
-
-peep_repository = PeepRepository.new
-user_repository = UserRepository.new
-app = Application.new('chitter', Kernel, user_repository, peep_repository)
-
-app.run
