ce8f1d4ca23df93358b02dd625ec1d328da3bd06
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-02 17:39:29 +0000

    Adds GET /peeps view

--- app.rb
+++ app.rb
@@ -1,10 +1,24 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/peep_repository'
+require_relative './lib/account_repository'
+
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+  end
+
+  get "/peeps" do
+    peep_repository = PeepRepository.new
+    account_repository = AccountRepository.new
+    @peeps = peep_repository.all
+    @authors = @peeps.map do |peep|
+      account_repository.find(peep.account_id)
+    end
+    return erb(:peeps)
   end
 end
--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -2,6 +2,12 @@ require "spec_helper"
 require "rack/test"
 require_relative '../../app'
 
+def reset_tables
+  sql_seed = File.read('spec/seeds/seeds_peeps.sql')
+  connection = DatabaseConnection.connect
+  connection.exec(sql_seed)
+end
+
 describe Application do
   # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
@@ -10,4 +16,25 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  before(:each) do
+    reset_tables
+  end
+
+  context "GET /peeps" do
+    it "Returns an HTML view with all peeps in reverse chronological order" do
+      get_response = get("/peeps")
+
+      expect(get_response.status).to eq 200
+      expect(get_response.body).to include(
+        '<h1>Chitter</h1>',
+        '<h3>TSeleiro</h3>',
+        '<p>My third peep</p>',
+        '<p>2022-11-03 07:13:49</p>'
+      )
+      # We check that the most recent entry is followed by the second most recent
+      expect(get_response.body).to match(
+        /<p>2022-11-03 07:13:49<\/p>\s*?<\/div>\s*?<div>\s*?<h3>RKirkbride<\/h3>/
+      )
+    end
+  end
 end
