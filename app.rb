bb340c129cb6ee9ab6a5d95f98dd182b0c7023be
Thomas Seleiro <thomas.seleiro@gmail.com>
2022-12-02 13:43:19 +0000

    Sinatra setup

---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+end
