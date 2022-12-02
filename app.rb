6eec451efeff4c65a3d5090bd712071f49120899
Eliza Thomas <elizamthomas1994@gmail.com>
2022-12-02 10:35:23 +0000

    Added home erb file to create visual for web app

--- app.rb
+++ app.rb
@@ -16,37 +16,37 @@ class Application < Sinatra::Base
     also_reload 'lib/account_repository'
   end
 
-  get '/peeps' do
+  get '/home' do
     repo = PeepRepository.new
     @peeps = repo.all
 
-    return erb(:peeps)
+    return erb(:home)
   end
 
-  get '/peeps/new' do
-    return erb(:new_peep)
-  end
+  # get '/peeps/new' do
+  #   return erb(:new_peep)
+  # end
 
-  post '/peeps' do
-    repo = PeepRepository.new
-    content = params[:content]
-    post_time = params[:post_time]
-    account_id = params[:account_id]
+  # post '/peeps' do
+  #   repo = PeepRepository.new
+  #   content = params[:content]
+  #   post_time = params[:post_time]
+  #   account_id = params[:account_id]
 
-    peep = Peep.new
-    peep.content = content
-    peep.post_time = post_time
-    peep.account_id = account_id
+  #   peep = Peep.new
+  #   peep.content = content
+  #   peep.post_time = post_time
+  #   peep.account_id = account_id
 
-    repo.create(peep)
-  end
+  #   repo.create(peep)
+  # end
 
-  get '/peeps/:id' do
-    id = params[:id]
-    repo = PeepRepository.new
-    account_repository = AccountRepository.new 
-    @peep = repo.find(params[:id])
-    @account = account_repository.find(@peep.account_id)
-    return erb(:account_id)
-  end 
+  # get '/peeps/:id' do
+  #   id = params[:id]
+  #   repo = PeepRepository.new
+  #   account_repository = AccountRepository.new 
+  #   @peep = repo.find(params[:id])
+  #   @account = account_repository.find(@peep.account_id)
+  #   return erb(:account_id)
+  # end 
 end
