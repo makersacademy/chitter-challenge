3ad959ce5b88ea9b966ec08e036a73d00a785cd0
Jude <ashworthjude@gmail.com>
2022-12-03 14:42:11 +0000

    ready for second deploy

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ require_relative 'lib/peep_repository'
 require_relative 'lib/comment_repository'
 require_relative 'lib/user_repository'
 
-DatabaseConnection.connect('chitter_database_test')
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   enable :sessions
