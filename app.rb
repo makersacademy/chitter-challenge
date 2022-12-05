a0c1c1f0049c7c456cbcd7232e349afee4839721
Anisha Hirani <anisha.hirani11@gmail.com>
2022-12-05 00:25:40 +0000

    app.rb, done sign up but rackup no longer working

--- app.rb
+++ app.rb
@@ -3,7 +3,6 @@ require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require_relative 'lib/user_repository'
 require_relative 'lib/peep_repository'
-require_relative 'lib/user'
 
 DatabaseConnection.connect
 
@@ -15,4 +14,48 @@ class Application < Sinatra::Base
     also_reload 'lib/user_repository'
     also_reload 'lib/peep_repository'
   end
+
+  get '/' do 
+    repo = PeepRepository.new 
+    user_repo = UserRepository.new 
+    @peeps = repo.all 
+    return erb(:homepage)
+  end
+  
+  get '/signup' do 
+    return erb(:signup)
+  end
+
+  post '/signup' do
+    @username = params[:username]
+    repo = UserRepository.new 
+    repo2 = User.new 
+    repo2.email = params[:email]
+    repo2.password = params[:password]
+    repo2.name = params[:name]
+    repo2.username = @username
+    repo.create(repo2)
+
+    return erb(:signup_done)
+  end 
+
+  get '/peeps' do 
+    repo = PeepRepository.new
+    @peeps = repo.all 
+    return erb(:homepage)
+  end
+
+  post '/peeps' do 
+    repo = PeepRepository.new 
+    repo2 = Peep.new 
+    repo2.message = params[:message]
+    @time = Time.now 
+    repo2.time = @Time
+    repo2.user_id = params[:user_id]
+
+    repo.create(repo2)
+    @peeps = repo.all 
+    return erb(:homepage)
+    return erb(:post_peep)
+  end 
 end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -19,4 +19,55 @@ describe Application do
   before :each do
     reset_users_peeps_table
   end
+
+  context 'GET /' do 
+    it "returns homepage of peeps" do 
+      response = get('/')
+
+      expect(response.status).to eq 200 
+      expect(response.body).to include 'Latest Peeps'
+      expect(response.body).to include 'Message: COMING HOME!'
+    end 
+  end
+
+  context 'GET /signup' do 
+    it "returns signup page" do 
+      response = get('/signup') 
+
+      expect(response.status).to eq 200 
+      expect(response.body). to include '<form action="/signup" method="POST">'
+      expect(response.body). to include '<input type="text" name="email" />'
+      expect(response.body). to include '<input type="text" name="password" />'
+      expect(response.body). to include '<input type="text" name="name" />'
+      expect(response.body). to include '<input type="text" name="username" />'
+      expect(response.body). to include '<input type="submit">'
+    end 
+  end
+
+  context 'POST /signup' do 
+    it "makes new user through sign up form" do 
+      response = post('/signup', email: 'testing@gmail.com', password: 'test123', name: 'Test', username: 'test')
+      expect(response.status).to eq 200 
+      expect(response.body).to include '<h2>Sign up Done! test</h2>'
+      expect(response.body).to include '<a href="/">Homepage</a>'
+    end 
+  end
+
+  context 'GET /peeps' do 
+    it "gets full list of peeps on the homepage" do 
+      response = get('/peeps')
+
+      expect(response.status).to eq 200 
+      expect(response.body).to include 'SCORE'
+    end 
+  end
+
+  context 'POST /peeps' do 
+    it "posts a peep" do 
+      response = post('/peeps', message: 'Rashford needs to start!', time: '2022-12-04 13:50:00', user_id: 3)
+
+      expect(response.status).to eq 200 
+      expect(response.body).to include 'Rashford needs to start!'
+    end 
+  end 
 end 
