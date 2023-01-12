96f2507a849b097be33a9ab4dfd1d9030ab967b0
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-01-12 16:46:02 +0000

    All tests passed for user and post repos

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -1,3 +1,53 @@
+require_relative 'user'
 class UserRepository
+  def all
+    users = []
+    sql = 'SELECT * FROM users;'
+    result_set = DatabaseConnection.exec_params(sql,[])
 
+    result_set.each do |record|
+      user = User.new
+      user.id = record['id']
+      user.name = record['name']
+      user.email = record['email']
+      user.user_name = record['user_name']
+      user.password = record['password']
+      user.logged_in = record['logged_in']
+      users << user
+    end
+    return users
+  end
+  
+  def sign_up(name, email, user_name, password)
+    sql = 'INSERT INTO users (name, email, user_name, password, logged_in) VALUES ($1, $2, $3, $4, $5)'
+    result_set = DatabaseConnection.exec_params(sql,[name, email, user_name, password, false])
+  end
+
+  def log_in(user_name, password)
+    sql = 'SELECT id, name, email, user_name, password FROM users WHERE user_name = $1;'
+    result_set = DatabaseConnection.exec_params(sql, [user_name])
+    user = User.new
+
+    user.id = result_set[0]['id']
+    user.name = result_set[0]['name']
+    user.email = result_set[0]['email']
+    user.user_name = result_set[0]['user_name']
+    user.password = result_set[0]['password']
+    user.logged_in = result_set[0]['logged_in']
+    
+
+    if user.password == password
+      change_log_in_sql = "UPDATE users SET logged_in=TRUE WHERE id=$1;"
+      result_set = DatabaseConnection.exec_params(change_log_in_sql,[user.id])
+      return "You are logged in"
+    else
+      return "Incorrect password or user name"
+    end
+  end
 end
+
+# Get the whole db record for the row that the username is in
+# Assign each to an instance of User.new
+# check user.password == password
+# if it is changed logged_in in db to true
+# else return 'password or username is incorrect
--- spec/user_repository_spec.rb
+++ spec/user_repository_spec.rb
@@ -11,12 +11,55 @@ describe UserRepository do
     reset_tables
   end
 
-  it "allows the user to sign up, checking the email or username does not
-  already exist" do
+  it "returns all users from the table" do
+    repo= UserRepository.new
+    users = repo.all
 
-  repo = UserRepository.new
-  repo.sign_up('Dave Jones', 'dave@madeup.com', 'BigDave', 'bigpassword')
+    expect(users[0].id).to eq "1"
+    expect(users[0].name).to eq "Adam Hoar"
+    expect(users[0].email).to eq "pretend@hotmail.com"
+    expect(users[0].user_name).to eq "adam1"
+    expect(users[0].password).to eq "password1"
+    expect(users[0].logged_in).to eq "f"
 
-  expect(repo.sign_up('Dave Jones', 'dave@madeup.com','BigDave', 'bigpassword')).to eq true
+    expect(users[1].id).to eq "2"
+    expect(users[1].name).to eq "Joe Bloggs"
+    expect(users[1].email).to eq "fake@icloud.com"
+    expect(users[1].user_name).to eq "joe1"
+    expect(users[1].password).to eq "pizza123"
+    expect(users[1].logged_in).to eq "f"
+
+    expect(users[2].id).to eq "3"
+    expect(users[2].name).to eq "John Doe"
+    expect(users[2].email).to eq "real@icloud.com"
+    expect(users[2].user_name).to eq "john3"
+    expect(users[2].password).to eq "makers1"
+    expect(users[2].logged_in).to eq "f"
+  end
+
+  it "inserts new user into users table" do
+    repo = UserRepository.new
+    repo.sign_up('Fake Name', 'notreal@hotmail.com', 'MD', '123')
+    users = repo.all
+
+    expect(users[-1].name).to eq 'Fake Name'
+    expect(users[-1].email).to eq 'notreal@hotmail.com'
+    expect(users[-1].user_name).to eq 'MD'
+    expect(users[-1].password).to eq '123'
+    expect(users[-1].logged_in).to eq 'f'
+  end
+
+  it "changes logged_in from false to true inside db" do
+    repo = UserRepository.new
+    first_users = repo.all
+    expect(repo.log_in('john3', 'makers1')).to eq "You are logged in"
+    second_users = repo.all
+    expect(second_users[-1].name).to eq "John Doe"
+    expect(second_users[-1].logged_in).to eq 't'
+  end
+
+  it "returns message for wrong user_name/password" do
+    repo = UserRepository.new
+    expect(repo.log_in('john3', 'makers')).to eq "Incorrect password or user name"
   end
 end
