09ccf263e1328688e7a00b447f6a9693e6901a74
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-01-13 09:57:23 +0000

    story

--- app.rb
+++ app.rb
@@ -7,16 +7,15 @@ require_relative "lib/replies"
 require_relative "lib/users"
 
 class Application < Sinatra::Base
-  enable :sessions
-
-  register Sinatra::ActiveRecordExtension
-
   configure :development do
+    enable :sessions
+    register Sinatra::ActiveRecordExtension
     register Sinatra::Reloader
   end
 
   get "/" do
-    @p = User.joins(:posts).select("users.*, posts.*")
+    @p = User.order("created_at DESC").joins(:posts).select("users.*, posts.*")
+
     return erb(:homepage)
   end
 
@@ -26,7 +25,7 @@ class Application < Sinatra::Base
 
   post "/login" do
     user = User.find_by(username: params[:username])
-    return erb(:signinfailed) if user == nil
+    return erb(:signinfailed) if user.nil?
     if BCrypt::Password.new(user.password_digest) == params[:password]
       session[:user_id] = user.id
       return erb(:signedin)
@@ -39,5 +38,44 @@ class Application < Sinatra::Base
     encrypted_password = BCrypt::Password.create(params[:password])
     User.create(first_name: params[:first_name], surname: params[:surname], email: params[:email], username: params[:username], password_digest: encrypted_password)
     return erb(:signedup)
+  end
+
+  post "/create-post" do
+    return redirect("/") if session[:user_id] == nil
+    Post.create(user_id: session[:user_id], content: params[:content])
+    redirect "/account"
+  end
+
+  get "/account" do
+    if session[:user_id] == nil
+      return redirect("/")
+    else
+      @p = User.order("created_at DESC").joins(:posts).select("users.*, posts.*")
+      return erb(:account)
+    end
+  end
+
+  get "/replies/:id" do
+    @t = User.joins(:posts, :replies).select("users.username, posts.content, replies.content")
+    return erb(:allreplies)
+  end
+
+  post "/reply" do
+    return redirect("/") if session[:user_id] == nil
+    Reply.create(user_id: session[:user_id], post_id: params[:post_id], content: params[:content])
+    return ""
+  end
+
+  post "/logout" do
+    session.clear
+    return erb(:logout)
+  end
+
+  private
+
+  def mail
+  end
+
+  def secure
   end
 end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -3,10 +3,94 @@ require "rack/test"
 require_relative "../../app"
 
 describe Application do
-  # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
-
-  # We need to declare the `app` value by instantiating the Application
-  # class so our tests work.
   let(:app) { Application.new }
+
+  context "GET /" do
+    it "returns homepage with 200" do
+      response = get("/")
+      expect(response.status).to eq 200
+      expect(response.body).to include("<h1>Chitter</h1>")
+    end
+
+    it "returns list of tweets in reverse order with 200" do
+      response = get("/")
+      expect(response.status).to eq 200
+      expect(response.body).to include("Time: 2023-01-12 07:29:56 UTC")
+    end
+
+    it "returns login and sign up forms" do
+      response = get("/")
+      expect(response.status).to eq 200
+      expect(response.body).to include('<form action="/login" method="POST">')
+      expect(response.body).to include('<input type="text" name="username" />')
+      expect(response.body).to include('<input type="text" name="password" />')
+    end
+
+    it "returns view replies hyperlink" do
+      response = get("/")
+      expect(response.status).to eq 200
+      expect(response.body).to include('<a href="/replies">View replies</a>')
+    end
+  end
+
+  context "GET /account" do
+    it "redirects to homepage when no session id" do
+      response = get("/account")
+      expect(response.status).to eq 302
+    end
+
+    it "if logged in sends to account page" do
+    end
+  end
+  context "GET /sign-up" do
+    it "returns sign up page forms with 200" do
+      response = get("/sign-up")
+      expect(response.status).to eq 200
+      expect(response.body).to include ('<input type="text" name="first_name" />')
+      expect(response.body).to include ('<input type="text" name="surname" />')
+      expect(response.body).to include ('<input type="text" name="email" />')
+      expect(response.body).to include ('<input type="text" name="username" />')
+      expect(response.body).to include ('<input type="text" name="password" />')
+    end
+  end
+
+  context "GET /replies" do
+  end
+
+  context "POST /reply" do
+  end
+
+  context "POST /sign-up" do
+    it "sends sign up form" do
+      response = post("/sign-up?first_name=testing&surname=testing&email=testing&username=testing&password=testing")
+      expect(response.status).to eq 200
+      expect(response.body).to include("<p>You're signed up to Chitter!</p>")
+    end
+  end
+
+  context "POST /login" do
+    it "logs in" do
+      response = post("/login?username=testing&password=testing")
+      expect(response.status).to eq 200
+      expect(response.body).to include("<p>You're signed in to Chitter!</p>")
+    end
+  end
+
+  context "POST /create-post" do
+    xit "creates post" do
+      t = post("/login?username=testing&password=testing")
+      response = post("/create-post?content=testing")
+      #expect(response.status).to eq 200
+      #expect(response.body).to include("Username: orhankhan1 Chit: testing Time: 2023-01-13 06:29:38 UTC")
+    end
+  end
+
+  context "POST /logout" do
+    it "logs out returns to homepage" do
+      response = post("/logout")
+      expect(response.status).to eq 200
+      expect(response.body).to include("<p>You're logged out!</p>")
+    end
+  end
 end
