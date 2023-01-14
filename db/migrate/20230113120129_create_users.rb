8c7349094d8d89144607a25027984de78e27288f
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-14 14:06:12 +0000

    Added user_id foreign key and drop_table statements to migrations

--- db/migrate/20230113120129_create_users.rb
+++ db/migrate/20230113120129_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
+    drop_table :users, if_exists: true
     create_table :users do |table|
       table.string :name
       table.string :email
--- db/migrate/20230114094543_create_cheeps.rb
+++ db/migrate/20230114094543_create_cheeps.rb
@@ -1,8 +1,10 @@
 class CreateCheeps < ActiveRecord::Migration[7.0]
   def change
+    drop_table :cheeps, if_exists: true
     create_table :cheeps do |table|
       table.string :content,
       table.timestamps
     end
+    add_reference :cheeps, :user, foreign_key: true
   end
 end
