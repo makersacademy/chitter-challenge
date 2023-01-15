f19c45f37739e6c40ec1561397ad5e1cb30054f8
Thomas McCoy <finnmccoy99@gmail.com>
2023-01-15 14:51:30 +0000

    Reverse chronological order of Cheep display is now tested

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,20 +3,30 @@ describe Chitter do
 
   let(:app) { Chitter.new }
 
-  def check_200(response)
-    expect(response.status).to eq 200
+  def check200
+    expect(@response.status).to eq 200
+  end
+
+  def check_order_within_body(reg1, reg2)
+    expect(@response.body =~ reg1).to be < (@response.body =~ reg2)
   end
 
   context "GET /" do
-    it "Returns HTML homepage with all Cheeps and 200 ok status" do
-      response = get("/")
-      check_200(response)
-      expect(response.body).to include("Posted at:").exactly(30).times
-      expect(response.body).to include(
+    it "Returns 200 ok status and HTML homepage with all Cheeps in reverse" \
+    "chronological order" do
+      @response = get("/")
+      check200
+      expect(@response.body).to include("Posted at:").exactly(30).times
+      expect(@response.body).to include(
         "<h1>Chitter</h1>",
-        "reinvent one-to-one relationships",
-        "seize innovative markets"
+        "productize robust relationships",
+        "embrace open-source architectures"
       )
+      check_order_within_body(
+        /embrace open-source architectures/,
+        /productize robust relationships/
+        )
+      
     end
   end
 end
