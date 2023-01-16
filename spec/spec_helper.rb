e4dc06478cd9f76b948370343cb4ddd66a03fddb
charlie lambert <char.lam@ntlworld.com>
2023-01-16 10:29:05 +0000

    DB now working

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,12 @@
 require 'simplecov'
 require 'simplecov-console'
 
+require 'database_connection'
+
+# Make sure this connects to your test database
+# (its name should end with '_test')
+DatabaseConnection.connect('chitter_test')
+
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
   # Want a nice code coverage website? Uncomment this next line!
