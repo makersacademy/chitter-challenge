d7286d642e24edaa2a407f651e220dc9f60961cf
Orhan Khanbayov <orhan.khanbayov@hotmail.co.uk>
2023-01-16 08:22:34 +0000

    heroku

--- config/enviroment.rb
+++ config/enviroment.rb
@@ -1,6 +1,24 @@
-require_relative "../app"
-
-ENV["RACK_ENV"] ||= "production"
-
 require "bundler/setup"
-Bundler.require(:default, ENV["RACK_ENV"])
+Bundler.require
+configure :development do
+  ENV["SINATRA_ENV"] ||= "development"
+  require "bundler/setup"
+  Bundler.require(:default, ENV["SINATRA_ENV"])
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV["SINATRA_ENV"]}.sqlite",
+  )
+end
+
+configure :production do
+  db = URI.parse(ENV["DATABASE_URL"] || "postgres:///localhost/mydb")
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == "postgres" ? "postgresql" : db.scheme,
+    :host => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => "utf8",
+  )
+end
