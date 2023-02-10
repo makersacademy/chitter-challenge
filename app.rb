07d93603c8cc7d07f5c58f717d1936f3a2a8a9e3
addauk <addauk@gmail.com>
2023-02-10 15:31:14 +0000

    Basic interface in place so you can view all conversations in reverse chronological order, view details of a peep including all replie, reply to individual peeps and create completely new peep/conversations. n.b No session control yet so app assumes you are the first user at all times.

--- app.rb
+++ app.rb
@@ -2,15 +2,96 @@
 require 'sinatra'
 require "sinatra/reloader"
 require_relative 'lib/database_connection'
+require_relative 'lib/maker_repository'
+require_relative 'lib/conversation_repository'
+require_relative 'lib/peep_repository'
+require_relative 'lib/tag_repository'
+
+require 'date'
 
 DatabaseConnection.connect
 
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
-    # TODO: see what repositories are needed
+    also_reload 'lib/maker_repository'
+    also_reload 'lib/conversation_repository'
+    also_reload 'lib/peep_repository'
     also_reload 'lib/tag_repository'
-    # also_reload 'lib/artist_repository'
+  end
+
+  get '/' do
+    return redirect('/conversations')
+  end
+
+  get '/conversations/:id' do
+    makerRepo = MakerRepository.new
+    peepRepo = PeepRepository.new
+    tagrepo = TagRepository.new
+    allPeeps = peepRepo.all
+    @makers = []
+    @peeps = allPeeps.select do |peep|
+      peep.conversation_id == params[:id].to_i
+    end
+    @peeps.each do |peep|
+      peep.created_datetime = pretty_time(peep.created_datetime)
+      @makers << makerRepo.find(peep.maker_id)
+    end
+    return erb(:conversation)
+  end
+
+  get '/conversations' do
+    convRepo = ConversationRepository.new
+    makerRepo = MakerRepository.new
+    peepRepo = PeepRepository.new
+    tagrepo = TagRepository.new
+    conversations = convRepo.all
+    allPeeps = peepRepo.all
+    @peeps = []
+    @makers = []
+    conversations.each do |conv|
+      @peeps << allPeeps.select do |peep|
+        peep.conversation_id == conv.id
+      end.first
+      @makers << makerRepo.find(conv.maker_id)
+    end
+    @peeps.each do |peep|
+      peep.created_datetime = pretty_time(peep.created_datetime)
+    end
+    @peeps.reverse!
+    @makers.reverse!
+    return erb(:conversations)
+  end
+
+  post '/conversations' do
+    convRepo = ConversationRepository.new
+    conv = Conversation.new
+    conv.maker_id = params[:maker_id]
+    convRepo.create(conv)
+    convid = convRepo.all.last.id
+    peepRepo = PeepRepository.new
+    peep = Peep.new
+    peep.content = params[:content]
+    peep.maker_id = params[:maker_id]
+    peep.conversation_id = convid
+    peepRepo.create(peep)
+    return redirect('/conversations')
+  end
+
+  post '/peeps' do
+    peepRepo = PeepRepository.new
+    peep = Peep.new
+    peep.content = params[:content]
+    peep.maker_id = params[:maker_id]
+    peep.conversation_id = params[:conv_id]
+    peepRepo.create(peep)
+    return redirect("/conversations/#{params[:conv_id]}")
+  end
+
+  private
+
+  def pretty_time(text)
+    return DateTime.parse(text).strftime("%k:%M %d/%m/%Y")
   end
 
 end
--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -10,4 +10,82 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
+  context 'GET /' do
+    it 'should currently redirect to /conversations' do
+      response = get('/')
+      expect(response.status).to eq(302)
+    end
+  end
+
+
+  context 'GET /conversations' do
+    it 'should display a list of all conversations in reverse chronological order of first peep' do
+      response = get('/conversations')
+      expect(response.status).to eq(200)
+      expect(response.body).to include("<h1>Welcome to Chitter</h1>")
+      expect(response.body).to include("Donna - Donut Peeped: ")
+      expect(response.body).to include("multi tag")
+      expect(response.body).to include('At: ')
+      expect(response.body).to include("Donna - Donut Peeped: ")
+      expect(response.body).to include("Clean for tags")
+      expect(response.body).to include('At: ')
+      expect(response.body).to include("Adam - Ad Peeped: ")
+      expect(response.body).to include("Second Peep")
+      expect(response.body).to include('At: ')
+      expect(response.body).to include("Adam - Ad Peeped: ")
+      expect(response.body).to include("First Peep")
+      expect(response.body).to include('At: ')
+    end
+
+    it 'should allow you to click into any peep to see all replies' do
+      response = get('/conversations')
+      expect(response.status).to eq(200)
+      expect(response.body).to include("At: <a href='conversations/1")
+      expect(response.body).to include("At: <a href='conversations/2")
+      expect(response.body).to include("At: <a href='conversations/3")
+      expect(response.body).to include("At: <a href='conversations/4")
+    end 
+
+    it 'should provide space to create a new peep' do
+      response = get('/conversations')
+      expect(response.status).to eq(200)
+      expect(response.body).to include('<form method="POST" action="/conversations">')
+      expect(response.body).to include('<input type="text" name="content" />')
+    end
+  end
+
+  context 'GET /conversations/:id' do
+    it 'should display a list of all peeps inside a conversation' do
+      response = get('/conversations/2')
+      expect(response.status).to eq(200)
+      expect(response.body).to include("Adam - Ad Peeped: ")
+      expect(response.body).to include("Second Peep")
+      expect(response.body).to include('At: ')
+      expect(response.body).to include("Bob - Big Peeped: ")
+      expect(response.body).to include("First reply")
+      expect(response.body).to include('At: ')
+    end
+  end
+
+  context 'POST /conversations' do
+    it 'should post a new conversation with the correct details' do
+      response = post(
+        '/conversations',
+        content:'this is a new peep'
+      )
+      response = get('/conversations')
+      expect(response.body).to include('this is a new peep')     
+    end
+  end
+
+  context 'POST /peeps' do
+    it 'should post a new peep within the current conversation' do
+      response = post(
+        '/peeps',
+        content:'this is a new reply'
+      )
+      expect(response.status).to eq(302)
+    end
+  end
+
 end
