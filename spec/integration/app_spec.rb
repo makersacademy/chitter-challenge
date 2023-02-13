cb3b38eba057fedca0a023162f12acb4436c1301
Luke Lai <lukelai.dev@gmail.com>
2023-02-13 09:54:17 +0000

    Fix bugs with css name

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -20,10 +20,10 @@ describe Application do
       response = get('/')
 
       expect(response.status).to eq 200
-      expect(response.body).to include '<a href="/sign-up">SIGN UP</a>'
-      expect(response.body).to include '<a href="/login">LOG IN</a>'
+      expect(response.body).to include 'SIGN UP'
+      expect(response.body).to include 'LOG IN'
       expect(response.body).to include 'First content'
-      expect(response.body).to include '<a href="/posts/1">2 comments</a>'
+      expect(response.body).to include '2 comments'
       expect(response.body).to include 'Second content'
       expect(response.body).to include 'Thrid content'
     end
@@ -44,7 +44,7 @@ describe Application do
       response = get('/sign-up')
 
       expect(response.status).to eq 200
-      expect(response.body).to include "<form class='sign-up' method='POST' action='/new-user'>"
+      expect(response.body).to include "<form class='form' method='POST' action='/new-user'>"
     end
   end
 
@@ -61,28 +61,28 @@ describe Application do
       response = post('new-user', email:'luke@gmail.com',password:'123456',username:'luke luke',name:'Luke123')
 
       expect(response.status).to eq 200
-      expect(response.body).to include '<p>No special characters or space allowed in username.</p>'
+      expect(response.body).to include '<p class="error_message">No special characters or space allowed in username.</p>'
     end
 
     it 'shows error message for invalid password length' do
       response = post('new-user', email:'luke@gmail.com',password:'123',username:'luke',name:'Luke123')
 
       expect(response.status).to eq 200
-      expect(response.body).to include '<p>Password must be at least 6 characters.</p>'
+      expect(response.body).to include '<p class="error_message">Password must be at least 6 characters.</p>'
     end
 
     it 'shows error message for exist username' do
       response = post('new-user', email:'luke@gmail.com',password:'123456',username:'luke',name:'Luke123')
 
       expect(response.status).to eq 200
-      expect(response.body).to include '<p>Username exists!</p>'
+      expect(response.body).to include '<p class="error_message">Username exists!</p>'
     end
 
     it 'shows error message for exist email' do
       response = post('new-user', email:'abc@gmail.com',password:'123456',username:'yoyo',name:'Yoyo')
 
       expect(response.status).to eq 200
-      expect(response.body).to include '<p>Email exists!</p>'
+      expect(response.body).to include '<p class="error_message">Email exists!</p>'
     end
   end
 
@@ -121,8 +121,8 @@ describe Application do
 
       # Verify that the form is displayed for the logged-in user
       expect(last_response.body).to include "<form method='POST' action='/new-post'>"
-      expect(last_response.body).to include '<textarea name="content" rows="4" cols="50" placeholder="Luke, What\'s happening?"></textarea>'        
-      expect(last_response.body).to include '<input type="submit">'
+      expect(last_response.body).to include '<textarea class="textarea" name="content" rows="4" cols="50" placeholder="Luke, What\'s happening?"></textarea>'        
+      expect(last_response.body).to include '<input class="btm " type="submit" value="Post">'
     end
 
     it 'posts a new post after user logins' do
@@ -139,7 +139,7 @@ describe Application do
       follow_redirect!
 
       # Verify that the form is displayed for the logged-in user
-      expect(last_response.body).to include '<textarea name="content" rows="4" cols="50" placeholder="Luke, What\'s happening?"></textarea>'        
+      expect(last_response.body).to include '<textarea class="textarea" name="content" rows="4" cols="50" placeholder="Luke, What\'s happening?"></textarea>'        
       # Stub the Time module to simulate 1 hour of elapsed time
       allow(Time).to receive(:now).and_return(Time.now + 3600)
       response = get('/')
@@ -155,8 +155,8 @@ describe Application do
       response = post('logout')
       expect(response.status).to eq 302
       follow_redirect!
-      expect(last_response.body).not_to include '<textarea name="content" rows="4" cols="50" placeholder="Luke, What\'s happening?"></textarea>'
-      expect(last_response.body).to include '<a href="/sign-up">SIGN UP</a>'
+      expect(last_response.body).not_to include '<textarea class="textarea" name="content" rows="4" cols="50" placeholder="Luke, What\'s happening?"></textarea>'
+      expect(last_response.body).to include '<a href="/sign-up" class="btm">SIGN UP</a>'
       expect(last_response.body).not_to include '<form method="POST" action="/logout">'
     end
   end
