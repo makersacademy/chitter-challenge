9f5b1fdec5d572937c6e2af417950babe10ffd0b
mirandaweston <mweston105@yahoo.co.uk>
2023-02-17 23:59:28 +0000

    Adding get '/welcome' having passed test

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
-require 'sinatra'
+require 'sinatra/base'
+require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require_relative './lib/user_repository'
 require_relative './lib/peep_repository'
@@ -10,6 +11,12 @@ DatabaseConnection.connect('chitter_database_test')
 
 class Application < Sinatra::Base
 
+  # enable :sessions
+
+  configure :development do
+    register Sinatra::Reloader
+  end
+
   def set_all_peeps
     peep_repo = PeepRepository.new
     user_repo = UserRepository.new
@@ -20,12 +27,12 @@ class Application < Sinatra::Base
     peeps.each do |peep|
       message = peep.message
       user = users.select { |user| user.id == peep.user_id}
-
+      if user[0]
       peep_data = {
         message: peep.message,
         time_posted: peep.time_posted,
         user_id: peep.user_id,
-        name: user[0].username,
+        name: user[0].name,
         email: user[0].email,
         pass_word: user[0].pass_word,
         username: user[0].username
@@ -33,44 +40,53 @@ class Application < Sinatra::Base
 
       @all_peeps << peep_data
     end
+    end
   end
 
   get '/' do
     set_all_peeps
-    erb(:index)
+    return erb(:index)
+  end
+
+  get '/welcome' do
+    return erb(:welcome)
   end
 
   get '/sign-up' do
-      erb(:sign_up)
+    return erb(:signup)
   end
 
   post '/sign-up' do
     user_repo = UserRepository.new
     new_user = User.new
+    new_user.id = params[:id]
+    new_user.name = params[:name]
+    new_user.username = params[:username]
+    new_user.email = params[:email]
+    new_user.pass_word = params[:pass_word]
+
+    user_repo.create(new_user)
+
+    return erb(:login)
   end
 
   get '/peep' do
-    erb(:peep)
+    return erb(:peep)
   end
 
   post '/peep' do
-    new_peep = Peep.new
-    peep_repo = PeepRepository.new
-    user_repo = UserRepository.new
-
-    new_peep.message = params[:message]
-    new_peep.time_posted = Time.now.strftime("%Y-%m-%d %H:%M:%S")
-
-    users = user_repo.all
-    user = users.select { |user| user.username == params[:username] }
-
-    if user.empty?
-      "No user found with username: #{params[:username]}"
-    else
-      new_peep.user_id = user[0].id
+    # if session[:user_id]
+      new_peep = Peep.new
+      peep_repo = PeepRepository.new
+  
+      new_peep.message = params[:message]
+      new_peep.time_posted = Time.now.strftime("%Y-%m-%d %H:%M:%S")
+      new_peep.user_id = session[:user_id]
+  
       peep_repo.create(new_peep)
-      set_all_peeps
-      erb(:index)
+      redirect '/'
+    else
+      redirect '/log-in'
     end
   end
 end
