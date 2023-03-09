2fafe160345e56e65da443d4979c64d2c2288b06
jpates1 <jamesandrewpates@gmail.com>
2023-03-09 13:39:37 +0100

    get / homepage test

--- app.rb
+++ app.rb
@@ -1,10 +1,19 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/database_connection'
+require_relative 'lib/peeps_repository'
+require_relative 'lib/users_repository'
+
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/peeps_repository'
+    also_reload 'lib/users_repository'
   end
+
+  
 end
--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -1,11 +1,34 @@
+# file: lib/database_connection.rb
+
 require 'pg'
 
-class DatabaseConnection
+# This class is a thin "wrapper" around the
+# PG library. We'll use it in our project to interact
+# with the database using SQL.
 
-  def self.connect(database_name)
+class DatabaseConnection
+  # This method connects to PostgreSQL using the 
+  # PG gem. We connect to 127.0.0.1, and select
+  # the database name given in argument.
+  def self.connect
+    # If the environment variable (set by Render)
+    # is present, use this to open the connection.
+    if ENV['DATABASE_URL'] != nil
+      @connection = PG.connect(ENV['DATABASE_URL'])
+      return
+    end
+  
+    if ENV['ENV'] == 'test'
+      database_name = 'chitter_test'
+    else
+      database_name = 'chitter'
+    end
     @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
   end
 
+  # This method executes an SQL query 
+  # on the database, providing some optional parameters
+  # (you will learn a bit later about when to provide these parameters).
   def self.exec_params(query, params)
     if @connection.nil?
       raise 'DatabaseConnection.exec_params: Cannot run a SQL query as the connection to'\
@@ -14,5 +37,4 @@ class DatabaseConnection
     end
     @connection.exec_params(query, params)
   end
-
 end
