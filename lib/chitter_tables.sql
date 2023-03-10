549bd0b7ecae3e498195e5a551fd1f3936e4d4d1
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-03-10 12:54:06 +0000

    seed data added to chitter_test db

--- lib/chitter_tables.sql
+++ lib/chitter_tables.sql
@@ -18,3 +18,17 @@ CREATE TABLE peeps (
     references users(id)
     on delete cascade
 );
+
+--NOTE: Password will be encrypted using hash algoritm SHA-256
+INSERT INTO users (name, username, password, email)
+VALUES ('John Doe', 'johndoe', 'password123', 'johndoe@example.com'),
+('Jane Smith', 'janesmith', 'abc123', 'janesmith@example.com'),
+('Bob Johnson', 'bobj', 'def456', 'bobj@example.com');
+
+INSERT INTO peeps (content, timestamp, tags, user_id)
+VALUES ('Just had the best burger ever!', '2022-09-01 12:30:00', 'food,burger', 1),
+('Can''t wait for the weekend!', '2022-09-02 16:45:00', 'weekend', 2),
+('Excited to start my new job!', '2022-09-03 09:00:00', 'work,newjob', 3),
+('Watching the game with friends tonight', '2022-09-04 20:15:00', 'sports', 1),
+('Trying out a new recipe for dinner', '2022-09-05 18:00:00', 'food,recipe', 2),
+('Just finished a great workout', '2022-09-06 07:30:00', 'fitness', 3);
