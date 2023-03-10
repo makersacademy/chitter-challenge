08eb02c6e04b1bbcc2b152c5e205eb8e3365429c
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-03-10 20:22:26 +0000

    write crud methods for peep_repository

--- lib/peep_repository.rb
+++ lib/peep_repository.rb
@@ -3,36 +3,67 @@ class PeepRepository
   # Selecting all records
   # No arguments
   def all
-    # Executes the SQL query:
-    # SELECT id, content, timestamp, user_id FROM peeps;
+    peeps = []
+    sql = 'SELECT id, content, timestamp, user_id FROM peeps;'
 
+    results = DatabaseConnection.exec_params(sql, [])
+
+    results.each do |record|
+      peep = peep.new
+      peep.id = record['id'].to_i
+      peep.content = record['content']
+      peep.timestamp = record['timestamp']
+      peep.user_id = record['user_id'].to_i
+      peeps << peep
+    end
     # Returns an array of Peep objects.
+    peeps
   end
 
   # Gets a single record by its ID
   # One argument: the id (number)
   def find(id)
     # Executes the SQL query:
-    # SELECT * FROM peeps WHERE id = $1;
+    sql = 'SELECT id, content, timestamp, user_id FROM peeps WHERE id = $1;'
+    result = DatabaseConnection.exec_params(sql, [id])
 
+    peep = Peep.new
+    peep.id = result[0]['id'].to_i
+    peep.content = result[0]['content']
+    peep.timestamp = result[0]['timestamp']
+    peep.user_id = result[0]['user_id'].to_i
     # Returns a single Peep object.
+    peep
   end
 
   def find_by_user_id(user_id)
+    peeps = []
     # Executes the SQL query:
-    # SELECT * FROM peeps WHERE user_id = $1;
-
+    sql = 'SELECT id, content, timestamp, user_id FROM peeps WHERE user_id = $1;'
+    result = DatabaseConnection.exec_params(sql, [user_id])
+    result.each do |record|
+      peep = Peep.new
+      peep.id = record['id'].to_i
+      peep.content = record['content']
+      peep.timestamp = record['timestamp']
+      peep.user_id = record['user_id'].to_i
+      peeps << peep
+    end
     # Returns an array of Peep objects associated to a user.
+    peeps
   end
-  # Add more methods below for each operation you'd like to implement.
 
   def post(peep)
     # Executes the SQL query:
-    # INSERT INTO peeps (content, timestamp, user_id) VALUES ($1, $2, $3)
+    sql = 'INSERT INTO peeps (content, timestamp, user_id) VALUES ($1, $2, $3)'
+    DatabaseConnection.exec_params(sql, [peep.content, peep.timestamp, peep.user_id])
+    peep
   end
 
   def delete(peep) # peep is a instance of Peep class
     # Executes the SQL query:
-    # DELETE FROM peeps WHERE id = $1 
+    sql = 'DELETE FROM peeps WHERE id = $1'
+    DatabaseConnection.exec_params(sql, [peep.id])
+    peep
   end
 end
