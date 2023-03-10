e98dee3efd5a72db54cef19f02ab379be9c4a3af
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-03-10 19:43:26 +0000

    set up files for classes

---
+++ lib/peep_repository.rb
@@ -0,0 +1,11 @@
+def reset_peeps_table
+  seed_sql = File.read('spec/seeds_peeps.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_test' })
+  connection.exec(seed_sql)
+end
+
+describe PeepRepository do
+  before(:each) do 
+    reset_peeps_table
+  end
+end
