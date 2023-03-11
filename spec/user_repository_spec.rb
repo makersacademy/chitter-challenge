1a6ea7fb381da9d3fa0d634443c742659314b247
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-11 18:15:53 +0000

    test to see if usernames are returned

--- spec/user_repository_spec.rb
+++ spec/user_repository_spec.rb
@@ -4,12 +4,20 @@ def reset_users_table
     connection = PG.connect({ host: '127.0.0.1', dbname: 'users' })
     connection.exec(seed_sql)
 end
-  
+
 describe UserRepository do
     before(:each) do 
       reset_users_table
     end
    # (your tests will go here).
 end
+RSpec.describe UserRepository do
+    context "test to see if users are returned" do
+        it "returns user_name" do
+            repo = UserRepository.new 
+            user = repo.all
+            expect(user).to eq ("")
+        end
 
-
+    end
+end
