8f0d2b59912613c24c340eb28bc0ee2141acdc74
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-11 17:14:49 +0000

    added require code and each method in test

--- spec/chitter_repository_spec.rb
+++ spec/chitter_repository_spec.rb
@@ -1,10 +1,29 @@
+require 'chitter_repository'
+require 'chitter'
+def reset_chitters_table
+    seed_sql = File.read('spec/seeds.sql')
+    connection = PG.connect({ host: '127.0.0.1', dbname: 'chitters' })
+    connection.exec(seed_sql)
+end
+  
+describe ChitterRepository do
+    before(:each) do 
+      reset_chitters_table
+    end
+  
+    # (your tests will go here).
+end
 
 RSpec.describe ChitterRepository do 
     context "tests all method" do
         it "returns all chitter data" do
             repo = ChitterRepository.new
             chitter = repo.all
-            expect(chitter).to eq (" ")
+            @contents = []
+            chitter.each do |content|
+                @contents.push(content.contents)
+            end
+            expect(@contents).to eq (" ")
         end
     end
 end
