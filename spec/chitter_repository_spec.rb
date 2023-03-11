e65bf77278e7b4f0e589798e1b386e29b3685d97
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-11 18:00:34 +0000

    added extra tests

--- spec/chitter_repository_spec.rb
+++ spec/chitter_repository_spec.rb
@@ -23,7 +23,45 @@ RSpec.describe ChitterRepository do
             chitter.each do |content|
                 @contents.push(content.contents)
             end
-            expect(@contents).to eq (" ")
+            expect(@contents).to eq ["Hey guys happy monday", "Happy monday to you too!", "Did you have any breakfast?", "Yea I had some chocolate"] 
+        end
+        it "returns all chitter dates" do
+            repo = ChitterRepository.new
+            chitter = repo.all
+            @contents = []
+            chitter.each do |content|
+                @contents.push(content.time)
+            end
+            expect(@contents).to eq ["1.13", "2.14", "3.25", "3.25"]
+        end
+        it "returns all chitter id" do
+            repo = ChitterRepository.new
+            chitter = repo.all
+            @id = []
+            chitter.each do |content|
+                @id.push(content.id)
+            end
+            expect(@id).to eq ["1", "2", "3", "4"]
+        end
+        it "returns all chitter user_id" do
+            repo = ChitterRepository.new
+            chitter = repo.all
+            @id = []
+            chitter.each do |content|
+                @id.push(content.user_id)
+            end
+            expect(@id).to eq ["1", "2", "3", "4"]
+        end
+
+    end
+    context "tests find method" do
+        it "returns exact chitter" do
+            repo = ChitterRepository.new
+            chitter = repo.find(2)
+            expect(chitter.contents).to eq("Happy monday to you too!")
+            expect(chitter.time).to eq("2.14")
+            expect(chitter.id).to eq("2")
         end
     end
+
 end
