590271fd16540b30ebc6ceb63e71087d37b6bed8
Valeria Serci <balerty84@gmail.com>
2023-03-12 22:09:09 +0000

    test drive show all peeps

--- lib/peep_repository.rb
+++ lib/peep_repository.rb
@@ -1,2 +1,21 @@
 class PeepRepository
+
+  def all
+    peeps = []
+    # Executes the SQL query:
+    sql = 'SELECT id, content, time, maker_id FROM peeps;'
+    result_set = DatabaseConnection.exec_params(sql, [])
+
+    result_set.each do |record|
+      peep = Peep.new
+      peep.id = record['id'].to_i
+      peep.content = record['content']
+      peep.time = record['time']
+      peep.maker_id = record['maker_id'].to_i
+
+      peeps << peep
+    end
+    return peeps
+  end
+
 end
--- spec/peep_repository_spec.rb
+++ spec/peep_repository_spec.rb
@@ -1,1 +1,36 @@
 require 'peep_repository'
+require 'peep'
+
+def reset_peeps_table
+  seed_sql = File.read('spec/seeds/test_seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_challenge_db_test' })
+  connection.exec(seed_sql)
+end
+
+describe PeepRepository do
+  before(:each) do
+    reset_peeps_table
+  end
+
+  it 'shows all peeps' do
+    repo = PeepRepository.new
+    peeps = repo.all
+    expect(peeps.length).to eq(3)
+    
+    expect(peeps[0].id).to eq(1)
+    expect(peeps[0].content).to eq('Content1')
+    expect(peeps[0].time).to eq('2023-02-14 10:27:15')
+    expect(peeps[0].maker_id).to eq(1)
+    
+    expect(peeps[1].id).to eq(2)
+    expect(peeps[1].content).to eq('Content2')
+    expect(peeps[1].time).to eq('2023-02-14 11:27:15')
+    expect(peeps[1].maker_id).to eq(2)
+
+    expect(peeps[2].id).to eq(3)
+    expect(peeps[2].content).to eq('Content3')
+    expect(peeps[2].time).to eq('2023-02-15 12:27:15')
+    expect(peeps[2].maker_id).to eq(1)
+  end
+end
+
