4faf5dcb2fb21a85d5d85ae3e8edcb508ffb143c
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-12 13:07:45 +0000

    created route to return blank page

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require_relative 'lib/user_repository'
 require_relative 'lib/chitter_repository'
+
 DatabaseConnection.connect
+
 class Application < Sinatra::Base 
     
     configure :development do
@@ -11,14 +13,9 @@ class Application < Sinatra::Base
         also_reload 'lib/user_repository'
         also_reload 'lib_chitter_repository'
     end
-    get '/peeps' do
-        repo = ChitterRepository.new
-        peep = repo.all
-        @contents = []
-        peep.each do |p|
-            @contents.push(p.contents)
-        end 
-        return @contents.reverse.join(', ')
+    get '/' do
+       
+        return erb(:index)
     end
 
 end
