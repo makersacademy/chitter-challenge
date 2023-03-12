ff63cc97eb86d2737d6eed3acf15eeb2fda5444e
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-12 09:44:09 +0000

    created all method for user repo with objects

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -3,6 +3,18 @@ class UserRepository
     # Selecting all records
     # No arguments
     def all
+            result_set = DatabaseConnection.exec_params('SELECT id, user_name, email, password FROM users;',[])
+            all_users = []
+            result_set.each do |a_user|
+              peep = User.new
+              peep.id = a_user['id']
+              peep.user_name = a_user['user_name']
+              peep.email = a_user['email']
+              peep.password = a_user['password']
+              all_users.push(peep)
+            end
+            return all_users
+          
       
     end
   
@@ -14,3 +26,4 @@ class UserRepository
   
       # Returns a single Book object.
     end
+end
