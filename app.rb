635077ae44df185df90a575847b21680dc1f978e
Dilan Patel <patel_dilan@hotmail.com>
2023-03-12 21:10:28 +0000

    Test-drive and implement basic GET '/' route; returns basic homepage

--- app.rb
+++ app.rb
@@ -5,12 +5,19 @@ require_relative 'lib/peep_repository'
 require_relative 'lib/user_repository'
 
 # We need to give the database name to the method `connect`.
-DatabaseConnection.connect('chitter')
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/peep_repository'
+    also_reload 'lib/user_repository'
   end
+
+  get '/' do
+    return erb(:homepage)
+  end
+
 end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -2,6 +2,12 @@ require "spec_helper"
 require "rack/test"
 require_relative '../../app'
 
+def reset_users_peeps_tables
+  seed_sql = File.read('spec/seeds_users_peeps.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_test' })
+  connection.exec(seed_sql)
+end
+
 describe Application do
   # This is so we can use rack-test helper methods.
   include Rack::Test::Methods
@@ -9,4 +15,27 @@ describe Application do
   # We need to declare the `app` value by instantiating the Application
   # class so our tests work.
   let(:app) { Application.new }
+
+  before(:each) do 
+    reset_users_peeps_tables
+  end
+  
+  context "GET /" do
+    it "returns homepage" do
+      response = get('/')
+
+      expect(response.status).to eq 200
+      expect(response.body).to include '<h1>Welcome to Chitter</h1>'
+    end
+  end
+
+  context "GET /peeps" do
+    it "returns all peeps in reverse chronological order" do
+      response = get('/peeps')
+
+      expect(response.status).to eq 200
+      expect(response.body).to include '<h1>Peep Board</h1>'
+    end
+  end
+
 end
