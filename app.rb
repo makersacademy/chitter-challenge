9b8ab1935a2e50b9afb30f34d5cf176aaf251b6a
Josh Neuwford <josh.c.gosling@gmail.com>
2023-03-13 07:10:30 +0000

    sinatra setup

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
-require 'date'
+require 'sinatra/base'
+require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require_relative 'lib/maker_repository'
+require_relative 'lib/peep_repository'
 
 DatabaseConnection.connect('chitter')
 
-p Time.now.round
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+    also_reload 'lib/maker_repository'
+    also_reload 'lib/peep_repository'
+  end
 
-repo = MakerRepository.new
-
-puts repo.all
+  get '/' do
+    return erb(:index)
+  end
+  
+end
