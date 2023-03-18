25f8f8ea12f4a1b2afe11cc72c7cac0f234c1dcc
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-18 12:26:47 +0000

    created method that resets table after test

--- spec/integration/application_spec.rb
+++ spec/integration/application_spec.rb
@@ -7,6 +7,14 @@ describe Application do
     include Rack::Test::Methods 
 
     let(:app) { Application.new}
+    def reset_tables
+        seed_sql = File.read('spec/seeds.sql')
+        connection = PG.connect({host: '127.0.0.1', dbname: 'chitters_database_test'})
+        connection.exec(seed_sql)
+    end
+    before(:each) do
+        reset_tables
+    end
     context 'test to see' do
         it "if chitter time and name is returned" do
             responses = get('/peeps')
