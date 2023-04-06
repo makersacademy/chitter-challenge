3c3fce0ba6341bd695eba61b95a3a3a8f2dceb1f
Sidra Iqbal <sidraiqbal@MASTUDE5GZHQ6L4.broadband>
2023-04-06 21:01:10 +0100

    set up Sinatra project

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
-# file: app.rb
-
+require 'sinatra'
+require "sinatra/reloader"
 require_relative 'lib/database_connection'
+require_relative 'lib/peep_repository'
+require_relative 'lib/user_repository'
 
-# We need to give the database name to the method `connect`.
-DatabaseConnection.connect('music_library')
+DatabaseConnection.connect('chitter_database')
 
-# Perform a SQL query on the database and get the result set.
-sql = 'SELECT id, title FROM albums;'
-result = DatabaseConnection.exec_params(sql, [])
-
-# Print out each record from the result set .
-result.each do |record|
-  p record
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
 end
---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,3 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
