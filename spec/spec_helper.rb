c87132a3b7e7ef426da92c6ea017d38ec0318286
Sidra Iqbal <sidraiqbal@MASTUDE5GZHQ6L4.broadband>
2023-04-06 12:42:40 +0100

    setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
+# file: spec/spec_helper.rb
+require 'database_connection'
 require 'simplecov'
 require 'simplecov-console'
+
+# Make sure this connects to your test database
+# (its name should end with '_test')
+DatabaseConnection.connect('your_database_name_test')
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
