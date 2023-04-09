52a309e6f35e3a9d42570a351088768838443983
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-09 12:23:53 +0100

    sinatra app setup

--- app.rb
+++ app.rb
@@ -1,12 +1,13 @@
 require_relative 'lib/database_connection'
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class Application < Sinatra::Base
+  # This allows the app code to refresh
+  # without having to restart the server.
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
 
 DatabaseConnection.connect('chitter')
-
-CREATE TABLE peeps (
-id SERIAL PRIMARY KEY,
-peep text,
-date timestamp,
-username_id int,
-constraint fk_username foreign key(username_id)
-references users(id)
-);
---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,19 @@
+require relative '../../app'
+require "spec_helper"
+require "rack/test"
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context 'GET to /' do
+    it 'displays all peeps in reverse chronological order' do
+      response = get('/')
+      expect(response.status).to eq 200
+      expect(response.body).to include('<body>')
+    end
+  end
