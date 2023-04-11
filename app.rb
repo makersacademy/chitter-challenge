2b33c4ce8afb8b295f6f3add07b6f6704836439e
Gary Solloway <124901657+GarySolloway@users.noreply.github.com>
2023-04-11 19:13:25 +0100

    Added missing code to app.rb

--- app.rb
+++ app.rb
@@ -13,6 +13,8 @@ class Application < Sinatra::Base
 
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/user_repository'
+    also_reload 'lib/user_repository'
   end
 
   get '/' do
