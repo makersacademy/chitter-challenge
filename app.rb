f06a8d892c27abe529272767cb8eb310181f2818
Gary Solloway <124901657+GarySolloway@users.noreply.github.com>
2023-04-11 19:15:20 +0100

    Fixed typo

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
     also_reload 'lib/user_repository'
-    also_reload 'lib/user_repository'
+    also_reload 'lib/peep_repository'
   end
 
   get '/' do
