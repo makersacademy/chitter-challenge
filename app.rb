31d5779fbd5afcba0305a04dbf004f6dc6f468d4
bookloverbabe <solmaz@hotmail.co.uk>
2023-04-12 10:42:01 +0100

    Final attempt

--- app.rb
+++ app.rb
@@ -9,15 +9,15 @@ class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
-#   post '/message' do
-#     #repo = MessageRepository.new
-#     new_message = Message.new
-#     new_message.time = params[:time].to_i
-#     new_message.date = params[:date]
-#     new_message.content = params[:content]
+  post '/message' do
+    new_message = Message.new
+    @message = new_message
+    new_message.time = params[:time].to_i
+    new_message.date = params[:date]
+    new_message.content = params[:content]
 
-#     return erb(new_message)
-#   end
+    return erb(new_message)
+  end
   # Confirm that new messages have been added to the database
   get '/message' do
     return erb(:message)
