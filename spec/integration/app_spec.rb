bed960dae2c8cae32ad2c3baebb805f984602c72
Sebastien Wylleman <31222029+sebwylleman@users.noreply.github.com>
2023-04-12 08:24:08 +0100

    first test failing for creating a new user

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -10,11 +10,25 @@ describe Application do
   # class so our tests work.
   let(:app) { Application.new }
 
-  context 'GET /signup' do
+  describe 'GET /signup' do
     it 'returns the signup page' do
       response = get('/signup')
       expect(response.status).to eq 200
       expect(response.body).to include '<input type="username" id="username" name="username" required>'
+    end
+  end
+  describe 'POST /signup' do
+    context 'when given valid user data' do
+      it 'creates a new user' do
+        response = post(
+          'post/signup',
+          username: 'user1',
+          email: 'user1@gmail.com',
+          password: '12345678'
+        )
+      expect(response.status).to eq(200)
+      expect(response.body).to change { UserRepository.new.count }.by(1)
+      end
     end
   end
   
