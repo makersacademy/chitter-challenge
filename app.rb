031398011879be2c5b74f5ebb31e07c974f74382
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-04-13 20:12:08 +0100

    Added post peep by form

--- app.rb
+++ app.rb
@@ -2,7 +2,9 @@ require 'sinatra/base'
 require "sinatra/reloader"
 
 require_relative "./lib/peep_repository"
+require_relative "./lib/peep"
 require_relative "./lib/user_repository"
+require_relative "./lib/user"
 
 require 'pg'
 require_relative './lib/database_connection'
@@ -21,13 +23,26 @@ class Application < Sinatra::Base
     @top_peeps = []
     repo = PeepRepository.new
     users = UserRepository.new
-    result = repo.all
+    result = repo.ordered
     result.each do |item|
       username = users.find_by_id(item.user_id)
       date = item.time_of_posting.slice!(0,16)
       @top_peeps << [item.content, date, username.username, item.user_id]
     end
     return erb(:index)
+  end
+
+  post '/' do
+    repo = PeepRepository.new
+    users = UserRepository.new
+    name = params[:username]
+    find_user_id = users.find_by_username(name)
+    user_id = find_user_id.id
+    content = params[:peep]
+    time = Time.now
+    new_peep = Peep.new(content, time, user_id)
+    repo.create(new_peep)
+    redirect '/'
   end
 
   get '/users/:id' do
@@ -38,14 +53,12 @@ class Application < Sinatra::Base
 
     @username = users.find_by_id(user_id).username
     result = repo.find(user_id)
+    
     result.each do |item|
       date = item.time_of_posting.slice!(0,16)
       @peeps << [item.content, date]
     end
-    return erb(:peeps)
-  end
 
-  post '/post_peep' do
-    #
+    return erb(:peeps)
   end
 end
