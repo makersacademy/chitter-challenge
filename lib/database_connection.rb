a58df1d4dff3b1baf592731ef026cb7a6f1834ea
Pablo <pablito@me.com>
2023-04-14 06:31:15 +0100

    DB connection set for local view

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -3,35 +3,37 @@ require 'pg'
 class DatabaseConnection
   # This method connects to PostgreSQL using the PG gem. We connect to 127.0.0.1
 
-  ################## RENDER connect method
-  def self.connect
-    # If the environment variable (set by Render)
-    # is present, use this to open the connection.
-    if ENV['DATABASE_URL'] != nil
-      @connection = PG.connect(ENV['DATABASE_URL'])
-      return
-    end
-  
-    if ENV['ENV'] == 'test'
-      database_name = 'chitter_test'
-    else
-      database_name = 'chitter_render_db'
-    end
-    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
-  end
+  # 👇🏻 ################# RENDER connect method
 
-
-
-
-  ################### local connect method - NOT for RENDER
   # def self.connect
+  #   # If the environment variable (set by Render)
+  #   # is present, use this to open the connection.
+  #   if ENV['DATABASE_URL'] != nil
+  #     @connection = PG.connect(ENV['DATABASE_URL'])
+  #     return
+  #   end
+  
   #   if ENV['ENV'] == 'test'
   #     database_name = 'chitter_test'
   #   else
-  #     database_name = 'chitter'
+  #     database_name = 'chitter_render_db'
   #   end
   #   @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
   # end
+
+
+
+
+  # 👇🏻 ################## local connect method - NOT for RENDER
+
+  def self.connect
+    if ENV['ENV'] == 'test'
+      database_name = 'chitter_test'
+    else
+      database_name = 'chitter'
+    end
+    @connection = PG.connect({ host: '127.0.0.1', dbname: database_name })
+  end
 
   def self.exec_params(query, params)
     if @connection.nil?
