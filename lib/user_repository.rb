b41415df90a119b000d99fee45a57daf3fb02986
Evan Thomas <evanthomas0292@gmail.com>
2023-05-05 17:28:11 +0100

    Write UserRepository#all and UserRepository#create

--- lib/user_repository.rb
+++ lib/user_repository.rb
@@ -1,10 +1,23 @@
 require_relative './user'
 
 class UserRepository
+  def all
+    sql = 'SELECT * FROM users;'
+    records = DatabaseConnection.exec_params(sql, [])
+    records.map { |record| create_user_from_record(record) }
+  end
+
   def find(id)
     sql = 'SELECT * FROM users WHERE id = $1'
     records = DatabaseConnection.exec_params(sql, [id])
     return create_user_from_record(records.first)
+  end
+
+  def create(user)
+    sql = 'INSERT INTO users (email, password, name, username)
+             VALUES ($1, $2, $3, $4)'
+    params = [user.email, user.password, user.name, user.username]
+    DatabaseConnection.exec_params(sql, params)
   end
 
   private
--- spec/user_repository_spec.rb
+++ spec/user_repository_spec.rb
@@ -11,6 +11,35 @@ describe UserRepository do
     reset_user_table
   end
 
+  describe '#all' do
+    it 'returns a list of all users' do
+      repo = UserRepository.new
+      users = repo.all
+
+      expect(users.length).to eq 4
+
+      expect(users).to include(
+        have_attributes(
+          id: 1,
+          email: 'email_1',
+          password: 'password_1',
+          name: 'name_1',
+          username: 'username_1'
+        )
+      )
+
+      expect(users).to include(
+        have_attributes(
+          id: 3,
+          email: 'email_3',
+          password: 'password_3',
+          name: 'name_3',
+          username: 'username_3'
+        )
+      )
+    end
+  end
+
   describe '#find' do
     it 'returns a user by id' do
       repo = UserRepository.new
@@ -30,6 +59,29 @@ describe UserRepository do
       expect(user.password).to eq 'password_3'
       expect(user.name).to eq 'name_3'
       expect(user.username).to eq 'username_3'
+    end
+  end
+
+  describe '#create' do
+    it "creates a new user in the database" do
+      user = User.new
+      user.email = 'new_email'
+      user.password = 'new_password'
+      user.name = 'new_name'
+      user.username = 'new_username'
+      
+      repo = UserRepository.new
+      repo.create(user)
+      
+      expect(repo.all).to include(
+        have_attributes(
+          id: 5,
+          email: 'new_email',
+          password: 'new_password',
+          name: 'new_name',
+          username: 'new_username'
+        )
+      )
     end
   end
 end
