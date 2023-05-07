8a4b6a5825ac66cf3334793a5a66627099d7b36b
awdem <wdemontmollin@gmail.com>
2023-05-07 20:15:33 +0100

    set up rspec to begin route block testing

---
+++ spec/integration/app_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+require "rack/test"
+require_relative "../../app"
+
+
+RSpec.describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  # tests go here
+end
