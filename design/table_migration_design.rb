618a13f975f860e4e85182e246842a28186095bc
awdem <wdemontmollin@gmail.com>
2023-05-07 17:54:22 +0100

    designed activerecord tables

---
+++ design/table_migration_design.rb
@@ -0,0 +1,41 @@
+# these are based on 
+# 1) database design 
+# 2) database design 
+# 3) database design 
+
+
+class PeepsTable <ActiveRecord::Migration[7.0]
+  def change
+    create_table :peeps do |t|
+      t.string :text
+      # below adds two columns that are automatically populated: created_at, updated_at
+      t.timestamps
+    end
+
+    add_foreign_key :peeps, :users, name: :fk_user,  on_delete: :cascade
+  end
+end
+
+class UsersTable <ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :password
+      t.string :email_address
+    end
+  end
+end 
+
+
+class TagsTable <ActiveRecord::Migration[7.0]
+  def change
+    create_table :tags do |t|
+      t.content :password
+    end
+  end
+end
+
+class PostsTagsJoinTable <ActiveRecord::Migration[7.0]
+  def change
+    create_join_table(:posts, :tags)
+  end
+end 
