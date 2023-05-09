e54671a3d5018fb8dea37a9b9674b1ff5163ac17
Aubrey Salmins <aubreysalmins@gmail.com>
2023-05-09 11:28:04 +0100

    Update seeds with test data

--- spec/seeds/chitter_tables.sql
+++ spec/seeds/chitter_tables.sql
@@ -15,3 +15,12 @@ CREATE TABLE peeps (
     references users(id)
     on delete cascade
 );
+
+TRUNCATE TABLE users RESTART IDENTITY CASCADE;
+TRUNCATE TABLE peeps RESTART IDENTITY CASCADE;
+
+INSERT INTO users (name, username, email, password) VALUES ('Aubrey Salmins', 'aubreysalmins', 'senditon@hotmail.co.uk', '123pswd');
+INSERT INTO users (name, username, email, password) VALUES ('Budsy', 'meow-meow', 'wherestheharringtons@feedme.com', 'pswd234');
+
+INSERT INTO peeps (title, content, user_id) VALUES ('Peep 1', 'Its a peep', 1);
+INSERT INTO peeps (title, content, user_id) VALUES ('Meep', 'Meow', 2);
