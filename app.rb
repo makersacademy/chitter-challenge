9cf70bae9a01e1bd42b62eb4684985f8a16df28f
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-09 16:14:04 +0100

    sinatra project setup

--- app.rb
+++ app.rb
@@ -1,3 +1,8 @@
-require_relative 'lib/database_connection'
+require 'sinatra/base'
+require 'sinatra/reloader'
 
-DatabaseConnection.connect('chitter_site')
+class Application < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+end
