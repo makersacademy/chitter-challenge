d7ae8c8d96686f0e373f56bfc777db9072ecd6b3
Caroline Evans <carolineevans261@gmail.com>
2023-05-10 16:03:44 +0100

    added sign-up page

--- app.rb
+++ app.rb
@@ -14,6 +14,8 @@ class Application < Sinatra::Base
     also_reload 'lib/peep_repository'
   end
 
+  # ------------- Homepage Route ----------------------------------------
+
   get '/' do
     peep_repo = PeepRepository.new
     @user_repo = UserRepository.new
@@ -22,17 +24,49 @@ class Application < Sinatra::Base
     return erb(:index)
   end
 
-  get'/signup' do
-    return "sign up page"
+  # ------------- Sign Up Routes ----------------------------------------
+
+  get '/signup' do
+    return erb(:signup)
+  end
+
+  post '/signup' do
+
+    users = UserRepository.new
+    
+    if users.all.any?{|user| user.email == params[:email]} then
+      status 400
+      return "Email address already signed up."
+    end
+
+    if users.all.any?{|user| user.username == params[:username]} then
+      status 400
+      return "Username already taken, please choose another."
+    end
+
+    if invalid_signup_params? || invalid_user_params? then
+      status 400
+      return 'Invalid credentials, please try again.'
+    end
+
+    user = User.new
+    user.name = params[:name]
+    user.email = params[:email]
+    user.username = params[:username]
+
+    users.create(user)
+
+    user = users.find_by_username(user.username)
+    redirect "/userpage/#{user.id}"
 
   end
 
+  # ------------- Peeps Routes ----------------------------------------
 
   get '/peeps' do
     peep_repo = PeepRepository.new
     @user_repo = UserRepository.new
     @peeps = peep_repo.all.sort_by!{|peep| peep.time}.reverse!
-
 
     return erb(:peeps)
   end
@@ -40,7 +74,7 @@ class Application < Sinatra::Base
   post '/peeps' do
     if invalid_peep_params? then
       status 400
-      return 'Invalid peep details'
+      return 'Invalid peep, please try again.'
     end
 
     peep = Peep.new
@@ -62,15 +96,24 @@ class Application < Sinatra::Base
   end
 
 
+  # ------------- User Page Routes ----------------------------------------
+
+
   post '/userpage' do
 
     if invalid_user_params? then
       status 400
-      return 'Invalid user details'
+      return 'Invalid user details.'
+    end
+
+    user_repo = UserRepository.new
+
+    if !user_repo.all.any?{|user| user.username == params[:username]} then
+      status 400
+      return "Username does not exist, please try again."
     end
 
     username = params[:username]
-    user_repo = UserRepository.new
 
     user = user_repo.find_by_username(username)
 
@@ -85,7 +128,7 @@ class Application < Sinatra::Base
     peep_repo = PeepRepository.new
 
     @user = @user_repo.find(user_id)
-    @peeps = peep_repo.find_by_owner(@user.id)
+    @peeps = peep_repo.find_by_owner(@user.id).sort_by!{|peep| peep.time}.reverse!
     @tagged_peeps = peep_repo.find_by_tagged_user(@user.id)
 
     return erb(:userpage)
@@ -99,6 +142,11 @@ class Application < Sinatra::Base
 
   def invalid_peep_params?
     params[:content] == nil ||  params[:content]== "" 
+  end
+
+  def invalid_signup_params?
+    params[:name] == nil ||  params[:name].match?(/[^a-zA-Z0-9 ]/) ||
+    params[:email] == nil ||  !params[:email].match?(/[@]/)
   end
 
 end
---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,226 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+
+def reset_test_database_tables
+  seed_sql = File.read('spec/seeds_chitter.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'chitter_test' })
+  connection.exec(seed_sql)
+end
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  before(:each) do 
+    reset_test_database_tables
+  end
+
+  # ------------- Homepage Route ----------------------------------------
+
+  context 'Homepage Route: ' do
+    it 'shows the two latest peeps' do
+      response = get('/')
+      expect(response.status).to eq 200
+
+      expect(response.body).to include '<h3> This is the forth Peep </h3>'
+      expect(response.body).to include '<h3> This is the third Peep </h3>'
+
+    end
+  end
+
+  # ------------- Sign Up Routes ----------------------------------------
+
+  context 'Sign Up Routes: ' do
+    it 'displays a form with entries for name, email, username, password' do
+      response = get('/signup')
+      expect(response.status).to eq 200
+
+      expect(response.body).to include '<form action="/signup" method="POST">'
+    end
+
+    it 'rejects a signup if an email address is already in the database' do
+      response = post('/signup',
+        name:'Test Name',
+        email:'carolinesemail@email.com',
+        username: 'testusername',
+        password: 'test')
+      expect(response.status).to eq 400
+
+      expect(response.body).to include 'Email address already signed up.'
+    end
+
+    it 'rejects a signup if a username is already in the database' do
+      response = post('/signup',
+        name:'Test Name',
+        email:'testemail@email.com',
+        username: 'caro',
+        password: 'test')
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Username already taken, please choose another.'
+    end
+
+    it 'rejects a signup if the name includes special characters' do
+      response = post('/signup',
+        name:'TestName!',
+        email:'testemail@email.com',
+        username: 'testusername',
+        password: 'test')
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Invalid credentials, please try again.'
+    end
+
+    it 'rejects a signup if the username includes special characters' do
+      response = post('/signup',
+        name:'TestName',
+        email:'testemail@email.com',
+        username: 'testusername!',
+        password: 'test')
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Invalid credentials, please try again.'
+    end
+
+    it 'rejects a signup if the email does not have an @ sign' do
+      response = post('/signup',
+        name:'TestName',
+        email:'testemailemail.com',
+        username: 'testusername',
+        password: 'test')
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Invalid credentials, please try again.'
+    end
+
+    it 'successfully adds a new user' do
+      response = post('/signup',
+        name:'TestName',
+        email:'testemail@email.com',
+        username: 'testusername',
+        password: 'test')
+      
+      expect(response.status).to eq 302
+      expect(response.original_headers['Location']).to include '/userpage/3'
+      
+      users = UserRepository.new.all
+
+      expect(users[2].id).to eq  3
+      expect(users[2].name).to eq  'TestName'
+      expect(users[2].email).to eq  'testemail@email.com'
+      expect(users[2].username).to eq 'testusername'
+    end
+
+  end
+
+  # ------------- Peeps Routes ----------------------------------------
+
+  context 'Peeps Routes' do
+    it 'displays all peeps' do
+      response = get('/peeps')
+      expect(response.status).to eq 200
+
+      expect(response.body).to include('<h3> This is the forth Peep </h3>')
+      expect(response.body).to include('<h3> This is the third Peep </h3>')
+      expect(response.body).to include('<h3> This is the second Peep </h3>')
+      expect(response.body).to include('<h3> This is the first Peep </h3>')
+
+    end
+
+    it 'rejects a peep post if content is empty' do
+      response = post('/peeps',
+        content:'',
+        user_id: 1)
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Invalid peep, please try again.'
+  
+    end
+
+    it 'creates a new peep' do
+      response = post('/peeps',
+        content:'Test Peep 1',
+        user_id: 1)
+
+      expect(response.status).to eq 200
+
+      peeps = PeepRepository.new.all
+
+      expect(peeps[4].id).to eq  5
+      expect(peeps[4].content).to eq  'Test Peep 1'
+      expect(peeps[4].user_id).to eq  1
+    
+    end
+
+  end
+
+ 
+  # ------------- User Page Routes ----------------------------------------
+
+  context 'User Page Routes: ' do
+    it 'rejects a log-in if password is empty' do
+      response = post('/userpage',
+        username:'caro',
+        password: '')
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Invalid user details.'
+
+    end
+
+    it 'rejects a log-in if password is empty' do
+      response = post('/userpage',
+        username:'caro',
+        password: '')
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Invalid user details.'
+
+    end
+
+    it 'rejects a log-in if username is not in the database' do
+      response = post('/userpage',
+        username:'test',
+        password: 'testing')
+      expect(response.status).to eq 400
+  
+      expect(response.body).to include 'Username does not exist, please try again.'
+
+    end
+
+    it 'directs a user to their userpage on a successful log-in' do
+      response = post('/userpage',
+        username:'caro',
+        password: 'test')
+      expect(response.status).to eq 302
+  
+      expect(response.original_headers['Location']).to include '/userpage/1'
+    end
+
+    it 'shows the users peeps' do
+      response = get('/userpage/1')
+      expect(response.status).to eq 200
+
+      expect(response.body).to include('<h3> This is the second Peep </h3>')
+      expect(response.body).to include('<h3> This is the first Peep </h3>')
+    end
+
+    it 'shows the peeps that the user has been tagged in' do
+      response = get('/userpage/2')
+      expect(response.status).to eq 200
+
+      expect(response.body).to include('<h3> This is the second Peep </h3>')
+      expect(response.body).to include('<h3> This is the first Peep </h3>')
+    end
+
+
+  end
+
+
+end
