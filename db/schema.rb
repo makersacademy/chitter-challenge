b15c3ff66443b076b443901f82d9de42839db058
awdem <wdemontmollin@gmail.com>
2023-05-10 16:39:12 +0100

    changed table schema: renamed users password column to password_hash

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_09_073506) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_153247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -31,7 +31,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_09_073506) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "password"
+    t.string "password_hash"
     t.string "email_address"
     t.string "name"
   end
