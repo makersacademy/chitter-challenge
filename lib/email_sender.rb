1e43f0fba3ffe0144c7d9578bef2d99dcd38c79c
Evan Thomas <evanthomas0292@gmail.com>
2023-05-11 17:49:17 +0100

    Refactor EmailSender

--- lib/email_sender.rb
+++ lib/email_sender.rb
@@ -14,24 +14,19 @@ class EmailSender
     end
   end
 
-  def send_email(
-    transactional_email_api=@email_api::TransactionalEmailsApi,
-    api_email_data=@email_api::SendSmtpEmail,
-    api_email_to=@email_api::SendSmtpEmailTo,
-    api_email_sender=@email_api::SendSmtpEmailSender
-  )
+  def send_email
 
-    api_instance = transactional_email_api.new
+    api_instance = @email_api::TransactionalEmailsApi.new
 
-    send_smtp_email = api_email_data.new
+    send_smtp_email = @email_api::SendSmtpEmail.new
     send_smtp_email.html_content = @html_message
     send_smtp_email.subject = "You've been tagged!"
     
-    email_to = api_email_to.new
+    email_to = @email_api::SendSmtpEmailTo.new
     email_to.email = @recipient
     send_smtp_email.to = [email_to]
     
-    email_from = api_email_sender.new
+    email_from = @email_api::SendSmtpEmailSender.new
     email_from.email = ENV['EMAIL_FROM']
     send_smtp_email.sender = email_from
     
