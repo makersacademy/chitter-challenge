5cfe10bbea956a25de38cea61aea1525ae2f3052
Caroline Evans <carolineevans261@gmail.com>
2023-05-11 17:24:59 +0100

    updated seed files and tests

--- spec/seeds_chitter.sql
+++ spec/seeds_chitter.sql
@@ -9,9 +9,10 @@ TRUNCATE TABLE peeps, users, peeps_users RESTART IDENTITY;
 -- Below this line there should only be `INSERT` statements.
 -- Replace these statements with your own seed data.
 
-INSERT INTO users (name, email, username, password) VALUES ('Caroline', 'carolinesemail@email.com', 'caro', 'pwtest1');
-INSERT INTO users (name, email, username, password) VALUES ('Philip', 'philsemail@email.com', 'phil', 'pwtest2');
+INSERT INTO users (name, email, username, password) VALUES ('Caroline', 'carolinesemail@email.com', 'caro', '$2a$12$RWbFtjHnA3kC2Gt31m7/l.N4f8ISipDp9T7KIyvSHhww/sGqohGHS');
+INSERT INTO users (name, email, username, password) VALUES ('Philip', 'philsemail@email.com', 'phil', '$2a$12$BV.yJVJm0fDOX0ikKe/6V.DjV5T4KqfMzWUh5B0Yd.QDIOGdyLNIK');
 
+-- the two passwords are 'pwtest1' and 'pwtest2'
 
 INSERT INTO peeps (content, time, user_id) VALUES ('This is the first Peep', '20230506 10:22:09 AM', 1);
 INSERT INTO peeps (content, time, user_id) VALUES ('This is the second Peep', '20230507 03:35:35 PM', 1);
