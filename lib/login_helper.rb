923ec7edd5f736c4ce99d5457bb6b32466a9930e
Louis Claremont <louisclaremont@MASTUDENT-Louis-Claremont-C17GMB3FQ6L4.local>
2023-05-11 11:47:43 +0100

    login helper implemented

--- lib/login_helper.rb
+++ lib/login_helper.rb
@@ -7,4 +7,20 @@ class LoginHelper
   def any_logged_in?
     return @users.any? { |user| user.logged_in }
   end
+
+  def login(user)
+    fail 'A user is already logged in' if any_logged_in?
+    user.logged_in = true
+  end
+
+  def logged_in_user
+    user = @users.select { |user| user.logged_in }[0]
+    return nil if user == nil
+    return user.id
+  end
+
+  def logout(user)
+    fail 'User is not logged in' if user.logged_in == false
+    user.logged_in = false
+  end
 end
--- spec/login_helper_spec.rb
+++ spec/login_helper_spec.rb
@@ -14,4 +14,55 @@ RSpec.describe LoginHelper do
 
     expect(result).to eq false
   end
+
+  describe 'login' do
+    it 'logs in a user' do
+      helper = LoginHelper.new(@users)
+      user_to_login = @users[0]
+      helper.login(user_to_login)
+
+      expect(user_to_login.logged_in).to eq true
+    end
+
+    it 'fails if a user already logged in' do
+      helper = LoginHelper.new(@users)
+      helper.login(@users[0])
+
+      expect { helper.login(@users[1]) }.to raise_error "A user is already logged in"
+    end
+  end
+
+  describe '#logged_in_user' do
+    it 'returns the ID of the logged in user' do
+      helper = LoginHelper.new(@users)
+      helper.login(@users[0])
+      result = helper.logged_in_user
+
+      expect(result).to eq(1)
+    end
+
+    it 'returns nil if no users logged in' do
+      helper = LoginHelper.new(@users)
+      result = helper.logged_in_user
+  
+      expect(result).to eq(nil)
+    end
+  end
+
+  describe 'logout' do
+    it 'logs out a user' do
+      helper = LoginHelper.new(@users)
+      user = @users[0]
+      helper.login(user)
+      helper.logout(user)
+
+      expect(user.logged_in).to eq false
+    end
+
+    it 'fails if the user is not logged in' do
+      helper = LoginHelper.new(@users)
+
+      expect { helper.logout(@users[0]) }.to raise_error "User is not logged in"
+    end
+  end
 end
