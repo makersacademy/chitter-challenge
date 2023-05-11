ef618c91864c09ea8a0cf4dac435844d45c82e32
hanguyen <lotusviet.uk@gmail.com>
2023-05-11 12:34:31 +0100

    Setup for deploy

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -30,7 +30,7 @@ describe Application do
     end
   end
 
-  context 'GET /peeps/:id' do
+   context 'GET /peeps/:id' do
     it 'returns the time at which peep was made' do 
       response = get('/peeps/1')
       expect(response.status).to eq(200)
@@ -48,6 +48,20 @@ describe Application do
     end
   end
 
+  context 'POST /sign_up' do
+    it 'create a new user' do 
+      response = post(
+        '/sign_up',
+        name: 'james',
+        username: 'jamesmith',
+        email: 'james@makers.com',
+        password: 'password'
+      )
+      expect(response.status).to eq(200)
+      expect(response.body).to include('You created a new user: jamesmith')
+    end
+  end
+  
   context 'GET /log_in' do
     it 'should return a form to fill the log in information' do 
       response = get('/log_in')
@@ -57,12 +71,58 @@ describe Application do
     end
   end
 
+  context 'POST /log_in' do
+        
+      it 'logs the user in and redirects to login_success page with valid credentials' do
+        response = post(
+          '/log_in',
+          password: 'password1',
+          email: 'david@makers.com'
+        )
+        expect(response.status).to eq(200)
+        expect(response.body).to include('<h1>You are sucessuful login</h1>')
+      end
+
+      it 'returns login_error with invalid credentials' do
+        response = post(
+          '/log_in',
+          password: 'password',
+          email: 'james@makers.com'
+        )
+        expect(response.status).to eq(200)
+        expect(response.body).to include("login_error")
+      end
+   
+  end 
+
+  context 'GET /log_out' do 
+   it 'logs the user out and redirects to login page' do
+    response = get('/log_out')
+    expect(response.status).to eq(200)
+    expect(response.body).to include('<h1 id="login-header">Login</h1>')
+   end
+  end
+
+
   context 'GET /new_peep' do
     it 'should return a form to create a new peep' do 
-      response = get('/new_peep')
+    response = get('/new_peep')
+    expect(response.status).to eq(200)
+    expect(response.body).to include("<h2>What's in your mind?</h2>")
+    expect(response.body).to include('form action="/new_peep" method="POST">')
+   end
+  end
+
+  context 'POST /new_peep' do
+    it 'create a new peep' do 
+      response = post(
+        '/new_peep',
+        content: 'what a sunny day',
+        time: Time.now,
+        user_id: 1
+      )
       expect(response.status).to eq(200)
-      expect(response.body).to include("<h2>What's in your mind?</h2>")
-      expect(response.body).to include('form action="/new_peep" method="POST">')
+      expect(response.body).to include('<h1>You created a new peep: what a sunny day</h1>')
     end
   end
 
