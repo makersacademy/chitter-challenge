b7e8f3b52900b8ce05118952b9f238239630800a
Sean Peters <sean@snptrs.dev>
2023-06-02 12:11:34 +0100

    Updated Peep object to also include maker_name

--- lib/peep.rb
+++ lib/peep.rb
@@ -1,3 +1,3 @@
 class Peep
-  attr_accessor :id, :content, :time, :maker_id
+  attr_accessor :id, :content, :time, :maker_id, :maker_name
 end
--- lib/peep_repository.rb
+++ lib/peep_repository.rb
@@ -4,19 +4,26 @@ class PeepRepository
   def all
     peeps = []
     
-    sql = 'SELECT id, content, time, maker_id FROM peeps;'
+    sql = 'SELECT makers.id AS maker_id,
+         makers.name AS maker_name,
+         makers.email AS maker_email,
+         peeps.id AS peep_id,
+         peeps.content AS peep_content,
+         peeps.time AS peep_time
+    FROM makers
+    JOIN peeps ON peeps.maker_id = makers.id;'
     result_set = DatabaseConnection.exec_params(sql, [])
     
     result_set.each do |record|
       peep = Peep.new
-      peep.id = record['id'].to_i
-      peep.content = record['content']
-      peep.time = record['time']
+      peep.id = record['peep_id'].to_i
+      peep.content = record['peep_content']
+      peep.time = record['peep_time']
       peep.maker_id = record['maker_id']
+      peep.maker_name = record['maker_name']
     
       peeps << peep
     end
-    
     return peeps
   end
   
