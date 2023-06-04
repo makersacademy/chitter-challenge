304b86fa26f3b33ed60b91dfddc2a3bd402b39af
Sean Peters <sean@snptrs.dev>
2023-06-04 10:35:58 +0100

    Create production SQL seeds

---
+++ spec/production_seeds.sql
@@ -0,0 +1,13 @@
+TRUNCATE TABLE makers, peeps RESTART IDENTITY; -- replace with your own table name.
+
+INSERT INTO makers (name, email, password) VALUES ('Sean', 'sean@makers.tech', '$2y$10$xkywp3MGsLUkWi3eN5KEEuW2VcMZ/Rhdj5m70Yg3qLwwIwO0qZP8m');
+INSERT INTO makers (name, email, password) VALUES ('Joe', 'joe@makers.tech', '$2y$10$xkywp3MGsLUkWi3eN5KEEuW2VcMZ/Rhdj5m70Yg3qLwwIwO0qZP8m');
+INSERT INTO makers (name, email, password) VALUES ('Eugene', 'eugene@makers.tech', '$2y$10$xkywp3MGsLUkWi3eN5KEEuW2VcMZ/Rhdj5m70Yg3qLwwIwO0qZP8m');
+
+INSERT INTO peeps (content, time, maker_id) VALUES ('You can''t compress the port without indexing the digital COM interface!', '2023-06-02 04:05:06', 1);
+INSERT INTO peeps (content, time, maker_id) VALUES ('Use the wireless DNS card, then you can copy the auxiliary matrix.', '2023-05-06 10:11:12', 2);
+INSERT INTO peeps (content, time, maker_id) VALUES ('If we generate the capacitor, we can get to the DRAM sensor through the redundant PNG bandwidth.', '2023-05-07 11:12:13', 3);
+INSERT INTO peeps (content, time, maker_id) VALUES ('You can''t generate the card without overriding the primary SSL panel!', '2023-05-08 12:13:14', 1);
+INSERT INTO peeps (content, time, maker_id) VALUES ('I''ll parse the virtual XML alarm, that should alarm the EXE port.', '2023-05-18 13:14:15', 2);
+INSERT INTO peeps (content, time, maker_id) VALUES ('Try to reboot the fireawall array, maybe it will navigate the primary circuit.', '2023-04-12 08:45:16', 3);
+
