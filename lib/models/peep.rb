835859f4ba59856fd44ba8ec0bf83612ada882c5
Yevhen Shakhrai <hemlokceo@gmail.com>
2023-06-04 14:03:51 +0100

    new method

--- lib/models/peep.rb
+++ lib/models/peep.rb
@@ -1,4 +1,11 @@
 class Peep
   attr_accessor :id, :content, :timestamp, :user_id
 
+  def author
+    UserRepository.find(user_id)
+  end
+
+  def tags
+    PeepTagRepository.find_by_peep(id)
+  end
 end
