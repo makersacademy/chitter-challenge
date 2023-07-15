eb6538718d2c269fa329b350a41e87419919e8ec
Pablo <pablito@me.com>
2023-07-15 14:40:25 +0100

    Updated tables and seeds

--- spec/crypt_seeds.sql
+++ spec/crypt_seeds.sql
@@ -7,13 +7,28 @@ INSERT INTO users ("name", "username", "email", "password") VALUES
 ;
 
 INSERT INTO peeps ("body", "time", "tags", "user_id") VALUES
-('Today I coded', '2023-04-01 13:00:00', '#code, #amber', 1),
-('Today I ate', '2023-04-02 14:00:00', '#food, #billy', 2),
-('Today I slept', '2023-04-03 15:00:00', '#sleep, #amber', 1),
-('Today I wept', '2023-04-04 16:00:00', '#cry, #caleb', 3),
-('Today I swam', '2023-04-05 17:00:00', '#swim, #amber', 1),
-('Today I meditated', '2023-04-06 18:00:00', '#meditate, #caleb', 3),
-('Today I relaxed', '2023-04-07 19:00:00', '#relax, #billy', 2)
+('Today I learned how to make SQL queries to get information from relational databases. It is so interesting! I cannnot wait to do something with this!', '2023-04-01 13:00:00', '#code, #render, #deploy, #amber', 1),
+('Today I ate a whole cheesecake by myself. It was really delicious but now I feel quite sick and full of cheesecake and regret.', '2023-04-02 14:00:00', '#food, #sick, #billy', 2),
+('Today I wrote a Chitter App so that I could share all my coding achievement with my friends!', '2023-04-03 15:00:00', '#code, #chitter, #amber', 1),
+('Today I watched some clouds drift by. It was quite beautiful.', '2023-04-04 16:00:00', '#dream, #caleb', 3),
+('Today I deployed my first app on Render with a PostgreSQL database.', '2023-04-05 17:00:00', '#new, #sql, #amber', 1),
+('Today I dreamed I was a cloud.', '2023-04-06 18:00:00', '#clouds, #caleb', 3),
+('Today I went to an all you can eat pizza place. The pizzas were pretty greasy and bland but it was all you can eat so I kept eating. The more I ate the more I felt a deep self-loathing but I could not stop and now I feel a deep sickness in my soul.', '2023-04-07 19:00:00', '#stuffed, #soul, #billy', 2)
 ;
 
--- All passwords are 'Password123'
+-- Ensure that your postgres server is accessable at the IP address 127.0.0.1.
+-- Create the postgreSQL databases for running and testing the project:
+-- ```bash
+-- createdb chitter
+-- createdb chitter_test
+-- ```
+-- Create the tables in the databases:
+-- ```bash 
+-- psql -h 127.0.0.1 chitter < spec/crypt_tables.sql
+-- psql -h 127.0.0.1 chitter_test < spec/tables.sql
+-- ```
+-- Populate the test database with seed data:
+-- ```bash
+-- psql -h 127.0.0.1 chitter < spec/crypt_seeds.sql
+-- psql -h 127.0.0.1 chitter_test < spec/seeds.sql
+-- ```
